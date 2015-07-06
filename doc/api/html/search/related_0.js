594b690f5a1c88c41266666c335b1f25bf205c2b
Daniel Lee <bearlee@alum.mit.edu>
2015-07-06 13:56:19 -0400

    fixes #13. Removing built documentation. [skip ci]

--- doc/api/html/search/related_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['operator_3c_3c',['operator&lt;&lt;',['../structstan_1_1math_1_1fvar.html#a2225724f72699f1d59e9aecc1b98f2b4',1,'stan::math::fvar::operator&lt;&lt;()'],['../classstan_1_1math_1_1var.html#a3545860122fa159c57e23dd0fbfbf5cd',1,'stan::math::var::operator&lt;&lt;()'],['../classstan_1_1math_1_1vari.html#abd24d4e4eb6b0c65c8447bd2cc73b200',1,'stan::math::vari::operator&lt;&lt;()']]]
-];
--- doc/api/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['chainablestack',['ChainableStack',['../namespacestan_1_1math.html#a947fbc8520096439705d2072a18b43c0',1,'stan::math']]]
-];
