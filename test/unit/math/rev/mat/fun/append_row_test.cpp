c7d89a7226203f81f3d65be3ef0917ca53e47a2e
Marco Inacio <marcoigarapava@gmail.com>
2015-07-07 17:21:47 -0300

    Change exp to square for more meaninful tests

--- test/unit/math/rev/mat/fun/append_row_test.cpp
+++ test/unit/math/rev/mat/fun/append_row_test.cpp
@@ -8,7 +8,7 @@
 #include <test/unit/math/rev/mat/fun/util.hpp>
 #include <stan/math/prim/mat/fun/sum.hpp>
 #include <stan/math/rev/mat/fun/sum.hpp>
-#include <stan/math/rev/scal/fun/exp.hpp>
+#include <stan/math/rev/scal/fun/square.hpp>
 #include <stan/math/rev/mat/fun/typedefs.hpp>
 
 using stan::math::sum;
@@ -20,12 +20,12 @@ using Eigen::VectorXd;
 
 TEST(AgradRevMatrix, append_row_matrix) {
   matrix_v a(2,2);
-  matrix_v a_exp(2,2);
+  matrix_v a_square(2,2);
   MatrixXd b(2,2);
-  
+
   a << 2.0, 3.0,
        9.0, -1.0;
-       
+
   b << 4.0, 3.0,
        0.0, 1.0;
 
@@ -33,42 +33,42 @@ TEST(AgradRevMatrix, append_row_matrix) {
   for (int i = 0; i < 2; ++i) {
     for (int j = 0; j < 2; ++j) {
       x.push_back(a(i,j));
-      a_exp(i, j) = stan::math::exp(a(i, j));
+      a_square(i, j) = stan::math::square(a(i, j));
     }
   }
-  
-  AVAR append_row_ab = sum(append_row(a_exp, b));
+
+  AVAR append_row_ab = sum(append_row(a_square, b));
 
   VEC g = cgradvec(append_row_ab, x);
-  
+
   size_t idx = 0;
   for (int i = 0; i < 2; i++)
     for (int j = 0; j < 2; j++)
-      EXPECT_FLOAT_EQ(std::exp(a(i, j).val()), g[idx++]);
+      EXPECT_FLOAT_EQ(a(i, j).val()*2.0, g[idx++]);
 }
 
 TEST(AgradRevMatrix, append_row_row_vector) {
   vector_v a(3);
-  vector_v a_exp(3);
+  vector_v a_square(3);
   VectorXd b(3);
-  
+
   a << 2.0, 3.0, 9.0;
-       
+
   b << 4.0, 3.0, 0.0;
 
   AVEC x;
   for (int i = 0; i < 3; ++i) {
     x.push_back(a(i));
-    a_exp(i) = stan::math::exp(a(i));
+    a_square(i) = stan::math::square(a(i));
   }
-  
-  AVAR append_row_ab = sum(append_row(a_exp, b));
+
+  AVAR append_row_ab = sum(append_row(a_square, b));
 
   VEC g = cgradvec(append_row_ab, x);
-  
+
   size_t idx = 0;
   for (int i = 0; i < 3; i++)
-    EXPECT_FLOAT_EQ(std::exp(a(i).val()), g[idx++]);
+    EXPECT_FLOAT_EQ(a(i).val()*2.0, g[idx++]);
 }
 
 template <typename T, int R, int C>
@@ -92,13 +92,13 @@ TEST(MathMatrix, append_row_different_types) {
   using stan::math::matrix_v;
   using stan::math::row_vector_v;
   using stan::math::vector_v;
-  using std::vector;  
-  
+  using std::vector;
+
   MatrixXd m33(3, 3);
   m33 << 1, 2, 3,
          4, 5, 6,
          7, 8, 9;
-        
+
   MatrixXd m32(3, 2);
   m32 << 11, 12,
          13, 14,
@@ -110,28 +110,28 @@ TEST(MathMatrix, append_row_different_types) {
   MatrixXd m23(2, 3);
   m23 << 21, 22, 23,
          24, 25, 26;
-         
+
   VectorXd v3(3);
-  v3 << 31, 
+  v3 << 31,
         32,
         33;
-        
+
   VectorXd v3b(3);
-  v3b << 34, 
+  v3b << 34,
          35,
          36;
 
   RowVectorXd rv3(3);
   rv3 << 41, 42, 43;
-  
+
   RowVectorXd rv3b(3);
   rv3b << 44, 45, 46;
-  
+
   MatrixXd vm33(3, 3);
   vm33 << 1, 2, 3,
          4, 5, 6,
          7, 8, 9;
-        
+
   MatrixXd vm32(3, 2);
   vm32 << 11, 12,
          13, 14,
@@ -143,20 +143,20 @@ TEST(MathMatrix, append_row_different_types) {
   MatrixXd vm23(2, 3);
   vm23 << 21, 22, 23,
          24, 25, 26;
-         
+
   VectorXd vv3(3);
-  vv3 << 31, 
+  vv3 << 31,
         32,
         33;
-        
+
   VectorXd vv3b(3);
-  vv3b << 34, 
+  vv3b << 34,
          35,
          36;
 
   RowVectorXd vrv3(3);
   vrv3 << 41, 42, 43;
-  
+
   RowVectorXd vrv3b(3);
   vrv3b << 44, 45, 46;
 
