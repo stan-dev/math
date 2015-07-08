019ec4f86cf6432a375de3a00fb7ca1f90f9f977
Rob Trangucci <robert.trangucci@gmail.com>
2015-07-08 07:52:32 -0400

    b f/i-48 cholesky finite diff and autodiff tests removed higher order tests

--- test/unit/math/mix/mat/fun/cholesky_decompose_test.cpp
+++ test/unit/math/mix/mat/fun/cholesky_decompose_test.cpp
@@ -10,9 +10,62 @@
 #include <stan/math/fwd/core.hpp>
 #include <stan/math/fwd/scal/fun/sqrt.hpp>
 #include <stan/math/rev/scal/fun/sqrt.hpp>
+#include <stan/math/rev/scal/fun/exp.hpp>
+#include <stan/math/rev/scal/fun/log.hpp>
 #include <stan/math/fwd/scal/fun/fabs.hpp>
 #include <stan/math/rev/scal/fun/fabs.hpp>
 #include <stan/math/prim/mat/fun/cholesky_decompose.hpp>
+#include <stan/math/prim/mat/fun/cov_matrix_constrain.hpp>
+#include <stan/math/rev/mat/functor/gradient.hpp>
+#include <stan/math/prim/mat/functor/finite_diff_gradient.hpp>
+
+struct chol_functor {
+  int i, j, K;
+  chol_functor(int i_, int j_, int K_) : i(i_), j(j_), K(K_) { }
+  template <typename T>
+  T operator()(Eigen::Matrix<T, -1, 1> x) const {
+    using stan::math::cov_matrix_constrain;
+    using stan::math::cholesky_decompose;
+    T lp(0.0);
+    Eigen::Matrix<T, -1, -1> x_c = cov_matrix_constrain(x, K, lp);
+    Eigen::Matrix<T, -1, -1> L = cholesky_decompose(x_c);
+    lp += L(i, j);
+    return lp;
+  }
+};
+
+void test_gradients(int size) {
+  std::vector<std::vector<chol_functor> > functowns;
+  std::vector<std::vector<Eigen::Matrix<double, -1, 1> > > grads_ad;
+  std::vector<std::vector<Eigen::Matrix<double, -1, 1> > > grads_fd;
+  Eigen::Matrix<double, -1, -1> evals_ad(size,size);
+  Eigen::Matrix<double, -1, -1> evals_fd(size,size);
+  functowns.resize(size);
+  grads_ad.resize(size);
+  grads_fd.resize(size);
+
+  for (int i = 0; i < size; ++i)
+    for (int j = 0; j < size; ++j) {
+      functowns[i].push_back(chol_functor(i, j, size));
+      grads_fd[i].push_back(Eigen::Matrix<double, -1, 1>(size));
+      grads_ad[i].push_back(Eigen::Matrix<double, -1, 1>(size));
+    }
+
+  int numels = size + size * (size - 1) / 2;
+  Eigen::Matrix<double, -1, 1> x(numels);
+  for (int i = 0; i < numels; ++i)
+    x(i) = i / 10.0;
+
+  for (size_t i = 0; i < static_cast<size_t>(size); ++i)
+    for (size_t j = 0; j < static_cast<size_t>(size); ++j) {
+      stan::math::gradient(functowns[i][j], x, evals_ad(i,j), grads_ad[i][j]);
+      stan::math::finite_diff_gradient(functowns[i][j], x,
+                                       evals_fd(i,j), grads_fd[i][j]);
+      for (int k = 0; k < numels; ++k) 
+        EXPECT_NEAR(grads_fd[i][j](k), grads_ad[i][j](k), 1e-11);
+      EXPECT_FLOAT_EQ(evals_fd(i, j), evals_ad(i, j));
+    }
+}
 
 TEST(AgradMixMatrixCholeskyDecompose, exception_mat_fv) {
   stan::math::matrix_fv m;
@@ -20,7 +73,7 @@ TEST(AgradMixMatrixCholeskyDecompose, exception_mat_fv) {
   m.resize(2,2);
   m << 1.0, 2.0, 
     2.0, 3.0;
-  EXPECT_NO_THROW(stan::math::cholesky_decompose(m));
+  EXPECT_THROW(stan::math::cholesky_decompose(m),std::domain_error);
 
   m.resize(0, 0);
   EXPECT_NO_THROW(stan::math::cholesky_decompose(m));
@@ -34,13 +87,14 @@ TEST(AgradMixMatrixCholeskyDecompose, exception_mat_fv) {
     3.0, 4.0;
   EXPECT_THROW(stan::math::cholesky_decompose(m), std::domain_error);
 }
+
 TEST(AgradMixMatrixCholeskyDecompose, exception_mat_ffv) {
   stan::math::matrix_ffv m;
   
   m.resize(2,2);
   m << 1.0, 2.0, 
     2.0, 3.0;
-  EXPECT_NO_THROW(stan::math::cholesky_decompose(m));
+  EXPECT_THROW(stan::math::cholesky_decompose(m),std::domain_error);
 
   m.resize(0, 0);
   EXPECT_NO_THROW(stan::math::cholesky_decompose(m));
@@ -56,130 +110,5 @@ TEST(AgradMixMatrixCholeskyDecompose, exception_mat_ffv) {
 }
 
 TEST(AgradMixMatrixCholeskyDecompose, mat_fv_1st_deriv) {
-  stan::math::matrix_fv m1(2,2);
-  m1 << 1, 2, 2, 4;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  using stan::math::cholesky_decompose;
-
-  stan::math::matrix_fv res = cholesky_decompose(m1);
-
-  EXPECT_FLOAT_EQ(1, res(0,0).val_.val());
-  EXPECT_FLOAT_EQ(0, res(0,1).val_.val());
-  EXPECT_FLOAT_EQ(2, res(1,0).val_.val());
-  EXPECT_FLOAT_EQ(4, res(1,1).val_.val());
-  EXPECT_FLOAT_EQ(0.5, res(0,0).d_.val());
-  EXPECT_FLOAT_EQ(0, res(0,1).d_.val());
-  EXPECT_FLOAT_EQ(0, res(1,0).d_.val());
-  EXPECT_FLOAT_EQ(1, res(1,1).d_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_,m1(0,1).val_,m1(1,0).val_,m1(1,1).val_);
-  VEC h;
-  res(0,0).val_.grad(z,h);
-  EXPECT_FLOAT_EQ(0.5,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(0.0,h[2]);
-  EXPECT_FLOAT_EQ(0.0,h[3]);
-}
-
-TEST(AgradMixMatrixCholeskyDecompose, mat_fv_2nd_deriv) {
-  stan::math::matrix_fv m1(2,2);
-  m1 << 1, 2, 2, 4;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  using stan::math::cholesky_decompose;
-
-  stan::math::matrix_fv res = cholesky_decompose(m1);
-
-  AVEC z = createAVEC(m1(0,0).val_,m1(0,1).val_,m1(1,0).val_,m1(1,1).val_);
-  VEC h;
-  res(0,0).d_.grad(z,h);
-  EXPECT_FLOAT_EQ(-0.25,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(0.0,h[2]);
-  EXPECT_FLOAT_EQ(0.0,h[3]);
-}
-
-TEST(AgradMixMatrixCholeskyDecompose, mat_ffv_1st_deriv) {
-  stan::math::matrix_ffv m1(2,2);
-  m1 << 1, 2, 2, 4;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  using stan::math::cholesky_decompose;
-
-  stan::math::matrix_ffv res = cholesky_decompose(m1);
-
-  EXPECT_FLOAT_EQ(1, res(0,0).val_.val_.val());
-  EXPECT_FLOAT_EQ(0, res(0,1).val_.val_.val());
-  EXPECT_FLOAT_EQ(2, res(1,0).val_.val_.val());
-  EXPECT_FLOAT_EQ(4, res(1,1).val_.val_.val());
-  EXPECT_FLOAT_EQ(0.5, res(0,0).d_.val_.val());
-  EXPECT_FLOAT_EQ(0, res(0,1).d_.val_.val());
-  EXPECT_FLOAT_EQ(0, res(1,0).d_.val_.val());
-  EXPECT_FLOAT_EQ(1, res(1,1).d_.val_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_.val_,m1(0,1).val_.val_,
-                      m1(1,0).val_.val_,m1(1,1).val_.val_);
-  VEC h;
-  res(0,0).val_.val_.grad(z,h);
-  EXPECT_FLOAT_EQ(0.5,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(0.0,h[2]);
-  EXPECT_FLOAT_EQ(0.0,h[3]);
-}
-
-TEST(AgradMixMatrixCholeskyDecompose, mat_ffv_2nd_deriv) {
-  stan::math::matrix_ffv m1(2,2);
-  m1 << 1, 2, 2, 4;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  using stan::math::cholesky_decompose;
-
-  stan::math::matrix_ffv res = cholesky_decompose(m1);
-
-  AVEC z = createAVEC(m1(0,0).val_.val_,m1(0,1).val_.val_,
-                      m1(1,0).val_.val_,m1(1,1).val_.val_);
-  VEC h;
-  res(0,0).d_.val_.grad(z,h);
-  EXPECT_FLOAT_EQ(-0.25,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(0.0,h[2]);
-  EXPECT_FLOAT_EQ(0.0,h[3]);
-}
-TEST(AgradMixMatrixCholeskyDecompose, mat_ffv_3rd_deriv) {
-  stan::math::matrix_ffv m1(2,2);
-  m1 << 1, 2, 2, 4;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-  m1(0,0).val_.d_ = 1.0;
-  m1(0,1).val_.d_ = 1.0;
-  m1(1,0).val_.d_ = 1.0;
-  m1(1,1).val_.d_ = 1.0;
-
-  using stan::math::cholesky_decompose;
-
-  stan::math::matrix_ffv res = cholesky_decompose(m1);
-
-  AVEC z = createAVEC(m1(0,0).val_.val_,m1(0,1).val_.val_,
-                      m1(1,0).val_.val_,m1(1,1).val_.val_);
-  VEC h;
-  res(0,0).d_.d_.grad(z,h);
-  EXPECT_FLOAT_EQ(0.375,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(0.0,h[2]);
-  EXPECT_FLOAT_EQ(0.0,h[3]);
+  test_gradients(3);
 }
