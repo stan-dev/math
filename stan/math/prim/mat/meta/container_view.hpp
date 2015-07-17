2a056908706ce9079be2523bbd8b000b9a16cd3a
Rob Trangucci <robert.trangucci@gmail.com>
2015-07-17 18:05:23 -0400

    b f/i-1449 doc changes and generalization of mat implementation

--- stan/math/prim/mat/meta/container_view.hpp
+++ stan/math/prim/mat/meta/container_view.hpp
@@ -10,118 +10,66 @@ namespace stan {
   namespace math {
 
     /**
-     * Template specialization for column vector view of
-     * array y with scalar type T2 with size inferred from
-     * input column vector x     
-     *
-     * operator[](int i) returns reference to view 
-     * broadcasts as if x is vector<Matrix>
-     *
-     * Intended for use in OperandsAndPartials
+     * Template specialization for Eigen::Map view of
+     * array with scalar type T2 with size inferred from
+     * input Eigen::Matrix    
      *
      * @tparam T1 scalar type of input matrix
-     * @tparam T2 scalar type returned by view.
-     * @param x input matrix
-     * @param y underlying array 
+     * @tparam T2 scalar type of view.
+     * @tparam R rows of input matrix and view
+     * @tparam C columns of input matrix and view
      */
-
-    template <typename T1, typename T2>
-    class container_view<Eigen::Matrix<T1, -1, 1>, Eigen::Matrix<T2, -1, 1> > {
+    template <typename T1, typename T2, int R, int C>
+    class container_view<Eigen::Matrix<T1, R, C>, Eigen::Matrix<T2, R, C> > {
       public:
-        container_view(const Eigen::Matrix<T1, -1, 1>& x, T2* y)
+        /**
+         * Initialize Map dimensions with input matrix
+         * dimensions
+         *
+         * @param x input matrix
+         * @param y underlying array 
+         */
+        container_view(const Eigen::Matrix<T1, R, C>& x, T2* y)
          : y_(y, x.rows(), x.cols()) { }
 
-        Eigen::Map<Eigen::Matrix<T2, -1, 1> >& operator[](int i) {
+        /**
+         * operator[](int i) returns Eigen::Map y
+         *
+         * @param i index
+         */
+        Eigen::Map<Eigen::Matrix<T2, R, C> >& operator[](int i) {
           return y_;
         }
       private:
-        Eigen::Map<Eigen::Matrix<T2, -1, 1> > y_;
+        Eigen::Map<Eigen::Matrix<T2, R, C> > y_;
     };
 
     /**
      * Template specialization for scalar view of
-     * array y with scalar type T2 
-     * input column vector x     
-     *
-     * operator[](int i) returns reference to scalar 
-     * of type T2 at appropriate index i in array y
-     *
-     * No bounds checking!
-     *
-     * Intended for use in OperandsAndPartials
+     * array y with scalar type T2
      *
      * @tparam T1 scalar type of input matrix
      * @tparam T2 scalar type returned by view.
-     * @param x input matrix
-     * @param y underlying array 
+     * @tparam R rows of input matrix and view
+     * @tparam C columns of input matrix and view
      */
 
-    template <typename T1, typename T2>
-    class container_view<Eigen::Matrix<T1, -1, 1>, T2> {
+    template <typename T1, typename T2, int R, int C>
+    class container_view<Eigen::Matrix<T1, R, C>, T2> {
       public:
-        container_view(const Eigen::Matrix<T1, -1, 1>& x, T2* y)
+        /** 
+         * Constructor
+         *
+         * @param x input matrix
+         * @param y underlying array 
+         */
+        container_view(const Eigen::Matrix<T1, R, C>& x, T2* y)
          : y_(y) { }
 
-        T2& operator[](int i) {
-          return y_[i];
-        }
-      private:
-        T2* y_;
-    };
-
-    /**
-     * Template specialization for row vector view of
-     * array y with scalar type T2 with size inferred from
-     * input row vector x     
-     *
-     * operator[](int i) returns reference to view 
-     * broadcasts as if x is vector<Matrix>
-     *
-     * Intended for use in OperandsAndPartials
-     *
-     * @tparam T1 scalar type of input matrix
-     * @tparam T2 scalar type returned by view.
-     * @param x input matrix
-     * @param y underlying array 
-     */
-
-    template <typename T1, typename T2>
-    class container_view<Eigen::Matrix<T1, 1, -1>, Eigen::Matrix<T2, 1, -1> > {
-      public:
-        container_view(const Eigen::Matrix<T1, 1, -1>& x, T2* y)
-         : y_(y, x.rows(), x.cols()) { }
-
-        Eigen::Map<Eigen::Matrix<T2, 1, -1> >& operator[](int i) {
-          return y_;
-        }
-      private:
-        Eigen::Map<Eigen::Matrix<T2, 1, -1> > y_;
-    };
-
-    /**
-     * Template specialization for scalar view of
-     * array y with scalar type T2 
-     * input row vector x     
-     *
-     * operator[](int i) returns reference to scalar 
-     * of type T2 at appropriate index i in array y
-     *
-     * No bounds checking!
-     *
-     * Intended for use in OperandsAndPartials
-     *
-     * @tparam T1 scalar type of input matrix
-     * @tparam T2 scalar type returned by view.
-     * @param x input matrix
-     * @param y underlying array 
-     */
-
-    template <typename T1, typename T2>
-    class container_view<Eigen::Matrix<T1, 1, -1>, T2> {
-      public:
-        container_view(const Eigen::Matrix<T1, 1, -1>& x, T2* y)
-         : y_(y) { }
-
+        /**
+         * operator[](int i) returns reference to scalar 
+         * of type T2 at appropriate index i in array y
+        */
         T2& operator[](int i) {
           return y_[i];
         }
@@ -131,56 +79,29 @@ namespace stan {
 
     /**
      * Template specialization for matrix view of
-     * array y with scalar type T2 with rows and columns
-     * inferred from input matrix x     
+     * array y with scalar type T2 with shape
+     * equal to x
      *
-     * operator[](int i) returns reference to view 
-     * broadcasts as if x is vector<Matrix>
-     *
-     * Intended for use in OperandsAndPartials
-     *
-     * @tparam T1 scalar type of input matrix
-     * @tparam T2 scalar type returned by view.
-     * @param x input matrix
-     * @param y underlying array 
+     * @tparam T1 scalar type of input vector of matrices
+     * @tparam T2 scalar type of matrix view
+     * @tparam R rows of input matrix and view
+     * @tparam C columns of input matrix and view
      */
-
-    template <typename T1, typename T2, int M, int N>
-    class container_view<Eigen::Matrix<T1, M, N>, Eigen::Matrix<T2, M, N> > {
+    template <typename T1, typename T2, int R, int C>
+    class container_view<std::vector<Eigen::Matrix<T1, R, C> >,
+                          Eigen::Matrix<T2, R, C> > {
       public:
-        container_view(const Eigen::Matrix<T1, M, N>& x, T2* y)
-         : y_(y, x.rows(), x.cols()) { }
-
-        Eigen::Map<Eigen::Matrix<T2, M, N> >& operator[](int i) {
-          return y_;
-        }
-      private:
-        Eigen::Map<Eigen::Matrix<T2, M, N> > y_;
-    };
-
-    /**
-     * Template specialization for matrix view of
-     * array y with scalar type T2 with proper indexing
-     * inferred from input vector of matrices x     
-     *
-     * operator[](int i) returns reference to view 
-     * as if indexed at i 
-     *
-     * No bounds checking!
-     *
-     * Intended for use in OperandsAndPartials
-     *
-     * @tparam T1 scalar type of input matrix
-     * @tparam T2 scalar type returned by view.
-     * @param x input matrix
-     * @param y underlying array 
-     */
-
-    template <typename T1, typename T2, int M, int N>
-    class container_view<std::vector<Eigen::Matrix<T1, M, N> >,
-                          Eigen::Matrix<T2, M, N> > {
-      public:
-        container_view(const std::vector<Eigen::Matrix<T1, M, N> >& x, T2* y)
+        /** 
+         * Constructor assumes all matrix elements in 
+         * std::vector are of same dimension
+         *
+         * Initializes y_view as 1x1 matrix because no
+         * nullary constructor for Eigen::Map
+         *
+         * @param x input matrix
+         * @param y underlying array 
+         */
+        container_view(const std::vector<Eigen::Matrix<T1, R, C> >& x, T2* y)
          : y_view(y, 1, 1), y_(y) {
            if (x.size() > 0) {
              rows = x[0].rows();
@@ -191,14 +112,18 @@ namespace stan {
            }
          }
 
-        Eigen::Map<Eigen::Matrix<T2, M, N> >& operator[](int i) {
+        /**
+         * operator[](int i) returns matrix view  
+         * of scalartype T2 at appropriate index i in array y
+        */
+        Eigen::Map<Eigen::Matrix<T2, R, C> >& operator[](int i) {
           int offset = i * rows * cols;
-          new (&y_view) Eigen::Map<Eigen::Matrix<T2, M, N> >
+          new (&y_view) Eigen::Map<Eigen::Matrix<T2, R, C> >
             (y_ + offset, rows, cols);
           return y_view;
         }
       private:
-        Eigen::Map<Eigen::Matrix<T2, M, N> > y_view;
+        Eigen::Map<Eigen::Matrix<T2, R, C> > y_view;
         T2* y_;
         int rows;
         int cols;
--- stan/math/prim/scal/meta/container_view.hpp
+++ stan/math/prim/scal/meta/container_view.hpp
@@ -7,31 +7,35 @@ namespace stan {
 
   namespace math {
 
-    struct dummy { };
 
     /**
      * Primary template class for container view of
      * array y with same structure as T1 and 
      * size as x
      *
-     * operator[](int i) returns reference to view, 
-     * indexed by i
-     * Specializations handle appropriate broadcasting
-     * if size of x is 1
-     *
-     * Intended for use in OperandsAndPartials
-     *
      * @tparam T1 type of view.
      * @tparam T2 type of scalar returned by view.
-     * @param x object from which size is to be inferred
-     * @param y underlying array 
      */
 
     template <typename T1, typename T2>
     class container_view {
       public:
+      /**
+       * Constructor
+       *
+       * @param x object from which size is to be inferred
+       * @param y underlying array 
+       */
         container_view(const T1& x, T2* y) : y_(y) { }
 
+        /**
+         * operator[](int i) returns reference to view, 
+         * indexed by i
+         * Specialization handle appropriate broadcasting
+         * if size of x is 1
+         *
+         * @param i index
+         */
         T2& operator[](int i) {
           return y_[0];
         }
@@ -40,23 +44,40 @@ namespace stan {
     };
 
     /**
-     * Intended for usage in OperandsAndPartials
+     * Empty struct for use in 
+     * boost::condtional<is_constant_struct<T1>::value, T1, dummy>::type
+     * as false condtion for safe indexing 
      *
-     * operator[](int i) 
-     * throws exception
-     *
-     * @tparam T1 type of x
-     * @tparam T2 type of scalar returned by view
-     * @param x object from which size is to be inferred
-     * @param y underlying array 
      */
+    struct dummy { };
 
+    /**
+     * Dummy type specialization, used in
+     * conjunction with struct dummy as 
+     * described above
+     *
+     * @tparam T2 type of scalar returned by view
+     */
     template <typename T2>
     class container_view<dummy, T2> {
       public:
         typedef typename stan::scalar_type<T2>::type scalar_t;
         template <typename T1>
+
+        /**
+         * Nothing initialized
+         *
+         * @param x input object
+         * @param y underlying array 
+         */
         container_view(const T1& x, scalar_t* y) { }
+
+        /**
+         * operator[](int i) 
+         * throws exception
+         *
+         * @param n index
+         */
         scalar_t operator[](int n) const {
           throw std::out_of_range("can't access dummy elements.");
         }
