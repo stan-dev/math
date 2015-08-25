cfddd1ee50d77ef287afa4782a8fe588e4bc223c
Ben Goodrich <goodrich.ben@gmail.com>
2015-08-25 17:17:13 -0400

    improve tests

--- test/unit/math/fwd/scal/fun/inv_Phi_test.cpp
+++ test/unit/math/fwd/scal/fun/inv_Phi_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <stan/math/fwd/scal/fun/Phi.hpp>
 #include <stan/math/fwd/scal/fun/inv_Phi.hpp>
 #include <stan/math/prim/scal/fun/inv_Phi.hpp>
 #include <stan/math/prim/scal/prob/normal_log.hpp>
@@ -9,38 +10,30 @@
 TEST(AgradFwdinv_Phi,Fvar) {
   using stan::math::fvar;
   using stan::math::inv_Phi;
+  using stan::math::Phi;
+
   fvar<double> x = 0.1;
   x.d_ = 1.0;
 
-  fvar<double> inv_Phi_x = inv_Phi(x);
+  fvar<double> y = Phi(inv_Phi(x));
 
-  EXPECT_FLOAT_EQ(inv_Phi(0.1), inv_Phi_x.val_);
-  EXPECT_FLOAT_EQ(1.0 / exp(stan::math::normal_log<false>(inv_Phi_x.val_,0.0,1.0)),
-                  inv_Phi_x.d_);
+  EXPECT_FLOAT_EQ(0.1, y.val_);
+  EXPECT_FLOAT_EQ(1.0, y.d_);
 }
 TEST(AgradFwdinv_Phi, FvarFvarDouble) {
   using stan::math::fvar;
   using stan::math::inv_Phi;
+  using stan::math::Phi;
 
   fvar<fvar<double> > x;
   x.val_.val_ = 0.1;
   x.val_.d_ = 1.0;
 
-  fvar<fvar<double> > a = inv_Phi(x);
+  fvar<fvar<double> > a = Phi(inv_Phi(x));
 
-  EXPECT_FLOAT_EQ(inv_Phi(0.1), a.val_.val_);
-  EXPECT_FLOAT_EQ(1.0 / exp(stan::math::normal_log<false>(a.val_.val_,0.0,1.0)), a.val_.d_);
+  EXPECT_FLOAT_EQ(0.1, a.val_.val_);
+  EXPECT_FLOAT_EQ(1.0, a.val_.d_);
   EXPECT_FLOAT_EQ(0, a.d_.val_);
-  EXPECT_FLOAT_EQ(0, a.d_.d_);
-
-  fvar<fvar<double> > y;
-  y.val_.val_ = 0.1;
-  y.d_.val_ = 1.0;
-
-  a = inv_Phi(y);
-  EXPECT_FLOAT_EQ(inv_Phi(0.1), a.val_.val_);
-  EXPECT_FLOAT_EQ(0, a.val_.d_);
-  EXPECT_FLOAT_EQ(1.0 / exp(stan::math::normal_log<false>(a.val_.val_,0.0,1.0)), a.d_.val_);
   EXPECT_FLOAT_EQ(0, a.d_.d_);
 }
 
--- test/unit/math/rev/scal/fun/inv_Phi_test.cpp
+++ test/unit/math/rev/scal/fun/inv_Phi_test.cpp
@@ -1,4 +1,5 @@
 #include <stan/math/rev/scal/fun/inv_Phi.hpp>
+#include <stan/math/rev/scal/fun/Phi.hpp>
 #include <test/unit/math/rev/mat/fun/util.hpp>
 #include <gtest/gtest.h>
 #include <test/unit/math/rev/scal/fun/nan_util.hpp>
@@ -11,22 +12,16 @@ TEST(AgradRev, inv_Phi) {
   p_values.push_back(0.5);
   p_values.push_back(0.75);
 
-  // d/dp = sqrt(2 * pi) / exp(normal_log(value_of(x), 0.0, 1.0))
-  std::vector<double> dp_values;
-  dp_values.push_back(5.69805985611701);
-  dp_values.push_back(2.506628274631);
-  dp_values.push_back(3.14686508056109);
-
   for (size_t i = 0; i < p_values.size(); i++) {
-    var p, inv_Phi_p;
+    var p, y;
     AVEC x;
     VEC dp;
     p = p_values[i];
-    inv_Phi_p = stan::math::inv_Phi(p);
+    y = stan::math::Phi(stan::math::inv_Phi(p));
     x = createAVEC(p);
-    inv_Phi_p.grad(x,dp);
-    EXPECT_FLOAT_EQ(stan::math::inv_Phi(p.val()), inv_Phi_p.val());
-    EXPECT_FLOAT_EQ(dp_values[i], dp[0])
+    y.grad(x,dp);
+    EXPECT_FLOAT_EQ(p_values[i], y.val());
+    EXPECT_FLOAT_EQ(1.0, dp[0])
       << "p = " << p;
   }
 }
