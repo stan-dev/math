bcee5757d0472b41fa434e06fb9ec80c677c761d
Daniel Lee <bearlee@alum.mit.edu>
2015-09-01 14:35:43 -0400

    fixes #164. Removing built documentation. [skip ci]

--- doc/api/html/search/classes_f.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['welford_5fcovar_5festimator',['welford_covar_estimator',['../classstan_1_1math_1_1welford__covar__estimator.html',1,'stan::math']]],
-  ['welford_5fvar_5festimator',['welford_var_estimator',['../classstan_1_1math_1_1welford__var__estimator.html',1,'stan::math']]]
-];
