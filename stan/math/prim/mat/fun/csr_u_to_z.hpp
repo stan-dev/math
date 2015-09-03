9895ee1dcf79b18f67e10009b6d68e135229240b
Bob Carpenter <carp@alias-i.com>
2015-09-03 15:20:33 -0400

    remove z from csr sparse mats, complete #171

--- stan/math/prim/mat/fun/csr_u_to_z.hpp
+++ stan/math/prim/mat/fun/csr_u_to_z.hpp
@@ -1,37 +1,30 @@
 #ifndef STAN_MATH_PRIM_MAT_FUN_CSR_U_TO_Z
 #define STAN_MATH_PRIM_MAT_FUN_CSR_U_TO_Z
 
+#include <stan/math/prim/mat/err/check_range.hpp>
+#include <stan/math/prim/scal/err/check_positive.hpp>
 #include <stdexcept>
 #include <vector>
 
 namespace stan {
   namespace math {
 
-    /**
-     * Convert the u data structure to the z data structure for CSR
-     * storage of matrices.
-     *
-     * @param[in] u List of row starts.
-     * @return List of non-zero entries in rows.
-     * @throws If list of indexes is empty.
+    /** \addtogroup csr_format 
+     *  @{
      */
-    vector<int> csr_u_to_z(const vector<int>& u) {
-      if (u.size() == 0)
-        throw std::domain_error("csr_u_to_z: u.size() == 0,"
-                                " but must be positive");
-      vector<int> z(u.size() - 1);
-      for (size_t i = 0; i < z.size(); ++i)
-        z[i] = u[i + 1] - u[i];
-      return z;
-    }
 
-    void print_vec(const vector<int>& x, const std::string& name) {
-      std::cout << std::endl
-                << name << ".size()=" << x.size()
-                << std::endl;
-      for (size_t i = 0; i < x.size(); ++i)
-        std::cout << name << "[" << i << "]=" << x[i] << std::endl;
-      std::cout << std::endl;
+    /**
+     * Return the z vector computed from the specified u vector at the
+     * index for the z vector.
+     *
+     * @param[in] u U vector.
+     * @param[in] i Index into resulting z vector.
+     * @return z[i] where z is conversion from u.
+     */
+    int csr_u_to_z(const std::vector<int>& u, int i) {
+      check_positive("csr_u_to_z", "u.size()", u.size());
+      check_range("csr_u_to_z", "i", u.size(), i + 1, "index out of range");
+      return u[i + 1] - u[i];
     }
 
   }
