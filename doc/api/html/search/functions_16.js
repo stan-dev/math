e20b86049418f1fc9f18803029d17eb5e92079cf
Daniel Lee <bearlee@alum.mit.edu>
2015-09-08 14:03:59 -0400

    fixes #177. Removing built documentation. [skip ci]

--- doc/api/html/search/functions_16.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['_7eaccumulator',['~accumulator',['../classstan_1_1math_1_1accumulator.html#a298d4088fbfdfca98d2c8e36a5ea4f1c',1,'stan::math::accumulator']]],
-  ['_7echainable',['~chainable',['../classstan_1_1math_1_1chainable.html#ad496bfaf684b78e944fa28c178e5ac83',1,'stan::math::chainable']]],
-  ['_7echainable_5falloc',['~chainable_alloc',['../classstan_1_1math_1_1chainable__alloc.html#aaca3bd4f138cfd11f43e984c241463ce',1,'stan::math::chainable_alloc']]],
-  ['_7egevv_5fvvv_5fvari',['~gevv_vvv_vari',['../classstan_1_1math_1_1gevv__vvv__vari.html#a1d061a4dc39e882750c673965e0d7be5',1,'stan::math::gevv_vvv_vari']]],
-  ['_7estack_5falloc',['~stack_alloc',['../classstan_1_1math_1_1stack__alloc.html#a946367b4b88dd893103db604c5f23669',1,'stan::math::stack_alloc']]],
-  ['_7evari',['~vari',['../classstan_1_1math_1_1vari.html#a14e0658a14d11a99e69f6d16f119bbf8',1,'stan::math::vari']]]
-];
--- doc/api/html/search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['compressed_20sparse_20row_20matrix_20format_2e',['Compressed Sparse Row matrix format.',['../group__csr__format.html',1,'']]]
-];
