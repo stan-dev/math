73a29b480f98889b11ae914bf7b985d2a7b3276c
Daniel Lee <bearlee@alum.mit.edu>
2015-09-08 14:02:52 -0400

    release/v2.8.0: adding built documentation. [skip ci]

---
+++ doc/api/html/search/classes_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dummy',['dummy',['../structstan_1_1math_1_1dummy.html',1,'stan::math']]]
+];
---
+++ doc/api/html/search/variables_11.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['v1_5f',['v1_',['../classstan_1_1math_1_1gevv__vvv__vari.html#a1b8223780565cab9a3d95dd0bb199b90',1,'stan::math::gevv_vvv_vari::v1_()'],['../rev_2mat_2fun_2dot__product_8hpp.html#a8c886c0e0b0df10a90267645bac46674',1,'v1_():&#160;dot_product.hpp'],['../rev_2mat_2fun_2squared__distance_8hpp.html#a83c5c6dc5ed503252611e6e50af3557a',1,'v1_():&#160;squared_distance.hpp']]],
+  ['v2_5f',['v2_',['../classstan_1_1math_1_1gevv__vvv__vari.html#a3cfca671d85ffed899debb9730235dbd',1,'stan::math::gevv_vvv_vari::v2_()'],['../rev_2mat_2fun_2dot__product_8hpp.html#a27e3d8b375e8f1fb1cb0078d150879ea',1,'v2_():&#160;dot_product.hpp'],['../rev_2mat_2fun_2squared__distance_8hpp.html#a2157c9243530cdafcfb1665c9e682ed7',1,'v2_():&#160;squared_distance.hpp']]],
+  ['v_5f',['v_',['../classstan_1_1math_1_1sum__v__vari.html#a02ca46a8d2ffc591718513c98c09c9b6',1,'stan::math::sum_v_vari::v_()'],['../rev_2mat_2fun_2dot__self_8hpp.html#ad96f2a078f01703996f5677e20cc34cd',1,'v_():&#160;dot_self.hpp']]],
+  ['val_5f',['val_',['../structstan_1_1math_1_1fvar.html#a5d2e56f89a8dec57599a7d3e62e5e256',1,'stan::math::fvar::val_()'],['../classstan_1_1math_1_1vari.html#a5c8a37e50654a282308ddde46e68ede6',1,'stan::math::vari::val_()']]],
+  ['var_5falloc_5fstack_5f',['var_alloc_stack_',['../structstan_1_1math_1_1_autodiff_stack_storage.html#a4e95a60d3e2d215f4f8020e6ce2f03d8',1,'stan::math::AutodiffStackStorage']]],
+  ['var_5fnochain_5fstack_5f',['var_nochain_stack_',['../structstan_1_1math_1_1_autodiff_stack_storage.html#ac281684e398a6a4cedcf7c4e75eeb2f1',1,'stan::math::AutodiffStackStorage']]],
+  ['var_5fstack_5f',['var_stack_',['../structstan_1_1math_1_1_autodiff_stack_storage.html#a3ee9208d01fab7f0d6b3d3fd6f186435',1,'stan::math::AutodiffStackStorage']]],
+  ['varirefa_5f',['variRefA_',['../classstan_1_1math_1_1cholesky__decompose__v__vari.html#acd3834b8b6dd6cfa3b799cac9e7739fc',1,'stan::math::cholesky_decompose_v_vari']]],
+  ['varirefl_5f',['variRefL_',['../classstan_1_1math_1_1cholesky__decompose__v__vari.html#a733ded3d060d5d780c73a05b6f1402df',1,'stan::math::cholesky_decompose_v_vari']]],
+  ['varis_5f',['varis_',['../classstan_1_1math_1_1precomputed__gradients__vari.html#aedb6cbfaa40f6882efda15856d4c7d0a',1,'stan::math::precomputed_gradients_vari']]],
+  ['vi_5f',['vi_',['../classstan_1_1math_1_1var.html#a179b1c61bcd82f0e6c1d109c07ae8e40',1,'stan::math::var']]],
+  ['vis_5f',['vis_',['../classstan_1_1math_1_1op__matrix__vari.html#a81caaf77e84614aada7a27f0984db8cc',1,'stan::math::op_matrix_vari::vis_()'],['../classstan_1_1math_1_1op__vector__vari.html#aa492abe0afa7a4a030bdeed4a8d36f3f',1,'stan::math::op_vector_vari::vis_()']]]
+];
