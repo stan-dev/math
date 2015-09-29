014b412a7a7431328d5f299675e6481c0c088079
Rayleigh L <rayleighl@yahoo.com>
2015-09-29 08:43:50 -0400

    Updated so all parameters use generic typenames

--- stan/math/prim/scal/fun/selfstart_asymp.hpp
+++ stan/math/prim/scal/fun/selfstart_asymp.hpp
@@ -2,34 +2,36 @@
 #define STAN_MATH_PRIM_SCAL_FUN_SELFSTART_ASYMP_HPP
 
 #include <vector>
+#include <cmath>
 #include <boost/math/tools/promotion.hpp>
 
 namespace stan {
-  
+ 
   namespace math {
 
-    template <typename T1, typename T2, typename T3> 
-    inline typename boost::math::tools::promote_args<T1, T2, T3>::type 
-    selfstart_asymp(double x, const T1& Asym, const T2& R0, const T3& exp_lrc) {
+    template <typename T1, typename T2, typename T3, typename T4>
+    inline typename boost::math::tools::promote_args<T1, T2, T3, T4>::type
+    selfstart_asymp(const T1& x, const T2& Asym, const T3& R0,
+                    const T4& exp_lrc) {
       using std::exp;
       return Asym + (R0 - Asym) * exp((-exp_lrc * x));
     }	
-    
-    template <typename T1, typename T2, typename T3> inline
-    std::vector<typename boost::math::tools::promote_args<T1, T2, T3>::type> 
-    selfstart_asymp(const std::vector<double>& x, const T1& Asym,
-                    const T2& R0, const T3& lrc) {
+   
+    template <typename T1, typename T2, typename T3, typename T4> inline
+    std::vector<typename boost::math::tools::promote_args<T1, T2, T3, T4>::type>
+    selfstart_asymp(const std::vector<T1>& x, const T2& Asym, const T3& R0,
+                    const T4& lrc) {
       using std::exp;
 
-      std::vector<typename boost::math::tools::promote_args<T1, T2, T3>::type> 
+      std::vector<typename boost::math::tools::promote_args<T1, T2, T3>::type>
       value(x.size());
-      
+     
       T3 exp_lrc = exp(lrc);
 
       for (std::size_t i = 0; i < x.size(); ++i)
         value[i] = selfstart_asymp(x[i], Asym, R0, exp_lrc);
 
-      return value; 
+      return value;
     }
 
   }
