2e1bcf602302413c04f50eb51746e0e2d19447a2
Marco Inacio <marcoigarapava@gmail.com>
2015-10-13 21:00:58 -0300

    Fix issue #161: (log_)falling_factorial as described in documentation

--- test/unit/math/rev/scal/fun/log_falling_factorial_test.cpp
+++ test/unit/math/rev/scal/fun/log_falling_factorial_test.cpp
@@ -7,23 +7,28 @@
 
 
 TEST(AgradRev,log_falling_factorial_var_double) {
-  double a(1);
+  double a(2);
   AVAR b(4.0);
   AVAR f = stan::math::log_falling_factorial(b,a);
-  EXPECT_FLOAT_EQ(std::log(24.0),f.val());
+  EXPECT_FLOAT_EQ(std::log(12.0), f.val());
 
   AVEC x = createAVEC(a,b);
   VEC g;
   f.grad(x,g);
   EXPECT_FLOAT_EQ(0, g[0]);
-  EXPECT_FLOAT_EQ(boost::math::digamma(5),g[1]);
+  EXPECT_FLOAT_EQ((boost::math::digamma(5) - boost::math::digamma(3)),
+                  g[1]);
+
+  double eps = 1e-6;
+  EXPECT_FLOAT_EQ((stan::math::log_falling_factorial(4.0 + eps, 2.0)
+                  - stan::math::log_falling_factorial(4.0 - eps, 2.0))
+                  / (2 * eps), g[1]);
 }
 
 TEST(AgradRev, log_falling_factorial_exceptions) {
   double a(1);
   AVAR b(-3.0);
   EXPECT_THROW(stan::math::log_falling_factorial(b,a), std::domain_error);
-  EXPECT_THROW(stan::math::log_falling_factorial(a,b), std::domain_error);
   EXPECT_THROW(stan::math::log_falling_factorial(b,b), std::domain_error);
 }
 
@@ -31,24 +36,38 @@ TEST(AgradRev, log_falling_factorial_double_var) {
   double a(5);
   AVAR b(4.0);
   AVAR f = stan::math::log_falling_factorial(a,b);
-  EXPECT_FLOAT_EQ(std::log(5.0), f.val());
+  EXPECT_FLOAT_EQ(std::log(120.0), f.val());
   AVEC x = createAVEC(a,b);
-  VEC g;
+  VEC g;  
   f.grad(x,g);
   EXPECT_FLOAT_EQ(0, g[0]);
-  EXPECT_FLOAT_EQ(-boost::math::digamma(5), g[1]);
+  EXPECT_FLOAT_EQ(boost::math::digamma(2), g[1]);
+
+  double eps = 1e-6;
+  EXPECT_FLOAT_EQ((stan::math::log_falling_factorial(5.0, 4.0 + eps)
+                  - stan::math::log_falling_factorial(5.0, 4.0 - eps))
+                  / (2 * eps), g[1]);
 }
 
 TEST(AgradRev, log_falling_factorial_var_var) {
+  AVAR a(6.0);
   AVAR b(4.0);
-  AVAR c(4.0);
-  AVAR f = stan::math::log_falling_factorial(b,c);
-  EXPECT_FLOAT_EQ(0.0, f.val());
-  AVEC x = createAVEC(b,c);
+  AVAR f = stan::math::log_falling_factorial(a,b);
+  EXPECT_FLOAT_EQ(std::log(360.0), f.val());
+  AVEC x = createAVEC(a,b);
   VEC g;
   f.grad(x,g);
-  EXPECT_FLOAT_EQ(boost::math::digamma(5), g[0]);
-  EXPECT_FLOAT_EQ(-boost::math::digamma(5), g[1]);
+  EXPECT_FLOAT_EQ((boost::math::digamma(7) - boost::math::digamma(3)),
+                  g[0]);
+  EXPECT_FLOAT_EQ(boost::math::digamma(3), g[1]);
+  
+  double eps = 1e-6;
+  EXPECT_FLOAT_EQ((stan::math::log_falling_factorial(6.0 + eps, 4.0)
+                  - stan::math::log_falling_factorial(6.0 - eps, 4.0))
+                  / (2 * eps), g[0]);
+  EXPECT_FLOAT_EQ((stan::math::log_falling_factorial(6.0, 4.0 + eps)
+                  - stan::math::log_falling_factorial(6.0, 4.0 - eps))
+                  / (2 * eps), g[1]);
 }
 
 struct log_falling_factorial_fun {
