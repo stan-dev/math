9fc2ead438c6686d79c0cc25b22254a59917fba8
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
2015-10-20 15:43:09 -0400

    Use Eigen::DenseIndex instead of size_t

--- stan/math/prim/mat/fun/LDLT_factor.hpp
+++ stan/math/prim/mat/fun/LDLT_factor.hpp
@@ -119,13 +119,13 @@ namespace stan {
         return _ldltP->matrixLDLT();
       }
 
-      inline size_t rows() const { return N_; }
-      inline size_t cols() const { return N_; }
+      inline Eigen::DenseIndex rows() const { return N_; }
+      inline Eigen::DenseIndex cols() const { return N_; }
 
-      typedef size_t size_type;
+      typedef Eigen::DenseIndex size_type;
       typedef double value_type;
 
-      size_t N_;
+      size_type N_;
       boost::shared_ptr< Eigen::LDLT< Eigen::Matrix<T, R, C> > > _ldltP;
     };
   }
