d74da57d0b40b7f76e31aa7a33e3cd5a2af1d798
Ben Goodrich <goodrich.ben@gmail.com>
2015-11-30 09:39:12 -0500

    refactor unit_vector_constrain
    add gradients, tests, rev/fwd specializations

---
+++ stan/math/fwd/mat/fun/unit_vector_constrain.hpp
@@ -0,0 +1,64 @@
+#ifndef STAN_MATH_FWD_MAT_FUN_UNIT_VECTOR_CONSTRAIN_HPP
+#define STAN_MATH_FWD_MAT_FUN_UNIT_VECTOR_CONSTRAIN_HPP
+
+#include <stan/math/fwd/core.hpp>
+#include <stan/math/fwd/scal/fun/sqrt.hpp>
+#include <stan/math/rev/scal/fun/sqrt.hpp>
+#include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/fwd/mat/fun/dot_self.hpp>
+#include <stan/math/prim/scal/fun/inv.hpp>
+#include <stan/math/prim/mat/fun/tcrossprod.hpp>
+#include <stan/math/rev/mat/fun/unit_vector_constrain.hpp>
+#include <stan/math/prim/mat/fun/unit_vector_constrain.hpp>
+
+namespace stan {
+  namespace math {
+
+    template <typename T, int R, int C>
+    inline Eigen::Matrix<fvar<T>, R, C>
+    unit_vector_constrain(const Eigen::Matrix<fvar<T>, R, C>& y) {
+      using Eigen::Matrix;
+      using stan::math::tcrossprod;
+      using stan::math::dot_self;
+      using stan::math::unit_vector_constrain;
+
+      Matrix<T,R,C> y_t(y.size());
+      for (int k = 0; k < y.size(); ++k)
+        y_t.coeffRef(k) = y.coeff(k).val_;
+
+      Matrix<T,R,C> unit_vector_y_t
+        = unit_vector_constrain(y_t);
+      Matrix<fvar<T>, R, C> unit_vector_y(y.size());
+      for (int k = 0; k < y.size(); ++k)
+        unit_vector_y.coeffRef(k).val_ = unit_vector_y_t.coeff(k);
+
+      const T squared_norm = dot_self(y_t);
+      const T norm = sqrt(squared_norm);
+      const T inv_norm = inv(norm);
+      Matrix<T,Eigen::Dynamic,Eigen::Dynamic> J
+        = tcrossprod(y_t) / (-norm * squared_norm);
+
+      // for each input position
+      for (int m = 0; m < y.size(); ++m) {
+        J.coeffRef(m,m) += inv_norm;
+        // for each output position
+        for (int k = 0; k < y.size(); ++k) {
+          // chain from input to output
+          unit_vector_y.coeffRef(k).d_ = J.coeff(k,m);
+        }
+      }
+      return unit_vector_y;
+    }
+
+    template <typename T, int R, int C>
+    inline Eigen::Matrix<fvar<T>, R, C>
+    unit_vector_constrain(const Eigen::Matrix<fvar<T>, R, C>& y, fvar<T>& lp) {
+      using stan::math::dot_self;
+      const fvar<T> squared_norm = dot_self(y);
+      lp -= 0.5 * squared_norm;
+      return unit_vector_constrain(y);
+    }
+
+  }
+}
+#endif
--- stan/math/prim/mat/fun/unit_vector_constrain.hpp
+++ stan/math/prim/mat/fun/unit_vector_constrain.hpp
@@ -2,73 +2,46 @@
 #define STAN_MATH_PRIM_MAT_FUN_UNIT_VECTOR_CONSTRAIN_HPP
 
 #include <stan/math/prim/mat/fun/Eigen.hpp>
-#include <stan/math/prim/mat/meta/index_type.hpp>
+#include <stan/math/prim/mat/fun/dot_self.hpp>
+#include <stan/math/prim/scal/err/check_positive.hpp>
+#include <stan/math/prim/scal/err/check_positive_finite.hpp>
+#include <stan/math/prim/scal/err/check_nonzero_size.hpp>
+#include <stan/math/prim/mat/err/check_vector.hpp>
 #include <cmath>
 
 namespace stan {
-
   namespace math {
 
-    // Unit vector
-
     /**
      * Return the unit length vector corresponding to the free vector y.
-     * The free vector contains K-1 spherical coordinates.
+     * See https://en.wikipedia.org/wiki/N-sphere#Generating_random_points
      *
-     * @param y of K - 1 spherical coordinates
-     * @return Unit length vector of dimension K
-     * @tparam T Scalar type.
-     **/
-    template <typename T>
-    Eigen::Matrix<T, Eigen::Dynamic, 1>
-    unit_vector_constrain(const Eigen::Matrix<T, Eigen::Dynamic, 1>& y) {
-      using Eigen::Matrix;
-      using Eigen::Dynamic;
-      using stan::math::index_type;
-      typedef typename index_type<Matrix<T, Dynamic, 1> >::type size_type;
-      int Km1 = y.size();
-      Matrix<T, Dynamic, 1> x(Km1 + 1);
-      x(0) = 1.0;
-      const T half_pi = T(M_PI/2.0);
-      for (size_type k = 1; k <= Km1; ++k) {
-        T yk_1 = y(k-1) + half_pi;
-        T sin_yk_1 = sin(yk_1);
-        x(k) = x(k-1)*sin_yk_1;
-        x(k-1) *= cos(yk_1);
-      }
-      return x;
-    }
-
-    /**
-     * Return the unit length vector corresponding to the free vector y.
-     * The free vector contains K-1 spherical coordinates.
-     *
-     * @param y of K - 1 spherical coordinates
+     * @param y vector of K unrestricted variables
      * @return Unit length vector of dimension K
      * @param lp Log probability reference to increment.
      * @tparam T Scalar type.
      **/
-    template <typename T>
-    Eigen::Matrix<T, Eigen::Dynamic, 1>
-    unit_vector_constrain(const Eigen::Matrix<T, Eigen::Dynamic, 1>& y, T &lp) {
-      using Eigen::Matrix;
-      using Eigen::Dynamic;
-      using stan::math::index_type;
-      typedef typename index_type<Matrix<T, Dynamic, 1> >::type size_type;
+    template <typename T, int R, int C>
+    Eigen::Matrix<T, R, C>
+    unit_vector_constrain(Eigen::Matrix<T, R, C>& y) {
+      stan::math::check_vector("unit_vector_constrain", "y", y);
+      stan::math::check_nonzero_size("unit_vector_constrain", "y", y);
+      using stan::math::dot_self;
+      const T SN = dot_self(y);
+      stan::math::check_positive("unit_vector_constrain", "norm", SN);
+      return y / sqrt(SN);
+    }
 
-      int Km1 = y.size();
-      Matrix<T, Dynamic, 1> x(Km1 + 1);
-      x(0) = 1.0;
-      const T half_pi = T(0.5 * M_PI);
-      for (size_type k = 1; k <= Km1; ++k) {
-        T yk_1 = y(k-1) + half_pi;
-        T sin_yk_1 = sin(yk_1);
-        x(k) = x(k-1) * sin_yk_1;
-        x(k-1) *= cos(yk_1);
-        if (k < Km1)
-          lp += (Km1 - k) * log(fabs(sin_yk_1));
-      }
-      return x;
+    template <typename T, int R, int C>
+    Eigen::Matrix<T, R, C>
+    unit_vector_constrain(const Eigen::Matrix<T, R, C>& y, T& lp) {
+      stan::math::check_vector("unit_vector_constrain", "y", y);
+      stan::math::check_nonzero_size("unit_vector_constrain", "y", y);
+      using stan::math::dot_self;
+      const T SN = dot_self(y);
+      stan::math::check_positive_finite("unit_vector_constrain", "norm", SN);
+      lp -= 0.5 * SN;
+      return y / sqrt(SN);
     }
 
   }
