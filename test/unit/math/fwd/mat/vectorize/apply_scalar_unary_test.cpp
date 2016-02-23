e3abd1836279cf4060c2a6d32dbff5179d03651c
Rayleigh L <rayleighl@yahoo.com>
2016-02-23 12:38:36 -0500

    Added type tests

--- test/unit/math/fwd/mat/vectorize/apply_scalar_unary_test.cpp
+++ test/unit/math/fwd/mat/vectorize/apply_scalar_unary_test.cpp
@@ -1,5 +1,7 @@
+#include <stan/math/fwd/core/fvar.hpp>
 #include <stan/math/fwd/scal/fun/exp.hpp>
 #include <stan/math/fwd/mat/vectorize/apply_scalar_unary.hpp>
+#include <test/unit/math/prim/mat/vectorize/expect_types.hpp>
 #include <test/unit/math/fwd/mat/vectorize/expect_values.hpp>
 #include <test/unit/math/fwd/mat/vectorize/expect_errors.hpp>
 #include <test/unit/math/prim/mat/vectorize/foo_base_test.hpp>
@@ -7,6 +9,9 @@
 
 // this tests that the expect_values test works on a mock function
 TEST(MathFwdMatVectorize, applyScalarUnaryMock) {
+  using stan::math::fvar;
+  expect_types<foo_base_test, fvar<double> >();
+  expect_types<foo_base_test, fvar<fvar<double> > >();
   expect_values<foo_base_test>();
   expect_errors<foo_base_test>();
 }
--- test/unit/math/prim/mat/vectorize/apply_scalar_unary_test.cpp
+++ test/unit/math/prim/mat/vectorize/apply_scalar_unary_test.cpp
@@ -1,3 +1,4 @@
+#include <test/unit/math/prim/mat/vectorize/expect_types.hpp>
 #include <test/unit/math/prim/mat/vectorize/expect_values.hpp>
 #include <test/unit/math/prim/mat/vectorize/expect_errors.hpp>
 #include <test/unit/math/prim/mat/vectorize/foo_base_test.hpp>
@@ -6,6 +7,8 @@
 // test that returned values are correct and that errors are thrown
 // for illegal inputs for foo
 TEST(MathPrimMatVectorize, applyScalarUnaryMock) {
+  expect_int_types<foo_base_test>();
+  expect_types<foo_base_test, double>();
   expect_values<foo_base_test>();
   expect_errors<foo_base_test>();
 }
