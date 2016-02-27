d12212d580ce12f09253b9bc07e63623efff7982
Daniel Lee <bearlee@alum.mit.edu>
2016-02-27 09:42:10 -0500

    Adding documentation. Fixing inconsistent test; current branch passes due to include order of headers; fixed the includes within the test.

--- stan/math/prim/mat/meta/is_vector_like.hpp
+++ stan/math/prim/mat/meta/is_vector_like.hpp
@@ -6,7 +6,18 @@
 
 namespace stan {
 
-  // handles eigen matrix
+  /**
+   * Template metaprogram indicates whether a type is vector_like.
+   *
+   * A type is vector_like if an instance can be accessed like a
+   * vector, i.e. square brackets.
+   *
+   * Access is_vector_like::value for the result.
+   *
+   * This metaprogram removes the const qualifier.
+   *
+   * @tparam T Type to test
+   */
   template <typename T>
   struct is_vector_like<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> > {
     enum { value = true };
