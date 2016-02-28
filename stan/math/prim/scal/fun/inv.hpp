b599a4a0a5989a7e2a053082e1eb84a3481c3c78
Rayleigh L <rayleighl@yahoo.com>
2016-02-27 19:01:12 -0500

    Removed inv files

--- stan/math/prim/scal/fun/inv.hpp
+++ stan/math/prim/scal/fun/inv.hpp
@@ -6,16 +6,13 @@
 namespace stan {
   namespace math {
 
-    /**
-     * Return the inverse of the specified value.  The inverse of x is
-     * defined as 1/x.
-     *
-     * @param x Value to invert.
-     * @return Inverse of value.
-     */
-    inline double inv(double x) {
+    template <typename T>
+    inline
+    typename boost::math::tools::promote_args<T>::type
+    inv(const T x) {
       return 1.0 / x;
     }
+
   }
 }
 
