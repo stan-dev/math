6899788695398ef005a2507bd71ef2e193085f72
Marco Inacio <marcoigarapava@gmail.com>
2016-03-01 15:22:09 -0300

    created array version of value_of

---
+++ stan/math/prim/arr/fun/value_of.hpp
@@ -0,0 +1,51 @@
+#ifndef STAN_MATH_PRIM_ARR_FUN_VALUE_OF_HPP
+#define STAN_MATH_PRIM_ARR_FUN_VALUE_OF_HPP
+
+#include <vector>
+#include <cstddef>
+#include <stan/math/prim/scal/fun/value_of.hpp>
+#include <stan/math/prim/scal/meta/child_type.hpp>
+
+namespace stan {
+  namespace math {
+
+    /**
+     * Convert a std::vector of type T to a std::vector of doubles.
+     *
+     * T must implement value_of. See
+     * test/math/fwd/mat/fun/value_of.cpp for fvar and var usage.
+     *
+     * @tparam T Scalar type in std::vector
+     * @param[in] x std::vector to be converted
+     * @return std::vector of values
+     **/
+    template <typename T>
+    inline std::vector<typename child_type<T>::type>
+    value_of(const std::vector<T>& x) {
+      size_t size = x.size();
+      std::vector<typename child_type<T>::type> result(size);
+      for (int i=0; i < size; i++)
+        result[i] = value_of(x[i]);
+      return result;
+    }
+
+    /**
+     * Return the specified argument.
+     *
+     * <p>See <code>value_of(T)</code> for a polymorphic
+     * implementation using static casts.
+     *
+     * <p>This inline pass-through no-op should be compiled away.
+     *
+     * @param x Specified std::vector.
+     * @return Specified std::vector.
+     */
+    template <>
+    inline std::vector<double> value_of(const std::vector<double>& x) {
+      return x;
+    }
+
+  }
+}
+
+#endif
---
+++ test/unit/math/prim/arr/fun/value_of_test.cpp
@@ -0,0 +1,40 @@
+#include <stan/math/prim/arr/fun/value_of.hpp>
+#include <gtest/gtest.h>
+
+template <typename T>
+void fill(const std::vector<double>& contents,
+          std::vector<T>& x){
+  size_t ij = 0;
+  x.assign(contents.size(), T());
+  for (int i = 0; i < contents.size(); ++i)
+    x[i] = T(contents[i]);
+}
+
+TEST(MathMatrix,value_of) {
+  using stan::math::value_of;
+  using std::vector;
+
+  vector<double> a_vals;
+
+  for (size_t i = 0; i < 10; ++i)
+    a_vals.push_back(i + 1);
+
+  vector<double> b_vals;
+
+  for (size_t i = 10; i < 15; ++i)
+    b_vals.push_back(i + 1);
+
+  vector<double> a;
+  fill(a_vals, a);
+  vector<double> b;
+  fill(b_vals, b);
+
+  vector<double> d_a = value_of(a);
+  vector<double> d_b = value_of(b);
+
+  for (int i = 0; i < 5; ++i)
+    EXPECT_FLOAT_EQ(b[i], d_b[i]);
+
+  for (int i = 0; i < 10; ++i)
+    EXPECT_FLOAT_EQ(a[i], d_a[i]);
+}
