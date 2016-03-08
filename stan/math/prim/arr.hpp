90940aa3db191a7b62aa51ab75c5d0f908a8a5bf
Marco Inacio <marcoigarapava@gmail.com>
2016-03-08 20:35:51 -0300

    added value_of and value_of_rec to stan/math/prim/arr.hpp

--- stan/math/prim/arr.hpp
+++ stan/math/prim/arr.hpp
@@ -14,7 +14,8 @@
 #include <stan/math/prim/arr/fun/rep_array.hpp>
 #include <stan/math/prim/arr/fun/scaled_add.hpp>
 #include <stan/math/prim/arr/fun/sub.hpp>
-#include <stan/math/prim/arr/fun/sum.hpp>
+#include <stan/math/prim/arr/fun/value_of.hpp>
+#include <stan/math/prim/arr/fun/value_of_rec.hpp>
 
 #include <stan/math/prim/arr/functor/coupled_ode_observer.hpp>
 #include <stan/math/prim/arr/functor/coupled_ode_system.hpp>
