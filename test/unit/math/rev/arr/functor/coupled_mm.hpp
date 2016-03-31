38cfe98e4b43785980bd6dcae2d62649aa02aed6
wds15 <sdw.post@gmail.com>
2016-03-31 22:13:22 +0200

    aligned include guards and removed defines in coupled_mm.hpp

--- test/unit/math/rev/arr/functor/coupled_mm.hpp
+++ test/unit/math/rev/arr/functor/coupled_mm.hpp
@@ -1,14 +1,7 @@
-#pragma once
+#ifndef TEST_UNIT_MATH_REV_ARR_FUNCTOR_COUPLED_MM_HPP
+#define TEST_UNIT_MATH_REV_ARR_FUNCTOR_COUPLED_MM_HPP
 
-#include <stan/math/prim/arr/functor/integrate_ode.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/rev/scal/fun/value_of_rec.hpp>
-#include <stan/math/rev/scal/fun/pow.hpp>
-
-#define act parms[0] 
- #define KmA parms[1] 
- #define deact parms[2] 
- #define KmAp parms[3] 
 
 struct coupled_mm_ode_fun {
   template <typename T0, typename T1, typename T2>
@@ -20,12 +13,18 @@ struct coupled_mm_ode_fun {
              const std::vector<double>& sx, // double data
              const std::vector<int>& sx_int,
              std::ostream* msgs) const { // integer data
-    using std::pow;
     std::vector<typename stan::return_type<T1,T2>::type> ydot(2);
 
+    const T2 act   = parms[0];
+    const T2 KmA   = parms[1];
+    const T2 deact = parms[2];
+    const T2 KmAp  = parms[3];
+
     ydot[0] = -1*(act*y[0]/(KmA+y[0]))+1*(deact*y[1]/(KmAp+y[1]));
-    ydot[1] = 1*(act*y[0]/(KmA+y[0]))-1*(deact*y[1]/(KmAp+y[1]));
+    ydot[1] =  1*(act*y[0]/(KmA+y[0]))-1*(deact*y[1]/(KmAp+y[1]));
 
     return(ydot);
   }
 };
+
+#endif
