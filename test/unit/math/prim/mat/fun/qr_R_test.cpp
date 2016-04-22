21ca2687d81899baebdd8c2d18dd2a109ef01262
Ben Goodrich <goodrich.ben@gmail.com>
2016-04-22 15:33:20 -0400

    make qr_R_test.cpp segfault

--- test/unit/math/prim/mat/fun/qr_R_test.cpp
+++ test/unit/math/prim/mat/fun/qr_R_test.cpp
@@ -3,8 +3,8 @@
 
 TEST(MathMatrix, qr_R) {
   stan::math::matrix_d m0(0,0);
-  stan::math::matrix_d m1(3,2);
-  m1 << 1, 2, 3, 4, 5, 6;
+  stan::math::matrix_d m1(4,2);
+  m1 << 1, 2, 3, 4, 5, 6, 7, 8;
 
   using stan::math::qr_R;
   using stan::math::qr_Q;
@@ -12,11 +12,11 @@ TEST(MathMatrix, qr_R) {
   EXPECT_THROW(qr_R(m0), std::invalid_argument);
   EXPECT_NO_THROW(qr_R(m1));
 
-  stan::math::matrix_d m2(3,2);
+  stan::math::matrix_d m2(4,2);
   m2 = qr_Q(m1) * qr_R(m1);
   for (unsigned int i=0; i<m1.rows(); i++) {
     for (unsigned int j=0; j<m1.cols(); j++) {
-      EXPECT_NEAR(m1(i,j), m2(i,j), 1e-8);
+      EXPECT_NEAR(m1(i,j), m2(i,j), 1e-12);
     }
   }
   EXPECT_THROW(qr_R(transpose(m1)),std::domain_error);
