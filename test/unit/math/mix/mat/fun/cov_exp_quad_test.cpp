3ff038caf8ffab798b3afff3d5bb32e368dfac2b
Rob Trangucci <robert.trangucci@gmail.com>
2016-06-13 16:09:22 -0400

    Extended cov_exp_quad test to mismatched dimensional vector inputs

--- test/unit/math/mix/mat/fun/cov_exp_quad_test.cpp
+++ test/unit/math/mix/mat/fun/cov_exp_quad_test.cpp
@@ -4671,54 +4671,183 @@ TEST(AgradMixMath, ffv_cov_exp_quad2_eigen_rvec_nan_values) {
   EXPECT_THROW(cov = cov_exp_quad(x1_d_bad, x2_d_bad, sigma_bad, l_d_bad), std::domain_error);
 }
 
-//
-//TEST(AgradMixMath, fv_cov_exp_quad2_eigen_vec_dim_error) {
-//  using stan::math::cov_exp_quad;
-//  using stan::math::fvar;
-//  using stan::math::var;
-//
-//  fvar<var> sigma(2.0, 1.0);
-//  fvar<var> l(1.0, 1.0);
-//  double sigma_d(2.0);
-//  double l_d(1.0);
-//
-//  double nan_d = std::numeric_limits<double>::quiet_NaN();
-//
-//  fvar<var> sigma_bad(nan_d, 1.0);
-//  fvar<var> l_bad(nan_d, 1.0);
-//  double sigma_d_bad(nan_d);
-//  double l_d_bad(nan_d);
-//
-//  std::vector<Eigen::Matrix<double, -1, 1> > x1_d(5);
-//  std::vector<Eigen::Matrix<fvar<var>, -1, 1> > x1_fv(5);
-//  for (size_t i = 0; i < x1_d.size(); ++i) {
-//    x1_d[i].resize(5,1);
-//    x1_fv[i].resize(5,1);
-//    x1_d[i] << 1, 2, 3, 4, 5;
-//    x1_fv[i] << 1, 2, 3, 4, 5;
-//  }
-//
-//  std::vector<Eigen::Matrix<double, -1, 1> > x2_d(5);
-//  std::vector<Eigen::Matrix<fvar<var>, -1, 1> > x2_fv(5);
-//  for (size_t i = 0; i < x2_d.size(); ++i) {
-//    x2_d[i].resize(5,1);
-//    x2_fv[i].resize(5,1);
-//    x2_d[i] << 5, 2, 1, 4, 5;
-//    x2_fv[i] << 4, 2, 2, 4, 5;
-//  }
-//
-//  std::vector<Eigen::Matrix<double,-1, 1> > x1_d_bad(x1_d);
-//  x1_d_bad[1](1) = nan_d;
-//
-//  std::vector<Eigen::Matrix<fvar<var>, -1, 1> > x1_fv_bad(x1_fv);
-//  x1_fv_bad[1](1) = nan_d;
-//
-//  std::vector<Eigen::Matrix<double,-1, 1> > x2_d_bad(x2_d);
-//  x2_d_bad[1](1) = nan_d;
-//
-//  std::vector<Eigen::Matrix<fvar<var>, -1, 1> > x2_fv_bad(x2_fv);
-//  x2_fv_bad[1](1) = nan_d;
-//
-//  std::vector<Eigen::Matrix<fvar<var>
-//  EXPECT_THROW(cov_exp_quad(, std::invalid_argument);
-//}
+TEST(AgradMixMath, fv_cov_exp_quad2_eigen_vec_dim_error) {
+  using stan::math::cov_exp_quad;
+  using stan::math::fvar;
+  using stan::math::var;
+
+  fvar<var> sigma_fv(2.0, 1.0);
+  fvar<var> l_fv(1.0, 1.0);
+  double sigma_d(2.0);
+  double l_d(1.0);
+
+  std::vector<Eigen::Matrix<double, -1, 1> > x1_d(5);
+  std::vector<Eigen::Matrix<fvar<var>, -1, 1> > x1_fv(5);
+  for (size_t i = 0; i < x1_d.size(); ++i) {
+    x1_d[i].resize(5,1);
+    x1_fv[i].resize(5,1);
+    x1_d[i] << 1, 2, 3, 4, 5;
+    x1_fv[i] << 1, 2, 3, 4, 5;
+  }
+
+  std::vector<Eigen::Matrix<double, -1, 1> > x2_d(5);
+  std::vector<Eigen::Matrix<fvar<var>, -1, 1> > x2_fv(5);
+  for (size_t i = 0; i < x2_d.size(); ++i) {
+    x2_d[i].resize(4,1);
+    x2_fv[i].resize(4,1);
+    x2_d[i] << 5, 2, 1, 4;
+    x2_fv[i] << 4, 2, 2, 4;
+  }
+
+  Eigen::Matrix<fvar<var>, -1, -1> cov;
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_fv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_d, l_fv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_fv, l_fv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_fv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_d, l_fv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_fv, l_fv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_fv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_d, l_fv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_fv, l_fv), std::invalid_argument);
+}
+
+TEST(AgradMixMath, fv_cov_exp_quad2_eigen_rvec_dim_error) {
+  using stan::math::cov_exp_quad;
+  using stan::math::fvar;
+  using stan::math::var;
+
+  fvar<var> sigma_fv(2.0, 1.0);
+  fvar<var> l_fv(1.0, 1.0);
+  double sigma_d(2.0);
+  double l_d(1.0);
+
+  std::vector<Eigen::Matrix<double, 1, -1> > x1_d(5);
+  std::vector<Eigen::Matrix<fvar<var>, 1, -1> > x1_fv(5);
+  for (size_t i = 0; i < x1_d.size(); ++i) {
+    x1_d[i].resize(1,5);
+    x1_fv[i].resize(1,5);
+    x1_d[i] << 1, 2, 3, 4, 5;
+    x1_fv[i] << 1, 2, 3, 4, 5;
+  }
+
+  std::vector<Eigen::Matrix<double, 1, -1> > x2_d(5);
+  std::vector<Eigen::Matrix<fvar<var>, 1, -1> > x2_fv(5);
+  for (size_t i = 0; i < x2_d.size(); ++i) {
+    x2_d[i].resize(1,4);
+    x2_fv[i].resize(1,4);
+    x2_d[i] << 5, 2, 1, 4;
+    x2_fv[i] << 4, 2, 2, 4;
+  }
+
+  Eigen::Matrix<fvar<var>, -1, -1> cov;
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_fv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_d, l_fv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_fv, sigma_fv, l_fv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_fv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_d, l_fv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_fv, sigma_fv, l_fv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_fv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_d, l_fv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_fv, x2_d, sigma_fv, l_fv), std::invalid_argument);
+}
+
+TEST(AgradMixMath, ffv_cov_exp_quad2_eigen_vec_dim_error) {
+  using stan::math::cov_exp_quad;
+  using stan::math::fvar;
+  using stan::math::var;
+
+  fvar<fvar<var> > sigma_ffv(2.0, 1.0);
+  fvar<fvar<var> > l_ffv(1.0, 1.0);
+  double sigma_d(2.0);
+  double l_d(1.0);
+
+  std::vector<Eigen::Matrix<double, -1, 1> > x1_d(5);
+  std::vector<Eigen::Matrix<fvar<fvar<var> >, -1, 1> > x1_ffv(5);
+  for (size_t i = 0; i < x1_d.size(); ++i) {
+    x1_d[i].resize(5,1);
+    x1_ffv[i].resize(5,1);
+    x1_d[i] << 1, 2, 3, 4, 5;
+    x1_ffv[i] << 1, 2, 3, 4, 5;
+  }
+
+  std::vector<Eigen::Matrix<double, -1, 1> > x2_d(5);
+  std::vector<Eigen::Matrix<fvar<fvar<var> >, -1, 1> > x2_ffv(5);
+  for (size_t i = 0; i < x2_d.size(); ++i) {
+    x2_d[i].resize(4,1);
+    x2_ffv[i].resize(4,1);
+    x2_d[i] << 5, 2, 1, 4;
+    x2_ffv[i] << 4, 2, 2, 4;
+  }
+
+  Eigen::Matrix<fvar<fvar<var> >, -1, -1> cov;
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_ffv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_d, l_ffv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_ffv, l_ffv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_ffv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_d, l_ffv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_ffv, l_ffv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_ffv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_d, l_ffv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_ffv, l_ffv), std::invalid_argument);
+}
+
+
+TEST(AgradMixMath, ffv_cov_exp_quad2_eigen_rvec_dim_error) {
+  using stan::math::cov_exp_quad;
+  using stan::math::fvar;
+  using stan::math::var;
+
+  fvar<fvar<var> > sigma_ffv(2.0, 1.0);
+  fvar<fvar<var> > l_ffv(1.0, 1.0);
+  double sigma_d(2.0);
+  double l_d(1.0);
+
+  std::vector<Eigen::Matrix<double, 1, -1> > x1_d(5);
+  std::vector<Eigen::Matrix<fvar<fvar<var> >, 1, -1> > x1_ffv(5);
+  for (size_t i = 0; i < x1_d.size(); ++i) {
+    x1_d[i].resize(1,5);
+    x1_ffv[i].resize(1,5);
+    x1_d[i] << 1, 2, 3, 4, 5;
+    x1_ffv[i] << 1, 2, 3, 4, 5;
+  }
+
+  std::vector<Eigen::Matrix<double, 1, -1> > x2_d(5);
+  std::vector<Eigen::Matrix<fvar<fvar<var> >, 1, -1> > x2_ffv(5);
+  for (size_t i = 0; i < x2_d.size(); ++i) {
+    x2_d[i].resize(1,4);
+    x2_ffv[i].resize(1,4);
+    x2_d[i] << 5, 2, 1, 4;
+    x2_ffv[i] << 4, 2, 2, 4;
+  }
+
+  Eigen::Matrix<fvar<fvar<var> >, -1, -1> cov;
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_ffv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_d, l_ffv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_d, x2_ffv, sigma_ffv, l_ffv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_ffv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_d, l_ffv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_ffv, sigma_ffv, l_ffv), std::invalid_argument);
+
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_d, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_ffv, l_d), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_d, l_ffv), std::invalid_argument);
+  EXPECT_THROW(cov = cov_exp_quad(x1_ffv, x2_d, sigma_ffv, l_ffv), std::invalid_argument);
+}
