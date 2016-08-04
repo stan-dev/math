515e9efad9397c278534a267ee0eb5c7fb64e185
Daniel Lee <bearlee@alum.mit.edu>
2016-08-04 12:48:42 -0400

    Removing extra throw

--- stan/math/prim/mat/fun/cholesky_factor_free.hpp
+++ stan/math/prim/mat/fun/cholesky_factor_free.hpp
@@ -23,9 +23,7 @@ namespace stan {
     cholesky_factor_free(const Eigen::Matrix
                          <T, Eigen::Dynamic, Eigen::Dynamic>& y) {
       using std::log;
-      if (!check_cholesky_factor("cholesky_factor_free", "y", y))
-        throw std::domain_error("cholesky_factor_free: "
-                                "y is not a Cholesky factor");
+      check_cholesky_factor("cholesky_factor_free", "y", y);
       int M = y.rows();
       int N = y.cols();
       Eigen::Matrix<T, Eigen::Dynamic, 1> x((N * (N + 1)) / 2 + (M - N) * N);
