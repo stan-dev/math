3a183c9116e8ab1f8c8bad5ef647d40d177f7ffa
Brian Bloniarz <brian.bloniarz@gmail.com>
2016-08-14 16:34:50 -0700

    PR feedback, add doxygen docs

--- stan/math/prim/scal/fun/grad_reg_inc_gamma.hpp
+++ stan/math/prim/scal/fun/grad_reg_inc_gamma.hpp
@@ -3,18 +3,39 @@
 
 #include <stan/math/prim/scal/fun/gamma_p.hpp>
 #include <stan/math/prim/scal/fun/gamma_q.hpp>
+#include <stan/math/prim/scal/fun/square.hpp>
 #include <cmath>
 #include <stdexcept>
 
 namespace stan {
   namespace math {
 
-    // Gradient of the regularized incomplete gamma functions igamma(a, g)
-    // Precomputed values
-    // g   = boost::math::tgamma(a)
-    // dig = boost::math::digamma(a)
+    /**
+     * Gradient of the regularized incomplete gamma functions igamma(a, z)
+     *
+     * For small z, the gradient is computed via the series expansion;
+     * for large z, the series is numerically inaccurate due to cancellation
+     * and the asymptotic expansion is used.
+     *
+     * @param a   shape parameter, a > 0
+     * @param z   location z >= 0
+     * @param g   boost::math::tgamma(a) (precomputed value)
+     * @param dig boost::math::digamma(a) (precomputed value)
+     *
+     * For the asymptotic expansion, the gradient is given by:
+       \f[
+       \begin{array}{rcl}
+       \Gamma(a, z) & = & z^{a-1}e^{-z} \sum_{k=0}^N \frac{(a-1)_k}{z^k} \qquad , z \gg a\\
+       Q(a, z) & = & \frac{z^{a-1}e^{-z}}{\Gamma(a)} \sum_{k=0}^N \frac{(a-1)_k}{z^k}\\
+       (a)_k & = & (a)_{k-1}(a-k)\\
+       \frac{d}{da} (a)_k & = & (a)_{k-1} + (a-k)\frac{d}{da} (a)_{k-1}\\
+       \frac{d}{da}Q(a, z) & = & (log(z) - \psi(a)) Q(a, z)\\
+       && + \frac{z^{a-1}e^{-z}}{\Gamma(a)} \sum_{k=0}^N \left(\frac{d}{da} (a-1)_k\right) \frac{1}{z^k}
+       \end{array}
+       \f]
+     */
     template<typename T>
-    T grad_reg_inc_gamma(T a, T z, T g, T dig, T precision = 1e-6) {
+    T grad_reg_inc_gamma(T a, T z, T g, T dig, double precision = 1e-6) {
       using boost::math::isinf;
       using std::domain_error;
       using std::exp;
@@ -22,40 +43,42 @@ namespace stan {
       using std::log;
 
       T l = log(z);
-      if (z >= a && z >= (T)8) {
-        // large values of z, compute gradient based on the asymptotic expansion
-        // http://dlmf.nist.gov/8.11#E2
+      if (z >= a && z >= 8) {
+        // asymptotic expansion http://dlmf.nist.gov/8.11#E2
         T S = 0;
-        T fac = a-1;  // falling_factorial(a-1, k)
+        T a_minus_one_minus_k = a - 1;
+        T fac = a_minus_one_minus_k;  // falling_factorial(a-1, k)
         T dfac = 1;   // d/da[falling_factorial(a-1, k)]
         T zpow = z;   // z ** k
         T delta = dfac / zpow;
 
         for (int k = 1; k < 10; ++k) {
+          a_minus_one_minus_k -= 1;
+
           S += delta;
 
           zpow *= z;
-          dfac = (a-1-k)*dfac + fac;
-          fac *= a-1-k;
+          dfac = a_minus_one_minus_k * dfac + fac;
+          fac *= a_minus_one_minus_k;
           delta = dfac / zpow;
 
           if (isinf(delta))
             throw domain_error("gradRegIncGamma not converging");
         }
 
-        return gamma_q(a, z) * (l - dig) + exp(-z + (a-1)*l) * S / g;
+        return gamma_q(a, z) * (l - dig) + exp(-z + (a - 1) * l) * S / g;
       } else {
         // gradient of series expansion http://dlmf.nist.gov/8.7#E3
 
         T S = 0;
         T s = 1;
         int k = 0;
-        T delta = s / (a * a);
+        T delta = s / square(a);
         while (fabs(delta) > precision) {
           S += delta;
           ++k;
           s *= - z / k;
-          delta = s / ((k + a) * (k + a));
+          delta = s / square(k + a);
           if (isinf(delta))
             throw domain_error("gradRegIncGamma not converging");
         }
