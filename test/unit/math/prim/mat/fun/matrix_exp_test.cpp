954dc8c24bcaa3903f95a53aadb4b38381d7ec9f
Charles Margossian <charlesm@metrumrg.com>
2016-08-24 15:56:11 -0400

    Added comments on test/unit/math/prim/mat/fun/matrix_exp_test.cpp indicating the unit test is incomplete

--- test/unit/math/prim/mat/fun/matrix_exp_test.cpp
+++ test/unit/math/prim/mat/fun/matrix_exp_test.cpp
@@ -2,6 +2,8 @@
 #include <gtest/gtest.h>
 #include <test/unit/math/prim/mat/fun/expect_matrix_eq.hpp>
 
+// Unit test is incomplete -- still useful to illustrate some bugs
+
 TEST(MathMatrix, matrix_exp) {
     using stan::math::matrix_exp;
     
@@ -12,15 +14,5 @@ TEST(MathMatrix, matrix_exp) {
     y << 1.0;
         
     expect_matrix_eq(y, matrix_exp(x));
-    
-    /*
-    Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> x_v, y_v;
-    x_v.resize(1, 1);
-    y_v.resize(1, 1);
-    x_v << 0.0;
-    y_v << 1.0;
-    
-    expect_matrix_eq(y_v, matrix_exp(x_v));
-    */
     
 }
