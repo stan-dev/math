2b26c0bc41646f8b7991915463d8d22452485008
Bob Carpenter <carp@alias-i.com>
2016-09-02 18:23:50 +0200

    added pos-def test to multi_normal_rng, fixes #44

--- stan/math/prim/mat/prob/multi_normal_rng.hpp
+++ stan/math/prim/mat/prob/multi_normal_rng.hpp
@@ -18,12 +18,25 @@
 namespace stan {
   namespace math {
 
+    /**
+     * Return a pseudo-random vector with a multi-variate normal
+     * distribution given the specified location parameter and
+     * covariance matrix and pseudo-random number generator.
+     *
+     * If calculating more than one multivariate-normal random draw
+     * then it is more efficient to calculate the Cholesky factor of
+     * the covariance matrix and use the function
+     * <code>stan::math::multi_normal_cholesky_rng</code>.
+     *
+     * @tparam RNG Type of pseudo-random number generator.
+     * @param mu Location parameter.
+     * @param S Covariance parameter.
+     * @param rng Pseudo-random number generator.
+     */
     template <class RNG>
     inline Eigen::VectorXd
-    multi_normal_rng(
-        const Eigen::Matrix<double, Eigen::Dynamic, 1>& mu,
-        const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>& S,
-        RNG& rng) {
+    multi_normal_rng(const Eigen::VectorXd& mu, const Eigen::MatrixXd& S,
+                     RNG& rng) {
       using boost::variate_generator;
       using boost::normal_distribution;
 
@@ -33,6 +46,13 @@ namespace stan {
       check_symmetric(function, "Covariance matrix", S);
       check_finite(function, "Location parameter", mu);
 
+      Eigen::LLT<Eigen::MatrixXd> llt_of_S = S.llt();
+      if (llt_of_S.info() != Eigen::Success) {
+        throw std::domain_error("multi_normal_rng:"
+                                " Matrix argument not solvable;"
+                                " probably not positive definite.");
+      }
+
       variate_generator<RNG&, normal_distribution<> >
         std_normal_rng(rng, normal_distribution<>(0, 1));
 
@@ -40,7 +60,7 @@ namespace stan {
       for (int i = 0; i < S.cols(); i++)
         z(i) = std_normal_rng();
 
-      return mu + S.llt().matrixL() * z;
+      return mu + llt_of_S.matrixL() * z;
     }
 
   }
