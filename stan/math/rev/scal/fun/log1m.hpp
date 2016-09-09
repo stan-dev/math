9b7fffbb17e40c348dc2896613efd97d9a247e11
Bob Carpenter <carp@alias-i.com>
2016-09-09 00:14:09 -0400

    included prim log1m for rev log1m

--- stan/math/rev/scal/fun/log1m.hpp
+++ stan/math/rev/scal/fun/log1m.hpp
@@ -2,7 +2,7 @@
 #define STAN_MATH_REV_SCAL_FUN_LOG1M_HPP
 
 #include <stan/math/rev/core.hpp>
-#include <stan/math/prim/scal/fun/log1p.hpp>
+#include <stan/math/prim/scal/fun/log1m.hpp>
 
 namespace stan {
   namespace math {
