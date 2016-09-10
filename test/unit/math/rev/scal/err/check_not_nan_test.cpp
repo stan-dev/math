8b3e26370ebd61ac98c14f836673ac1e60b6e05b
Daniel Lee <bearlee@alum.mit.edu>
2016-09-09 23:05:09 -0400

    Fixing compiler warnings

--- test/unit/math/rev/scal/err/check_not_nan_test.cpp
+++ test/unit/math/rev/scal/err/check_not_nan_test.cpp
@@ -64,11 +64,11 @@ TEST(ErrorHandlingScalar, CheckNotNanVarCheckUnivariate) {
 
   size_t stack_size = stan::math::ChainableStack::var_stack_.size();
 
-  EXPECT_NO_THROW(1U == stack_size);
+  EXPECT_TRUE(1U == stack_size);
   EXPECT_NO_THROW(check_not_nan(function,"a",a));
 
   size_t stack_size_after_call = stan::math::ChainableStack::var_stack_.size();
-  EXPECT_NO_THROW(1U == stack_size_after_call);
+  EXPECT_TRUE(1U == stack_size_after_call);
 
   stan::math::recover_memory();
 }
