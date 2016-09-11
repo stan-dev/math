a7994238eb5bf927c1fe58f318b2f0dd7e4f50cc
Charles Margossian <charlesm@metrumrg.com>
2016-09-11 17:27:53 -0400

    remove eigen 3.3 and make some correction for ldexp

--- stan/math/prim/scal/fun/ldexp.hpp
+++ stan/math/prim/scal/fun/ldexp.hpp
@@ -1,25 +1,25 @@
 #ifndef STAN_MATH_PRIM_SCAL_FUN_LDEXP_HPP
 #define STAN_MATH_PRIM_SCAL_FUN_LDEXP_HPP
 
-#include <stan/math/rev/core.hpp>
 #include <cmath>
 
 namespace stan {
   namespace math {
 
-      /**
-       * Returns the product of a (the significand) and 
-       * 2 to power b (the exponent).
-       *
-       * @param a the significand
-       * @param b an integer that is the exponent
-       * @return product of a times 2 to the power b
-       */
+    /**
+      * Returns the product of a (the significand) and
+      * 2 to power b (the exponent).
+      *
+      * @param[in] a the significand
+      * @param[in] b an integer that is the exponent
+      * @return product of a times 2 to the power b
+      */
 
-      template <typename T>
-      inline T ldexp(T a, int b) {
-          return a * std::pow(2.0, b);
-      }
+    template <typename T>
+    inline T ldexp(const T& a, int b) {
+      return a * std::pow(2.0, b);
+    }
   }
 }
+
 #endif
