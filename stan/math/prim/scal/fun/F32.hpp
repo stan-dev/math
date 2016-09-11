c03e06712b48c4b79c4abc63353c1e481ccf54d1
Daniel Lee <bearlee@alum.mit.edu>
2016-09-10 22:50:40 -0400

    Added doc for F32; updating tests

--- stan/math/prim/scal/fun/F32.hpp
+++ stan/math/prim/scal/fun/F32.hpp
@@ -9,25 +9,37 @@ namespace stan {
 
     /**
      * Generalized hypergeometric function, 3F2.
-     * Implementation isn't general and doesn't work for all values
-     * of inputs.
+     *
+     * The generalized hypergeometric function is a power series. This
+     * implementation computes the power series directly stopping when
+     * the series converges to within <code>precision</code> or takes
+     * <code>max_steps</code>.
+     *
+     * Although some convergence conditions and divergent conditions are known,
+     * this function does not check the inputs for known converent conditions.
+     *
+     * This function will converge if:
+     *   - <code>a1</code>, <code>a2</code>, or <code>a3</code> is a
+     *     non-positive integer
+     *   - <code>b1</code> or <code>b2</code> is a non-positive integer
+     *   - <code>z</code> is less than 1
+     * This function will diverge if <code>z</code> is greater than 1.
+     * When <code>z</code> is 1, which is the case for the beta binomial
+     * cdf, it is hard to determine.
      *
      * @tparam T type of arguments and result
-     * @param a1 a1 (always called with 1 from beta binomial cdfs)
-     * @param a2 a2 (always called with a2 > 1)
-     * @param a3 a3 (always called with int a3 <= 0)
-     * @param b1 b1 (always called with int b1 < |a3|)
-     * @param b2 b2 (always <= 1)
-     * @param z z (is always called with 1 from beta binomial cdfs)
-     * @param precision precision of the infinite sum. defaults to 1e-6
-     * @param max_steps number of steps to take. defaults to 10000
+     * @param[in] a1 a1 (always called with 1 from beta binomial cdfs)
+     * @param[in] a2 a2 (always called with a2 > 1)
+     * @param[in] a3 a3 (always called with int a3 <= 0)
+     * @param[in] b1 b1 (always called with int b1 < |a3|)
+     * @param[in] b2 b2 (always <= 1)
+     * @param[in] z z (is always called with 1 from beta binomial cdfs)
+     * @param[in] precision precision of the infinite sum. defaults to 1e-6
+     * @param[in] max_steps number of steps to take. defaults to 10000
      */
     template<typename T>
-    T F32(T a1, T a2, T a3,
-          T b1, T b2,
-          T z,
-          double precision = 1e-6,
-          int max_steps = 10000) {
+    T F32(const T& a1, const T& a2, const T& a3, const T& b1, const T& b2,
+          const T& z, double precision = 1e-6, int max_steps = 10000) {
       using std::exp;
       using std::log;
       using std::fabs;
@@ -38,11 +50,10 @@ namespace stan {
       T logZ = log(z);
 
       int k = 0;
+      T p = 0;
       do {
-        T p = (a1 + k) * (a2 + k) * (a3 + k) / ((b1 + k) * (b2 + k) * (k + 1));
+        p = (a1 + k) * (a2 + k) * (a3 + k) / ((b1 + k) * (b2 + k) * (k + 1));
 
-        // If a1, a2, or a3 is a negative integer then the series terminates
-        // after a finite number of interations
         if (p == 0)
           break;
 
@@ -50,7 +61,7 @@ namespace stan {
         tNew = exp(logT);
         F += tNew;
         ++k;
-      } while (fabs(tNew) > precision && k < max_steps);
+      } while (tNew > precision && k < max_steps);
       return F;
     }
 
--- test/unit/math/prim/scal/fun/F32_test.cpp
+++ test/unit/math/prim/scal/fun/F32_test.cpp
@@ -2,20 +2,19 @@
 #include <stan/math/prim/scal/fun/F32.hpp>
 
 TEST(MathPrimScalFun, F32) {
-  // Only testing this with z = 1
   EXPECT_NEAR(2.5,
               stan::math::F32(1.0, 1.0, 1.0, 1.0, 1.0, 0.6),
               1e-6);
   EXPECT_NEAR(1.0,
               stan::math::F32(1.0, 3.0, -1.0, 0.0, 1.0, 1.0),
               1e-6);
-  // EXPECT_NEAR(0.4,
-  //             stan::math::F32(1.0, 3.0, -1.0, 10.0, 0.5, 1.0),
-  //             1e-6);
-  // EXPECT_NEAR(1.56474718,
-  //             stan::math::F32(1.0, 2.0, 3.0, 4.0, 5.0, 1.0),
-  //             1e-6);
-  // EXPECT_NEAR(0.73,
-  //             stan::math::F32(1.0, 4.0, 1.0, -5.0, 4.0, 1.0),
-  //             1e-6);
+  EXPECT_NEAR(0.4,
+              stan::math::F32(1.0, 3.0, -1.0, 10.0, 0.5, 1.0),
+              1e-6);
+  EXPECT_NEAR(1.56474718,
+              stan::math::F32(1.0, 2.0, 3.0, 4.0, 5.0, 1.0),
+              1e-6);
+  EXPECT_NEAR(1.0,
+              stan::math::F32(1.0, 4.0, 0.0, -5.0, 4.0, 1.0),
+              1e-6);
 }
