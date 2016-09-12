1d946a0785d8e45776a84f3a6ebf095f9a656ff2
Charles Margossian <charlesm@metrumrg.com>
2016-09-12 17:07:17 -0400

    Brush C++ coding style for matrix_exp_2x2.hpp

--- stan/math/prim/mat/fun/matrix_exp_2x2.hpp
+++ stan/math/prim/mat/fun/matrix_exp_2x2.hpp
@@ -7,9 +7,6 @@
 namespace stan {
     namespace math {
 
-        using Eigen::Matrix;
-        using Eigen::Dynamic;
-
         /**
          * Return the matrix exponential of a 2x2 matrix. Reference for 
          * algorithm: http://mathworld.wolfram.com/MatrixExponential.html
@@ -18,23 +15,25 @@ namespace stan {
          * @return Matrix exponential of A.
          */
         template <typename T>
-        Matrix<T, Dynamic, Dynamic>
-        matrix_exp_compute_2x2(const Matrix<T, Dynamic, Dynamic>& A) {
-            Matrix<T, Dynamic, Dynamic> B;
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
+        matrix_exp_compute_2x2(const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& A) {
+            Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B;
             T a = A(0, 0), b = A(0, 1), c = A(1, 0), d = A(1, 1), delta;
-            delta = sqrt((a - d)*(a - d) + 4*b*c);
+            delta = sqrt((a - d) * (a - d) + 4 * b * c);
 
-            B.resize(2, 2);
-            B(0, 0) = exp(.5*(a+d))
-                       * (delta*cosh(.5*delta) + (a-d)*sinh(.5*delta));
-            B(0, 1) = 2*b*exp(.5*(a+d)) * sinh(.5*delta);
-            B(1, 0) = 2*c*exp(.5*(a+d)) * sinh(.5*delta);
-            B(1, 1) = exp(.5*(a+d))
-                       * (delta*cosh(.5*delta) + (d-a)*sinh(.5*delta));
+			Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B(2, 2);
+			T half_delta = 0.5 * delta;
+			T cosh_half_delta = cosh(half_delta);
+			T sinh_half_delta = sinh(half_delta);
+			T exp_half_a_plus_d = exp(0.5 * (a + d));
+			
+			B(0, 0) = exp_half_a_plus_d * (delta * cosh_half_delta + 
+					  (a - d) * sinh_half_delta;
+			B(0, 1) = 2 * b * exp_half_a_plus_d * sinh_half_delta;
+			B(1, 0) = 2 * c * exp_half_a_plus_d * sinh_half_delta;
+			B(1, 1) = exp_half_a_plus_d;
 
-            B = 1/delta * B;
-
-            return B;
+            return 1/delta * B;
         }
 
     }
