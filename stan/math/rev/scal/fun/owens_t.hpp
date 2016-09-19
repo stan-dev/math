ad3419d989ae876dd43bc4dff1c3a63c807ccd97
Bob Carpenter <carp@alias-i.com>
2016-09-19 00:10:24 -0400

    remove math.h from owens_t includes

--- stan/math/rev/scal/fun/owens_t.hpp
+++ stan/math/rev/scal/fun/owens_t.hpp
@@ -1,7 +1,6 @@
 #ifndef STAN_MATH_REV_SCAL_FUN_OWENS_T_HPP
 #define STAN_MATH_REV_SCAL_FUN_OWENS_T_HPP
 
-#include <math.h>
 #include <stan/math/rev/core.hpp>
 #include <stan/math/prim/scal/fun/constants.hpp>
 #include <stan/math/prim/scal/fun/erf.hpp>
