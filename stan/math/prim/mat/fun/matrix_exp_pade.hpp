61f1cb036606faff3422f1dcdbaff4a7c2d61213
Charles Margossian <charlesm@metrumrg.com>
2016-09-19 15:38:50 -0400

    Added // NOLINT in matrix_exp_pade to stop error messages from cpplint when it goes over the code from Eigen/unsupported.

--- stan/math/prim/mat/fun/matrix_exp_pade.hpp
+++ stan/math/prim/mat/fun/matrix_exp_pade.hpp
@@ -25,41 +25,41 @@ namespace stan {
 
         template <typename RealScalar>
         struct MatrixExponentialScalingOp
-        {
+        {  // NOLINT
             /** \brief Constructor.
              *
              * \param[in] squarings  The integer \f$ s \f$ in this document.
              */
-            MatrixExponentialScalingOp(int squarings) : m_squarings(squarings) { }
-        
+            MatrixExponentialScalingOp(int squarings) : m_squarings(squarings) { }   // NOLINT
+
 
             /** \brief Scale a matrix coefficient.
              *
              * \param[in,out] x  The scalar to be scaled, becoming \f$ 2^{-s} x \f$.
              */
             inline const RealScalar operator() (const RealScalar& x) const
-            {
+            {  // NOLINT
                 using std::ldexp;
                 return ldexp(x, -m_squarings);
             }
-        
+
             typedef std::complex<RealScalar> ComplexScalar;
-        
+
             /** \brief Scale a matrix coefficient.
              *
              * \param[in,out] x  The scalar to be scaled, becoming \f$ 2^{-s} x \f$.
              */
             inline const ComplexScalar operator() (const ComplexScalar& x) const
-            {
+            {  // NOLINT
                 using std::ldexp;
-                return ComplexScalar(ldexp(x.real(), -m_squarings), ldexp(x.imag(), -m_squarings));
+                return ComplexScalar(ldexp(x.real(), -m_squarings), ldexp(x.imag(), -m_squarings));  // NOLINT
             }
-        
+
             private:
                 int m_squarings;
         };
-    
-    
+
+
         /** \brief Compute the (5,5)-Pad&eacute; approximant to the exponential.
          *
          *  After exit, \f$ (V+U)(V-U)^{-1} \f$ is the Pad&eacute;
@@ -67,15 +67,15 @@ namespace stan {
          */
         template <typename MatrixType>
         void matrix_exp_pade3(const MatrixType &A, MatrixType &U, MatrixType &V)
-        {
-            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;
+        {  // NOLINT
+            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;  // NOLINT
             const RealScalar b[] = {120.L, 60.L, 12.L, 1.L};
             const MatrixType A2 = A * A;
-            const MatrixType tmp = b[3] * A2 + b[1] * MatrixType::Identity(A.rows(), A.cols());
+            const MatrixType tmp = b[3] * A2 + b[1] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
             U.noalias() = A * tmp;
             V = b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());
         }
-    
+
         /** \brief Compute the (5,5)-Pad&eacute; approximant to the exponential.
          *
          *  After exit, \f$ (V+U)(V-U)^{-1} \f$ is the Pad&eacute;
@@ -83,16 +83,16 @@ namespace stan {
          */
         template <typename MatrixType>
         void matrix_exp_pade5(const MatrixType &A, MatrixType &U, MatrixType &V)
-        {
-            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;
+        {  // NOLINT
+            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;  // NOLINT
             const RealScalar b[] = {30240.L, 15120.L, 3360.L, 420.L, 30.L, 1.L};
             const MatrixType A2 = A * A;
             const MatrixType A4 = A2 * A2;
-            const MatrixType tmp = b[5] * A4 + b[3] * A2 + b[1] * MatrixType::Identity(A.rows(), A.cols());
+            const MatrixType tmp = b[5] * A4 + b[3] * A2 + b[1] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
             U.noalias() = A * tmp;
-            V = b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());
+            V = b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
         }
-    
+
         /** \brief Compute the (7,7)-Pad&eacute; approximant to the exponential.
          *
          *  After exit, \f$ (V+U)(V-U)^{-1} \f$ is the Pad&eacute;
@@ -100,19 +100,18 @@ namespace stan {
          */
         template <typename MatrixType>
         void matrix_exp_pade7(const MatrixType &A, MatrixType &U, MatrixType &V)
-        {
-            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;
-            const RealScalar b[] = {17297280.L, 8648640.L, 1995840.L, 277200.L, 25200.L, 1512.L, 56.L, 1.L};
+        {  // NOLINT
+            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;  // NOLINT
+            const RealScalar b[] = {17297280.L, 8648640.L, 1995840.L, 277200.L, 25200.L, 1512.L, 56.L, 1.L};  // NOLINT
             const MatrixType A2 = A * A;
             const MatrixType A4 = A2 * A2;
             const MatrixType A6 = A4 * A2;
             const MatrixType tmp = b[7] * A6 + b[5] * A4 + b[3] * A2
             + b[1] * MatrixType::Identity(A.rows(), A.cols());
             U.noalias() = A * tmp;
-            V = b[6] * A6 + b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());
-        
+            V = b[6] * A6 + b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
         }
-    
+
         /** \brief Compute the (9,9)-Pad&eacute; approximant to the exponential.
          *
          *  After exit, \f$ (V+U)(V-U)^{-1} \f$ is the Pad&eacute;
@@ -120,9 +119,9 @@ namespace stan {
          */
         template <typename MatrixType>
         void matrix_exp_pade9(const MatrixType &A, MatrixType &U, MatrixType &V)
-        {
-            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;
-            const RealScalar b[] = {17643225600.L, 8821612800.L, 2075673600.L, 302702400.L, 30270240.L,
+        {  // NOLINT
+            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;  // NOLINT
+            const RealScalar b[] = {17643225600.L, 8821612800.L, 2075673600.L, 302702400.L, 30270240.L,  // NOLINT
             2162160.L, 110880.L, 3960.L, 90.L, 1.L};
             const MatrixType A2 = A * A;
             const MatrixType A4 = A2 * A2;
@@ -131,36 +130,36 @@ namespace stan {
             const MatrixType tmp = b[9] * A8 + b[7] * A6 + b[5] * A4 + b[3] * A2
             + b[1] * MatrixType::Identity(A.rows(), A.cols());
             U.noalias() = A * tmp;
-            V = b[8] * A8 + b[6] * A6 + b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());
+            V = b[8] * A8 + b[6] * A6 + b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
         }
-    
+
         /** \brief Compute the (13,13)-Pad&eacute; approximant to the exponential.
          *
          *  After exit, \f$ (V+U)(V-U)^{-1} \f$ is the Pad&eacute;
          *  approximant of \f$ \exp(A) \f$ around \f$ A = 0 \f$.
          */
         template <typename MatrixType>
-        void matrix_exp_pade13(const MatrixType &A, MatrixType &U, MatrixType &V)
-        {
-            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;
-            const RealScalar b[] = {64764752532480000.L, 32382376266240000.L, 7771770303897600.L,
-                1187353796428800.L, 129060195264000.L, 10559470521600.L, 670442572800.L,
-                33522128640.L, 1323241920.L, 40840800.L, 960960.L, 16380.L, 182.L, 1.L};
+        void matrix_exp_pade13(const MatrixType &A, MatrixType &U, MatrixType &V)  // NOLINT
+        {  // NOLINT
+            typedef typename NumTraits<typename traits<MatrixType>::Scalar>::Real RealScalar;  // NOLINT
+            const RealScalar b[] = {64764752532480000.L, 32382376266240000.L, 7771770303897600.L,  // NOLINT
+                1187353796428800.L, 129060195264000.L, 10559470521600.L, 670442572800.L,  // NOLINT
+                33522128640.L, 1323241920.L, 40840800.L, 960960.L, 16380.L, 182.L, 1.L};  // NOLINT
             const MatrixType A2 = A * A;
             const MatrixType A4 = A2 * A2;
             const MatrixType A6 = A4 * A2;
-            V = b[13] * A6 + b[11] * A4 + b[9] * A2; // used for temporary storage
+            V = b[13] * A6 + b[11] * A4 + b[9] * A2; // used for temporary storage  // NOLINT
             MatrixType tmp = A6 * V;
-            tmp += b[7] * A6 + b[5] * A4 + b[3] * A2 + b[1] * MatrixType::Identity(A.rows(), A.cols());
+            tmp += b[7] * A6 + b[5] * A4 + b[3] * A2 + b[1] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
             U.noalias() = A * tmp;
             tmp = b[12] * A6 + b[10] * A4 + b[8] * A2;
             V.noalias() = A6 * tmp;
-            V += b[6] * A6 + b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());
+            V += b[6] * A6 + b[4] * A4 + b[2] * A2 + b[0] * MatrixType::Identity(A.rows(), A.cols());  // NOLINT
         }
-    
-        template <typename MatrixType, typename RealScalar = typename NumTraits<typename traits<MatrixType>::Scalar>::Real>
+
+        template <typename MatrixType, typename RealScalar = typename NumTraits<typename traits<MatrixType>::Scalar>::Real>  // NOLINT
         struct matrix_exp_computeUV
-        {
+        {  // NOLINT
             /** \brief Compute Pad&eacute; approximant to the exponential.
              *
              * Computes \c U, \c V and \c squarings such that \f$ (V+U)(V-U)^{-1} \f$ is a Pad&eacute;
@@ -173,16 +172,16 @@ namespace stan {
              * argument, which tells the function the type of elements used in the 
              * matrix. 
              */
-            static void run(const MatrixType& arg, MatrixType& U, MatrixType& V, int& squarings);
+            static void run(const MatrixType& arg, MatrixType& U, MatrixType& V, int& squarings);  // NOLINT
         };
-    
+
         template <typename MatrixType>
         struct matrix_exp_computeUV<MatrixType>
-        {
-        	template <typename T>
-            static void run(const MatrixType& arg, MatrixType& U, MatrixType& V, int& squarings,
-            				T scalar_type)
-            {
+        {  // NOLINT
+        	template <typename T>  // NOLINT
+            static void run(const MatrixType& arg, MatrixType& U, MatrixType& V, int& squarings,  // NOLINT
+            				T scalar_type)  // NOLINT
+            {  // NOLINT
                 using std::frexp;
                 using std::pow;
                 const T l1norm = arg.cwiseAbs().colwise().sum().maxCoeff();
@@ -199,7 +198,7 @@ namespace stan {
                     const double maxnorm = 5.371920351148152;
                     frexp(l1norm / maxnorm, &squarings);
                     if (squarings < 0) squarings = 0;
-                    MatrixType A = arg.unaryExpr(MatrixExponentialScalingOp<T>(squarings));
+                    MatrixType A = arg.unaryExpr(MatrixExponentialScalingOp<T>(squarings));  // NOLINT
                     matrix_exp_pade13(A, U, V);
                 }
             }
