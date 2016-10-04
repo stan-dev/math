e5f7a3a3df86df1af551d3fcdd302ca684c57b6f
Rayleigh L <rayleighl@yahoo.com>
2016-10-04 15:51:25 -0400

    Updated round to use stan::math::round

--- stan/math/prim/mat/fun/round.hpp
+++ stan/math/prim/mat/fun/round.hpp
@@ -2,7 +2,7 @@
 #define STAN_MATH_PRIM_MAT_FUN_ROUND_HPP
 
 #include <stan/math/prim/mat/vectorize/apply_scalar_unary.hpp>
-#include <math.h>
+#include <stan/math/prim/scal/fun/round.hpp>
 
 namespace stan {
   namespace math {
@@ -16,7 +16,7 @@ namespace stan {
     struct round_fun {
       template <typename T>
       static inline T fun(const T& x) {
-        using ::round;
+        using stan::math::round;
         return round(x);
       }
     };
