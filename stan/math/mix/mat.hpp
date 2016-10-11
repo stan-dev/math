126727d18fc86ba7350672c3c1587ed7a77b1195
Bob Carpenter <carp@alias-i.com>
2016-10-11 18:05:53 -0400

    reordered mix/mat.hpp includes

--- stan/math/mix/mat.hpp
+++ stan/math/mix/mat.hpp
@@ -1,20 +1,26 @@
 #ifndef STAN_MATH_MIX_MAT_HPP
 #define STAN_MATH_MIX_MAT_HPP
 
-#include <stan/math/fwd/core.hpp>
-#include <stan/math/fwd/scal/meta/is_fvar.hpp>
-#include <stan/math/fwd/scal/meta/partials_type.hpp>
+#include <stan/math/mix/mat/fun/typedefs.hpp>
 
-#include <stan/math/rev/core.hpp>
-#include <stan/math/rev/scal/meta/is_var.hpp>
-#include <stan/math/rev/scal/meta/partials_type.hpp>
+#include <stan/math/mix/mat/functor/derivative.hpp>
+#include <stan/math/mix/mat/functor/finite_diff_grad_hessian.hpp>
+#include <stan/math/mix/mat/functor/grad_hessian.hpp>
+#include <stan/math/mix/mat/functor/grad_tr_mat_times_hessian.hpp>
+#include <stan/math/mix/mat/functor/gradient_dot_vector.hpp>
+#include <stan/math/mix/mat/functor/hessian.hpp>
+#include <stan/math/mix/mat/functor/hessian_times_vector.hpp>
+#include <stan/math/mix/mat/functor/partial_derivative.hpp>
 
-#include <stan/math/prim/mat.hpp>
-#include <stan/math/fwd/mat.hpp>
 #include <stan/math/rev/mat.hpp>
 
+#include <stan/math/fwd/mat.hpp>
+
+#include <stan/math/prim/mat.hpp>
+
 // FIXME(carpenter): sort out ordering to include in rev, etc.
-// have to come after scalar (fwd, rev) defs
+// currently have to come after scalar (fwd, rev) defs
+// probably due to overincludes in some of the headers
 #include <stan/math/prim/mat/fun/acosh.hpp>
 #include <stan/math/prim/mat/fun/asinh.hpp>
 #include <stan/math/prim/mat/fun/atanh.hpp>
@@ -26,16 +32,5 @@
 #include <stan/math/prim/mat/fun/Phi_approx.hpp>
 #include <stan/math/prim/mat/fun/trigamma.hpp>
 #include <stan/math/prim/mat/fun/trunc.hpp>
-
-#include <stan/math/mix/mat/fun/typedefs.hpp>
-
-#include <stan/math/mix/mat/functor/derivative.hpp>
-#include <stan/math/mix/mat/functor/finite_diff_grad_hessian.hpp>
-#include <stan/math/mix/mat/functor/grad_hessian.hpp>
-#include <stan/math/mix/mat/functor/grad_tr_mat_times_hessian.hpp>
-#include <stan/math/mix/mat/functor/gradient_dot_vector.hpp>
-#include <stan/math/mix/mat/functor/hessian.hpp>
-#include <stan/math/mix/mat/functor/hessian_times_vector.hpp>
-#include <stan/math/mix/mat/functor/partial_derivative.hpp>
 
 #endif
