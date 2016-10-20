084c7e8ff961f39f8878627d4439a581f948fe00
Daniel Lee <bearlee@alum.mit.edu>
2016-10-20 00:56:28 -0400

    Checking that mat/prob functions put varis on the stack.

--- test/unit/math/rev/mat/prob/multi_normal_prec_test.cpp
+++ test/unit/math/rev/mat/prob/multi_normal_prec_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/math/rev/mat.hpp>
 #include <gtest/gtest.h>
+#include <test/unit/math/rev/mat/util.hpp>
 
 using Eigen::Dynamic;
 using Eigen::Matrix;
@@ -17,4 +18,24 @@ TEST(ProbDistributionsMultiNormalPrec,MultiNormalVar) {
     0.0, 0.0, 5.0;
   Matrix<var,Dynamic,Dynamic> L = Sigma.inverse();
   EXPECT_FLOAT_EQ(-11.73908, stan::math::multi_normal_prec_log(y,mu,L).val());
+}
+
+TEST(AgradRev, check_varis_on_stack) {
+  using stan::math::to_var;
+  Matrix<double,Dynamic,1> y(3,1);
+  y << 2.0, -2.0, 11.0;
+  Matrix<double,Dynamic,1> mu(3,1);
+  mu << 1.0, -1.0, 3.0;
+  Matrix<double,Dynamic,Dynamic> Sigma(3,3);
+  Sigma << 9.0, -3.0, 0.0,
+    -3.0,  4.0, 0.0,
+    0.0, 0.0, 5.0;
+  Matrix<double,Dynamic,Dynamic> L = Sigma.inverse();
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(to_var(y), to_var(mu), to_var(L)));
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(to_var(y), to_var(mu), L));
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(to_var(y), mu, to_var(L)));
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(to_var(y), mu, L));
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(y, to_var(mu), to_var(L)));
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(y, to_var(mu), L));
+  test::check_varis_on_stack(stan::math::multi_normal_prec_log(y, mu, to_var(L)));
 }
