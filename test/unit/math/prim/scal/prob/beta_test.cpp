ab0f377b3d8812c22731dccdc40762ca77982602
Thel Seraphim <thel.seraphim@gmail.com>
2016-10-31 12:11:53 -0400

    fix generation of gamma variates to avoid underflow for small parameters, similar to fix in dirichlet. add test for small params

--- test/unit/math/prim/scal/prob/beta_test.cpp
+++ test/unit/math/prim/scal/prob/beta_test.cpp
@@ -7,6 +7,7 @@
 TEST(ProbDistributionsBeta, error_check) {
   boost::random::mt19937 rng;
   EXPECT_NO_THROW(stan::math::beta_rng(2.0, 1.0, rng));
+  EXPECT_NO_THROW(stan::math::beta_rng(1e-10, 1e-10, rng));
 
   EXPECT_THROW(stan::math::beta_rng(2.0,-1.0, rng),std::domain_error);
   EXPECT_THROW(stan::math::beta_rng(-2.0, 1.0, rng),std::domain_error);
