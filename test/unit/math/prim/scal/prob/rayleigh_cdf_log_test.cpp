b5f754ef81a2e89b2ecf926c662cfedc332c0d55
Daniel Lee <bearlee@alum.mit.edu>
2016-11-23 10:53:54 -0500

    Adding _lpdf, _lpmf, _lcdf, _lccdf functions with tests.

---
+++ test/unit/math/prim/scal/prob/rayleigh_cdf_log_test.cpp
@@ -0,0 +1,12 @@
+#include <stan/math/prim/scal.hpp>
+#include <gtest/gtest.h>
+
+TEST(ProbRayleigh, cdf_log_matches_lcdf) {
+  double y = 0.8;
+  double sigma = 2.3;
+
+  EXPECT_FLOAT_EQ((stan::math::rayleigh_lcdf(y, sigma)),
+                  (stan::math::rayleigh_cdf_log(y, sigma)));
+  EXPECT_FLOAT_EQ((stan::math::rayleigh_lcdf<double, double>(y, sigma)),
+                  (stan::math::rayleigh_cdf_log<double, double>(y, sigma)));
+}
