a2219f4f1b74e9895b89a5ae76cc156fde8a73c2
Daniel Lee <bearlee@alum.mit.edu>
2016-11-23 10:53:29 -0500

    Adding _lpdf, _lpmf, _lcdf, _lccdf functions with tests.

---
+++ test/unit/math/prim/mat/prob/multi_normal_cholesky_log_test.cpp
@@ -0,0 +1,27 @@
+#include <stan/math/prim/mat.hpp>
+#include <gtest/gtest.h>
+
+TEST(ProbMultiNormalCholesky, log_matches_lpmf) {
+  Eigen::Matrix<double,Eigen::Dynamic,1> y(3,1);
+  y << 2.0, -2.0, 11.0;
+  Eigen::Matrix<double,Eigen::Dynamic,1> mu(3,1);
+  mu << 1.0, -1.0, 3.0;
+  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> Sigma(3,3);
+  Sigma << 9.0, -3.0, 0.0,
+    -3.0,  4.0, 0.0,
+    0.0, 0.0, 5.0;
+  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> L = Sigma.llt().matrixL();
+
+  EXPECT_FLOAT_EQ((stan::math::multi_normal_cholesky_lpdf(y,mu,L)),
+                  (stan::math::multi_normal_cholesky_log(y,mu,L)));
+  EXPECT_FLOAT_EQ((stan::math::multi_normal_cholesky_lpdf<true>(y,mu,L)),
+                  (stan::math::multi_normal_cholesky_log<true>(y,mu,L)));
+  EXPECT_FLOAT_EQ((stan::math::multi_normal_cholesky_lpdf<false>(y,mu,L)),
+                  (stan::math::multi_normal_cholesky_log<false>(y,mu,L)));
+  EXPECT_FLOAT_EQ((stan::math::multi_normal_cholesky_lpdf<true, Eigen::VectorXd, Eigen::VectorXd, Eigen::MatrixXd>(y,mu,L)),
+                  (stan::math::multi_normal_cholesky_log<true, Eigen::VectorXd, Eigen::VectorXd, Eigen::MatrixXd>(y,mu,L)));
+  EXPECT_FLOAT_EQ((stan::math::multi_normal_cholesky_lpdf<false, Eigen::VectorXd, Eigen::VectorXd, Eigen::MatrixXd>(y,mu,L)),
+                  (stan::math::multi_normal_cholesky_log<false, Eigen::VectorXd, Eigen::VectorXd, Eigen::MatrixXd>(y,mu,L)));
+  EXPECT_FLOAT_EQ((stan::math::multi_normal_cholesky_lpdf<Eigen::VectorXd, Eigen::VectorXd, Eigen::MatrixXd>(y,mu,L)),
+                  (stan::math::multi_normal_cholesky_log<Eigen::VectorXd, Eigen::VectorXd, Eigen::MatrixXd>(y,mu,L)));
+}
