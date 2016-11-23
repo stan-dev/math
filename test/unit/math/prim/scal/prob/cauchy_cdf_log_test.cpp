804bafc70d7af8f5a49a78bc80de4cb3b73262ba
Daniel Lee <bearlee@alum.mit.edu>
2016-11-23 10:53:33 -0500

    Adding _lpdf, _lpmf, _lcdf, _lccdf functions with tests.

---
+++ test/unit/math/prim/scal/prob/cauchy_cdf_log_test.cpp
@@ -0,0 +1,13 @@
+#include <stan/math/prim/scal.hpp>
+#include <gtest/gtest.h>
+
+TEST(ProbCauchy, cdf_log_matches_lcdf) {
+  double y = 2;
+  double mu = 1.5;
+  double sigma = 0.5;
+
+  EXPECT_FLOAT_EQ((stan::math::cauchy_lcdf(y, mu, sigma)),
+                  (stan::math::cauchy_cdf_log(y, mu, sigma)));
+  EXPECT_FLOAT_EQ((stan::math::cauchy_lcdf<double, double, double>(y, mu, sigma)),
+                  (stan::math::cauchy_cdf_log<double, double, double>(y, mu, sigma)));
+}
