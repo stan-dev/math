a33b27de157f3ba257df09e1bb49b843dc0d96fe
Thel Seraphim <thel.seraphim@gmail.com>
2016-12-23 15:26:53 -0800

    clean up test file

--- test/unit/math/rev/scal/prob/neg_binomial_2_test.cpp
+++ test/unit/math/rev/scal/prob/neg_binomial_2_test.cpp
@@ -2,19 +2,19 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-TEST(ProbDistributionsNegBinomial, derivatives_not_nan) {
+TEST(ProbDistributionsNegBinomial, derivatives) {
   using stan::math::is_nan;
   using stan::math::var;
   using stan::math::neg_binomial_2_log;
 
   int N = 100;
   double mu_dbl = 8;
-  double phi_dbl = 1;
+  double phi_dbl = 1.5;
   
-  var mu(mu_dbl);
-  var phi(phi_dbl);
 
   for (int k = 0; k < 20; ++k) {
+    var mu(mu_dbl);
+    var phi(phi_dbl);
     var val = neg_binomial_2_log(N, mu, phi);
 
     std::vector<var> x;
@@ -24,80 +24,24 @@ TEST(ProbDistributionsNegBinomial, derivatives_not_nan) {
     std::vector<double> gradients;
     val.grad(x, gradients);
 
-    EXPECT_FALSE(is_nan(gradients[0]));
-    EXPECT_FALSE(is_nan(gradients[1]));
+    for (int i = 0; i < 2; ++i) {
+      EXPECT_FALSE(is_nan(gradients[i]));
+    }
 
-    phi *= 10;
-  }
+    std::vector<double> finite_diffs;
+    double eps = 1e-10;
+    double inv2e = 0.5 / eps;
+    double dmu = neg_binomial_2_log(N, mu_dbl + eps, phi_dbl) 
+      - neg_binomial_2_log(N, mu_dbl - eps, phi_dbl);
+    double dphi = neg_binomial_2_log(N, mu_dbl, phi_dbl + eps) 
+      - neg_binomial_2_log(N, mu_dbl, phi_dbl - eps);
+    finite_diffs.push_back(dmu * inv2e);
+    finite_diffs.push_back(dphi * inv2e);
 
-}
+    for (int i = 0; i < 2; ++i) {
+      EXPECT_NEAR(gradients[i], finite_diffs[i], 1.0);
+    }
 
-void test_value_and_derivatives(double expected_val,
-                                double y_dbl, double mu_dbl, double sigma_dbl) {
-  using stan::math::is_nan;
-  using stan::math::var;  
-  using stan::math::normal_cdf;
-  std::stringstream msg_ss;
-  msg_ss << "parameters: (" << y_dbl << ", " << mu_dbl << ", " << sigma_dbl << ")";
-  std::string msg = msg_ss.str();
-
-  SCOPED_TRACE(msg);
-  
-  var y(y_dbl);
-  var mu(mu_dbl);
-  var sigma(sigma_dbl);
-
-  std::vector<double> gradients;
-  var val = normal_cdf(y, mu, sigma);
-  std::vector<var> x;
-  x.push_back(y);
-  x.push_back(mu);
-  x.push_back(sigma);
-  gradients.clear();
-  val.grad(x, gradients);
-
-  double e = 1e-10;
-  double inv2e = 0.5 / e;
-  std::vector<double> finite_diffs;
-  finite_diffs.resize(3);
-  finite_diffs[0] = (normal_cdf(y_dbl + e, mu_dbl, sigma_dbl)
-                     - normal_cdf(y_dbl - e, mu_dbl, sigma_dbl)) * inv2e;
-  finite_diffs[1] = (normal_cdf(y_dbl, mu_dbl + e, sigma_dbl)
-                     - normal_cdf(y_dbl, mu_dbl - e, sigma_dbl)) * inv2e;
-  finite_diffs[2] = (normal_cdf(y_dbl, mu_dbl, sigma_dbl + e)
-                     - normal_cdf(y_dbl, mu_dbl, sigma_dbl - e)) * inv2e;
-
-  
-  EXPECT_FLOAT_EQ(expected_val, val.val());
-  EXPECT_FALSE(is_nan(gradients[0]));
-  EXPECT_FALSE(is_nan(gradients[1]));
-  EXPECT_FALSE(is_nan(gradients[2]));
-  if (!is_nan(gradients[0])) {
-    if (!is_nan(finite_diffs[0]))
-      EXPECT_NEAR(finite_diffs[0], gradients[0], 1e-2);
-    else
-      EXPECT_FLOAT_EQ(0.0, gradients[0]);
-  }
-  if (!is_nan(gradients[1])) {
-    if (!is_nan(finite_diffs[1]))
-      EXPECT_NEAR(finite_diffs[1], gradients[1], 1e-2);
-    else
-      EXPECT_FLOAT_EQ(0.0, gradients[1]);
-  }
-  if (!is_nan(gradients[2])) {
-    if (!is_nan(finite_diffs[2]))
-      EXPECT_NEAR(finite_diffs[2], gradients[2], 1e-2);
-    else
-      EXPECT_FLOAT_EQ(0.0, gradients[2]);
+    phi_dbl *= 10;
   }
 }
-
-
-// TEST(normal_cdf, derivatives) {
-//   test_value_and_derivatives(0.5, 10.0, 10.0, 0.5);
-//   test_value_and_derivatives(0.0, -20.0, 10.0, 0.5);
-//   test_value_and_derivatives(0.0, -30.0, 10.0, 0.5);
-//   test_value_and_derivatives(0.0, -50.0, 10.0, 1.0);
-// 
-//   test_value_and_derivatives(1.0, 30.0, 10.0, 0.5);
-// }
