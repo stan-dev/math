bc1b45dd9961018d79fa94e6d472e8a607523a26
Thel Seraphim <thel.seraphim@gmail.com>
2016-12-26 16:28:50 -0800

    add tests asserting chain rule

---
+++ test/unit/math/rev/scal/prob/poisson_log_cdf_test.cpp
@@ -0,0 +1,39 @@
+#include <stan/math/rev/scal.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+
+TEST(ProbPoisson, log_cdf_chain_rule) {
+  using std::exp;
+
+  using stan::math::is_nan;
+  using stan::math::var;
+
+  using stan::math::poisson_log_cdf;
+  using stan::math::poisson_cdf;
+
+  int count = 20;
+  double log_rate_dbl = 2.1;
+  double rate_dbl = exp(log_rate_dbl);
+  
+  var log_rate(log_rate_dbl);
+  var rate(rate_dbl);
+
+  var val1 = poisson_log_cdf(count, log_rate);
+  var val2 = poisson_cdf(count, rate);
+  
+  std::vector<var> x1;
+  x1.push_back(log_rate);
+
+  std::vector<var> x2;
+  x2.push_back(rate);
+
+  std::vector<double> gradients1;
+  std::vector<double> gradients2;
+
+  val1.grad(x1, gradients1);
+  val2.grad(x2, gradients2);
+
+  EXPECT_FALSE(is_nan(gradients1[0]));
+  EXPECT_FALSE(is_nan(gradients2[0]));
+  EXPECT_NEAR(gradients1[0], gradients2[0] * rate_dbl, 0.01);
+}
