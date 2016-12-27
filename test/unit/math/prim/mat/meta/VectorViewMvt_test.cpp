8b821603a6041bee74f481ae452ae8e8389bf346
Sean Talts <sean.talts@gmail.com>
2016-12-27 17:27:36 -0500

    Address pull request comments
    Fix some style issues, test Matrices as well as the two vector types
    (apparently they are different underlying classes after all).

--- test/unit/math/prim/mat/meta/VectorViewMvt_test.cpp
+++ test/unit/math/prim/mat/meta/VectorViewMvt_test.cpp
@@ -5,47 +5,87 @@
 #include <vector>
 
 // Just test the use case where a single matrix is passed in to VectorViewMvt.
-// The case where a std::vector of matrices is passed in is broken and will be refactored.
+// The case where a std::vector of matrices is passed in is broken and will be
+// refactored.
 
-TEST(MetaTraits, VectorViewMvt_matrix_double) {
+TEST(MetaTraits, VectorViewMvt_vector_double) {
   using stan::VectorViewMvt;
-  using Eigen::Matrix;
+  using Eigen::VectorXd;
+  using Eigen::RowVectorXd;
   using Eigen::Dynamic;
 
-  Matrix<double,Dynamic,1> a(10);
+  VectorXd a(10);
   for (size_t n = 0; n < 10; ++n)
     a[n] = n;
-  VectorViewMvt<Matrix<double,Dynamic,1> > av(a);
+  VectorViewMvt<VectorXd> av(a);
   for (size_t n = 0; n < 10; ++n)
     EXPECT_FLOAT_EQ(a[n], av[0][n]);
   for (size_t n = 0; n < 10; ++n)
-    av[120*n][n] = n+10;
+    av[120*n][n] = n + 10;
   for (size_t n = 0; n < 10; ++n) {
-    EXPECT_FLOAT_EQ(10+n, av[0][n]);
-    EXPECT_FLOAT_EQ(10+n, a[n]);
+    EXPECT_FLOAT_EQ(10 + n, av[0][n]);
+    EXPECT_FLOAT_EQ(10 + n, a[n]);
   }
 
-  const Matrix<double,Dynamic,1> b(a);
-  VectorViewMvt<const Matrix<double,Dynamic,1> > bv(b);
+  const VectorXd b(a);
+  VectorViewMvt<const VectorXd> bv(b);
   for (size_t n = 0; n < 10; ++n)
     EXPECT_FLOAT_EQ(b[n], bv[0][n]);
 
-  Matrix<double,1,Dynamic> c(10);
+  RowVectorXd c(10);
   for (size_t n = 0; n < 10; ++n)
     c[n] = n;
-  VectorViewMvt<Matrix<double,1,Dynamic> > cv(c);
+  VectorViewMvt<RowVectorXd> cv(c);
   for (size_t n = 0; n < 10; ++n)
     EXPECT_FLOAT_EQ(c[n], cv[0][n]);
   for (size_t n = 0; n < 10; ++n)
-    cv[0][n] = n+10;
+    cv[0][n] = n + 10;
   for (size_t n = 0; n < 10; ++n) {
-    EXPECT_FLOAT_EQ(10+n, cv[0][n]);
-    EXPECT_FLOAT_EQ(10+n, c[n]);
+    EXPECT_FLOAT_EQ(10 + n, cv[0][n]);
+    EXPECT_FLOAT_EQ(10 + n, c[n]);
   }
 
-  const Matrix<double,1,Dynamic> d(c);
-  VectorViewMvt<const Matrix<double,1,Dynamic>,
-          stan::is_vector<Matrix<double,1,Dynamic> >::value > dv(d);
+  const RowVectorXd d(c);
+  VectorViewMvt<const RowVectorXd, stan::is_vector<RowVectorXd>::value > dv(d);
   for (size_t n = 0; n < 10; ++n)
     EXPECT_FLOAT_EQ(d[n], dv[0][n]);
+}
+
+TEST(MetaTraits, VectorViewMvt_matrix_double) {
+  using stan::VectorViewMvt;
+  using Eigen::MatrixXd;
+  using Eigen::Dynamic;
+
+  MatrixXd a(10, 10);
+  for (size_t i = 0; i < 10; ++i) {
+    for (size_t j = 0; j < 10; ++j) {
+      a(i, j) = 100 * i + j;
+    }
+  }
+  VectorViewMvt<MatrixXd> av(a);
+  for (size_t i = 0; i < 10; ++i) {
+    for (size_t j = 0; j < 10; ++j) {
+      EXPECT_FLOAT_EQ(av[0](i, j), a(i, j));
+    }
+  }
+
+  //Test mutation passes thru to original matrix
+  for (size_t i = 0; i < 10; ++i) {
+    for (size_t j = 0; j < 10; ++j) {
+      av[0](i, j) = a(i, j) + 10;
+    }
+  }
+  for (size_t i = 0; i < 10; ++i) {
+    for (size_t j = 0; j < 10; ++j) {
+      EXPECT_FLOAT_EQ(av[0](i, j), a(i, j));
+    }
+  }
+
+  const MatrixXd b(a);
+  VectorViewMvt<const MatrixXd> bv(b);
+  for (size_t i = 0; i < 10; ++i) {
+    for (size_t j = 0; j < 10; ++j) {
+      EXPECT_FLOAT_EQ(bv[0](i, j), b(i, j));
+    }
+  }
 }
