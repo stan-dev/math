1ef014f8ad0d313c5426333e686d7ba3703af176
Charles Margossian <charlesm@metrumrg.com>
2017-02-03 16:10:25 -0500

    Include example running Eigen Hybrid solver in "unit test"

--- stan/math/prim/mat/fun/dogleg.hpp
+++ stan/math/prim/mat/fun/dogleg.hpp
@@ -6,10 +6,56 @@
 
 namespace stan {
   namespace math {
-
+/*
     struct NLOFunctor {
       int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const;
       int df(const Eigen::VectorXd &x, Eigen::MatrixXd &fjac) const;
+    }; */
+
+    template <typename _Scalar, int NX = Eigen::Dynamic, int NY = Eigen::Dynamic>
+    struct NLOFunctor {
+      typedef _Scalar Scalar;
+      enum {
+        InputsAtCompileTime = NX,
+        ValuesAtCompileTime = NY
+      };
+
+      typedef Eigen::Matrix<Scalar, InputsAtCompileTime, 1> InputType;
+      typedef Eigen::Matrix<Scalar, ValuesAtCompileTime, 1> ValueType;
+      typedef Eigen::Matrix<Scalar,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
+
+      const int m_inputs, m_values;
+
+      NLOFunctor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
+      NLOFunctor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
+
+      int inputs() const { return m_inputs; }
+      int values() const { return m_values; }
+      
+      // int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const;
+      // int df(const Eigen::VectorXd &x, Eigen::MatrixXd &fjac) const;
+    };
+
+    template <typename F1, typename F2>
+    struct hybrj_functor : NLOFunctor<double> {
+    private:
+      F1 f1;
+      F2 f2;
+
+    public:
+      hybrj_functor(const F1& f1_param, const F2& f2_param) {
+        f1 = f1_param;
+        f2 = f2_param;
+      }
+
+      int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) {
+        fvec = f1(x);
+        return 0;
+      }
+      int df(const Eigen::VectorXd &x, Eigen::MatrixXd &fjac) {
+        fjac = f2(x);
+        return 0;
+      }
     };
 
     /**
@@ -18,29 +64,25 @@ namespace stan {
      * @param F2 Functor that evaluates the Jacobian matrix
      * @return Vector that solves the system of equations
      */
+    // template <typename F1, typename F2>
     template <typename F1, typename F2>
     inline
     Eigen::VectorXd
-    dogleg(const Eigen::VectorXd& x, const F1, const F2) {
+    dogleg(const Eigen::VectorXd& x, const F1 f1, const F2 f2) {
 
-      struct hybrj_functor : NLOFunctor {
-
-        int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) {
-          fvec = F1(x);
-          return 0;
-        }
-        int df(const Eigen::VectorXd &x, Eigen::MatrixXd &fjac) {
-          fjac = F2(x);
-          return 0;
-        }
-      };
-
-      hybrj_functor functor;
-      Eigen::HybridNonLinearSolver<NLOFunctor> solver(functor);
-      Eigen::VectorXd theta = x;
+      stan::math::hybrj_functor<F1, F2> functor(f1, f2);
+      Eigen::HybridNonLinearSolver<NLOFunctor<double> > solver(functor);
+      Eigen::VectorXd theta = x, fvec;
+      functor(x, fvec);
+      /*std::cout << x << std::endl;
+      std::cout << fvec << std::endl;
+      Eigen::MatrixXd fjac;
+      functor.df(x, fjac);
+      std::cout << fjac << std::endl; */
       solver.solve(theta);
       return theta;
     }
+
   }
 }
 #endif
--- test/unit/math/prim/mat/fun/dogleg_test.cpp
+++ test/unit/math/prim/mat/fun/dogleg_test.cpp
@@ -35,10 +35,75 @@ struct jacobian_functor {
   }
 };
 
+
+// will want to remove these eventually
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using Eigen::Matrix;
+using Eigen::Dynamic;
+
+/***
+ * Implement original Eigen hybrid solver.
+ */
+template <typename _Scalar, int NX = Dynamic, int NY = Dynamic>
+struct Functor {
+  typedef _Scalar Scalar;
+  enum {
+    InputsAtCompileTime = NX,
+    ValuesAtCompileTime = NY
+  };
+  
+  typedef Matrix<Scalar, InputsAtCompileTime, 1> InputType;
+  typedef Matrix<Scalar, ValuesAtCompileTime, 1> ValueType;
+  typedef Matrix<Scalar,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
+  
+  const int m_inputs, m_values;
+  
+  Functor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
+  Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
+  
+  int inputs() const { return m_inputs; }
+  int values() const { return m_values; }
+};
+
+struct hybrj_functor : Functor<double> {
+  hybrj_functor(void) : Functor<double>(9, 9) { }
+  
+  int operator()(const VectorXd& x, VectorXd& fvec) {
+    fvec = algebraEq(x);
+    return 0;
+  }
+
+  int df(const VectorXd& x, MatrixXd& fjac) {
+    fjac = jacobian(x);
+    return 0;
+  }
+};
+
+TEST(MathMatrix, hybrid_eigen) {
+
+  const int n = 2;
+  VectorXd x(n);
+  x << 32, 5;
+
+  // do the computation
+  hybrj_functor functor;
+  Eigen::HybridNonLinearSolver<hybrj_functor> solver(functor);
+  Eigen::VectorXd theta = x;
+  solver.solve(theta);
+  
+  EXPECT_EQ(36, theta(0));
+  EXPECT_EQ(6, theta(1));
+}
+
+/*
+ * Test dogleg function.
+ */ /*
 TEST(MathMatrix, dogleg) {
   Eigen::VectorXd x(2);
   x << 32, 5;
 
   Eigen::VectorXd theta;
   theta = stan::math::dogleg(x, algebraEq_functor(), jacobian_functor());
-}
+} */
+
