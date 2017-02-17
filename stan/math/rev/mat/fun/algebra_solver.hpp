2951c731f43552526d724ee15a4b858de34f46ab
Charles Margossian <charlesm@metrumrg.com>
2017-02-17 11:48:13 -0500

    basic implementation of function calculating the Jacobian in the algebra solver.

--- stan/math/rev/mat/fun/algebra_solver.hpp
+++ stan/math/rev/mat/fun/algebra_solver.hpp
@@ -4,45 +4,45 @@
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include<stan/math/prim/mat/fun/dogleg.hpp>
 #include <unsupported/Eigen/NonLinearOptimization>
+#include <stan/math/rev/mat/functor/jacobian.hpp>
+#include <stan/math/prim/mat/fun/inverse.hpp>
 
 namespace stan {
   namespace math {
 
-    template <typename F1, typename F2, typename T>
+    template <typename F1, typename T1, typename T2>
     struct hybrj_functor_solver : stan::math::NLOFunctor<double> {
     private:
       F1 f1;
-      F2 f2;
-//      std::vector<T> parms;
-//      std::vector<double> dat;
-//      std::vector<int> dat_int;
+      Eigen::MatrixXd J;
 
     public:
       hybrj_functor_solver(const F1& f1_param,
-                           const F2& f2_param,
-                           const std::vector<T> parms_param,
-                           const std::vector<double> dat_param,
-                           const std::vector<int> dat_int_param) :
-                           f1(parms_param, dat_param, dat_int_param),
-                           f2(parms_param, dat_param, dat_int_param) {
-//        f1 = f1_param(parms_param, dat_param, dat_int_param);
-//        f2 = f2_param(parms_param, dat_param, dat_int_param);
-//        parms = parms_param;
-//        dat = dat_param;
-//        dat_int = dat_int_param;
-      }
+                           const Eigen::Matrix<T1, Eigen::Dynamic, 1> theta,
+                           const Eigen::Matrix<T2, Eigen::Dynamic, 1> parms,
+                           const std::vector<double> dat,
+                           const std::vector<int> dat_int,
+                           const std::string variable) :
+                           f1(theta, parms, dat, dat_int, variable) { }
 
       int operator()(const Eigen::VectorXd &x, 
                      Eigen::VectorXd &fvec) {
-        fvec = f1(x);
+        stan::math::jacobian(f1, x, fvec, J);
         return 0;
       }
+
       int df(const Eigen::VectorXd &x, Eigen::MatrixXd &fjac) {
-        fjac = f2(x);
+        fjac = J;  //f2(x);
         return 0;
+      }
+
+      Eigen::MatrixXd get_jacobian(const Eigen::VectorXd &parms) {
+        Eigen::VectorXd fvec;
+        stan::math::jacobian(f1, parms, fvec, J);
+        return J;
       }
     };
-    
+
     /**
      * @tparam F1 type of equation system function.
      * @tparam F2 type of Jacobian system function.
@@ -56,22 +56,44 @@ namespace stan {
      * @param[in] dat_int integer data vector for the equation system.
      * @return Vector that solves the system of equations.
      */
-     template <typename F1, typename F2, typename T1, typename T2>
+     template <typename F1, typename T1, typename T2>
      inline
      Eigen::Matrix<typename stan::return_type<T1, T2>::type,
        Eigen::Dynamic, Eigen::Dynamic>
      algebra_solver(const F1& f1,
-                    const F2& f2,
                     const Eigen::Matrix<T1, Eigen::Dynamic, 1>& x,
-                    const std::vector<T2>& parms,
+                    const Eigen::Matrix<T2, Eigen::Dynamic, 1>& parms,
                     const std::vector<double>& dat,
                     const std::vector<int>& dat_int) {
-       stan::math::hybrj_functor_solver<F1, F2, T2>
-         functor(f1, f2, parms, dat, dat_int);
-       Eigen::HybridNonLinearSolver<hybrj_functor_solver<F1, F2, T2> >
+       typedef typename boost::math::tools::promote_args<T1, T2>::type scalar;
+
+       stan::math::hybrj_functor_solver<F1, T1, T2>
+         functor(f1, x, parms, dat, dat_int, "theta");
+       Eigen::HybridNonLinearSolver<hybrj_functor_solver<F1, T1, T2> >
          solver(functor);
-       Eigen::Matrix<T1, Eigen::Dynamic, 1> theta = x;  // CHECK - element type for theta
+       Eigen::Matrix<scalar, Eigen::Dynamic, 1> theta = x;
        solver.solve(theta);
+
+       // CHECK - should the argument be x or theta ?
+       // Do we care about the derivative w.r.t x.
+       Eigen::MatrixXd Jf_x = functor.get_jacobian(theta);
+
+       stan::math::hybrj_functor_solver<F1, T1, T2>
+         functor_parm(f1, theta, parms, dat, dat_int, "parms");
+       Eigen::MatrixXd Jf_p = functor_parm.get_jacobian(parms);
+       
+       Eigen::MatrixXd Jx_p = - stan::math::inverse(Jf_x) * Jf_p;
+
+       // print statements
+       /*
+       using std::cout;
+       using std::endl;
+       cout << "Jf_x" << endl << Jf_x << endl;
+       cout << "Jf_p" << endl << Jf_p << endl;
+       cout << "inv(Jf_x)" << endl << stan::math::inverse(Jf_x) << endl;
+       cout << "Jx_p" << endl << Jx_p << endl << endl;
+       */
+       
        return theta;
     }
   }
--- test/unit/math/rev/mat/fun/algebra_solver_test.cpp
+++ test/unit/math/rev/mat/fun/algebra_solver_test.cpp
@@ -1,6 +1,7 @@
-#include <stan/math/prim/mat.hpp>
+#include <stan/math/rev/core.hpp>
+#include <stan/math/prim/mat.hpp>  // probably don't need this include
 #include <gtest/gtest.h>
-#include <stan/math/prim/mat/fun/algebra_solver.hpp>
+#include <stan/math/rev/mat/fun/algebra_solver.hpp>
 
 // will want to remove these eventually
 using Eigen::VectorXd;
@@ -8,99 +9,141 @@ using Eigen::MatrixXd;
 using Eigen::Matrix;
 using Eigen::Dynamic;
 
-template <typename T>
+template <typename T1, typename T2>
 inline
-Eigen::Matrix<T, Eigen::Dynamic, 1>
-algebraEq(const Eigen::VectorXd x,
-          const std::vector<T>& parms,
+Eigen::Matrix<typename boost::math::tools::promote_args<T1, T2>::type, 
+              Eigen::Dynamic, 1>
+algebraEq(const Eigen::Matrix<T1, Eigen::Dynamic, 1> x,
+          const Eigen::Matrix<T2, Eigen::Dynamic, 1>& parms,
           const std::vector<double>& dat,
           const std::vector<int>& dat_int) {
-  T a0 = parms[0], a1 = parms[1];
-  Eigen::Matrix<T, Dynamic, 1> y(2);
-  y(0) = x(0) - a0;
-  y(1) = x(1) - a1;
+  typedef typename boost::math::tools::promote_args<T1, T2>::type scalar;
+  Eigen::Matrix<scalar, Dynamic, 1> y(2);
+  y(0) = x(0) - parms(0);
+  y(1) = x(1) - parms(1);
   return y;
 }
 
-template <typename T>
+template <typename T1, typename T2>
 struct algebraEq_functor {
 private:
-  std::vector<T> parms;
+  Eigen::Matrix<T1, Eigen::Dynamic, 1> theta;
+  Eigen::Matrix<T2, Eigen::Dynamic, 1> parms;
   std::vector<double> dat;
   std::vector<int> dat_int;
-  
+  std::string variable;
+
 public:
-  algebraEq_functor(const std::vector<T> parms_para,
-                    const std::vector<double> dat_para,
-                    const std::vector<int> dat_int_para) {
+  algebraEq_functor() { };
+
+  algebraEq_functor(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta_para,
+                    const Eigen::Matrix<T2, Eigen::Dynamic, 1>& parms_para,
+                    const std::vector<double>& dat_para,
+                    const std::vector<int>& dat_int_para,
+                    const std::string& variable_para) {
+    theta = theta_para;
     parms = parms_para;
     dat = dat_para;
     dat_int = dat_int_para;
+    variable = variable_para;
   }
 
-  inline Eigen::VectorXd
-  operator()(const Eigen::VectorXd x) const {
-    return algebraEq(x, parms, dat, dat_int);
-  }
-};
-
-template <typename T>
-inline Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
-jacobian(const Eigen::VectorXd x,
-         const std::vector<T>& parms,
-         const std::vector<double>& dat,
-         const std::vector<int>& dat_int) {
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> y(2, 2);
-  y(0, 0) = 1;
-  y(0, 1) = 0;
-  y(1, 0) = 0;
-  y(1, 1) = 1;
-  return y;
-}
-
-template <typename T>
-struct jacobian_functor {
-private:
-  std::vector<T> parms;
-  std::vector<double> dat;
-  std::vector<int> dat_int;
-
-public:
-  jacobian_functor(const std::vector<T>& parms_para,
-                   const std::vector<double>& dat_para,
-                   const std::vector<int>& dat_int_para) {
-    parms = parms_para;
-    dat = dat_para;
-    dat_int = dat_int_para;
-  }
-
+  template <typename T>
   inline
-  Eigen::MatrixXd
-  operator()(const Eigen::VectorXd x) const {
-    return jacobian(x, parms, dat, dat_int);
+  Eigen::Matrix<typename boost::math::tools::promote_args<T, T1, T2>::type,
+                Eigen::Dynamic, 1>
+  operator()(const Eigen::Matrix<T, Eigen::Dynamic, 1> x) const {
+    if (variable == "theta")
+      return algebraEq(x, parms, dat, dat_int);
+    else
+      return algebraEq(theta, x, dat, dat_int);
   }
 };
 
 
-TEST(MathMatrix, dogleg_eq1) {
-  Eigen::VectorXd x(2);
+TEST(MathMatrix, algebra_solver_eq1) {
+  Eigen::VectorXd x(2), parms(2);
   x << 32, 5;
-
-  std::vector<double> parms(2);
-  parms[0] = 36;
-  parms[1] = 6;
+  parms << 36, 6;
 
   std::vector<double> dummy_dat(0);
   std::vector<int> dummy_dat_int(0);
 
-  algebraEq_functor<double> equation(parms, dummy_dat, dummy_dat_int);
-  jacobian_functor<double> jacobian(parms, dummy_dat, dummy_dat_int);
-
   Eigen::VectorXd theta;
-  theta = stan::math::algebra_solver(equation, 
-                                     jacobian,
+  theta = stan::math::algebra_solver(algebraEq_functor<double, double>(),
                                      x, parms, dummy_dat, dummy_dat_int);
 
   EXPECT_EQ(36, theta(0));
   EXPECT_EQ(6, theta(1));
 }
+
+
+template <typename T1, typename T2>
+inline
+Eigen::Matrix<typename boost::math::tools::promote_args<T1, T2>::type, 
+              Eigen::Dynamic, 1>
+algebraEq2(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& x,
+           const Eigen::Matrix<T2, Eigen::Dynamic, 1>& parms,
+           const std::vector<double>& dat,
+           const std::vector<int>& dat_int) {
+  typedef typename boost::math::tools::promote_args<T1, T2>::type scalar;
+  Eigen::Matrix<scalar, Eigen::Dynamic, 1> y(3);
+  y(0) = x(0) - parms(0);
+  y(1) = x(1) - parms(1);
+  y(2) = x(2) * parms(2);
+  return y;
+}
+
+template <typename T1, typename T2>
+struct algebraEq2_functor {
+private:
+  Eigen::Matrix<T1, Eigen::Dynamic, 1> theta;
+  Eigen::Matrix<T2, Eigen::Dynamic, 1> parms;
+  std::vector<double> dat;
+  std::vector<int> dat_int;
+  std::string variable;
+
+public:
+  algebraEq2_functor() { };
+
+  algebraEq2_functor(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta_para,
+                     const Eigen::Matrix<T2, Eigen::Dynamic, 1>& parms_para,
+                     const std::vector<double>& dat_para,
+                     const std::vector<int>& dat_int_para,
+                     const std::string& variable_para) {
+    theta = theta_para;
+    parms = parms_para;
+    dat = dat_para;
+    dat_int = dat_int_para;
+    variable = variable_para;
+  }
+
+  template <typename T>
+  inline
+  Eigen::Matrix<typename boost::math::tools::promote_args<T, T1, T2>::type,
+                Eigen::Dynamic, 1>
+  operator()(const Eigen::Matrix<T, Eigen::Dynamic, 1> x) const {
+    if (variable == "theta")
+      return algebraEq2(x, parms, dat, dat_int);
+    else
+      return algebraEq2(theta, x, dat, dat_int);
+  }
+};
+
+TEST(MathMatrix, algebra_solver_eq2) {
+  Eigen::VectorXd x(3), parms(3);
+  x << 32, 5, 2;
+  parms << 36, 6, 42;
+
+  std::vector<double> dummy_dat(0);
+  std::vector<int> dummy_dat_int(0);
+
+  Eigen::VectorXd theta;
+  theta = stan::math::algebra_solver(algebraEq2_functor<double, double>(),
+                                     x, parms, dummy_dat, dummy_dat_int);
+
+  EXPECT_EQ(36, theta(0));
+  EXPECT_EQ(6, theta(1));
+  EXPECT_EQ(0, theta(2));
+}
+
