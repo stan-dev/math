c705a7164564d948db12842013dc0830a3561e44
Charles Margossian <charlesm@metrumrg.com>
2017-02-23 13:38:13 -0500

    add harder test (with nonlinear system).

--- test/unit/math/rev/mat/functor/algebra_solver_test.cpp
+++ test/unit/math/rev/mat/functor/algebra_solver_test.cpp
@@ -98,7 +98,7 @@ TEST(MathMatrix, algebra_solver_eq1) {
   
 }
 
-/*
+
 template <typename T1, typename T2>
 inline Eigen::Matrix<typename boost::math::tools::promote_args<T1, T2>::type,
                      Eigen::Dynamic, 1>
@@ -108,9 +108,9 @@ algebraEq2(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& x,
            const std::vector<int>& dat_int) {
   typedef typename boost::math::tools::promote_args<T1, T2>::type scalar;
   Eigen::Matrix<scalar, Eigen::Dynamic, 1> y(3);
-  y(0) = x(0) - parms(0);
-  y(1) = x(1) - parms(1);
-  y(2) = x(2) - parms(2);
+  y(0) = x(0) * x(1) * x(2) - parms(0);
+  y(1) = x(0) - parms(1);
+  y(2) = x(1) - parms(2);
   return y;
 }
 
@@ -144,9 +144,9 @@ public:
                 Eigen::Dynamic, 1>
   operator()(const Eigen::Matrix<T, Eigen::Dynamic, 1> x) const {
     if (variable == "theta")
-      return algebraEq(x, parms, dat, dat_int);
+      return algebraEq2(x, parms, dat, dat_int);
     else
-      return algebraEq(theta, x, dat, dat_int);
+      return algebraEq2(theta, x, dat, dat_int);
   }
 };
 
@@ -161,30 +161,35 @@ TEST(MathMatrix, algebra_solver_eq2) {
   for (int k = 0; k < nParms; k++) {
     Eigen::VectorXd x(nSolutions);
     x << 1, 1, 1;  // initial guess
-    
+
     Matrix<var, Dynamic, 1> parms(nParms);
-    parms << 36, 6, 42;
-    
+    parms << 5, 4, 2;
+
     std::vector<double> dummy_dat(0);
     std::vector<int> dummy_dat_int(0);
-    
+
     Matrix<var, Dynamic, 1> theta;
     theta = algebra_solver(algebraEq2_functor<double, double>(),
                            x, parms, dummy_dat, dummy_dat_int);
-    
-    EXPECT_EQ(36, theta(0));
-    EXPECT_EQ(6, theta(1));
-    EXPECT_EQ(42, theta(2));
-    
+
+    EXPECT_EQ(4, theta(0));
+    EXPECT_EQ(2, theta(1));
+    EXPECT_EQ(0.625, theta(2));
+
+    double J31 = 1 / (parms(1).val() * parms(2).val());
+    double J32 = - parms(0).val() / (parms(1).val() * parms(1).val()
+                                      * parms(2).val());
+    double J33 = - parms(0).val() / (parms(1).val() * parms(2).val()
+                                      * parms(2).val());
     Matrix<double, Dynamic, Dynamic> dtheta_dparms(nParms, nSolutions);
-    dtheta_dparms << 1, 0, 0,
-                     0, 1, 0,
-                     0, 0, 1;
-    
+    dtheta_dparms << 0, 1, 0,
+                     0, 0, 1,
+                     J31, J32, J33;
+
     AVEC parms_vec = createAVEC(parms(0), parms(1), parms(2));
     VEC g;
     theta(k).grad(parms_vec, g);
     for (int i = 0; i < nParms; i++)
       EXPECT_EQ(dtheta_dparms(k, i), g[i]);
   }
-} */
+}
