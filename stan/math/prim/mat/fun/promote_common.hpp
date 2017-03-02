ef13066b449108bb6c5a61a9e359584e9302a020
Mitzi Morris <mitzi@panix.com>
2017-03-02 13:42:44 -0500

    better doc comment

--- stan/math/prim/mat/fun/promote_common.hpp
+++ stan/math/prim/mat/fun/promote_common.hpp
@@ -13,7 +13,7 @@ namespace stan {
      *
      * @tparam T1 first type
      * @tparam T2 second type
-     * @tparam F container type (deduced from param u)
+     * @tparam F container type (deduced from type of param u)
      * @param u elements to promote
      * @return the result of promoting elements
      */
