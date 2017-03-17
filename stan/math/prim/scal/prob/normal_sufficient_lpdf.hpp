3ae571f7e08188963b36892adb46a4ca158eaa48
Marco Inacio <marcoigarapava@gmail.com>
2017-03-17 12:57:49 -0300

    doc and fixes

--- stan/math/prim/scal/prob/normal_sufficient_lpdf.hpp
+++ stan/math/prim/scal/prob/normal_sufficient_lpdf.hpp
@@ -8,8 +8,8 @@
 #include <stan/math/prim/scal/meta/scalar_seq_view.hpp>
 #include <stan/math/prim/scal/err/check_consistent_sizes.hpp>
 #include <stan/math/prim/scal/err/check_finite.hpp>
-#include <stan/math/prim/scal/err/check_not_nan.hpp>
 #include <stan/math/prim/scal/err/check_positive.hpp>
+#include <stan/math/prim/scal/err/check_nonnegative.hpp>
 #include <stan/math/prim/scal/fun/constants.hpp>
 #include <stan/math/prim/scal/fun/value_of.hpp>
 #include <stan/math/prim/scal/meta/include_summand.hpp>
@@ -22,19 +22,26 @@ namespace stan {
 
     /**
      * The log of the normal density for the specified scalar(s) given
-     * the specified mean(s) and deviation(s). y, mu, or sigma can
-     * each be either a scalar or a std vector. Any vector inputs
-     * must be the same length.
+     * the specified mean(s) and deviation(s).
+     * y, s_quared, mu, or sigma can each be either
+     * a scalar, a std vector or Eigen vector.
+     * n can be either a single int or an std vector of ints.
+     * Any vector inputs must be the same length.
      *
      * <p>The result log probability is defined to be the sum of the
      * log probabilities for each observation/mean/deviation triple.
-     * @param y (Sequence of) scalar(s).
+     * @param y (Sequence of) scalar(s) (sample average(s)).
+     * @param s_squared (Sequence of) sum(s) of sample squared errors
+     * @param n (Sequence of) sample size(s)
      * @param mu (Sequence of) location parameter(s)
      * for the normal distribution.
      * @param sigma (Sequence of) scale parameters for the normal
      * distribution.
      * @return The log of the product of the densities.
-     * @throw std::domain_error if the scale is not positive.
+     * @throw std::domain_error
+     * if either n or sigma are not positive,
+     * if s_squared is negative or
+     * if any parameter is not finite.
      * @tparam T_y Underlying type of scalar in sequence.
      * @tparam T_loc Type of location parameter.
      */
@@ -71,18 +78,19 @@ namespace stan {
       T_partials_return logp(0.0);
 
       // validate args (here done over var, which should be OK)
-      check_not_nan(function,
+      check_finite(function,
                     "Location parameter sufficient statistic", y_bar);
-      check_not_nan(function,
+      check_finite(function,
                     "Scale parameter sufficient statistic", s_squared);
-      check_not_nan(function,
-                    "Number of observations", n_obs);
+      check_nonnegative(function,
+                    "Scale parameter sufficient statistic", s_squared);
       check_finite(function,
                     "Number of observations", n_obs);
       check_positive(function,
                     "Number of observations", n_obs);
       check_finite(function,
                     "Location parameter", mu);
+      check_finite(function, "Scale parameter", sigma);
       check_positive(function, "Scale parameter", sigma);
       check_consistent_sizes(function,
                              "Location parameter sufficient statistic",
