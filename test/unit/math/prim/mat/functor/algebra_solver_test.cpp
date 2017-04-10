92f583db7d51bc5be1e16774c2fe1cfc4b368054
Charles Margossian <charlesm@metrumrg.com>
2017-04-10 14:23:51 -0400

    Add prim algebra_solver.

--- test/unit/math/prim/mat/functor/algebra_solver_test.cpp
+++ test/unit/math/prim/mat/functor/algebra_solver_test.cpp
@@ -1,6 +1,4 @@
-#include <stan/math/rev/core.hpp>
-#include <stan/math/rev/mat/functor/algebra_solver.hpp>
-#include <test/unit/math/rev/mat/fun/util.hpp>
+#include <stan/math/prim/mat/functor/algebra_solver.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
 #include <fstream>
@@ -38,74 +36,46 @@ struct simpleEq_functor {
 
 TEST(MathMatrix, simple_Eq) {
   using stan::math::algebra_solver;
-  using stan::math::sum;
-  using stan::math::var;
 
-  int n_x = 2, n_y = 3;
-  for (int k = 0; k < n_x; k++) {
+ Eigen::VectorXd x(2);
+ x << 1, 1;  // initial guess
+ Eigen::VectorXd y(3);
+ y << 5, 4, 2;
+ std::vector<double> dummy_dat(0);
+ std::vector<int> dummy_dat_int(0);
+ Eigen::VectorXd theta;
 
-    Eigen::VectorXd x(n_x);
-    x << 1, 1;  // initial guess
-    Eigen::Matrix<var, Eigen::Dynamic, 1> y(n_y);
-    y << 5, 4, 2;
-    std::vector<double> dummy_dat(0);
-    std::vector<int> dummy_dat_int(0);
-    Eigen::Matrix<var, Eigen::Dynamic, 1> theta;
+ theta = algebra_solver(simpleEq_functor(),
+                        x, y, dummy_dat, dummy_dat_int);
 
-    theta = algebra_solver(simpleEq_functor(),
-                           x, y, dummy_dat, dummy_dat_int);
-    
-    EXPECT_EQ(20, theta(0));
-    EXPECT_EQ(2, theta(1));
+ EXPECT_EQ(20, theta(0));
+ EXPECT_EQ(2, theta(1));
 
-    Eigen::MatrixXd J(n_x, n_y);
-    J << 4, 5, 0, 0, 0, 1;
-
-    AVEC y_vec = createAVEC(y(0), y(1), y(2));
-    VEC g;
-    theta(k).grad(y_vec, g);
-
-    for (int i = 0; i < n_y; i++)
-      EXPECT_EQ(J(k, i), g[i]);
-  }
 }
 
 TEST(MathMatrix, simple_Eq_tuned) {
   using stan::math::algebra_solver;
-  using stan::math::sum;
   using stan::math::var;
 
   int n_x = 2, n_y = 3;
-  for (int k = 0; k < n_x; k++) {
 
-    Eigen::VectorXd x(n_x);
-    x << 1, 1;  // initial guess
-    Eigen::Matrix<var, Eigen::Dynamic, 1> y(n_y);
-    y << 5, 4, 2;
-    std::vector<double> dummy_dat(0);
-    std::vector<int> dummy_dat_int(0);
+  Eigen::VectorXd x(n_x);
+  x << 1, 1;  // initial guess
+  Eigen::VectorXd y(n_y);
+  y << 5, 4, 2;
+  std::vector<double> dummy_dat(0);
+  std::vector<int> dummy_dat_int(0);
 
-    Eigen::Matrix<var, Eigen::Dynamic, 1> theta;
-    double xtol = 1e-6, ftol = 1e-6;
-    int maxfev = 1e+4;
+  Eigen::VectorXd theta;
+  double xtol = 1e-6, ftol = 1e-6;
+  int maxfev = 1e+4;
 
-    theta = algebra_solver(simpleEq_functor(),
-                           x, y, dummy_dat, dummy_dat_int,
-                           0, xtol, ftol, maxfev);
+  theta = algebra_solver(simpleEq_functor(),
+                         x, y, dummy_dat, dummy_dat_int,
+                         0, xtol, ftol, maxfev);
 
-    EXPECT_EQ(20, theta(0));
-    EXPECT_EQ(2, theta(1));
-
-    Eigen::MatrixXd J(n_x, n_y);
-    J << 4, 5, 0, 0, 0, 1;
-
-    AVEC y_vec = createAVEC(y(0), y(1), y(2));
-    VEC g;
-    theta(k).grad(y_vec, g);
-
-    for (int i = 0; i < n_y; i++)
-      EXPECT_EQ(J(k, i), g[i]);
-  }
+  EXPECT_EQ(20, theta(0));
+  EXPECT_EQ(2, theta(1));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -131,38 +101,23 @@ struct nonLinearEq_functor {
 
 TEST(MathMatrix, nonLinearEq) {
   using stan::math::algebra_solver;
-  using stan::math::sum;
   using stan::math::var;
 
   int n_x = 3, n_y = 3;
-  for (int k = 0; k < n_x; k++) {
-    Eigen::VectorXd x(n_x);
-    x << -3, -3, 3;  // Note: need a pretty good guess to solve this one.
-    Eigen::Matrix<var, Eigen::Dynamic, 1> y(n_y);
-    y << 4, 6, 3;
-    std::vector<double> dummy_dat(0);
-    std::vector<int> dummy_dat_int(0);
+  Eigen::VectorXd x(n_x);
+  x << -3, -3, 3;  // Note: need a pretty good guess to solve this one.
+  Eigen::VectorXd y(n_y);
+  y << 4, 6, 3;
+  std::vector<double> dummy_dat(0);
+  std::vector<int> dummy_dat_int(0);
 
-    Eigen::Matrix<var, Eigen::Dynamic, 1> theta;
+  Eigen::VectorXd theta;
+  theta = algebra_solver(nonLinearEq_functor(),
+                         x, y, dummy_dat, dummy_dat_int);
 
-    theta = algebra_solver(nonLinearEq_functor(),
-                           x, y, dummy_dat, dummy_dat_int);
-
-    EXPECT_FLOAT_EQ(- y(0).val(), theta(0).val());
-    EXPECT_FLOAT_EQ(- y(1).val(), theta(1).val());
-    EXPECT_FLOAT_EQ(y(2).val(), theta(2).val());
-
-    Eigen::MatrixXd J(n_x, n_y);
-    J << -1, 0, 0, 0, -1, 0, 0, 0, 1;
-
-    AVEC y_vec = createAVEC(y(0), y(1), y(2));
-    VEC g;
-    theta(k).grad(y_vec, g);
-
-    double err = 1e-11;
-    for (int i = 0; i < n_y; i++)
-      EXPECT_NEAR(J(k, i), g[i], err);
-  }
+  EXPECT_FLOAT_EQ(- y(0), theta(0));
+  EXPECT_FLOAT_EQ(- y(1), theta(1));
+  EXPECT_FLOAT_EQ(y(2), theta(2));
 }
 
 
@@ -188,13 +143,12 @@ struct nonSquareEq_functor {
 
 TEST(MathMatrix, error_conditions) {
   using stan::math::algebra_solver;
-  using stan::math::sum;
   using stan::math::var;
 
   int n_x = 3, n_y = 2;
   Eigen::VectorXd x(n_x);
   x << 1, 1, 1;
-  Eigen::Matrix<var, Eigen::Dynamic, 1> y(n_y);
+  Eigen::VectorXd y(n_y);
   y << 4, 6;
   std::vector<double> dat(0);
   std::vector<int> dat_int(0);
@@ -219,7 +173,7 @@ TEST(MathMatrix, error_conditions) {
                    std::invalid_argument,
                    msg2);
 
-  typedef Eigen::Matrix<var, Eigen::Dynamic, 1> matrix_v;
+  typedef Eigen::VectorXd matrix_v;
   matrix_v y_bad(static_cast<matrix_v::Index>(0));
   std::stringstream err_msg3;
   err_msg3 << "algebra_solver: parameter vector has size 0, but "
@@ -293,14 +247,13 @@ struct unsolvableEq_functor {
 
 TEST(MathMatrix, unsolvable) {
   using stan::math::algebra_solver;
-  using stan::math::sum;
   using stan::math::var;
 
   Eigen::VectorXd x(2);
   x << 1, 1;
-  Eigen::Matrix<var, Eigen::Dynamic, 1> y(2);
+  Eigen::VectorXd y(2);
   y << 1, 1;  // should be positive
-  Eigen::Matrix<var, Eigen::Dynamic, 1> theta;
+  Eigen::VectorXd theta;
   std::vector<double> dat(0);
   std::vector<int> dat_int(0);
   double relative_tolerance = 1e-6, absolute_tolerance = 1e-6;
@@ -344,60 +297,27 @@ struct degenerateEq_functor {
 
 TEST(MathMatrix, degenerate) {
   using stan::math::algebra_solver;
-  using stan::math::sum;
   using stan::math::var;
-
-  int n_x = 2, n_y = 2;
 
   // This first initial guess produces the
   // solution x = {8, 8}
-  for (int k = 0; k < n_x; k++) {
-    Eigen::Matrix<var, Eigen::Dynamic, 1> y(2);
-    y << 5, 8;
-    Eigen::VectorXd x(2);
-    x << 10, 1;  // Initial Guess
-    Eigen::Matrix<var, Eigen::Dynamic, 1> theta;
-    std::vector<double> dat(0);
-    std::vector<int> dat_int(0);
-    theta = algebra_solver(degenerateEq_functor(),
+  Eigen::VectorXd y(2);
+  y << 5, 8;
+  Eigen::VectorXd x(2);
+  x << 10, 1;  // Initial Guess
+  Eigen::VectorXd theta;
+  std::vector<double> dat(0);
+  std::vector<int> dat_int(0);
+  theta = algebra_solver(degenerateEq_functor(),
                          x, y, dat, dat_int);
-    EXPECT_EQ(8, theta(0));
-    EXPECT_EQ(8, theta(1));
-
-    Eigen::MatrixXd J(n_x, n_y);
-    J << 0, 1, 0, 1;
-
-    AVEC y_vec = createAVEC(y(0), y(1));
-    VEC g;
-    theta(k).grad(y_vec, g);
-
-    for (int l = 0; l < n_y; l++)
-      EXPECT_EQ(J(k, l), g[l]);
-  }
+  EXPECT_EQ(8, theta(0));
+  EXPECT_EQ(8, theta(1));
 
   // This next initial guess produces the
   // solution x = {5, 5}
-  for (int k = 0; k < 1; k++) {
-    Eigen::Matrix<var, Eigen::Dynamic, 1> y(2);
-    y << 5, 8;
-    Eigen::VectorXd x(2);
-    x << 1, 1;  // Initial Guess
-    Eigen::Matrix<var, Eigen::Dynamic, 1> theta;
-    std::vector<double> dat(0);
-    std::vector<int> dat_int(0);
-    theta = algebra_solver(degenerateEq_functor(),
-                           x, y, dat, dat_int);
-    EXPECT_FLOAT_EQ(5, theta(0).val());
-    EXPECT_FLOAT_EQ(5, theta(0).val());
-
-    Eigen::MatrixXd J(n_x, n_y);
-    J << 1, 0, 1, 0;
-
-    AVEC y_vec = createAVEC(y(0), y(1));
-    VEC g;
-    theta(k).grad(y_vec, g);
-
-    for (int l = 0; l < n_y; l++)
-      EXPECT_NEAR(J(k, l), g[l], 1e-13);
-  }
+  x << 1, 1;  // Initial Guess
+  theta = algebra_solver(degenerateEq_functor(),
+                         x, y, dat, dat_int);
+  EXPECT_FLOAT_EQ(5, theta(0));
+  EXPECT_FLOAT_EQ(5, theta(0));
 }
