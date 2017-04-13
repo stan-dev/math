04ec64b7cb56f82404710c1a6f57da9d062ffbf3
Mitzi Morris <mitzi@panix.com>
2017-04-13 15:14:42 -0400

    fixes #519. Removing built documentation. [skip ci]

--- doc/api/html/search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['compressed_20sparse_20row_20matrix_20format_2e',['Compressed Sparse Row matrix format.',['../dc/d79/group__csr__format.html',1,'']]]
-];
--- doc/api/html/search/typedefs_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['block_5f',['Block_',['../de/d48/classstan_1_1math_1_1cholesky__block.html#afb6f6cf460a34e7ea1447bf4ea9d8091',1,'stan::math::cholesky_block']]],
-  ['boost_5fpolicy_5ft',['boost_policy_t',['../d4/d84/namespacestan_1_1math.html#a4af698c024d1cc25e211028af5d2e502',1,'stan::math']]]
-];
