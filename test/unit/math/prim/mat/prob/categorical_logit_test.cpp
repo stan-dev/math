7e9c93aa5c131a26e3b5c278b04f4bdde88455f9
Ben Bales <bbbales2@gmail.com>
2017-04-14 20:03:24 -0700

    Added categorical_logit_rng to Stan Math for issue 517

--- test/unit/math/prim/mat/prob/categorical_logit_test.cpp
+++ test/unit/math/prim/mat/prob/categorical_logit_test.cpp
@@ -1,11 +1,13 @@
 #include <stan/math/prim/mat.hpp>
 #include <gtest/gtest.h>
 #include <limits>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/math/distributions.hpp>
 
 using Eigen::Dynamic;
 using Eigen::Matrix;
 using stan::math::log_softmax;
-using stan::math::log_softmax;
+using stan::math::softmax;
 
 TEST(ProbDistributionsCategoricalLogit,Categorical) {
   Matrix<double,Dynamic,1> theta(3,1);
@@ -78,4 +80,59 @@ TEST(ProbDistributionsCategoricalLogit, error) {
   ns[0] = 1;
   ns[1] = 12;
   EXPECT_THROW(categorical_logit_log(ns, theta), std::domain_error);
+}
+
+TEST(ProbDistributionsCategoricalLogit, error_check) {
+  using stan::math::categorical_logit_rng;
+  boost::random::mt19937 rng;
+
+  Matrix<double, Dynamic, 1> beta(3);
+
+  beta << 1.0, 10.0, -10.0;
+
+  EXPECT_NO_THROW(categorical_logit_rng(beta, rng));
+
+  beta << -1e3, 1.1e3, 1e5;
+
+  EXPECT_NO_THROW(categorical_logit_rng(beta, rng));
+
+  beta(1) = std::numeric_limits<double>::quiet_NaN();
+  EXPECT_THROW(categorical_logit_rng(beta, rng), std::domain_error);
+
+  beta(1) = std::numeric_limits<double>::infinity();
+  EXPECT_THROW(categorical_logit_rng(beta, rng), std::domain_error);
+}
+
+TEST(ProbDistributionsCategoricalLogit, chiSquareGoodnessFitTest) {
+  boost::random::mt19937 rng;
+
+  int N = 10000;
+  Matrix<double, Dynamic, 1> beta(3);
+  beta << -0.5,
+    0.1,
+    0.3;
+
+  Matrix<double, Dynamic, 1> theta = softmax(beta);
+
+  int K = theta.rows();
+  boost::math::chi_squared mydist(K - 1);
+
+  int bin [K];
+  double expect [K];
+  for(int i = 0 ; i < K; i++) {
+    bin[i] = 0;
+    expect[i] = N * theta(i);
+  }
+
+  for(int i = 0;  i < N; i++) {
+    int a = stan::math::categorical_logit_rng(beta, rng);
+    bin[a - 1]++;
+  }
+
+  double chi = 0;
+
+  for(int j = 0; j < K; j++)
+    chi += ((bin[j] - expect[j]) * (bin[j] - expect[j]) / expect[j]);
+
+  EXPECT_TRUE(chi < quantile(complement(mydist, 1e-6)));
 }
