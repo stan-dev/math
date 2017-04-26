d538dca674578e29e8ec0f5788b53038b02838a4
Bob Carpenter <carp@alias-i.com>
2017-04-26 14:27:52 -0400

    fixes #534, simplifying fvar ctors

--- test/unit/math/fwd/mat/vectorize/build_fwd_vector.hpp
+++ test/unit/math/fwd/mat/vectorize/build_fwd_vector.hpp
@@ -6,24 +6,18 @@
 
 template <typename F>
 static inline std::vector<double>
-build_fwd_vector(std::vector<double> double_vector,
-                          int seed_index = -1) { 
+build_fwd_vector(std::vector<double> double_vector, int seed_index = -1) {
   return F::valid_inputs();
 }
 
 template <typename F, typename T>
 static inline std::vector<stan::math::fvar<T> >
 build_fwd_vector(std::vector<stan::math::fvar<T> > fvar_vector,
-                        int seed_index = -1) { 
+                 int seed_index = -1) {
   using std::vector;
   using stan::math::fvar;
-
   vector<T> template_v = build_fwd_vector<F>(vector<T>(), seed_index);
-
   for (size_t i = 0; i < template_v.size(); ++i) {
-  
-    // For fvar<fvar<double> >, this will fill in 
-    // all four components
     if (seed_index == static_cast<int>(i))
       fvar_vector.push_back(fvar<T>(template_v[i], template_v[i]));
     else
