d249fa035d56f1a32cfa8b1331caa31fabd16c5f
Sean Talts <sean.talts@gmail.com>
2017-05-05 14:43:58 -0400

    Add test for return_type

---
+++ test/unit/math/mix/scal/meta/return_type_test.cpp
@@ -0,0 +1,27 @@
+#include <stan/math/mix/scal.hpp>
+#include <gtest/gtest.h>
+#include <test/unit/util.hpp>
+
+TEST(MetaTraits, ReturnTypeMix) {
+  using stan::math::fvar;
+  using stan::math::var;
+  using stan::return_type;
+
+  test::expect_same_type<fvar<var>,
+                         return_type<fvar<var> >::type>();
+  test::expect_same_type<fvar<var>,
+                         return_type<fvar<var>, fvar<var> >::type>();
+  test::expect_same_type<fvar<var>,
+                         return_type<fvar<var>, fvar<var>, fvar<var> >::type>();
+  test::expect_same_type<fvar<var>,
+                         return_type<fvar<var>, fvar<var>, var >::type>();
+
+  test::expect_same_type<fvar<fvar<var> >,
+                         return_type<fvar<fvar<var> > >::type>();
+  test::expect_same_type<fvar<fvar<var> >,
+                         return_type<fvar<fvar<var> >, fvar<fvar<var> > >::type>();
+  test::expect_same_type<fvar<fvar<var> >,
+                         return_type<fvar<fvar<var> >, fvar<fvar<var> >, fvar<fvar<var> > >::type>();
+  test::expect_same_type<fvar<fvar<var> >,
+                         return_type<fvar<fvar<var> >, fvar<fvar<var> >, var >::type>();
+}
