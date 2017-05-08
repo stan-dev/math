430b9d14295ce0b820d2cb4dc7bd8fb26846c66b
Rob Trangucci <robert.trangucci@gmail.com>
2017-05-08 13:30:17 -0400

    Update cholesky_decompose.hpp
    Added transpose of vcl_Lbar before copy

--- stan/math/rev/mat/fun/cholesky_decompose.hpp
+++ stan/math/rev/mat/fun/cholesky_decompose.hpp
@@ -304,8 +304,8 @@ namespace stan {
             vcl_L(j, i) = vcl_L(i, j);
         viennacl::linalg::inplace_solve(vcl_L, vcl_Lbar, viennacl::linalg::upper_tag());
         viennacl::linalg::inplace_solve(vcl_L, viennacl::trans(vcl_Lbar), viennacl::linalg::upper_tag());  
-        viennacl::copy(vcl_Lbar, Lbar);
-        for (int i = 0; i < M_; i++) Lbar(i,i) /= 2.0;
+        viennacl::copy(viennacl::trans(vcl_Lbar), Lbar);
+        Lbar.diagonal() *= 0.5;
         pos = 0;
         for (size_type j = 0; j < M_; ++j)
           for (size_type i = j; i < M_; ++i)
