90443608883764437cc21df9077492cf77a3298d
Sean Talts <sean.talts@gmail.com>
2017-05-16 15:51:59 -0400

    Unlikely.

--- stan/math/prim/mat/meta/operands_and_partials.hpp
+++ stan/math/prim/mat/meta/operands_and_partials.hpp
@@ -2,8 +2,9 @@
 #define STAN_MATH_PRIM_MAT_META_OPERANDS_AND_PARTIALS_HPP
 
 #include <stan/math/prim/mat/fun/typedefs.hpp>
-#include <stan/math/prim/scal/meta/operands_and_partials.hpp>
 #include <stan/math/prim/scal/meta/broadcast_array.hpp>
+#include <stan/math/prim/scal/meta/operands_and_partials.hpp>
+#include <stan/math/prim/scal/meta/likely.hpp>
 #include <vector>
 
 namespace stan {
@@ -91,7 +92,7 @@ namespace stan {
         friend class stan::math::operands_and_partials;
         const Ops& operands_;
         int size() {
-          if (this->operands_.size() == 0) return 0;
+          if (unlikely(this->operands_.size() == 0)) return 0;
           return this->operands_.size() * this->operands_[0].size();
         }
       };
