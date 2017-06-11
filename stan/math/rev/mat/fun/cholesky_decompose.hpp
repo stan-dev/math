b3effc6fada3d7093de69b0d6b11f15b05d0355a
Steve <stevo15025@gmail.com>
2017-06-11 19:21:35 -0400

    updated with Rok's code for derivative of chlesky

--- stan/math/rev/mat/fun/cholesky_decompose.hpp
+++ stan/math/rev/mat/fun/cholesky_decompose.hpp
@@ -275,72 +275,160 @@ namespace stan {
        *
        */
     #if STAN_GPU
+
     virtual void chain() {
-        using Eigen::MatrixXd;
-        using Eigen::Lower;
-            using Eigen::Block;
-            using Eigen::Upper;
-            using Eigen::StrictlyUpper;
-            using Eigen::StrictlyLower;
-            MatrixXd Lbar(M_, M_);
-            MatrixXd L(M_, M_);
 
-        viennacl::matrix<double>  vcl_L(M_, M_);
-            viennacl::matrix<double>  vcl_Lbar(M_, M_);
-            viennacl::matrix<double> vcl_Lbar_temp(M_, M_);
+      using Eigen::MatrixXd;
+      using Eigen::Lower;
+      using Eigen::Block;
+      using Eigen::Upper;
+      using Eigen::StrictlyUpper;
+      using Eigen::StrictlyLower;
+      MatrixXd Lbar(M_, M_);
+      MatrixXd Lbar1(M_, M_);
+      MatrixXd L(M_, M_);
+      Lbar.setZero();
+      L.setZero();
 
-            Lbar.setZero();
-            L.setZero();
-            size_t pos = 0;
-            for (size_type j = 0; j < M_; ++j) {
-              for (size_type i = j; i < M_; ++i) {
-                Lbar.coeffRef(i, j) = variRefL_[pos]->adj_;
-                L.coeffRef(i, j) = variRefL_[pos]->val_;
-                ++pos;
-              }
-            }
+      int parts;
+      if(M_ > 2500){
+        parts = 64;
+      } else {
+        parts = 32;
+      }
+      //need to setup a smarter way of doing this
+      int remainder = M_ % parts;
+      int part_size_fixed = M_ / parts;
+      int repeat = 1;
+      int sizePad;
+      
+      size_t pos = 0;
+      for (size_type j = 0; j < M_; ++j) {
+        for (size_type i = j; i < M_; ++i) {
+          Lbar.coeffRef(i, j) = variRefL_[pos]->adj_;
+          L.coeffRef(i, j) = variRefL_[pos]->val_;
+          ++pos;
+        }
+      }
 
-        viennacl::copy(L, vcl_L);
-        viennacl::copy(Lbar, vcl_Lbar);
-        vcl_L =  viennacl::trans(vcl_L);
-        vcl_Lbar =  viennacl::linalg::prod(vcl_L, vcl_Lbar);
+      viennacl::matrix<double>  vcl_L(M_, M_);
+      viennacl::matrix<double>  vcl_temp(M_, M_);
+      viennacl::matrix<double>  vcl_Lbar(M_, M_);
+      
+      viennacl::copy(L, vcl_L);
+      viennacl::copy(Lbar, vcl_Lbar);
+      vcl_L =  viennacl::trans(vcl_L);
+      vcl_Lbar =  viennacl::linalg::prod(vcl_L, vcl_Lbar); 
 
+      my_kernel_mul.global_work_size(0, vcl_Lbar.internal_size1());
+      my_kernel_mul.global_work_size(1, vcl_Lbar.internal_size2());		
 
-        // internal_size is used because ViennaCL pads the matrices in
-        // global memory
-        my_kernel_mul.global_work_size(0, vcl_Lbar.internal_size1());
-        my_kernel_mul.global_work_size(1, vcl_Lbar.internal_size2());
+      my_kernel_mul.local_work_size(0, 32);
+      my_kernel_mul.local_work_size(1, 32);
 
-        // arbitrary block size, could also be different
-        my_kernel_mul.local_work_size(0, 32);
-        my_kernel_mul.local_work_size(1, 32);
+      viennacl::ocl::enqueue(
+        my_kernel_mul(
+          vcl_Lbar,
+          static_cast<cl_uint>(vcl_Lbar.size1()),
+          static_cast<cl_uint>(vcl_Lbar.size2()), 
+          static_cast<cl_uint>(vcl_Lbar.internal_size2())
+        )
+      );
 
-        viennacl::ocl::enqueue(my_kernel_mul(
+      viennacl::matrix<double>  vcl_temp2(M_, M_ * 2);
+      std::vector<int> stl_sizes(parts);
+      viennacl::vector<int> vcl_sizes(parts);
+      
+      for(int i = 0; i < parts; i++){
+        if(i < remainder)
+          stl_sizes[i] = part_size_fixed + 1;
+        else
+          stl_sizes[i] = part_size_fixed;
+      }
+      
+      viennacl::copy(stl_sizes, vcl_sizes);
+
+      my_kernel_inv1.global_work_size(0, parts);
+      my_kernel_inv1.local_work_size(0, 1);
+
+      viennacl::ocl::enqueue(
+        my_kernel_inv1(
+          vcl_L,vcl_temp,
+          static_cast<cl_uint>(remainder),
+          static_cast<cl_uint>(part_size_fixed),
+          static_cast<cl_uint>(vcl_L.size2()),
+          static_cast<cl_uint>(vcl_L.internal_size2())
+        )
+      );
+      
+
+      for(int pp = parts; pp > 1; pp /= 2){
+
+        sizePad = (((part_size_fixed + 1) * repeat + 31) / 32) * 32;
+        my_kernel_inv2.global_work_size(0, sizePad);
+        my_kernel_inv2.global_work_size(1, sizePad / 4);
+        my_kernel_inv2.global_work_size(2, pp / 2);
+        my_kernel_inv2.local_work_size(0, 32);
+        my_kernel_inv2.local_work_size(1, 32 / 4);
+        my_kernel_inv2.local_work_size(2, 1);
+        my_kernel_inv3.global_work_size(0, sizePad);
+        my_kernel_inv3.global_work_size(1, sizePad / 4);
+        my_kernel_inv3.global_work_size(2, pp / 2);
+        my_kernel_inv3.local_work_size(0, 32);
+        my_kernel_inv3.local_work_size(1, 32/4);
+        my_kernel_inv3.local_work_size(2, 1);
+        
+        viennacl::ocl::enqueue(
+          my_kernel_inv2(
+            vcl_L, vcl_sizes, vcl_temp2,
+            static_cast<cl_uint>(repeat),
+            static_cast<cl_uint>(remainder),
+            static_cast<cl_uint>(part_size_fixed),
+            static_cast<cl_uint>(vcl_L.size2()),
+            static_cast<cl_uint>(vcl_L.internal_size2())
+          )
+        );
+        viennacl::ocl::enqueue(
+          my_kernel_inv3(
+            vcl_L, vcl_sizes, vcl_temp2,
+            static_cast<cl_uint>(repeat),
+            static_cast<cl_uint>(remainder),
+            static_cast<cl_uint>(part_size_fixed),
+            static_cast<cl_uint>(vcl_L.size2()),
+            static_cast<cl_uint>(vcl_L.internal_size2())
+          )
+        );
+        
+        repeat *= 2;
+
+      }
+      
+      vcl_Lbar =  viennacl::linalg::prod(vcl_L, vcl_Lbar);
+      vcl_Lbar =  viennacl::linalg::prod(vcl_L, viennacl::trans(vcl_Lbar));
+      viennacl::trans(vcl_Lbar);
+
+      my_kernel_diag.global_work_size(0, vcl_Lbar.internal_size1());
+      my_kernel_diag.global_work_size(1, vcl_Lbar.internal_size2());
+      my_kernel_diag.local_work_size(0,32);
+      my_kernel_diag.local_work_size(1,32);
+
+      viennacl::ocl::enqueue(
+        my_kernel_diag(
           vcl_Lbar,
           static_cast<cl_uint>(vcl_Lbar.size1()),
           static_cast<cl_uint>(vcl_Lbar.size2()),
-          static_cast<cl_uint>(vcl_Lbar.internal_size2())));
+          static_cast<cl_uint>(vcl_Lbar.internal_size2())
+        )
+      );
 
-            viennacl::linalg::inplace_solve(
-             vcl_L,
-             vcl_Lbar,
-             viennacl::linalg::upper_tag());
+      viennacl::copy(vcl_Lbar, Lbar);
 
-            viennacl::linalg::inplace_solve(
-              vcl_L,
-              viennacl::trans(vcl_Lbar),
-              viennacl::linalg::upper_tag());
+      pos = 0;
+      for (size_type j = 0; j < M_; ++j)
+        for (size_type i = j; i < M_; ++i)
+          variRefA_[pos++]->adj_ += Lbar.coeffRef(i, j);
 
-        viennacl::trans(vcl_Lbar);
-        viennacl::copy(vcl_Lbar, Lbar);
-        viennacl::copy(vcl_L, L);
-        Lbar.diagonal() *= 0.5;
-
-            pos = 0;
-            for (size_type j = 0; j < M_; ++j)
-              for (size_type i = j; i < M_; ++i)
-                variRefA_[pos++]->adj_ += Lbar.coeffRef(i, j);
-           }
+    }
     #endif
     };
 
