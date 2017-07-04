f5f372a9aae4cd3157ec898b027b27f714c16dcd
Steve <stevo15025@gmail.com>
2017-07-04 11:21:35 -0400

    Receiving ERROR when running tests about append_array not having a rule.

--- stan/math/prim/mat/fun/ViennaCL.hpp
+++ stan/math/prim/mat/fun/ViennaCL.hpp
@@ -1,36 +1,35 @@
 #ifndef STAN_MATH_PRIM_MAT_FUN_VIENNACL_HPP
-  #define STAN_MATH_PRIM_MAT_FUN_VIENNACL_HPP
+#define STAN_MATH_PRIM_MAT_FUN_VIENNACL_HPP
 
-  #ifdef STAN_GPU
-    #ifndef VIENNACL_WITH_OPENCL
-      #define VIENNACL_WITH_OPENCL 1
-    #endif
+#ifdef STAN_GPU
+  #ifndef VIENNACL_WITH_OPENCL
+    #define VIENNACL_WITH_OPENCL 1
   #endif
+#endif
 
-  #ifndef VIENNACL_WITH_EIGEN
-    #define VIENNACL_WITH_EIGEN 1
-  #endif
+#ifndef VIENNACL_WITH_EIGEN
+  #define VIENNACL_WITH_EIGEN 1
+#endif
 
-  #include <viennacl/vector.hpp>
-  #include <viennacl/matrix.hpp>
-  #include <viennacl/linalg/direct_solve.hpp>
-  #include <viennacl/linalg/lu.hpp>
-  #include <viennacl/linalg/ilu_operations.hpp>
+#include <viennacl/vector.hpp>
+#include <viennacl/matrix.hpp>
+#include <viennacl/linalg/direct_solve.hpp>
+#include <viennacl/linalg/lu.hpp>
+#include <viennacl/linalg/ilu_operations.hpp>
 
-  #ifdef STAN_GPU
-    #include <iostream>
-    #include <string>
-    #include <fstream>
-    std::ifstream in("stan/math/prim/mat/fun/custom_kernels.cl", std::ios::in | std::ios::binary);
-    std::string custom_kernels = std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+#ifdef STAN_GPU
+  #include <iostream>
+  #include <string>
+  #include <fstream>
+  std::ifstream in("stan/math/prim/mat/fun/custom_kernels.cl", std::ios::in | std::ios::binary);
+  std::string custom_kernels = std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+  viennacl::ocl::program & my_prog = viennacl::ocl::current_context().add_program(custom_kernels, "custom_kernels");
+  viennacl::ocl::kernel & my_kernel_mul = my_prog.get_kernel("copy_lower_tri_upper_tri");
+  viennacl::ocl::kernel & my_kernel_diag = my_prog.get_kernel("diagonal_mul");
+  viennacl::ocl::kernel & my_kernel_inv1 = my_prog.get_kernel("inverse_step1");
+  viennacl::ocl::kernel & my_kernel_inv2 = my_prog.get_kernel("inverse_step2");
+  viennacl::ocl::kernel & my_kernel_inv3 = my_prog.get_kernel("inverse_step3");
+#endif
 
-    
-    viennacl::ocl::program & my_prog = viennacl::ocl::current_context().add_program(custom_kernels, "custom_kernels");
-    viennacl::ocl::kernel & my_kernel_mul = my_prog.get_kernel("copy_lower_tri_upper_tri");
-    viennacl::ocl::kernel & my_kernel_diag = my_prog.get_kernel("diagonal_mul");
-    viennacl::ocl::kernel & my_kernel_inv1 = my_prog.get_kernel("inverse_step1");
-    viennacl::ocl::kernel & my_kernel_inv2 = my_prog.get_kernel("inverse_step2");
-    viennacl::ocl::kernel & my_kernel_inv3 = my_prog.get_kernel("inverse_step3");
-  #endif
 #endif
 
