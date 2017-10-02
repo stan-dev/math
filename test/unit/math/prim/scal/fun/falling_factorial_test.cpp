48637f0e59747ba62fb43fef78877b2cf8dd75ec
ARJ-Research <andrew.r.johnson@postgrad.curtin.edu.au>
2017-10-02 08:46:36 +0800

    Tests using real n, only defined for integer n

--- test/unit/math/prim/scal/fun/falling_factorial_test.cpp
+++ test/unit/math/prim/scal/fun/falling_factorial_test.cpp
@@ -7,7 +7,6 @@ TEST(MathFunctions, falling_factorial) {
   
   EXPECT_FLOAT_EQ(24, falling_factorial(4.0,3));
   EXPECT_FLOAT_EQ(120, falling_factorial(5.0,4));
-  EXPECT_FLOAT_EQ(144.8261, falling_factorial(6.1,3.1));
   EXPECT_THROW(falling_factorial(1, -4), std::domain_error);
 }
 
