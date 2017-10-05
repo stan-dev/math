cb3f4f7cff38aadcc775eb46e923f21d4d1303a1
Steve Bronder <stevo15025@gmail.com>
2017-10-04 20:08:11 -0400

    random fixings

--- stan/math/prim/mat/fun/ocl_gpu.hpp
+++ stan/math/prim/mat/fun/ocl_gpu.hpp
@@ -21,7 +21,7 @@
 
 #define DEVICE_FILTER CL_DEVICE_TYPE_GPU
 
-/*  
+/*
 *    @file stanmathcl/ocl.hpp
 *    @brief Initialization for OpenCL: find platforms, devices,
 *      create context, command queue etc.
