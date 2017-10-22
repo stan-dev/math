e85114172c0a7cc00d3aa52f324c65e3b33aa255
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2017-10-22 20:28:01 +0200

    - fixed the inverse semifrequent nan error, all test pass

--- stan/math/prim/mat/fun/inverse_gpu.hpp
+++ stan/math/prim/mat/fun/inverse_gpu.hpp
@@ -29,7 +29,7 @@ namespace stan {
 
       try{
 
-        int parts = 4;
+        int parts = 32;
         //this will be managed by the library core with self-adaptive strategies
         if (A.rows() < 65)
           parts = 1;
