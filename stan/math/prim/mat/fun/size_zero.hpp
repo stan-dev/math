64417c5239fead08c644f7d250cdfaf1c7b3d9be
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2017-10-25 12:35:36 +0800

    Address comments, fix cpplint failing

--- stan/math/prim/mat/fun/size_zero.hpp
+++ stan/math/prim/mat/fun/size_zero.hpp
@@ -2,15 +2,16 @@
 #define STAN_MATH_PRIM_MAT_FUN_SIZE_ZERO_HPP
 
 #include <stan/math/prim/mat/meta/length.hpp>
+#include <utility>
 
 namespace stan {
   namespace math {
 
     /**
-     * Returns 1 if any inputs are of length 0, returns 0
+     * Returns 1 if input is of length 0, returns 0
      * otherwise
      *
-     * @param x arguments
+     * @param x argument
      * @return 0 or 1
      */
     template <typename T>
@@ -18,6 +19,14 @@ namespace stan {
       return !length(x);
     }
 
+    /**
+     * Returns 1 if any inputs are of length 0, returns 0
+     * otherwise
+     *
+     * @param x first argument
+     * @param xs parameter pack of remaining arguments to forward to function
+     * @return 0 or 1
+     */
     template <typename T, typename ... Ts>
     inline bool size_zero(T& x, Ts&& ... xs) {
       return (size_zero(x) || size_zero(std::forward<Ts>(xs)...));
--- stan/math/prim/scal/prob/von_mises_lpdf.hpp
+++ stan/math/prim/scal/prob/von_mises_lpdf.hpp
@@ -16,6 +16,7 @@
 #include <stan/math/prim/scal/meta/scalar_seq_view.hpp>
 #include <stan/math/prim/scal/meta/VectorBuilder.hpp>
 #include <stan/math/prim/scal/fun/value_of.hpp>
+#include <stan/math/prim/mat/fun/size_zero.hpp>
 #include <cmath>
 
 namespace stan {
@@ -29,7 +30,7 @@ namespace stan {
       typedef typename stan::partials_return_type<T_y, T_loc, T_scale>::type
         T_partials_return;
 
-      if (!(stan::length(y) && stan::length(mu) && stan::length(kappa)))
+      if (size_zero(y, mu, kappa))
         return 0.0;
 
       using stan::is_constant_struct;
