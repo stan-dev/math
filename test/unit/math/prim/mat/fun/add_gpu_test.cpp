968466827ae23504af1c550ed9d2bde92a48a370
Steve Bronder <stevo15025@gmail.com>
2017-10-30 00:15:32 -0400

    update check_diagonal_zero test

--- test/unit/math/prim/mat/fun/add_gpu_test.cpp
+++ test/unit/math/prim/mat/fun/add_gpu_test.cpp
@@ -1,53 +1,104 @@
 #include <stan/math/prim/mat.hpp>
 #include <gtest/gtest.h>
 
-TEST(MathMatrix,add_v_exception) {
+TEST(MathMatrix,add_v_exception_pass) {
   stan::math::vector_d d1, d2;
 
   d1.resize(3);
   d2.resize(3);
-  EXPECT_NO_THROW(stan::math::add(d1, d2));
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(3,1);
+  EXPECT_NO_THROW(stan::math::add(d11, d22, d33));
+}
 
+TEST(MathMatrix,add_v_exception_pass_zero) {
+  stan::math::vector_d d1, d2;
   d1.resize(0);
   d2.resize(0);
-  EXPECT_NO_THROW(stan::math::add(d1, d2));
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(1,1);
+  EXPECT_NO_THROW(stan::math::add(d11, d22, d33));
+}
+
+TEST(MathMatrix,add_v_exception_pass_invalid_arg) {
+  stan::math::row_vector_d d1, d2;
 
   d1.resize(2);
   d2.resize(3);
-  EXPECT_THROW(stan::math::add(d1, d2), std::invalid_argument);
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(3,0);
+  EXPECT_THROW(stan::math::add(d11, d22, d33), std::invalid_argument);
 }
-TEST(MathMatrix,add_rv_exception) {
+
+TEST(MathMatrix,add_rv_exception_pass) {
   stan::math::row_vector_d d1, d2;
 
   d1.resize(3);
   d2.resize(3);
-  EXPECT_NO_THROW(stan::math::add(d1, d2));
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(1,1);
+  EXPECT_NO_THROW(stan::math::add(d11, d22, d33));
+}
+
+TEST(MathMatrix,add_rv_exception_pass_zero) {
+  stan::math::row_vector_d d1, d2;
 
   d1.resize(0);
   d2.resize(0);
-  EXPECT_NO_THROW(stan::math::add(d1, d2));
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(1,1);
+  EXPECT_NO_THROW(stan::math::add(d11, d22, d33));
+}
+
+TEST(MathMatrix,add_rv_exception_fail_invalid_arg) {
+  stan::math::row_vector_d d1, d2;
 
   d1.resize(2);
   d2.resize(3);
-  EXPECT_THROW(stan::math::add(d1, d2), std::invalid_argument);
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(3,1);
+  EXPECT_THROW(stan::math::add(d11, d22, d33), std::invalid_argument);
 }
-TEST(MathMatrix,add_m_exception) {
+
+TEST(MathMatrix,add_m_exception_pass_simple) {
   stan::math::matrix_d d1, d2;
 
   d1.resize(2,3);
   d2.resize(2,3);
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(2,3);
   EXPECT_NO_THROW(stan::math::add(d1, d2));
+}
 
+TEST(MathMatrix,add_m_exception_pass_zero) {
+  stan::math::matrix_d d1, d2;
   d1.resize(0,0);
   d2.resize(0,0);
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(0,0);
   EXPECT_NO_THROW(stan::math::add(d1, d2));
+}
 
+TEST(MathMatrix,add_m_exception_fail_invalid_arg) {
+  stan::math::matrix_d d1, d2;
   d1.resize(2,3);
   d2.resize(3,3);
+  stan::math::matrix_gpu d11(d1);
+  stan::math::matrix_gpu d22(d2);
+  stan::math::matrix_gpu d33(2,3);
   EXPECT_THROW(stan::math::add(d1, d2), std::invalid_argument);
 }
 
-
+//TODO(Steve): Matrix GPU function for adding constant
+/*
 TEST(MathMatrix,add_c_m) {
   stan::math::matrix_d v(2,2);
   v << 1, 2, 3, 4;
@@ -98,6 +149,7 @@ TEST(MathMatrix,add_c_v) {
   EXPECT_FLOAT_EQ(4.0,result(1));
   EXPECT_FLOAT_EQ(5.0,result(2));
 }
+*/
 
 TEST(MathMatrix, add) {
   stan::math::vector_d v1(2);
@@ -116,7 +168,18 @@ TEST(MathMatrix, add) {
   m2 << 10, 100, 1000, 0, -10, -12;
 
   using stan::math::add;
-  EXPECT_THROW(add(v1,v2),std::invalid_argument);
-  EXPECT_THROW(add(rv1,rv2),std::invalid_argument);
-  EXPECT_THROW(add(m1,m2),std::invalid_argument);
+  using stan::math::matrix_gpu;
+  matrix_gpu v11(v1);
+  matrix_gpu v22(v2);
+  matrix_gpu v33(v1);
+  matrix_gpu rv11(rv1);
+  matrix_gpu rv22(rv2);
+  matrix_gpu rv33(rv1);
+  matrix_gpu m11(m1);
+  matrix_gpu m22(m2);
+  matrix_gpu m33(m1);
+
+  EXPECT_THROW(add(v11, v22, v33),std::invalid_argument);
+  EXPECT_THROW(add(rv11, rv22, rv33),std::invalid_argument);
+  EXPECT_THROW(add(m11, m22, m33),std::invalid_argument);
 }
