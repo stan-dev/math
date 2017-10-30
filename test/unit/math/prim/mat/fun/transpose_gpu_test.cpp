6686a4f669930fd54ebb146a364cb1725e70f259
Steve Bronder <stevo15025@gmail.com>
2017-10-29 23:18:16 -0400

    Change OpenCL header

--- test/unit/math/prim/mat/fun/transpose_gpu_test.cpp
+++ test/unit/math/prim/mat/fun/transpose_gpu_test.cpp
@@ -6,8 +6,20 @@ TEST(MathMatrix, transpose) {
   stan::math::row_vector_d rv0;
   stan::math::matrix_d m0;
 
+  stan::math::matrix_gpu v00(v0);
+  stan::math::matrix_gpu rv00(rv0);
+  stan::math::matrix_gpu m00(m0);
+
+  stan::math::matrix_gpu v00_dst(v0);
+  stan::math::matrix_gpu rv00_dst(rv0);
+  stan::math::matrix_gpu m00_dst(m0);
+  
   using stan::math::transpose;
-  EXPECT_NO_THROW(transpose(v0));
-  EXPECT_NO_THROW(transpose(rv0));
-  EXPECT_NO_THROW(transpose(m0));
+  EXPECT_NO_THROW(transpose(v00));
+  EXPECT_NO_THROW(transpose(rv00));
+  EXPECT_NO_THROW(transpose(m00));
+  EXPECT_NO_THROW(transpose(v00, v00_dst));
+  EXPECT_NO_THROW(transpose(rv00, v00_dst));
+  EXPECT_NO_THROW(transpose(m00, v00_dst));
+
 }
