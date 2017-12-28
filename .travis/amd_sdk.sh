fa0376b2df6e0d375a7115eaf724d0e5f6254ee7
stevo15025 <stevo15025@gmail.com>
2017-12-28 17:26:44 -0500

    update AMD SDK download script

--- .travis/amd_sdk.sh
+++ .travis/amd_sdk.sh
@@ -3,17 +3,16 @@
 # Original script from https://github.com/gregvw/amd_sdk/
 
 # Location from which get nonce and file name from
-URL="http://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/"
-URLDOWN="http://developer.amd.com/amd-license-agreement-appsdk/"
+URL="https://developer.amd.com/amd-accelerated-parallel-processing-app-sdk/"
+URLDOWN="https://developer.amd.com/amd-license-agreement-appsdk/"
 
 NONCE1_STRING='name="amd_developer_central_downloads_page_nonce"'
 FILE_STRING='name="f"'
 POSTID_STRING='name="post_id"'
 NONCE2_STRING='name="amd_developer_central_nonce"'
 
-# This gets the second latest (2.9.1 ATM, latest is 3.0)
-# For newest: FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
-FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
+#For newest FORM=`wget -qO - $URL | sed -n '/download-2/,/64-bit/p'`
+FORM=`wget --no-check-certificate -qO - $URL | sed -n '/download-5/,/64-bit/p'`
 
 # Get nonce from form
 NONCE1=`echo $FORM | awk -F ${NONCE1_STRING} '{print $2}'`
@@ -30,11 +29,10 @@ FILE=`echo $FORM | awk -F ${FILE_STRING} '{print $2}'`
 FILE=`echo $FILE | awk -F'"' '{print $2}'`
 echo $FILE
 
-FORM=`wget -qO - $URLDOWN --post-data "amd_developer_central_downloads_page_nonce=${NONCE1}&f=${FILE}&post_id=${POSTID}"`
+FORM=`wget --no-check-certificate -qO - $URLDOWN --post-data "amd_developer_central_downloads_page_nonce=${NONCE1}&f=${FILE}&post_id=${POSTID}"`
 
 NONCE2=`echo $FORM | awk -F ${NONCE2_STRING} '{print $2}'`
 NONCE2=`echo $NONCE2 | awk -F'"' '{print $2}'`
 echo $NONCE2
 
-wget --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" -O AMD-SDK.tar.bz2;
-
+wget --no-check-certificate --content-disposition --trust-server-names $URLDOWN --post-data "amd_developer_central_nonce=${NONCE2}&f=${FILE}" -O AMD-SDK.tar.bz2;
