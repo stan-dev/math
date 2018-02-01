6821524cd641585fa481cd09131dcc8045a86ba4
Marco Colombo <m.colombo@ed.ac.uk>
2018-02-01 21:48:02 +0000

    Add missing header.

--- stan/math/prim/scal/fun/log1m.hpp
+++ stan/math/prim/scal/fun/log1m.hpp
@@ -2,6 +2,7 @@
 #define STAN_MATH_PRIM_SCAL_FUN_LOG1M_HPP
 
 #include <stan/math/prim/scal/fun/log1p.hpp>
+#include <stan/math/prim/scal/fun/is_nan.hpp>
 #include <stan/math/prim/scal/err/check_less_or_equal.hpp>
 
 namespace stan {
