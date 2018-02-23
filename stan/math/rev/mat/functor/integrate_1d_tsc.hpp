93dfbcffad662f34b6141e9818ac6a96a8b4b34c
Bob Carpenter <carp@alias-i.com>
2018-02-23 13:57:12 -0500

    integrate 1d cleanup

--- stan/math/rev/mat/functor/integrate_1d_tsc.hpp
+++ stan/math/rev/mat/functor/integrate_1d_tsc.hpp
@@ -1,17 +1,16 @@
 #ifndef STAN_MATH_PRIM_ARR_FUNCTOR_integrate_1d_tsc_HPP
 #define STAN_MATH_PRIM_ARR_FUNCTOR_integrate_1d_tsc_HPP
 
-#include <stan/math/prim/scal/meta/is_constant_struct.hpp>
-#include <stan/math/prim/mat/fun/value_of.hpp>
-#include <stan/math/rev/scal/fun/value_of.hpp>
-#include <stan/math/rev/scal/fun/to_var.hpp>
 #include <stan/math/rev/mat/fun/to_var.hpp>
-#include <stan/math/rev/arr/fun/to_var.hpp>
+#include <stan/math/prim/mat/fun/value_of.hpp>
+#include <stan/math/prim/scal/meta/is_constant_struct.hpp>
 #include <stan/math/prim/scal/err/check_finite.hpp>
-
 #include <stan/math/prim/scal/meta/scalar_seq_view.hpp>
 #include <stan/math/prim/scal/meta/operands_and_partials.hpp>
+#include <stan/math/rev/arr/fun/to_var.hpp>
 #include <stan/math/rev/mat/functor/de_integrator.hpp>
+#include <stan/math/rev/scal/fun/value_of.hpp>
+#include <stan/math/rev/scal/fun/to_var.hpp>
 #include <functional>
 #include <cmath>
 #include <ostream>
@@ -22,26 +21,28 @@ namespace stan {
 namespace math {
 
 /**
- * Return the numerical integral of a function f given its gradient
- * g.
+ * Return the numerical integral of the specified function and
+ * gradient function, within the specified integration limits, given
+ * parameters, data, and tolerances for integration.
  *
- * This function uses the algorithm of integrate_1d_tscg for
- * numerical integration.
+ * This function uses the algorithm of <code>integrate_1d_tscg</code>
+ * for numerical integration.
  *
  * @tparam T Type of f.
  * @tparam G Type of g.
  * @tparam T_param Type of param.
  * @tparam T_x_r Type of x_r.
  * @tparam T_x_i Type of x_i.
+
  * @param f a functor with signature
- * double (double, std::vector<T_param>, std::vector<T_x_r>,
- * std::vector<T_x_i>, std::ostream&) or with signature
- * double (double, T_param, T_x_r, T_x_i, std::ostream&)
- * or a "mixture" of these, where the first argument is one being
- * integrated and the second one is either an extra scalar or vector
- * being passed to f.
+ * <code>double(double, C_param, data C_x_r, data C_x_i, ostream&)</code>
+ * where `C_param` is `T_param` or `vector<T_param>`, `C_x_r` is
+ * `T_x_r` or `vector<T_x_r>`, and `C_x_i` is `T_x_i` or
+ * `vector<T_x_i>`, with the first argument being integrated, the
+ * second being parameters, the next two being fixed data, and the
+ * last being a stream for messages.
  * @param g a functor with signature
- * double (double, std::vector<T_param>, std::vector<T_x_r>,
+ * double(double, std::vector<T_param>, std::vector<T_x_r>,
  * std::vector<T_x_i>, int, std::ostream&) or with signature double
  * (double, T_param, T_x_r, T_x_i, int, std::ostream&)
  * where the first argument is being integrated and the second one is
@@ -64,39 +65,30 @@ inline typename scalar_type<T_param>::type integrate_1d_tscg(
     const F& f, const G& g, const double a, const double b,
     const T_param& param, const T_x_r& x_r, const T_x_i& x_i,
     std::ostream& msgs, const double tre = 1e-6, const double tae = 1e-6) {
+  using std::placeholders::_1;
   check_finite("integrate_1d_tsc", "lower limit", a);
   check_finite("integrate_1d_tsc", "upper limit", b);
-
-  using std::placeholders::_1;
-
-  // hard case, we want a normalizing factor
-  if (!is_constant_struct<T_param>::value) {
-    size_t N = length(param);
-    std::vector<double> grad(N);
-
-    auto value_of_param = value_of(param);
-
-    for (size_t n = 0; n < N; n++)
-      grad[n] = de_integrator(
-          std::bind<double>(g, _1, value_of_param, x_r, x_i,
-                            static_cast<int>(n + 1), std::ref(msgs)),
-          a, b, tre, tae);
-
-    double val_ = de_integrator(
-        std::bind<double>(f, _1, value_of_param, x_r, x_i, std::ref(msgs)), a,
-        b, tre, tae);
-
-    operands_and_partials<T_param> ops_partials(param);
-    for (size_t n = 0; n < N; n++)
-      ops_partials.edge1_.partials_[n] += grad[n];
-
-    return ops_partials.build(val_);
-  }
-  // easy case, here we are calculating a normalizing constant,
-  // not a normalizing factor, so g doesn't matter at all
-  return de_integrator(
-      std::bind<double>(f, _1, value_of(param), x_r, x_i, std::ref(msgs)), a, b,
-      tre, tae);
+  // no normalizing case required
+  if (is_constant_struct<T_param>::value)
+    return de_integrator(std::bind<double>(f, _1, value_of(param),
+                                           x_r, x_i, std::ref(msgs)),
+                         a, b, tre, tae);
+  // requires normalization
+  size_t N = length(param);
+  std::vector<double> grad(N);
+  auto value_of_param = value_of(param);
+  for (size_t n = 0; n < N; n++)
+    grad[n] = de_integrator(
+        std::bind<double>(g, _1, value_of_param, x_r, x_i,
+                          static_cast<int>(n + 1), std::ref(msgs)),
+        a, b, tre, tae);
+  double val_ = de_integrator(
+      std::bind<double>(f, _1, value_of_param, x_r, x_i, std::ref(msgs)), a,
+      b, tre, tae);
+  operands_and_partials<T_param> ops_partials(param);
+  for (size_t n = 0; n < N; n++)
+    ops_partials.edge1_.partials_[n] += grad[n];
+  return ops_partials.build(val_);
 }
 
 /**
