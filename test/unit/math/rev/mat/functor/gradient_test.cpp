7550dc046543fbc0a3075946b60deca5fcf697e0
Sebastian Weber <sebastian.weber@novartis.com>
2018-04-04 09:30:58 +0200

    make test fire up different functions and work around linux issues and old mac compilers (hopefully)

--- test/unit/math/rev/mat/functor/gradient_test.cpp
+++ test/unit/math/rev/mat/functor/gradient_test.cpp
@@ -34,38 +34,50 @@ TEST(AgradAutoDiff, gradient) {
 // test threaded AD
 TEST(AgradAutoDiff, gradient_threaded) {
   fun1 f;
-  Matrix<double, Dynamic, 1> x(2);
-  x << 5, 7;
+  Matrix<double, Dynamic, 1> x_ref(2);
+  x_ref << 5, 7;
   double fx_ref;
   Matrix<double, Dynamic, 1> grad_fx_ref;
-  stan::math::gradient(f, x, fx_ref, grad_fx_ref);
-  EXPECT_FLOAT_EQ(5 * 5 * 7 + 3 * 7 * 7, fx_ref);
+  stan::math::gradient(f, x_ref, fx_ref, grad_fx_ref);
+  EXPECT_FLOAT_EQ(x_ref(0) * x_ref(0) * x_ref(1) + 3 * x_ref(1) * x_ref(1), fx_ref);
   EXPECT_EQ(2, grad_fx_ref.size());
-  EXPECT_FLOAT_EQ(2 * x(0) * x(1), grad_fx_ref(0));
-  EXPECT_FLOAT_EQ(x(0) * x(0) + 3 * 2 * x(1), grad_fx_ref(1));
+  EXPECT_FLOAT_EQ(2 * x_ref(0) * x_ref(1), grad_fx_ref(0));
+  EXPECT_FLOAT_EQ(x_ref(0) * x_ref(0) + 3 * 2 * x_ref(1), grad_fx_ref(1));
 
-  auto thread_job = [&]() {
+  auto thread_job = [&](double x1, double x2) {
     double fx;
+    VectorXd x_local(2);
+    x_local << x1, x2;
     VectorXd grad_fx;
-    stan::math::gradient(fun1(), x, fx, grad_fx);
+    stan::math::gradient(fun1(), x_local, fx, grad_fx);
     VectorXd res(1 + grad_fx.size());
     res(0) = fx;
     res.tail(grad_fx.size()) = grad_fx;
     return res;
   };
 
-  std::vector<std::future<VectorXd>> ad_futures;
+  // schedule a bunch of jobs which all do the same
+  std::vector<std::future<VectorXd>> ad_futures_ref;
 
   for (std::size_t i = 0; i < 100; i++) {
     // the use pattern in stan-math will be to defer the first job in
-    // order to make the main thread to some work which is why we
+    // order to make the main thread do some work which is why we
     // alter the execution policy here
-    ad_futures.emplace_back(std::async(
-        i == 0 ? std::launch::deferred : std::launch::async, thread_job));
+    ad_futures_ref.emplace_back(std::async(
+                                           i == 0 ? std::launch::deferred : std::launch::async, thread_job, x_ref(0), x_ref(1)));
+  }
+
+  // and schedule a bunch of jobs which all do different things (all
+  // at the same time)
+  std::vector<std::future<VectorXd>> ad_futures_local;
+
+  for (std::size_t i = 0; i < 100; i++) {
+    ad_futures_local.emplace_back(std::async(
+                                             i == 0 ? std::launch::deferred : std::launch::async, thread_job, 1.0*i, 2.0*i));
   }
 
   for (std::size_t i = 0; i < 100; i++) {
-    const VectorXd& ad_result = ad_futures[i].get();
+    const VectorXd& ad_result = ad_futures_ref[i].get();
     double fx_job = ad_result(0);
     VectorXd grad_fx_job = ad_result.tail(ad_result.size() - 1);
 
@@ -73,6 +85,19 @@ TEST(AgradAutoDiff, gradient_threaded) {
     EXPECT_EQ(grad_fx_ref.size(), grad_fx_job.size());
     EXPECT_FLOAT_EQ(grad_fx_ref(0), grad_fx_job(0));
     EXPECT_FLOAT_EQ(grad_fx_ref(1), grad_fx_job(1));
+  }
+  
+  for (std::size_t i = 0; i < 100; i++) {
+    const VectorXd& ad_result = ad_futures_local[i].get();
+    double fx_job = ad_result(0);
+    VectorXd x_local(2);
+    x_local << 1.0*i, 2.0*i;
+    VectorXd grad_fx_job = ad_result.tail(ad_result.size() - 1);
+
+    EXPECT_FLOAT_EQ(x_local(0) * x_local(0) * x_local(1) + 3 * x_local(1) * x_local(1), fx_job);
+    EXPECT_EQ(2, grad_fx_job.size());
+    EXPECT_FLOAT_EQ(2 * x_local(0) * x_local(1), grad_fx_job(0));
+    EXPECT_FLOAT_EQ(x_local(0) * x_local(0) + 3 * 2 * x_local(1), grad_fx_job(1));
   }
 }
 
