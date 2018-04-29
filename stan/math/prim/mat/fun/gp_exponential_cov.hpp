b88c6f21f9bfbae8fde063a561b231b46413edcf
drezap <drezap@umich.edu>
2018-04-30 05:18:22 +0800

    move include order, jenkins doesn't like cmath #1

--- stan/math/prim/mat/fun/gp_exponential_cov.hpp
+++ stan/math/prim/mat/fun/gp_exponential_cov.hpp
@@ -1,11 +1,11 @@
 #ifndef STAN_MATH_PRIM_MAT_FUN_GP_EXPONENTIAL_COV_HPP
 #define STAN_MATH_PRIM_MAT_FUN_GP_EXPONENTIAL_COV_HPP
 
-#include <cmath>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/prim/scal/fun/square.hpp>
 #include <stan/math/prim/scal/fun/squared_distance.hpp>
 #include <stan/math/prim/scal/meta/return_type.hpp>
+#include <cmath>
 #include <vector>
 
 namespace stan {
