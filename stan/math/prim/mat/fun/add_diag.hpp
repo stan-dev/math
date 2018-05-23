b62e3dc1aeed1fe7a3ac4833cd2c3c4e25cd8309
Stan Jenkins <mc.stanislaw@gmail.com>
2018-05-23 10:25:59 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/mat/fun/add_diag.hpp
+++ stan/math/prim/mat/fun/add_diag.hpp
@@ -12,10 +12,10 @@ namespace stan {
 namespace math {
 /**
  * Returns a Matrix with a constant added along the main diagonal.
- * 
+ *
  * @tparam T_m type of element in Eigen::Matrix
  * @tparam T_a type of element to add along the diagonal
- * 
+ *
  * @param mat a matrix of dimensions greater than 1, 1
  * @param to_add a constant to add along the diagonal
  * @return a matrix with a constant added along main diagonal
@@ -27,8 +27,8 @@ add_diag(const Eigen::Matrix<T_m, -1, -1> &mat, const T_a &to_add) {
   size_t length_diag;
   length_diag = fmin(mat.rows(), mat.cols());
   Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out = mat;
-  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1>
-    temp_vec = mat.diagonal();
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1> temp_vec
+      = mat.diagonal();
 
   for (size_t i = 0; i < length_diag; ++i)
     temp_vec[i] += to_add;
@@ -39,10 +39,10 @@ add_diag(const Eigen::Matrix<T_m, -1, -1> &mat, const T_a &to_add) {
 
 /**
  * Returns a Matrix with a vector added along the main diagonal.
- * 
+ *
  * @tparam T_m type of element in Eigen::Matrix
  * @tparam T_a type of element to add along the diagonal
- * 
+ *
  * @param mat a matrix of dimensions greater than 1, 1
  * @param to_add a vector of elements to be added along the main diagonal
  * @return a matrix with a constant added along main diagonal
@@ -53,8 +53,8 @@ inline typename Eigen::Matrix<typename return_type<T_m, T_a>::type,
 add_diag(const Eigen::Matrix<T_m, -1, -1> &mat,
          const Eigen::Matrix<T_a, 1, -1> &to_add) {
   Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out = mat;
-  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1>
-    temp_vec = mat.diagonal();
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1> temp_vec
+      = mat.diagonal();
 
   out.diagonal() += to_add;
   return out;
@@ -62,10 +62,10 @@ add_diag(const Eigen::Matrix<T_m, -1, -1> &mat,
 
 /**
  * Returns a Matrix with a vector added along the main diagonal.
- * 
+ *
  * @tparam T_m type of element in Eigen::Matrix
  * @tparam T_a type of element to add along the diagonal
- * 
+ *
  * @param mat a matrix of dimensions greater than 1, 1
  * @param to_add a vector of elements to be added along the main diagonal
  * @return a matrix with a constant added along main diagonal
@@ -76,12 +76,12 @@ inline typename Eigen::Matrix<typename return_type<T_m, T_a>::type,
 add_diag(const Eigen::Matrix<T_m, -1, -1> &mat,
          const Eigen::Matrix<T_a, -1, 1> &to_add) {
   Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out = mat;
-  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1>
-    temp_vec = mat.diagonal();
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1> temp_vec
+      = mat.diagonal();
 
   out.diagonal() += to_add;
   return out;
 }
-}
-}
+}  // namespace math
+}  // namespace stan
 #endif
