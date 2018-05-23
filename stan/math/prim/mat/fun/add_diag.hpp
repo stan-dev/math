8e02dc780ace6137993ac15ddc81e00f6685b295
andre <andre@cluster-local.node.drelen>
2018-05-23 17:21:57 +0300

    add diag stuff

--- stan/math/prim/mat/fun/add_diag.hpp
+++ stan/math/prim/mat/fun/add_diag.hpp
@@ -2,39 +2,86 @@
 #define STAN_MATH_PRIM_MAT_FUN_ADD_DIAG_HPP
 
 #include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/prim/scal/err/check_not_nan.hpp>
+#include <stan/math/prim/scal/err/check_finite.hpp>
+#include <stan/math/prim/scal/err/check_greater_or_equal.hpp>
 #include <stan/math/prim/scal/fun/fmin.hpp>
-
-// testing
-#include </home/andre/math/stan/math/prim/scal/meta/return_type.hpp>
+#include <stan/math/prim/scal/meta/return_type.hpp>
 
 namespace stan {
 namespace math {
-
+/**
+ * Returns a Matrix with a constant added along the main diagonal.
+ * 
+ * @tparam T_m type of element in Eigen::Matrix
+ * @tparam T_a type of element to add along the diagonal
+ * 
+ * @param mat a matrix of dimensions greater than 1, 1
+ * @param to_add a constant to add along the diagonal
+ * @return a matrix with a constant added along main diagonal
+ */
 template <typename T_m, typename T_a>
-inline typename Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1>
+inline typename Eigen::Matrix<typename return_type<T_m, T_a>::type,
+                              Eigen::Dynamic, Eigen::Dynamic>
 add_diag(const Eigen::Matrix<T_m, -1, -1> &mat, const T_a &to_add) {
-  // check matrix non-empty
-  // check nrow, ncol >= 1
-  // check to_add not nan, not inf,
-
   size_t length_diag;
-  length_diag = fmin(mat.rows(), mat.cols()); // or use Eigen's function?
-  //  Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out;
-  Eigen::MatrixXd out;
-  //  out = mat; //  I could likewise remote const and motify mat itself
-  //  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1> temp_vec;
+  length_diag = fmin(mat.rows(), mat.cols());
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out = mat;
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1>
+    temp_vec = mat.diagonal();
 
-  // Eigen::Matrix<T_m, 1, -1> temp_vec;
-  // temp_vec = mat.diagonal(); //  so we need not call .diagonal() more than
-  // once
+  for (size_t i = 0; i < length_diag; ++i)
+    temp_vec[i] += to_add;
 
-  // for (size_t i = 0; i < length_diag; ++i)
-  //   out(i, i) = temp_vec[i] + to_add;
-  //    temp_vec[i] += to_add;
-  //  return out;
-  return mat;
+  out.diagonal() = temp_vec;
+  return out;
+}
+
+/**
+ * Returns a Matrix with a vector added along the main diagonal.
+ * 
+ * @tparam T_m type of element in Eigen::Matrix
+ * @tparam T_a type of element to add along the diagonal
+ * 
+ * @param mat a matrix of dimensions greater than 1, 1
+ * @param to_add a vector of elements to be added along the main diagonal
+ * @return a matrix with a constant added along main diagonal
+ */
+template <typename T_m, typename T_a>
+inline typename Eigen::Matrix<typename return_type<T_m, T_a>::type,
+                              Eigen::Dynamic, Eigen::Dynamic>
+add_diag(const Eigen::Matrix<T_m, -1, -1> &mat,
+         const Eigen::Matrix<T_a, 1, -1> &to_add) {
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out = mat;
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1>
+    temp_vec = mat.diagonal();
+
+  out.diagonal() += to_add;
+  return out;
+}
+
+/**
+ * Returns a Matrix with a vector added along the main diagonal.
+ * 
+ * @tparam T_m type of element in Eigen::Matrix
+ * @tparam T_a type of element to add along the diagonal
+ * 
+ * @param mat a matrix of dimensions greater than 1, 1
+ * @param to_add a vector of elements to be added along the main diagonal
+ * @return a matrix with a constant added along main diagonal
+ */
+template <typename T_m, typename T_a>
+inline typename Eigen::Matrix<typename return_type<T_m, T_a>::type,
+                              Eigen::Dynamic, Eigen::Dynamic>
+add_diag(const Eigen::Matrix<T_m, -1, -1> &mat,
+         const Eigen::Matrix<T_a, -1, 1> &to_add) {
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, -1, -1> out = mat;
+  Eigen::Matrix<typename return_type<T_m, T_a>::type, 1, -1>
+    temp_vec = mat.diagonal();
+
+  out.diagonal() += to_add;
+  return out;
 }
 }
 }
-
 #endif
--- test/unit/math/prim/mat/fun/add_diag_test.cpp
+++ test/unit/math/prim/mat/fun/add_diag_test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
-#include <limits>
 #include <stan/math/prim/mat.hpp>
+#include <limits>
 #include <string>
 #include <vector>
 
@@ -18,16 +18,16 @@ std::string pull_msg(Eigen::Matrix<T_m, -1, -1> &mat, T_a to_add) {
 }
 
 TEST(MathPrimMat, double_mat_double_add_diag) {
-  // Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
-  // mat << 1, 1, 1, 1, 1, 1;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
 
-  // double jitter = 1e-10;
+  double jitter = 1e-10;
 
-  // Eigen::MatrixXd out_mat;
-  // EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, jitter));
-  // for (int i = 0; i < 2; ++i)
-  //   EXPECT_FLOAT_EQ(1.0 + jitter, out_mat(i, i))
-  //     << "index: ( " << i << ", " << i << ")";
+  Eigen::MatrixXd out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, jitter));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1.0 + jitter, out_mat(i, i))
+      << "index: ( " << i << ", " << i << ")";
 }
 
 TEST(MathPrimMat, var_mat_double_add_diag) {
@@ -36,9 +36,121 @@ TEST(MathPrimMat, var_mat_double_add_diag) {
 
   double jitter = 1e-10;
 
-  Eigen::MatrixXd out_mat;
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
   EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, jitter));
-  // for (int i = 0; i < 2; ++i)
-  //   EXPECT_FLOAT_EQ(1.0 + jitter, out_mat(i, i))
-  //     << "index: ( " << i << ", " << i << ")";
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1.0 + jitter, value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, double_mat_var_add_diag) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  stan::math::var jitter = 1e-10;
+
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, jitter));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1.0 + value_of(jitter), value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, var_mat_var_add_diag) {
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  stan::math::var jitter = 1e-10;
+
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, jitter));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1.0 + value_of(jitter), value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+//  add vector to diag
+
+TEST(MathPrimMat, double_mat_double_vec_add_diag) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  Eigen::Matrix<double, 1, -1> to_add(2);
+  to_add << 0, 1;
+
+  Eigen::MatrixXd out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1 + to_add[i], out_mat(i, i))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, double_mat_double_rvec_add_diag) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  Eigen::Matrix<double, -1, 1> to_add(2);
+  to_add << 0, 1;
+
+  Eigen::MatrixXd out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1 + to_add[i], out_mat(i, i))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, var_mat_double_vec_add_diag) {
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  Eigen::Matrix<double, 1, -1> to_add(2);
+  to_add << 0, 1;
+
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1 + to_add[i], value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, var_mat_double_rvec_add_diag) {
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  Eigen::Matrix<double, -1, 1> to_add(2);
+  to_add << 0, 1;
+
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1 + to_add[i], value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, double_mat_var_vec_add_diag) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  Eigen::Matrix<stan::math::var, 1, -1> to_add(2);
+  to_add << 0, 1;
+
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1 + value_of(to_add[i]), value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
+}
+
+TEST(MathPrimMat, double_mat_var_rvec_add_diag) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
+  mat << 1, 1, 1, 1, 1, 1;
+
+  Eigen::Matrix<stan::math::var, -1, 1> to_add(2);
+  to_add << 0, 1;
+
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> out_mat;
+  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
+  for (int i = 0; i < 2; ++i)
+    EXPECT_FLOAT_EQ(1 + value_of(to_add[i]), value_of(out_mat(i, i)))
+      << "index: ( " << i << ", " << i << ")";
 }
