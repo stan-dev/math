36b22aba2c85cf0178beb3a6d6354b3cfe4d413d
Ben Bales <bbbales2@gmail.com>
2018-05-23 17:50:26 -0700

    Switching 1D integrator to use Boost. Incorporating changes from pull #759 (Issue #210)
    Differences:
    1. Technically support integrals with infinite limits (doesn't work great)
    2. No more relative/absolute tolerance split

---
+++ stan/math/rev/arr/functor/integrate_1d.hpp
@@ -0,0 +1,123 @@
+c#ifndef STAN_MATH_REV_ARR_FUNCTOR_integrate_1d_HPP
+#define STAN_MATH_REV_ARR_FUNCTOR_integrate_1d_HPP
+
+#include <stan/math/prim/scal/meta/is_constant_struct.hpp>
+#include <stan/math/prim/mat/fun/value_of.hpp>
+#include <stan/math/rev/scal/fun/value_of.hpp>
+#include <stan/math/rev/arr/fun/to_var.hpp>
+#include <stan/math/prim/scal/err/check_finite.hpp>
+
+#include <stan/math/prim/scal/meta/scalar_seq_view.hpp>
+#include <cstdint>
+#include <functional>
+#include <cmath>
+#include <ostream>
+#include <vector>
+
+namespace stan {
+namespace math {
+
+/**
+ * Calculate first derivative of f(x, param, std::ostream&)
+ * with respect to the nth parameter. Uses nested reverse mode autodiff
+ */
+template <typename F>
+inline double gradient_of_f(const F& f, const double x,
+                            const std::vector<double>& theta_vals,
+                            const std::vector<double>& x_r, const std::vector<int>& x_i,
+                            size_t n, std::ostream& msgs) {
+  double gradient = 0.0;
+  start_nested();
+  std::vector<var> theta_var(theta_vals.size());
+  try {
+    for(size_t i = 0; i < theta_vals.size(); i++)
+      theta_var[i] = theta_vals[i];
+    f(x, theta_var, x_r, x_i, msgs).grad();
+    gradient = theta_var[n].adj();
+  } catch (const std::exception& e) {
+    recover_memory_nested();
+    throw;
+  }
+  recover_memory_nested();
+
+  return gradient;
+}
+
+/**
+ * Calculate gradient of f(x, param, std::ostream&)
+ * with respect to the nth parameter. Uses forward mode autodiff
+ */
+template <typename F>
+inline double gradient_of_f_fwd(const F& f, const double x,
+                                const std::vector<double>& theta_vals,
+                                const std::vector<double>& x_r, const std::vector<int>& x_i,
+                                size_t n, std::ostream& msgs) {
+  std::vector< fvar<double> > theta_fvar(theta_vals.size());
+  for(size_t i = 0; i < theta_vals.size(); i++) {
+    theta_fvar[i].val_ = theta_vals[i];
+  }
+  theta_fvar[n].d_ = 1.0;
+
+  return f(x, theta_fvar, x_r, x_i, msgs).d_;
+}
+
+/**
+ * Compute the integral of the single variable function f from a to b to within a
+ * specified tolerance. a and b can be finite or infinite.
+ *
+ * a and b cannot be parameters.
+ *
+ * f should have the signature:
+ *   double (double, std::vector<var>, std::vector<double>, std::vector<int>, std::ostream&)
+ *
+ * The first argument is the variable over which f is integrated.
+ * The second argument are parameters, which should be a vector of stan::math::var
+ * The third argument is extra real data, which should always be a vector of doubles
+ * The fourth argument is extra integer data, which should always be a vector of ints
+ * The fifth argument is a stream to which error messages are printed
+ *
+ * Boost decides the integration is converged when subsequent estimates of the
+ * integral are less than tolerance * abs(integral). This means the tolerance is
+ * relative to the actual scale of the integral.
+ *
+ * @tparam T Type of f
+ * @param f a functor with signature
+ *   double (double, std::vector<var>, std::vector<double>, std::vector<int>, std::ostream&)
+ * @param a lower limit of integration
+ * @param b upper limit of integration
+ * @param param additional parameters to be passed to f
+ * @param[in, out] msgs the print stream for warning messages
+ * @param tolerance integrator tolerance passed to Boost quadrature
+ * @return numeric integral of function f
+ */
+template <typename F>
+inline var
+integrate_1d(const F& f, const double a, const double b, const std::vector<var>& theta,
+             const std::vector<double>& x_r, const std::vector<int>& x_i,
+    std::ostream& msgs, const double tolerance = 1e-6) {
+  static const char *function = "integrate_1d";
+  check_less_or_equal(function, "lower limit", a, b);
+
+  if(a == b) {
+    return var(0.0);
+  } else {
+    double integral =
+        integrate(std::bind<double>(f, std::placeholders::_1, value_of(theta), x_r, x_i, std::ref(msgs)),
+                  a, b, tolerance);
+
+    size_t N = theta.size();
+    std::vector<double> dintegral_dtheta(N);
+    std::vector<double> theta_vals = value_of(theta);
+
+    for (size_t n = 0; n < N; ++n) {
+      dintegral_dtheta[n] = integrate(std::bind<double>(gradient_of_f<F>, f, std::placeholders::_1, theta_vals, x_r, x_i, n, std::ref(msgs)), a, b, tolerance);
+    }
+
+    return precomputed_gradients(integral, theta, dintegral_dtheta);
+  }
+}
+
+}  // namespace math
+}  // namespace stan
+
+#endif
---
+++ test/unit/math/rev/arr/functor/integrate_1d_test.cpp
@@ -0,0 +1,168 @@
+#include <stan/math.hpp>
+#include <stan/math/mix/mat.hpp>
+#include <gtest/gtest.h>
+#include <test/unit/util.hpp>
+#include <test/unit/math/rev/mat/fun/util.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+std::ostringstream msgs;
+
+struct f1 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(x) + theta[0];
+  }
+};
+
+struct f2 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(theta[0] * cos(2 * 3.141593 * x)) + theta[0];
+  }
+};
+
+struct f3 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(x) + pow(theta[0], x_r[0]) + 2 * pow(theta[1], x_r[1]) + 2 * theta[2];
+  }
+};
+
+struct f4 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(-x) / sqrt(x);
+  }
+};
+
+struct f5 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(-theta[0] * x) / sqrt(theta[1] * x);
+  }
+};
+
+struct f6 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return sqrt(x / (1 - theta[0] * x * x));
+  }
+};
+
+struct f7 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(-theta[0] * x);
+  }
+};
+
+struct f8 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(theta[0] * x);
+  }
+};
+
+struct f9 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return exp(-pow(x - x_r[0], x_i[0]) / pow(x_r[1], x_i[0]));
+  }
+};
+
+struct f10 {
+  template <typename T1, typename T2>
+  inline typename stan::return_type<T1, T2>::type operator()(
+      const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+    return 1 / (1 + pow(x, x_i[0]) / x_r[0]);
+  }
+};
+
+/*
+ * test_derivatives is a helper function to make it easy to test the integrate_1d
+ * function.
+ *
+ * It takes in a callable function object, integration limits, parameters, real and
+ * integer data. It integrates the provided function and compares the computed integral
+ * and gradients against the provided integral value (val) and gradients (grad).
+ *
+ * The prototype for f is:
+ *   struct f10 {
+ *     template <typename T1, typename T2>
+ *     inline typename stan::return_type<T1, T2>::type operator()(
+ *         const T1& x, const std::vector<T2>& theta, const std::vector<double>& x_r, const std::vector<int>& x_i, std::ostream& msgs) const {
+ *     }
+ *  };
+ *
+ * @tparam F Type of f
+ * @param f a functor with signature given above
+ * @param a lower limit of integration
+ * @param b upper limit of integration
+ * @param param parameters to be passed to f (should be std::vector<stan::math::var>)
+ * @param x_r real data to be passed to f (should be std::vector<double>)
+ * @param x_i integer data to be passed to f (should be std::vector<int>)
+ * @param val correct value of integral
+ * @param grad correct value of gradients
+ */
+template <typename F>
+void test_derivatives(const F& f, double a, double b, std::vector<double> thetas, const std::vector<double>& x_r, const std::vector<int>& x_i, double val, std::vector<double> grad) {
+  using stan::math::var;
+  
+  std::vector<double> tolerances = { 1e-4, 1e-6, 1e-8 };
+
+  for(auto tolerance : tolerances) {
+    // Reset autodiff stack
+    stan::math::recover_memory();
+    // Convert parameters into vars for actual calls
+    std::vector<var> theta_vars(thetas.size());
+    for(size_t i = 0; i < thetas.size(); ++i) {
+      theta_vars[i] = thetas[i];
+    }
+    
+    var integral = stan::math::integrate_1d(f, a, b, theta_vars, x_r, x_i, msgs, tolerance);
+    std::vector<double> computed_grad;
+    integral.grad(theta_vars, computed_grad);
+    EXPECT_FLOAT_EQ(val, integral.val());
+    for(size_t i = 0; i < grad.size(); ++i) {
+      EXPECT_FLOAT_EQ(grad[i], computed_grad[i]);
+    }
+  }
+}
+
+TEST(StanMath_integrate_1d, TestDerivatives) {
+  // Easy integrals
+  test_derivatives(f1{}, 0.2, 0.7, {0.75}, {}, {}, 0.7923499493102901 + 0.5 * 0.75, {0.5});
+  test_derivatives(f2{}, 0.0, 1.0, {0.5}, {}, {}, 1.563483370741325, {1.257894305390898}) ;
+  test_derivatives(f1{}, 0.0, 0.0, {0.75}, {}, {}, 0.0, {0.0});
+  test_derivatives(f2{}, 1.0, 1.0, {0.5}, {}, {}, 0.0, {0.0}) ;
+  // Zero crossing integral + test x_r
+  test_derivatives(f3{}, -1.0, 1.0, {0.5, 1.75, 3.9}, {2.5, 3.0}, {}, 2.350402387287579 + 2.0 * pow(0.5, 2.5) + 4.0 * pow(1.75, 3.0) + 4.0 * 3.9, { 5 * pow(0.5, 1.5), 12 * 1.75 * 1.75, 4.0});
+  // Tricky integral from Boost docs + limit at infinity + no gradients
+  test_derivatives(f4{}, 0.0, std::numeric_limits<double>::infinity(), {}, {}, {}, 1.772453850905516, {});
+  // Tricky integral from Boost docs + limit at infinity with gradients
+  test_derivatives(f5{}, 0.0, std::numeric_limits<double>::infinity(), {0.5, 3.0}, {}, {},
+                   1.772453850905516 / sqrt(0.5 * 3.0),
+                   {-1.772453850905516 * 3.0 / (2 * pow(0.5 * 3.0, 1.5)),
+                         -1.772453850905516 * 0.5 / (2 * pow(0.5 * 3.0, 1.5))});
+  // Tricky integral from Boost docs
+  test_derivatives(f6{}, 0.0, 1.0, {0.75}, {}, {}, 0.851926727945904, {0.4814066053874294});
+  // Zero crossing integral + limit at infinity
+  test_derivatives(f7{}, -5.0, std::numeric_limits<double>::infinity(), {1.5}, {}, {}, 1205.361609637375, {5223.23364176196});
+  // Zero crossing integral + limit at negative infinity
+  test_derivatives(f8{}, -std::numeric_limits<double>::infinity(), 5.0, {1.5}, {}, {}, 1205.361609637375, {5223.23364176196});
+  // Both limits at infinity + test x_r/x_i + no gradients
+  test_derivatives(f9{}, -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), {}, {5.0, 1.7}, {2}, 3.013171546539377, {});
+  // Both limits at infinity + test x_r/x_i + no gradients
+  test_derivatives(f10{}, -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), {}, {1.7}, {4}, 2.536571480364399, {});
+}
