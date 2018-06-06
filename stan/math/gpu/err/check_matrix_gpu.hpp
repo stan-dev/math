450747eb382bca501b5dc7c6d0014eed249bfa73
Steve Bronder <stevo15025@gmail.com>
2018-06-05 20:46:09 -0400

    cpplint error

--- stan/math/gpu/err/check_matrix_gpu.hpp
+++ stan/math/gpu/err/check_matrix_gpu.hpp
@@ -180,7 +180,6 @@ inline void check_diagonal_zeros(const char* function, const char* name,
     if (flag) {
       domain_error(function, name, "has zeros on the diagonal.", "");
     }
-
   } catch (const cl::Error& e) {
     check_opencl_error("diag_zeros_check", e);
   }
