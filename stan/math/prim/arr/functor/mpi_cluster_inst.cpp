237b69e68a041f75cdea4be192417fca2f759303
Sebastian Weber <sdw.post@gmail.com>
2018-06-24 12:43:41 +0200

    align MPI code instantiation with stan conventions as good as possible

--- stan/math/prim/arr/functor/mpi_cluster_inst.cpp
+++ stan/math/prim/arr/functor/mpi_cluster_inst.cpp
@@ -2,7 +2,8 @@
 
 #include <stan/math/prim/arr/functor/mpi_cluster.hpp>
 
-// register stop worker command
+// register stop worker command (instantiates boost serialization
+// templates)
 STAN_REGISTER_MPI_COMMAND(stan::math::mpi_stop_worker)
 
 #endif
