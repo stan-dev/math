5e7eb9c54ff2a67736715ae6935edf89d2bc580d
drezap <drezap@umich.edu>
2018-06-25 15:08:51 +0300

    Update gp_periodic_cov_test.cpp

--- test/unit/math/rev/mat/fun/gp_periodic_cov_test.cpp
+++ test/unit/math/rev/mat/fun/gp_periodic_cov_test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
-#include <limits>
 #include <stan/math/rev/mat.hpp>
-#include <string>
 #include <test/unit/math/rev/mat/util.hpp>
+#include <limits>
+#include <string>
+
 #include <vector>
 
 template <typename T_x1, typename T_x2, typename T_sigma, typename T_l,
