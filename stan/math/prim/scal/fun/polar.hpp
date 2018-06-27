dc5f2c2fa3163881578eefd7bc546af23b25ed57
Stan Jenkins <mc.stanislaw@gmail.com>
2018-06-27 19:06:21 -0400

    [Jenkins] auto-formatting by clang-format version 5.0.2-svn328729-1~exp1~20180406212027.91 (branches/release_50)

--- stan/math/prim/scal/fun/polar.hpp
+++ stan/math/prim/scal/fun/polar.hpp
@@ -12,7 +12,7 @@ namespace std {
  * Returns a complex number with magnitude r
  * and phase theta
  *
- * This is in the std namespace because the 
+ * This is in the std namespace because the
  * relevant call we are catching is fully
  * qualified with std, making ADL useless.
  *
@@ -22,14 +22,12 @@ namespace std {
  * @param theta angle, radians
  * @return complex with magnitude r & phase theta
  */
-template <class R, class TH, class RT = typename
- stan::return_type<stan::rm_zeroing_t<R>,
-  stan::rm_zeroing_t<TH>>::type,
- std::enable_if_t<
- !std::is_same<R, TH>::value>* =nullptr>
-inline std::complex<RT>
-polar(R const& r, TH const& theta) {
- return polar(RT(r),RT(theta));
+template <class R, class TH,
+          class RT = typename stan::return_type<stan::rm_zeroing_t<R>,
+                                                stan::rm_zeroing_t<TH>>::type,
+          std::enable_if_t<!std::is_same<R, TH>::value>* = nullptr>
+inline std::complex<RT> polar(R const& r, TH const& theta) {
+  return polar(RT(r), RT(theta));
 }
 
 }  // namespace std
