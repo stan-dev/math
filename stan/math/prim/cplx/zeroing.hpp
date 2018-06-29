eab43598764228a45b8f4eba55330ea6890f6cc1
Chris Chiasson <chris.chiasson@gmail.com>
2018-06-29 15:59:00 +0000

    added more description to zeroing, and removed single level upcast that won't be used

--- stan/math/prim/cplx/zeroing.hpp
+++ stan/math/prim/cplx/zeroing.hpp
@@ -7,17 +7,20 @@ namespace stan {
 namespace math {
 
 /**
- *  z<T> is a zeroing T that std::complex<T> implicitly uses
- *  when inheriting from stan::math::complex. It has the
- *  property that z() results in T(0). See stan's
- *  complex class for details.
+ *  zeroing<T> is a zeroing T that std::complex<T> implicitly 
+ *  uses  when inheriting from stan::math::complex. It has the
+ *  property that zeroing() results in T(0). See stan's
+ *  complex class for details. Without this, in libstdc++'s
+ *  complex class, when T() is called, vars (and fvar<vars>)
+ *  will have a null pointer, which is bad because libstdc++
+ *  immediately uses such expressions as if they were
+ *  initialized to zero..
  */
 template <class T = double>
 struct zeroing : T {
   using T::T;  // inherit ctors
   template <std::enable_if_t<stan::is_arith_like<T>::value>* = nullptr>
   zeroing(T const& t = 0) : T(t) {}  // NOLINT(runtime/explicit)
-  operator T() const { return *static_cast<T*>(this); }  // upcast
 };
 
 }  // namespace math
