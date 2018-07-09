09f901945d0a10e0bea124e162f361106075a9ad
Chris Chiasson <chris@chiasson.name>
2018-07-09 11:53:07 -0500

    fix return type on abs functions (whoops)

--- stan/math/rev/scal/fun/abs.hpp
+++ stan/math/rev/scal/fun/abs.hpp
@@ -40,7 +40,7 @@ inline var abs(const var& a) { return fabs(a); }
 
 namespace std {
 // constrained complex overload to forward zeroing var to std::abs
-inline std::complex<stan::math::var> abs(
+inline stan::math::var abs(
     std::complex<stan::math::var> const& t) {
   return abs(std::complex<stan::math::zeroing<stan::math::var>>(t));
 }
