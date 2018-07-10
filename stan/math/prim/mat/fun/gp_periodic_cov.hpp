249aad83221764306f4b5d3dd6b9c0c8bd67483e
Stan Jenkins <mc.stanislaw@gmail.com>
2018-07-10 11:42:48 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/mat/fun/gp_periodic_cov.hpp
+++ stan/math/prim/mat/fun/gp_periodic_cov.hpp
@@ -73,8 +73,9 @@ gp_periodic_cov(const std::vector<T_x> &x, const T_sigma &sigma, const T_l &l,
   for (size_t j = 0; j < x_size; ++j) {
     cov(j, j) = sigma_sq;
     for (size_t i = j + 1; i < x_size; ++i) {
-      cov(i, j) = sigma_sq * exp(square(sin(pi_div_p * distance(x[i], x[j]))) *
-                                 neg_two_inv_l_sq);
+      cov(i, j) = sigma_sq
+                  * exp(square(sin(pi_div_p * distance(x[i], x[j])))
+                        * neg_two_inv_l_sq);
       cov(j, i) = cov(i, j);
     }
   }
@@ -141,14 +142,14 @@ gp_periodic_cov(const std::vector<T_x1> &x1, const std::vector<T_x2> &x2,
 
   for (size_t i = 0; i < x1.size(); ++i) {
     for (size_t j = 0; j < x2.size(); ++j) {
-      cov(i, j) =
-          sigma_sq * exp(square(sin(pi_div_p * distance(x1[i], x2[j]))) *
-                         neg_two_inv_l_sq);
+      cov(i, j) = sigma_sq
+                  * exp(square(sin(pi_div_p * distance(x1[i], x2[j])))
+                        * neg_two_inv_l_sq);
     }
   }
   return cov;
 }
-} // namespace math
-} // namespace stan
+}  // namespace math
+}  // namespace stan
 
 #endif
