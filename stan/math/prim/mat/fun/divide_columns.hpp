7401f218173570e083113b2bafdc7302c1375880
andre <andre@cluster-local.node.drelen>
2018-07-12 21:25:20 +0300

    add doxygen for the divide_columns function

--- stan/math/prim/mat/fun/divide_columns.hpp
+++ stan/math/prim/mat/fun/divide_columns.hpp
@@ -8,6 +8,20 @@
 namespace stan {
 namespace math {
 
+/**
+ * Takes Stan data type vector[n] x[D] and divides each
+ * dimension by the specified scalar.
+ *
+ *
+ * @tparam T_x type of elements contained in vector x, usually Eigen::Matrix
+ * @tparam T_s type of element of scalar, usually double or var
+ * @tparam R   number of rows in the submatrix of x
+ * @tparam C   number of columns in the submatrix of x
+ *
+ * @param x std::vector of elements representing a matrix
+ * @param scalar a scalar type of elementsx
+ *
+ */
 template <typename T_x, typename T_s, int R, int C>
 inline typename std::vector<Eigen::Matrix<T_x, R, C>> divide_columns(
     const std::vector<Eigen::Matrix<T_x, R, C>> &x, const T_s &scalar) {
@@ -19,6 +33,20 @@ inline typename std::vector<Eigen::Matrix<T_x, R, C>> divide_columns(
   return out;
 }
 
+/**
+ * Takes Stan data type vector[n] x[D] and divides each
+ * dimension sequentially by each element of the vector
+ *
+ * @tparam T_x type of elements contained in vector x, usually Eigen::Matrix
+ * @tparam T_v vector of elements
+ * @tparam R   number of rows in the submatrix of x
+ * @tparam C   number of columns in the submatrix of x
+ *
+ * @param x std::vector of elements representing a matrix
+ * @param scalar a scalar type of elementsx
+ * @throw std::invalid argument if D != length of vector
+ *
+ */
 template <typename T_x, typename T_v, int R, int C>
 inline typename std::vector<Eigen::Matrix<T_x, R, C>> divide_columns(
     const std::vector<Eigen::Matrix<T_x, R, C>> &x,
