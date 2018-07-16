5957b45f23f87c5f9b6e2390e1df72aa6ea1fdbe
Steve Bronder <stevo15025@gmail.com>
2018-07-16 18:52:15 -0400

    Add documentation to set_kernel_args

--- stan/math/gpu/set_kernel_args.hpp
+++ stan/math/gpu/set_kernel_args.hpp
@@ -7,7 +7,18 @@ namespace stan {
 namespace math {
 inline void _set_kernel_args(cl::Kernel &k, int i) {}  // terminating function
 
-// simpleopencl.blogspot.com/2013/04/calling-kernels-with-large-number-of.html
+/**
+ * Used in <code>set_kernel_args()</code> to add arguments to an OpenCL kernel.
+ *
+ * @param kernel An OpenCL kernel.
+ * @param i the position of the argument to the OpenCL kernel.
+ * @param first_arg The first argument to go into the OpenCL kernel.
+ * @param extra_args The remaining arguments to go into the OpenCL kernel.
+ * @tparam T the type of <code>first_arg</code>.
+ * @tparam Args The types of <code>extra_args</code>.
+ * @note Comes from:
+ * simpleopencl.blogspot.com/2013/04/calling-kernels-with-large-number-of.html
+ */
 template <typename T, typename... Args>
 inline void _set_kernel_args(cl::Kernel &kernel, int i, const T &first_arg,
                              const Args &... extra_args) {
@@ -15,6 +26,15 @@ inline void _set_kernel_args(cl::Kernel &kernel, int i, const T &first_arg,
   _set_kernel_args(kernel, i + 1, extra_args...);
 }
 
+/**
+ * Adds arguments to an OpenCL kernel.
+ *
+ * @param kernel An OpenCL kernel.
+ * @param args The arguments to mote to the OpenCL kernel.
+ * @tparam Args The types of <code>extra_args</code>.
+ * @note Comes from:
+ * simpleopencl.blogspot.com/2013/04/calling-kernels-with-large-number-of.html
+ */
 template <typename... Args>
 inline void set_kernel_args(cl::Kernel &kernel, const Args &... args) {
   _set_kernel_args(kernel, 0, args...);
