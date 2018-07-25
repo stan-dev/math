152ec3132211b12c037147ad30f03a4ecea02073
Chris Chiasson <chris.chiasson@gmail.com>
2018-07-25 19:57:56 +0000

    in rev/core.hpp, std_numeric_limits appears after std_is_inf, but with the forward of the unprefixed ADL-looked-up is infinity call to the std version, numeric limits of the argument type gets checked in the template SFINAE for libc++, so this additional header in the std_is_inf is required

--- stan/math/rev/core/std_isinf.hpp
+++ stan/math/rev/core/std_isinf.hpp
@@ -2,6 +2,7 @@
 #define STAN_MATH_REV_CORE_STD_ISINF_HPP
 
 #include <stan/math/prim/scal/fun/is_inf.hpp>
+#include <stan/math/rev/core/std_numeric_limits.hpp>
 #include <stan/math/rev/core/var.hpp>
 
 namespace std {
