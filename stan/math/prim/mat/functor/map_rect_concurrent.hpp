7edd7a60f0caa3383fb7d70a5cd21bdaca97b999
Stan Jenkins <mc.stanislaw@gmail.com>
2018-07-30 07:01:57 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/mat/functor/map_rect_concurrent.hpp
+++ stan/math/prim/mat/functor/map_rect_concurrent.hpp
@@ -52,12 +52,14 @@ inline int get_num_threads(int num_jobs) {
         num_threads = std::thread::hardware_concurrency();
       else
         invalid_argument("get_num_threads(int)", "STAN_NUM_THREADS",
-          env_stan_num_threads, "The STAN_NUM_THREADS environment variable is '"
-          "' but it must be positive or -1");
+                         env_stan_num_threads,
+                         "The STAN_NUM_THREADS environment variable is '"
+                         "' but it must be positive or -1");
     } catch (boost::bad_lexical_cast) {
-        invalid_argument("get_num_threads(int)", "STAN_NUM_THREADS",
-          env_stan_num_threads, "The STAN_NUM_THREADS environment variable is '"
-          "' but it must be a positive number or -1");
+      invalid_argument("get_num_threads(int)", "STAN_NUM_THREADS",
+                       env_stan_num_threads,
+                       "The STAN_NUM_THREADS environment variable is '"
+                       "' but it must be a positive number or -1");
     }
   }
   if (num_threads > num_jobs)
