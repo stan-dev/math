0d705afcf5fa1bfa43af797f4b84bf26ddecc9a0
Andre Zapico <drezap@umich.edu>
2018-08-02 14:53:54 +0300

    Update dot_product.hpp

--- stan/math/prim/mat/fun/dot_product.hpp
+++ stan/math/prim/mat/fun/dot_product.hpp
@@ -1,10 +1,9 @@
 #ifndef STAN_MATH_PRIM_MAT_FUN_DOT_PRODUCT_HPP
 #define STAN_MATH_PRIM_MAT_FUN_DOT_PRODUCT_HPP
 
-#include <stan/math/prim/arr/err/check_matching_sizes.hpp>
-#include <stan/math/prim/mat/err/check_vector.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
-#include <stan/math/prim/scal/meta/return_type.hpp>
+#include <stan/math/prim/mat/err/check_vector.hpp>
+#include <stan/math/prim/arr/err/check_matching_sizes.hpp>
 #include <vector>
 
 namespace stan {
