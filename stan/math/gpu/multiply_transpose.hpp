9336f8eef1d085d11d1bd5530c5f6a93454bf210
Stan Jenkins <mc.stanislaw@gmail.com>
2018-08-05 15:56:44 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/gpu/multiply_transpose.hpp
+++ stan/math/gpu/multiply_transpose.hpp
@@ -6,50 +6,46 @@
 #include <Eigen/Dense>
 
 namespace stan {
-  namespace math {
-    /**
-     * Computes the product of a square GPU matrix with its transpose.
-     *
-     * Computes the matrix multiplication C = A x A^T
-     *
-     * @param A input matrix
-     * @return the product of the input matrix and its transpose
-     *
-     */
-    inline matrix_gpu multiply_transpose(const matrix_gpu & A) {
-      matrix_gpu temp(A.rows(), A.rows());
-      if (temp.size() == 0)
-        return temp;
-      // padding the matrices so the dimensions are divisible with local
-      // improves performance becasuse we can omit if statements in the
-      // multiply kernel
-      int local = gpu::multiply_workgroup_size;
-      int Mpad = ((A.rows() + local-1)/local)*local;
-      int Npad = ((A.cols() + local-1)/local)*local;
-      matrix_gpu tempPad(Mpad, Mpad);
-      matrix_gpu Apad(Mpad, Npad);
-      Apad.sub_block(A, 0, 0, 0, 0, A.rows(), A.cols());
-      cl::Kernel kernel = opencl_context.get_kernel("multiply_transpose");
-      cl::CommandQueue& cmdQueue = opencl_context.queue();
-      int wpt = 4;
-      try {
-        opencl_context.set_kernel_args(kernel, Apad.buffer(), tempPad.buffer(),
+namespace math {
+/**
+ * Computes the product of a square GPU matrix with its transpose.
+ *
+ * Computes the matrix multiplication C = A x A^T
+ *
+ * @param A input matrix
+ * @return the product of the input matrix and its transpose
+ *
+ */
+inline matrix_gpu multiply_transpose(const matrix_gpu& A) {
+  matrix_gpu temp(A.rows(), A.rows());
+  if (temp.size() == 0)
+    return temp;
+  // padding the matrices so the dimensions are divisible with local
+  // improves performance becasuse we can omit if statements in the
+  // multiply kernel
+  int local = gpu::multiply_workgroup_size;
+  int Mpad = ((A.rows() + local - 1) / local) * local;
+  int Npad = ((A.cols() + local - 1) / local) * local;
+  matrix_gpu tempPad(Mpad, Mpad);
+  matrix_gpu Apad(Mpad, Npad);
+  Apad.sub_block(A, 0, 0, 0, 0, A.rows(), A.cols());
+  cl::Kernel kernel = opencl_context.get_kernel("multiply_transpose");
+  cl::CommandQueue& cmdQueue = opencl_context.queue();
+  int wpt = 4;
+  try {
+    opencl_context.set_kernel_args(kernel, Apad.buffer(), tempPad.buffer(),
                                    Apad.rows(), Apad.cols());
-        cmdQueue.enqueueNDRangeKernel(
-          kernel,
-          cl::NullRange,
-          cl::NDRange(Mpad,  Mpad/wpt),
-          cl::NDRange(local, local/wpt),
-          NULL,
-          NULL);
-      } catch (cl::Error& e) {
-        check_opencl_error("multiply self transpose", e);
-      }
-      temp.sub_block(tempPad, 0, 0, 0, 0, temp.rows(), temp.cols());
-      return temp;
-    }
+    cmdQueue.enqueueNDRangeKernel(kernel, cl::NullRange,
+                                  cl::NDRange(Mpad, Mpad / wpt),
+                                  cl::NDRange(local, local / wpt), NULL, NULL);
+  } catch (cl::Error& e) {
+    check_opencl_error("multiply self transpose", e);
   }
+  temp.sub_block(tempPad, 0, 0, 0, 0, temp.rows(), temp.cols());
+  return temp;
 }
+}  // namespace math
+}  // namespace stan
 
 #endif
 #endif
