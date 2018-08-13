eb3676cf01c13f5d9fc92fa286d4ff429ded48d3
Stan Jenkins <mc.stanislaw@gmail.com>
2018-08-13 12:52:47 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/scal/meta/partials_return_type.hpp
+++ stan/math/prim/scal/meta/partials_return_type.hpp
@@ -11,27 +11,27 @@
 
 namespace stan {
 
-  /**
-   * Template metaprogram to calculate the base scalar return type resulting
-   * from promoting all the scalar types of the template parameters. The
-   * metaprogram can take an arbitrary number of template parameters.
-   *
-   * All C++ primitive types (except <code>long double</code>) are automatically
-   * promoted to <code>double</code>.
-   *
-   * <code>partials_return_type<...></code> is a class defining a single public
-   * typedef <code>type</code> that is <code>var</code> if there is a forward
-   * mode variable type and is <code>double</code> otherwise (this is the most
-   * common case).
-   * Example usage:
-   *
-   *  - <code>return_type<int,double,var>::type</code> is <code>double</code>
-   *  - The same thing with <code>var</code> replaced with a forward mode type
-   *  like <code>fvar<T></code> will return <code>T</code>.
-   *
-   * @tparam T (required) A type
-   * @tparam T_pack (optional) A parameter pack containing further types.
-   */
+/**
+ * Template metaprogram to calculate the base scalar return type resulting
+ * from promoting all the scalar types of the template parameters. The
+ * metaprogram can take an arbitrary number of template parameters.
+ *
+ * All C++ primitive types (except <code>long double</code>) are automatically
+ * promoted to <code>double</code>.
+ *
+ * <code>partials_return_type<...></code> is a class defining a single public
+ * typedef <code>type</code> that is <code>var</code> if there is a forward
+ * mode variable type and is <code>double</code> otherwise (this is the most
+ * common case).
+ * Example usage:
+ *
+ *  - <code>return_type<int,double,var>::type</code> is <code>double</code>
+ *  - The same thing with <code>var</code> replaced with a forward mode type
+ *  like <code>fvar<T></code> will return <code>T</code>.
+ *
+ * @tparam T (required) A type
+ * @tparam T_pack (optional) A parameter pack containing further types.
+ */
 
 template <typename T, typename... T_pack>
 struct partials_return_type {
