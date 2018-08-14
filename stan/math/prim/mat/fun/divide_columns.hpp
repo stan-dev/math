366a33426c7c6fa17fe8b8c5066d8d79f3b6fb85
Stan Jenkins <mc.stanislaw@gmail.com>
2018-08-14 05:52:20 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/mat/fun/divide_columns.hpp
+++ stan/math/prim/mat/fun/divide_columns.hpp
@@ -24,18 +24,16 @@ namespace math {
  *
  */
 template <typename T_x, typename T_v, int R, int C>
-inline typename std::vector<Eigen::Matrix<typename return_type<T_x, T_v,
-                                                               double>::type,
-                                          R, C>>
+inline typename std::vector<
+    Eigen::Matrix<typename return_type<T_x, T_v, double>::type, R, C>>
 divide_columns(const std::vector<Eigen::Matrix<T_x, R, C>> &x,
                const std::vector<T_v> &vec) {
   size_t N = x.size();
   size_t D = x[0].size();
   check_size_match("divide_columns", "x dimension", D, "vector", vec.size());
 
-  std::vector<Eigen::Matrix<
-    typename return_type<T_x, T_v, double>::type, R, C>>
-    out(N);
+  std::vector<Eigen::Matrix<typename return_type<T_x, T_v, double>::type, R, C>>
+      out(N);
   for (size_t n = 0; n < N; ++n) {
     out[n].resize(D);
     for (size_t d = 0; d < D; ++d) {
