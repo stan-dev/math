0353ccd44505cb91aadbb7d03bcf27fce6329cf8
Edward A. Roualdes <eroualdes@csuchico.edu>
2018-08-15 10:39:11 -0700

    sorry jenkins, Imissed some stuff

--- test/prob/beta_proportion/beta_proportion_test.hpp
+++ test/prob/beta_proportion/beta_proportion_test.hpp
@@ -69,50 +69,27 @@ class AgradDistributionsBetaProportion : public AgradDistributionTest {
   template <typename T_y, typename T_loc, typename T_prec, typename T3,
             typename T4, typename T5>
   typename stan::return_type<T_y, T_loc, T_prec>::type log_prob(
-<<<<<<< HEAD
       const T_y& y, const T_loc& mu, const T_prec& kappa, const T3&,
       const T4&, const T5&) {
     return stan::math::beta_proportion_lpdf(y, mu, kappa);
-=======
-      const T_y& y, const T_loc& p, const T_prec& c, const T3&, const T4&,
-      const T5&) {
-    return stan::math::beta_proportion_lpdf(y, p, c);
->>>>>>> ac54bb82df38cde516377196192eaa1ff3f9f3a2
   }
 
   template <bool propto, typename T_y, typename T_loc, typename T_prec,
             typename T3, typename T4, typename T5>
   typename stan::return_type<T_y, T_loc, T_prec>::type log_prob(
-<<<<<<< HEAD
       const T_y& y, const T_loc& mu, const T_prec& kappa, const T3&,
       const T4&, const T5&) {
     return stan::math::beta_proportion_lpdf<propto>(y, mu, kappa);
-=======
-      const T_y& y, const T_loc& p, const T_prec& c, const T3&, const T4&,
-      const T5&) {
-    return stan::math::beta_proportion_lpdf<propto>(y, p, c);
->>>>>>> ac54bb82df38cde516377196192eaa1ff3f9f3a2
   }
 
   template <typename T_y, typename T_loc, typename T_prec, typename T3,
             typename T4, typename T5>
   typename stan::return_type<T_y, T_loc, T_prec, T3, T4, T5>::type
-<<<<<<< HEAD
   log_prob_function(const T_y& y, const T_loc& mu, const T_prec& kappa,
                     const T3&, const T4&, const T5&) {
     using stan::math::log1m;
     using std::log;
-
     return (mu*kappa - 1.0) * log(y) + ((1.0 - mu) * kappa - 1.0) * log1m(y)
       + lgamma(kappa) - lgamma(mu * kappa) - lgamma((1.0 - mu) * kappa);
-=======
-  log_prob_function(const T_y& y, const T_loc& p, const T_prec& c, const T3&,
-                    const T4&, const T5&) {
-    using stan::math::log1m;
-    using std::log;
-
-    return (p * c - 1.0) * log(y) + ((1.0 - p) * c - 1.0) * log1m(y) + lgamma(c)
-           - lgamma(p * c) - lgamma((1.0 - p) * c);
->>>>>>> ac54bb82df38cde516377196192eaa1ff3f9f3a2
   }
 };
