3bacc3244cc77a7a2a6d3934621cfb52deccc512
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2018-08-15 23:44:45 +0200

    moved compiling to the kernel_cl_base constructor, compiles everything at once

--- stan/math/gpu/kernel_cl.hpp
+++ stan/math/gpu/kernel_cl.hpp
@@ -51,6 +51,36 @@ class kernel_cl_base {
   const char* add_symmetric =
 #include <stan/math/gpu/kernels/add_matrix.cl>  // NOLINT
       ;                                         // NOLINT
+  kernel_cl_base() {
+    std::string kernel_opts = "";
+    std::string kernel_source = std::string(helpers);
+    for (auto kernel_info : kernel_table) {
+      kernel_source += kernel_info.second.raw_code;
+      kernel_info.second.exists = true;
+      for (auto comp_opts : kernel_info.second.opts) {
+        if (strcmp(comp_opts, "") != 0) {
+          kernel_opts += std::string(" -D") + comp_opts + "="
+                         + std::to_string(base_opts[comp_opts]);
+        }
+      }
+    }
+    try {
+      cl::Program::Sources source(
+          1,
+          std::make_pair(kernel_source.c_str(), strlen(kernel_source.c_str())));
+      cl::Program program_ = cl::Program(opencl_context.context(), source);
+      program_.build({opencl_context.device()}, kernel_opts.c_str());
+
+      cl_int err = CL_SUCCESS;
+      // Iterate over all the kernels and create the cl::Kernel objects
+      for (auto kernel_info : kernel_table) {
+        kernels[kernel_info.first] =
+            cl::Kernel(program_, kernel_info.first, &err);
+      }
+    } catch (const cl::Error& e) {
+      check_opencl_error("Kernel Compilation", e);
+    }
+  }
 
  protected:
   const char* helpers =
@@ -58,8 +88,8 @@ class kernel_cl_base {
       ;                                      // NOLINT
   // Holds Default parameter values for each Kernel.
   typedef std::map<const char*, int> map_base_opts;
-  const map_base_opts base_opts
-      = {{"LOWER", static_cast<int>(TriangularViewGPU::Lower)},
+  map_base_opts base_opts =
+        {{"LOWER", static_cast<int>(TriangularViewGPU::Lower)},
          {"UPPER", static_cast<int>(TriangularViewGPU::Upper)},
          {"ENTIRE", static_cast<int>(TriangularViewGPU::Entire)},
          {"UPPER_TO_LOWER", static_cast<int>(TriangularMapGPU::UpperToLower)},
@@ -81,9 +111,7 @@ class kernel_cl_base {
    */
   typedef std::map<const char*, kernel_meta_info> map_table;
   const map_table kernel_table
-      = {{"dummy", {false, {}, "_ void dummy(__global const int* foo) { };"}},
-         {"dummy2", {false, {}, "_ void dummy2(__global const int* foo) { };"}},
-         {"copy", {false, {}, copy_matrix}},
+      = {{"copy", {false, {}, copy_matrix}},
          {"transpose", {false, {}, transpose_matrix}},
          {"zeros", {false, {"LOWER", "UPPER", "ENTIRE"}, zeros_matrix}},
          {"identity", {false, {}, identity_matrix}},
@@ -105,7 +133,6 @@ class kernel_cl_base {
     static kernel_cl_base instance_;
     return instance_;
   }
-  kernel_cl_base() = default;
   kernel_cl_base(kernel_cl_base const&) = delete;
   void operator=(kernel_cl_base const&) = delete;
 };
@@ -114,76 +141,17 @@ class kernel_cl {
  public:
   cl::Kernel compiled_;
   /**
-   * Compiles all the kernels in the specified group. The side effect of this
-   *  method places all compiled kernels for a group inside of <code> kernels
-   *  </code>.
-   *
-   * @param kernel_name The kernel name.
-   *
-   * @throw std::system_error if there are compilation errors
-   * when compiling the specified kernel group's source code
-   * or std::domain_error if the kernel with the specified
-   * name does not exist
-   */
-  inline void compile_group(const char* kernel_name) {
-    if (this->kernel_table().count(kernel_name) == 0) {
-      // throws if the kernel does not exist
-      domain_error("compiling kernels", kernel_name, " kernel does not exist",
-                   "");
-    }
-    kernel_cl_base::kernel_meta_info kernel_info
-        = this->kernel_table()[kernel_name];
-    std::string kernel_opts = "";
-    std::string kernel_source = std::string(get_helpers());
-    kernel_source += kernel_info.raw_code;
-    for (auto comp_opts : kernel_info.opts) {
-      if (strcmp(comp_opts, "") != 0) {
-        kernel_opts += std::string(" -D") + comp_opts + "="
-                       + std::to_string(this->base_options()[comp_opts]);
-      }
-    }
-    try {
-      cl::Program::Sources source(
-          1,
-          std::make_pair(kernel_source.c_str(), strlen(kernel_source.c_str())));
-      cl::Program program_ = cl::Program(opencl_context.context(), source);
-      program_.build({opencl_context.device()}, kernel_opts.c_str());
-
-      cl_int err = CL_SUCCESS;
-      // Iterate over the kernel list and get all the kernels from this group
-      // and mark them as compiled.
-      kernel_cl_base::getInstance().kernels[(kernel_name)]
-          = cl::Kernel(program_, kernel_name, &err);
-      kernel_info.exists = true;
-    } catch (const cl::Error& e) {
-      check_opencl_error("Kernel Compilation", e);
-    }
-  }
-  /**
    * Returns the kernel specified in kernel_name.
-   * If the kernel has not yet been compiled, the kernel group is compiled
-   * first.
-   *
-   * @brief Passing the name of a kernel compiles all kernels in the same group
-   * as the selected kernel.
-   * OpenCL kernels are compiled JIT, instead of compiling each kernel
-   * individually this function will compile all kernels
-   * in a predefined group. Groupings are made such that kernels commonly
-   * called with one another will be compiled at the same time. For example,
-   * An arithmetic group of kernels compiled together could contain the kernels
-   * for <code> add() </code>, <code> subtract() </code>,
-   * and <code> multiply() </code>. This function will only return the kernel
-   * which was called, but when a user asks for a kernel within the group those
-   * kernels will already be compiled.
-   *
-   * @param kernel_name The kernel name.
+   * 
+   * @throw std::domain_error
    */
   explicit kernel_cl(const char* kernel_name) {
-    // Compile the kernel group and return the kernel
-    if (!this->kernel_table()[kernel_name].exists) {
-      this->compile_group(kernel_name);
+    if (kernel_cl_base::getInstance().kernels.count((kernel_name)) == 0) {
+      domain_error("compiling kernels", kernel_name, " kernel does not exist",
+                   "");
+    } else {
+      compiled_ = kernel_cl_base::getInstance().kernels[(kernel_name)];
     }
-    compiled_ = kernel_cl_base::getInstance().kernels[(kernel_name)];
   }
   /**
    * Terminating function for recursively setting arguments in an OpenCL kernel.
@@ -232,12 +200,6 @@ class kernel_cl {
   inline void set_args(const Args&... args) {
     this->recursive_args(this->compiled_, 0, args...);
   }
-  /**
-   * return information on the kernel_source
-   */
-  inline kernel_cl_base::map_table kernel_table() {
-    return kernel_cl_base::getInstance().kernel_table;
-  }
 
   /**
    * return all compiled kernels.
@@ -245,21 +207,6 @@ class kernel_cl {
   inline kernel_cl_base::map kernels() {
     return kernel_cl_base::getInstance().kernels;
   }
-
-  /*
-   * return the base kernel compiler options
-   */
-  inline kernel_cl_base::map_base_opts base_options() {
-    return kernel_cl_base::getInstance().base_opts;
-  }
-
-  /**
-   * Returns helper macros for use in the kernels
-   *  For ease of coding, several helper macros have been placed
-   *   in stan/math/gpu/kernels/helpers.cl. This function retrieves those
-   *   macros so they can be added to the kernel string pre-compilation.
-   */
-  const char* get_helpers() { return kernel_cl_base::getInstance().helpers; }
 };
 }  // namespace math
 }  // namespace stan
