43118a025e523c818b00abf4683f14a03422fb25
Andre Zapico <drezap@umich.edu>
2018-08-16 10:10:36 +0300

    Update add_diag_test.cpp

--- test/unit/math/prim/mat/fun/add_diag_test.cpp
+++ test/unit/math/prim/mat/fun/add_diag_test.cpp
@@ -59,23 +59,3 @@ TEST(MathPrimMat, double_mat_double_rvec_add_diag) {
   }
 }
 
-TEST(MathPrimMat, double_mat_double_rvec_add_diag) {
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mat(2, 3);
-  mat << 1, 1, 1, 1, 1, 1;
-
-  Eigen::Matrix<double, -1, 1> to_add(2);
-  to_add << 0, 1;
-
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> out_mat;
-  EXPECT_NO_THROW(out_mat = stan::math::add_diag(mat, to_add));
-  for (int i = 0; i < 2; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      if (i == j)
-        EXPECT_FLOAT_EQ(1 + to_add[i], out_mat(i, i))
-            << "index: ( " << i << ", " << i << ")";
-      else
-        EXPECT_FLOAT_EQ(1, out_mat(i, j))
-            << "index: ( " << i << ", " << i << ")";
-    }
-  }
-}
