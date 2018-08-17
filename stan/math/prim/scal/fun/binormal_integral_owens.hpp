611636be8dd2f39199b37b7f17c5d8e9c84d507a
Rob Trangucci <robert.trangucci@gmail.com>
2018-08-17 14:08:59 -0400

    Accuracy tested vs. MM acceptable

--- stan/math/prim/scal/fun/binormal_integral_owens.hpp
+++ stan/math/prim/scal/fun/binormal_integral_owens.hpp
@@ -12,8 +12,7 @@
 #include <stan/math/prim/scal/fun/owens_t.hpp>
 #include <stan/math/prim/scal/fun/fmin.hpp>
 #include <stan/math/prim/scal/fun/exp.hpp>
-#include <boost/math/quadrature/gauss.hpp>
-#include <stan/math/prim/scal/fun/sign.hpp>
+#include <boost/math/quadrature/tanh_sinh.hpp>
 #include <iostream>
 
 namespace stan {
@@ -36,7 +35,7 @@ binormal_integral_owens(const T_z1& z1, const T_z2& z2, const T_rho& rho) {
   
   if (z1 > 10 && z2 > 10)
     return 1;
-  if (z1 < -15 || z2 < -15)
+  if (z1 < -37.5 || z2 < -37.5)
     return 0;
   if (rho == 0) 
     return Phi(z1) * Phi(z2);
@@ -44,7 +43,11 @@ binormal_integral_owens(const T_z1& z1, const T_z2& z2, const T_rho& rho) {
     return 0.5 / pi() * exp(-0.5 * z2 * z2) * asin(rho) + Phi(z1) * Phi(z2);
   if (z1 * rho == z2) 
     return 0.5 / pi() * exp(-0.5 * z1 * z1) * asin(rho) + Phi(z1) * Phi(z2);
-  if (fabs(rho) < 0.98 && (z1 > -2.5 || z2 > -2.5)) {
+  if (z2 > 40)
+    return Phi(z1);
+  if (z1 > 40)
+    return Phi(z2);
+  if (fabs(rho) < 0.7 && fabs(z1) < 1.0 && fabs(z2) < 1.0) {
     T_rho denom = sqrt((1 + rho) * (1 - rho));
     T_return a1 = (z2 / z1 - rho) / denom;
     T_return a2 = (z1 / z2 - rho) / denom;
@@ -56,16 +59,17 @@ binormal_integral_owens(const T_z1& z1, const T_z2& z2, const T_rho& rho) {
     int s = rho > 0 ? 1 : -1;
     const T_z1 h = -z1;
     const T_z2 k = -z2;
-    T_rho sqrt_one_minus_r_sq = sqrt((1 + rho) * (1 - rho));
 
-    auto f = [&h, &k, s](const T_return& x) {
-      T_return sqrt_one_minus_r_sq = sqrt((1 - x) * (1 + x));
-      return 1 / sqrt_one_minus_r_sq * exp(0.5 * (-h * h - k * k + h * k * s * 2 * sqrt_one_minus_r_sq)/(x * x));
+    auto f = [&h, &k](const T_return& x) {
+      return exp(0.5 * (-h * h - k * k + h * k * 2 * sin(x))/(cos(x) * cos(x)));
     };
-    T_return accum = boost::math::quadrature::gauss<T_return, 25>::integrate(f, 0, sqrt_one_minus_r_sq);
+    boost::math::quadrature::tanh_sinh<T_return> integrator;
+    T_return accum = s == 1 ? integrator.integrate(f, 0, asin(rho))
+                            : integrator.integrate(f, static_cast<T_return>(asin(rho)),
+                                                   static_cast<T_return>(0));
 
-    accum *= -s * 0.5 / pi();
-    accum += s == 1 ? Phi(-fmax(h,k)) : fmax(0, Phi(-h) - Phi(k)); 
+    accum *= s * 0.5 / pi();
+    accum += Phi(z1) * Phi(z2); 
     return accum;
   }
   if (rho == 1) 
