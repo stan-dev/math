3e2bb3a8e9e11083e35b380fef4a4e2238f14a7f
Sean Talts <sean.talts@gmail.com>
2018-08-27 12:12:38 -0400

    value_of is probably kinder to the autodiff stack.

--- stan/math/prim/mat/err/check_finite.hpp
+++ stan/math/prim/mat/err/check_finite.hpp
@@ -3,6 +3,7 @@
 
 #include <stan/math/prim/scal/err/domain_error.hpp>
 #include <stan/math/prim/scal/err/check_finite.hpp>
+#include <stan/math/prim/mat/fun/value_of.hpp>
 #include <Eigen/Dense>
 #include <boost/math/special_functions/fpclassify.hpp>
 
@@ -13,7 +14,7 @@ template <typename T, int R, int C>
 struct finite<Eigen::Matrix<T, R, C>, true> {
   static void check(const char* function, const char* name,
                     const Eigen::Matrix<T, R, C>& y) {
-    if (!y.allFinite()) {
+    if (!value_of(y).allFinite()) {
       for (int n = 0; n < y.size(); ++n) {
         if (!(boost::math::isfinite)(y(n)))
           domain_error_vec(function, name, y, n, "is ",
