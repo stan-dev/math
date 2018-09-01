e76a076e4cc80a9374854428c41cecb8280727f5
Sean Talts <sean.talts@gmail.com>
2018-09-01 07:09:15 -0400

    Add Matrix flag for GPU transfers.

--- stan/math/prim/mat/matrix_addons.h
+++ stan/math/prim/mat/matrix_addons.h
@@ -5,3 +5,7 @@ mutable bool is_positive_ = false;
 mutable bool is_non_negative_ = false;
 mutable bool is_corr_ = false;
 mutable bool is_cov_ = false;
+
+#ifdef STAN_GPU
+mutable cl::Buffer* opencl_buffer_;
+#endif
