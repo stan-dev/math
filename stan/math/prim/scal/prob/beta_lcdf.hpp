a9a2aa7c22a07262dfb27afc2faea8b7a2a80752
Edward A. Roualdes <eroualdes@csuchico.edu>
2018-09-06 09:18:00 -0700

    doc changes

--- stan/math/prim/scal/prob/beta_lcdf.hpp
+++ stan/math/prim/scal/prob/beta_lcdf.hpp
@@ -35,16 +35,16 @@ namespace math {
  * Returns the beta log cumulative distribution function for the given
  * probability, success, and failure parameters.  Any arguments other
  * than scalars must be containers of the same size.  With non-scalar
- * arguments, the return is the sum of the log ccdfs with scalars
+ * arguments, the return is the sum of the log cdfs with scalars
  * broadcast as necessary.
  *
- * @tparam T_y type of probability parameter
+ * @tparam T_y type of y
  * @tparam T_scale_succ type of success parameter
  * @tparam T_scale_fail type of failure parameter
- * @param y probability parameter
- * @param alpha success parameter
- * @param beta failure parameter
- * @return log probability or log sum of probabilities
+ * @param y (Sequence of) scalar(s) between zero and one
+ * @param alpha (Sequence of) success parameter(s)
+ * @param beta (Sequence of) failure parameter(s)
+ * @return log probability or sum of log of proabilities
  * @throw std::domain_error if alpha or beta is negative
  * @throw std::domain_error if y is not a valid probability
  * @throw std::invalid_argument if container sizes mismatch
