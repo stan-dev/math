9e22ffa7e63c673466cd5990d294e6a239380687
Stan Jenkins <mc.stanislaw@gmail.com>
2018-09-09 10:00:46 -0400

    [Jenkins] auto-formatting by clang-format version 5.0.2-svn328729-1~exp1~20180509124008.99 (branches/release_50)

--- stan/math/gpu/multiply.hpp
+++ stan/math/gpu/multiply.hpp
@@ -71,9 +71,12 @@ inline matrix_gpu multiply(const matrix_gpu& A, const matrix_gpu& B) {
   matrix_gpu tempPad(Mpad, Npad);
   matrix_gpu Apad(Mpad, Kpad);
   matrix_gpu Bpad(Kpad, Npad);
-  opencl_kernels::zeros(cl::NDRange(Mpad, Kpad), Apad.buffer(), Mpad, Kpad, TriangularViewGPU::Entire);
-  opencl_kernels::zeros(cl::NDRange(Kpad, Npad), Bpad.buffer(), Kpad, Npad, TriangularViewGPU::Entire);
-  opencl_kernels::zeros(cl::NDRange(Mpad, Npad), tempPad.buffer(), Mpad, Npad, TriangularViewGPU::Entire);
+  opencl_kernels::zeros(cl::NDRange(Mpad, Kpad), Apad.buffer(), Mpad, Kpad,
+                        TriangularViewGPU::Entire);
+  opencl_kernels::zeros(cl::NDRange(Kpad, Npad), Bpad.buffer(), Kpad, Npad,
+                        TriangularViewGPU::Entire);
+  opencl_kernels::zeros(cl::NDRange(Mpad, Npad), tempPad.buffer(), Mpad, Npad,
+                        TriangularViewGPU::Entire);
   Apad.sub_block(A, 0, 0, 0, 0, A.rows(), A.cols());
   Bpad.sub_block(B, 0, 0, 0, 0, B.rows(), B.cols());
   int wpt = opencl_kernels::matrix_multiply.make_functor.get_opts().at(
