cf1a9e0a6893107008cd66459edebd72c141692a
Rok Češnovar <rok.cesnova@fri.uni-lj.si>
2018-10-13 23:27:10 +0200

    a naive fix for the windows seg fault issue

--- stan/math/prim/mat/fun/matrix_exp_pade.hpp
+++ stan/math/prim/mat/fun/matrix_exp_pade.hpp
@@ -18,14 +18,18 @@ namespace math {
  */
 template <typename MatrixType>
 MatrixType matrix_exp_pade(const MatrixType& arg) {
-  MatrixType U, V;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> U, V;
   int squarings;
-  Eigen::matrix_exp_computeUV<MatrixType>::run(arg, U, V, squarings, arg(0, 0));
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> tmp(arg.rows(), arg.cols());
+  for (int i = 0; i < arg.size(); ++i) {
+      tmp(i) = value_of(arg(i));
+    }
+  Eigen::matrix_exp_computeUV<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>::run(tmp, U, V, squarings, tmp(0, 0));
   // Pade approximant is
   // (U+V) / (-U+V)
-  MatrixType numer = U + V;
-  MatrixType denom = -U + V;
-  MatrixType pade_approximation = denom.partialPivLu().solve(numer);
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> numer = U + V;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> denom = -U + V;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> pade_approximation = denom.partialPivLu().solve(numer);
   for (int i = 0; i < squarings; ++i)
     pade_approximation *= pade_approximation;  // undo scaling by
                                                // repeated squaring
