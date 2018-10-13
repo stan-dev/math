fd6079d1339d44a033f22b31af64bc49ca4493e1
Stan Jenkins <mc.stanislaw@gmail.com>
2018-10-13 21:27:41 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- stan/math/prim/mat/fun/matrix_exp_pade.hpp
+++ stan/math/prim/mat/fun/matrix_exp_pade.hpp
@@ -20,16 +20,21 @@ template <typename MatrixType>
 MatrixType matrix_exp_pade(const MatrixType& arg) {
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> U, V;
   int squarings;
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> tmp(arg.rows(), arg.cols());
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> tmp(arg.rows(),
+                                                            arg.cols());
   for (int i = 0; i < arg.size(); ++i) {
-      tmp(i) = value_of(arg(i));
-    }
-  Eigen::matrix_exp_computeUV<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>::run(tmp, U, V, squarings, tmp(0, 0));
+    tmp(i) = value_of(arg(i));
+  }
+  Eigen::matrix_exp_computeUV<
+      Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>::run(tmp, U, V,
+                                                                  squarings,
+                                                                  tmp(0, 0));
   // Pade approximant is
   // (U+V) / (-U+V)
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> numer = U + V;
   Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> denom = -U + V;
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> pade_approximation = denom.partialPivLu().solve(numer);
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> pade_approximation
+      = denom.partialPivLu().solve(numer);
   for (int i = 0; i < squarings; ++i)
     pade_approximation *= pade_approximation;  // undo scaling by
                                                // repeated squaring
