8ab5bd385ba45622e2002ce6c56747fa33467b2f
andre <andre@cluster-local.node.drelen>
2018-10-15 19:57:59 -0400

    enable if it again

--- stan/math/prim/mat/fun/gp_exp_quad_cov.hpp
+++ stan/math/prim/mat/fun/gp_exp_quad_cov.hpp
@@ -34,9 +34,9 @@ namespace math {
  *   x is nan or infinite
  */
 template <typename T_x, typename T_sigma, typename T_l>
-inline
-    typename Eigen::Matrix<typename stan::return_type<T_x, T_sigma, T_l>::type,
-                           Eigen::Dynamic, Eigen::Dynamic>
+inline typename boost::enable_if_c<
+  is_constant<typename return_type<T_l>::type>::value,
+    Eigen::Matrix<double, -1, -1>>::type
     gp_exp_quad_cov(const std::vector<T_x> &x, const T_sigma &sigma,
                     const T_l &length_scale) {
   using std::exp;
