6c602703923930ef4029f28e6b30cc71c2340391
rok <rok.cesnovar@gmail.com>
2018-10-16 17:00:06 +0200

    gtest windows fix for expect_death, reverting back matrix_exp_pade changes

--- stan/math/prim/mat/fun/matrix_exp_pade.hpp
+++ stan/math/prim/mat/fun/matrix_exp_pade.hpp
@@ -18,22 +18,21 @@ namespace math {
  */
 template <typename MatrixType>
 MatrixType matrix_exp_pade(const MatrixType& arg) {
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> U, V;
+  MatrixType U, V;
   int squarings;
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> tmp(arg.rows(),
+  MatrixType tmp(arg.rows(),
                                                             arg.cols());
   for (int i = 0; i < arg.size(); ++i) {
     tmp(i) = value_of(arg(i));
   }
-  Eigen::matrix_exp_computeUV<
-      Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>::run(tmp, U, V,
+  Eigen::matrix_exp_computeUV<MatrixType>::run(tmp, U, V,
                                                                   squarings,
                                                                   tmp(0, 0));
   // Pade approximant is
   // (U+V) / (-U+V)
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> numer = U + V;
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> denom = -U + V;
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> pade_approximation
+  MatrixType numer = U + V;
+  MatrixType denom = -U + V;
+  MatrixType pade_approximation
       = denom.partialPivLu().solve(numer);
   for (int i = 0; i < squarings; ++i)
     pade_approximation *= pade_approximation;  // undo scaling by
