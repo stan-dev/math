d8a00d01ed3f1387d5edcfe56b7fdcb5c44a9250
Stan Jenkins <mc.stanislaw@gmail.com>
2018-10-16 00:03:25 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- stan/math/prim/mat/fun/gp_exp_quad_cov.hpp
+++ stan/math/prim/mat/fun/gp_exp_quad_cov.hpp
@@ -36,7 +36,7 @@ namespace math {
  */
 template <typename T_x, typename T_sigma, typename T_l>
 inline typename boost::enable_if_c<
-  is_constant<typename return_type<T_l>::type>::value,
+    is_constant<typename return_type<T_l>::type>::value,
     Eigen::Matrix<double, -1, -1>>::type
 gp_exp_quad_cov(const std::vector<T_x> &x, const T_sigma &sigma,
                 const T_l &length_scale) {
@@ -60,8 +60,8 @@ gp_exp_quad_cov(const std::vector<T_x> &x, const T_sigma &sigma,
   for (size_t j = 0; j < (x_size - 1); ++j) {
     cov(j, j) = sigma_sq;
     for (size_t i = j + 1; i < x_size; ++i) {
-      cov(i, j) =
-          sigma_sq * exp(squared_distance(x[i], x[j]) * neg_half_inv_l_sq);
+      cov(i, j)
+          = sigma_sq * exp(squared_distance(x[i], x[j]) * neg_half_inv_l_sq);
       cov(j, i) = cov(i, j);
     }
   }
@@ -168,8 +168,8 @@ gp_exp_quad_cov(const std::vector<T_x1> &x1, const std::vector<T_x2> &x2,
 
   for (size_t i = 0; i < x1.size(); ++i) {
     for (size_t j = 0; j < x2.size(); ++j) {
-      cov(i, j) =
-          sigma_sq * exp(squared_distance(x1[i], x2[j]) * neg_half_inv_l_sq);
+      cov(i, j)
+          = sigma_sq * exp(squared_distance(x1[i], x2[j]) * neg_half_inv_l_sq);
     }
   }
   return cov;
@@ -238,6 +238,6 @@ gp_exp_quad_cov(const std::vector<Eigen::Matrix<T_x1, Eigen::Dynamic, 1>> &x1,
   }
   return cov;
 }
-} // namespace math
-} // namespace stan
+}  // namespace math
+}  // namespace stan
 #endif
