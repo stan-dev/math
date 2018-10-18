9ba23e2d8b238682cccb41477dfea1074c67936c
Charles Margossian <charlesm@metrumrg.com>
2018-10-17 21:31:12 -0400

    Initial commit for dense newton solver.

---
+++ stan/math/laplace/lgp_dense_newton_solver.hpp
@@ -0,0 +1,182 @@
+#ifndef STAN_MATH_LAPLACE_LGP_NEWTON_SOLVER_HPP
+#define STAN_MATH_LAPLACE_LGP_NEWTON_SOLVER_HPP
+
+#include <stan/math/laplace/lgp_conditional_system.hpp>
+
+
+namespace stan {
+namespace math {
+
+  /**
+   * The vari class for the latent gaussian poisson (lgp)
+   * Newton solver.
+   */
+  template <typename T>
+  struct lgp_newton_solver_vari : public vari {
+    /** global parameter */
+    vari** phi_;
+    /** size of solution */
+    int theta_size_;
+    /** vector of solution */
+    vari** theta_;
+    /** Jacobian of the solution with respect to the global parameter */
+    Eigen::VectorXd J_;
+    // double* J_;
+
+    lgp_newton_solver_vari(const T& phi,
+                           const lgp_conditional_system<double>& system,
+                           const Eigen::VectorXd& theta_dbl)
+      : vari(theta_dbl(0)),
+        phi_(ChainableStack::instance_.memalloc_.alloc_array<vari*>(1)),
+        theta_size_(theta_dbl.size()),
+        theta_(ChainableStack::instance_.memalloc_.alloc_array<vari*>(
+          theta_size_)) {
+      using Eigen::Map;
+      using Eigen::VectorXd;
+
+      *phi_ = phi.vi_;  // CHECK - should be phi_ = phi.vi_?
+
+      theta_[0] = this;
+      for (int i = 0; i < theta_size_; i++)
+        theta_[i] = new vari(theta_dbl(i), false);
+
+      // Compute the Jacobian and store in array, using
+      // the operator in the lgp_conditional_system structure.
+      J_ = system.solver_gradient(theta_dbl);
+
+      // CHECK - more memory efficient?
+      // std::cout << system.solver_gradient(theta_dbl) << std::endl;
+      // Map<VectorXd>(&J_[0], theta_size_) = system.solver_gradient(theta_dbl);
+      // std::cout << "marker d" << std::endl;
+    }
+
+    void chain() {
+      for (int i = 0; i < theta_size_; i++)
+        phi_[0]->adj_ += theta_[i]->adj_ * J_[i];
+    }
+  };
+  
+  /**
+   * Newton solver for lgp model.
+   * In this instantiation, the global parameter phi has type double.
+   * The initial guess can be passed as a parameter or fixed data.
+   */
+  template<typename T>  // template for variables
+  Eigen::Matrix<T, Eigen::Dynamic, 1> lgp_newton_solver(
+    const Eigen::Matrix<T, Eigen::Dynamic, 1>& theta_0,  // initial guess
+    const lgp_conditional_system<double>& system,
+    double tol = 1e-3,
+    long int max_num_steps = 100,
+    bool line_search = false) {  // NOLINT(runtime/int)
+
+    Eigen::VectorXd theta_dbl = value_of(theta_0);
+    Eigen::MatrixXd gradient; 
+    Eigen::MatrixXd direction;
+
+    for (int i = 0; i <= max_num_steps; i++) {
+
+      // check if the max number of steps has been reached
+      if (i == max_num_steps) {
+        std::ostringstream message;
+        message << "lgp_newton_solver: max number of iterations:"
+                << max_num_steps << " exceeded.";
+        throw boost::math::evaluation_error(message.str());
+      }
+
+      gradient = system.cond_gradient(theta_dbl);
+      direction = - elt_divide(gradient, system.cond_hessian(theta_dbl));
+
+      if (line_search == false) {
+        theta_dbl += direction;
+      } else {
+        // do line search using Armijo's method (and tuning parameters from
+        // his paper).
+        double alpha = 1;  // max step size
+        double c = 0.5; 
+        double tau = 0.5;
+        double m = multiply(transpose(direction), gradient)(0);
+        Eigen::VectorXd theta_candidate = theta_dbl + alpha * direction;
+
+        while (system.log_density(theta_candidate) 
+                 > system.log_density(theta_dbl) + c * m) {
+          alpha = tau * alpha;
+          theta_candidate = theta_dbl + alpha * direction;
+        }
+
+        theta_dbl = theta_candidate;
+      }
+
+      // Should really be an mdivide. However, the hessian is diagonal and 
+      // stored as a vector, so need to use elt_divide.
+      // theta_dbl -= mdivide_left(gradient, system.cond_hessian(theta_dbl));
+
+      // Check solution is a root of the gradient
+      if (gradient.norm() <= tol) break;
+    }
+
+    return theta_dbl;
+  }
+
+  /**
+   * lgp Newton solver.
+   * In this instantiation, phi is of type var.
+   * The initial guess can be passed as a parameter or fixed data.
+   */
+  template <typename T1, typename T2>
+  Eigen::Matrix<T2, Eigen::Dynamic, 1> lgp_newton_solver(
+    const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta_0,  // initial guess
+    const lgp_conditional_system<T2>& system,
+    double tol = 1e-6,
+    long int max_num_steps = 100,  // NOLINT(runtime/int)
+    bool line_search = false) {
+
+    lgp_conditional_system<double> 
+      system_dbl(value_of(system.get_phi()),
+                 system.get_n_samples(), 
+                 system.get_sums());
+
+    Eigen::VectorXd theta_dbl 
+      = lgp_newton_solver(value_of(theta_0), system_dbl, tol, max_num_steps,
+                          line_search);
+
+    // construct vari
+    lgp_newton_solver_vari<T2>* vi0
+      = new lgp_newton_solver_vari<T2>(system.get_phi(), system_dbl, theta_dbl);
+
+    Eigen::Matrix<var, Eigen::Dynamic, 1> theta(theta_dbl.size());
+    theta(0) = var(vi0->theta_[0]);
+    for (int i = 1; i < theta_dbl.size(); i++)
+      theta(i) = var(vi0->theta_[i]);
+
+    return theta;
+  }
+
+  /**
+   * Wrapper of the lgp solver for use in the Stan language. This is
+   * because we cannot pass an object of type lgp_conditional_system.
+   * Note the wrapper automatically handles cases where phi is double
+   * or var.
+   */
+  template <typename T1, typename T2>
+  Eigen::Matrix<T2, Eigen::Dynamic, 1> lgp_newton_solver(
+    const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta_0,  // initial guess
+    const T2& phi,
+    const std::vector<int>& n_samples,
+    const std::vector<int>& sums,
+    double tol = 1e-6,
+    long int max_num_steps = 100,  // NOLINT(runtime/int)
+    int is_line_search = 0) {
+
+    return lgp_newton_solver(theta_0,
+                             lgp_conditional_system<T2>(phi, 
+                                                        to_vector(n_samples), 
+                                                        to_vector(sums)),
+                              tol,
+                              max_num_steps,
+                              is_line_search);
+  }
+
+}  // namespace math
+}  // namespace stan
+
+#endif
---
+++ stan/math/laplace/lgp_dense_system.hpp
@@ -0,0 +1,157 @@
+#ifndef STAN_MATH_LAPLACE_LGP_DENSE_SYSTEM_HPP
+#define STAN_MATH_LAPLACE_LGP_DENSE_SYSTEM_HPP
+
+#include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/rev/mat.hpp>
+// #include <stan/math/prim/scal/meta/return_type.hpp>
+// #include <stan/math/prim/mat/fun/exp.hpp>
+// #include <stan/math/rev/mat/fun/multiply.hpp>
+// #include <stan/math/rev/mat/fun/mdivide_left.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+// FIX ME - include specific files instead of rev/mat.hpp to
+// improve compilation speed of unit tests.
+
+namespace stan {
+namespace math {
+
+  // EXPERIMENT.
+  // Functors for finding the mode of the conditional density
+  // when doing a Poisson model with a latent gaussian
+  // parameter. See Dan's experiment.
+
+  /**
+   * A structure for the parameters and data in a latent
+   * Gaussian Poisson (lgp) model. Returns the gradient of 
+   * the density of the local parameter (theta) conditioned on 
+   * the observed data (y) and the global parameter (phi), and
+   * also returns the conditional hessian.
+   *
+   * Note theta is not a member of the structure. This is because
+   * in one HMC iteration, theta is computed using a Newton 
+   * solver (i.e. unlike the other parameters, it is not fixed
+   * during that iteration).
+   */
+  template<typename T0>
+  struct lgp_dense_system {
+    Eigen::Matrix<T0, Eigen::Dynamic, 1> theta_;  // local parameter
+    Eigen::VectorXd n_samples_;  // number of samples for local parameter
+    Eigen::VectorXd sums_;  // sums of observation for local parameter
+    Eigen::MatrixXd Sigma_;  // covariance matrix
+
+    /**
+     * An operator which constructs the covariance matrix,
+     * based on the global paramter, phi.
+     * Note the covariance matrix is assumed to have a specific structure,
+     * namely same variance for all terms, and same correlation.
+     */
+    template <typename T>
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
+      covariance (Eigen::Matrix<T0, Eigen::Dynamic, 1> phi, int M) {
+        T0 sigma = phi[0];
+        T0 rho = phi[1];
+
+        Eigen::Matrix<T0, Eigen::Dynamic, Eigen::Dynamic> Sigma;
+        for (int i = 0; i < M; i++) {
+          for (int j = 0; j < (i - 1); j++) {
+            Sigma[i, j] = rho * sigma;
+            Sigma[j, i] = rho * sigma;
+          }
+          Sigma[i, i] = sigma;
+        }
+      return Sigma;
+    }
+
+    // Constructors
+    lgp_conditional_system() {}
+
+    lgp_conditional_system(const Eigen::Matrix<T0, Eigen::Dynamic, 1>& phi,
+                           const Eigen::VectorXd n_samples,
+                           const Eigen::VectorXd& sums)
+      : phi_(phi), n_samples_(n_samples), sums_(sums) {
+      int M = n_samples.size();
+      Sigma_ = Sigma(phi, M);
+    }
+
+    /**
+     * Functions which retun class members
+     */
+    Eigen::Matrix<T0, Eigen::Dynamic, 1> get_phi() const { return phi_; }
+    Eigen::VectorXd get_n_samples() const { return n_samples_; }
+    Eigen::VectorXd get_sums() const { return sums_; }
+    Eigen::MatrixXd get_Sigma() const { return Sigma_; }
+
+    /**
+     * An operator that returns the conditional density, up to a
+     * constant term. The returned object works as an
+     * objective function we can optimize to find the mode. 
+     */
+    T0 log_density (const Eigen::VectorXd theta) const {
+      Eigen::VectorXd poisson_term = elt_multiply(sums_, theta) - exp(theta);
+      return sum(poisson_term) - theta.transpose() * m_divide_left(Sigma_, theta);
+    }
+
+    /**
+     * An operator that returns the gradient of the conditional density.
+     */
+    template <typename T1>
+    Eigen::Matrix<typename stan::return_type<T0, T1>::type, 
+                  Eigen::Dynamic, Eigen::Dynamic>
+    cond_gradient(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta) const {
+      return sums_ - elt_multiply(n_samples_, exp(theta)) - m_divide_left(Sigma_, theta);
+    }
+
+    /**
+     * An operator that returns the hessian of the conditional density.
+     * Required for Newton solver.
+     */
+    template <typename T1>
+    Eigen::Matrix<typename stan::return_type<T0, T1>::type, 
+                         Eigen::Dynamic, Eigen::Dynamic>
+    cond_hessian(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta) const {
+      return - add(elt_multiply(n_samples, exp(theta)), inverse(Sigma_));
+    }
+    
+    /**
+     * A functor on which the Jacobian function can be called,
+     * to compute the derivative of the objective function with
+     * respect to phi.
+     * NOTE: because we only want the derivatives with respect to
+     * phi, we only compute the (second) term, which depends on phi.
+     */
+    struct deriv_objective {
+      template <typename T>
+      inline Eigen::Matirx<T, Eigen::Dynamic, 1>
+      operator ()(const Eigen::Matrix<T, Eigen::Dynamic, 1>& phi) const {
+        using Eigen::VectorXd;
+        VectorXd theta_dbl = value_of(theta_);
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>
+          Sigma = covariance(phi);
+
+        return - m_divide_left(Sigma_, theta);
+      }
+    };
+
+    /**
+     * An operator that returns the Jacobian of theta with respect
+     * to phi. This is done using the implicit function theorem.
+     * The calculations are greatly simplified by the fact phi
+     * is one dimensional and the hessian is a diagonal.
+     */
+    template<typename T1>
+    Eigen::Matrix<typename stan::return_type<T0, T1>::type,
+                  Eigen::Dynamic, 1>
+    solver_gradient(const Eigen::Matrix<T1, Eigen::Dynamic, 1>& theta) const {
+      Eigen::VectorXd dummy;
+      Eigen::MatrixXd phi_sensitivities;
+      Jacobian(deriv_objective(), phi, dummy, phi_sensitivities);
+
+      return - m_divide_left(cond_hessian(theta), phi_sensitivities);
+    }
+  };
+
+}  // namespace math
+}  // namespace stan
+
+#endif
