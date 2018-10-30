27beacc4e8ba7bdf83f85370178b94044795c3dc
Steve Bronder <stevo15025@gmail.com>
2018-10-30 00:33:24 -0400

    Adds to docs, cleans up some code, use auto and const where possible

--- stan/math/gpu/cholesky_decompose.hpp
+++ stan/math/gpu/cholesky_decompose.hpp
@@ -19,89 +19,104 @@ namespace math {
  * The return value \f$L\f$ will be a lower-traingular matrix such that the
  * original matrix \f$A\f$ is given by
  * <p>\f$A = L \times L^T\f$.
- * The Cholesky decomposition is computed on the GPU. The
- * input matrix is transfered to the GPU and the resulting
- * lower-triangular matrix is then copied from the GPU.
+ * The Cholesky decomposition is computed on the GPU. This algorithm is recursive,
+ * where The parameters <code>block</code>, <code>divider</code>, and
+ *  <code>min_block</code> act as tuning parameters for the recursive step of
+ *  the GPU based Cholesky decompostion. The matrix is subset by the
+ *  <code>block</code> size, and if the <code>block</code> size is less than
+ * <code>min_block</code> then the cholesky decomposition on the GPU is computed
+ * using that submatrix. If <code>block</code> is greater than
+ * <code>block_size</code> then <code>cholesky_decompose</code> is run again
+ * with <code>block</code> equal to <code>block/divider</code>. Once the
+ * Cholesky Decomposition is computed, the full matrix cholesky is created
+ * by propogating the cholesky forward as given in the reference report below.
  *
  * For a full guide to how this works
  * see the Cholesy decompostion chapter in the  reference report
  * <a href="https://github.com/SteveBronder/stancon2018/blob/master/report.pdf">
- * 
+ *
  * @param A Symmetric matrix on the GPU.
- * @param block size of the block for each cholesky decomposition step
+ * @param block Size of the block used to compute the cholesky decomposition.
+ * @param divider Proportion to divide the submatrix by at each recursive step.
+ * @param min_block The amount that block is checked against to decide
+ *  whether to continue the recursion or to perform the cholesky.
  * @return Square root of matrix on the GPU.
  * @throw std::domain_error if m is not
  *  positive definite (if m has more than 0 elements)
  */
-inline matrix_gpu cholesky_decompose(matrix_gpu& A, int block = 100, int divider = 2, int min_block = 100) {
-  int offset = 0;
-    
+inline auto cholesky_decompose(matrix_gpu  &A,
+   const int block = 100, const int divider = 2, const int min_block = 100) {
+  auto offset = 0;
+  // NOTE: The code in this section follows the naming conventions
+  // in the report linked in the docs.
   matrix_gpu A_11(block, block);
   matrix_gpu L_11(block, block);
   // Repeats the blocked cholesky decomposition until the size of the remaining
   // submatrix is smaller or equal to the block size
   while ((offset + block) < (A.rows())) {
-    matrix_gpu A_21(A.rows() - offset - block, block);
-    matrix_gpu A_22(A.rows() - offset - block, A.rows() - offset - block);
+    auto block_subset = A.rows() - offset - block;
+    matrix_gpu A_21(block_subset, block);
+    matrix_gpu A_22(block_subset, block_subset);
     // Copies the A_11 submatrix from the input
     A_11.sub_block(A, offset, offset, 0, 0, block, block);
     // Calls the blocked cholesky for the submatrix A_11
     // or calls the kernel  directly if the size of the block is small enough
-    if (block <= min_block || divider<=1) {
+    if (block <= min_block || divider <= 1) {
       L_11.zeros();
       try {
-        opencl_kernels::cholesky_decompose(cl::NDRange(A_11.rows()), cl::NDRange(A_11.rows()),
-                              A_11.buffer(), L_11.buffer(), A_11.rows());
+        opencl_kernels::cholesky_decompose(
+            cl::NDRange(A_11.rows()), cl::NDRange(A_11.rows()), A_11.buffer(),
+            L_11.buffer(), A_11.rows());
       } catch (const cl::Error& e) {
         check_opencl_error("cholesky_decompose", e);
       }
-    }else{
-      // Recursive call
-      L_11 = cholesky_decompose(A_11, block/divider);
+    } else {
+      L_11 = cholesky_decompose(A_11, block / divider, divider, min_block);
     }
     // Copies the cholesky factor of A_11 back to the input matrix
     A.sub_block(L_11, 0, 0, offset, offset, block, block);
-    // Copise the A_21 submatrix from the input matrix,
+    // Copies the A_21 submatrix from the input matrix,
+    auto block_offset = offset + block;
+    A_21.sub_block(A, block_offset, offset, 0, 0, block_subset, block);
     // computes A_21*((L_11^-1)^T)
     // and copies the resulting submatrix to the input matrix
-    A_21.sub_block(A, (offset + block), offset, 0, 0, (A.rows() - offset - block),
-                block);    
     matrix_gpu A_11_inverse = lower_triangular_inverse(L_11);
     A_11_inverse = transpose(A_11_inverse);
-    matrix_gpu L_21 =  multiply(A_21, A_11_inverse);  
-    A.sub_block(L_21, 0, 0, (offset + block), offset, (A.rows() - offset - block),
-                block);
+    // TODO(Steve): Replace with mult operator when that PR goes through 
+    matrix_gpu L_21 = multiply(A_21, A_11_inverse);
+    A.sub_block(L_21, 0, 0, block_offset, offset, block_subset, block);
     // Copies the A_22 submatrix from the input matrix,
+    A_22.sub_block(A, block_offset, block_offset, 0, 0, block_subset,
+      block_subset);
     // computes A_22 - L_21*(L_21^T)
     // and copies the resulting submatrix back to the input matrix
-    A_22.sub_block(A, (offset + block), (offset + block), 0, 0,
-                       (A.rows() - offset - block),
-                       (A.rows() - offset - block));
     matrix_gpu temp = multiply_transpose(L_21);
+    // TODO(Steve): Replace with subtraction operator when that PR goes through
     matrix_gpu L_22 = subtract(A_22, temp);
-    A.sub_block(L_22, 0, 0, (offset + block), (offset + block),
-                (A.rows() - offset - block), (A.rows() - offset - block));
+    A.sub_block(L_22, 0, 0, block_offset, block_offset, block_subset,
+       block_subset);
     offset += block;
   }
   // Computes the Cholesky factor for the remaining part of the matrix
-  int remaining_rows = A.rows() - offset;
+  const auto remaining_rows = A.rows() - offset;
   if (remaining_rows > 0) {
     matrix_gpu A_11(remaining_rows, remaining_rows);
     matrix_gpu L_11(remaining_rows, remaining_rows);
     A_11.sub_block(A, offset, offset, 0, 0, remaining_rows, remaining_rows);
     // Calls the blocked cholesky for the submatrix A_11
     // or calls the kernel  directly if the size of the block is small enough
-    if (block <= min_block || divider<=1) {
+    if (block <= min_block || divider <= 1) {
       L_11.zeros();
       try {
-        opencl_kernels::cholesky_decompose(cl::NDRange(A_11.rows()), cl::NDRange(A_11.rows()),
-                              A_11.buffer(), L_11.buffer(), A_11.rows());
+        opencl_kernels::cholesky_decompose(
+            cl::NDRange(A_11.rows()), cl::NDRange(A_11.rows()), A_11.buffer(),
+            L_11.buffer(), A_11.rows());
       } catch (const cl::Error& e) {
         check_opencl_error("cholesky_decompose", e);
       }
-    }else{
-      L_11 = cholesky_decompose(A_11, block/divider);
-    }    
+    } else {
+      L_11 = cholesky_decompose(A_11, block / divider);
+    }
     A.sub_block(L_11, 0, 0, offset, offset, remaining_rows, remaining_rows);
   }
   check_nan("cholesky_decompose_gpu", "Matrix m", A);
--- stan/math/gpu/kernels/cholesky_decompose.hpp
+++ stan/math/gpu/kernels/cholesky_decompose.hpp
@@ -13,56 +13,57 @@ const char *cholesky_decompose_kernel_code = STRINGIFY(
     /**
      * Calculates the Cholesky Decomposition of a matrix on a GPU
      *
-     * This kernel is run with threads organized in one dimension and 
-     * in a single thread block. The kernel is best suited for 
+     * This kernel is run with threads organized in one dimension and
+     * in a single thread block. The kernel is best suited for
      * small input matrices as it only utilizes a single streaming
      * multiprocessor. The kernels is used as a part of a blocked
      * cholesky decompose.
-     * 
+     *
      * @param[in] A The input matrix
      * @param[in, out] B The result of cholesky decompositon of A.
-     *  It must be set to zeros before execution of this kernel.     * 
+     *  It must be set to zeros before execution of this kernel.     *
      * @param rows The number of rows for A and B.
      * @note Code is a <code>const char*</code> held in
      * <code>cholesky_decompose_kernel_code.</code>
      *  Used in math/gpu/cholesky_decompose.hpp.
      *  This kernel uses the helper macros available in helpers.cl.
-     * 
+     *
      */
     __kernel void cholesky_decompose(__global double *A, __global double *B,
-                                 int rows) {
-       int index = get_local_id(0);
-      // The following code is the sequential version of the non-inplace cholesky
-      // decomposition. Only the innermost loops are parallelized.
-      // The rows are processed sequentially. This loop process all the rows:
+                                     int rows) {
+      int local_index = get_local_id(0);
+      // The following code is the sequential version of the non-inplace
+      // cholesky decomposition. Only the innermost loops are parallelized. The
+      // rows are processed sequentially. This loop process all the rows:
       for (int j = 0; j < rows; j++) {
-        //one thread calculates the 		
-        if (index == 0) {
-            double sum = 0;
-            for (int k = 0; k < j; k++) {
-            sum += B(j, k) * B(j, k);
-            }
-            B(j, j) = sqrt(A(j, j) - sum);
-        }
-        barrier(CLK_LOCAL_MEM_FENCE);
-        if (index >= (j + 1) && index < rows) {
-          double s = 0;
+        // First thread calculates the diagonal element
+        if (local_index == 0) {
+          double sum = 0;
           for (int k = 0; k < j; k++) {
-            s += B(index, k) * B(j, k);
+            sum += B(j, k) * B(j, k);
           }
-          B(index, j) = (1.0 / B(j, j) * (A(index, j) - s));
+          B(j, j) = sqrt(A(j, j) - sum);
         }
         barrier(CLK_LOCAL_MEM_FENCE);
-      }      
+        // Calculates the rest of the row
+        if (local_index >= (j + 1) && local_index < rows) {
+          double inner_sum = 0;
+          for (int k = 0; k < j; k++) {
+            inner_sum += B(local_index, k) * B(j, k);
+          }
+          B(local_index, j) = (1.0 / B(j, j) * (A(local_index, j) - inner_sum));
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+      }
     }
     // \cond
 );
 // \endcond
 
 /**
- * See the docs for \link kernels/cholesky_decompose.hpp cholesky_decompose() \endlink
+ * See the docs for \link kernels/cholesky_decompose.hpp cholesky_decompose()
+ * \endlink
  */
-
 const local_range_kernel<cl::Buffer, cl::Buffer, int> cholesky_decompose(
     "cholesky_decompose", cholesky_decompose_kernel_code);
 
