6c0daffb25f6fc7f18678e267c9cb1ef78ec6bf9
Steve Bronder <stevo15025@gmail.com>
2018-11-08 23:10:10 -0500

    move check for square and symmetric to top of cholesky decompose prim

--- stan/math/prim/mat/fun/cholesky_decompose.hpp
+++ stan/math/prim/mat/fun/cholesky_decompose.hpp
@@ -27,6 +27,8 @@ namespace math {
 template <typename T>
 Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> cholesky_decompose(
     const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& m) {
+check_square("cholesky_decompose", "m", m);
+check_symmetric("cholesky_decompose", "m", m);
 #ifdef STAN_OPENCL
   matrix_gpu m_gpu(m);
   Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> m_chol(m.rows(), m.cols());
@@ -34,8 +36,6 @@ Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> cholesky_decompose(
   copy(m_chol, m_gpu);
   return m_chol;
 #else
-  check_square("cholesky_decompose", "m", m);
-  check_symmetric("cholesky_decompose", "m", m);
   Eigen::LLT<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> > llt(m.rows());
   llt.compute(m);
   check_pos_definite("cholesky_decompose", "m", llt);
