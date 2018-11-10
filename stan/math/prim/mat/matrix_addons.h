c6e478b96cdc42f42f95937746563c2261bbc5b5
Steve Bronder <stevo15025@gmail.com>
2018-11-10 01:33:33 -0500

    Tests for using var and double types

--- stan/math/prim/mat/matrix_addons.h
+++ stan/math/prim/mat/matrix_addons.h
@@ -1,4 +1,3 @@
 #ifdef STAN_OPENCL
-mutable bool opencl_cache_ = false;
 mutable cl::Buffer opencl_buffer_;
 #endif
--- test/unit/math/gpu/opencl_matrix_test.cpp
+++ test/unit/math/gpu/opencl_matrix_test.cpp
@@ -8,12 +8,26 @@
 #include <algorithm>
 #include <vector>
 
-void testy_mctest() {
+void testy_mcVartest() {
   double pos = 1.1;
   Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> m(5000, 5000);
   for (int i = 0; i < 5000; ++i)
     for (int j = 0; j < 5000; ++j)
-      m(i, j) = stan::math::var(pos++);
+      m(i, j) = pos++;
+  std::chrono::steady_clock::time_point begin1 = std::chrono::steady_clock::now();
+  stan::math::matrix_gpu d33(m);
+  std::chrono::steady_clock::time_point end1 = std::chrono::steady_clock::now();
+  size_t current1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - begin1).count();
+  std::cout << "First Copy Time: " << current1 << "\n";
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    stan::math::matrix_gpu d33b(m);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  size_t current = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+  std::cout << "Second Copy Time:" << current << "\n";
+}
+
+void test_mcDoubleTest() {
+  auto m = stan::math::matrix_d::Random(5000, 5000).eval();
   std::chrono::steady_clock::time_point begin1 = std::chrono::steady_clock::now();
   stan::math::matrix_gpu d33(m);
   std::chrono::steady_clock::time_point end1 = std::chrono::steady_clock::now();
@@ -23,28 +37,18 @@ void testy_mctest() {
     stan::math::matrix_gpu d33b(m);
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
   size_t current = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
-  std::cout << "Second Copy Time:" << current << "\n";
+  std::cout << "Second Copy Time:  " << current << "\n";
 }
 
 TEST(MathMatrixGPU, matrix_gpu_creation) {
   std::cout << "Var test:  \n";
 
   for (int i = 0; i <= 10; i++) {
-    testy_mctest();
+    testy_mcVarTest();
   }
   std::cout << "Normie test:\n";
   for (int i = 0; i <= 10; i++) {
-    auto m = stan::math::matrix_d::Random(5000, 5000).eval();
-    std::chrono::steady_clock::time_point begin1 = std::chrono::steady_clock::now();
-    stan::math::matrix_gpu d33(m);
-    std::chrono::steady_clock::time_point end1 = std::chrono::steady_clock::now();
-    size_t current1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - begin1).count();
-      std::cout << "First Copy Time: " << current1 << "\n";
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-      stan::math::matrix_gpu d33b(m);
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    size_t current = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
-    std::cout << "Second Copy Time:  " << current << "\n";
+    testy_mcDoubleTest();
   }
 }
 
