d2758b5597e50b5baa4c4703615658bcb8812d15
Sebastian Weber <sdw.post@waebers.de>
2018-12-01 17:16:27 +0100

    add tests for the coupled_ode_observer which correspond to the former decoupling tests

--- test/unit/math/prim/arr/functor/coupled_ode_observer_test.cpp
+++ test/unit/math/prim/arr/functor/coupled_ode_observer_test.cpp
@@ -13,7 +13,7 @@ struct StanMathOde : public ::testing::Test {
   std::vector<int> x_int;
 };
 
-TEST_F(StanMathOde, decouple_states_dd) {
+TEST_F(StanMathOde, observe_states_dd) {
   using stan::math::coupled_ode_system;
 
   harm_osc_ode_fun harm_osc;
@@ -25,29 +25,39 @@ TEST_F(StanMathOde, decouple_states_dd) {
   y0[1] = 0.5;
   theta[0] = 0.15;
 
-  coupled_ode_system<harm_osc_ode_fun, double, double> coupled_system(
-      harm_osc, y0, theta, x, x_int, &msgs);
+  stan::math::coupled_ode_system<harm_osc_ode_fun, double, double>
+      coupled_system(harm_osc, y0, theta, x, x_int, &msgs);
 
+  std::vector<std::vector<double>> y;
+  double t0 = 0;
   int T = 10;
+  std::vector<double> ts(T);
+  for (int t = 0; t < T; t++)
+    ts[t] = t;
+
+  stan::math::coupled_ode_observer<harm_osc_ode_fun, double, double, double,
+                                   double>
+      observer(harm_osc, y0, theta, t0, ts, x, x_int, &msgs, y);
+
+  observer(std::vector<double>(coupled_system.size(), 0.0), 0);
+
   int k = 0;
-  std::vector<std::vector<double> > ys_coupled(T);
+  std::vector<std::vector<double>> ys_coupled(T);
   for (int t = 0; t < T; t++) {
     std::vector<double> coupled_state(coupled_system.size(), 0.0);
     for (int n = 0; n < coupled_system.size(); n++)
       coupled_state[n] = ++k;
     ys_coupled[t] = coupled_state;
+    observer(coupled_state, ts[t]);
   }
 
-  std::vector<std::vector<double> > ys;
-  ys = coupled_system.decouple_states(ys_coupled);
-
-  ASSERT_EQ(T, ys.size());
+  ASSERT_EQ(T, y.size());
   for (int t = 0; t < T; t++)
-    ASSERT_EQ(2, ys[t].size());
+    ASSERT_EQ(2, y[t].size());
 
   for (int t = 0; t < T; t++)
     for (int n = 0; n < 2; n++)
-      EXPECT_FLOAT_EQ(ys_coupled[t][n], ys[t][n])
+      EXPECT_FLOAT_EQ(ys_coupled[t][n], y[t][n])
           << "(" << n << ", " << t << "): "
           << "for (double, double) the coupled system is the base system";
 }
--- test/unit/math/rev/arr/functor/coupled_ode_observer_test.cpp
+++ test/unit/math/rev/arr/functor/coupled_ode_observer_test.cpp
@@ -14,13 +14,11 @@ struct StanAgradRevOde : public ::testing::Test {
   std::vector<int> x_int;
 };
 
-TEST_F(StanAgradRevOde, decouple_states_dv) {
+TEST_F(StanAgradRevOde, observe_states_dv) {
   using stan::math::coupled_ode_system;
   using stan::math::var;
 
   mock_ode_functor mock_ode;
-
-  size_t T = 10;
 
   std::vector<double> y0(2);
   y0[0] = 1.0;
@@ -32,33 +30,49 @@ TEST_F(StanAgradRevOde, decouple_states_dv) {
   coupled_ode_system<mock_ode_functor, double, var> coupled_system(
       mock_ode, y0, theta, x, x_int, &msgs);
 
+  std::vector<std::vector<var>> y;
+  double t0 = 0;
+  int T = 10;
+  std::vector<double> ts(T);
+  for (int t = 0; t < T; t++)
+    ts[t] = t;
+
+  stan::math::coupled_ode_observer<mock_ode_functor, double, var, double,
+                                   double>
+      observer(mock_ode, y0, theta, t0, ts, x, x_int, &msgs, y);
+
+  observer(std::vector<double>(coupled_system.size(), 0.0), 0);
+
   size_t k = 0;
-  std::vector<std::vector<double> > ys_coupled(T);
+  std::vector<std::vector<double>> ys_coupled(T);
   for (size_t t = 0; t < T; t++) {
     std::vector<double> coupled_state(coupled_system.size(), 0.0);
     for (size_t n = 0; n < coupled_system.size(); n++)
       coupled_state[n] = ++k;
     ys_coupled[t] = coupled_state;
+    observer(coupled_state, ts[0]);
   }
 
-  std::vector<std::vector<var> > ys;
-  ys = coupled_system.decouple_states(ys_coupled);
-
-  ASSERT_EQ(T, ys.size());
+  ASSERT_EQ(T, y.size());
   for (size_t t = 0; t < T; t++)
-    ASSERT_EQ(2U, ys[t].size());
+    ASSERT_EQ(2U, y[t].size());
 
-  for (size_t t = 0; t < T; t++)
+  for (size_t t = 0; t < T; t++) {
     for (size_t n = 0; n < 2; n++)
-      EXPECT_FLOAT_EQ(ys_coupled[t][n], ys[t][n].val());
+      EXPECT_FLOAT_EQ(ys_coupled[t][n], y[t][n].val());
+    for (size_t n = 0; n < 2; n++) {
+      y[t][n].grad();
+      EXPECT_FLOAT_EQ(ys_coupled[t][2 + n], theta[0].adj());
+      stan::math::set_zero_all_adjoints();
+    }
+  }
 }
 
-TEST_F(StanAgradRevOde, decouple_states_vd) {
+TEST_F(StanAgradRevOde, observe_states_vd) {
   using stan::math::coupled_ode_system;
   using stan::math::var;
 
   mock_ode_functor mock_ode;
-  size_t T = 10;
 
   std::vector<var> y0(2);
   std::vector<double> theta(1);
@@ -70,28 +84,47 @@ TEST_F(StanAgradRevOde, decouple_states_vd) {
   coupled_ode_system<mock_ode_functor, var, double> coupled_system(
       mock_ode, y0, theta, x, x_int, &msgs);
 
+  std::vector<std::vector<var>> y;
+  double t0 = 0;
+  int T = 10;
+  std::vector<double> ts(T);
+  for (int t = 0; t < T; t++)
+    ts[t] = t;
+
+  stan::math::coupled_ode_observer<mock_ode_functor, var, double, double,
+                                   double>
+      observer(mock_ode, y0, theta, t0, ts, x, x_int, &msgs, y);
+
+  observer(std::vector<double>(coupled_system.size(), 0.0), 0);
+
   size_t k = 0;
-  std::vector<std::vector<double> > ys_coupled(T);
+  std::vector<std::vector<double>> ys_coupled(T);
   for (size_t t = 0; t < T; t++) {
     std::vector<double> coupled_state(coupled_system.size(), 0.0);
     for (size_t n = 0; n < coupled_system.size(); n++)
       coupled_state[n] = ++k;
     ys_coupled[t] = coupled_state;
+    observer(coupled_state, ts[t]);
   }
 
-  std::vector<std::vector<var> > ys;
-  ys = coupled_system.decouple_states(ys_coupled);
-
-  ASSERT_EQ(T, ys.size());
+  ASSERT_EQ(T, y.size());
   for (size_t t = 0; t < T; t++)
-    ASSERT_EQ(2U, ys[t].size());
+    ASSERT_EQ(2U, y[t].size());
 
-  for (size_t t = 0; t < T; t++)
-    for (size_t n = 0; n < 2; n++)
-      EXPECT_FLOAT_EQ(ys_coupled[t][n], ys[t][n].val());
+  for (size_t t = 0; t < T; t++) {
+    for (size_t n = 0; n < 2; n++) {
+      EXPECT_FLOAT_EQ(ys_coupled[t][n], y[t][n].val());
+    }
+    for (size_t n = 0; n < 2; n++) {
+      y[t][n].grad();
+      EXPECT_FLOAT_EQ(ys_coupled[t][2 + n], y0[0].adj());
+      EXPECT_FLOAT_EQ(ys_coupled[t][2 + 2 + n], y0[1].adj());
+      stan::math::set_zero_all_adjoints();
+    }
+  }
 }
 
-TEST_F(StanAgradRevOde, decouple_states_vv) {
+TEST_F(StanAgradRevOde, observe_states_vv) {
   using stan::math::coupled_ode_system;
   using stan::math::var;
 
@@ -107,24 +140,42 @@ TEST_F(StanAgradRevOde, decouple_states_vv) {
   coupled_ode_system<harm_osc_ode_fun, var, var> coupled_system(
       harm_osc, y0, theta, x, x_int, &msgs);
 
-  size_t T = 10;
+  std::vector<std::vector<var>> y;
+  double t0 = 0;
+  int T = 10;
+  std::vector<double> ts(T);
+  for (int t = 0; t < T; t++)
+    ts[t] = t;
+
+  stan::math::coupled_ode_observer<harm_osc_ode_fun, var, var, double, double>
+      observer(harm_osc, y0, theta, t0, ts, x, x_int, &msgs, y);
+
+  observer(std::vector<double>(coupled_system.size(), 0.0), 0);
+
   size_t k = 0;
-  std::vector<std::vector<double> > ys_coupled(T);
+  std::vector<std::vector<double>> ys_coupled(T);
   for (size_t t = 0; t < T; t++) {
     std::vector<double> coupled_state(coupled_system.size(), 0.0);
     for (size_t n = 0; n < coupled_system.size(); n++)
       coupled_state[n] = ++k;
     ys_coupled[t] = coupled_state;
+    observer(coupled_state, ts[t]);
   }
 
-  std::vector<std::vector<var> > ys;
-  ys = coupled_system.decouple_states(ys_coupled);
-
-  ASSERT_EQ(T, ys.size());
+  ASSERT_EQ(T, y.size());
   for (size_t t = 0; t < T; t++)
-    ASSERT_EQ(2U, ys[t].size());
+    ASSERT_EQ(2U, y[t].size());
 
-  for (size_t t = 0; t < T; t++)
+  for (size_t t = 0; t < T; t++) {
     for (size_t n = 0; n < 2; n++)
-      EXPECT_FLOAT_EQ(ys_coupled[t][n], ys[t][n].val());
+      EXPECT_FLOAT_EQ(ys_coupled[t][n], y[t][n].val());
+
+    for (size_t n = 0; n < 2; n++) {
+      y[t][n].grad();
+      EXPECT_FLOAT_EQ(ys_coupled[t][2 + n], y0[0].adj());
+      EXPECT_FLOAT_EQ(ys_coupled[t][2 + 2 + n], y0[1].adj());
+      EXPECT_FLOAT_EQ(ys_coupled[t][2 + 2 * 2 + n], theta[0].adj());
+      stan::math::set_zero_all_adjoints();
+    }
+  }
 }
