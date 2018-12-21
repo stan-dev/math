31985ee04adcc827e64c980fbea11cea73a9841e
Stan Jenkins <mc.stanislaw@gmail.com>
2018-12-21 12:16:40 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- test/unit/math/prim/scal/fun/offset_multiplier_transform_test.cpp
+++ test/unit/math/prim/scal/fun/offset_multiplier_transform_test.cpp
@@ -14,7 +14,8 @@ TEST(prob_transform, offset_multiplier_j) {
   double L = 2.0;
   double U = 5.0;
   double x = -1.0;
-  EXPECT_FLOAT_EQ(L + U * x, stan::math::offset_multiplier_constrain(x, L, U, lp));
+  EXPECT_FLOAT_EQ(L + U * x,
+                  stan::math::offset_multiplier_constrain(x, L, U, lp));
   EXPECT_FLOAT_EQ(-17.0 + log(U), lp);
 
   double lp1 = -12.9;
@@ -25,22 +26,25 @@ TEST(ProbTransform, offset_multiplierException) {
   using stan::math::offset_multiplier_constrain;
   using stan::math::offset_multiplier_free;
   EXPECT_THROW(offset_multiplier_constrain(5.0, 1.0, 0.0), std::domain_error);
-  EXPECT_THROW(
-      offset_multiplier_constrain(5.0, std::numeric_limits<double>::infinity(), 1.0),
-      std::domain_error);
+  EXPECT_THROW(offset_multiplier_constrain(
+                   5.0, std::numeric_limits<double>::infinity(), 1.0),
+               std::domain_error);
   EXPECT_THROW(offset_multiplier_constrain(5.0, NAN, 1.0), std::domain_error);
   EXPECT_NO_THROW(offset_multiplier_constrain(5.0, 1.0, 0.01));
   EXPECT_THROW(offset_multiplier_free(5.0, 1.0, 0.0), std::domain_error);
-  EXPECT_THROW(offset_multiplier_free(5.0, std::numeric_limits<double>::infinity(), 1.0),
-               std::domain_error);
+  EXPECT_THROW(
+      offset_multiplier_free(5.0, std::numeric_limits<double>::infinity(), 1.0),
+      std::domain_error);
   EXPECT_THROW(offset_multiplier_free(5.0, NAN, 1.0), std::domain_error);
   EXPECT_NO_THROW(offset_multiplier_free(5.0, 1.0, 0.01));
   double lp = 12;
-  EXPECT_THROW(offset_multiplier_constrain(5.0, 1.0, 0.0, lp), std::domain_error);
-  EXPECT_THROW(
-      offset_multiplier_constrain(5.0, std::numeric_limits<double>::infinity(), 1.0, lp),
-      std::domain_error);
-  EXPECT_THROW(offset_multiplier_constrain(5.0, NAN, 1.0, lp), std::domain_error);
+  EXPECT_THROW(offset_multiplier_constrain(5.0, 1.0, 0.0, lp),
+               std::domain_error);
+  EXPECT_THROW(offset_multiplier_constrain(
+                   5.0, std::numeric_limits<double>::infinity(), 1.0, lp),
+               std::domain_error);
+  EXPECT_THROW(offset_multiplier_constrain(5.0, NAN, 1.0, lp),
+               std::domain_error);
   EXPECT_NO_THROW(offset_multiplier_constrain(5.0, 1.0, 0.01, lp));
 }
 TEST(prob_transform, offset_multiplier_f) {
@@ -49,18 +53,21 @@ TEST(prob_transform, offset_multiplier_f) {
   double y = 3.0;
   EXPECT_FLOAT_EQ(y, stan::math::offset_multiplier_constrain(
                          stan::math::offset_multiplier_free(y, L, U), L, U));
-  EXPECT_FLOAT_EQ(y, stan::math::offset_multiplier_free(
-                         stan::math::offset_multiplier_constrain(y, L, U), L, U));
+  EXPECT_FLOAT_EQ(y,
+                  stan::math::offset_multiplier_free(
+                      stan::math::offset_multiplier_constrain(y, L, U), L, U));
   L = 0.0;
   U = 1.0;
   y = 3.0;
   EXPECT_FLOAT_EQ(y, stan::math::offset_multiplier_constrain(
                          stan::math::offset_multiplier_free(y, L, U), L, U));
-  EXPECT_FLOAT_EQ(y, stan::math::offset_multiplier_free(
-                         stan::math::offset_multiplier_constrain(y, L, U), L, U));
+  EXPECT_FLOAT_EQ(y,
+                  stan::math::offset_multiplier_free(
+                      stan::math::offset_multiplier_constrain(y, L, U), L, U));
 }
 TEST(prob_transform, offset_multiplier_f_exception) {
   double L = -10.0;
   double U = -27.0;
-  EXPECT_THROW(stan::math::offset_multiplier_free(L - 0.01, L, U), std::domain_error);
+  EXPECT_THROW(stan::math::offset_multiplier_free(L - 0.01, L, U),
+               std::domain_error);
 }
