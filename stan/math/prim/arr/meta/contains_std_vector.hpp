05f07da2d31fb51bc2f76c4e8bdbe02cd87a5218
Daniel Lee <daniel@generable.com>
2019-01-31 13:03:34 -0500

    adding test for contains_std_vector

--- stan/math/prim/arr/meta/contains_std_vector.hpp
+++ stan/math/prim/arr/meta/contains_std_vector.hpp
@@ -1,9 +1,9 @@
 #ifndef STAN_MATH_PRIM_ARR_META_CONTAINS_STD_VECTOR_HPP
 #define STAN_MATH_PRIM_ARR_META_CONTAINS_STD_VECTOR_HPP
 
+#include <stan/math/prim/scal/meta/contains_std_vector.hpp>
 #include <type_traits>
 #include <vector>
-#include <stan/math/prim/scal/meta/contains_std_vector.hpp>
 
 namespace stan {
 
