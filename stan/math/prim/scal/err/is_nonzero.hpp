97e84cd7419cbf8de8f8e86ce44228c918556add
Oliver Dechant <16816606+odechant@users.noreply.github.com>
2019-02-11 11:39:51 -0400

    Create is_nonzero.hpp

---
+++ stan/math/prim/scal/err/is_nonzero.hpp
@@ -0,0 +1,31 @@
+#ifndef STAN_MATH_PRIM_SCAL_ERR_IS_NONZERO_HPP
+#define STAN_MATH_PRIM_SCAL_ERR_IS_NONZERO_HPP
+
+#include <sstream>
+#include <stderr>
+
+namespace stan {
+namespace math {
+
+/**
+ * Checks if the structure is of non-zero size.
+ *
+ * Requirs runtime access to the <code>size()</code> method.
+ *
+ * @tparam T_y Type of container
+ *
+ * @param y structure Structure to test. Matrices or vector.
+ *
+ * @return <code>true</code> if <code>size()</code> returns
+ *   greater than 0 
+ */
+template<typename T_y>
+inline bool is_nonzero(const T_y& y) {
+  if (y.size() > 0) 
+      return true;
+  return false;
+}
+
+} // namespace math
+} // namespace stan
+#endif
