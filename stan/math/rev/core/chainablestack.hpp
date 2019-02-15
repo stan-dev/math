52fffe95f52276d8a3a5090a889be8aae8d07727
Sebastian Weber <sdw.post@waebers.de>
2019-02-15 20:47:36 +0100

    initialize AD tape within unnamed namespace

--- stan/math/rev/core/chainablestack.hpp
+++ stan/math/rev/core/chainablestack.hpp
@@ -3,6 +3,8 @@
 
 #include <stan/math/rev/core/autodiffstackstorage.hpp>
 
+#include <iostream>
+
 namespace stan {
 namespace math {
 
@@ -27,6 +29,28 @@ static inline ChainableStack::AutodiffStackStorage* init() {
   return ChainableStack::instance_;
 }
 */
+
+namespace {
+
+struct ad_tape_initializer {
+  ad_tape_initializer() {
+    std::cout << "Initializer created" << std::endl;
+    tape_ = stan::math::ChainableStack::init();
+  }
+  ~ad_tape_initializer() {
+    // std::lock_guard<std::mutex> cout_lock(cout_mutex_init);
+    std::cout << "Initializer destructed" << std::endl;
+  }
+
+  typedef ChainableStack::AutodiffStackStorage* tape_ptr_t;
+  thread_local static tape_ptr_t tape_;
+};
+
+thread_local ad_tape_initializer::tape_ptr_t ad_tape_initializer::tape_
+    = ChainableStack::init();
+ad_tape_initializer global_initializer;
+
+}  // namespace
 
 }  // namespace math
 }  // namespace stan
