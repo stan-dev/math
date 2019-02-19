98b7e806f12046ac8d23fc63603c2b69e15ac309
Sebastian Weber <sdw.post@waebers.de>
2019-02-19 19:54:04 +0100

    upgrading to sundials v4.1.0; removing printf and fprintf for CRAN

--- lib/sundials_4.1.0/src/nvector/petsc/nvector_petsc.c
+++ lib/sundials_4.1.0/src/nvector/petsc/nvector_petsc.c
@@ -129,7 +129,7 @@ N_Vector N_VNewEmpty_Petsc(MPI_Comm comm,
   ierr = MPI_Allreduce(&n, &Nsum, 1, PVEC_INTEGER_MPI_TYPE, MPI_SUM, comm);
   CHKERRABORT(comm,ierr);
   if (Nsum != global_length) {
-    fprintf(stderr, BAD_N);
+    STAN_SUNDIALS_FPRINTF(stderr, BAD_N);
     return(NULL);
   }
 
