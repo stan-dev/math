52596482daf48fd694118c68055ae0fa535db060
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-02-19 21:22:38 +0100

    the template specialization comes after the default function, rok needs to read more

--- stan/math/prim/mat/fun/cholesky_decompose.hpp
+++ stan/math/prim/mat/fun/cholesky_decompose.hpp
@@ -26,6 +26,31 @@ namespace math {
  * @return Square root of matrix.
  * @note Because OpenCL only works on doubles there are two
  * <code>cholesky_decompose</code> functions. One that works on doubles
+ * and another that works on all other types (this one).
+ * @throw std::domain_error if m is not a symmetric matrix or
+ *   if m is not positive definite (if m has more than 0 elements)
+ */
+template <typename T>
+inline Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> cholesky_decompose(
+    const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& m) {
+  check_square("cholesky_decompose", "m", m);
+  check_symmetric("cholesky_decompose", "m", m);
+  Eigen::LLT<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> > llt(m.rows());
+  llt.compute(m);
+  check_pos_definite("cholesky_decompose", "m", llt);
+  return llt.matrixL();
+}
+
+/**
+ * Return the lower-triangular Cholesky factor (i.e., matrix
+ * square root) of the specified square, symmetric matrix.  The return
+ * value \f$L\f$ will be a lower-traingular matrix such that the
+ * original matrix \f$A\f$ is given by
+ * <p>\f$A = L \times L^T\f$.
+ * @param m Symmetrix matrix.
+ * @return Square root of matrix.
+ * @note Because OpenCL only works on doubles there are two
+ * <code>cholesky_decompose</code> functions. One that works on doubles
  * (this one) and another that works on all other types.
  * @throw std::domain_error if m is not a symmetric matrix or
  *   if m is not positive definite (if m has more than 0 elements)
@@ -57,31 +82,6 @@ inline Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> cholesky_decompose(
   check_pos_definite("cholesky_decompose", "m", llt);
   return llt.matrixL();
 #endif
-}
-
-/**
- * Return the lower-triangular Cholesky factor (i.e., matrix
- * square root) of the specified square, symmetric matrix.  The return
- * value \f$L\f$ will be a lower-traingular matrix such that the
- * original matrix \f$A\f$ is given by
- * <p>\f$A = L \times L^T\f$.
- * @param m Symmetrix matrix.
- * @return Square root of matrix.
- * @note Because OpenCL only works on doubles there are two
- * <code>cholesky_decompose</code> functions. One that works on doubles
- * and another that works on all other types (this one).
- * @throw std::domain_error if m is not a symmetric matrix or
- *   if m is not positive definite (if m has more than 0 elements)
- */
-template <typename T>
-inline Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> cholesky_decompose(
-    const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& m) {
-  check_square("cholesky_decompose", "m", m);
-  check_symmetric("cholesky_decompose", "m", m);
-  Eigen::LLT<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> > llt(m.rows());
-  llt.compute(m);
-  check_pos_definite("cholesky_decompose", "m", llt);
-  return llt.matrixL();
 }
 }  // namespace math
 
