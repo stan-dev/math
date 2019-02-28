a78d3b2b17185bd8289b7dda95661d7b005d6de0
Oliver Dechant <16816606+oliverdechant@users.noreply.github.com>
2019-02-28 12:40:17 -0400

    Update is_size_match.hpp

--- stan/math/prim/scal/err/is_size_match.hpp
+++ stan/math/prim/scal/err/is_size_match.hpp
@@ -8,15 +8,15 @@ namespace stan {
 namespace math {
 
 /**
- * Check if the provided sizes match
+ * @return <code>true</code> if provided dimensions match
  *
- * @tparam T_size1 Type of size 1
- * @tparam T_size2 Type of size 2
+ * @tparam T_size1 Type of size 1, requires function 
+ *    access to <code>.size()</code>
+ * @tparam T_size2 Type of size 2, requires function 
+ *    access to <code>.size()</code>
  *
  * @param i Size 1
  * @param j Size 2
- *
- * @return <code>true</code> if provided dimensions match
  */
 template <typename T_size1, typename T_size2>
 inline bool is_size_match(T_size1 i, T_size2 j) {
