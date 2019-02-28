756c0267bb1463a64e44cf6d9817ca5d1e781569
Oliver Dechant <16816606+oliverdechant@users.noreply.github.com>
2019-02-28 12:48:53 -0400

    Update is_size_match_test.cpp

--- test/unit/math/prim/scal/err/is_size_match_test.cpp
+++ test/unit/math/prim/scal/err/is_size_match_test.cpp
@@ -11,9 +11,13 @@ TEST(ErrorHandlingMatrix, checkSizeMatch) {
   y = 4;
   EXPECT_FALSE(is_size_match(x, y));
   EXPECT_FALSE(is_size_match(y, x));
+  EXPECT_FALSE(is_size_match(x, y)) << "int, size_t";
+  EXPECT_FALSE(is_size_match(y, x)) << "size_t, int";
 
   x = 2;
   y = 2;
   EXPECT_TRUE(is_size_match(x, y));
   EXPECT_TRUE(is_size_match(y, x));
+  EXPECT_FALSE(is_size_match(x, y)) << "int, size_t";
+  EXPECT_FALSE(is_size_match(y, x)) << "size_t, int";
 }
