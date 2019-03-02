0ca64c5445e1864627d5269600653cc7573a393d
oliver <dechant@dal.ca>
2019-03-02 14:05:00 -0400

    oliver: pr#1136

--- stan/math/prim/scal.hpp
+++ stan/math/prim/scal.hpp
@@ -59,6 +59,8 @@
 #include <stan/math/prim/scal/err/invalid_argument_vec.hpp>
 #include <stan/math/prim/scal/err/out_of_range.hpp>
 
+#include <stan/math/prim/scal/err/is_size_match.hpp>
+
 #include <stan/math/prim/scal/fun/abs.hpp>
 #include <stan/math/prim/scal/fun/acosh.hpp>
 #include <stan/math/prim/scal/fun/asinh.hpp>
