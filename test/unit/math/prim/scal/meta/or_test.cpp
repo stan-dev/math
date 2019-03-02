78a0daf8308950ca1603d304a5ad049cef5abbc8
Stan Jenkins <mc.stanislaw@gmail.com>
2019-03-02 11:49:28 -0500

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- test/unit/math/prim/scal/meta/or_test.cpp
+++ test/unit/math/prim/scal/meta/or_test.cpp
@@ -3,31 +3,43 @@
 #include <gtest/gtest.h>
 
 TEST(MathMeta, or_type) {
-    bool temp = stan::math::or_<std::true_type, std::true_type, std::true_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_<std::false_type, std::false_type, std::false_type>::value;
-    EXPECT_FALSE(temp);
-    temp = stan::math::or_<std::false_type, std::true_type, std::true_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_<std::true_type, std::true_type, std::false_type>::value;
-    EXPECT_TRUE(temp);
+  bool temp
+      = stan::math::or_<std::true_type, std::true_type, std::true_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_<std::false_type, std::false_type,
+                         std::false_type>::value;
+  EXPECT_FALSE(temp);
+  temp
+      = stan::math::or_<std::false_type, std::true_type, std::true_type>::value;
+  EXPECT_TRUE(temp);
+  temp
+      = stan::math::or_<std::true_type, std::true_type, std::false_type>::value;
+  EXPECT_TRUE(temp);
 }
 
 TEST(MathMeta, or_not_type) {
-    bool temp = stan::math::or_not_<std::false_type, std::false_type, std::false_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::false_type, std::false_type, std::true_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::false_type, std::true_type, std::false_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::false_type, std::true_type, std::true_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::true_type, std::false_type, std::false_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::true_type, std::false_type, std::true_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::true_type, std::true_type, std::false_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::or_not_<std::true_type, std::true_type, std::true_type>::value;
-    EXPECT_FALSE(temp);
+  bool temp = stan::math::or_not_<std::false_type, std::false_type,
+                                  std::false_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::false_type, std::false_type,
+                             std::true_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::false_type, std::true_type,
+                             std::false_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::false_type, std::true_type,
+                             std::true_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::true_type, std::false_type,
+                             std::false_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::true_type, std::false_type,
+                             std::true_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::true_type, std::true_type,
+                             std::false_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::or_not_<std::true_type, std::true_type,
+                             std::true_type>::value;
+  EXPECT_FALSE(temp);
 }
