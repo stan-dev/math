0688da8f20081b6800aa09bc7beef34a38c313e0
Stan Jenkins <mc.stanislaw@gmail.com>
2019-03-02 11:49:28 -0500

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/scal/meta/or.hpp
+++ stan/math/prim/scal/meta/or.hpp
@@ -18,7 +18,8 @@ struct or_not_ : std::false_type {};
 
 template <typename Cond, typename... Conds>
 struct or_not_<Cond, Conds...>
-    : std::conditional<!Cond::value, std::true_type, or_not_<Conds...>>::type {};
+    : std::conditional<!Cond::value, std::true_type, or_not_<Conds...>>::type {
+};
 
 }  // namespace math
 }  // namespace stan
--- test/unit/math/prim/scal/meta/and_test.cpp
+++ test/unit/math/prim/scal/meta/and_test.cpp
@@ -3,12 +3,16 @@
 #include <gtest/gtest.h>
 
 TEST(MathMeta, and_type) {
-    bool temp = stan::math::and_<std::true_type, std::true_type, std::true_type>::value;
-    EXPECT_TRUE(temp);
-    temp = stan::math::and_<std::false_type, std::false_type, std::false_type>::value;
-    EXPECT_FALSE(temp);
-    temp = stan::math::and_<std::false_type, std::true_type, std::true_type>::value;
-    EXPECT_FALSE(temp);
-    temp = stan::math::and_<std::true_type, std::true_type, std::false_type>::value;
-    EXPECT_FALSE(temp);
+  bool temp
+      = stan::math::and_<std::true_type, std::true_type, std::true_type>::value;
+  EXPECT_TRUE(temp);
+  temp = stan::math::and_<std::false_type, std::false_type,
+                          std::false_type>::value;
+  EXPECT_FALSE(temp);
+  temp = stan::math::and_<std::false_type, std::true_type,
+                          std::true_type>::value;
+  EXPECT_FALSE(temp);
+  temp = stan::math::and_<std::true_type, std::true_type,
+                          std::false_type>::value;
+  EXPECT_FALSE(temp);
 }
