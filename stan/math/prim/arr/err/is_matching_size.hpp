4bb2b7a28d12332d81ccbffae8f2113a52ba8be5
oliver <dechant@dal.ca>
2019-03-04 03:02:48 -0400

    oliver: pr1141

---
+++ stan/math/prim/arr/err/is_matching_size.hpp
@@ -0,0 +1,29 @@
+#ifndef STAN_MATH_PRIM_ARR_ERR_IS_MATCHING_SIZE_HPP
+#define STAN_MATH_PRIM_ARR_ERR_IS_MATCHING_SIZE_HPP
+
+#include <stan/math/prim/scal/err/is_size_match.hpp>
+
+namespace stan {
+namespace math {
+
+/**
+ * Return <code>true</code> if two structures are the same size.
+ *
+ * @tparam T_y1 Type of the first variable, requires class method
+ *    <code>.size()</code>
+ * @tparam T_y2 Type of the second variable, requires class method
+ *    <code>.size()</code>
+ *
+ * @param y1 First variable
+ * @param y2 Second variable
+ *
+ * @return <code>true</code> if the variable sizes match
+ */
+template <typename T_y1, typename T_y2>
+inline bool is_matching_size(const T_y1& y1, const T_y2& y2) {
+  return is_size_match(y1.size(), y2.size());
+}
+
+}  // namespace math
+}  // namespace stan
+#endif
