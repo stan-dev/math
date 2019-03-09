fc518592bdda26f3727e9e2295aed9d9717ef2f4
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-03-09 13:38:45 +0100

    changed comments in is_var_or_arithmetic

--- stan/math/prim/scal/meta/is_var_or_arithmetic.hpp
+++ stan/math/prim/scal/meta/is_var_or_arithmetic.hpp
@@ -9,12 +9,12 @@
 namespace stan {
 
 /**
- * Metaprogram to determine if the provided
- * type is either a var or an arithmetic
- * type.
+ * Defines a public enum named value which is defined to be true (1)
+ * if the type is either var or an aritmetic type
+ * and false (0) otherwise.
  */
 template <typename T>
-struct is_var_or_arithmetic_ {
+struct is_var_or_arithmetic_type {
   enum {
     value = (is_var<typename scalar_type<T>::type>::value
              || std::is_arithmetic<typename scalar_type<T>::type>::value)
@@ -22,12 +22,11 @@ struct is_var_or_arithmetic_ {
 };
 
 /**
- * A generalized metaprogram to determine
- * if all provided types are either a var
- * or an arithmetic type.
+ * Extends std::true_type if all the provided types are either var or 
+ * an arithmetic type, extends std::false_type otherwise.
  */
 template <typename... T>
-using is_var_or_arithmetic = math::conjunction<is_var_or_arithmetic_<T>...>;
+  using is_var_or_arithmetic = math::conjunction<is_var_or_arithmetic_type<T>...>;
 
 }  // namespace stan
 #endif
