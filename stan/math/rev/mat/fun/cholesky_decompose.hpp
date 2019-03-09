fdae5fb4c5fbe170cf9357c54509397fefa31351
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-03-09 20:34:03 +0100

    use triangular multiply where applicable

--- stan/math/rev/mat/fun/cholesky_decompose.hpp
+++ stan/math/rev/mat/fun/cholesky_decompose.hpp
@@ -303,10 +303,10 @@ class cholesky_opencl : public vari {
    * @param L_adj matrix of adjoints of L
    */
   inline void symbolic_rev(matrix_cl& L, matrix_cl& L_adj) {
-    L_adj = transpose(L) * L_adj;
+    L_adj = multiply<TriangularViewCL::Upper, TriangularViewCL::Entire>(transpose(L), L_adj);
     L_adj.triangular_transpose<TriangularMapCL::LowerToUpper>();
     L = transpose(lower_triangular_inverse(L));
-    L_adj = L * transpose(L * L_adj);
+    L_adj = L * transpose(multiply<TriangularViewCL::Upper, TriangularViewCL::Entire>(L, L_adj));
     L_adj.triangular_transpose<TriangularMapCL::LowerToUpper>();
   }
 
@@ -366,7 +366,8 @@ class cholesky_opencl : public vari {
       B_adj.sub_block(L_adj, k, 0, 0, 0, m_k_ind, j);
       C_adj.sub_block(L_adj, k, j, 0, 0, m_k_ind, k_j_ind);
 
-      C_adj = C_adj * lower_triangular_inverse(D);
+      C_adj = multiply<TriangularViewCL::Entire, TriangularViewCL::Lower>
+                    (C_adj, lower_triangular_inverse(D));
       B_adj = B_adj - C_adj * R;
       D_adj = D_adj - transpose(C_adj) * C;
 
