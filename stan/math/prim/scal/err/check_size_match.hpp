5d43cb890b78b95c802286ae8552a378ec5cff0d
oliver <dechant@dal.ca>
2019-03-11 08:34:12 -0300

    oliver: pr#1148 -- Jenkins fix

--- stan/math/prim/scal/err/check_size_match.hpp
+++ stan/math/prim/scal/err/check_size_match.hpp
@@ -23,7 +23,7 @@ namespace math {
 template <typename T_size1, typename T_size2>
 inline void check_size_match(const char* function, const char* name_i,
                              T_size1 i, const char* name_j, T_size2 j) {
-  if i == static_cast<T_size1>(j)
+  if (i == static_cast<T_size1>(j))
     return;
 
   std::ostringstream msg;
@@ -49,7 +49,7 @@ template <typename T_size1, typename T_size2>
 inline void check_size_match(const char* function, const char* expr_i,
                              const char* name_i, T_size1 i, const char* expr_j,
                              const char* name_j, T_size2 j) {
-  if i == static_cast<T_size1>(j)
+  if (i == static_cast<T_size1>(j))
     return;
   std::ostringstream updated_name;
   updated_name << expr_i << name_i;
