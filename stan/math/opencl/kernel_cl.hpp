a63e3c761c52b091795bbc822ff547c1e00e969f
Sean Talts <sean.talts@gmail.com>
2019-03-17 12:19:32 -0400

    Rename get_event_cl to select_events

--- stan/math/opencl/kernel_cl.hpp
+++ stan/math/opencl/kernel_cl.hpp
@@ -5,7 +5,7 @@
 #include <stan/math/opencl/kernels/helpers.hpp>
 #include <stan/math/opencl/matrix_cl.hpp>
 #include <stan/math/opencl/get_kernel_arg.hpp>
-#include <stan/math/opencl/get_event_cl.hpp>
+#include <stan/math/opencl/select_events.hpp>
 #include <stan/math/prim/arr/fun/vec_concat.hpp>
 #include <CL/cl.hpp>
 #include <string>
@@ -143,7 +143,7 @@ struct global_range_kernel {
       cl::NDRange global_thread_size,
       const typename internal::to_matrix<Args>::type&... args) const {
     auto f = make_functor();
-    std::vector<cl::Event> kernel_events = vec_concat(get_event_cl(args)...);
+    std::vector<cl::Event> kernel_events = vec_concat(select_events(args)...);
     cl::EnqueueArgs eargs(opencl_context.queue(), kernel_events,
                           global_thread_size);
     return f(eargs, get_kernel_arg(args)...);
@@ -178,7 +178,7 @@ struct local_range_kernel {
       cl::NDRange global_thread_size, cl::NDRange thread_block_size,
       const typename internal::to_matrix<Args>::type&... args) const {
     auto f = make_functor();
-    std::vector<cl::Event> kernel_events = vec_concat(get_event_cl(args)...);
+    std::vector<cl::Event> kernel_events = vec_concat(select_events(args)...);
     cl::EnqueueArgs eargs(opencl_context.queue(), kernel_events,
                           global_thread_size, thread_block_size);
     return f(eargs, get_kernel_arg(args)...);
--- stan/math/opencl/select_events.hpp
+++ stan/math/opencl/select_events.hpp
@@ -1,10 +1,8 @@
-#ifndef STAN_MATH_OPENCL_GET_EVENT_CL_HPP
-#define STAN_MATH_OPENCL_GET_EVENT_CL_HPP
+#ifndef STAN_MATH_OPENCL_SELECT_EVENTS_HPP
+#define STAN_MATH_OPENCL_SELECT_EVENTS_HPP
 #ifdef STAN_OPENCL
 
 #include <stan/math/opencl/matrix_cl.hpp>
-#include <CL/cl.hpp>
-#include <type_traits>
 #include <vector>
 
 namespace stan {
@@ -12,19 +10,19 @@ namespace math {
 namespace opencl_kernels {
 
 template <typename T>
-inline const std::vector<cl::Event> get_event_cl(const T& t) {
+inline const std::vector<cl::Event> select_events(const T& t) {
   return std::vector<cl::Event>();
 }
 
-inline const std::vector<cl::Event>& get_event_cl(const matrix_cl& m) {
+inline const std::vector<cl::Event>& select_events(const matrix_cl& m) {
   return m.events();
 }
 
-inline const std::vector<cl::Event>& get_event_cl(matrix_cl* const& m) {
+inline const std::vector<cl::Event>& select_events(matrix_cl* const& m) {
   return m->events();
 }
 
-inline const std::vector<cl::Event>& get_event_cl(
+inline const std::vector<cl::Event>& select_events(
     const std::vector<cl::Event>& m) {
   return m;
 }
