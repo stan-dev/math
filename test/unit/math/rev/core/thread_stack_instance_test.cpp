236b6d96cecf476ba6deddda73b0e272b6f03aac
Sebastian Weber <sdw.post@waebers.de>
2019-03-21 23:44:19 +0100

    make explicit use of pointer to AD tape for faster access without relying on compiler optimizations

--- test/unit/math/rev/core/thread_stack_instance_test.cpp
+++ test/unit/math/rev/core/thread_stack_instance_test.cpp
@@ -9,21 +9,21 @@ TEST(thread_stack_instance, initialize) {
   // the main thread must be initialized by the time this code is
   // reached. This will actually segfault if this is not the case.
   // The pointer to the reference returned must evaluate to true.
-  EXPECT_TRUE(&ChainableStack::instance());
+  EXPECT_TRUE(ChainableStack::instance_);
 
-  ChainableStack::AutodiffStackStorage& main_ad_stack
-      = ChainableStack::instance();
+  ChainableStack::AutodiffStackStorage* main_ad_stack
+      = ChainableStack::instance_;
 
   auto thread_tester = [&]() -> void {
     ChainableStack thread_instance;
-    EXPECT_TRUE(&ChainableStack::instance());
-    EXPECT_TRUE(&ChainableStack::instance()
+    EXPECT_TRUE(ChainableStack::instance_);
+    EXPECT_TRUE(ChainableStack::instance_
 #ifdef STAN_THREADS
                 !=
 #else
                 ==
 #endif
-                &main_ad_stack);
+                main_ad_stack);
   };
   std::thread other_work(thread_tester);
 
@@ -37,18 +37,18 @@ TEST(thread_stack_instance, child_instances) {
   // set)
   stan::math::var a = 1;
 
-  ChainableStack::AutodiffStackStorage& main_ad_stack
-      = ChainableStack::instance();
+  ChainableStack::AutodiffStackStorage* main_ad_stack
+      = ChainableStack::instance_;
 
   auto thread_tester = [&]() -> void {
     ChainableStack thread_instance;
-    EXPECT_TRUE(main_ad_stack.var_stack_.size()
+    EXPECT_TRUE(main_ad_stack->var_stack_.size()
 #ifdef STAN_THREADS
                 >
 #else
                 ==
 #endif
-                ChainableStack::instance().var_stack_.size());
+                ChainableStack::instance_->var_stack_.size());
   };
 
   std::thread other_work(thread_tester);
@@ -59,21 +59,21 @@ TEST(thread_stack_instance, child_instances) {
 TEST(thread_stack_instance, persistence) {
   using stan::math::ChainableStack;
 
-  ChainableStack::AutodiffStackStorage& main_ad_stack
-      = ChainableStack::instance();
+  ChainableStack::AutodiffStackStorage* main_ad_stack
+      = ChainableStack::instance_;
 
   {
     // this obviously must be true
-    EXPECT_TRUE(&ChainableStack::instance() == &main_ad_stack);
+    EXPECT_TRUE(ChainableStack::instance_ == main_ad_stack);
 
     // and when constructing a new instance ...
     ChainableStack another_tape;
 
     // ... this must still be true ...
-    EXPECT_TRUE(&ChainableStack::instance() == &main_ad_stack);
+    EXPECT_TRUE(ChainableStack::instance_ == main_ad_stack);
   }
 
   // ... and if we now let another_tape fade away we still expect the
   // main tape to stay around
-  EXPECT_TRUE(&ChainableStack::instance() == &main_ad_stack);
+  EXPECT_TRUE(ChainableStack::instance_ == main_ad_stack);
 }
