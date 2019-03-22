203d8a21a59e0ed26387d266ce11e771a3640f0b
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-03-22 17:16:20 +0100

    added inline to to_size_t and template parameter to calls

--- stan/math/opencl/opencl_context.hpp
+++ stan/math/opencl/opencl_context.hpp
@@ -43,7 +43,7 @@ namespace opencl {
  * @return the cl::size_t<N> converted from the input array
  */
 template <int N>
-cl::size_t<N> to_size_t(const size_t (&values)[N]) {
+inline cl::size_t<N> to_size_t(const size_t (&values)[N]) {
   throw std::domain_error("cl::size_t<N> is not supported for N != 3");
 }
 
@@ -55,7 +55,7 @@ cl::size_t<N> to_size_t(const size_t (&values)[N]) {
  * @return the cl::size_t<3> converted from the input array
  */
 template <>
-cl::size_t<3> to_size_t(const size_t (&values)[3]) {
+inline cl::size_t<3> to_size_t(const size_t (&values)[3]) {
   cl::size_t<3> s;
   for (size_t i = 0; i < 3; i++)
     s[i] = values[i];
