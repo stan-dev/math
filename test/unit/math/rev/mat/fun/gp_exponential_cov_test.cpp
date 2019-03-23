38d99a19f76c8eb3458d749208efcab66b25ce1b
andre <andre@cluster-local.node.drelen>
2019-03-22 23:30:42 -0400

    cleanup first test and add second

--- test/unit/math/rev/mat/fun/gp_exponential_cov_test.cpp
+++ test/unit/math/rev/mat/fun/gp_exponential_cov_test.cpp
@@ -32,7 +32,7 @@ std::string pull_msg(std::vector<T_x1> x1, T_sigma sigma, T_l l) {
   return message;
 }
 
-TEST(RevMath, gp_exp_quad_cov_vvv) {
+TEST(RevMath, gp_exponential_cov_vvv) {
   Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> cov;
 
   for (std::size_t i = 0; i < 3; ++i) {
@@ -63,81 +63,70 @@ TEST(RevMath, gp_exp_quad_cov_vvv) {
       EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val * dist / (l.val() * l.val()),
           grad[1])
           << "index: (" << i << ", " << j << ")";
-
-      // I can use this code later...
-      // if (x[i] < x[j]) {  // since the distance isn't squared
-      //   EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val / l.val(),
-      //                   grad[2])
-      //     << "index: (" << i << ", " << j << ")";
-      // } else if (x[i] > x[j]) {
-      //   EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * -exp_val / l.val(),
-      //                   grad[2])
-      //     << "index: (" << i << ", " << j << ")";
-      // } else {
-      //   EXPECT_FLOAT_EQ(0, grad[2])
-      //     << "index: (" << i << ", " << j << ")";        
-      // }
-
-      // if (x[i] > x[j]) {  // since the distance isn't squared
-      //   EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val / l.val(),
-      //                   grad[3])
-      //     << "index: (" << i << ", " << j << ")";
-      // } else if (x[i] < x[j]) {
-      //   EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * -exp_val / l.val(),
-      //                   grad[3])
-      //     << "index: (" << i << ", " << j << ")";
-      // } else {
-      //   EXPECT_FLOAT_EQ(0, grad[3])
-      //     << "index: (" << i << ", " << j << ")";    
-      // }
       stan::math::recover_memory();
     }
   }
 }
 
-// TEST(RevMath, gp_exp_quad_cov_vvd) {
-//   Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> cov;
+TEST(RevMath, gp_exponential_cov_vvd) {
+  Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> cov;
 
-//   double l = 5;
+  double l = 5;
 
-//   for (std::size_t i = 0; i < 3; ++i) {
-//     for (std::size_t j = 0; j < 3; ++j) {
-//       std::vector<stan::math::var> x(3);
-//       stan::math::var sigma = 0.2;
-//       x[0] = -2;
-//       x[1] = -1;
-//       x[2] = -0.5;
+  for (std::size_t i = 0; i < 3; ++i) {
+    for (std::size_t j = 0; j < 3; ++j) {
+      std::vector<stan::math::var> x(3);
+      stan::math::var sigma = 0.2;
+      x[0] = -2;
+      x[1] = -1;
+      x[2] = -0.5;
 
-//       EXPECT_NO_THROW(cov = stan::math::gp_exp_quad_cov(x, sigma, l));
+      EXPECT_NO_THROW(cov = stan::math::gp_exponential_cov(x, sigma, l));
 
-//       std::vector<double> grad;
-//       std::vector<stan::math::var> params;
-//       params.push_back(sigma);
-//       params.push_back(x[i]);
-//       params.push_back(x[j]);
+      std::vector<double> grad;
+      std::vector<stan::math::var> params;
+      params.push_back(sigma);
+      params.push_back(x[i]);
+      params.push_back(x[j]);
 
-//       cov(i, j).grad(params, grad);
+      cov(i, j).grad(params, grad);
+      double dist = std::abs(x[i].val() - x[j].val());
+      double exp_val = exp(-dist / l);
+      EXPECT_FLOAT_EQ(stan::math::square(sigma.val()) * exp_val,
+                      cov(i, j).val())
+          << "index: (" << i << ", " << j << ")";
+      EXPECT_FLOAT_EQ(2 * sigma.val() * exp_val, grad[0])
+          << "index: (" << i << ", " << j << ")";
+      if (x[i] < x[j]) {  // since the distance isn't squared
+        EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val / l,
+                        grad[1])
+          << "index: (" << i << ", " << j << ")";
+      } else if (x[i] > x[j]) {
+        EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * -exp_val / l,
+                        grad[1])
+          << "index: (" << i << ", " << j << ")";
+      } else {
+        EXPECT_FLOAT_EQ(0, grad[1])
+          << "index: (" << i << ", " << j << ")";        
+      }
 
-//       double distance = x[i].val() - x[j].val();
-//       double sq_distance = stan::math::square(distance);
-//       double sq_l = stan::math::square(l);
-//       double exp_val = exp(sq_distance / (-2.0 * sq_l));
-//       EXPECT_FLOAT_EQ(stan::math::square(sigma.val()) * exp_val,
-//                       cov(i, j).val())
-//           << "index: (" << i << ", " << j << ")";
-//       EXPECT_FLOAT_EQ(2 * sigma.val() * exp_val, grad[0])
-//           << "index: (" << i << ", " << j << ")";
-//       EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val * -distance / sq_l,
-//                       grad[1])
-//           << "index: (" << i << ", " << j << ")";
-//       EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val * distance / sq_l,
-//                       grad[2])
-//           << "index: (" << i << ", " << j << ")";
+      if (x[i] > x[j]) {  // since the distance isn't squared
+        EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * exp_val / l,
+                        grad[2])
+          << "index: (" << i << ", " << j << ")";
+      } else if (x[i] < x[j]) {
+        EXPECT_FLOAT_EQ(sigma.val() * sigma.val() * -exp_val / l,
+                        grad[2])
+          << "index: (" << i << ", " << j << ")";
+      } else {
+        EXPECT_FLOAT_EQ(0, grad[2])
+          << "index: (" << i << ", " << j << ")";    
+      }
 
-//       stan::math::recover_memory();
-//     }
-//   }
-// }
+      stan::math::recover_memory();
+    }
+  }
+}
 
 // TEST(RevMath, gp_exp_quad_cov_vdv) {
 //   Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> cov;
