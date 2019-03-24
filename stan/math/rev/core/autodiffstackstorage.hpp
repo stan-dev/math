13cf353288a7800ed6a375c95f392fc728147b1d
Sebastian Weber <sdw.post@waebers.de>
2019-03-24 09:12:02 +0100

    address reviewer comments

--- stan/math/rev/core/autodiffstackstorage.hpp
+++ stan/math/rev/core/autodiffstackstorage.hpp
@@ -8,46 +8,49 @@ namespace stan {
 namespace math {
 
 /**
- * Provides a thread_local singleton if needed. Read warnings below!
- * For performance reasons the singleton is a global static pointer
- * which is stored as thread local (TLS) if STAN_THREADS is
- * defined. The use of a pointer is motivated by performance reasons
- * for the threading case. When a TLS is used then initialization with
- * a constant expression is required for fast access to the TLS. As
- * the AD storage struct is non-POD it must be initialized as a
- * dynamic expression such that compilers will wrap any access to the
- * TLS by a TLS wrapper function which causes a significant
- * slow-down. A pointer to the AD storage instance can be initialized
- * to a compile-time constant expression of nullptr. In this case the
- * compiler avoids the use of a TLS wrapper function. Furthermore we
- * use the __thread keyword on compilers which support it. The
- * __thread keyword is a compiler-specific (gcc, clang, Intel)
+ * This struct always provides access to an optionally thread local
+ * stack using the singleton pattern. Read warnings below!  For
+ * performance reasons the singleton is a global static pointer which
+ * is stored as thread local (TLS) if STAN_THREADS is defined. The use
+ * of a pointer is motivated by performance reasons for the threading
+ * case. When a TLS is used then initialization with a constant
+ * expression at compile time is required for fast access to the
+ * TLS. As the AD storage struct is non-POD its initialization is a
+ * dynamic expression at compile time. These dynamic expressions are
+ * wrapped in the TLS case by a TLS wrapper function which slows down
+ * its access. Using a pointer instead allows to initialize at compile
+ * time to the nullptr, which is a compile time constant. In this case
+ * the compiler avoids the use of a TLS wrapper function. Furthermore
+ * we use the __thread keyword on compilers which support it. The
+ * __thread keyword is a GNU compiler-specific (gcc, clang, Intel)
  * extension which requires initialization with a compile time
  * constant expression. The C++11 keyword thread_local does allow for
  * constant and dynamic initialization of the TLS. Thus, only the
  * __thread keyword gurantees that constant initialization and it's
  * implied speedup, is used.
  *
- * The initialzation of the AD instance is handled by the lifetime of
- * a AutodiffStackSingleton object. More specifically, the first
- * instance of the AutodiffStackSingleton object will initialize the
- * AD instance and take ownership. Thus whenever the first instance of
- * the AutodiffStackSingleton object gets destructed, the AD tape will
- * be destructed as well.  Within stan-math the initialization of the
- * AD instance for the main thread of the program is handled by
- * instantiating once the singleton once in the
- * init_chainablestack.hpp file. Whenever STAN_THREADS is defined then
- * all created child threads must call the init() method of the AD
- * singleton in order to initialize the TLS if child threads want to
- * perform AD operations (the initialization in the main process is
- * already taken care of in any case).
+ * The initialzation of the AD instance at run-time is handled by the
+ * lifetime of a AutodiffStackSingleton object. More specifically, the
+ * first instance of the AutodiffStackSingleton object will initialize
+ * the AD instance and take ownership (it is the only one instance
+ * with the private member own_instance_ being true). Thus, whenever
+ * the first instance of the AutodiffStackSingleton object gets
+ * destructed, the AD tape will be destructed as well. Within
+ * stan-math the initialization of the AD instance for the main thread
+ * of the program is handled by instantiating once the singleton in
+ * the init_chainablestack.hpp file. Whenever STAN_THREADS is defined
+ * then all created child threads must instantiate a
+ * AutodiffStackSingleton object within the child thread before
+ * accessing the AD system in order to initialize the TLS AD tape
+ * within the child thread.
  *
  * The design of a globally held (optionally TLS) pointer, which is
  * globally initialized, allows the compiler to apply necessary
  * inlining to get maximal performance. However, the design suffers
  * from "the static init order fiasco"[0]. Whenever the static init
- * order fiasco occurs, the C++ client of the library may call the
- * init method as needed to ensure proper initialization order. In
+ * order fiasco occurs, the C++ client of the library may instantiate
+ * a AutodiffStackSingleton object at the adequate code position prior
+ * to any AD tape access to ensure proper initialization order. In
  * exchange, we get a more performant singleton pattern with automatic
  * initialization of the AD stack for the main thread. There has been
  * some discussion on earlier designs using the Mayer singleton
--- stan/math/rev/core/init_chainablestack.hpp
+++ stan/math/rev/core/init_chainablestack.hpp
@@ -7,8 +7,12 @@ namespace stan {
 namespace math {
 namespace {
 
+/**
+ * Initializes the AD stack for the main process. See
+ * autodiffstackstorage.hpp for more explanations.
+ */
 ChainableStack global_stack_instance_init;
-}
+}  // namespace
 }  // namespace math
 }  // namespace stan
 #endif
