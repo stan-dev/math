17ab9cfd5f79abb827a38c1560b19dd58adfe442
Oliver Dechant <oliver@debian>
2019-03-31 16:44:39 -0300

    code cleanup

--- stan/math/prim/mat/err/is_lower_triangular.hpp
+++ stan/math/prim/mat/err/is_lower_triangular.hpp
@@ -2,13 +2,12 @@
 #define STAN_MATH_PRIM_MAT_ERR_IS_LOWER_TRIANGULAR_HPP
 
 #include <Eigen/Dense>
-#include <math.h>
 
 namespace stan {
 namespace math {
 
 namespace internal {
-inline double notLowerNan(double x) { return std::isnan(x) ? 1.0 : x; }
+inline double notNan(double x) { return std::isnan(x) ? 1.0 : x; }
 }  // namespace internal
 
 /**
@@ -16,15 +15,14 @@ inline double notLowerNan(double x) { return std::isnan(x) ? 1.0 : x; }
  * A matrix x is not lower triangular if there is a non-zero entry
  * x[m, n] with m &lt; n. This function only inspect the upper and
  * triangular portion of the matrix, not including the diagonal.
- * @tparam T Type of scalar of the matrix, requires class method
- *   <code>.rows()</code> and <code>.cols()</code>
+ * @tparam T Type of scalar of the matrix
  * @param y Matrix to test
  * @return <code>true</code> is matrix is lower triangular
  */
 template <typename T_y>
 inline bool is_lower_triangular(
     const Eigen::Matrix<T_y, Eigen::Dynamic, Eigen::Dynamic>& y) {
-  return y.unaryExpr(std::ptr_fun(internal::notLowerNan))
+  return y.unaryExpr(std::ptr_fun(internal::notNan))
       .transpose()
       .isUpperTriangular();
 }
