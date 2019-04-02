9b6fab78e04b4da7829afd65c4e39a026d670dec
andre <andre@cluster-local.node.drelen>
2019-04-02 15:24:29 -0400

    add constraint tolerance test

--- test/unit/math/prim/mat/err/constraint_tolerance_test.cpp
+++ test/unit/math/prim/mat/err/constraint_tolerance_test.cpp
@@ -1,2 +1,7 @@
 #include <stan/math/prim/mat.hpp>
 #include <gtest/gtest.h>
+
+TEST(MathPrimMat, constraint_tolerance_exists) {
+  using stan::math::CONSTRAINT_TOLERANCE;
+  EXPECT_FLOAT_EQ(CONSTRAINT_TOLERANCE, 1E-8);
+}
