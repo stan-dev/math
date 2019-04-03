a349d2801175754974f00fe5ca719ef866517b04
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-04-03 19:32:48 +0200

    fix for the add_batch test

--- test/unit/math/opencl/add_test.cpp
+++ test/unit/math/opencl/add_test.cpp
@@ -205,9 +205,8 @@ TEST(MathMatrixGPU, add_batch) {
   }
   stan::math::matrix_cl a_cl(a);
   stan::math::matrix_cl a_cl_res(size, size);
-  stan::math::opencl_kernels::add_batch(cl::NDRange(size, size), a_cl.buffer(),
-                                        size, size, batch_size);
-  a_cl_res.sub_block(a_cl, 0, 0, 0, 0, size, size);
+  stan::math::opencl_kernels::add_batch(cl::NDRange(size, size), a_cl_res.buffer(),
+                                        a_cl.buffer(), size, size, batch_size);
   copy(a_res, a_cl_res);
   for (int k = 0; k < batch_size; k++) {
     for (int i = 0; i < size; i++)
