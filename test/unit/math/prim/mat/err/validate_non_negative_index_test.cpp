3073b86fab52e710a6f0b3206714ae21196d1bcd
andre <andre@cluster-local.node.drelen>
2019-04-11 18:13:04 -0400

    add validate_non_neg_index_test

--- test/unit/math/prim/mat/err/validate_non_negative_index_test.cpp
+++ test/unit/math/prim/mat/err/validate_non_negative_index_test.cpp
@@ -1,2 +1,15 @@
 #include <stan/math/prim/mat.hpp>
 #include <gtest/gtest.h>
+
+TEST(MathPrimMat, val_non_neg_index) {
+  const char* var_name = "var_name";
+  const char* expr_name = "expr";
+  const int val_good = 1;
+  const int val_bad = -1;
+
+  EXPECT_THROW(stan::math::validate_non_negative_index(var_name,
+                                                       expr_name, val_bad),
+               std::invalid_argument);
+  EXPECT_NO_THROW(stan::math::validate_non_negative_index(var_name,
+                                                          expr_name, val_good));
+}
