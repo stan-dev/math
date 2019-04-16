ea557b3fde1d3cfe11499d2415908c628985156a
Oliver Dechant <oliver@debian>
2019-04-16 18:48:15 -0300

    oliver: #1202 breaking test case removed

--- test/unit/math/prim/scal/err/is_not_nan_test.cpp
+++ test/unit/math/prim/scal/err/is_not_nan_test.cpp
@@ -2,19 +2,18 @@
 #include <gtest/gtest.h>
 #include <limits>
 
-using stan::math::is_not_nan;
-
 TEST(ErrorHandlingScalar, isNotNan) {
   double x = 0;
 
-  EXPECT_TRUE(is_not_nan(x));
+  EXPECT_TRUE(stan::math::is_not_nan(x));
 
   x = std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(is_not_nan(x));
+  EXPECT_TRUE(stan::math::is_not_nan(x));
 
   x = -std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(is_not_nan(x));
+  EXPECT_TRUE(stan::math::is_not_nan(x));
 
   x = std::numeric_limits<double>::quiet_NaN();
-  EXPECT_FALSE(is_not_nan(x));
+  EXPECT_FALSE(stan::math::is_not_nan(x));
 }
+
