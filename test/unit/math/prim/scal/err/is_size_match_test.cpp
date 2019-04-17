1c2944c82b144716e90e8184f576bfc65927559f
Oliver Dechant <16816606+ode33@users.noreply.github.com>
2019-04-17 12:50:31 -0300

    fixes

--- test/unit/math/prim/scal/err/is_size_match_test.cpp
+++ test/unit/math/prim/scal/err/is_size_match_test.cpp
@@ -2,19 +2,15 @@
 #include <gtest/gtest.h>
 #include <test/unit/util.hpp>
 
-TEST(ErrorHandlingMatrix, checkSizeMatch) {
+TEST(ErrorHandlingMatrix, isSizeMatch) {
   int x = 3;
   size_t y = 4;
 
   EXPECT_FALSE(stan::math::is_size_match(x, y));
   EXPECT_FALSE(stan::math::is_size_match(y, x));
-  EXPECT_FALSE(stan::math::is_size_match(x, y));
-  EXPECT_FALSE(stan::math::is_size_match(y, x));
 
   x = 2;
   y = 2;
-  EXPECT_TRUE(stan::math::is_size_match(x, y));
-  EXPECT_TRUE(stan::math::is_size_match(y, x));
   EXPECT_TRUE(stan::math::is_size_match(x, y));
   EXPECT_TRUE(stan::math::is_size_match(y, x));
 }
