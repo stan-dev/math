52beae24e307d1772105c3afd4ea52fb86446fda
Edward A. Roualdes <eroualdes@csuchico.edu>
2019-05-03 20:01:56 -0700

    figuring out size of dst

--- stan/math/prim/mat/fun/autocorrelation.hpp
+++ stan/math/prim/mat/fun/autocorrelation.hpp
@@ -125,11 +125,17 @@ void autocorrelation(const Eigen::MatrixBase<DerivedA>& y,
   freqvec = freqvec.cwiseAbs2();
 
   Eigen::Matrix<T, Eigen::Dynamic, 1> ac_tmp(N);
+  std::cout << "ac_tmp.size() = " << ac_tmp.size() << std::endl;
   fft.inv(ac_tmp, freqvec);
-  fft.ClearFlag(fft.HalfSpectrum);
+  //fft.ClearFlag(fft.HalfSpectrum);
+
+  std::cout << "ac_tmp.size() = " << ac_tmp.size() << std::endl;
 
   for (size_t i = 0; i < N; ++i)
     ac_tmp(i) /= (N - i);
+
+  std::cout << "Mt2 = " << Mt2 << std::endl;
+  std::cout << "ac.size() = " << ac.size() << std::endl;
 
   ac = ac_tmp.array() / ac_tmp(0);
 }
