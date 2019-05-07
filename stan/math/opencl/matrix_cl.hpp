827bb563502307fd914b05cfa2982d7aff635955
Steve Bronder <stevo15025@gmail.com>
2019-05-06 21:09:54 -0400

    Revert "[Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)"
    This reverts commit 875a6576cc824fc9c4eb52597633aeb2cccff6ba.

--- stan/math/opencl/matrix_cl.hpp
+++ stan/math/opencl/matrix_cl.hpp
@@ -91,7 +91,9 @@ class matrix_cl {
    * Get the events from the event stacks.
    * @return The read/write event stack.
    */
-  inline const std::vector<cl::Event>& in_events() const { return in_events_; }
+  inline const std::vector<cl::Event>& in_events() const {
+    return in_events_;
+  }
 
   /**
    * Get the events from the event stacks.
