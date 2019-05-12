ad06c0c50802d05cece9cf4a438f12438205b002
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2019-05-12 23:19:35 +0800

    Add doc

--- stan/math/prim/scal/fun/is_nan.hpp
+++ stan/math/prim/scal/fun/is_nan.hpp
@@ -20,6 +20,15 @@ inline bool is_nan(T x) {
   return std::isnan(x);
 }
 
+/**
+ * Returns true if the input is NaN and false otherwise.
+ *
+ * Delegates to <code>std::isnan</code>.
+ *
+ * @param x first argument
+ * @param xs parameter pack of remaining arguments to forward to function
+ * @return <code>true</code> if any value is NaN
+ */
 template <typename T, typename... Ts>
 inline bool is_nan(T x, Ts... xs) {
   return (is_nan(x) || is_nan(std::forward<Ts>(xs)...));
