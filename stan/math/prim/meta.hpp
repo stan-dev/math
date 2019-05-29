485576668766904b855dc0a1b4428b0f0aea97ef
Daniel Lee <daniel@generable.com>
2019-05-29 12:06:30 -0400

    flatten: collecting all meta traits

---
+++ stan/math/prim/meta.hpp
@@ -0,0 +1,26 @@
+#ifndef STAN_MATH_PRIM_META_HPP
+#define STAN_MATH_PRIM_META_HPP
+
+#include <stan/math/prim/arr/meta/get.hpp>
+#include <stan/math/prim/arr/meta/index_type.hpp>
+#include <stan/math/prim/arr/meta/is_vector.hpp>
+#include <stan/math/prim/arr/meta/length.hpp>
+
+#include <stan/math/prim/mat/meta/as_array_or_scalar.hpp>
+#include <stan/math/prim/mat/meta/as_column_vector_or_scalar.hpp>
+#include <stan/math/prim/mat/meta/as_scalar.hpp>
+#include <stan/math/prim/mat/meta/broadcast_array.hpp>
+#include <stan/math/prim/mat/meta/get.hpp>
+#include <stan/math/prim/mat/meta/index_type.hpp>
+#include <stan/math/prim/mat/meta/is_constant_struct.hpp>
+#include <stan/math/prim/mat/meta/is_vector.hpp>
+#include <stan/math/prim/mat/meta/is_vector_like.hpp>
+#include <stan/math/prim/mat/meta/length.hpp>
+#include <stan/math/prim/mat/meta/length_mvt.hpp>
+#include <stan/math/prim/mat/meta/operands_and_partials.hpp>
+#include <stan/math/prim/mat/meta/scalar_type.hpp>
+#include <stan/math/prim/mat/meta/seq_view.hpp>
+#include <stan/math/prim/mat/meta/value_type.hpp>
+#include <stan/math/prim/mat/meta/vector_seq_view.hpp>
+
+#endif
