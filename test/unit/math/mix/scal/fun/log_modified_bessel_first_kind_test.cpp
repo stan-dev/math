f66526f0104802f73cf9f548e82572f1624dc58e
Bob Carpenter <carp@alias-i.com>
2019-06-06 01:07:33 -0400

    log mod bessel 1st to new ad test framework

--- test/unit/math/mix/scal/fun/log_modified_bessel_first_kind_test.cpp
+++ test/unit/math/mix/scal/fun/log_modified_bessel_first_kind_test.cpp
@@ -1,24 +1,15 @@
-#include <test/unit/math/mix/mat/util/autodiff_tester.hpp>
-#include <vector>
+#include <test/unit/math/test_ad.hpp>
 
-struct log_modified_bessel_first_kind_f {
-  template <typename T1, typename T2>
-  static typename boost::math::tools::promote_args<T1, T2>::type apply(
-      const T1& x1, const T2& x2) {
-    using stan::math::log_modified_bessel_first_kind;
-    return log_modified_bessel_first_kind(x1, x2);
-  }
-};
-
-TEST(mathMixCore, logModifiedBesselFirstKind) {
-  std::vector<double> xs
-      = {12.5, 2.3, 1.2, 0.5, stan::math::positive_infinity()};
-  stan::math::test::test_args<log_modified_bessel_first_kind_f, false>(xs);
-
-  // TODO(carpenter):  need to fix boundary conditions in function
-  //   50 : tolerance too tight for autodiff, but basically OK
-  //   0 : known that won't work for finite diffs test
-  //   -0.5  : autodiff doesn't throw
-  //   stan::math::not_a_number() : autodiff doesn't throw
-  //   stan::math::negative_infinity() : autodiff doesn't throw
+TEST(mathMixScalFun, logModifiedBesselFirstKind) {
+  auto f = [](const auto& x1, const auto& x2) {
+    return stan::math::log_modified_bessel_first_kind(x1, x2);
+  };
+  // finite diff tests fail at x1 = 0; remove 0 and run tests by hand
+  std::vector<double> xs1{-1.3, 0.5, stan::math::positive_infinity(),
+                          stan::math::negative_infinity(),
+                          stan::math::not_a_number()};
+  std::vector<double> xs2 = stan::test::common_args();
+  for (double x1 : xs1)
+    for (double x2 : xs2)
+      stan::test::expect_ad(f, x1, x2);
 }
