7b8735f91582f34e13a727c27b42622305f11d76
Steve Bronder <stevo15025@gmail.com>
2019-06-09 22:16:21 -0400

    Fix for check_not_nan instantiation for rev test

--- stan/math/prim/mat/fun/gp_exp_quad_cov.hpp
+++ stan/math/prim/mat/fun/gp_exp_quad_cov.hpp
@@ -611,9 +611,11 @@ inline typename Eigen::MatrixXd gp_exp_quad_cov(
                 / opencl_context.tuning_opts().gp_exp_quad_cov_coeff2
       < 1) {
     for (size_t i = 0; i < x1_size; ++i)
-      check_not_nan(function_name, "x1", x1[i]);
+      for (size_t j = 0; j < x1[i].size(); ++j)
+        check_not_nan(function_name, "x1", x1[i][j]);
     for (size_t i = 0; i < x2_size; ++i)
-      check_not_nan(function_name, "x2", x2[i]);
+      for (size_t j = 0; j < x2[i].size(); ++j)
+        check_not_nan(function_name, "x1", x2[i][j]);
     cov = internal::gp_exp_quad_cov(divide_columns(x1, length_scale),
                                     divide_columns(x2, length_scale),
                                     square(sigma));
