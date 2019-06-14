2336d3dd77039f2660842c4a8dacb6adddad9155
andre <andre@cluster-local.node.drelen>
2019-06-14 14:23:01 -0400

    prim/meta/functor meta.hpp

--- stan/math/prim/mat/functor/finite_diff_hessian.hpp
+++ stan/math/prim/mat/functor/finite_diff_hessian.hpp
@@ -1,6 +1,7 @@
 #ifndef STAN_MATH_PRIM_MAT_FUNCTOR_FINITE_DIFF_HESSIAN_HPP
 #define STAN_MATH_PRIM_MAT_FUNCTOR_FINITE_DIFF_HESSIAN_HPP
 
+#include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/prim/mat/functor/finite_diff_gradient.hpp>
 
