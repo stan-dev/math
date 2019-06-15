67e5adb80667985e5fe5c4fe34ec7df518fe1d08
andre <andre@cluster-local.node.drelen>
2019-06-15 12:18:08 -0400

    meta.hpp rev/mat/functor/

--- stan/math/rev/mat/functor/jacobian.hpp
+++ stan/math/rev/mat/functor/jacobian.hpp
@@ -1,6 +1,7 @@
 #ifndef STAN_MATH_REV_MAT_FUNCTOR_JACOBIAN_HPP
 #define STAN_MATH_REV_MAT_FUNCTOR_JACOBIAN_HPP
 
+#include <stan/math/rev/meta.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/rev/core.hpp>
 #include <stdexcept>
