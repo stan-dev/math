a98c6ef9d8e925c5b804728b492fddf87316e4af
Bob Carpenter <carp@alias-i.com>
2019-06-15 17:11:46 -0400

    finite difference framework

--- stan/math/prim/mat/functor/finite_diff_hessian.hpp
+++ stan/math/prim/mat/functor/finite_diff_hessian.hpp
@@ -6,50 +6,60 @@
 
 namespace stan {
 namespace math {
+namespace internal {
 
+/**
+ * Return the finite difference approximation to the gradient of the
+ * specified function at the specified index of the specified argument
+ * using the specified step size.
+ *
+ * <p>The functor must implement
+ * <code>
+ * double operator()(const Eigen::VectorXd&) const;
+ * </code>
+ *
+ * @tparam F type of function
+ * @param f function to differentiate
+ * @param x argument to function
+ * @param i dimensio of argument for derivative
+ * @param epsilon step size for finite differences
+ * @return derivative of f(x) with respect to x(i)
+ */
 template <typename F>
-double finite_diff_hess_helper(
-    const F& f, const Eigen::Matrix<double, Eigen::Dynamic, 1>& x, int lambda,
-    double epsilon = 1e-03) {
-  using Eigen::Dynamic;
-  using Eigen::Matrix;
+double finite_diff_grad4(const F& f, const Eigen::VectorXd& x, int i,
+                         double epsilon = 1e-03) {
+  Eigen::VectorXd x_temp(x);
 
-  Matrix<double, Dynamic, 1> x_temp(x);
+  x_temp(i) = x(i) + 2 * epsilon;
+  double grad = -f(x_temp);
 
-  double grad = 0.0;
-  x_temp(lambda) = x(lambda) + 2.0 * epsilon;
-  grad = -f(x_temp);
-
-  x_temp(lambda) = x(lambda) + -2.0 * epsilon;
+  x_temp(i) = x(i) + -2 * epsilon;
   grad += f(x_temp);
 
-  x_temp(lambda) = x(lambda) + epsilon;
-  grad += 8.0 * f(x_temp);
+  x_temp(i) = x(i) + epsilon;
+  grad += 8 * f(x_temp);
 
-  x_temp(lambda) = x(lambda) + -epsilon;
-  grad -= 8.0 * f(x_temp);
+  x_temp(i) = x(i) + -epsilon;
+  grad -= 8 * f(x_temp);
 
   return grad;
 }
+}  // namespace internal
 
 /**
- * Calculate the value and the Hessian of the specified function
- * at the specified argument using second-order finite difference.
+ * Calculate the value and the Hessian of the specified function at
+ * the specified argument using second-order finite difference with
+ * the specified perturbation step size.
  *
  * <p>The functor must implement
- *
  * <code>
  * double
- * operator()(const
- * Eigen::Matrix<double, Eigen::Dynamic, 1>&)
+ * operator()(const Eigen::VectorXd&) const;
  * </code>
  *
- * Error should be on order of epsilon ^ 4, with 4
- * calls to the function f.
- *
- * Reference:
- * Eberly: Derivative Approximation by Finite Differences
- * Page 6
+ * <p>For details of the algorithm, see
+ * <br />Eberly, D., 2008. Derivative approximation by finite
+ * differences. Magic Software, Inc., Page 6.
  *
  * @tparam F Type of function
  * @param[in] f Function
@@ -57,49 +67,41 @@ double finite_diff_hess_helper(
  * @param[out] fx Function applied to argument
  * @param[out] grad_fx Gradient of function at argument
  * @param[out] hess_fx Hessian of function at argument
- * @param[in] epsilon perturbation size
+ * @param[in] epsilon perturbation step size
  */
 template <typename F>
-void finite_diff_hessian(const F& f, const Eigen::Matrix<double, -1, 1>& x,
-                         double& fx, Eigen::Matrix<double, -1, 1>& grad_fx,
-                         Eigen::Matrix<double, -1, -1>& hess_fx,
+void finite_diff_hessian(const F& f, const Eigen::VectorXd& x, double& fx,
+                         Eigen::VectorXd& grad_fx, Eigen::MatrixXd& hess_fx,
                          double epsilon = 1e-03) {
-  using Eigen::Dynamic;
-  using Eigen::Matrix;
-
   int d = x.size();
-
-  Matrix<double, Dynamic, 1> x_temp(x);
+  Eigen::VectorXd x_temp(x);
   hess_fx.resize(d, d);
-
   finite_diff_gradient(f, x, fx, grad_fx);
-  double f_diff(0.0);
   for (int i = 0; i < d; ++i) {
     for (int j = i; j < d; ++j) {
-      x_temp(i) += 2.0 * epsilon;
+      double f_diff = 0;
+      x_temp(i) += 2 * epsilon;
       if (i != j) {
-        f_diff = -finite_diff_hess_helper(f, x_temp, j);
-        x_temp(i) = x(i) + -2.0 * epsilon;
-        f_diff += finite_diff_hess_helper(f, x_temp, j);
+        f_diff = -internal::finite_diff_grad4(f, x_temp, j);
+        x_temp(i) = x(i) + -2 * epsilon;
+        f_diff += internal::finite_diff_grad4(f, x_temp, j);
         x_temp(i) = x(i) + epsilon;
-        f_diff += 8.0 * finite_diff_hess_helper(f, x_temp, j);
+        f_diff += 8 * internal::finite_diff_grad4(f, x_temp, j);
         x_temp(i) = x(i) + -epsilon;
-        f_diff -= 8.0 * finite_diff_hess_helper(f, x_temp, j);
-        f_diff /= 12.0 * epsilon * 12.0 * epsilon;
+        f_diff -= 8 * internal::finite_diff_grad4(f, x_temp, j);
+        f_diff /= 12 * epsilon * 12 * epsilon;
       } else {
         f_diff = -f(x_temp);
         f_diff -= 30 * fx;
-        x_temp(i) = x(i) + -2.0 * epsilon;
+        x_temp(i) = x(i) + -2 * epsilon;
         f_diff -= f(x_temp);
         x_temp(i) = x(i) + epsilon;
-        f_diff += 16.0 * f(x_temp);
+        f_diff += 16 * f(x_temp);
         x_temp(i) = x(i) - epsilon;
-        f_diff += 16.0 * f(x_temp);
+        f_diff += 16 * f(x_temp);
         f_diff /= 12 * epsilon * epsilon;
       }
-
       x_temp(i) = x(i);
-
       hess_fx(j, i) = f_diff;
       hess_fx(i, j) = hess_fx(j, i);
     }
