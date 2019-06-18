2046700b8d0f86339331e280631458435bc8ef0e
Bob Carpenter <carp@alias-i.com>
2019-06-18 01:15:56 -0400

    fix nonworking tests for Phi_approx and acosh

--- test/unit/math/mix/mat/fun/Phi_approx_test.cpp
+++ test/unit/math/mix/mat/fun/Phi_approx_test.cpp
@@ -3,6 +3,5 @@
 TEST(mathMixMatFun, PhiApprox) {
   auto f = [](const auto& x1) { return stan::math::Phi_approx(x1); };
   stan::test::expect_common_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, -27.5, -15.2, -10, 1, 1.3, 5, 10,
-                                      27.5);
+  stan::test::expect_unary_vectorized(f, -3.0, 1, 1.3, 3);
 }
--- test/unit/math/mix/mat/fun/acosh_test.cpp
+++ test/unit/math/mix/mat/fun/acosh_test.cpp
@@ -3,5 +3,5 @@
 TEST(mathMixMatFun, acosh) {
   auto f = [](const auto& x1) { return stan::math::acosh(x1); };
   stan::test::expect_common_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, 1.5, 1, 3.2, 5, 10, 12.9);
+  stan::test::expect_unary_vectorized(f, 1.5, 3.2, 5, 10, 12.9);
 }
