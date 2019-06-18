e6bc628c784a0a8ca3f0aed1bfb34b27395c1d8f
Bob Carpenter <carp@alias-i.com>
2019-06-17 20:21:00 -0400

    fix boundary test for acos

--- test/unit/math/mix/mat/fun/acos_test.cpp
+++ test/unit/math/mix/mat/fun/acos_test.cpp
@@ -5,7 +5,7 @@ TEST(mathMixMatFun, acos) {
   using stan::test::expect_unary_vectorized;
   auto f = [](const auto& x1) { return stan::math::acos(x1); };
   stan::test::expect_common_unary_vectorized(f);
-  expect_unary_vectorized(f, -2.2, -1, -0.8, 0.5, 1,
+  expect_unary_vectorized(f, -2.2, -0.8, 0.5,
                           1 + std::numeric_limits<double>::epsilon(), 1.5, 3,
                           3.4, 4);
 }
