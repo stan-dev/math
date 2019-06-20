970b57d788c7e1fbb2f5d9cc9055b40667b71402
Steve Bronder <stevo15025@gmail.com>
2019-06-20 09:49:47 -0400

    fixup headers in cov_matrix_free

--- stan/math/prim/mat/fun/cov_matrix_free.hpp
+++ stan/math/prim/mat/fun/cov_matrix_free.hpp
@@ -5,7 +5,6 @@
 #include <stan/math/prim/arr/err/check_nonzero_size.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/prim/mat/err/check_square.hpp>
-#include <stan/math/prim/arr/err/check_nonzero_size.hpp>
 #include <stan/math/prim/scal/err/check_positive.hpp>
 #include <cmath>
 
