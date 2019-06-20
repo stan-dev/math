0fb70de72fba01b74e9ce21f7b6b6850e8298a92
Steve Bronder <Stevo15025@gmail.com>
2019-06-20 13:39:07 -0400

    Update stan/math/prim/mat/fun/corr_matrix_constrain.hpp

--- stan/math/prim/mat/fun/corr_matrix_constrain.hpp
+++ stan/math/prim/mat/fun/corr_matrix_constrain.hpp
@@ -5,6 +5,7 @@
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/prim/scal/err/check_size_match.hpp>
 #include <stan/math/prim/scal/fun/corr_constrain.hpp>
+#include <stan/math/prim/mat/fun/read_corr_matrix.hpp>
 #include <stdexcept>
 
 namespace stan {
