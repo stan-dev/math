5552b7d93a3148aa66fc7f904e67bdd0e3028009
Stan Jenkins <mc.stanislaw@gmail.com>
2019-06-29 14:27:00 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/prim/mat/fun/multiply.hpp
+++ stan/math/prim/mat/fun/multiply.hpp
@@ -55,10 +55,10 @@ inline Eigen::Matrix<double, R1, C2> multiply(
     const Eigen::Matrix<double, R2, C2>& m2) {
   check_multiplicable("multiply", "m1", m1, "m2", m2);
 #ifdef STAN_OPENCL
-  if (((m1.rows() * m2.cols()) >
-      opencl_context.tuning_opts().multiply_result_size_worth_transfer) &&
-      (m1.cols() >
-      opencl_context.tuning_opts().multiply_common_dim_worth_transfer)) {
+  if (((m1.rows() * m2.cols())
+       > opencl_context.tuning_opts().multiply_result_size_worth_transfer)
+      && (m1.cols()
+          > opencl_context.tuning_opts().multiply_common_dim_worth_transfer)) {
     matrix_cl m1_cl(m1);
     matrix_cl m2_cl(m2);
     matrix_cl m3_cl = m1_cl * m2_cl;
