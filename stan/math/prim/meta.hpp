47a9c1bbba97039b5913a592747b3b7ccaa6978b
andre <andre@cluster-local.node.drelen>
2019-07-18 10:19:09 -0400

    vector seq view

--- stan/math/prim/meta.hpp
+++ stan/math/prim/meta.hpp
@@ -14,18 +14,18 @@
 #include <stan/math/prim/meta/as_column_vector_or_scalar.hpp>
 #include <stan/math/prim/meta/as_scalar.hpp>
 #include <stan/math/prim/mat/meta/broadcast_array.hpp>
-//#include <stan/math/prim/meta/is_constant.hpp>
+//#include <stan/math/prim/meta/is_constant.hpp>  // come back to it
 #include <stan/math/prim/meta/is_vector_like.hpp>
 #include <stan/math/prim/meta/length_mvt.hpp>
-#include <stan/math/prim/mat/meta/operands_and_partials.hpp>
-#include <stan/math/prim/mat/meta/scalar_type.hpp>
-#include <stan/math/prim/mat/meta/seq_view.hpp>
-#include <stan/math/prim/mat/meta/value_type.hpp>
-#include <stan/math/prim/mat/meta/vector_seq_view.hpp>
+//#include <stan/math/prim/meta/operands_and_partials.hpp>  // come back to it
+#include <stan/math/prim/meta/scalar_type.hpp>
+#include <stan/math/prim/meta/seq_view.hpp>
+//#include <stan/math/prim/meta/value_type.hpp>
+#include <stan/math/prim/meta/vector_seq_view.hpp>
 
 #include <stan/math/prim/arr/meta/contains_std_vector.hpp>
 //#include <stan/math/prim/arr/meta/is_constant.hpp>
-#include <stan/math/prim/arr/meta/scalar_type.hpp>
+//#include <stan/math/prim/arr/meta/scalar_type.hpp>
 #include <stan/math/prim/arr/meta/value_type.hpp>
 #include <stan/math/prim/arr/meta/VectorBuilderHelper.hpp>
 
@@ -35,7 +35,7 @@
 #include <stan/math/prim/scal/meta/contains_std_vector.hpp>
 #include <stan/math/prim/scal/meta/contains_vector.hpp>
 #include <stan/math/prim/scal/meta/error_index.hpp>
-//#include <stan/math/prim/scal/meta/include_summand.hpp>
+//#include <stan/math/prim/scal/meta/include_summand.hpp>  // causing bug
 //#include <stan/math/prim/scal/meta/is_constant.hpp>
 #include <stan/math/prim/scal/meta/is_fvar.hpp>
 #include <stan/math/prim/scal/meta/is_var.hpp>
@@ -43,12 +43,11 @@
 #include <stan/math/prim/scal/meta/likely.hpp>
 #include <stan/math/prim/scal/meta/max_size.hpp>
 //#include <stan/math/prim/scal/meta/max_size_mvt.hpp>
-#include <stan/math/prim/scal/meta/operands_and_partials.hpp>
 #include <stan/math/prim/scal/meta/partials_return_type.hpp>
 #include <stan/math/prim/scal/meta/partials_type.hpp>
 #include <stan/math/prim/scal/meta/return_type.hpp>
 #include <stan/math/prim/scal/meta/scalar_seq_view.hpp>
-#include <stan/math/prim/scal/meta/scalar_type.hpp>
+//#include <stan/math/prim/scal/meta/scalar_type.hpp>
 #include <stan/math/prim/scal/meta/scalar_type_pre.hpp>
 #include <stan/math/prim/scal/meta/size_of.hpp>
 #include <stan/math/prim/scal/meta/value_type.hpp>
