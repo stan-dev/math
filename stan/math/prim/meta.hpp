c1712a1107305a40d91b8b00a979bfd40ffbba12
andre <andre@cluster-local.node.drelen>
2019-07-22 05:24:07 -0400

    contains std_Vector FIX ME

--- stan/math/prim/meta.hpp
+++ stan/math/prim/meta.hpp
@@ -32,7 +32,7 @@
 #include <stan/math/prim/meta/ad_promotable.hpp>
 //#include <stan/math/prim/meta/child_type.hpp>
 #include <stan/math/prim/meta/contains_fvar.hpp>
-#include <stan/math/prim/scal/meta/contains_std_vector.hpp>
+//#include <stan/math/prim/meta/contains_std_vector.hpp>
 #include <stan/math/prim/scal/meta/contains_vector.hpp>
 #include <stan/math/prim/scal/meta/error_index.hpp>
 //#include <stan/math/prim/scal/meta/include_summand.hpp>  // causing bug
