af6d4bf726e4381f25240cf2e9405eaa7d7d9c2f
Charles Margossian <charlesm@metrumrg.com>
2019-07-23 09:15:44 +0300

    Add laplace marginal functions to the laplace header.

--- stan/math/laplace/laplace.hpp
+++ stan/math/laplace/laplace.hpp
@@ -1,6 +1,10 @@
 // Laplace header files
 
+// New prototypes
+#include <stan/math/laplace/laplace_marginal_bernoulli.hpp>
+#include <stan/math/laplace/laplace_marginal_poisson.hpp>
+
+// First prototypes
 #include <stan/math/laplace/lgp_newton_solver.hpp>
 #include <stan/math/laplace/lgp_dense_newton_solver.hpp>
 #include <stan/math/laplace/lgp_solver.hpp>
-#include <stan/math/laplace/laplace_marginal_bernoulli.hpp>
