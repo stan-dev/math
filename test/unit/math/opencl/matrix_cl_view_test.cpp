1efb73bdebb9ebacad8da3e524a50c72adea7559
Stan Jenkins <mc.stanislaw@gmail.com>
2019-08-16 04:23:47 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- test/unit/math/opencl/matrix_cl_view_test.cpp
+++ test/unit/math/opencl/matrix_cl_view_test.cpp
@@ -98,10 +98,8 @@ TEST(matrix_cl_view, from_eigen_uplo_type) {
   using stan::math::matrix_cl_view;
   EXPECT_EQ(matrix_cl_view::Lower, from_eigen_uplo_type(Eigen::Lower));
   EXPECT_EQ(matrix_cl_view::Upper, from_eigen_uplo_type(Eigen::Upper));
-  EXPECT_EQ(matrix_cl_view::Entire,
-            from_eigen_uplo_type(Eigen::SelfAdjoint));
-  EXPECT_EQ(matrix_cl_view::Entire,
-            from_eigen_uplo_type(Eigen::UnitDiag));
+  EXPECT_EQ(matrix_cl_view::Entire, from_eigen_uplo_type(Eigen::SelfAdjoint));
+  EXPECT_EQ(matrix_cl_view::Entire, from_eigen_uplo_type(Eigen::UnitDiag));
 }
 
 #endif
