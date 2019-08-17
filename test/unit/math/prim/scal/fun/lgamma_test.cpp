3429945cc3db26095bc592c061650eb666286499
Sebastian Weber <sdw.post@waebers.de>
2019-08-17 09:54:00 +0200

    add test to ensure we have the right boost lgamma

--- test/unit/math/prim/scal/fun/lgamma_test.cpp
+++ test/unit/math/prim/scal/fun/lgamma_test.cpp
@@ -12,4 +12,12 @@ TEST(MathFunctions, lgamma_nan) {
   double nan = std::numeric_limits<double>::quiet_NaN();
   EXPECT_PRED1(boost::math::isnan<double>, stan::math::lgamma(nan));
   EXPECT_PRED1(boost::math::isinf<double>, stan::math::lgamma(0));
+  // up to boost 1.70.0 the boost::math::lgamma return NaN for large
+  // arguments (large is creater than sqrt(largest double) when used
+  // with the policy which avoids propagation of double to long double
+  // see https://github.com/boostorg/math/issues/242
+  // this boost::math::lgamma is used and as such we need to make sure
+  // that the appropiate boost is available
+  EXPECT_PRED1(boost::math::isnormal<double>,
+               boost::math::lgamma(1.0E50, stan::math::boost_policy_t()));
 }
