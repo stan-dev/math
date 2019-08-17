89293dfd9af365ddc6aeeab956f6de00c02c856f
stevo15025 <stevo15025@gmail.com>
2019-08-17 03:46:11 +0300

    revert changes to fvar

--- stan/math/fwd/scal/fun/to_fvar.hpp
+++ stan/math/fwd/scal/fun/to_fvar.hpp
@@ -3,7 +3,6 @@
 
 #include <stan/math/fwd/meta.hpp>
 #include <stan/math/fwd/core.hpp>
-#include <stan/math/prim/meta.hpp>
 
 namespace stan {
 namespace math {
@@ -17,24 +16,6 @@ template <typename T>
 inline fvar<T> to_fvar(const fvar<T>& x) {
   return x;
 }
-
-template <typename T1, typename T2, enable_if_all_not_same<T2, T2>* = nullptr, enable_if_fvar<T2>* = nullptr>
-inline fvar<T2> to_fvar(const T1& x, const T2& x2) {
-  return fvar<T2>(x);
-}
-
-template <typename T1, typename T2, enable_if_all_same<T1, T2>* = nullptr>
-inline T1& to_fvar(T1& x, const T2& x2) {
-  return x;
-}
-
-template <typename T1, typename T2, enable_if_not_same<T1, T2>* = nullptr, enable_if_not_arithmetic<T2>* = nullptr>
-inline fvar<T2> to_fvar(const fvar<T1>& x, const fvar<T2>& x2) {
-  return fvar<T2>(x);
-}
-
-
-
 
 }  // namespace math
 }  // namespace stan
