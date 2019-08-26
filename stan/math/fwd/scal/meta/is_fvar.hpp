cdb9e0d479451eaba6e3dcc0bc9edc0787f5b7ce
Stan Jenkins <mc.stanislaw@gmail.com>
2019-08-26 13:07:05 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/fwd/scal/meta/is_fvar.hpp
+++ stan/math/fwd/scal/meta/is_fvar.hpp
@@ -11,8 +11,9 @@ namespace stan {
  * when instantiated with the stan::math::fvar type.
  */
 template <typename T>
-struct is_fvar<T, std::enable_if_t<std::is_same<stan::math::fvar<std::decay_t<T>>,
-                                           std::decay_t<T>>::value>>
+struct is_fvar<T,
+               std::enable_if_t<std::is_same<stan::math::fvar<std::decay_t<T>>,
+                                             std::decay_t<T>>::value>>
     : std::true_type {};
 
 }  // namespace stan
