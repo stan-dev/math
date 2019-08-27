187eb4061dab6bc918f661b64a576d0817ac8988
stevo15025 <stevo15025@gmail.com>
2019-08-27 14:46:59 +0300

    add const and non-const specialization of to_var/fvar for std::vector methods in arr

--- stan/math/fwd/arr/fun/to_fvar.hpp
+++ stan/math/fwd/arr/fun/to_fvar.hpp
@@ -17,7 +17,7 @@ inline std::vector<fvar<T> > to_fvar(const std::vector<T>& v) {
 }
 
 template <typename T>
-inline std::vector<fvar<T> > to_fvar(const std::vector<T>& v,
+inline std::vector<fvar<T>> to_fvar(const std::vector<T>& v,
                                      const std::vector<T>& d) {
   std::vector<fvar<T> > x(v.size());
   for (size_t i = 0; i < v.size(); ++i)
@@ -25,8 +25,27 @@ inline std::vector<fvar<T> > to_fvar(const std::vector<T>& v,
   return x;
 }
 
+/**
+ * Specialization of to_fvar for const fvar input
+ *
+ * @tparam The inner type of the fvar.
+ * @param[in,out] v A vector of forward automatic differentiation variable.
+ * @return The input vector of forward automatic differentiation variable.
+ */
 template <typename T>
-inline std::vector<fvar<T> > to_fvar(const std::vector<fvar<T> >& v) {
+inline const std::vector<fvar<T>>& to_fvar(const std::vector<fvar<T>>& v) {
+  return v;
+}
+
+/**
+ * Specialization of to_fvar for non-const fvar input
+ *
+ * @tparam The inner type of the fvar.
+ * @param[in,out] v A vector of forward automatic differentiation variable.
+ * @return The input vector of forward automatic differentiation variable.
+ */
+template <typename T>
+inline std::vector<fvar<T>>& to_fvar(std::vector<fvar<T>>& v) {
   return v;
 }
 
