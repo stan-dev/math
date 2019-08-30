d7ccc42176385290d926c10045d737d21309056b
stevo15025 <stevo15025@gmail.com>
2019-08-30 19:38:08 +0300

    Merge to remote

--- test/unit/math/prim/mat/fun/value_of_test.cpp
+++ test/unit/math/prim/mat/fun/value_of_test.cpp
@@ -2,6 +2,42 @@
 #include <gtest/gtest.h>
 #include <type_traits>
 
+#include <type_traits>
+#include <typeinfo>
+#ifndef _MSC_VER
+#   include <cxxabi.h>
+#endif
+#include <memory>
+#include <string>
+#include <cstdlib>
+
+template <class T>
+std::string
+type_name()
+{
+    typedef typename std::remove_reference<T>::type TR;
+    std::unique_ptr<char, void(*)(void*)> own
+           (
+#ifndef _MSC_VER
+                abi::__cxa_demangle(typeid(TR).name(), nullptr,
+                                           nullptr, nullptr),
+#else
+                nullptr,
+#endif
+                std::free
+           );
+    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    if (std::is_const<TR>::value)
+        r += " const";
+    if (std::is_volatile<TR>::value)
+        r += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        r += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        r += "&&";
+    return r;
+}
+
 TEST(MathMatrix, value_of) {
   using stan::math::value_of;
 
@@ -32,6 +68,7 @@ TEST(MathFunctions, value_of_return_type_short_circuit_vector_xd) {
   EXPECT_FALSE(
       (std::is_same<decltype(stan::math::value_of(a)),
                     Eigen::Matrix<double, Eigen::Dynamic, 1>>::value));
+std::cout << "decltype(stan::math::value_of(a)) is " << type_name<decltype(stan::math::value_of(a))>() << '\n';                    
   EXPECT_FALSE(
       (std::is_same<decltype(stan::math::value_of(a)),
                     const Eigen::Matrix<double, Eigen::Dynamic, 1>>::value));
