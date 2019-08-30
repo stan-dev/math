0e11d7638a4b4c0ac4d010959f90c7114ae5d1c8
stevo15025 <stevo15025@gmail.com>
2019-08-30 11:56:06 +0300

    include prim value_of in rev value_of

--- stan/math/rev/scal/fun/value_of.hpp
+++ stan/math/rev/scal/fun/value_of.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/core.hpp>
 #include <stan/math/prim/meta.hpp>
+#include <stan/math/prim/mat/fun/value_of.hpp>
 #include <type_traits>
 
 namespace stan {
