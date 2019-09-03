dadcc079b36293880050a5b1d63a9faacf2a1182
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2019-09-03 12:46:34 +0300

    Reordered headers

--- stan/math/rev/mat/fun/inverse.hpp
+++ stan/math/rev/mat/fun/inverse.hpp
@@ -1,12 +1,12 @@
 #ifndef STAN_MATH_REV_MAT_FUN_INVERSE_HPP
 #define STAN_MATH_REV_MAT_FUN_INVERSE_HPP
 
-#include <stan/math/rev/meta.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
-#include <stan/math/prim/mat/fun/typedefs.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/rev/mat/fun/typedefs.hpp>
+#include <stan/math/rev/meta.hpp>
 #include <stan/math/prim/mat/err/check_square.hpp>
+#include <stan/math/rev/mat/fun/typedefs.hpp>
+#include <stan/math/prim/mat/fun/typedefs.hpp>
 
 namespace stan {
 namespace math {
