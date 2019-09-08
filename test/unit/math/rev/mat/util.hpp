ead4ff085bb392bdfa3cd7d63c8fe56fe5a5eead
stevo15025 <stevo15025@gmail.com>
2019-09-08 17:52:48 +0300

    Show template style with add function

--- test/unit/math/rev/mat/util.hpp
+++ test/unit/math/rev/mat/util.hpp
@@ -1,14 +1,17 @@
 #ifndef TEST_UNIT_MATH_REV_MAT_UTIL_HPP
 #define TEST_UNIT_MATH_REV_MAT_UTIL_HPP
 
+#include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/rev/mat.hpp>
-#include <gtest/gtest.h>
+#include <stan/math/prim/meta.hpp>
 #include <test/unit/math/rev/arr/util.hpp>
+#include <gtest/gtest.h>
 
 namespace test {
 
-template <int R, int C>
-void check_varis_on_stack(const Eigen::Matrix<stan::math::var, R, C>& x) {
+  template <typename Mat, stan::require_eigen_t<stan::is_var, Mat>...>
+void check_varis_on_stack(Mat&& x) {
+  x.eval();
   for (int j = 0; j < x.cols(); ++j)
     for (int i = 0; i < x.rows(); ++i)
       EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(
