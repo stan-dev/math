c55ad9023e4eed792b9b2247760cfe637d67f666
tadej <tadej.c@gmail.com>
2019-09-09 11:31:55 +0200

    fixed test

--- test/unit/math/rev/mat/prob/normal_id_glm_lpdf_cpu_test.cpp
+++ test/unit/math/rev/mat/prob/normal_id_glm_lpdf_cpu_test.cpp
@@ -1,4 +1,4 @@
 #ifdef STAN_OPENCL
 #undef STAN_OPENCL
-#include <test\unit\math\rev\mat\prob\normal_id_glm_lpdf_test.cpp>
+#include <test/unit/math/rev/mat/prob/normal_id_glm_lpdf_test.cpp>
 #endif
