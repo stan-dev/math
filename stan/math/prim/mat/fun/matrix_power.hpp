07a58a582de02601c2a0a51617f92590fc38c842
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2019-09-11 12:50:11 -0500

    Stub out implementation of matrix power function.

---
+++ stan/math/prim/mat/fun/matrix_power.hpp
@@ -0,0 +1,36 @@
+#ifndef STAN_MATH_PRIM_MAT_FUN_MATRIX_POWER_HPP
+#define STAN_MATH_PRIM_MAT_FUN_MATRIX_POWER_HPP
+
+#include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/prim/mat/err/check_square.hpp>
+#include <stan/math/prim/scal/err/invalid_argument.hpp>
+#include <vector>
+
+namespace stan {
+namespace math {
+/**
+ * Returns the nth power of the specific matrix.
+ *
+ * @param M A square matrix.
+ * @param n Exponent.
+ * @return nth power of M. M^n = M * ... * M.
+ * @throw std::invalid_argument if the exponent is negative or the matrix is not
+ * square.
+ */
+template <typename T>
+inline Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> matrix_power(
+    const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &M, const int n) {
+  check_square("matrix_power", "M", M);
+  if (n < 0)
+    invalid_argument("matrix_power", "n", n, "is ", ", but must be >= 0!");
+  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> result
+      = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Identity(M.rows(),
+                                                                   M.rows());
+  for (int i = 0; i < n; i++)
+    result *= M;
+  return result;
+}
+
+}  // namespace math
+}  // namespace stan
+#endif
---
+++ test/unit/math/prim/mat/fun/matrix_power_test.cpp
@@ -0,0 +1,77 @@
+#include <stan/math/prim/mat.hpp>
+#include <test/unit/math/prim/mat/fun/expect_matrix_eq.hpp>
+#include <test/unit/math/test_ad.hpp>
+#include <gtest/gtest.h>
+
+TEST(MathMatrixPower, matrix_power_two_by_two) {
+  using stan::math::matrix_power;
+
+  Eigen::MatrixXd I = Eigen::MatrixXd::Identity(2, 2);
+  Eigen::MatrixXd M(2, 2);
+  M << 1.0, 2.0, 3.0, 4.0;
+  Eigen::MatrixXd M2(2, 2);
+  M2 << 7.0, 10.0, 15.0, 22.0;
+  Eigen::MatrixXd M3(2, 2);
+  M3 << 37.0, 54.0, 81.0, 118.0;
+
+  expect_matrix_eq(I, matrix_power(M, 0));
+  expect_matrix_eq(M, matrix_power(M, 1));
+  expect_matrix_eq(M2, matrix_power(M, 2));
+  expect_matrix_eq(M3, matrix_power(M, 3));
+}
+
+TEST(MathMatrixPower, matrix_power_one_by_one) {
+  using stan::math::matrix_power;
+
+  Eigen::MatrixXd I(1, 1);
+  I << 1.0;
+  Eigen::MatrixXd M(1, 1);
+  M << 3.0;
+  Eigen::MatrixXd M2(1, 1);
+  M2 << 9.0;
+  Eigen::MatrixXd M3(1, 1);
+  M3 << 27.0;
+
+  expect_matrix_eq(I, matrix_power(M, 0));
+  expect_matrix_eq(M, matrix_power(M, 1));
+  expect_matrix_eq(M2, matrix_power(M, 2));
+  expect_matrix_eq(M3, matrix_power(M, 3));
+}
+
+TEST(MathMatrixPower, matrix_power_compare_to_simple_impl) {
+  using stan::math::matrix_power;
+
+  int size = 3;
+  Eigen::MatrixXd M = Eigen::MatrixXd::Random(size, size);
+  Eigen::MatrixXd expected = Eigen::MatrixXd::Identity(size, size);
+  int exponent = 3;
+  for (int i = 0; i < exponent; i++)
+    expected *= M;
+
+  expect_matrix_eq(expected, matrix_power(M, exponent));
+}
+
+TEST(MathMatrixPower, matrix_power_invalid) {
+  using stan::math::matrix_power;
+
+  Eigen::MatrixXd not_square = Eigen::MatrixXd::Identity(3, 4);
+  Eigen::MatrixXd good = Eigen::MatrixXd::Identity(2, 2);
+
+  EXPECT_THROW(matrix_power(not_square, 2), std::invalid_argument);
+  EXPECT_THROW(matrix_power(good, -2), std::invalid_argument);
+}
+
+TEST(mathMixCore, matrix_power) {
+  using stan::math::matrix_power;
+
+  auto f = [](const auto& x1) {
+             int size = 3;
+             typedef T = auto;
+             Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> M = Eigen::Matrix<auto, Eigen::Dynamic, Eigen::Dynamic>::Random(size, size);
+             M(1, 1) = x1;
+             int exponent = 3;
+             Eigen::Matrix<auto, Eigen::Dynamic, Eigen::Dynamic> result = matrix_power(M, exponent);
+             return result(1, 1);
+           };
+  stan::test::expect_common_unary(f);
+}
