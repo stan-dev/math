19f5cd33583e0c39ab61867e8ef4e48ad1ebecaa
Stan Jenkins <mc.stanislaw@gmail.com>
2019-09-12 17:30:42 -0400

    [Jenkins] auto-formatting by clang-format version 5.0.2-svn328729-1~exp1~20180509124008.99 (branches/release_50)

--- stan/math/prim/scal/prob/exp_mod_normal_lcdf.hpp
+++ stan/math/prim/scal/prob/exp_mod_normal_lcdf.hpp
@@ -20,8 +20,8 @@ inline auto exp_mod_normal_lcdf(const T_y& y, const T_loc& mu,
                                 const T_scale& sigma,
                                 const T_inv_scale& lambda) {
   static const char* function = "exp_mod_normal_lcdf";
-  using T_partials =  partials_return_t<T_y, T_loc, T_scale>;
-  using T_return =  return_type_t<T_y, T_loc, T_scale>;
+  using T_partials = partials_return_t<T_y, T_loc, T_scale>;
+  using T_return = return_type_t<T_y, T_loc, T_scale>;
 
   T_partials cdf_log(0.0);
   if (size_zero(y, mu, sigma, lambda)) {
