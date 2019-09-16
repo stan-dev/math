54c465402491e2d379305f3550d1505b6c17e75c
tadej <tadej.c@gmail.com>
2019-09-16 15:35:06 +0200

    split opencl implementation of normal_id_glm_lpdf into separate function

--- stan/math/opencl/normal_id_glm_lpdf.hpp
+++ stan/math/opencl/normal_id_glm_lpdf.hpp
@@ -1,5 +1,7 @@
-#ifndef STAN_MATH_PRIM_MAT_PROB_NORMAL_ID_GLM_LPDF_HPP
-#define STAN_MATH_PRIM_MAT_PROB_NORMAL_ID_GLM_LPDF_HPP
+#ifndef STAN_MATH_OPENCL_NORMAL_ID_GLM_LPDF_HPP
+#define STAN_MATH_OPENCL_NORMAL_ID_GLM_LPDF_HPP
+#ifdef STAN_OPENCL
+
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/scal/err/check_consistent_sizes.hpp>
@@ -10,11 +12,9 @@
 #include <stan/math/prim/scal/fun/sum.hpp>
 #include <stan/math/prim/arr/fun/value_of_rec.hpp>
 
-#ifdef STAN_OPENCL
 #include <stan/math/opencl/kernels/normal_id_glm_lpdf.hpp>
 #include <stan/math/opencl/matrix_cl.hpp>
 #include <stan/math/opencl/multiply.hpp>
-#endif
 
 #include <cmath>
 
@@ -51,14 +51,14 @@ namespace math {
  * @throw std::domain_error if the scale is not positive.
  * @throw std::invalid_argument if container sizes mismatch.
  */
-template <bool propto, typename T_y, typename T_x, typename T_alpha,
+template <bool propto, typename T_alpha,
           typename T_beta, typename T_scale>
-return_type_t<T_y, T_x, T_alpha, T_beta, T_scale> normal_id_glm_lpdf(
-    const T_y &y, const T_x &x, const T_alpha &alpha, const T_beta &beta,
+return_type_t<T_alpha, T_beta, T_scale> normal_id_glm_lpdf(
+    const matrix_cl<double> &y_cl, const matrix_cl<double> &x_cl, const T_alpha &alpha, const T_beta &beta,
     const T_scale &sigma) {
-  static const char *function = "normal_id_glm_lpdf";
+  static const char *function = "normal_id_glm_lpdf(OpenCL)";
   using T_partials_return
-      = partials_return_t<T_y, T_x, T_alpha, T_beta, T_scale>;
+      = partials_return_t<T_alpha, T_beta, T_scale>;
   using T_scale_val = typename std::conditional_t<
       is_vector<T_scale>::value,
       Eigen::Array<partials_return_t<T_scale>, -1, 1>,
@@ -69,91 +69,100 @@ return_type_t<T_y, T_x, T_alpha, T_beta, T_scale> normal_id_glm_lpdf(
   using Eigen::Matrix;
   using Eigen::VectorXd;
 
-  const size_t N = x.rows();
-  const size_t M = x.cols();
+  const size_t N = x_cl.rows();
+  const size_t M = x_cl.cols();
 
   check_positive_finite(function, "Scale vector", sigma);
-  check_consistent_size(function, "Vector of dependent variables", y, N);
+  check_size_match(function, "Rows of ", "x_cl", N, "rows of ", "y_cl", y_cl.rows());
   check_consistent_size(function, "Weight vector", beta, M);
   if (is_vector<T_scale>::value) {
-    check_consistent_sizes(function, "Vector of scale parameters", sigma,
-                           "Vector of dependent variables", y);
+    check_size_match(function, "Rows of ", "y_cl", N, "size of ", "sigma", length(sigma));
   }
   if (is_vector<T_alpha>::value) {
-    check_consistent_sizes(function, "Vector of intercepts", alpha,
-                           "Vector of dependent variables", y);
+    check_size_match(function, "Rows of ", "y_cl", N, "size of ", "alpha", length(alpha));
   }
-  if (size_zero(y, x, beta, sigma)) {
+
+  if (!include_summand<propto, T_alpha, T_beta, T_scale>::value) {
     return 0;
   }
 
-  if (!include_summand<propto, T_y, T_x, T_alpha, T_beta, T_scale>::value) {
-    return 0;
-  }
-
-  const auto &x_val = value_of_rec(x);
   const auto &beta_val = value_of_rec(beta);
   const auto &alpha_val = value_of_rec(alpha);
   const auto &sigma_val = value_of_rec(sigma);
-  const auto &y_val = value_of_rec(y);
 
   const auto &beta_val_vec = as_column_vector_or_scalar(beta_val);
   const auto &alpha_val_vec = as_column_vector_or_scalar(alpha_val);
   const auto &sigma_val_vec = as_column_vector_or_scalar(sigma_val);
-  const auto &y_val_vec = as_column_vector_or_scalar(y_val);
 
   T_scale_val inv_sigma = 1 / as_array_or_scalar(sigma_val_vec);
   Matrix<T_partials_return, Dynamic, 1> y_minus_mu_over_sigma_mat(N);
   auto y_scaled = y_minus_mu_over_sigma_mat.array();
 
-  double y_scaled_sq_sum;  // the most efficient way to
-  // calculate this depends on
-  // template parameters
+  const int local_size
+      = opencl_kernels::normal_id_glm.get_option("LOCAL_SIZE_");
+  const int wgs = (N + local_size - 1) / local_size;
 
-  y_scaled = x_val * beta_val_vec;
-  y_scaled = (as_array_or_scalar(y_val_vec) - y_scaled
-              - as_array_or_scalar(alpha_val_vec))
-             * inv_sigma;
+  matrix_cl<double> beta_cl(beta_val_vec);
+  matrix_cl<double> alpha_cl(alpha_val_vec);
+  matrix_cl<double> sigma_cl(sigma_val_vec);
 
-  operands_and_partials<T_y, T_x, T_alpha, T_beta, T_scale> ops_partials(
-      y, x, alpha, beta, sigma);
+  const bool need_mu_derivative
+      = !is_constant_all<T_beta, T_alpha>::value;
+  matrix_cl<double> mu_derivative_cl(need_mu_derivative ? N : 0, 1);
+  const bool need_mu_derivative_sum
+      = !is_constant_all<T_alpha>::value && !is_vector<T_alpha>::value;
+  matrix_cl<double> mu_derivative_sum_cl(need_mu_derivative_sum ? wgs : 0, 1);
+  matrix_cl<double> y_minus_mu_over_sigma_squared_sum_cl(wgs, 1);
+  const bool need_sigma_derivative
+      = !is_constant_all<T_scale>::value && is_vector<T_scale>::value;
+  matrix_cl<double> sigma_derivative_cl(need_sigma_derivative ? N : 0, 1);
+  const bool need_log_sigma_sum
+      = include_summand<propto, T_scale>::value && is_vector<T_scale>::value;
+  matrix_cl<double> log_sigma_sum_cl(need_log_sigma_sum ? wgs : 0, 1);
 
-  if (!(is_constant_all<T_y, T_x, T_beta, T_alpha>::value)) {
-    Matrix<T_partials_return, Dynamic, 1> mu_derivative = inv_sigma * y_scaled;
-    if (!is_constant_all<T_y>::value) {
-      ops_partials.edge1_.partials_ = -mu_derivative;
+  try {
+    opencl_kernels::normal_id_glm(
+        cl::NDRange(local_size * wgs), cl::NDRange(local_size),
+        mu_derivative_cl, mu_derivative_sum_cl,
+        y_minus_mu_over_sigma_squared_sum_cl, sigma_derivative_cl,
+        log_sigma_sum_cl, y_cl, x_cl, alpha_cl, beta_cl, sigma_cl, N, M,
+        length(alpha) != 1, length(sigma) != 1, need_mu_derivative,
+        need_mu_derivative_sum, need_sigma_derivative, need_log_sigma_sum);
+  } catch (const cl::Error &e) {
+    check_opencl_error(function, e);
+  }
+  double y_scaled_sq_sum = sum(from_matrix_cl(y_minus_mu_over_sigma_squared_sum_cl));
+
+  operands_and_partials<T_alpha, T_beta, T_scale> ops_partials(
+      alpha, beta, sigma);
+
+  if (!is_constant_all<T_alpha>::value && is_vector<T_alpha>::value) {
+    ops_partials.edge1_.partials_ = from_matrix_cl<Dynamic, 1>(mu_derivative_cl);
+  }
+  if (need_mu_derivative_sum) {
+    ops_partials.edge1_.partials_[0]
+        = sum(from_matrix_cl(mu_derivative_sum_cl));
+  }
+  if (!is_constant_all<T_beta>::value) {
+    const matrix_cl<double> mu_derivative_transpose_cl(
+        mu_derivative_cl.buffer(), 1,
+        mu_derivative_cl.rows());  // transposition of a vector can be done
+    // without copying
+    ops_partials.edge2_.partials_
+        = from_matrix_cl<1, Dynamic>(mu_derivative_transpose_cl * x_cl);
+  }
+  if (!is_constant_all<T_scale>::value) {
+    if (is_vector<T_scale>::value) {
+      ops_partials.edge3_.partials_
+          = from_matrix_cl<Dynamic, 1>(sigma_derivative_cl);
+    } else {
+      ops_partials.edge3_.partials_[0]
+          = (y_scaled_sq_sum - N) * as_scalar(inv_sigma);
     }
-    if (!is_constant_all<T_x>::value) {
-      ops_partials.edge2_.partials_
-          = (beta_val_vec * mu_derivative.transpose()).transpose();
-    }
-    if (!is_constant_all<T_beta>::value) {
-      ops_partials.edge4_.partials_ = mu_derivative.transpose() * x_val;
-    }
-    if (!is_constant_all<T_alpha>::value) {
-      if (is_vector<T_alpha>::value) {
-        ops_partials.edge3_.partials_ = mu_derivative;
-      } else {
-        ops_partials.edge3_.partials_[0] = sum(mu_derivative);
-      }
-    }
-    if (!is_constant_all<T_scale>::value) {
-      if (is_vector<T_scale>::value) {
-        Array<T_partials_return, Dynamic, 1> y_scaled_sq = y_scaled * y_scaled;
-        y_scaled_sq_sum = sum(y_scaled_sq);
-        ops_partials.edge5_.partials_ = (y_scaled_sq - 1) * inv_sigma;
-      } else {
-        y_scaled_sq_sum = sum(y_scaled * y_scaled);
-        ops_partials.edge5_.partials_[0]
-            = (y_scaled_sq_sum - N) * as_scalar(inv_sigma);
-      }
-    }
-  } else {
-    y_scaled_sq_sum = sum(y_scaled * y_scaled);
   }
 
   if (!std::isfinite(y_scaled_sq_sum)) {
-    check_finite(function, "Vector of dependent variables", y);
+    check_finite(function, "Vector of dependent variables", from_matrix_cl(y_cl));
     check_finite(function, "Weight vector", beta);
     check_finite(function, "Intercept", alpha);
     // if all other checks passed, next will only fail if x is not finite
@@ -167,24 +176,26 @@ return_type_t<T_y, T_x, T_alpha, T_beta, T_scale> normal_id_glm_lpdf(
   }
   if (include_summand<propto, T_scale>::value) {
     if (is_vector<T_scale>::value) {
-      logp -= sum(log(sigma_val_vec));
+      logp -= sum(from_matrix_cl(log_sigma_sum_cl));
     } else {
       logp -= N * log(as_scalar(sigma_val));
     }
   }
-  if (include_summand<propto, T_y, T_x, T_alpha, T_beta, T_scale>::value) {
+  if (include_summand<propto, T_alpha, T_beta, T_scale>::value) {
     logp -= 0.5 * y_scaled_sq_sum;
   }
   return ops_partials.build(logp);
 }
 
-template <typename T_y, typename T_x, typename T_alpha, typename T_beta,
-          typename T_scale>
-inline return_type_t<T_y, T_x, T_alpha, T_beta, T_scale> normal_id_glm_lpdf(
-    const T_y &y, const T_x &x, const T_alpha &alpha, const T_beta &beta,
+template <typename T_alpha, typename T_beta, typename T_scale>
+inline return_type_t<T_alpha, T_beta, T_scale> normal_id_glm_lpdf(
+    const matrix_cl<double> &y, const matrix_cl<double> &x, const T_alpha &alpha, const T_beta &beta,
     const T_scale &sigma) {
   return normal_id_glm_lpdf<false>(y, x, alpha, beta, sigma);
 }
-}  // namespace math
-}  // namespace stan
+
+}
+}
+
+#endif
 #endif
