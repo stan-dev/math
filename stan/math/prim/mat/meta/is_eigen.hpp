853c534c97f050ee6c93712de23230cb830ab1ac
stevo15025 <stevo15025@gmail.com>
2019-09-16 11:06:04 +0300

    clang format

--- stan/math/prim/mat/meta/is_eigen.hpp
+++ stan/math/prim/mat/meta/is_eigen.hpp
@@ -37,21 +37,24 @@ struct is_eigen<
     : std::true_type {};
 
 namespace internal {
-  template<typename... Ts> struct make_void { typedef void type;};
-  template<typename... Ts> using void_t = typename make_void<Ts...>::type;
+template <typename... Ts>
+struct make_void {
+  typedef void type;
+};
+template <typename... Ts>
+using void_t = typename make_void<Ts...>::type;
 
-  template<class, class = void>
-  struct is_eigen_matrix_impl : std::false_type
-  { };
-  template<class T>
-  struct is_eigen_matrix_impl<T, void_t<typename T::Matrix()>> : std::true_type
-  { };
+template <class, class = void>
+struct is_eigen_matrix_impl : std::false_type {};
+template <class T>
+struct is_eigen_matrix_impl<T, void_t<typename T::Matrix()>> : std::true_type {
+};
 
-  template<class T>
-  struct is_eigen_matrix_impl<T, void_t<typename T::SparseMatrix()>> : std::true_type
-  { };
+template <class T>
+struct is_eigen_matrix_impl<T, void_t<typename T::SparseMatrix()>>
+    : std::true_type {};
 
-}
+}  // namespace internal
 
 template <typename T>
 struct is_eigen_matrix : internal::is_eigen_matrix_impl<std::decay_t<T>> {};
