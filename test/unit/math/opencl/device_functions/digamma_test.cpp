3e402fae4e22476c824afe04fc1e996842de0b18
tadej <tadej.c@gmail.com>
2019-09-23 12:59:54 +0200

    added test for digamma device function

---
+++ test/unit/math/opencl/device_functions/digamma_test.cpp
@@ -0,0 +1,28 @@
+#ifdef STAN_OPENCL
+#include <stan/math.hpp>
+#include <stan/math/opencl/device_functions/digamma.hpp>
+#include <stan/math/opencl/kernel_cl.hpp>
+#include <gtest/gtest.h>
+#include <test/unit/math/expect_near_rel.hpp>
+
+static const std::string test_kernel_code = STRINGIFY(
+    __kernel void test(__global double *B, __global double *A) {
+      const int i = get_global_id(0);
+      B[i] = digamma(A[i]);
+    }
+);
+
+const stan::math::opencl_kernels::kernel_cl<stan::math::opencl_kernels::out_buffer, stan::math::opencl_kernels::in_buffer> digamma("test", {stan::math::opencl_kernels::digamma_device_function, test_kernel_code});
+
+TEST(MathMatrixCL, digamma) {
+  Eigen::VectorXd a = Eigen::VectorXd::Random(1000).array()*30;
+
+  stan::math::matrix_cl<double> a_cl(a);
+  stan::math::matrix_cl<double> res_cl(1000,1);
+  digamma(cl::NDRange(1000), res_cl, a_cl);
+  Eigen::VectorXd res = stan::math::from_matrix_cl<-1,1>(res_cl);
+
+  stan::test::expect_near_rel("digamma (OpenCL)", res, stan::math::digamma(a));
+}
+
+#endif
