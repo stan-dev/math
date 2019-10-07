a328ff4024f0a93270507c4f32c17641d6076c01
Bob Carpenter <carp@alias-i.com>
2019-10-07 17:36:17 -0400

    extra falling factorial test

--- test/unit/math/mix/scal/fun/falling_factorial_test.cpp
+++ test/unit/math/mix/scal/fun/falling_factorial_test.cpp
@@ -5,7 +5,8 @@ TEST(mathMixScalFun, fallingFactorial) {
     return
         [&](const auto& x1) { return stan::math::falling_factorial(x1, x2); };
   };
-  stan::test::expect_ad(f(2), -3.0);  // throws
+  stan::test::expect_ad(f(-2), -3.0);  // throws
+  stan::test::expect_ad(f(2), -3.0);   // throws
   stan::test::expect_ad(f(2), 4.0);
   stan::test::expect_ad(f(4), 4.0);
   stan::test::expect_ad(f(3), 5);
