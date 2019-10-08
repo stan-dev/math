d6ffb2885492672786ef2f389c52f6eb144081bc
Bob Carpenter <carp@alias-i.com>
2019-10-08 15:28:56 -0400

    fix boundary of binary log loss test

--- test/unit/math/mix/scal/fun/binary_log_loss_test.cpp
+++ test/unit/math/mix/scal/fun/binary_log_loss_test.cpp
@@ -7,9 +7,8 @@ TEST(mathMixScalFun, binaryLogLoss) {
     return [=](const auto& x2) { return stan::math::binary_log_loss(x1, x2); };
   };
   for (int y = 0; y <= 1; ++y) {
-    for (double y_hat = 0; y_hat <= 1.0; y_hat += 0.1) {
+    stan::test::expect_ad(f(y), std::numeric_limits<double>::quiet_NaN());
+    for (double y_hat = 0.05; y_hat < 1.0; y_hat += 0.05)
       stan::test::expect_ad(f(y), y_hat);
-      stan::test::expect_ad(f(y), std::numeric_limits<double>::quiet_NaN());
-    }
   }
 }
