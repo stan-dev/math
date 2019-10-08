945619b47c7cf37538da970fc29d6a2f9f102c42
Bob Carpenter <carp@alias-i.com>
2019-10-08 14:45:58 -0400

    scalar squared_distance to ad test framework

---
+++ test/unit/math/mix/scal/fun/squared_distance_test.cpp
@@ -0,0 +1,16 @@
+#include <test/unit/math/test_ad.hpp>
+
+TEST(mathMixScalFun, squaredDistance) {
+  auto f = [](const auto& x1, const auto& x2) {
+    return stan::math::squared_distance(x1, x2);
+  };
+  stan::test::expect_ad(f, 1.0, 1.0);
+  stan::test::expect_ad(f, 1.0, 4.0);
+  stan::test::expect_ad(f, 4.0, 1.0);
+  stan::test::expect_ad(f, 4.0, 4.0);
+
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  stan::test::expect_ad(f, 1.0, nan);
+  stan::test::expect_ad(f, nan, 1.0);
+  stan::test::expect_ad(f, nan, nan);
+}
