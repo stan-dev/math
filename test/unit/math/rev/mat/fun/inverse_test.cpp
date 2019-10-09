1d860dc0c95d5527218b0b4258afa91e0bac1195
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2019-10-09 10:36:42 +0300

    Added additional test for rev mat inverse.
    Added tests for 0x0, 1x1 input. Input containing NaN, Inf. Copied and modified test_ad_unary from test_ad_test.cpp.

--- test/unit/math/rev/mat/fun/inverse_test.cpp
+++ test/unit/math/rev/mat/fun/inverse_test.cpp
@@ -1,7 +1,9 @@
 #include <stan/math/rev/mat.hpp>
 #include <gtest/gtest.h>
+#include <test/unit/math/test_ad.hpp>
 #include <test/unit/math/rev/mat/fun/util.hpp>
 #include <test/unit/math/rev/mat/util.hpp>
+#include <limits>
 
 TEST(AgradRevMatrix, inverse_val) {
   using stan::math::inverse;
@@ -79,4 +81,85 @@ TEST(AgradRevMatrix, check_varis_on_stack) {
   a << 2.0, 3.0, 5.0, 7.0;
 
   test::check_varis_on_stack(stan::math::inverse(a));
+}
+
+TEST(AgradRevMatrix, infinite_input_inverse) {
+  using stan::math::inverse;
+  using stan::math::matrix_v;
+
+  matrix_v a(2, 2);
+  a << 2.0, 3.0, 5.0, std::numeric_limits<double>::infinity();
+
+  // inf input is allowed
+  EXPECT_NO_THROW(inverse(a));
+
+  // a_inv contains nan
+  matrix_v a_inv = inverse(a);
+  EXPECT_THROW(stan::math::multiply(a, a_inv), std::domain_error);
+}
+
+TEST(AgradRevMatrix, nan_input_inverse) {
+  using stan::math::inverse;
+  using stan::math::matrix_v;
+
+  matrix_v a(2, 2);
+  a << 2.0, 3.0, 5.0, std::numeric_limits<double>::quiet_NaN();
+
+  // nan input is allowed
+  EXPECT_NO_THROW(inverse(a));
+
+  // a_inv contains nan
+  matrix_v a_inv = inverse(a);
+  EXPECT_THROW(stan::math::multiply(a, a_inv), std::domain_error);
+}
+
+TEST(AgradRevMatrix, exception_mat_inverse) {
+  using stan::math::inverse;
+  using stan::math::matrix_v;
+
+  matrix_v a;
+
+  // singular matrix, inverse does not exist.
+  // Eigen does not throw errors, but returns inf
+  a.resize(2, 2);
+  a << 6.0, 4.0, 3.0, 2.0;
+  EXPECT_NO_THROW(stan::math::inverse(a));
+
+  // zero size
+  a.resize(0, 0);
+  EXPECT_THROW(stan::math::inverse(a), std::invalid_argument);
+
+  // not square
+  a.resize(2, 3);
+  EXPECT_THROW(stan::math::inverse(a), std::invalid_argument);
+}
+
+TEST(AgradRevMatrix, inverse_1x1) {
+  using stan::math::inverse;
+  using stan::math::matrix_v;
+
+  matrix_v a;
+  a.resize(1, 1);
+  a << 2.0;
+
+  matrix_v a_inv = inverse(a);
+  EXPECT_NEAR(0.5, a_inv(0, 0).val(), 1.0E-12);
+}
+
+TEST(AgradRevMatrix, inverse_ad) {
+  Eigen::MatrixXd x(2, 2);
+  x << 1.9, 0.3, 0.3, 1.7;
+
+  auto g = [](const auto& u) { return stan::math::inverse(u); };
+  stan::test::expect_ad(g, x);
+
+  x << 1.9, 0.3, 0.3, std::numeric_limits<double>::infinity();
+  stan::test::expect_ad(g, x);
+
+  x << 1.9, 0.3, 0.3, std::numeric_limits<double>::quiet_NaN();
+  EXPECT_THROW(stan::test::expect_ad(g, x), std::domain_error);
+
+  x.resize(1, 1);
+  x << 2.0;
+  stan::test::expect_ad(g, x);
 }
