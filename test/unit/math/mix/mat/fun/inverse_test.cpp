46e1a6d967331677d017851762163b58b9d36fb7
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2019-10-10 16:47:00 +0300

    Updated inverse tests with Bob's suggestions
    https://github.com/stan-dev/math/pull/1334#issuecomment-540231245

--- test/unit/math/mix/mat/fun/inverse_test.cpp
+++ test/unit/math/mix/mat/fun/inverse_test.cpp
@@ -2,7 +2,19 @@
 #include <limits>
 
 TEST(mathMixMatFun, inverse) {
-  auto f = [](const auto& x) { return stan::math::inverse(x); };
+  auto f = [](const auto& x) {
+    return stan::math::inverse(x);
+  };
+
+  Eigen::Matrix<stan::math::var, -1, -1> t(0, 0);
+  EXPECT_THROW(stan::math::inverse(t), std::invalid_argument);
+
+  // fails with assertion from Eigen
+  // stan::test::expect_ad(f, t);
+
+  // fails with assertion from Eigen
+  // Eigen::MatrixXd t2(0, 0);
+  // EXPECT_THROW(stan::math::inverse(t2), std::invalid_argument);
 
   Eigen::MatrixXd u(1, 1);
   u << 2;
@@ -10,6 +22,8 @@ TEST(mathMixMatFun, inverse) {
 
   Eigen::MatrixXd v(2, 2);
   v << 2, 3, 5, 7;
+  stan::test::expect_ad(f, v);
+  v << 1.9, 0.3, 0.3, 1.7;
   stan::test::expect_ad(f, v);
 
   // issues around zero require looser tolerances for hessians
@@ -21,11 +35,34 @@ TEST(mathMixMatFun, inverse) {
   w << 2, 3, 5, 7, 11, 13, 17, 19, 23;
   stan::test::expect_ad(tols, f, w);
 
-  // even worse here in some cases
+  // even lower tolerance, again for cases around zero
   stan::test::ad_tolerances tols2;
   tols2.hessian_hessian_ = 3.0;
   tols2.hessian_fvar_hessian_ = 3.0;
+
   Eigen::MatrixXd x(4, 4);
   x << 2, 3, 4, 5, 9, -1, 2, 2, 4, 3, 7, -1, 0, 1, 19, 112;
   stan::test::expect_ad(tols2, f, x);
+
+  Eigen::MatrixXd y(3, 2);
+  y << 1, 2, 3, 4, 5, 6;
+  EXPECT_THROW(stan::math::inverse(y), std::invalid_argument);
+  stan::test::expect_ad(f, y);
+
+  Eigen::MatrixXd z(2, 2);
+  z << 1, 2, 5, std::numeric_limits<double>::quiet_NaN();
+  EXPECT_NO_THROW(stan::math::inverse(z));
+
+  // autodiff throws, so following fails (throw behavior must match to pass)
+  // stan::test::expect_ad(f, z);
+
+  Eigen::MatrixXd a(2, 2);
+  a << 1.9, 0.3, 0.3, std::numeric_limits<double>::infinity();
+  stan::test::expect_ad(f, a);
+
+  // singular matrix, inverse does not exist.
+  // Eigen does not throw errors, but returns inf
+  Eigen::MatrixXd m(2, 2);
+  m << 6.0, 4.0, 3.0, 2.0;
+  EXPECT_NO_THROW(stan::math::inverse(m));
 }
