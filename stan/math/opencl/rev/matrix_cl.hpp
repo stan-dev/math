289d4fc017d11cc4e07f97f7843df56d854f216d
Steve Bronder <stevo15025@gmail.com>
2019-10-14 23:33:00 -0400

    Have OpenCL backend understand concept of a var

---
+++ stan/math/opencl/rev/matrix_cl.hpp
@@ -0,0 +1,125 @@
+#ifndef STAN_MATH_OPENCL_REV_MATRIX_CL_HPP
+#define STAN_MATH_OPENCL_REV_MATRIX_CL_HPP
+#ifdef STAN_OPENCL
+#include <stan/math/prim/mat/fun/Eigen.hpp>
+#include <stan/math/rev/mat.hpp>
+#include <stan/math/opencl/matrix_cl.hpp>
+#include <stan/math/prim/mat/fun/typedefs.hpp>
+#include <stan/math/prim/scal/err/check_size_match.hpp>
+#include <stan/math/prim/scal/err/domain_error.hpp>
+#include <stan/math/prim/arr/fun/vec_concat.hpp>
+#include <stan/math/rev/core.hpp>
+#include <CL/cl.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+namespace stan {
+namespace math {
+
+template <typename T>
+class matrix_cl<T, require_var_t<T>> {
+ private:
+  /**
+   * cl::Buffer provides functionality for working with the OpenCL buffer.
+   * An OpenCL buffer allocates the memory in the device that
+   * is provided by the context.
+   */
+  const int rows_;
+  const int cols_;
+  mutable matrix_cl<double> val_;
+  mutable matrix_cl<double> adj_;
+  matrix_cl_view view_{matrix_cl_view::Entire};
+ public:
+   using Scalar = T;
+   using type = T;
+  int rows() const { return rows_; }
+
+  int cols() const { return cols_; }
+
+  int size() const { return rows_ * cols_; }
+
+  matrix_cl<double>& val() const {return val_;}
+  matrix_cl<double>& adj() const {return adj_;}
+  explicit matrix_cl() : rows_(0), cols_(0) {}
+
+  // Forward declare the methods that work in place on the matrix
+  template <matrix_cl_view matrix_view = matrix_cl_view::Entire>
+  void zeros();
+  template <matrix_cl_view matrix_view = matrix_cl_view::Entire>
+  void zeros_strict_tri();
+  template <TriangularMapCL triangular_map = TriangularMapCL::LowerToUpper>
+  void triangular_transpose();
+
+  void sub_block(const matrix_cl<T, require_var_t<T>>& A, size_t A_i,
+                 size_t A_j, size_t this_i, size_t this_j, size_t nrows,
+                 size_t ncols);
+
+   const matrix_cl_view& view() const { return view_; }
+
+   void view(const matrix_cl_view& view) { view_ = view; }
+
+  template <typename Mat, require_eigen_st<is_var, Mat>...>
+  explicit matrix_cl(Mat&& A, matrix_cl_view partial_view = matrix_cl_view::Entire)
+      : rows_(A.rows()), cols_(A.cols()),
+      val_(A.val().eval(), partial_view),
+      adj_(A.adj().eval(), partial_view), view_(partial_view) {}
+
+  explicit matrix_cl(const matrix_vi& A, matrix_cl_view partial_view = matrix_cl_view::Entire)
+      : rows_(A.rows()), cols_(A.cols()),
+      val_(A.val().eval(), partial_view),
+      adj_(A.adj().eval(), partial_view), view_(partial_view) {}
+
+  explicit matrix_cl(vari** A, const int& R, const int& C, matrix_cl_view partial_view = matrix_cl_view::Entire) :
+    rows_(R), cols_(C), view_(partial_view),
+    val_(R, C, partial_view), adj_(R, C, partial_view) {
+    cl::Context& ctx = opencl_context.context();
+    cl::CommandQueue& queue = opencl_context.queue();
+    cl::Event transfer_event1;
+    cl::Event transfer_event2;
+    if (view_ == matrix_cl_view::Entire) {
+      const matrix_vi foo = Eigen::Map<const matrix_vi>(A, R, C);
+      val_.buffer() = cl::Buffer(ctx, CL_MEM_READ_WRITE, sizeof(double) * R * C);
+      adj_.buffer() = cl::Buffer(ctx, CL_MEM_READ_WRITE, sizeof(double) * R * C);
+      queue.enqueueWriteBuffer(val_.buffer(), CL_FALSE, 0,
+                               sizeof(double) * R * C,
+                               foo.val().eval().data(), NULL,
+                               &transfer_event1);
+      queue.enqueueWriteBuffer(adj_.buffer(), CL_FALSE, 0,
+                              sizeof(double) * R * C,
+                              foo.adj().eval().data(), NULL,
+                              &transfer_event2);
+    } else {
+      const int packed_size = R * (R + 1) / 2;
+      val_.buffer() = cl::Buffer(ctx, CL_MEM_READ_WRITE, sizeof(double) * packed_size);
+      adj_.buffer() = cl::Buffer(ctx, CL_MEM_READ_WRITE, sizeof(double) * packed_size);
+      const matrix_vi foo = Eigen::Map<const matrix_vi>(A, 1, packed_size);
+      queue.enqueueWriteBuffer(val_.buffer(), CL_FALSE, 0,
+                               sizeof(double) * packed_size,
+                               foo.val().eval().data(), NULL,
+                               &transfer_event1);
+      queue.enqueueWriteBuffer(adj_.buffer(), CL_FALSE, 0,
+                              sizeof(double) * packed_size,
+                              foo.adj().eval().data(), NULL,
+                              &transfer_event2);
+    }
+    val_.add_write_event(transfer_event1);
+    adj_.add_write_event(transfer_event2);
+  }
+
+  explicit matrix_cl(const int& rows, const int& cols, matrix_cl_view partial_view = matrix_cl_view::Entire) :
+  rows_(rows), cols_(cols), val_(rows, cols), adj_(rows, cols), view_(partial_view) {}
+
+  matrix_cl<var> operator=(const matrix_cl<var>& A) {
+    val_ = A.val();
+    adj_ = A.adj();
+    return *this;
+  }
+};
+
+}
+}
+
+#endif
+#endif
---
+++ stan/math/opencl/rev/sub_block.hpp
@@ -0,0 +1,28 @@
+#ifndef STAN_MATH_OPENCL_REV_SUB_BLOCK_HPP
+#define STAN_MATH_OPENCL_REV_SUB_BLOCK_HPP
+#ifdef STAN_OPENCL
+
+#include <stan/math/opencl/opencl_context.hpp>
+#include <stan/math/opencl/kernels/sub_block.hpp>
+#include <stan/math/prim/scal/err/domain_error.hpp>
+#include <stan/math/opencl/rev/matrix_cl.hpp>
+#include <CL/cl.hpp>
+#include <vector>
+
+namespace stan {
+namespace math {
+  template <typename T>
+  inline void matrix_cl<T, require_var_t<T>>::sub_block(const matrix_cl<T, require_var_t<T>>& A, size_t A_i,
+                   size_t A_j, size_t this_i, size_t this_j, size_t nrows,
+                   size_t ncols) try {
+    this->val().sub_block(A.val(), A_i, A_j, this_i, this_j, nrows, ncols);
+    this->adj().sub_block(A.adj(), A_i, A_j, this_i, this_j, nrows, ncols);
+  } catch (const cl::Error& e) {
+   check_opencl_error("copy_submatrix", e);
+  }
+
+  }  // namespace math
+  }  // namespace stan
+
+  #endif
+  #endif
