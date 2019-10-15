99bacea8e9fa68712d01222317a606f6a51b32c2
Sebastian Weber <sdw.post@waebers.de>
2019-10-15 19:29:44 +0200

    add testing for repeated init within thread of stack

--- test/unit/math/rev/core/thread_stack_instance_test.cpp
+++ test/unit/math/rev/core/thread_stack_instance_test.cpp
@@ -30,6 +30,54 @@ TEST(thread_stack_instance, initialize) {
   other_work.join();
 }
 
+// tear down in a thread the stack and refresh it
+TEST(thread_stack_instance, repeated_initialize) {
+  using stan::math::ChainableStack;
+
+  // the main thread must be initialized by the time this code is
+  // reached. This will actually segfault if this is not the case.
+  // The pointer to the reference returned must evaluate to true.
+  EXPECT_TRUE(ChainableStack::instance_);
+
+  ChainableStack::AutodiffStackStorage* main_ad_stack
+      = ChainableStack::instance_;
+
+  auto thread_tester_with_refresh = [&]() -> void {
+    ChainableStack* thread_instance_ptr = new ChainableStack();
+    EXPECT_TRUE(ChainableStack::instance_);
+    EXPECT_TRUE(ChainableStack::instance_
+#ifdef STAN_THREADS
+                !=
+#else
+                ==
+#endif
+                main_ad_stack);
+
+    delete thread_instance_ptr;
+#ifdef STAN_THREADS
+    // if STAN_THREADS is set, then the stack instance is now
+    // cleaned
+    EXPECT_FALSE(ChainableStack::instance_);
+#else
+    // if STAN_THREADS is not set, then thread_instance_ptr does
+    // not own the stack and as such there is no tear down
+    EXPECT_TRUE(ChainableStack::instance_);
+#endif
+    thread_instance_ptr = new ChainableStack();
+    EXPECT_TRUE(ChainableStack::instance_);
+    EXPECT_TRUE(ChainableStack::instance_
+#ifdef STAN_THREADS
+                !=
+#else
+                ==
+#endif
+                main_ad_stack);
+  };
+  std::thread other_work(thread_tester_with_refresh);
+
+  other_work.join();
+}
+
 TEST(thread_stack_instance, child_instances) {
   using stan::math::ChainableStack;
   // place a var on the stack such that a fresh stack in another
