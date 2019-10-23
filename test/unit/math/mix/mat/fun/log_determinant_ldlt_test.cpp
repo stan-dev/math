aee9a4827dbc65a9f4e8dab842774059cabd5504
Bob Carpenter <carp@alias-i.com>
2019-10-23 10:11:26 -0400

    log_determinant_ldlt to ad test framework

--- test/unit/math/mix/mat/fun/log_determinant_ldlt_test.cpp
+++ test/unit/math/mix/mat/fun/log_determinant_ldlt_test.cpp
@@ -1,185 +1,81 @@
-#include <stan/math/mix/mat.hpp>
-#include <gtest/gtest.h>
+#include <test/unit/math/test_ad.hpp>
+// non-framework tests require following includes
 #include <test/unit/math/rev/mat/fun/util.hpp>
+#include <test/unit/math/rev/mat/util.hpp>
+#include <vector>
 
-TEST(AgradMixMatrixLogDeterminantLDLT, fv_1stDeriv) {
-  using stan::math::fvar;
-  using stan::math::log_determinant_ldlt;
-  using stan::math::matrix_fv;
-  using stan::math::var;
-
-  fvar<var> a(3.0, 1.0);
-  fvar<var> b(0.0, 2.0);
-  fvar<var> c(0.0, 2.0);
-  fvar<var> d(4.0, 2.0);
-
-  matrix_fv v(2, 2);
-  v << a, b, c, d;
-
-  stan::math::LDLT_factor<fvar<var>, -1, -1> ldlt_v;
-  ldlt_v.compute(v);
-
-  fvar<var> det;
-  det = log_determinant_ldlt(ldlt_v);
-  EXPECT_FLOAT_EQ(std::log(12.0), det.val_.val());
-  EXPECT_FLOAT_EQ(0.83333333, det.d_.val());
-
-  AVEC q = createAVEC(a.val(), b.val(), c.val(), d.val());
-  VEC h;
-  det.val_.grad(q, h);
-  EXPECT_FLOAT_EQ(0.33333333, h[0]);
-  EXPECT_FLOAT_EQ(0, h[1]);
-  EXPECT_FLOAT_EQ(0, h[2]);
-  EXPECT_FLOAT_EQ(0.25, h[3]);
+template <typename T, int R, int C>
+stan::math::LDLT_factor<T, R, C> ldlt_factor(const Eigen::Matrix<T, R, C>& x) {
+  stan::math::LDLT_factor<T, R, C> ldlt;
+  ldlt.compute(x);
+  return ldlt;
 }
-TEST(AgradMixMatrixLogDeterminantLDLT, fv_2ndDeriv) {
-  using stan::math::fvar;
-  using stan::math::log_determinant_ldlt;
-  using stan::math::matrix_fv;
-  using stan::math::var;
 
-  fvar<var> a(3.0, 1.0);
-  fvar<var> b(0.0, 2.0);
-  fvar<var> c(0.0, 2.0);
-  fvar<var> d(4.0, 2.0);
-  matrix_fv v(2, 2);
-  v << a, b, c, d;
+TEST(MathMixMatFun, logDeterminantLdlt) {
+  auto f = [](const auto& x) {
+    auto z = ((x + x.transpose()) * 0.5).eval();
+    auto y = ldlt_factor(x);
+    return stan::math::log_determinant_ldlt(y);
+  };
 
-  stan::math::LDLT_factor<fvar<var>, -1, -1> ldlt_v;
-  ldlt_v.compute(v);
+  Eigen::MatrixXd a(2, 2);
+  a << 3, 0, 0, 4;
+  stan::test::expect_ad(f, a);
 
-  fvar<var> det;
-  det = log_determinant_ldlt(ldlt_v);
+  Eigen::MatrixXd c(2, 2);
+  c << 1, 0, 0, 3;
+  stan::test::expect_ad(f, c);
 
-  AVEC q = createAVEC(a.val(), b.val(), c.val(), d.val());
-  VEC h;
-  det.d_.grad(q, h);
-  EXPECT_FLOAT_EQ(-0.11111111, h[0]);
-  EXPECT_FLOAT_EQ(0, h[1]);
-  EXPECT_FLOAT_EQ(-0.33333333, h[2]);
-  EXPECT_FLOAT_EQ(-0.125, h[3]);
+  for (const auto& rho : std::vector<double>{0, 0.9}) {
+    for (int k = 1; k < 4; ++k) {
+      Eigen::MatrixXd y(k, k);
+      for (int i = 0; i < k; ++i) {
+        y(i, i) = 0;
+        for (int j = 0; j < i; ++j) {
+          y(i, j) = std::pow(rho, std::fabs(i - j));
+          y(j, i) = y(i, j);
+        }
+      }
+      stan::test::expect_ad(f, y);
+    }
+  }
+
+  // TODO(carpenter): figure out why this example requires loose tolerance
+  // original derivative test for this case is included below
+  stan::test::ad_tolerances tols;
+  tols.gradient_grad_ = 1;  // default is tiny
+
+  Eigen::MatrixXd b(2, 2);
+  b << 2, 1, 1, 3;
+  stan::test::expect_ad(tols, f, b);
 }
-TEST(AgradMixMatrixLogDeterminantLDLT, ffv_1stDeriv) {
-  using stan::math::fvar;
-  using stan::math::log_determinant_ldlt;
-  using stan::math::matrix_ffv;
-  using stan::math::var;
 
-  fvar<fvar<var> > a(3.0, 1.0);
-  fvar<fvar<var> > b(0.0, 2.0);
-  fvar<fvar<var> > c(0.0, 2.0);
-  fvar<fvar<var> > d(4.0, 2.0);
+TEST(MathMixMatFun, logDeterminantLdltSpecial) {
+  using stan::math::determinant;
+  using stan::math::fabs;
+  using stan::math::log;
+  using stan::math::matrix_v;
 
-  matrix_ffv v(2, 2);
-  v << a, b, c, d;
+  // expected from auto-diff/Eigen
+  AVEC x1 = createAVEC(2, 1, 1, 3);
+  matrix_v v1(2, 2);
+  v1 << x1[0], x1[1], x1[2], x1[3];
+  AVAR det1 = log(fabs(v1.determinant()));
+  std::vector<double> g1;
+  det1.grad(x1, g1);
 
-  stan::math::LDLT_factor<fvar<fvar<var> >, -1, -1> ldlt_v;
-  ldlt_v.compute(v);
+  stan::math::LDLT_factor<stan::math::var, -1, -1> ldlt_v;
+  AVEC x2 = createAVEC(2, 1, 1, 3);
+  matrix_v v2(2, 2);
+  v2 << x2[0], x2[1], x2[2], x2[3];
+  ldlt_v.compute(v2);
+  ASSERT_TRUE(ldlt_v.success());
+  AVAR det2 = log_determinant_ldlt(ldlt_v);
+  std::vector<double> g2;
+  det2.grad(x2, g2);
 
-  fvar<fvar<var> > det;
-  det = log_determinant_ldlt(ldlt_v);
-  EXPECT_FLOAT_EQ(std::log(12.0), det.val_.val().val());
-  EXPECT_FLOAT_EQ(0.83333333, det.d_.val().val());
-
-  AVEC q
-      = createAVEC(a.val().val(), b.val().val(), c.val().val(), d.val().val());
-  VEC h;
-  det.val_.val().grad(q, h);
-  EXPECT_FLOAT_EQ(0.33333333, h[0]);
-  EXPECT_FLOAT_EQ(0, h[1]);
-  EXPECT_FLOAT_EQ(0, h[2]);
-  EXPECT_FLOAT_EQ(0.25, h[3]);
-}
-TEST(AgradMixMatrixLogDeterminantLDLT, ffv_2ndDeriv_1) {
-  using stan::math::fvar;
-  using stan::math::log_determinant_ldlt;
-  using stan::math::matrix_ffv;
-  using stan::math::var;
-
-  fvar<fvar<var> > a(3.0, 1.0);
-  fvar<fvar<var> > b(0.0, 2.0);
-  fvar<fvar<var> > c(0.0, 2.0);
-  fvar<fvar<var> > d(4.0, 2.0);
-  a.val_.d_ = 1.0;
-  b.val_.d_ = 1.0;
-  c.val_.d_ = 1.0;
-  d.val_.d_ = 1.0;
-  matrix_ffv v(2, 2);
-  v << a, b, c, d;
-
-  stan::math::LDLT_factor<fvar<fvar<var> >, -1, -1> ldlt_v;
-  ldlt_v.compute(v);
-
-  fvar<fvar<var> > det;
-  det = log_determinant_ldlt(ldlt_v);
-
-  AVEC q
-      = createAVEC(a.val().val(), b.val().val(), c.val().val(), d.val().val());
-  VEC h;
-  det.val().d_.grad(q, h);
-  EXPECT_FLOAT_EQ(-0.11111111, h[0]);
-  EXPECT_FLOAT_EQ(0, h[1]);
-  EXPECT_FLOAT_EQ(-0.16666667, h[2]);
-  EXPECT_FLOAT_EQ(-0.0625, h[3]);
-}
-TEST(AgradMixMatrixLogDeterminantLDLT, ffv_2ndDeriv_2) {
-  using stan::math::fvar;
-  using stan::math::log_determinant_ldlt;
-  using stan::math::matrix_ffv;
-  using stan::math::var;
-
-  fvar<fvar<var> > a(3.0, 1.0);
-  fvar<fvar<var> > b(0.0, 2.0);
-  fvar<fvar<var> > c(0.0, 2.0);
-  fvar<fvar<var> > d(4.0, 2.0);
-  matrix_ffv v(2, 2);
-  v << a, b, c, d;
-
-  stan::math::LDLT_factor<fvar<fvar<var> >, -1, -1> ldlt_v;
-  ldlt_v.compute(v);
-
-  fvar<fvar<var> > det;
-  det = log_determinant_ldlt(ldlt_v);
-
-  AVEC q
-      = createAVEC(a.val().val(), b.val().val(), c.val().val(), d.val().val());
-  VEC h;
-  det.d_.val().grad(q, h);
-  EXPECT_FLOAT_EQ(-0.11111111, h[0]);
-  EXPECT_FLOAT_EQ(0, h[1]);
-  EXPECT_FLOAT_EQ(-0.33333333, h[2]);
-  EXPECT_FLOAT_EQ(-.125, h[3]);
-}
-TEST(AgradMixMatrixLogDeterminantLDLT, ffv_3rdDeriv) {
-  using stan::math::fvar;
-  using stan::math::log_determinant_ldlt;
-  using stan::math::matrix_ffv;
-  using stan::math::var;
-
-  fvar<fvar<var> > a(3.0, 1.0);
-  fvar<fvar<var> > b(0.0, 2.0);
-  fvar<fvar<var> > c(0.0, 2.0);
-  fvar<fvar<var> > d(4.0, 2.0);
-  a.val_.d_ = 1.0;
-  b.val_.d_ = 1.0;
-  c.val_.d_ = 1.0;
-  d.val_.d_ = 1.0;
-
-  matrix_ffv v(2, 2);
-  v << a, b, c, d;
-
-  stan::math::LDLT_factor<fvar<fvar<var> >, -1, -1> ldlt_v;
-  ldlt_v.compute(v);
-
-  fvar<fvar<var> > det;
-  det = log_determinant_ldlt(ldlt_v);
-
-  AVEC q
-      = createAVEC(a.val().val(), b.val().val(), c.val().val(), d.val().val());
-  VEC h;
-  det.d_.d_.grad(q, h);
-  EXPECT_FLOAT_EQ(0.18518518, h[0]);
-  EXPECT_FLOAT_EQ(0, h[1]);
-  EXPECT_FLOAT_EQ(0.33333333, h[2]);
-  EXPECT_FLOAT_EQ(0.14583333, h[3]);
+  EXPECT_FLOAT_EQ(det1.val(), det2.val());
+  EXPECT_EQ(g1.size(), g2.size());
+  for (size_t i = 0; i < g1.size(); ++i)
+    EXPECT_FLOAT_EQ(g1[i], g2[i]);
 }
