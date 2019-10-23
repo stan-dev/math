969d8b8bd94676e25c577f2e3c890a7d46943c02
Bob Carpenter <carp@alias-i.com>
2019-10-23 13:31:45 -0400

    log_softmax to ad test framework

--- test/unit/math/mix/mat/fun/log_softmax_test.cpp
+++ test/unit/math/mix/mat/fun/log_softmax_test.cpp
@@ -1,3 +1,30 @@
+#include <test/unit/math/test_ad.hpp>
+
+TEST(MathMixMatFun, logSoftmax) {
+  auto f = [](const auto& x) { return stan::math::log_softmax(x); };
+  Eigen::VectorXd x0(0);  // error case
+  stan::test::expect_ad(f, x0);
+
+  Eigen::VectorXd x1(1);
+  x1 << 0;
+  stan::test::expect_ad(f, x1);
+
+  Eigen::VectorXd x2(2);
+  x2 << -1, 1;
+  stan::test::expect_ad(f, x2);
+
+  Eigen::VectorXd x3(3);
+  x3 << -1, 1, 10;
+  stan::test::expect_ad(f, x3);
+
+  Eigen::VectorXd x3b(3);
+  x3 << 0, 1, 2;
+  stan::test::expect_ad(f, x3);
+
+  Eigen::VectorXd x3c(3);
+  x3 << 2, 1, 1;
+  stan::test::expect_ad(f, x3);
+}
 #include <stan/math/mix/mat.hpp>
 #include <gtest/gtest.h>
 #include <test/unit/math/rev/mat/fun/util.hpp>
