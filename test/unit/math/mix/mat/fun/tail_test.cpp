bdde554936dfab8767bcb1970a051f3adb75545d
Bob Carpenter <carp@alias-i.com>
2019-10-27 01:15:25 -0400

    tail to ad test framework

--- test/unit/math/mix/mat/fun/tail_test.cpp
+++ test/unit/math/mix/mat/fun/tail_test.cpp
@@ -1,200 +1,30 @@
-#include <stan/math/mix/mat.hpp>
-#include <gtest/gtest.h>
-#include <stdexcept>
-#include <vector>
+#include <test/unit/math/test_ad.hpp>
 
-using stan::math::fvar;
-using stan::math::var;
-
-TEST(AgradMixMatrixTail, TailVector1_fv) {
-  using stan::math::tail;
-  stan::math::vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, tail(v, 0).size());
-}
-TEST(AgradMixMatrixTail, TailVector2_fv) {
-  using stan::math::tail;
-  stan::math::vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, tail(v, 3).size());
-}
-TEST(AgradMixMatrixTail, TailVector3_fv) {
-  using stan::math::tail;
-  stan::math::vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(tail(v, 4), std::out_of_range);
-}
-TEST(AgradMixMatrixTail, TailVector4_fv) {
-  using stan::math::tail;
-  stan::math::vector_fv v(3);
-  v << 1, 2, 3;
-
-  stan::math::vector_fv v12 = tail(v, 2);
-  EXPECT_EQ(2, v12.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n + 1].val_.val(), v12[n].val_.val());
-  }
+auto f(int i) {
+  return [=](const auto& y) { return stan::math::tail(y, i); };
 }
 
-TEST(AgradMixMatrixTail, tailRowVector1_fv) {
-  using stan::math::tail;
-  stan::math::row_vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, tail(v, 0).size());
-}
-TEST(AgradMixMatrixTail, tailRowVector2_fv) {
-  using stan::math::tail;
-  stan::math::row_vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, tail(v, 3).size());
-}
-TEST(AgradMixMatrixTail, tailRowVector3_fv) {
-  using stan::math::tail;
-  stan::math::row_vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(tail(v, 4), std::out_of_range);
-}
-TEST(AgradMixMatrixTail, tailRowVector4_fv) {
-  using stan::math::tail;
-  stan::math::row_vector_fv v(3);
-  v << 1, 2, 3;
-
-  stan::math::row_vector_fv v12 = tail(v, 2);
-  EXPECT_EQ(2, v12.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n + 1].val_.val(), v12[n].val_.val());
-  }
+template <typename T>
+void expect_tail(const T& x, int n) {
+  Eigen::VectorXd v = stan::test::to_vector(x);
+  Eigen::RowVectorXd rv = stan::test::to_row_vector(x);
+  stan::test::expect_ad(f(n), x);
+  stan::test::expect_ad(f(n), v);
+  stan::test::expect_ad(f(n), rv);
 }
 
-TEST(AgradMixMatrixTail, tailStdVector1_fv) {
-  using stan::math::tail;
-  std::vector<fvar<var> > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  EXPECT_EQ(0U, tail(v, 0).size());
-}
-TEST(AgradMixMatrixTail, tailStdVector2_fv) {
-  using stan::math::tail;
-  std::vector<fvar<var> > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  EXPECT_EQ(3U, tail(v, 3).size());
-}
-TEST(AgradMixMatrixTail, tailStdVector3_fv) {
-  using stan::math::tail;
-  std::vector<fvar<var> > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  EXPECT_THROW(tail(v, 4), std::out_of_range);
-}
-TEST(AgradMixMatrixTail, tailStdVector4_fv) {
-  using stan::math::tail;
-  std::vector<fvar<var> > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  std::vector<fvar<var> > v12 = tail(v, 2);
-  EXPECT_EQ(2U, v12.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n + 1].val_.val(), v12[n].val_.val());
-  }
-}
-TEST(AgradMixMatrixTail, TailVector1_ffv) {
-  using stan::math::tail;
-  stan::math::vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, tail(v, 0).size());
-}
-TEST(AgradMixMatrixTail, TailVector2_ffv) {
-  using stan::math::tail;
-  stan::math::vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, tail(v, 3).size());
-}
-TEST(AgradMixMatrixTail, TailVector3_ffv) {
-  using stan::math::tail;
-  stan::math::vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(tail(v, 4), std::out_of_range);
-}
-TEST(AgradMixMatrixTail, TailVector4_ffv) {
-  using stan::math::tail;
-  stan::math::vector_ffv v(3);
-  v << 1, 2, 3;
+TEST(MathMixMatFun, tail) {
+  std::vector<double> a{};
+  expect_tail(a, 0);
+  expect_tail(a, 1);
 
-  stan::math::vector_ffv v12 = tail(v, 2);
-  EXPECT_EQ(2, v12.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n + 1].val_.val_.val(), v12[n].val_.val_.val());
-  }
-}
+  std::vector<double> b{1};
+  expect_tail(b, 0);
+  expect_tail(b, 1);
+  expect_tail(b, 2);
 
-TEST(AgradMixMatrixTail, tailRowVector1_ffv) {
-  using stan::math::tail;
-  stan::math::row_vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, tail(v, 0).size());
-}
-TEST(AgradMixMatrixTail, tailRowVector2_ffv) {
-  using stan::math::tail;
-  stan::math::row_vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, tail(v, 3).size());
-}
-TEST(AgradMixMatrixTail, tailRowVector3_ffv) {
-  using stan::math::tail;
-  stan::math::row_vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(tail(v, 4), std::out_of_range);
-}
-TEST(AgradMixMatrixTail, tailRowVector4_ffv) {
-  using stan::math::tail;
-  stan::math::row_vector_ffv v(3);
-  v << 1, 2, 3;
-
-  stan::math::row_vector_ffv v12 = tail(v, 2);
-  EXPECT_EQ(2, v12.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n + 1].val_.val_.val(), v12[n].val_.val_.val());
-  }
-}
-
-TEST(AgradMixMatrixTail, tailStdVector1_ffv) {
-  using stan::math::tail;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  EXPECT_EQ(0U, tail(v, 0).size());
-}
-TEST(AgradMixMatrixTail, tailStdVector2_ffv) {
-  using stan::math::tail;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  EXPECT_EQ(3U, tail(v, 3).size());
-}
-TEST(AgradMixMatrixTail, tailStdVector3_ffv) {
-  using stan::math::tail;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  EXPECT_THROW(tail(v, 4), std::out_of_range);
-}
-TEST(AgradMixMatrixTail, tailStdVector4_ffv) {
-  using stan::math::tail;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  std::vector<fvar<fvar<var> > > v12 = tail(v, 2);
-  EXPECT_EQ(2U, v12.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n + 1].val_.val_.val(), v12[n].val_.val_.val());
+  std::vector<double> v{1, 2, 3};
+  for (int n = 0; n < 5; ++n) {
+    expect_tail(v, n);
   }
 }
