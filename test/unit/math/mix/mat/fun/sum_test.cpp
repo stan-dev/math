7e3bb93e51944e61e49ca2e6a7fba2e1f2567a17
Bob Carpenter <carp@alias-i.com>
2019-10-27 13:03:58 -0400

    std vector tests for sum

--- test/unit/math/mix/mat/fun/sum_test.cpp
+++ test/unit/math/mix/mat/fun/sum_test.cpp
@@ -6,9 +6,11 @@ void expect_sum(const T& m) {
   auto f = [](const auto& x) { return stan::math::sum(x); };
   Eigen::VectorXd v = stan::test::to_vector(m);
   Eigen::RowVectorXd rv = stan::test::to_row_vector(m);
+  auto sv = stan::test::to_std_vector(m);
   stan::test::expect_ad(f, m);
   stan::test::expect_ad(f, v);
   stan::test::expect_ad(f, rv);
+  stan::test::expect_ad(f, sv);
 }
 
 TEST(MathMixMatFun, sum) {
