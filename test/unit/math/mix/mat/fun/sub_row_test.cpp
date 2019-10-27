2debda9086577a2f00282ea4b8a978a4b316d744
Bob Carpenter <carp@alias-i.com>
2019-10-27 09:14:06 -0400

    sub_row to ad test framework

--- test/unit/math/mix/mat/fun/sub_row_test.cpp
+++ test/unit/math/mix/mat/fun/sub_row_test.cpp
@@ -1,114 +1,24 @@
-#include <stan/math/mix/mat.hpp>
-#include <gtest/gtest.h>
-#include <stdexcept>
+#include <test/unit/math/test_ad.hpp>
 
-TEST(AgradMixMatrixSubRow, SubRow1_matrix_fv) {
-  using stan::math::sub_row;
-  stan::math::matrix_fv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_EQ(0, sub_row(m, 1, 1, 0).size());
+auto f(int i, int j, int k) {
+  return [=](const auto& y) { return stan::math::sub_row(y, i, j, k); };
 }
-TEST(AgradMixMatrixSubRow, SubRow2_matrix_fv) {
-  using stan::math::sub_row;
-  stan::math::matrix_fv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_EQ(3, sub_row(m, 1, 1, 3).size());
-}
-TEST(AgradMixMatrixSubRow, SubRow3_matrix_fv) {
-  using stan::math::sub_row;
-  stan::math::matrix_fv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_THROW(sub_row(m, 1, 1, 7), std::out_of_range);
-}
-TEST(AgradMixMatrixSubRow, SubRow4_matrix_fv) {
-  using stan::math::sub_row;
-  stan::math::matrix_fv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_THROW(sub_row(m, 7, 1, 1), std::out_of_range);
-}
-TEST(AgradMixMatrixSubRow, SubRow5_matrix_fv) {
-  using stan::math::sub_row;
-  Eigen::MatrixXd m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_THROW(sub_row(m, 1, 7, 1), std::out_of_range);
-}
-TEST(AgradMixMatrixSubRow, SubRow6_matrix_fv) {
-  using stan::math::sub_row;
-  stan::math::matrix_fv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j) {
-      m(i, j) = (i + 1) * (j + 1);
-      m(i, j).d_ = 1.0;
-    }
-  stan::math::row_vector_fv v = sub_row(m, 1, 2, 2);
-  EXPECT_EQ(2, v.size());
-  for (int i = 0; i < 2; ++i) {
-    EXPECT_FLOAT_EQ(m(0, 1 + i).val_.val(), v(i).val_.val());
-    EXPECT_FLOAT_EQ(m(0, 1 + i).d_.val(), v(i).d_.val());
-  }
-}
-TEST(AgradMixMatrixSubRow, SubRow1_matrix_ffv) {
-  using stan::math::sub_row;
-  stan::math::matrix_ffv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_EQ(0, sub_row(m, 1, 1, 0).size());
-}
-TEST(AgradMixMatrixSubRow, SubRow2_matrix_ffv) {
-  using stan::math::sub_row;
-  stan::math::matrix_ffv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_EQ(3, sub_row(m, 1, 1, 3).size());
-}
-TEST(AgradMixMatrixSubRow, SubRow3_matrix_ffv) {
-  using stan::math::sub_row;
-  stan::math::matrix_ffv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_THROW(sub_row(m, 1, 1, 7), std::out_of_range);
-}
-TEST(AgradMixMatrixSubRow, SubRow4_matrix_ffv) {
-  using stan::math::sub_row;
-  stan::math::matrix_ffv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_THROW(sub_row(m, 7, 1, 1), std::out_of_range);
-}
-TEST(AgradMixMatrixSubRow, SubRow5_matrix_ffv) {
-  using stan::math::sub_row;
-  Eigen::MatrixXd m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j)
-      m(i, j) = (i + 1) * (j + 1);
-  EXPECT_THROW(sub_row(m, 1, 7, 1), std::out_of_range);
-}
-TEST(AgradMixMatrixSubRow, SubRow6_matrix_ffv) {
-  using stan::math::sub_row;
-  stan::math::matrix_ffv m(3, 4);
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 4; ++j) {
-      m(i, j) = (i + 1) * (j + 1);
-      m(i, j).d_ = 1.0;
-    }
-  stan::math::row_vector_ffv v = sub_row(m, 1, 2, 2);
-  EXPECT_EQ(2, v.size());
-  for (int i = 0; i < 2; ++i) {
-    EXPECT_FLOAT_EQ(m(0, 1 + i).val_.val_.val(), v(i).val_.val_.val());
-    EXPECT_FLOAT_EQ(m(0, 1 + i).d_.val_.val(), v(i).d_.val_.val());
-  }
+
+TEST(MathMixMatFun, subRow) {
+  Eigen::MatrixXd a(1, 1);
+  a << 3.2;
+  stan::test::expect_ad(f(1, 1, 0), a);
+  stan::test::expect_ad(f(1, 1, 1), a);
+
+  Eigen::MatrixXd b(3, 4);
+  b << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12;
+  stan::test::expect_ad(f(1, 1, 0), b);
+  stan::test::expect_ad(f(1, 1, 1), b);
+  stan::test::expect_ad(f(1, 1, 3), b);
+  stan::test::expect_ad(f(1, 2, 2), b);
+  stan::test::expect_ad(f(3, 4, 1), b);
+  stan::test::expect_ad(f(2, 3, 2), b);
+  stan::test::expect_ad(f(1, 1, 7), b);  // exception--range
+  stan::test::expect_ad(f(7, 1, 1), b);  // exception--range
+  stan::test::expect_ad(f(1, 7, 1), b);  // exception--range
 }
