786b0904b15b46f934984adb87354bf551a220d7
Steve Bronder <stevo15025@gmail.com>
2019-11-03 17:43:31 -0500

    Forgot to include bool_constant in is_index.hpp

--- stan/math/prim/scal/meta/is_index.hpp
+++ stan/math/prim/scal/meta/is_index.hpp
@@ -1,6 +1,7 @@
 #ifndef STAN_MATH_PRIM_SCAL_META_IS_INDEX_HPP
 #define STAN_MATH_PRIM_SCAL_META_IS_INDEX_HPP
 
+#include <stan/math/prim/scal/meta/bool_constant.hpp>
 #include <type_traits>
 
 namespace stan {
