4bbc1e76d07163b4939fa0b9de6806410f3c0c9f
Stan Jenkins <mc.stanislaw@gmail.com>
2019-11-03 21:46:37 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- stan/math/prim/scal/meta/is_index.hpp
+++ stan/math/prim/scal/meta/is_index.hpp
@@ -13,6 +13,6 @@ template <typename T>
 using is_index = bool_constant<!std::is_floating_point<std::decay_t<T>>::value
                                && std::is_arithmetic<std::decay_t<T>>::value>;
 
-}
+}  // namespace stan
 
 #endif
