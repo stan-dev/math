7261c42ca63acfb419a0a52e48ad8bafd8d74be2
Bob Carpenter <carp@alias-i.com>
2019-11-10 11:13:09 -0500

    trace_gen_quad_form to ad test framework; templating fixed

--- stan/math/prim/mat/fun/trace_gen_quad_form.hpp
+++ stan/math/prim/mat/fun/trace_gen_quad_form.hpp
@@ -5,17 +5,70 @@
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/mat/err/check_multiplicable.hpp>
 #include <stan/math/prim/mat/err/check_square.hpp>
+#include <stan/math/prim/mat/fun/trace.hpp>
+#include <stan/math/prim/mat/fun/multiply.hpp>
+#include <stan/math/prim/mat/fun/transpose.hpp>
+#include <exception>
 
 namespace stan {
 namespace math {
 /**
- * Compute trace(D B^T A B).
- **/
+ * Return the trace of D times the quadratic form of B and A.
+ * That is, `trace_gen_quad_form(D, A, B) = trace(D * B' * A * B).`
+ *
+ * @tparam TD type of first argument scalar
+ * @tparam RD row type of first argument
+ * @tparam CD col type of first argument
+ * @tparam TA type of second argument scalar
+ * @tparam RA row type of second argument
+ * @tparam CA col type of second argument
+ * @tparam TB type of third argument scalar
+ * @tparam RB row type of third argument
+ * @tparam CB col type of third argument
+ * @param D multiplier
+ * @param A outside term in quadratic form
+ * @param B inner term in quadratic form
+ * @return trace(D * B' * A * B)
+ * @throw std::domain_error if A or D is not square
+ * @throw std::domain_error if A cannot be multiplied by B or B cannot
+ * be multiplied by D.
+ */
 template <typename TD, int RD, int CD, typename TA, int RA, int CA, typename TB,
           int RB, int CB, typename = require_any_not_var_t<TD, TA, TB>>
 inline return_type_t<TD, TA, TB> trace_gen_quad_form(
     const Eigen::Matrix<TD, RD, CD> &D, const Eigen::Matrix<TA, RA, CA> &A,
     const Eigen::Matrix<TB, RB, CB> &B) {
+  check_square("trace_gen_quad_form", "A", A);
+  check_square("trace_gen_quad_form", "D", D);
+  check_multiplicable("trace_gen_quad_form", "A", A, "B", B);
+  check_multiplicable("trace_gen_quad_form", "B", B, "D", D);
+  return trace(multiply(multiply(D, transpose(B)), multiply(A, B)));
+}
+
+/**
+ * Return the trace of D times the quadratic form of B and A.
+ * That is, `trace_gen_quad_form(D, A, B) = trace(D * B' * A * B).`
+ * This is the double-only overload to allow Eigen's expression
+ * templates to be used for efficiency.
+ *
+ * @tparam RD row type of first argument
+ * @tparam CD col type of first argument
+ * @tparam RA row type of second argument
+ * @tparam CA col type of second argument
+ * @tparam RB row type of third argument
+ * @tparam CB col type of third argument
+ * @param D multiplier
+ * @param A outside term in quadratic form
+ * @param B inner term in quadratic form
+ * @return trace(D * B' * A * B)
+ * @throw std::domain_error if A or D is not square
+ * @throw std::domain_error if A cannot be multiplied by B or B cannot
+ * be multiplied by D.
+ */
+template <int RD, int CD, int RA, int CA, typename TB, int RB, int CB>
+inline double trace_gen_quad_form(const Eigen::Matrix<double, RD, CD> &D,
+                                  const Eigen::Matrix<double, RA, CA> &A,
+                                  const Eigen::Matrix<double, RB, CB> &B) {
   check_square("trace_gen_quad_form", "A", A);
   check_square("trace_gen_quad_form", "D", D);
   check_multiplicable("trace_gen_quad_form", "A", A, "B", B);
