fb6b885611f68701592865b4d028b461ac7afc56
Bob Carpenter <carp@alias-i.com>
2019-11-10 18:00:19 -0500

    moved matix_exp_handler rev test to prim

--- test/unit/math/prim/mat/fun/matrix_exp_action_handler_test.cpp
+++ test/unit/math/prim/mat/fun/matrix_exp_action_handler_test.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-// #include <stan/math/prim/scal/err/check_not_nan.hpp>
 #include <test/unit/math/prim/mat/util.hpp>
 #include <stan/math/prim/mat/fun/matrix_exp.hpp>
 #include <stan/math/prim/mat/fun/matrix_exp_action_handler.hpp>
