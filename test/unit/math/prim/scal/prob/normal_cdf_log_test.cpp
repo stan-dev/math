055c640a6f8ec867de945305b562f0fd3cccde73
Phil Clemson <p.clemson@liverpool.ac.uk>
2019-11-14 11:47:44 +0000

    Added the proposed changes and autodiff tests, with some additional improvements to the numerical approximations.

--- test/unit/math/prim/scal/prob/normal_cdf_log_test.cpp
+++ test/unit/math/prim/scal/prob/normal_cdf_log_test.cpp
@@ -1,5 +1,7 @@
 #include <stan/math/prim/scal.hpp>
 #include <gtest/gtest.h>
+#include <test/unit/math/test_ad.hpp>
+#include <limits>
 
 TEST(ProbNormal, cdf_log_matches_lcdf) {
   double y = 0.8;
@@ -11,4 +13,20 @@ TEST(ProbNormal, cdf_log_matches_lcdf) {
   EXPECT_FLOAT_EQ(
       (stan::math::normal_lcdf<double, double, double>(y, mu, sigma)),
       (stan::math::normal_cdf_log<double, double, double>(y, mu, sigma)));
+}
+
+TEST(mathMixScalFun, lcdf_derivatives) {
+  auto f = [](const double mu, const double sigma) {
+    return [=](const auto& y) { return stan::math::normal_lcdf(y, mu, sigma); };
+  };
+  stan::test::expect_ad(f(0.0, 1.0), -50.0);
+  stan::test::expect_ad(f(0.0, 1.0), -20.0 * stan::math::SQRT_2);
+  stan::test::expect_ad(f(0.0, 1.0), -5.5);
+  stan::test::expect_ad(f(0.0, 1.0), 0.0);
+  stan::test::expect_ad(f(0.0, 1.0), 0.15);
+  stan::test::expect_ad(f(0.0, 1.0), 1.14);
+  stan::test::expect_ad(f(0.0, 1.0), 3.00);
+  stan::test::expect_ad(f(0.0, 1.0), 10.00);
+  stan::test::expect_ad(f(-1.0, 2.0), 1.50);
+  stan::test::expect_ad(f(2.0, 1.0), 0.50);
 }
