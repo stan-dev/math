93226e29eaca9a547648ec6cfca7d95f76abb07d
Marco Colombo <m.colombo@ed.ac.uk>
2019-11-18 17:49:30 +0000

    Use the nullary constructor.

--- stan/math/prim/mat/fun/read_corr_matrix.hpp
+++ stan/math/prim/mat/fun/read_corr_matrix.hpp
@@ -25,7 +25,7 @@ template <typename T>
 Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> read_corr_matrix(
     const Eigen::Array<T, Eigen::Dynamic, 1>& CPCs, size_t K) {
   if (K == 0)
-    return Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>();
+    return {};
 
   Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> L = read_corr_L(CPCs, K);
   return multiply_lower_tri_self_transpose(L);
