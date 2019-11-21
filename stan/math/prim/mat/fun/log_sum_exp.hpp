4ea4cbf15a9f5b47abbf827276fcde15fdbf595e
Marco Colombo <m.colombo@ed.ac.uk>
2019-11-21 21:55:50 +0000

    Make log_sum_exp return negative infinity for size 0 inputs

--- stan/math/prim/mat/fun/log_sum_exp.hpp
+++ stan/math/prim/mat/fun/log_sum_exp.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <vector>
 #include <cmath>
+#include <limits>
 
 namespace stan {
 namespace math {
@@ -24,6 +25,9 @@ namespace math {
  */
 template <int R, int C>
 double log_sum_exp(const Eigen::Matrix<double, R, C>& x) {
+  if (x.size() == 0)
+    return -std::numeric_limits<double>::infinity();
+
   const double max = x.maxCoeff();
   if (!std::isfinite(max)) {
     return max;
