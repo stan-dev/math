0f9089d4796708e5c8f8c49f49052c0fbbc3037f
Rok Češnovar <rok.cesnovar@fri.uni-lj.si>
2019-11-25 14:03:59 +0100

    Update test/unit/math/fwd/arr/meta/partials_return_type_test.cpp
    Co-Authored-By: Tadej Ciglarič <tadej.c@gmail.com>

--- test/unit/math/fwd/arr/meta/partials_return_type_test.cpp
+++ test/unit/math/fwd/arr/meta/partials_return_type_test.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-TEST(MetaTraitsMixArr, partials_return_type) {
+TEST(MetaTraitsFwdArr, partials_return_type) {
   using stan::math::fvar;
   using stan::partials_return_type;
 
