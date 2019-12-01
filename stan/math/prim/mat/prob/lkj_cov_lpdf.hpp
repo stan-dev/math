7ef54d54e3f1d3baab7a013967ce9dc104931d8e
Stan Jenkins <mc.stanislaw@gmail.com>
2019-12-01 12:51:17 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- stan/math/prim/mat/prob/lkj_cov_lpdf.hpp
+++ stan/math/prim/mat/prob/lkj_cov_lpdf.hpp
@@ -56,11 +56,11 @@ return_type_t<T_y, T_loc, T_scale, T_shape> lkj_cov_lpdf(
 }
 
 template <typename T_y, typename T_loc, typename T_scale, typename T_shape>
-inline promote_args_t<T_y, T_loc, T_scale, T_shape>
-lkj_cov_lpdf(const Eigen::Matrix<T_y, Eigen::Dynamic, Eigen::Dynamic>& y,
-             const Eigen::Matrix<T_loc, Eigen::Dynamic, 1>& mu,
-             const Eigen::Matrix<T_scale, Eigen::Dynamic, 1>& sigma,
-             const T_shape& eta) {
+inline promote_args_t<T_y, T_loc, T_scale, T_shape> lkj_cov_lpdf(
+    const Eigen::Matrix<T_y, Eigen::Dynamic, Eigen::Dynamic>& y,
+    const Eigen::Matrix<T_loc, Eigen::Dynamic, 1>& mu,
+    const Eigen::Matrix<T_scale, Eigen::Dynamic, 1>& sigma,
+    const T_shape& eta) {
   return lkj_cov_lpdf<false>(y, mu, sigma, eta);
 }
 
