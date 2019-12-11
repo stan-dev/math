48a28f9186fa6951ab28e37389ea983314408a49
Marco Colombo <m.colombo@ed.ac.uk>
2019-12-11 09:05:43 +0000

    Mark domain_error as deprecated and delegate to throw_domain_error

--- stan/math/prim/scal/err/domain_error.hpp
+++ stan/math/prim/scal/err/domain_error.hpp
@@ -2,6 +2,7 @@
 #define STAN_MATH_PRIM_SCAL_ERR_DOMAIN_ERROR_HPP
 
 #include <stan/math/prim/meta.hpp>
+#include <stan/math/prim/scal/err/throw_domain_error.hpp>
 #include <typeinfo>
 #include <sstream>
 #include <stdexcept>
@@ -10,46 +11,21 @@ namespace stan {
 namespace math {
 
 /**
- * Throw a domain error with a consistently formatted message.
- * This is an abstraction for all Stan functions to use when throwing
- * domain errors. This will allow us to change the behavior for all
- * functions at once.
- * The message is: "<function>: <name> <msg1><y><msg2>"
- * @tparam T Type of variable.
- * @param[in] function Name of the function.
- * @param[in] name Name of the variable.
- * @param[in] y Variable.
- * @param[in] msg1 Message to print before the variable.
- * @param[in] msg2 Message to print after the variable.
- * @throw std::domain_error Always.
+ * @deprecated use <code>throw_domain_error</code>
  */
 template <typename T>
 inline void domain_error(const char* function, const char* name, const T& y,
                          const char* msg1, const char* msg2) {
-  std::ostringstream message;
-  // hack to remove -Waddress, -Wnonnull-compare warnings from GCC 6
-  const T* y_ptr = &y;
-  message << function << ": " << name << " " << msg1 << (*y_ptr) << msg2;
-  throw std::domain_error(message.str());
+  throw_domain_error(function, name, y, msg1, msg2);
 }
 
 /**
- * Throw a domain error with a consistently formatted message.
- * This is an abstraction for all Stan functions to use when throwing
- * domain errors. This will allow us to change the behavior for all
- * functions at once.
- * The message is: * "<function>: <name> <msg1><y>"
- * @tparam T Type of variable.
- * @param[in] function Name of the function.
- * @param[in] name Name of the variable.
- * @param[in] y Variable.
- * @param[in] msg1 Message to print before the variable.
- * @throw std::domain_error Always.
+ * @deprecated use <code>throw_domain_error</code>
  */
 template <typename T>
 inline void domain_error(const char* function, const char* name, const T& y,
                          const char* msg1) {
-  domain_error(function, name, y, msg1, "");
+  throw_domain_error(function, name, y, msg1);
 }
 
 }  // namespace math
