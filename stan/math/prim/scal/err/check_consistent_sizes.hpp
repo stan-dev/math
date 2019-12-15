4ad12a5e4dd413c8f3c6b0ecdaee15774cc245f0
Stan Jenkins <mc.stanislaw@gmail.com>
2019-12-15 15:30:15 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- stan/math/prim/scal/err/check_consistent_sizes.hpp
+++ stan/math/prim/scal/err/check_consistent_sizes.hpp
@@ -104,7 +104,9 @@ inline void check_consistent_sizes(const char* function, const char* name1,
                                    const T5& x5) {
   size_t max_size = std::max(
       stan::size(x1),
-      std::max(stan::size(x2), std::max(stan::size(x3), std::max(stan::size(x4), stan::size(x5)))));
+      std::max(
+          stan::size(x2),
+          std::max(stan::size(x3), std::max(stan::size(x4), stan::size(x5)))));
   check_consistent_size(function, name1, x1, max_size);
   check_consistent_size(function, name2, x2, max_size);
   check_consistent_size(function, name3, x3, max_size);
