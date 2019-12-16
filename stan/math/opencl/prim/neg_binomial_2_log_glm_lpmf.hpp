a09ffe63bafff9870d426bb29b0b2ab909ef3d46
Marco Colombo <m.colombo@ed.ac.uk>
2019-12-16 14:33:22 +0000

    Fix typo in variable name introduced by previous commit

--- stan/math/opencl/prim/neg_binomial_2_log_glm_lpmf.hpp
+++ stan/math/opencl/prim/neg_binomial_2_log_glm_lpmf.hpp
@@ -123,7 +123,7 @@ return_type_t<T_alpha, T_beta, T_precision> neg_binomial_2_log_glm_lpmf(
   matrix_cl<double> phi_derivative_cl(
       need_phi_derivative ? (need_phi_derivative_sum ? wgs : N) : 0, 1);
   const bool need_logp1 = include_summand_b<propto>;
-  const bool need_logp
+  const bool need_logp2
       = include_summand_b<propto, T_precision> && is_vector<T_precision>;
   const bool need_logp3
       = include_summand_b<propto, T_alpha, T_beta, T_precision>;
