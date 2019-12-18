d44cb69563b003ce620c912ba64a4855e46f0d82
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-12-18 22:05:22 +0100

    fix include

--- stan/math/prim/mat/err/check_ldlt_factor.hpp
+++ stan/math/prim/mat/err/check_ldlt_factor.hpp
@@ -1,7 +1,6 @@
 #ifndef STAN_MATH_PRIM_MAT_ERR_CHECK_LDLT_FACTOR_HPP
 #define STAN_MATH_PRIM_MAT_ERR_CHECK_LDLT_FACTOR_HPP
 
-#include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/prim/scal/err/throw_domain_error.hpp>
 #include <stan/math/prim/mat/fun/LDLT_factor.hpp>
