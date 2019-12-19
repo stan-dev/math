88d5d6bc4edbd6fabaac56163ad437717feafbb3
Stan Jenkins <mc.stanislaw@gmail.com>
2019-12-19 18:11:42 -0500

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/rev/mat/fun/multiply.hpp
+++ stan/math/rev/mat/fun/multiply.hpp
@@ -582,15 +582,13 @@ inline Eigen::Matrix<var, R2, C2> multiply(const T1& c,
   return to_var(m) * to_var(c);
 }
 
-template <typename T1, int R2, int C2,
-typename = require_arithmetic_t<T1>>
+template <typename T1, int R2, int C2, typename = require_arithmetic_t<T1>>
 inline Eigen::Matrix<var, R2, C2> multiply(
     const T1& c, const Eigen::Matrix<var, R2, C2>& m) {
   return m * c;
 }
 
-template <typename T2, int R2, int C2,
-          typename = require_arithmetic_t<T2>>
+template <typename T2, int R2, int C2, typename = require_arithmetic_t<T2>>
 inline Eigen::Matrix<var, R2, C2> multiply(const var& c,
                                            const Eigen::Matrix<T2, R2, C2>& m) {
   return m * c;
@@ -613,15 +611,13 @@ inline Eigen::Matrix<var, R1, C1> multiply(const Eigen::Matrix<T1, R1, C1>& m,
   return to_var(m) * to_var(c);
 }
 
-template <typename T1, int R1, int C1,
-          typename = require_arithmetic_t<T1>>
+template <typename T1, int R1, int C1, typename = require_arithmetic_t<T1>>
 inline Eigen::Matrix<var, R1, C1> multiply(const Eigen::Matrix<T1, R1, C1>& m,
                                            const var& c) {
   return m * c;
 }
 
-template <int R1, int C1, typename T2,
-          typename = require_arithmetic_t<T2>>
+template <int R1, int C1, typename T2, typename = require_arithmetic_t<T2>>
 inline Eigen::Matrix<var, R1, C1> multiply(const Eigen::Matrix<var, R1, C1>& m,
                                            const T2& c) {
   return m * c;
