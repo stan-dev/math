6901666784f8c70542705ec22a90d518b0d57aa2
tadej <tadej.c@gmail.com>
2019-12-19 14:03:10 +0100

    fixed more includes

--- stan/math/prim/arr/fun/promote_scalar.hpp
+++ stan/math/prim/arr/fun/promote_scalar.hpp
@@ -3,7 +3,6 @@
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/scal/fun/promote_scalar.hpp>
-#include <stan/math/prim/meta/promote_scalar_type.hpp>
 #include <vector>
 
 namespace stan {
