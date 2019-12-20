a6b292b290b8218b719b9872ae359dc94960af86
Stan Jenkins <mc.stanislaw@gmail.com>
2019-12-20 17:00:32 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- test/unit/math/mix/mat/fun/sort_indices_test.cpp
+++ test/unit/math/mix/mat/fun/sort_indices_test.cpp
@@ -139,8 +139,8 @@ void test_sort_indices_desc2(Eigen::Matrix<T, R, C> val) {
   const size_t val_size = val.size();
 
   AVEC x(val_size);
-  for (size_t i = 0U; i < val_size; i++) x.data()[i]
-      = fvar<var>(val[i]);
+  for (size_t i = 0U; i < val_size; i++)
+    x.data()[i] = fvar<var>(val[i]);
 
   std::vector<int> val_sorted = sort_indices_desc(val);
   std::vector<int> x_sorted = sort_indices_desc(x);
