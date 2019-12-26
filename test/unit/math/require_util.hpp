82e972571f7754c5d31d28e76013ed1ce83e506e
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2019-12-25 23:24:53 -0600

    Strengthen require_generics tests.

--- test/unit/math/require_util.hpp
+++ test/unit/math/require_util.hpp
@@ -30,6 +30,14 @@ struct conjunction<B1> : B1 {};
 template <class B1, class... Bn>
 struct conjunction<B1, Bn...>
     : std::conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
+
+template <class...>
+struct disjunction : std::true_type {};
+template <class B1>
+struct disjunction<B1> : B1 {};
+template <class B1, class... Bn>
+struct disjunction<B1, Bn...>
+    : std::conditional_t<bool(B1::value), B1, disjunction<Bn...>> {};
 }  // namespace internal
 
 /**
@@ -70,6 +78,22 @@ template <template <class...> class Check>
 struct unary_require_tester<Check, void> : std::true_type {};
 
 /**
+ * Used for checking an enable_if alias, when the enable_if goes off
+ * the struct holds a /c value of true, else false.
+ * @tparam Check The enable_if alias.
+ * @tparam Types Parameter pack of types that will be checked individually.
+ * @note Because of /c disjunction, the member /c value will only be true
+ *  if at least one of the types passes.
+ */
+template <template <class...> class Check, typename... Types>
+struct unary_not_require_tester
+    : internal::disjunction<unary_require_tester_impl<Check, Types>...> {};
+
+// For the end of recursion
+template <template <class...> class Check>
+struct unary_not_require_tester<Check, void> : std::true_type {};
+
+/**
  * This is the same as above, but instead all types are checked at once
  * since this is used for the require_any/all type checks
  */
@@ -95,8 +119,8 @@ struct require_scal_checker {
   }
   static void not_unary() {
     using stan::test::unary_require_tester;
-    EXPECT_TRUE((unary_require_tester<checker, dummy>::value));
-    EXPECT_FALSE((unary_require_tester<checker, Types...>::value));
+    EXPECT_TRUE((unary_not_require_tester<checker, dummy>::value));
+    EXPECT_FALSE((unary_not_require_tester<checker, Types...>::value));
   }
   static void all() {
     using stan::test::require_variadic_checker;
@@ -104,19 +128,16 @@ struct require_scal_checker {
     EXPECT_FALSE((require_variadic_checker<checker, Types..., dummy>::value));
     EXPECT_TRUE((require_variadic_checker<checker, Types...>::value));
   }
-
   static void all_not() {
     using stan::test::require_variadic_checker;
     EXPECT_TRUE((require_variadic_checker<checker, dummy, dummy>::value));
     EXPECT_FALSE((require_variadic_checker<checker, Types..., dummy>::value));
   }
-
   static void any() {
     using stan::test::require_variadic_checker;
     EXPECT_FALSE((require_variadic_checker<checker, dummy, dummy>::value));
     EXPECT_TRUE((require_variadic_checker<checker, Types..., dummy>::value));
   }
-
   static void any_not() {
     using stan::test::require_variadic_checker;
     EXPECT_TRUE((require_variadic_checker<checker, dummy, Types...>::value));
@@ -183,6 +204,7 @@ struct require_container_checker {
     EXPECT_TRUE((unary_container_require_tester<checker, ContainerCheck,
                                                 Container<float>>::value));
   }
+
   template <template <class...> class ContainerCheck>
   static void not_unary() {
     using stan::test::unary_container_require_tester;
