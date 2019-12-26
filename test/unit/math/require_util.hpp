4d2543ec332de3b9aac6b09f65a85ec1db30a3b2
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2019-12-25 23:31:44 -0600

    Further strengthen require_generics tests.

--- test/unit/math/require_util.hpp
+++ test/unit/math/require_util.hpp
@@ -124,6 +124,7 @@ struct require_scal_checker {
   }
   static void all() {
     using stan::test::require_variadic_checker;
+    EXPECT_FALSE((require_variadic_checker<checker, dummy, dummy>::value));
     EXPECT_FALSE((require_variadic_checker<checker, dummy, Types...>::value));
     EXPECT_FALSE((require_variadic_checker<checker, Types..., dummy>::value));
     EXPECT_TRUE((require_variadic_checker<checker, Types...>::value));
@@ -131,15 +132,20 @@ struct require_scal_checker {
   static void all_not() {
     using stan::test::require_variadic_checker;
     EXPECT_TRUE((require_variadic_checker<checker, dummy, dummy>::value));
+    EXPECT_FALSE((require_variadic_checker<checker, dummy, Types...>::value));
     EXPECT_FALSE((require_variadic_checker<checker, Types..., dummy>::value));
+    EXPECT_FALSE((require_variadic_checker<checker, Types...>::value));
   }
   static void any() {
     using stan::test::require_variadic_checker;
     EXPECT_FALSE((require_variadic_checker<checker, dummy, dummy>::value));
+    EXPECT_TRUE((require_variadic_checker<checker, dummy, Types...>::value));
     EXPECT_TRUE((require_variadic_checker<checker, Types..., dummy>::value));
+    EXPECT_TRUE((require_variadic_checker<checker, Types...>::value));
   }
   static void any_not() {
     using stan::test::require_variadic_checker;
+    EXPECT_TRUE((require_variadic_checker<checker, dummy, dummy>::value));
     EXPECT_TRUE((require_variadic_checker<checker, dummy, Types...>::value));
     EXPECT_TRUE((require_variadic_checker<checker, Types..., dummy>::value));
     EXPECT_FALSE((require_variadic_checker<checker, Types...>::value));
