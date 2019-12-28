a365bdfa291ccc1d393d46fb96cb19eea35e748f
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2019-12-28 12:57:50 -0600

    Make compile guards match filename.

--- stan/math/prim/scal/fun/inc_beta_ddz.hpp
+++ stan/math/prim/scal/fun/inc_beta_ddz.hpp
@@ -1,5 +1,5 @@
-#ifndef STAN_MATH_PRIM_SCAL_FUN_INC_BETA_DERIVATIVES_HPP
-#define STAN_MATH_PRIM_SCAL_FUN_INC_BETA_DERIVATIVES_HPP
+#ifndef STAN_MATH_PRIM_SCAL_FUN_INC_BETA_DDZ_HPP
+#define STAN_MATH_PRIM_SCAL_FUN_INC_BETA_DDZ_HPP
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/scal/fun/lgamma.hpp>
