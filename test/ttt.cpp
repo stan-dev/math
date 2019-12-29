e22e29f348f5e21f3433ce270868a3a83d374e71
Stan Jenkins <mc.stanislaw@gmail.com>
2019-12-29 16:00:31 -0500

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- test/ttt.cpp
+++ test/ttt.cpp
@@ -11,25 +11,24 @@
 */
 
 int main() {
-
   double y = -0.8;
   double mu = 0.4;
   double kappa = 0;
 
   stan::math::von_mises_lpdf(y, mu, kappa);
 
-/*
-  const double t = 1.0;
-  Eigen::MatrixXd A(0, 0);
-  Eigen::MatrixXd B(0, 0);
+  /*
+    const double t = 1.0;
+    Eigen::MatrixXd A(0, 0);
+    Eigen::MatrixXd B(0, 0);
 
-  Eigen::Matrix<var, -1, -1> C(0, 2);
-  auto D = stan::math::scale_matrix_exp_multiply(t, A, C);
-  std::cout << "rows: " << D.rows() << ", cols: " << D.cols() << std::endl;
-  double nan = std::numeric_limits<double>::quiet_NaN();
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> y(1, 1);
-  y << nan;
+    Eigen::Matrix<var, -1, -1> C(0, 2);
+    auto D = stan::math::scale_matrix_exp_multiply(t, A, C);
+    std::cout << "rows: " << D.rows() << ", cols: " << D.cols() << std::endl;
+    double nan = std::numeric_limits<double>::quiet_NaN();
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> y(1, 1);
+    y << nan;
 
-  stan::math::check_pos_definite("ciao", "y", y);
-*/
+    stan::math::check_pos_definite("ciao", "y", y);
+  */
 }
