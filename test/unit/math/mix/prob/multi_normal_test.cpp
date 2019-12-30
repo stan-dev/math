09d698fb461a5a90b683aaf70eedb1d2bddcc0cd
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-12-30 10:48:24 +0100

    Revert "fix merge"
    This reverts commit 4b13552852560cb12d58d336ae1101b6541973f6.

---
+++ test/unit/math/mix/prob/multi_normal_test.cpp
@@ -0,0 +1,52 @@
+#include <stan/math/mix/mat.hpp>
+#include <gtest/gtest.h>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/math/distributions.hpp>
+
+using Eigen::Dynamic;
+using Eigen::Matrix;
+using std::vector;
+
+TEST(ProbDistributionsMultiNormal, fvar_var) {
+  using stan::math::fvar;
+  using stan::math::var;
+
+  Matrix<fvar<var>, Dynamic, 1> y(3, 1);
+  y << 2.0, -2.0, 11.0;
+  Matrix<fvar<var>, Dynamic, 1> mu(3, 1);
+  mu << 1.0, -1.0, 3.0;
+  Matrix<fvar<var>, Dynamic, Dynamic> Sigma(3, 3);
+  Sigma << 9.0, -3.0, 0.0, -3.0, 4.0, 0.0, 0.0, 0.0, 5.0;
+  for (int i = 0; i < 3; i++) {
+    y(i).d_ = 1.0;
+    mu(i).d_ = 1.0;
+    for (int j = 0; j < 3; j++)
+      Sigma(i, j).d_ = 1.0;
+  }
+
+  fvar<var> res = stan::math::multi_normal_log(y, mu, Sigma);
+  EXPECT_FLOAT_EQ(-11.73908, res.val_.val());
+  EXPECT_FLOAT_EQ(0.54899865, res.d_.val());
+}
+
+TEST(ProbDistributionsMultiNormal, fvar_fvar_var) {
+  using stan::math::fvar;
+  using stan::math::var;
+
+  Matrix<fvar<fvar<var> >, Dynamic, 1> y(3, 1);
+  y << 2.0, -2.0, 11.0;
+  Matrix<fvar<fvar<var> >, Dynamic, 1> mu(3, 1);
+  mu << 1.0, -1.0, 3.0;
+  Matrix<fvar<fvar<var> >, Dynamic, Dynamic> Sigma(3, 3);
+  Sigma << 9.0, -3.0, 0.0, -3.0, 4.0, 0.0, 0.0, 0.0, 5.0;
+  for (int i = 0; i < 3; i++) {
+    y(i).d_ = 1.0;
+    mu(i).d_ = 1.0;
+    for (int j = 0; j < 3; j++)
+      Sigma(i, j).d_ = 1.0;
+  }
+
+  fvar<fvar<var> > res = stan::math::multi_normal_log(y, mu, Sigma);
+  EXPECT_FLOAT_EQ(-11.73908, res.val_.val_.val());
+  EXPECT_FLOAT_EQ(0.54899865, res.d_.val_.val());
+}
