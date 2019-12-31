31845c4c394db5b09c953e00211ea6faa6f38928
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2019-12-31 13:12:08 -0600

    Use inc_beta_dd(a|b|z) to compute derivatives for inc_beta.
    In particular, don't use grad_reg_inc_beta. Also, specialize the varis to avoid
    overpromotion and wasted work.

--- stan/math/fwd/fun/inc_beta.hpp
+++ stan/math/fwd/fun/inc_beta.hpp
@@ -2,56 +2,88 @@
 #define STAN_MATH_FWD_FUN_INC_BETA_HPP
 
 #include <stan/math/prim/scal/fun/digamma.hpp>
-#include <stan/math/prim/scal/fun/grad_reg_inc_beta.hpp>
-#include <stan/math/prim/scal/fun/lbeta.hpp>
+#include <stan/math/prim/scal/fun/inc_beta_dda.hpp>
+#include <stan/math/prim/scal/fun/inc_beta_ddb.hpp>
+#include <stan/math/prim/scal/fun/inc_beta_ddz.hpp>
 #include <stan/math/fwd/core.hpp>
 #include <stan/math/fwd/meta.hpp>
-#include <stan/math/fwd/fun/beta.hpp>
 #include <stan/math/fwd/fun/digamma.hpp>
-#include <stan/math/fwd/fun/exp.hpp>
-#include <stan/math/fwd/fun/pow.hpp>
-#include <cmath>
 
 namespace stan {
 namespace math {
 
 template <typename T>
 inline fvar<T> inc_beta(const fvar<T>& a, const fvar<T>& b, const fvar<T>& x) {
-  using std::pow;
-  T d_a;
-  T d_b;
-  const T beta_ab = beta(a.val_, b.val_);
-  grad_reg_inc_beta(d_a, d_b, a.val_, b.val_, x.val_, digamma(a.val_),
-                    digamma(b.val_), digamma(a.val_ + b.val_), beta_ab);
-  T d_x = pow((1 - x.val_), b.val_ - 1) * pow(x.val_, a.val_ - 1) / beta_ab;
-  return fvar<T>(inc_beta(a.val_, b.val_, x.val_),
-                 a.d_ * d_a + b.d_ * d_b + x.d_ * d_x);
+  T aT = a.val_;
+  T bT = b.val_;
+  T xT = x.val_;
+  T digamma_ab = digamma(aT + bT);
+  T d_a = inc_beta_dda(aT, bT, xT, digamma(aT), digamma_ab);
+  T d_b = inc_beta_ddb(aT, bT, xT, digamma(bT), digamma_ab);
+  T d_x = inc_beta_ddz(aT, bT, xT);
+  return fvar<T>(inc_beta(aT, bT, xT), a.d_ * d_a + b.d_ * d_b + x.d_ * d_x);
 }
 
 template <typename T>
 inline fvar<T> inc_beta(double a, const fvar<T>& b, const fvar<T>& x) {
-  return inc_beta(fvar<T>(a), b, x);
+  T aT = T(a);
+  T bT = b.val_;
+  T xT = x.val_;
+  T digamma_ab = digamma(aT + bT);
+  T d_b = inc_beta_ddb(aT, bT, xT, digamma(bT), digamma_ab);
+  T d_x = inc_beta_ddz(aT, bT, xT);
+  return fvar<T>(inc_beta(aT, bT, xT), b.d_ * d_b + x.d_ * d_x);
 }
+
 template <typename T>
 inline fvar<T> inc_beta(const fvar<T>& a, double b, const fvar<T>& x) {
-  return inc_beta(a, fvar<T>(b), x);
+  T aT = a.val_;
+  T bT = T(b);
+  T xT = x.val_;
+  T digamma_ab = digamma(aT + bT);
+  T d_a = inc_beta_dda(aT, bT, xT, digamma(aT), digamma_ab);
+  T d_x = inc_beta_ddz(aT, bT, xT);
+  return fvar<T>(inc_beta(aT, bT, xT), a.d_ * d_a + x.d_ * d_x);
 }
+
 template <typename T>
 inline fvar<T> inc_beta(const fvar<T>& a, const fvar<T>& b, double x) {
-  return inc_beta(a, b, fvar<T>(x));
+  T aT = a.val_;
+  T bT = b.val_;
+  T xT = T(x);
+  T digamma_ab = digamma(aT + bT);
+  T d_a = inc_beta_dda(aT, bT, xT, digamma(aT), digamma_ab);
+  T d_b = inc_beta_ddb(aT, bT, xT, digamma(bT), digamma_ab);
+  return fvar<T>(inc_beta(aT, bT, xT), a.d_ * d_a + b.d_ * d_b);
 }
 
 template <typename T>
 inline fvar<T> inc_beta(double a, double b, const fvar<T>& x) {
-  return inc_beta(fvar<T>(a), fvar<T>(b), x);
+  T aT = T(a);
+  T bT = T(b);
+  T xT = x.val_;
+  T d_x = inc_beta_ddz(aT, bT, xT);
+  return fvar<T>(inc_beta(aT, bT, xT), x.d_ * d_x);
 }
+
 template <typename T>
 inline fvar<T> inc_beta(const fvar<T>& a, double b, double x) {
-  return inc_beta(a, fvar<T>(b), fvar<T>(x));
+  T aT = a.val_;
+  T bT = T(b);
+  T xT = T(x);
+  T digamma_ab = digamma(aT + bT);
+  T d_a = inc_beta_dda(aT, bT, xT, digamma(aT), digamma_ab);
+  return fvar<T>(inc_beta(aT, bT, xT), a.d_ * d_a);
 }
+
 template <typename T>
 inline fvar<T> inc_beta(double a, const fvar<T>& b, double x) {
-  return inc_beta(fvar<T>(a), b, fvar<T>(x));
+  T aT = T(a);
+  T bT = b.val_;
+  T xT = T(x);
+  T digamma_ab = digamma(aT + bT);
+  T d_b = inc_beta_ddb(aT, bT, xT, digamma(bT), digamma_ab);
+  return fvar<T>(inc_beta(aT, bT, xT), b.d_ * d_b);
 }
 
 }  // namespace math
--- stan/math/rev/fun/inc_beta.hpp
+++ stan/math/rev/fun/inc_beta.hpp
@@ -3,11 +3,10 @@
 
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/prim/scal/fun/grad_reg_inc_beta.hpp>
-#include <stan/math/rev/fun/pow.hpp>
-#include <stan/math/prim/scal/fun/beta.hpp>
+#include <stan/math/prim/scal/fun/inc_beta_dda.hpp>
+#include <stan/math/prim/scal/fun/inc_beta_ddb.hpp>
+#include <stan/math/prim/scal/fun/inc_beta_ddz.hpp>
 #include <stan/math/prim/scal/fun/digamma.hpp>
-#include <cmath>
 
 namespace stan {
 namespace math {
@@ -19,17 +18,84 @@ class inc_beta_vvv_vari : public op_vvv_vari {
   inc_beta_vvv_vari(vari* avi, vari* bvi, vari* cvi)
       : op_vvv_vari(inc_beta(avi->val_, bvi->val_, cvi->val_), avi, bvi, cvi) {}
   void chain() {
-    double d_a;
-    double d_b;
-    const double beta_ab = beta(avi_->val_, bvi_->val_);
-    grad_reg_inc_beta(d_a, d_b, avi_->val_, bvi_->val_, cvi_->val_,
-                      digamma(avi_->val_), digamma(bvi_->val_),
-                      digamma(avi_->val_ + bvi_->val_), beta_ab);
+    const double& ad = avi_->val_;
+    const double& bd = bvi_->val_;
+    const double& cd = cvi_->val_;
+    const double digamma_abd = digamma(ad + bd);
+    avi_->adj_ += adj_ * inc_beta_dda(ad, bd, cd, digamma(ad), digamma_abd);
+    bvi_->adj_ += adj_ * inc_beta_ddb(ad, bd, cd, digamma(bd), digamma_abd);
+    cvi_->adj_ += adj_ * inc_beta_ddz(ad, bd, cd);
+  }
+};
 
-    avi_->adj_ += adj_ * d_a;
-    bvi_->adj_ += adj_ * d_b;
-    cvi_->adj_ += adj_ * std::pow(1 - cvi_->val_, bvi_->val_ - 1)
-                  * std::pow(cvi_->val_, avi_->val_ - 1) / beta_ab;
+class inc_beta_dvv_vari : public op_dvv_vari {
+ public:
+  inc_beta_dvv_vari(double ad, vari* bvi, vari* cvi)
+      : op_dvv_vari(inc_beta(ad, bvi->val_, cvi->val_), ad, bvi, cvi) {}
+  void chain() {
+    const double& bd = bvi_->val_;
+    const double& cd = cvi_->val_;
+    bvi_->adj_
+        += adj_ * inc_beta_ddb(ad_, bd, cd, digamma(bd), digamma(ad_ + bd));
+    cvi_->adj_ += adj_ * inc_beta_ddz(ad_, bd, cd);
+  }
+};
+
+class inc_beta_vdv_vari : public op_vdv_vari {
+ public:
+  inc_beta_vdv_vari(vari* avi, double bd, vari* cvi)
+      : op_vdv_vari(inc_beta(avi->val_, bd, cvi->val_), avi, bd, cvi) {}
+  void chain() {
+    const double& ad = avi_->val_;
+    const double& cd = cvi_->val_;
+    avi_->adj_
+        += adj_ * inc_beta_dda(ad, bd_, cd, digamma(ad), digamma(ad + bd_));
+    cvi_->adj_ += adj_ * inc_beta_ddz(ad, bd_, cd);
+  }
+};
+
+class inc_beta_vvd_vari : public op_vvd_vari {
+ public:
+  inc_beta_vvd_vari(vari* avi, vari* bvi, const double cd)
+      : op_vvd_vari(inc_beta(avi->val_, bvi->val_, cd), avi, bvi, cd) {}
+  void chain() {
+    const double& ad = avi_->val_;
+    const double& bd = bvi_->val_;
+    const double digamma_abd = digamma(ad + bd);
+    avi_->adj_ += adj_ * inc_beta_dda(ad, bd, cd_, digamma(ad), digamma_abd);
+    bvi_->adj_ += adj_ * inc_beta_ddb(ad, bd, cd_, digamma(bd), digamma_abd);
+  }
+};
+
+class inc_beta_ddv_vari : public op_ddv_vari {
+ public:
+  inc_beta_ddv_vari(const double ad, const double bd, vari* cvi)
+      : op_ddv_vari(inc_beta(ad, bd, cvi->val_), ad, bd, cvi) {}
+  void chain() {
+    const double& cd = cvi_->val_;
+    cvi_->adj_ += adj_ * inc_beta_ddz(ad_, bd_, cd);
+  }
+};
+
+class inc_beta_vdd_vari : public op_vdd_vari {
+ public:
+  inc_beta_vdd_vari(vari* avi, double bd, double cd)
+      : op_vdd_vari(inc_beta(avi->val_, bd, cd), avi, bd, cd) {}
+  void chain() {
+    const double& ad = avi_->val_;
+    avi_->adj_
+        += adj_ * inc_beta_dda(ad, bd_, cd_, digamma(ad), digamma(ad + bd_));
+  }
+};
+
+class inc_beta_dvd_vari : public op_dvd_vari {
+ public:
+  inc_beta_dvd_vari(double ad, vari* bvi, double cd)
+      : op_dvd_vari(inc_beta(ad, bvi->val_, cd), ad, bvi, cd) {}
+  void chain() {
+    const double& bd = bvi_->val_;
+    bvi_->adj_
+        += adj_ * inc_beta_ddb(ad_, bd, cd_, digamma(bd), digamma(ad_ + bd));
   }
 };
 
@@ -37,6 +103,30 @@ class inc_beta_vvv_vari : public op_vvv_vari {
 
 inline var inc_beta(const var& a, const var& b, const var& c) {
   return var(new internal::inc_beta_vvv_vari(a.vi_, b.vi_, c.vi_));
+}
+
+inline var inc_beta(const double a, const var& b, const var& c) {
+  return var(new internal::inc_beta_dvv_vari(a, b.vi_, c.vi_));
+}
+
+inline var inc_beta(const var& a, const double b, const var& c) {
+  return var(new internal::inc_beta_vdv_vari(a.vi_, b, c.vi_));
+}
+
+inline var inc_beta(const var& a, const var& b, const double c) {
+  return var(new internal::inc_beta_vvd_vari(a.vi_, b.vi_, c));
+}
+
+inline var inc_beta(const double a, const double b, const var& c) {
+  return var(new internal::inc_beta_ddv_vari(a, b, c.vi_));
+}
+
+inline var inc_beta(const var& a, const double b, const double c) {
+  return var(new internal::inc_beta_vdd_vari(a.vi_, b, c));
+}
+
+inline var inc_beta(const double a, const var& b, const double c) {
+  return var(new internal::inc_beta_dvd_vari(a, b.vi_, c));
 }
 
 }  // namespace math
