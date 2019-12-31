6d781c42d1d7908575b689547ccbc25b088c16c8
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2019-12-31 08:37:34 +0100

    fix after merge with develop

--- stan/math/opencl/sub_block.hpp
+++ stan/math/opencl/sub_block.hpp
@@ -8,7 +8,6 @@
 #include <stan/math/opencl/matrix_cl_view.hpp>
 #include <stan/math/opencl/matrix_cl.hpp>
 #include <stan/math/opencl/kernel_generator/block.hpp>
-#include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/scal/err/throw_domain_error.hpp>
 #include <CL/cl2.hpp>
 #include <vector>
