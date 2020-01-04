28d4b612bbdf4a46066cce1998f67838345d0135
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-01-04 13:03:25 +0100

    merge value_of and value_of_rec

--- test/unit/math/fwd/fun/value_of_rec_test.cpp
+++ test/unit/math/fwd/fun/value_of_rec_test.cpp
@@ -1,5 +1,74 @@
+#include <stan/math/fwd/arr.hpp>
 #include <stan/math/fwd/mat.hpp>
+#include <stan/math/fwd/scal.hpp>
+#include <stan/math/fwd/core.hpp>
 #include <gtest/gtest.h>
+#include <vector>
+
+
+TEST(AgradFwd, value_of_rec) {
+  using stan::math::fvar;
+
+  fvar<double> a = 5.0;
+  fvar<fvar<double> > ff_a(5.0);
+  fvar<fvar<fvar<fvar<double> > > > ffff_a(5.0);
+  EXPECT_FLOAT_EQ(5.0, value_of_rec(a));
+  EXPECT_FLOAT_EQ(5.0, value_of_rec(ff_a));
+  EXPECT_FLOAT_EQ(5.0, value_of_rec(ffff_a));
+}
+
+TEST(MathMatrixFwdArr, value_of_rec) {
+  using stan::math::fvar;
+  using stan::math::value_of_rec;
+  using std::vector;
+
+  vector<double> a_vals;
+
+  for (size_t i = 0; i < 10; ++i)
+    a_vals.push_back(i + 1);
+
+  vector<double> b_vals;
+
+  for (size_t i = 10; i < 15; ++i)
+    b_vals.push_back(i + 1);
+
+  {
+    vector<fvar<double> > a;
+    a = stan::math::to_fvar(a_vals);
+    vector<fvar<double> > b;
+    b = stan::math::to_fvar(b_vals);
+
+    vector<double> d_a = value_of_rec(a);
+    vector<double> d_b = value_of_rec(b);
+
+    for (int i = 0; i < 5; ++i)
+      EXPECT_FLOAT_EQ(b[i].val_, d_b[i]);
+
+    for (int i = 0; i < 10; ++i)
+      EXPECT_FLOAT_EQ(a[i].val_, d_a[i]);
+  }
+
+  {
+    vector<fvar<double> > a_vals_fd = stan::math::to_fvar(a_vals);
+    vector<fvar<double> > b_vals_fd = stan::math::to_fvar(b_vals);
+    vector<fvar<double> > zeros_fd(10);
+    std::fill(zeros_fd.begin(), zeros_fd.end(), 0);
+
+    vector<fvar<fvar<double> > > a;
+    a = stan::math::to_fvar(a_vals_fd, zeros_fd);
+    vector<fvar<fvar<double> > > b;
+    b = stan::math::to_fvar(b_vals_fd, zeros_fd);
+
+    vector<double> d_a = value_of_rec(a);
+    vector<double> d_b = value_of_rec(b);
+
+    for (int i = 0; i < 5; ++i)
+      EXPECT_FLOAT_EQ(b[i].val_.val_, d_b[i]);
+
+    for (int i = 0; i < 10; ++i)
+      EXPECT_FLOAT_EQ(a[i].val_.val_, d_a[i]);
+  }
+}
 
 TEST(AgradMatrixFwd, value_of_rec) {
   using stan::math::fvar;
--- test/unit/math/fwd/fun/value_of_test.cpp
+++ test/unit/math/fwd/fun/value_of_test.cpp
@@ -1,5 +1,75 @@
+#include <stan/math/fwd/arr.hpp>
 #include <stan/math/fwd/mat.hpp>
+#include <stan/math/fwd/scal.hpp>
 #include <gtest/gtest.h>
+#include <limits>
+#include <vector>
+
+TEST(AgradFwd, value_of) {
+  using stan::math::fvar;
+  using stan::math::value_of;
+
+  fvar<double> a = 5.0;
+  EXPECT_FLOAT_EQ(5.0, value_of(a));
+  // make sure all work together
+  EXPECT_FLOAT_EQ(5.0, value_of(5.0));
+  EXPECT_FLOAT_EQ(5.0, value_of(5));
+}
+
+TEST(AgradFwd, value_of_nan) {
+  using stan::math::fvar;
+  using stan::math::value_of;
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  fvar<double> a = nan;
+  EXPECT_TRUE(stan::math::is_nan(value_of(a)));
+  EXPECT_TRUE(stan::math::is_nan(value_of(nan)));
+}
+
+TEST(MathMatrixFwdArr, value_of) {
+  using stan::math::fvar;
+  using stan::math::value_of;
+  using std::vector;
+
+  vector<double> a_vals;
+  for (size_t i = 0; i < 10; ++i)
+    a_vals.push_back(i + 1);
+
+  vector<double> b_vals;
+  for (size_t i = 10; i < 15; ++i)
+    b_vals.push_back(i + 1);
+
+  vector<fvar<double> > a_fd;
+  a_fd = stan::math::to_fvar(a_vals);
+  vector<fvar<double> > b_fd;
+  b_fd = stan::math::to_fvar(b_vals);
+
+  vector<double> d_a = value_of(a_fd);
+  vector<double> d_b = value_of(b_fd);
+
+  for (int i = 0; i < 5; ++i)
+    EXPECT_FLOAT_EQ(b_fd[i].val_, d_b[i]);
+
+  for (int i = 0; i < 10; ++i)
+    EXPECT_FLOAT_EQ(a_fd[i].val_, d_a[i]);
+
+  vector<fvar<double> > a_zeros(10);
+  vector<fvar<fvar<double> > > a_ffd;
+  a_ffd = stan::math::to_fvar(a_fd, a_zeros);
+
+  vector<fvar<double> > b_zeros(5);
+  vector<fvar<fvar<double> > > b_ffd;
+  b_ffd = stan::math::to_fvar(b_fd, b_zeros);
+
+  vector<fvar<double> > d_a_fd = value_of(a_ffd);
+  vector<fvar<double> > d_b_fd = value_of(b_ffd);
+
+  for (int i = 0; i < 5; ++i)
+    EXPECT_FLOAT_EQ(b_ffd[i].val_.val_, d_b_fd[i].val_);
+
+  for (int i = 0; i < 10; ++i)
+    EXPECT_FLOAT_EQ(a_ffd[i].val_.val_, d_a_fd[i].val_);
+}
 
 TEST(AgradFwdMatrix, value_of) {
   using stan::math::fvar;
