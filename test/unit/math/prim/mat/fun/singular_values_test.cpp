a09f0dd3ff71a9ceb8ffb9741eb6fdc310844bab
Marco Colombo <m.colombo@ed.ac.uk>
2020-01-05 12:48:24 +0000

    Add prim test for size zero input

--- test/unit/math/prim/mat/fun/singular_values_test.cpp
+++ test/unit/math/prim/mat/fun/singular_values_test.cpp
@@ -2,9 +2,12 @@
 #include <gtest/gtest.h>
 
 TEST(MathMatrixPrimMat, singular_values) {
-  stan::math::matrix_d m0(1, 1);
-  m0 << 1.0;
-
   using stan::math::singular_values;
+
+  stan::math::matrix_d m0(0, 0);
   EXPECT_NO_THROW(singular_values(m0));
+
+  stan::math::matrix_d m1(1, 1);
+  m1 << 1.0;
+  EXPECT_NO_THROW(singular_values(m1));
 }
