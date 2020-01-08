4360a77985b07d5aa902a4b43853359d0ae0b8e8
Marco Colombo <m.colombo@ed.ac.uk>
2020-01-08 20:12:23 +0000

    Clarify reason for failing tests

--- test/unit/math/mix/prob/std_normal_cdf_test.cpp
+++ test/unit/math/mix/prob/std_normal_cdf_test.cpp
@@ -5,8 +5,11 @@ TEST(mathMixScalFun, std_normal_cdf_derivatives) {
   auto f = [](const auto& y) { return stan::math::std_normal_cdf(y); };
 
   stan::test::expect_ad(f, -50.0);
+  // the following fails because AD returns -nan in the Hessian
   // stan::test::expect_ad(f, -20.0 * stan::math::SQRT_TWO);
-  // stan::test::expect_ad(f, -10.0);
+  // the following fails because AD returns inf in the Hessian
+  // stan::test::expect_ad(f, -20.0);
+  stan::test::expect_ad(f, -19);
   stan::test::expect_ad(f, -5.5);
   stan::test::expect_ad(f, 0.0);
   stan::test::expect_ad(f, 0.15);
