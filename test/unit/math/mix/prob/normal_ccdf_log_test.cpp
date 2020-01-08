484e955a610c65a1f7417cc9189869a5d45db7ea
Marco Colombo <m.colombo@ed.ac.uk>
2020-01-08 20:09:28 +0000

    Add expect_ad tests for normal_{cdf|lcdf|lccdf}

---
+++ test/unit/math/mix/prob/normal_ccdf_log_test.cpp
@@ -0,0 +1,19 @@
+#include <test/unit/math/test_ad.hpp>
+#include <limits>
+
+TEST(mathMixScalFun, normal_lccdf_derivatives) {
+  auto f = [](const double mu, const double sigma) {
+    return [=](const auto& y) { return stan::math::normal_lccdf(y, mu, sigma); };
+  };
+
+  stan::test::expect_ad(f(0.0, 1.0), -50.0);
+  stan::test::expect_ad(f(0.0, 1.0), -20.0 * stan::math::SQRT_TWO);
+  stan::test::expect_ad(f(0.0, 1.0), -5.5);
+  stan::test::expect_ad(f(0.0, 1.0), 0.0);
+  stan::test::expect_ad(f(0.0, 1.0), 0.15);
+  stan::test::expect_ad(f(0.0, 1.0), 1.14);
+  stan::test::expect_ad(f(0.0, 1.0), 3.00);
+  stan::test::expect_ad(f(0.0, 1.0), 10.00);
+  stan::test::expect_ad(f(-1.0, 2.0), 1.50);
+  stan::test::expect_ad(f(2.0, 1.0), 0.50);
+}
