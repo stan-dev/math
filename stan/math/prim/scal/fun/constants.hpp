96c4946a2e87fc84e8067e5a48ee0b060cef2b2d
Marco Colombo <m.colombo@ed.ac.uk>
2020-01-10 19:19:31 +0000

    Reorder constants and add docs

--- stan/math/prim/scal/fun/constants.hpp
+++ stan/math/prim/scal/fun/constants.hpp
@@ -11,34 +11,23 @@ namespace stan {
 namespace math {
 
 /**
- * The base of the natural logarithm,
- * \f$ e \f$.
+ * Return the base of the natural logarithm.
+ *
+ * @return Base of natural logarithm.
  */
-const double E = boost::math::constants::e<double>();
+inline double e() { return boost::math::constants::e<double>(); }
 
 /**
- * The value of the square root of 2,
- * \f$ \sqrt{2} \f$.
+ * Return the value of pi.
+ *
+ * @return Pi.
  */
-const double SQRT_TWO = std::sqrt(2.0);
+inline double pi() { return boost::math::constants::pi<double>(); }
 
 /**
- * The value of 1 over the square root of 2,
- * \f$ 1 / \sqrt{2} \f$.
+ * Smallest positive value.
  */
-const double INV_SQRT_TWO = inv(SQRT_TWO);
-
-/**
- * The natural logarithm of 2,
- * \f$ \log 2 \f$.
- */
-const double LOG_TWO = std::log(2.0);
-
-/**
- * The natural logarithm of 10,
- * \f$ \log 10 \f$.
- */
-const double LOG_TEN = std::log(10.0);
+const double EPSILON = std::numeric_limits<double>::epsilon();
 
 /**
  * Positive infinity.
@@ -56,28 +45,129 @@ const double NEGATIVE_INFTY = -INFTY;
 const double NOT_A_NUMBER = std::numeric_limits<double>::quiet_NaN();
 
 /**
- * Smallest positive value.
+ * Twice the value of \f$ \pi \f$,
+ * \f$ 2\pi \f$.
  */
-const double EPSILON = std::numeric_limits<double>::epsilon();
+const double TWO_PI = 2.0 * pi();
+
+/**
+ * The natural logarithm of 0,
+ * \f$ \log 0 \f$.
+ */
+const double LOG_ZERO = std::log(0.0);
+
+/**
+ * The natural logarithm of machine precision \f$ \epsilon \f$,
+ * \f$ \log \epsilon \f$.
+ */
+const double LOG_EPSILON = std::log(EPSILON);
+
+/**
+ * The natural logarithm of \f$ \pi \f$,
+ * \f$ \log \pi \f$.
+ */
+const double LOG_PI = std::log(pi());
+
+/**
+ * The natural logarithm of 0.5,
+ * \f$ \log 0.5 \f$.
+ */
+const double LOG_HALF = std::log(0.5);
+
+/**
+ * The natural logarithm of 2,
+ * \f$ \log 2 \f$.
+ */
+const double LOG_TWO = std::log(2.0);
+
+/**
+ * The natural logarithm of 2 plus the natural logarithm of \f$ \pi \f$,
+ * \f$ \log(2\pi) \f$.
+ */
+const double LOG_TWO_PI = LOG_TWO + LOG_PI;
+
+/**
+ * The value of one quarter the natural logarithm of \f$ \pi \f$,
+ * \f$ \log(\pi) / 4 \f$.
+ */
+const double LOG_PI_OVER_FOUR = 0.25 * LOG_PI;
+
+/**
+ * The natural logarithm of the square root of \f$ \pi \f$,
+ * \f$ \log(sqrt{\pi}) \f$.
+ */
+const double LOG_SQRT_PI = std::log(std::sqrt(pi()));
+
+/**
+ * The natural logarithm of 10,
+ * \f$ \log 10 \f$.
+ */
+const double LOG_TEN = std::log(10.0);
+
+/**
+ * The value of the square root of 2,
+ * \f$ \sqrt{2} \f$.
+ */
+const double SQRT_TWO = std::sqrt(2.0);
+
+/**
+ * The value of the square root of \f$ \pi \f$,
+ * \f$ \sqrt{\pi} \f$.
+ */
+const double SQRT_PI = std::sqrt(pi());
+
+/**
+ * The value of the square root of \f$ 2\pi \f$,
+ * \f$ \sqrt{2\pi} \f$.
+ */
+const double SQRT_TWO_PI = std::sqrt(TWO_PI);
+
+/**
+ * The square root of 2 divided by the square root of \f$ \pi \f$,
+ * \f$ \sqrt{2} / \sqrt{\pi} \f$.
+ */
+const double SQRT_TWO_OVER_SQRT_PI = SQRT_TWO / SQRT_PI;
+
+/**
+ * The value of 1 over the square root of 2,
+ * \f$ 1 / \sqrt{2} \f$.
+ */
+const double INV_SQRT_TWO = inv(SQRT_TWO);
+
+/**
+ * The value of 1 over the square root of \f$ \pi \f$,
+ * \f$ 1 / \sqrt{\pi} \f$.
+ */
+const double INV_SQRT_PI = inv(SQRT_PI);
+
+/**
+ * The value of 1 over the square root of \f$ 2\pi \f$,
+ * \f$ 1 / \sqrt{2\pi} \f$.
+ */
+const double INV_SQRT_TWO_PI = inv(SQRT_TWO_PI);
+
+/**
+ * The value of 2 over the square root of \f$ \pi \f$,
+ * \f$ 2 / \sqrt{\pi} \f$.
+ */
+const double TWO_OVER_SQRT_PI = 2.0 / SQRT_PI;
+
+/**
+ * The value of minus half the natural logarithm 2,
+ * \f$ -\log(2) / 2 \f$.
+ */
+const double NEG_LOG_TWO_OVER_TWO = -0.5 * LOG_TWO;
+
+/**
+ * The value of minus the natural logarithm of the square root of \f$ 2\pi \f$,
+ * \f$ -\log(\sqrt{2\pi}) \f$.
+ */
+const double NEG_LOG_SQRT_TWO_PI = -std::log(SQRT_TWO_PI);
 
 /**
  * Largest rate parameter allowed in Poisson RNG
  */
 const double POISSON_MAX_RATE = std::pow(2.0, 30);
-
-/**
- * Return the value of pi.
- *
- * @return Pi.
- */
-inline double pi() { return boost::math::constants::pi<double>(); }
-
-/**
- * Return the base of the natural logarithm.
- *
- * @return Base of natural logarithm.
- */
-inline double e() { return E; }
 
 /**
  * Return positive infinity.
@@ -107,38 +197,6 @@ inline double not_a_number() { return NOT_A_NUMBER; }
  * @return Minimum positive number.
  */
 inline double machine_precision() { return EPSILON; }
-
-const double SQRT_PI = std::sqrt(pi());
-
-const double INV_SQRT_PI = inv(SQRT_PI);
-
-const double TWO_OVER_SQRT_PI = 2.0 / SQRT_PI;
-
-const double SQRT_TWO_OVER_SQRT_PI = SQRT_TWO / SQRT_PI;
-
-const double TWO_PI = 2.0 * pi();
-
-const double SQRT_TWO_PI = std::sqrt(TWO_PI);
-
-const double INV_SQRT_TWO_PI = inv(SQRT_TWO_PI);
-
-const double LOG_PI = std::log(pi());
-
-const double LOG_PI_OVER_FOUR = LOG_PI / 4.0;
-
-const double LOG_SQRT_PI = std::log(SQRT_PI);
-
-const double LOG_ZERO = std::log(0.0);
-
-const double LOG_HALF = std::log(0.5);
-
-const double NEG_LOG_SQRT_TWO_PI = -std::log(SQRT_TWO_PI);
-
-const double NEG_LOG_TWO_OVER_TWO = -LOG_TWO / 2.0;
-
-const double LOG_TWO_PI = LOG_TWO + LOG_PI;
-
-const double LOG_EPSILON = std::log(EPSILON);
 
 }  // namespace math
 }  // namespace stan
