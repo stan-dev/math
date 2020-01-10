83e414b520117dcbc027f5f34e92e0e7aefad160
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-01-10 08:28:30 +0100

    fix includes in test/prob

--- test/prob/neg_binomial/neg_binomial_test.hpp
+++ test/prob/neg_binomial/neg_binomial_test.hpp
@@ -1,5 +1,5 @@
 // Arguments: Ints, Doubles, Doubles
-#include <stan/math/prim/scal.hpp>
+#include <stan/math/prim.hpp>
 #include <stdexcept>
 
 using stan::math::var;
--- test/prob/wiener/wiener_test.hpp
+++ test/prob/wiener/wiener_test.hpp
@@ -1,5 +1,5 @@
 // Arguments: Doubles, Doubles, Doubles, Doubles, Doubles
-#include <stan/math/prim/scal.hpp>
+#include <stan/math/prim.hpp>
 #include <limits>
 #include <vector>
 
