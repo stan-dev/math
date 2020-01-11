c4d575e07ab07fcf7dda5e178b0d82a47cf293a3
Stan Jenkins <mc.stanislaw@gmail.com>
2020-01-11 20:20:03 +0000

    [Jenkins] auto-formatting by clang-format version 5.0.0-3~16.04.1 (tags/RELEASE_500/final)

--- stan/math/rev/fun/inverse.hpp
+++ stan/math/rev/fun/inverse.hpp
@@ -1,7 +1,6 @@
 #ifndef STAN_MATH_REV_FUN_INVERSE_HPP
 #define STAN_MATH_REV_FUN_INVERSE_HPP
 
-
 #include <stan/math/rev/core.hpp>
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/fun/typedefs.hpp>
