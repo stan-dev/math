2e1c73bceca601693150c4a70972afcee76fc9a8
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-01-11 13:33:18 -0600

    Add boundary case tests for inc_beta.

--- test/unit/math/mix/fun/inc_beta_test.cpp
+++ test/unit/math/mix/fun/inc_beta_test.cpp
@@ -3,24 +3,27 @@
 #include <boost/math/special_functions/beta.hpp>
 #include <stan/math/prim/scal/fun/boost_policy.hpp>
 
-TEST(mathMixScalFun, inc_beta) {
-  auto f = [](const auto& x1, const auto& x2, const auto& x3) {
-    return stan::math::inc_beta(x1, x2, x3);
-  };
+auto f = [](const auto& a, const auto& b, const auto& x) {
+  return stan::math::inc_beta(a, b, x);
+};
 
+TEST(mathMixScalFun, inc_beta) {
   // replicate pre-existing test cases
   stan::test::expect_ad(f, 0.6, 0.3, 0.5);
   stan::test::expect_ad(f, 0.9, 0.9, 0.9);
   stan::test::expect_ad(f, 1.0, 1.0, 0.4);
+}
 
-  // integer instantiations
+TEST(mathMixScalFun, inc_beta_integer) {
   stan::test::expect_ad(f, 3, 2, 0.2);
   stan::test::expect_ad(f, 3, 2.0, 0.2);
   stan::test::expect_ad(f, 3.0, 2, 0.2);
   stan::test::expect_ad(f, 3.0, 2.0, 0.2);
+}
 
-  // test all nan instantiations
-  double nan = std::numeric_limits<double>::quiet_NaN();
+TEST(mathMixScalFun, inc_beta_nan) {
+  double nan = stan::math::NOT_A_NUMBER;
+
   stan::test::expect_ad(f, 0.6, 0.3, nan);
   stan::test::expect_ad(f, 0.6, nan, 0.5);
   stan::test::expect_ad(f, 0.6, nan, nan);
@@ -42,4 +45,35 @@ TEST(mathMixScalFun, ibeta_vs_ibeta_derivative) {
                std::domain_error);
   EXPECT_THROW(boost::math::ibeta_derivative(0, 0, 0.5, boost_policy_t()),
                std::domain_error);
+}
+
+// Don't try to finite difference right next to the boundaries, the perturbation
+// goes across and causes spurious failures.
+
+TEST(mathMixScalFun, inc_beta_a_boundary) {
+  const double inf = stan::math::INFTY;
+  // Succeeds when called with doubles, throws exception when called with
+  // autodiff types, due to different behavior of boost's ibeta and
+  // ibeta_derivative.
+  // stan::test::expect_value(f, 0, 0.5, 0.5);
+  stan::test::expect_ad(f, inf, 0.5, 0.5);
+  stan::test::expect_all_throw(f, -0.1, 0.5, 0.5);
+}
+
+TEST(mathMixScalFun, inc_beta_b_boundary) {
+  const double inf = stan::math::INFTY;
+  // stan::test::expect_value(f, 0.5, 0, 0.5);
+  stan::test::expect_ad(f, 0.5, inf, 0.5);
+  stan::test::expect_all_throw(f, 0.5, -0.1, 0.5);
+}
+
+TEST(mathMixScalFun, inc_beta_x_boundary) {
+  // stan::test::expect_value(f, 0.5, 0.5, 0);
+  // stan::test::expect_value(f, 0.5, 0.5, 1);
+  stan::test::expect_ad(f, 0.5, 0.5, -0.1);
+  stan::test::expect_ad(f, 0.5, 0.5, 1.1);
+}
+
+TEST(mathMixScalFun, inc_beta_a_b_boundary) {
+  stan::test::expect_ad(f, 0.0, 0.0, 0.5);
 }
