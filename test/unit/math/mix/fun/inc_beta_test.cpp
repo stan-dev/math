114d4cd9d8e3978623208ce5e1a8a75813b3389b
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-01-12 12:14:00 -0600

    Cleanup tests.

--- test/unit/math/mix/fun/inc_beta_test.cpp
+++ test/unit/math/mix/fun/inc_beta_test.cpp
@@ -33,41 +33,21 @@ TEST(mathMixScalFun, inc_beta_nan) {
   stan::test::expect_ad(f, nan, nan, nan);
 }
 
-TEST(mathMixScalFun, ibeta_vs_ibeta_derivative) {
-  using stan::math::boost_policy_t;
-  EXPECT_NO_THROW(boost::math::ibeta(0, 0.5, 0.5, boost_policy_t()));
-  EXPECT_NO_THROW(boost::math::ibeta(0.5, 0, 0.5, boost_policy_t()));
-  EXPECT_THROW(boost::math::ibeta(0, 0, 0.5, boost_policy_t()),
-               std::domain_error);
-  EXPECT_THROW(boost::math::ibeta_derivative(0, 0.5, 0.5, boost_policy_t()),
-               std::domain_error);
-  EXPECT_THROW(boost::math::ibeta_derivative(0.5, 0, 0.5, boost_policy_t()),
-               std::domain_error);
-  EXPECT_THROW(boost::math::ibeta_derivative(0, 0, 0.5, boost_policy_t()),
-               std::domain_error);
-}
-
-// Don't try to finite difference right next to the boundaries, the perturbation
-// goes across and causes spurious failures.
-
 TEST(mathMixScalFun, inc_beta_a_boundary) {
   const double inf = stan::math::INFTY;
-  // Succeeds when called with doubles, throws exception when called with
-  // autodiff types, due to different behavior of boost's ibeta and
-  // ibeta_derivative.
-  // stan::test::expect_value(f, 0, 0.5, 0.5);
   stan::test::expect_ad(f, inf, 0.5, 0.5);
   stan::test::expect_all_throw(f, -0.1, 0.5, 0.5);
 }
 
 TEST(mathMixScalFun, inc_beta_b_boundary) {
   const double inf = stan::math::INFTY;
-  // stan::test::expect_value(f, 0.5, 0, 0.5);
   stan::test::expect_ad(f, 0.5, inf, 0.5);
   stan::test::expect_all_throw(f, 0.5, -0.1, 0.5);
 }
 
 TEST(mathMixScalFun, inc_beta_x_boundary) {
+  // Don't try to finite difference right next to the boundaries, the perturbation
+  // goes across and causes spurious failures.
   stan::test::expect_value(f, 0.5, 0.5, 0);
   stan::test::expect_value(f, 0.5, 0.5, 1);
   stan::test::expect_ad(f, 0.5, 0.5, -0.1);
