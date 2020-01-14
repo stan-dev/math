c74b15219502ed43bbc4379a38cc90ef4f4a5ed8
martin_cerny <cerny.m@gmail.com>
2020-01-14 17:37:16 +0100

    Fixed headers

--- stan/math/prim/fun/lbeta.hpp
+++ stan/math/prim/fun/lbeta.hpp
@@ -4,13 +4,14 @@
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun/constants.hpp>
+#include <stan/math/prim/fun/inv.hpp>
 #include <stan/math/prim/fun/is_any_nan.hpp>
 #include <stan/math/prim/fun/lgamma.hpp>
 #include <stan/math/prim/fun/lgamma_stirling.hpp>
 #include <stan/math/prim/fun/lgamma_stirling_diff.hpp>
 #include <stan/math/prim/fun/log_sum_exp.hpp>
+#include <stan/math/prim/fun/log1m.hpp>
 #include <stan/math/prim/fun/multiply_log.hpp>
-#include <stan/math/prim/fun/inv.hpp>
 
 namespace stan {
 namespace math {
