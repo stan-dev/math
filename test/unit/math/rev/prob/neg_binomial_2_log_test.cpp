c20490da9c15f282ca20b8c79d64f3f858e3b55a
martin_cerny <cerny.m@gmail.com>
2020-01-17 09:20:58 +0100

    Small direct derivatives test

--- test/unit/math/rev/prob/neg_binomial_2_log_test.cpp
+++ test/unit/math/rev/prob/neg_binomial_2_log_test.cpp
@@ -1,9 +1,11 @@
 #include <stan/math/rev.hpp>
+#include <test/unit/math/test_ad.hpp>
+#include <gtest/gtest.h>
 #include <boost/math/differentiation/finite_difference.hpp>
 #include <boost/math/special_functions/digamma.hpp>
-#include <gtest/gtest.h>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 namespace neg_binomial_2_log_test_internal {
 struct TestValue {
@@ -545,4 +547,14 @@ TEST(ProbDistributionsNegBinomial2, derivativesComplexStep) {
       }
     }
   }
+}
+
+TEST(mathMixScalFun, neg_binomial_2_log_lpmf_derivatives) {
+  auto f1 = [](const auto& eta, const auto& phi) { return stan::math::neg_binomial_2_log_lpmf(0, eta, phi); };
+  auto f2 = [](const auto& eta, const auto& phi) { return stan::math::neg_binomial_2_log_lpmf(6, eta, phi); };
+
+  stan::test::expect_ad(f1, -1.5, 4.1);
+  stan::test::expect_ad(f1, 2.0, 1.1);
+  stan::test::expect_ad(f2, -1.5, 4.1);
+  stan::test::expect_ad(f2, 2.0, 1.1);
 }
