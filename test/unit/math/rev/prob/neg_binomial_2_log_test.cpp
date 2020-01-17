b86512ae097658002575069a53f856254d5703b3
Stan Jenkins <mc.stanislaw@gmail.com>
2020-01-17 03:21:51 -0500

    [Jenkins] auto-formatting by clang-format version 5.0.2-svn328729-1~exp1~20180509124008.99 (branches/release_50)

--- test/unit/math/rev/prob/neg_binomial_2_log_test.cpp
+++ test/unit/math/rev/prob/neg_binomial_2_log_test.cpp
@@ -550,8 +550,12 @@ TEST(ProbDistributionsNegBinomial2, derivativesComplexStep) {
 }
 
 TEST(mathMixScalFun, neg_binomial_2_log_lpmf_derivatives) {
-  auto f1 = [](const auto& eta, const auto& phi) { return stan::math::neg_binomial_2_log_lpmf(0, eta, phi); };
-  auto f2 = [](const auto& eta, const auto& phi) { return stan::math::neg_binomial_2_log_lpmf(6, eta, phi); };
+  auto f1 = [](const auto& eta, const auto& phi) {
+    return stan::math::neg_binomial_2_log_lpmf(0, eta, phi);
+  };
+  auto f2 = [](const auto& eta, const auto& phi) {
+    return stan::math::neg_binomial_2_log_lpmf(6, eta, phi);
+  };
 
   stan::test::expect_ad(f1, -1.5, 4.1);
   stan::test::expect_ad(f1, 2.0, 1.1);
