fb2de8aa8b56d5291ee399dfcf4567f58f580e8a
tadej <tadej.c@gmail.com>
2020-01-21 08:45:35 +0100

    generalized transpose.hpp() input

--- stan/math/prim/fun/transpose.hpp
+++ stan/math/prim/fun/transpose.hpp
@@ -1,14 +1,21 @@
 #ifndef STAN_MATH_PRIM_FUN_TRANSPOSE_HPP
 #define STAN_MATH_PRIM_FUN_TRANSPOSE_HPP
 
+#include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
 
 namespace stan {
 namespace math {
 
-template <typename T, int R, int C>
-Eigen::Matrix<T, C, R> inline transpose(const Eigen::Matrix<T, R, C>& m) {
-  return m.transpose();
+/**
+ * Transposes a matrix.
+ * @tparam T type of the matrix or expression
+ * @param m matrix or expression
+ * @return transposed matrix
+ */
+template <typename T, typename = require_eigen_t<T>>
+auto inline transpose(const T& m) {
+  return m.transpose().eval();
 }
 
 }  // namespace math
