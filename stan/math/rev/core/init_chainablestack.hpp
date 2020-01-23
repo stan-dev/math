d8f4326582be679081ba7170cdfa242eb1c7211e
Stan Jenkins <mc.stanislaw@gmail.com>
2020-01-23 07:17:02 -0500

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- stan/math/rev/core/init_chainablestack.hpp
+++ stan/math/rev/core/init_chainablestack.hpp
@@ -33,9 +33,7 @@ class ad_tape_observer : public tbb::task_scheduler_observer {
     observe(true);             // activates the observer
   }
 
-  ~ad_tape_observer() {
-    observe(false);
-  }
+  ~ad_tape_observer() { observe(false); }
 
   void on_scheduler_entry(bool worker) {
     std::lock_guard<std::mutex> thread_tape_map_lock(thread_tape_map_mutex_);
