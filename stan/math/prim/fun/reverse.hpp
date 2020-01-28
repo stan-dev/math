432a722a26d4c402065cc96934a5edb690fc58c8
Marco Colombo <m.colombo@ed.ac.uk>
2020-01-28 14:07:55 +0000

    Use apply_vector_unary to unify implementations

--- stan/math/prim/fun/reverse.hpp
+++ stan/math/prim/fun/reverse.hpp
@@ -2,50 +2,27 @@
 #define STAN_MATH_PRIM_FUN_REVERSE_HPP
 
 #include <stan/math/prim/fun/Eigen.hpp>
-#include <algorithm>
-#include <vector>
+#include <stan/math/prim/vectorize/apply_vector_unary.hpp>
 
 namespace stan {
 namespace math {
 
 /**
- * Return a copy of the specified array in reversed order.
+ * Return a copy of the specified vector, row vector or array
+ * in reversed order.
  *
- * @tparam T type of elements in the array
- * @param x array to reverse
- * @return Array in reversed order.
+ * This implicitly allows for reverse with nested arrays (e.g.
+ * reverse(std::vector<VectorXd>)) which returns an array with each vector
+ * reversed.
+ *
+ * @tparam T type of container
+ * @param x container to reverse
+ * @return Container in reversed order.
  */
 template <typename T>
-inline std::vector<T> reverse(const std::vector<T>& x) {
-  std::vector<T> rev(x.size());
-  std::reverse_copy(x.begin(), x.end(), rev.begin());
-  return rev;
-}
-
-/**
- * Return a copy of the specified vector in reversed order.
- *
- * @tparam T type of elements in the vector
- * @param x vector to reverse
- * @return Vector in reversed order.
- */
-template <typename T>
-inline Eigen::Matrix<T, Eigen::Dynamic, 1> reverse(
-    const Eigen::Matrix<T, Eigen::Dynamic, 1>& x) {
-  return x.reverse();
-}
-
-/**
- * Return a copy of the specified row vector in reversed order.
- *
- * @tparam T type of elements in the row vector
- * @param x row vector to reverse
- * @return Row vector in reversed order.
- */
-template <typename T>
-inline Eigen::Matrix<T, 1, Eigen::Dynamic> reverse(
-    const Eigen::Matrix<T, 1, Eigen::Dynamic>& x) {
-  return x.reverse();
+inline auto reverse(const T& x) {
+  return apply_vector_unary<T>::apply(
+      x, [&](const auto& v) { return v.reverse(); });
 }
 
 }  // namespace math
