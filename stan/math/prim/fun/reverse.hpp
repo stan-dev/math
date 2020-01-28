4df4edf593d6d9e3f9f197e7949e3b9079bf1200
Marco Colombo <m.colombo@ed.ac.uk>
2020-01-28 23:38:53 +0000

    Revert to not using apply_vector_unary to disallow elementwise reverse

--- stan/math/prim/fun/reverse.hpp
+++ stan/math/prim/fun/reverse.hpp
@@ -2,27 +2,37 @@
 #define STAN_MATH_PRIM_FUN_REVERSE_HPP
 
 #include <stan/math/prim/fun/Eigen.hpp>
-#include <stan/math/prim/vectorize/apply_vector_unary.hpp>
+#include <algorithm>
+#include <vector>
 
 namespace stan {
 namespace math {
 
 /**
- * Return a copy of the specified vector, row vector or array
- * in reversed order.
+ * Return a copy of the specified array in reversed order.
  *
- * This implicitly allows for reverse with nested arrays (e.g.
- * reverse(std::vector<VectorXd>)) which returns an array with each vector
- * reversed.
- *
- * @tparam T type of container
- * @param x container to reverse
- * @return Container in reversed order.
+ * @tparam T type of elements in the array
+ * @param x array to reverse
+ * @return Array in reversed order.
  */
 template <typename T>
+inline std::vector<T> reverse(const std::vector<T>& x) {
+  std::vector<T> rev(x.size());
+  std::reverse_copy(x.begin(), x.end(), rev.begin());
+  return rev;
+}
+
+/**
+ * Return a copy of the specified vector or row vector
+ * in reversed order.
+ *
+ * @tparam T type of container (vector or row vector)
+ * @param x vector or row vector to reverse
+ * @return Vector or row vector in reversed order.
+ */
+template <typename T, typename = require_eigen_vector_t<T>>
 inline auto reverse(const T& x) {
-  return apply_vector_unary<T>::apply(
-      x, [&](const auto& v) { return v.reverse(); });
+  return x.reverse();
 }
 
 }  // namespace math
