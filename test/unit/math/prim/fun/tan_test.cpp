b7e741b6f62d1e0ac013ff571b53085d23899bb9
tadej <tadej.c@gmail.com>
2020-01-28 09:23:15 +0100

    fix copy-paste

--- test/unit/math/prim/fun/tan_test.cpp
+++ test/unit/math/prim/fun/tan_test.cpp
@@ -1,9 +1,9 @@
 #include <stan/math/prim.hpp>
 
-TEST(MathFunctions, acos_works_with_other_functions) {
+TEST(MathFunctions, tan_works_with_other_functions) {
   Eigen::VectorXd a(5);
   a << 1.1, 1.2, 1.3, 1.4, 1.5;
   Eigen::RowVectorXd b(5);
   b << 1.1, 1.2, 1.3, 1.4, 1.5;
-  stan::math::multiply(a, stan::math::acos(b));
+  stan::math::multiply(a, stan::math::tan(b));
 }
