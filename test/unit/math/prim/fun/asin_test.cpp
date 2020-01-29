9689cb6817903ee400aead908be13e5fb3ea8d46
tadej <tadej.c@gmail.com>
2020-01-29 08:55:35 +0100

    fix some more test input values

--- test/unit/math/prim/fun/asin_test.cpp
+++ test/unit/math/prim/fun/asin_test.cpp
@@ -8,8 +8,8 @@ TEST(primScalFun, asin) {
 
 TEST(MathFunctions, asin_works_with_other_functions) {
   Eigen::VectorXd a(5);
-  a << 1.1, 1.2, 1.3, 1.4, 1.5;
+  a << 0.1, 0.2, 0.3, 0.4, 0.5;
   Eigen::RowVectorXd b(5);
-  b << 1.1, 1.2, 1.3, 1.4, 1.5;
+  b << 0.1, 0.2, 0.3, 0.4, 0.5;
   stan::math::multiply(a, stan::math::asin(b));
 }
--- test/unit/math/prim/fun/inv_Phi_test.cpp
+++ test/unit/math/prim/fun/inv_Phi_test.cpp
@@ -37,8 +37,8 @@ TEST(MathFunctions, inv_Phi_nan) {
 
 TEST(MathFunctions, inv_Phi_works_with_other_functions) {
   Eigen::VectorXd a(5);
-  a << 1.1, 1.2, 1.3, 1.4, 1.5;
+  a << 0.1, 0.2, 0.3, 0.4, 0.5;
   Eigen::RowVectorXd b(5);
-  b << 1.1, 1.2, 1.3, 1.4, 1.5;
+  b << 0.1, 0.2, 0.3, 0.4, 0.5;
   stan::math::multiply(a, stan::math::inv_Phi(b));
 }
