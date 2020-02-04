50702ef7c7244b9fa0386a21032a16945840df54
Marco Colombo <m.colombo@ed.ac.uk>
2020-02-04 11:00:59 +0000

    Move mix test to the mix directory

--- test/unit/math/rev/prob/neg_binomial_2_log_test.cpp
+++ test/unit/math/rev/prob/neg_binomial_2_log_test.cpp
@@ -1,11 +1,10 @@
 #include <stan/math/rev.hpp>
-#include <test/unit/math/test_ad.hpp>
 #include <gtest/gtest.h>
 #include <boost/math/differentiation/finite_difference.hpp>
 #include <boost/math/special_functions/digamma.hpp>
-#include <vector>
 #include <algorithm>
 #include <limits>
+#include <vector>
 
 namespace neg_binomial_2_log_test_internal {
 struct TestValue {
@@ -455,7 +454,7 @@ TEST(ProbDistributionsNegBinomial2Log, derivativesPrecomputed) {
   }
 }
 
-TEST(ProbDistributionsNegBinomial2, derivativesComplexStep) {
+TEST(ProbDistributionsNegBinomial2Log, derivativesComplexStep) {
   using boost::math::differentiation::complex_step_derivative;
   using stan::math::is_nan;
   using stan::math::neg_binomial_2_log_lpmf;
@@ -547,18 +546,4 @@ TEST(ProbDistributionsNegBinomial2, derivativesComplexStep) {
       }
     }
   }
-}
-
-TEST(mathMixScalFun, neg_binomial_2_log_lpmf_derivatives) {
-  auto f1 = [](const auto& eta, const auto& phi) {
-    return stan::math::neg_binomial_2_log_lpmf(0, eta, phi);
-  };
-  auto f2 = [](const auto& eta, const auto& phi) {
-    return stan::math::neg_binomial_2_log_lpmf(6, eta, phi);
-  };
-
-  stan::test::expect_ad(f1, -1.5, 4.1);
-  stan::test::expect_ad(f1, 2.0, 1.1);
-  stan::test::expect_ad(f2, -1.5, 4.1);
-  stan::test::expect_ad(f2, 2.0, 1.1);
 }
