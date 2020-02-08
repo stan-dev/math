a8b17725b6e08cb6c23ce6065d66ae71e2100668
Mitzi Morris <mitzi@panix.com>
2020-02-08 09:57:48 -0500

    new fn check_multiplicable_positive

--- stan/math/prim/err/check_multiplicable_positive.hpp
+++ stan/math/prim/err/check_multiplicable_positive.hpp
@@ -1,15 +1,17 @@
-#ifndef STAN_MATH_PRIM_ERR_CHECK_MULTIPLICABLE_HPP
-#define STAN_MATH_PRIM_ERR_CHECK_MULTIPLICABLE_HPP
+#ifndef STAN_MATH_PRIM_ERR_CHECK_MULTIPLICABLE_POSITIVE_HPP
+#define STAN_MATH_PRIM_ERR_CHECK_MULTIPLICABLE_POSITIVE_HPP
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/prim/err/check_positive.hpp>
 #include <stan/math/prim/err/check_size_match.hpp>
 
 namespace stan {
 namespace math {
 
 /**
- * Throw exception if the matrices cannot be multiplied.
+ * Throw exception if the matrices cannot be multiplied or
+ * if any row or column has size 0.
  * This requires the number of the columns of the first matrix
  * to match the number of rows of the second matrix.
  *
@@ -21,13 +23,17 @@ namespace math {
  * @param name2 name of second matrix
  * @param y2 second matrix
  * @throw <code>std::invalid_argument</code> if the matrices are not
- *   multiplicable
+ *   multiplicable_positive
  */
 template <typename T1, typename T2>
-inline void check_multiplicable(const char* function, const char* name1,
+inline void check_multiplicable_positive(const char* function, const char* name1,
                                 const T1& y1, const char* name2, const T2& y2) {
+
+  check_positive(function, name1, "rows()", y1.rows());
+  check_positive(function, name2, "cols()", y2.cols());
   check_size_match(function, "Columns of ", name1, y1.cols(), "Rows of ", name2,
                    y2.rows());
+  check_positive(function, name1, "cols()", y1.cols());
 }
 }  // namespace math
 }  // namespace stan
---
+++ test/unit/math/prim/err/check_multiplicable_positive_test.cpp
@@ -0,0 +1,82 @@
+#include <stan/math/prim.hpp>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <limits>
+
+TEST(ErrorHandlingMatrix, checkMultiplicablePositiveMatrix) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> y;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> x;
+
+  y.resize(3, 3);
+  x.resize(3, 3);
+  EXPECT_NO_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y));
+  x.resize(3, 2);
+  y.resize(2, 4);
+  EXPECT_NO_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y));
+
+  y.resize(1, 2);
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+
+  x.resize(2, 2);
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+}
+
+TEST(ErrorHandlingMatrix, checkMultiplicablePositiveMatrix_0) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> y;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> x;
+
+  x.resize(3, 0);
+  y.resize(0, 3);
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+
+  x.resize(0, 4);
+  y.resize(4, 3);
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+
+  x.resize(3, 4);
+  y.resize(4, 0);
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+}
+
+TEST(ErrorHandlingMatrix, checkMultiplicablePositiveMatrix_nan) {
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> y;
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> x;
+  double nan = std::numeric_limits<double>::quiet_NaN();
+
+  y.resize(3, 3);
+  x.resize(3, 3);
+  y << nan, nan, nan, nan, nan, nan, nan, nan, nan;
+  x << nan, nan, nan, nan, nan, nan, nan, nan, nan;
+  EXPECT_NO_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y));
+  x.resize(3, 2);
+  y.resize(2, 4);
+  y << nan, nan, nan, nan, nan, nan, nan, nan;
+  x << nan, nan, nan, nan, nan, nan;
+  EXPECT_NO_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y));
+
+  y.resize(1, 2);
+  y << nan, nan;
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+
+  x.resize(2, 2);
+  x << nan, nan, nan, nan;
+  EXPECT_THROW(
+      stan::math::check_multiplicable_positive("checkMultiplicablePositive", "x", x, "y", y),
+      std::invalid_argument);
+}
