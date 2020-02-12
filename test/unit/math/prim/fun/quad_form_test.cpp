23eded070ca8dacda30e57feb1ec9c475308bdca
Marco Colombo <m.colombo@ed.ac.uk>
2020-02-12 12:30:19 +0000

    Add more tests for boundary conditions

--- test/unit/math/prim/fun/quad_form_test.cpp
+++ test/unit/math/prim/fun/quad_form_test.cpp
@@ -3,6 +3,24 @@
 
 TEST(MathMatrixPrim, quad_form_mat) {
   using stan::math::matrix_d;
+  using stan::math::quad_form;
+
+  matrix_d resd;
+  matrix_d m0;
+  EXPECT_THROW(quad_form(m0, m0), std::invalid_argument);
+
+  matrix_d m1(1, 1);
+  m1 << 2;
+  resd = quad_form(m1, m1);
+  EXPECT_FLOAT_EQ(8, resd(0));
+
+  matrix_d m2(1, 2);
+  m2 << 1, 2;
+  resd = quad_form(m1, m2);
+  EXPECT_FLOAT_EQ(2, resd(0, 0));
+  EXPECT_FLOAT_EQ(4, resd(0, 1));
+  EXPECT_FLOAT_EQ(4, resd(1, 0));
+  EXPECT_FLOAT_EQ(8, resd(1, 1));
 
   matrix_d ad(4, 4);
   matrix_d bd(4, 2);
@@ -11,8 +29,7 @@ TEST(MathMatrixPrim, quad_form_mat) {
   ad << 2.0, 3.0, 4.0, 5.0, 6.0, 10.0, 2.0, 2.0, 7.0, 2.0, 7.0, 1.0, 8.0, 2.0,
       1.0, 112.0;
 
-  // double-double
-  matrix_d resd = stan::math::quad_form(ad, bd);
+  resd = quad_form(ad, bd);
   EXPECT_FLOAT_EQ(26033, resd(0, 0));
   EXPECT_FLOAT_EQ(3456, resd(0, 1));
   EXPECT_FLOAT_EQ(3396, resd(1, 0));
@@ -21,7 +38,18 @@ TEST(MathMatrixPrim, quad_form_mat) {
 
 TEST(MathMatrixPrim, quad_form_vec) {
   using stan::math::matrix_d;
+  using stan::math::quad_form;
   using stan::math::vector_d;
+
+  matrix_d m0;
+  vector_d v0;
+  EXPECT_THROW(quad_form(m0, v0), std::invalid_argument);
+
+  matrix_d m1(1, 1);
+  m1 << 2;
+  vector_d v1(1);
+  v1 << 2;
+  EXPECT_FLOAT_EQ(8, quad_form(m1, v1));
 
   matrix_d ad(4, 4);
   vector_d bd(4);
@@ -31,7 +59,6 @@ TEST(MathMatrixPrim, quad_form_vec) {
   ad << 2.0, 3.0, 4.0, 5.0, 6.0, 10.0, 2.0, 2.0, 7.0, 2.0, 7.0, 1.0, 8.0, 2.0,
       1.0, 112.0;
 
-  // double-double
   res = stan::math::quad_form(ad, bd);
   EXPECT_FLOAT_EQ(26033, res);
 }
