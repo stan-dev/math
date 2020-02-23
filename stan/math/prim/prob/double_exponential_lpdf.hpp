72044dc297c71bcf26db929dd4ab8e597fd5cf7e
Marco Colombo <m.colombo@ed.ac.uk>
2020-02-23 21:48:51 +0000

    Reuse intermediate computations in exponential_* and double_exponential_*

--- stan/math/prim/prob/double_exponential_lpdf.hpp
+++ stan/math/prim/prob/double_exponential_lpdf.hpp
@@ -5,11 +5,13 @@
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun/constants.hpp>
 #include <stan/math/prim/fun/fabs.hpp>
+#include <stan/math/prim/fun/inv.hpp>
 #include <stan/math/prim/fun/max_size.hpp>
 #include <stan/math/prim/fun/log.hpp>
 #include <stan/math/prim/fun/sign.hpp>
 #include <stan/math/prim/fun/size.hpp>
 #include <stan/math/prim/fun/size_zero.hpp>
+#include <stan/math/prim/fun/square.hpp>
 #include <stan/math/prim/fun/value_of.hpp>
 #include <cmath>
 
@@ -57,34 +59,33 @@ return_type_t<T_y, T_loc, T_scale> double_exponential_lpdf(
   scalar_seq_view<T_y> y_vec(y);
   scalar_seq_view<T_loc> mu_vec(mu);
   scalar_seq_view<T_scale> sigma_vec(sigma);
+  size_t size_sigma = stan::math::size(sigma);
   size_t N = max_size(y, mu, sigma);
   operands_and_partials<T_y, T_loc, T_scale> ops_partials(y, mu, sigma);
 
   VectorBuilder<include_summand<propto, T_y, T_loc, T_scale>::value,
                 T_partials_return, T_scale>
-      inv_sigma(size(sigma));
+      inv_sigma(size_sigma);
   VectorBuilder<!is_constant_all<T_scale>::value, T_partials_return, T_scale>
-      inv_sigma_squared(size(sigma));
+      inv_sigma_squared(size_sigma);
   VectorBuilder<include_summand<propto, T_scale>::value, T_partials_return,
                 T_scale>
-      log_sigma(size(sigma));
-  for (size_t i = 0; i < stan::math::size(sigma); i++) {
+      log_sigma(size_sigma);
+  for (size_t i = 0; i < size_sigma; i++) {
     const T_partials_return sigma_dbl = value_of(sigma_vec[i]);
-    inv_sigma[i] = 1.0 / sigma_dbl;
+    inv_sigma[i] = inv(sigma_dbl);
     if (include_summand<propto, T_scale>::value) {
-      log_sigma[i] = log(value_of(sigma_vec[i]));
+      log_sigma[i] = log(sigma_dbl);
     }
     if (!is_constant_all<T_scale>::value) {
-      inv_sigma_squared[i] = inv_sigma[i] * inv_sigma[i];
+      inv_sigma_squared[i] = square(inv_sigma[i]);
     }
   }
 
   for (size_t n = 0; n < N; n++) {
     const T_partials_return y_dbl = value_of(y_vec[n]);
     const T_partials_return mu_dbl = value_of(mu_vec[n]);
-
-    const T_partials_return y_m_mu = y_dbl - mu_dbl;
-    const T_partials_return fabs_y_m_mu = fabs(y_m_mu);
+    const T_partials_return scaled_diff = fabs(y_dbl - mu_dbl) * inv_sigma[n];
 
     if (include_summand<propto>::value) {
       logp -= LOG_TWO;
@@ -92,21 +93,19 @@ return_type_t<T_y, T_loc, T_scale> double_exponential_lpdf(
     if (include_summand<propto, T_scale>::value) {
       logp -= log_sigma[n];
     }
-    logp -= fabs_y_m_mu * inv_sigma[n];
+    logp -= scaled_diff;
 
-    T_partials_return sign_y_m_mu_times_inv_sigma(0);
-    if (!is_constant_all<T_y, T_loc>::value) {
-      sign_y_m_mu_times_inv_sigma = sign(y_m_mu) * inv_sigma[n];
-    }
+    T_partials_return rep_deriv = is_constant_all<T_y, T_loc>::value
+                                      ? 0
+                                      : sign(scaled_diff) * inv_sigma[n];
     if (!is_constant_all<T_y>::value) {
-      ops_partials.edge1_.partials_[n] -= sign_y_m_mu_times_inv_sigma;
+      ops_partials.edge1_.partials_[n] -= rep_deriv;
     }
     if (!is_constant_all<T_loc>::value) {
-      ops_partials.edge2_.partials_[n] += sign_y_m_mu_times_inv_sigma;
+      ops_partials.edge2_.partials_[n] += rep_deriv;
     }
     if (!is_constant_all<T_scale>::value) {
-      ops_partials.edge3_.partials_[n]
-          += -inv_sigma[n] + fabs_y_m_mu * inv_sigma_squared[n];
+      ops_partials.edge3_.partials_[n] -= inv_sigma[n] * (1 + scaled_diff);
     }
   }
   return ops_partials.build(logp);
