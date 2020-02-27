4021cde213792ddd18b3fe5d9c7e66a7f51bc089
Bob Carpenter <carp@alias-i.com>
2020-02-27 16:44:22 -0500

    doc tweaksfor return types

--- test/unit/math/mix/meta/scalar_type_test.cpp
+++ test/unit/math/mix/meta/scalar_type_test.cpp
@@ -25,32 +25,21 @@ void test_base() {
   using std::vector;
   expect_base<T, T>();
   expect_base<T, vector<T>>();
-  // std::vector value type has to be copyable _and_ movable, const not movable
-  //  expect_base<T, vector<const T>>();
-
-  /**
-   *
- (Steve): Idt we have to worry about std vector ref either either
-    Unless we start using std::reference_wrapper<T>.
-
- test/unit/math/mix/meta/scalar_type_test.cpp:57:18:   required from here
- /usr/include/c++/8/ext/new_allocator.h:63:26: error: forming pointer to
- reference type ‘double&’ typedef _Tp*       pointer;
-                          ^~~~~~~
- /usr/include/c++/8/ext/new_allocator.h:64:26: error: forming pointer to
- reference type ‘double&’ typedef const _Tp* const_pointer;
-                          ^~~~~~~~~~~~~
-   */
-  // expect_base<T, vector<T&>>();
   expect_base<T, vector<vector<T>>>();
+
   expect_base<T, Matrix<T, -1, -1>>();
-  expect_base<T, Matrix<T, 1, -1>>();
-  expect_base<T, Matrix<T, -1, 1>>();
   expect_base<T, vector<Matrix<T, -1, -1>>>();
+
+  expect_base<T, Matrix<T, 1, -1>>();
   expect_base<T, vector<Matrix<T, 1, -1>>>();
+
+  expect_base<T, Matrix<T, -1, 1>>();
   expect_base<T, vector<Matrix<T, -1, 1>>>();
 
   expect_base<complex<T>, complex<T>>();
+  expect_base<complex<T>, vector<complex<T>>>();
+
+  expect_base<complex<T>, Matrix<complex<T>, -1, -1>>();
 }
 
 TEST(MathMetaPrim, scalarType) {
