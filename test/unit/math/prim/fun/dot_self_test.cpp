39b4c207039b5d10be77af1f93af6602a9725388
tadej <tadej.c@gmail.com>
2020-03-02 15:12:34 +0100

    removed redundant tests

--- test/unit/math/prim/fun/dot_self_test.cpp
+++ test/unit/math/prim/fun/dot_self_test.cpp
@@ -56,21 +56,4 @@ TEST(MathMatrixPrimMat, dot_self) {
   Eigen::Matrix<double, 1, Eigen::Dynamic> rv3(3);
   rv3 << 2.0, 3.0, 4.0;
   EXPECT_NEAR(29.0, dot_self(rv3), 1E-12);
-
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> m1(1, 1);
-  m1 << 2.0;
-  EXPECT_NEAR(4.0, dot_self(m1), 1E-12);
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> m2(2, 1);
-  m2 << 2.0, 3.0;
-  EXPECT_NEAR(13.0, dot_self(m2), 1E-12);
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> m3(3, 1);
-  m3 << 2.0, 3.0, 4.0;
-  EXPECT_NEAR(29.0, dot_self(m3), 1E-12);
-
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mm2(1, 2);
-  mm2 << 2.0, 3.0;
-  EXPECT_NEAR(13.0, dot_self(mm2), 1E-12);
-  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> mm3(1, 3);
-  mm3 << 2.0, 3.0, 4.0;
-  EXPECT_NEAR(29.0, dot_self(mm3), 1E-12);
 }
