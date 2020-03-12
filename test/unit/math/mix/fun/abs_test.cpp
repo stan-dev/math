5f3267c7b3207cd99e705c5c38cdef7ccd30f03c
Bob Carpenter <carp@alias-i.com>
2020-03-12 11:53:52 -0400

    include std::abs for abs test

--- test/unit/math/mix/fun/abs_test.cpp
+++ test/unit/math/mix/fun/abs_test.cpp
@@ -1,9 +1,13 @@
 #include <test/unit/math/test_ad.hpp>
+#include <cmath>
 #include <complex>
 #include <vector>
 
 TEST(mixScalFun, abs) {
-  auto f = [](const auto& x) { return abs(x); };
+  auto f = [](const auto& x) {
+    using std::abs;
+    return abs(x);
+  };
   stan::test::expect_common_nonzero_unary(f);
   stan::test::expect_value(f, 0);
   stan::test::expect_value(f, 0.0);
@@ -19,13 +23,8 @@ TEST(mixScalFun, abs) {
   stan::test::expect_ad(f, 4.0);
 
   // not differentiable at zero
-  for (double re :
-       std::vector<double>{-4, -2.5, -1.5, -0.3, -0.0, 0.0, 1.3, 2.1, 3.9}) {
-    for (double im :
-         std::vector<double>{-4, -2.5, -1.5, -0.3, -0.0, 0.0, 1.3, 2.1, 3.9}) {
-      if ((re == 0.0 || re == -0.0) && (im == 0.0 || im == -0.0)) {
-        continue;
-      }
+  for (double re : std::vector<double>{-4, -2.5, -1.5, -0.3, 1.3, 2.1, 3.9}) {
+    for (double im : std::vector<double>{-4, -2.5, -1.5, -0.3, 1.3, 2.1, 3.9}) {
       stan::test::expect_ad(f, std::complex<double>(re, im));
     }
   }
