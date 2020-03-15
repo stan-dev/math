b5e2c39a91db162deb285ee930194ea4b4e31774
Peter Wicks Stringfield <peterwicksstringfield@gmail.com>
2020-03-15 12:32:10 -0500

    Reimplement max_size(_mvt)?.

--- stan/math/prim/fun/max_size.hpp
+++ stan/math/prim/fun/max_size.hpp
@@ -2,43 +2,21 @@
 #define STAN_MATH_PRIM_FUN_MAX_SIZE_HPP
 
 #include <stan/math/prim/fun/size.hpp>
+#include <algorithm>
 
 namespace stan {
 namespace math {
 
-template <typename T1, typename T2>
-size_t max_size(const T1& x1, const T2& x2) {
-  size_t result = stan::math::size(x1);
-  result = result > stan::math::size(x2) ? result : stan::math::size(x2);
-  return result;
-}
-
-template <typename T1, typename T2, typename T3>
-size_t max_size(const T1& x1, const T2& x2, const T3& x3) {
-  size_t result = stan::math::size(x1);
-  result = result > stan::math::size(x2) ? result : stan::math::size(x2);
-  result = result > stan::math::size(x3) ? result : stan::math::size(x3);
-  return result;
-}
-
-template <typename T1, typename T2, typename T3, typename T4>
-size_t max_size(const T1& x1, const T2& x2, const T3& x3, const T4& x4) {
-  size_t result = stan::math::size(x1);
-  result = result > stan::math::size(x2) ? result : stan::math::size(x2);
-  result = result > stan::math::size(x3) ? result : stan::math::size(x3);
-  result = result > stan::math::size(x4) ? result : stan::math::size(x4);
-  return result;
-}
-
-template <typename T1, typename T2, typename T3, typename T4, typename T5>
-size_t max_size(const T1& x1, const T2& x2, const T3& x3, const T4& x4,
-                const T5& x5) {
-  size_t result = stan::math::size(x1);
-  result = result > stan::math::size(x2) ? result : stan::math::size(x2);
-  result = result > stan::math::size(x3) ? result : stan::math::size(x3);
-  result = result > stan::math::size(x4) ? result : stan::math::size(x4);
-  result = result > stan::math::size(x5) ? result : stan::math::size(x5);
-  return result;
+/** Calculate the size of the largest input.
+ * @tparam T1 type of the first input
+ * @tparam Ts types of the other inputs
+ * @param x1 first input
+ * @param xs other inputs
+ * @return the size of the largest input
+ */
+template <typename T1, typename... Ts>
+size_t max_size(const T1& x1, const Ts&... xs) {
+  return std::max({stan::math::size(x1), stan::math::size(xs)...});
 }
 
 }  // namespace math
--- stan/math/prim/fun/max_size_mvt.hpp
+++ stan/math/prim/fun/max_size_mvt.hpp
@@ -2,33 +2,21 @@
 #define STAN_MATH_PRIM_FUN_MAX_SIZE_MVT_HPP
 
 #include <stan/math/prim/fun/size_mvt.hpp>
-#include <cstdlib>
+#include <algorithm>
 
 namespace stan {
 namespace math {
 
-template <typename T1, typename T2>
-size_t max_size_mvt(const T1& x1, const T2& x2) {
-  size_t result = size_mvt(x1);
-  result = result > size_mvt(x2) ? result : size_mvt(x2);
-  return result;
-}
-
-template <typename T1, typename T2, typename T3>
-size_t max_size_mvt(const T1& x1, const T2& x2, const T3& x3) {
-  size_t result = size_mvt(x1);
-  result = result > size_mvt(x2) ? result : size_mvt(x2);
-  result = result > size_mvt(x3) ? result : size_mvt(x3);
-  return result;
-}
-
-template <typename T1, typename T2, typename T3, typename T4>
-size_t max_size_mvt(const T1& x1, const T2& x2, const T3& x3, const T4& x4) {
-  size_t result = size_mvt(x1);
-  result = result > size_mvt(x2) ? result : size_mvt(x2);
-  result = result > size_mvt(x3) ? result : size_mvt(x3);
-  result = result > size_mvt(x4) ? result : size_mvt(x4);
-  return result;
+/** Calculate the size of the largest multivariate input.
+ * @tparam T1 type of the first input
+ * @tparam Ts types of the other inputs
+ * @param x1 first input
+ * @param xs other inputs
+ * @return the size of the largest input
+ */
+template <typename T1, typename... Ts>
+size_t max_size_mvt(const T1& x1, const Ts&... xs) {
+  return std::max({stan::math::size_mvt(x1), stan::math::size_mvt(xs)...});
 }
 
 }  // namespace math
