3fc51ae02f71eff83c0d45a632f1b3fd415c9e25
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-03-18 21:31:15 +0800

    Exp testing

--- stan/math/rev/fun/exp.hpp
+++ stan/math/rev/fun/exp.hpp
@@ -14,6 +14,57 @@ class exp_vari : public op_v_vari {
   explicit exp_vari(vari* avi) : op_v_vari(std::exp(avi->val_), avi) {}
   void chain() { avi_->adj_ += adj_ * val_; }
 };
+
+template <typename T>
+class exp_matrix_vari : public vari {
+ public:
+  int A_rows_;
+  int A_cols_;
+  int A_size_;
+  double* Ad_;
+  vari** variRefA_;
+  vari** variRefExp_;
+
+  /**
+   * Constructor for exp_matrix_vari.
+   *
+   * All memory allocated in
+   * ChainableStack's stack_alloc arena.
+   *
+   * It is critical for the efficiency of this object
+   * that the constructor create new varis that aren't
+   * popped onto the var_stack_, but rather are
+   * popped onto the var_nochain_stack_. This is
+   * controlled by the second argument to
+   * vari's constructor.
+   *
+   * @param A matrix
+   */
+  explicit exp_matrix_vari(const T& A)
+      : vari(0.0),
+        A_rows_(A.rows()),
+        A_cols_(A.cols()),
+        A_size_(A.size()),
+        Ad_(ChainableStack::instance_->memalloc_.alloc_array<double>(A_size_)),
+        variRefA_(
+            ChainableStack::instance_->memalloc_.alloc_array<vari*>(A_size_)),
+        variRefExp_(ChainableStack::instance_->memalloc_.alloc_array<vari*>(
+            A_size_)) {
+    using Eigen::Map;
+    Map<matrix_vi>(variRefA_, A_rows_, A_cols_) = A.vi();
+    Map<matrix_d> Ad(Ad_, A_rows_, A_cols_);
+    Ad = A.val();
+    Map<matrix_vi>(variRefExp_, A_rows_, A_cols_).array()
+        = Ad.array().exp().unaryExpr([](double x) { return new vari(x, false); });
+  }
+
+  virtual void chain() {
+    using Eigen::Map;
+    Map<matrix_vi> RefExp(variRefExp_, A_rows_, A_cols_);
+    Map<matrix_vi>(variRefA_, A_rows_, A_cols_).adj()
+          += RefExp.val().cwiseProduct(RefExp.adj());
+  }
+};
 }  // namespace internal
 
 /**
@@ -39,6 +90,21 @@ class exp_vari : public op_v_vari {
  * @return Exponentiated variable.
  */
 inline var exp(const var& a) { return var(new internal::exp_vari(a.vi_)); }
+
+template <typename Container,
+          require_container_st<is_container, is_var, Container>...>
+inline auto exp(const Container& x) {
+  return apply_vector_unary<Container>::apply(
+      x, [](const auto& v) {
+        using T_vec = decltype(v);
+        // Memory managed with the arena allocator.
+        auto* baseVari = new internal::exp_matrix_vari<T_vec>(v);
+        plain_type_t<T_vec> AB_v(v.rows(), v.cols());
+        AB_v.vi() = Eigen::Map<matrix_vi>(baseVari->variRefExp_, v.rows(), v.cols());
+
+        return AB_v;
+});
+}
 
 }  // namespace math
 }  // namespace stan
