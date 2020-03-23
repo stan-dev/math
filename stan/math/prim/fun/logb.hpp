508fbc2fb9f52f1bbaf1c5fa5f18ca39d46bd805
Bob Carpenter <carp@alias-i.com>
2020-03-23 12:53:22 -0400

    added doc for logb

--- stan/math/prim/fun/logb.hpp
+++ stan/math/prim/fun/logb.hpp
@@ -7,6 +7,19 @@
 namespace stan {
 namespace math {
 
+/**
+ * Returns the value of the unbiased radix-independent exponent from
+ * the floating-point argument.
+ *
+ * Implementation note: This implementation works for all autodiff
+ * types because it is a step function, so can return a `double` which
+ * will produce the correct zero derivative if promoted to an autodiff
+ * variable.
+ *
+ * @tparam T floating-point type
+ * @param[in] x floating-point argument
+ * @return unbiased radix-independent exponent of the argument
+ */
 template <typename T, typename = require_autodiff_t<T>>
 double logb(const T& x) {
   return std::logb(value_of_rec(x));
