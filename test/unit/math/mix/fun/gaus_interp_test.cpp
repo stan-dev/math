63172f13b315514eab966ebe9c854b6ac7dd0b24
Philip Greengard <pgreenga@gmail.com>
2020-03-31 17:57:10 -0400

    moved prim tests to new files

--- test/unit/math/mix/fun/gaus_interp_test.cpp
+++ test/unit/math/mix/fun/gaus_interp_test.cpp
@@ -2,83 +2,6 @@
 #include <limits>
 #include <vector>
 
-double ABS_TOL = 1e-12;
-
-TEST(mathMixGausInterp, interp_line) {
-  using stan::math::gaus_interp;
-
-  // check that interpolation of line returns the same function
-  // generate function tabulation
-  int n = 2;
-  double xmin = 0;
-  double xmax = 1;
-  vector<double> xs = {0, 1};
-  vector<double> ys = {0, 2};
-
-  // vector of pts at which to compute interpolation
-  vector<double> xs_new;
-  int n_interp = 100;
-  double t0 = xmin;
-  double t1 = xmax;
-  double t;
-  for (int i=0; i < n_interp; i++) {
-    t = t0 + i * (t1 - t0) / (n_interp - 1);
-    xs_new.push_back(t);
-  }
-
-  // create interpolation
-  vector<double> ys_new;
-  ys_new = gaus_interp(n, xs, ys, n_interp, xs_new);
-
-  // test points
-  double tmp, y;
-  for (int i=0; i < n_interp; i++) {
-    tmp = (ys[1] - ys[0]) / (xs[1] - xs[0]);
-    y = tmp * xs_new[i] + ys[0] - tmp * xs[0];
-    ASSERT_NEAR(ys_new[i], y, ABS_TOL);
-  }
-}
-
-TEST(mathMixGausInterp, gaus_and_lin_interp) {
-  using stan::math::gaus_interp;
-  using stan::math::lin_interp;
-
-  // check that interpolation of line returns the same function
-  // generate function tabulation
-  int n = 30;
-  double xmin = 0;
-  double xmax = 1;
-  double x;
-  vector<double> xs, ys;
-  for (int i=0; i < n; i++) {
-    x = xmin + i * (xmax - xmin) / (n - 1);
-    xs.push_back(x);
-    ys.push_back(x*x);
-  }
-
-  // vector of pts at which to compute interpolation
-  vector<double> xs_new;
-  int n_interp = 100;
-  double t0 = xmin;
-  double t1 = xmax;
-  double t;
-  for (int i=0; i < n_interp; i++) {
-    t = t0 + i * (t1 - t0) / (n_interp - 1);
-    xs_new.push_back(t);
-  }
-
-  // create interpolation
-  vector<double> ys_new_gaus, ys_new_lin;
-  ys_new_gaus = gaus_interp(n, xs, ys, n_interp, xs_new);
-  ys_new_lin = lin_interp(n, xs, ys, n_interp, xs_new);
-
-  // test points
-  double tmp, y;
-  for (int i=0; i < n_interp; i++) {
-    ASSERT_NEAR(ys_new_lin[i], ys_new_gaus[i], 1e-4);
-  }
-}
-
 
 TEST(mathMixGausInterp, derivs) {
   using stan::math::var;
@@ -129,7 +52,7 @@ TEST(mathMixGausInterp, derivs) {
   }
 
   // take derivative of interpolation using finite differencing
-  double h = 1e-4;
+  double h = 1e-8;
   xs_new_p = xs_new;
   xs_new_n = xs_new;
   for (int i=0; i < n_interp; i++) {
@@ -138,7 +61,6 @@ TEST(mathMixGausInterp, derivs) {
     ys_new_p = gaus_interp(n, xs, ys, n_interp, xs_new_p);
     ys_new_n = gaus_interp(n, xs, ys, n_interp, xs_new_n);
     dder2 = (ys_new_n[i] - ys_new_p[i]) / (2 * h);
+    ASSERT_NEAR(ys_new_dder[i], dder2, 1e-5);
   }
-
-
 }
---
+++ test/unit/math/prim/fun/conv_gaus_line_test.cpp
@@ -0,0 +1,53 @@
+#include <test/unit/math/test_ad.hpp>
+#include <limits>
+#include <vector>
+
+/*
+evaluate the convolution using trapezoid rule
+*/
+double check_int_dumb(double t0, double t1, double a, double b, double x0,
+		      double sig2) {
+  double x, y, t, tot;
+  vector<double> ys, ts;
+  double pi = stan::math::pi();
+  int n;
+
+  // evaluate function at equispaced nodes
+  n = 100000;
+  for (int i = 0; i < n; i++) {
+    t = t0 + i * (t1 - t0) / (n - 1);
+    ts.push_back(t);
+
+    y = a * t + b;
+    y = y * exp(-pow(t - x0, 2) / (2 * sig2));
+    ys.push_back(y);
+  }
+
+  // sum up function evaluations
+  tot = 0;
+  for (int i = 0; i < n; i++) {
+    tot += ys[i];
+  }
+  tot *= (t1 - t0) / n;
+  tot /= sqrt(2 * pi * sig2);
+
+  return tot;
+}
+
+/*
+check that integral using formula is close to trapezoid approximation
+*/
+TEST(mathMixConvGausLin, trap_test) {
+  double t0, t1, a, b, x0, sig2, y, y2;
+  using stan::math::conv_gaus_line;
+  t0 = 0;
+  t1 = 1;
+  a = 1;
+  b = 0;
+  x0 = 1;
+  sig2 = 3;
+  y = check_int_dumb(t0, t1, a, b, x0, sig2);
+  y2 = conv_gaus_line(t0, t1, a, b, x0, sig2);
+
+  ASSERT_NEAR(y, y2, 1e-5);
+}
