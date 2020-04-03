38f1141a1f8ecb6e93a70e294be8defbba67a69a
Stan Jenkins <mc.stanislaw@gmail.com>
2020-04-03 23:18:28 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/functor/reduce_sum_static.hpp
+++ stan/math/prim/functor/reduce_sum_static.hpp
@@ -59,11 +59,10 @@ auto reduce_sum_static(Vec&& vmapped, int grainsize, std::ostream* msgs,
   }
 
   return_type_t<Vec, Args...> sum = 0.0;
-  for (size_t i = 0; i < (vmapped.size() + grainsize - 1) / grainsize;
-       ++i) {
+  for (size_t i = 0; i < (vmapped.size() + grainsize - 1) / grainsize; ++i) {
     size_t start = i * grainsize;
     size_t end = std::min((i + 1) * grainsize, vmapped.size()) - 1;
-    
+
     std::decay_t<Vec> sub_slice;
     sub_slice.reserve(end - start + 1);
     for (size_t i = start; i <= end; ++i) {
@@ -71,7 +70,7 @@ auto reduce_sum_static(Vec&& vmapped, int grainsize, std::ostream* msgs,
     }
 
     sum += ReduceFunction()(start, end, std::forward<Vec>(sub_slice), msgs,
-			    std::forward<Args>(args)...);
+                            std::forward<Args>(args)...);
   }
   return sum;
 #endif
