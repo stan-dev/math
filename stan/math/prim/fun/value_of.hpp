09292804b759fc7297b4c03954ceb3a0ac114279
Steve Bronder <stevo15025@gmail.com>
2020-04-19 02:03:02 -0400

    Fix ambuguity in arithmetic std vector value_of

--- stan/math/prim/fun/value_of.hpp
+++ stan/math/prim/fun/value_of.hpp
@@ -59,7 +59,8 @@ inline auto value_of(ComplexT&& x) {
  * @param[in] x std::vector to be converted
  * @return std::vector with `value_of` applied to each element.
  **/
-template <typename Vec, require_std_vector_t<Vec>* = nullptr>
+template <typename Vec, require_std_vector_t<Vec>* = nullptr,
+  require_not_vt_arithmetic<Vec>* = nullptr>
 inline auto value_of(Vec&& x) {
   const size_t x_size = x.size();
   std::vector<partials_type_t<value_type_t<Vec>>> result(x_size);
