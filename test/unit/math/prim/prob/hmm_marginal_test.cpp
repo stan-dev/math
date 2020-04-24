5a0786bad6785fc70a1d49e67bcf52bcfe3b9110
Stan Jenkins <mc.stanislaw@gmail.com>
2020-04-24 14:43:50 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/prim/prob/hmm_marginal_test.cpp
+++ test/unit/math/prim/prob/hmm_marginal_test.cpp
@@ -113,7 +113,7 @@ class hmm_marginal_lpdf_test : public ::testing::Test {
 
 // For evaluation of the density, the C++ code is benchmarked against
 // a forward algorithm written in R.
-// TODO(charlesm93): Add public repo link with R script. 
+// TODO(charlesm93): Add public repo link with R script.
 TEST_F(hmm_marginal_lpdf_test, ten_transitions) {
   using stan::math::hmm_marginal_lpdf;
 
