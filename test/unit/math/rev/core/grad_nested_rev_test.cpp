8f5f71bbbe1f0caa2bb3504f41ca5510b2fc1a2d
Stan Jenkins <mc.stanislaw@gmail.com>
2020-04-29 21:29:48 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0

--- test/unit/math/rev/core/grad_nested_rev_test.cpp
+++ test/unit/math/rev/core/grad_nested_rev_test.cpp
@@ -8,15 +8,14 @@ namespace math {
 
 class test_vari : public vari {
  public:
-  explicit test_vari() : vari(0.0) {
-  }
+  explicit test_vari() : vari(0.0) {}
 
   virtual void chain() {
     stan::math::nested_rev_autodiff nested;
 
     // Add enough vars to make the the var_stack_ vector reallocate
     int N_new_vars = ChainableStack::instance_->var_stack_.capacity() + 1;
-    
+
     var total = 0.0;
     for (int i = 0; i < N_new_vars; ++i) {
       total += i;
@@ -26,8 +25,8 @@ class test_vari : public vari {
   }
 };
 
-}
-}
+}  // namespace math
+}  // namespace stan
 
 TEST(AgradRev, grad_in_reverse_mode) {
   using stan::math::var;
