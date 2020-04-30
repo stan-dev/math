65c97acc37766b2dd053c82272d97de4daed1624
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-04-30 21:48:24 +0200

    add inv_cloglog

---
+++ stan/math/opencl/prim/inv_cloglog.hpp
@@ -0,0 +1,57 @@
+#ifndef STAN_MATH_OPENCL_PRIM_FUN_INV_SQUARE_HPP
+#define STAN_MATH_OPENCL_PRIM_FUN_INV_SQUARE_HPP
+
+#include <stan/math/opencl/matrix_cl.hpp>
+#include <stan/math/opencl/kernel_generator.hpp>
+#include <stan/math/prim/fun/constants.hpp>
+
+namespace stan {
+namespace math {
+
+/**
+ * The inverse complementary log-log function.
+ *
+ * The function is defined by
+ *
+ * <code>inv_cloglog(x) = 1 - exp(-exp(x))</code>.
+ *
+ * This function can be used to implement the inverse link
+ * function for complementary-log-log regression.
+ *
+ *
+   \f[
+   \mbox{inv\_cloglog}(y) =
+   \begin{cases}
+     \mbox{cloglog}^{-1}(y) & \mbox{if } -\infty\leq y \leq \infty \\[6pt]
+     \textrm{NaN} & \mbox{if } y = \textrm{NaN}
+   \end{cases}
+   \f]
+
+   \f[
+   \frac{\partial\, \mbox{inv\_cloglog}(y)}{\partial y} =
+   \begin{cases}
+     \frac{\partial\, \mbox{cloglog}^{-1}(y)}{\partial y} & \mbox{if }
+ -\infty\leq y\leq \infty \\[6pt] \textrm{NaN} & \mbox{if } y = \textrm{NaN}
+   \end{cases}
+   \f]
+
+   \f[
+   \mbox{cloglog}^{-1}(y) = 1 - \exp \left( - \exp(y) \right)
+   \f]
+
+   \f[
+   \frac{\partial \, \mbox{cloglog}^{-1}(y)}{\partial y} = \exp(y-\exp(y))
+   \f]
+ *
+ * @param x Argument.
+ * @return Inverse complementary log-log of the argument.
+ */
+template <typename T_a,
+          typename = require_all_valid_expressions_and_none_scalar_t<T_a>>
+inline auto inv_cloglog(T_a&& a) {  // NOLINT
+  return 1 - exp(-exp(a));
+}
+}  // namespace math
+}  // namespace stan
+
+#endif
