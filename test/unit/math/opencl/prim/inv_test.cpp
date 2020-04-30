274fffdd17942825d73227d7292178dc7acc6de7
Stan Jenkins <mc.stanislaw@gmail.com>
2020-04-30 15:59:34 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0

--- test/unit/math/opencl/prim/inv_test.cpp
+++ test/unit/math/opencl/prim/inv_test.cpp
@@ -9,16 +9,16 @@
     EXPECT_NEAR(A(i), B(i), DELTA);
 
 TEST(MathMatrixCL, inv_nan) {
-  stan::math::matrix_d y(1,4);
+  stan::math::matrix_d y(1, 4);
   y << 2.0, 0.0, -32.7, std::numeric_limits<double>::quiet_NaN();
 
   stan::math::matrix_cl<double> y_cl(y);
   stan::math::matrix_cl<double> z_cl = stan::math::inv(y_cl);
   stan::math::matrix_d z = stan::math::from_matrix_cl(z_cl);
-  EXPECT_FLOAT_EQ(1 / 2.0, z(0,0));
-  EXPECT_FLOAT_EQ(stan::math::positive_infinity(), z(0,1));
-  EXPECT_FLOAT_EQ(1 / -32.7, z(0,2));
-  EXPECT_TRUE(std::isnan(stan::math::inv(z(0,3))));
+  EXPECT_FLOAT_EQ(1 / 2.0, z(0, 0));
+  EXPECT_FLOAT_EQ(stan::math::positive_infinity(), z(0, 1));
+  EXPECT_FLOAT_EQ(1 / -32.7, z(0, 2));
+  EXPECT_TRUE(std::isnan(stan::math::inv(z(0, 3))));
 
   stan::math::vector_d av(9);
   av << 1, 2, 3, 4, 22, 0.5, 5, 18, 99;
