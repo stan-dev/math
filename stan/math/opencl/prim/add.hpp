2c2b443e7d0cc1ed1303438f5a108276b337de56
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-04-30 11:33:00 +0200

    simplify with tadej's suggestion

--- stan/math/opencl/prim/add.hpp
+++ stan/math/opencl/prim/add.hpp
@@ -8,39 +8,20 @@
 namespace stan {
 namespace math {
 /** \ingroup opencl
- * Computes the sum of the input matrices.
- *
- * @param A first matrix
- * @param B second matrix
- * @return the product of the first and second matrix
+ * Computes the sum on kernel generator expressions.
+ * 
+ * @param A first expression
+ * @param B second expression
+ * @return the sum of the first and second expression
  *
  * @throw <code>std::invalid_argument</code> if the
- *   number of columns in A and rows in B do not match
+ *   dimensions dont match if none of tha arguments are a scalar.
  */
 template <typename T_a, typename T_b,
-          typename = require_all_valid_expressions_and_none_scalar_t<T_a, T_b>>
-inline addition_<as_operation_cl_t<T_a>, as_operation_cl_t<T_b>> add(
-    T_a&& a, T_b&& b) {  // NOLINT
-  return {as_operation_cl(std::forward<T_a>(a)),
-          as_operation_cl(std::forward<T_b>(b))};
-}
-
-template <typename T_a, typename T_b,
-          typename = require_all_valid_expressions_and_none_scalar_t<T_a>,
-          typename = require_arithmetic_t<T_b>>
-inline addition_<as_operation_cl_t<T_a>, scalar_<T_b>> add(
-    T_a&& a, T_b&& b) {  // NOLINT
-  return {as_operation_cl(std::forward<T_a>(a)),
-          as_operation_cl(b)};
-}
-
-template <typename T_a, typename T_b,
-          typename = require_arithmetic_t<T_a>,
-          typename = require_all_valid_expressions_and_none_scalar_t<T_b>>
-inline addition_<scalar_<T_a>, as_operation_cl_t<T_b>> add(
-    T_a&& a, T_b&& b) {  // NOLINT
-  return {as_operation_cl(a),
-          as_operation_cl(std::forward<T_b>(b))};
+          typename = require_all_valid_expressions_t<T_a, T_b>,
+          typename = require_any_not_arithmetic_t<T_a, T_b>>
+inline auto add(T_a&& a, T_b&& b) {  // NOLINT
+  return std::forward<T_a>(a) + std::forward<T_b>(b);
 }
 }  // namespace math
 }  // namespace stan
