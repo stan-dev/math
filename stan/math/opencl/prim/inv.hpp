086b665e8277d2501c070575cea24e3e4a31cc72
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-05-01 14:10:30 +0200

    missing ifdef

--- stan/math/opencl/prim/inv.hpp
+++ stan/math/opencl/prim/inv.hpp
@@ -1,6 +1,6 @@
 #ifndef STAN_MATH_OPENCL_PRIM_FUN_INV_SQUARE_HPP
 #define STAN_MATH_OPENCL_PRIM_FUN_INV_SQUARE_HPP
-
+#ifdef STAN_OPENCL
 #include <stan/math/opencl/matrix_cl.hpp>
 #include <stan/math/opencl/kernel_generator.hpp>
 #include <stan/math/prim/fun/constants.hpp>
@@ -23,4 +23,5 @@ inline auto inv(T_x&& x) {  // NOLINT
 }  // namespace math
 }  // namespace stan
 
+#endif
 #endif
