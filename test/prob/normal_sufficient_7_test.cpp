2805874bbdcc21126c7f35e5fe86ff501702d268
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-05-05 22:04:07 +0200

    remove created files

--- test/prob/normal_sufficient_7_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <test/prob/normal_sufficient/normal_sufficient_00036_generated_fd_test.cpp>
-#include <test/prob/normal_sufficient/normal_sufficient_00036_generated_ffd_test.cpp>
-#include <test/prob/normal_sufficient/normal_sufficient_00036_generated_ffv_test.cpp>
-#include <test/prob/normal_sufficient/normal_sufficient_00036_generated_fv_test.cpp>
-#include <test/prob/normal_sufficient/normal_sufficient_00036_generated_v_test.cpp>
-#include <test/prob/normal_sufficient/normal_sufficient_00037_generated_v_test.cpp>
-#include <test/prob/normal_sufficient/normal_sufficient_00038_generated_v_test.cpp>
