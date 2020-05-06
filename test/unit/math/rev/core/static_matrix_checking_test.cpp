692365f09a6b28dc963eba4614aec0f6c17ce6e3
Steve Bronder <stevo15025@gmail.com>
2020-05-05 20:02:03 -0400

    move tests back to one file

--- test/unit/math/rev/core/static_matrix_checking_test.cpp
+++ test/unit/math/rev/core/static_matrix_checking_test.cpp
@@ -16,7 +16,7 @@ TEST(MathRev, TestVarEigen) {
   var_type<Eigen::Matrix<double, -1, -1>> y = y_vals;
   var lp = 0;
   auto mul_xy = x * y;
-  auto sum_mul_xy = sum(mul_xy);
+  auto sum_mul_xy = sum(mul_xy + x);
   lp -= sum_mul_xy;
   lp.grad();
   puts("-------------");
@@ -37,4 +37,32 @@ TEST(MathRev, TestVarEigen) {
 
   std::cout << "y stat val: \n" << y.val() << "\n";
   std::cout << "y stat adj: \n" << y.adj() << "\n";
+  stan::math::set_zero_all_adjoints();
+  puts("---------");
+  puts("Dynamic Matrix");
+  puts("---------");
+  Eigen::Matrix<var, -1, -1> x_dyn = x_vals;
+  Eigen::Matrix<var, -1, -1> y_dyn = y_vals;
+  Eigen::Matrix<var, -1, -1> mul_xy_dyn = x_dyn * y_dyn;
+  var sum_mul_xy_dyn = sum(mul_xy_dyn + x_dyn);
+  var lp_dyn = 0;
+  lp_dyn -= sum_mul_xy_dyn;
+  lp.grad();
+  std::cout << "lp dyn val: \n" << lp.val() << "\n";
+  std::cout << "lp dyn adj: \n" << lp.adj() << "\n";
+  puts("---------");
+  std::cout << "sum_mul_xy dyn val: \n" << sum_mul_xy.val() << "\n";
+  std::cout << "sum_mul_xy dyn adj: \n" << sum_mul_xy.adj() << "\n";
+  puts("---------");
+
+  std::cout << "mul_xy dyn val: \n" << mul_xy.val() << "\n";
+  std::cout << "mul_xy dyn adj: \n" << mul_xy.adj() << "\n";
+  puts("---------");
+
+  std::cout << "x dyn val: \n" << x.val() << "\n";
+  std::cout << "x dyn adj: \n" << x.adj() << "\n";
+  puts("---------");
+
+  std::cout << "y dyn val: \n" << y.val() << "\n";
+  std::cout << "y dyn adj: \n" << y.adj() << "\n";
 }
