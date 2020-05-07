6a1d8ed542966dec0f04569793c5568e8d277095
Stan Jenkins <mc.stanislaw@gmail.com>
2020-05-07 04:05:28 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0

--- test/unit/math/mix/fun/positive_ordered_constrain_test.cpp
+++ test/unit/math/mix/fun/positive_ordered_constrain_test.cpp
@@ -25,7 +25,7 @@ void expect_positive_ordered_transform(const T& x) {
   stan::test::expect_ad(f2, x);
   stan::test::expect_ad(f3, x);
 }
-}
+}  // namespace positive_ordered_constrain_test
 
 TEST(MathMixMatFun, positiveOrderedTransform) {
   Eigen::VectorXd v0(0);
