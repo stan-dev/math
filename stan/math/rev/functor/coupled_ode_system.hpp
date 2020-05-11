fa2c7f24be25e7043b46af7d567660bfcfba9a27
Steve Bronder <stevo15025@gmail.com>
2020-05-11 12:03:28 -0400

    cpplint

--- stan/math/rev/functor/coupled_ode_system.hpp
+++ stan/math/rev/functor/coupled_ode_system.hpp
@@ -5,7 +5,6 @@
 #include <stan/math/rev/fun/value_of.hpp>
 #include <stan/math/rev/core.hpp>
 #include <stan/math/rev/fun.hpp>
-#include <stan/math/rev/meta.hpp>
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun.hpp>
 #include <stan/math/prim/functor.hpp>
