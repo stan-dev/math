ee28b7900fa8e9e3f4a13853dc06a80c4733fb37
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-05-19 00:07:02 +0800

    Start doc

--- stan/math/prim/meta/apply_scalar_binary.hpp
+++ stan/math/prim/meta/apply_scalar_binary.hpp
@@ -16,48 +16,124 @@ template <typename T1, typename T2, typename Enable = void,
           typename Enable2 = void>
 struct apply_scalar_binary {};
 
+/**
+ * Base template class for vectorization of binary scalar functions
+ * defined by applying a functor to a combination of scalars,
+ * containers of matching sizes, or a combination of a scclar and a container.
+ * These containers can be a standard library vector, Eigen dense
+ * matrix expression template, or container of these. For each specialisation,
+ * the same type as the largest (dimension) input is returned.
+ *
+ * This base template class takes (and returns) Eigen expression templates.
+ */
 template <typename T1, typename T2>
 struct apply_scalar_binary<T1, T2, require_all_stan_scalar_t<T1, T2>> {
-
+  /**
+   * Member function for applying a functor to two scalars and subsequently
+   * returning a scalar. 
+   *
+   * @tparam T1 Type of first argument to which functor is applied.
+   * @tparam T2 Type of second argument to which functor is applied.
+   * @tparam F Type of functor to apply.
+   * @param x First input to which operation is applied.
+   * @param y Second input to which operation is applied.
+   * @param f functor to apply to inputs.
+   * @return Scalar with result of applying functor to input.
+   */
   template <typename F>
   static inline auto apply(const T1& x, const T2& y, const F& f) {
     return f(x,y);
   }
-
 };
 
+/**
+ * Specialisation for use with two Eigen inputs. Eigen's binaryExpr framework
+ * is used for more efficient indexing of both row- and column-major inputs
+ * without separate loops.
+ */
 template <typename T1, typename T2>
 struct apply_scalar_binary<T1, T2, require_all_eigen_t<T1, T2>> {
-
+  /**
+   * Member function for applying a functor to two Eigen objects and
+   * subsequently returning an Eigen object. 
+   *
+   * @tparam T1 Type of first argument to which functor is applied.
+   * @tparam T2 Type of second argument to which functor is applied.
+   * @tparam F Type of functor to apply.
+   * @param x First Eigen input to which operation is applied.
+   * @param y Second Eigen input to which operation is applied.
+   * @param f functor to apply to Eigen input.
+   * @return Eigen object with result of applying functor to inputs.
+   */
   template <typename F>
   static inline auto apply(const T1& x, const T2& y, const F& f) {
     return x.binaryExpr(y, f).eval();
   }
-
 };
 
+/**
+ * Specialisation for use with one Eigen input and one scalar. Eigen's
+ * unaryExpr framework is used for more efficient indexing of both row-
+ * and column-major inputs. The unaryExpr framework also allows for the
+ * scalar to be captured and applied to each element in the Eigen object.
+ */
 template <typename T1, typename T2>
 struct apply_scalar_binary<T1, T2, require_eigen_t<T1>,
                            require_stan_scalar_t<T2>> {
-
+  /**
+   * Member function for applying a functor to an Eigen object and
+   * a scalar, and subsequently returning an Eigen object. 
+   *
+   * @tparam T1 Type of Eigen object to which functor is applied.
+   * @tparam T2 Type of scalar to which functor is applied.
+   * @tparam F Type of functor to apply.
+   * @param x Eigen input to which operation is applied.
+   * @param y Scalar input to which operation is applied.
+   * @param f functor to apply to Eigen and scalar inputs.
+   * @return Eigen object with result of applying functor to inputs.
+   */
   template <typename F>
   static inline auto apply(const T1& x, const T2& y, const F& f) {
     return x.unaryExpr([&f,&y](const auto& v){ return f(v, y); }).eval();
   }
-
 };
 
+/**
+ * Specialisation for use with one scalar and one Eigen input. Eigen's
+ * unaryExpr framework is used for more efficient indexing of both row-
+ * and column-major inputs. The unaryExpr framework also allows for the
+ * scalar to be captured and applied to each element in the Eigen object.
+ */
 template <typename T1, typename T2>
 struct apply_scalar_binary<T1, T2, require_stan_scalar_t<T1>,
                            require_eigen_t<T2>> {
-
+  /**
+   * Member function for applying a functor to a scalar and
+   * an Eigen object, and subsequently returning an Eigen object. 
+   *
+   * @tparam T1 Type of scalar to which functor is applied.
+   * @tparam T2 Type of Eigen object to which functor is applied.
+   * @tparam F Type of functor to apply.
+   * @param x Scalar input to which operation is applied.
+   * @param y Eigen input to which operation is applied.
+   * @param f functor to apply to scalar and Eigen inputs.
+   * @return Eigen object with result of applying functor to inputs.
+   */
   template <typename F>
   static inline auto apply(const T1& x, const T2& y, const F& f) {
     return y.unaryExpr([&f,&x](const auto& v){ return f(x, v); }).eval();
   }
-
 };
 
+/**
+ * Specialisation for use with (non-nested) std::vectors. Inputs are mapped
+ * to Eigen column vectors and then the result is evaluated directly into the
+ * returned std::vector (via Eigen::Map).
+ *
+ * The returned scalar type is deduced to allow for cases where the input and
+ * return scalar types differ (e.g., functions implicitly promoting
+ * integers).
+ */
 template <typename T1, typename T2>
 struct apply_scalar_binary<T1, T2,
                            require_all_std_vector_vt<is_stan_scalar, T1, T2>> {
