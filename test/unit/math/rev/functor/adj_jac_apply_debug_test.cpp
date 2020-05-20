4d86285cfa5f2b2395ad5c68d2cd5fd2e24e5378
Steve Bronder <stevo15025@gmail.com>
2020-05-20 16:25:53 -0400

    update debug test with static matrix version

--- test/unit/math/rev/functor/adj_jac_apply_debug_test.cpp
+++ test/unit/math/rev/functor/adj_jac_apply_debug_test.cpp
@@ -107,3 +107,37 @@ TEST(AgradRev, test_vector_sin_multiple_jac) {
   EXPECT_FLOAT_EQ(x2(0).adj(), 1.73 * -0.4161468365471424);
   EXPECT_FLOAT_EQ(x2(1).adj(), 1.57 * 0.5403023058681398);
 }
+
+TEST(AgradRev, test_vector_sin_multiple_jac_static_matrix) {
+  using stan::math::var_value;
+  Eigen::Matrix<double, Eigen::Dynamic, 1> x1(1), x2(2);
+  x1 << 1.0;
+  x2 << 2.0, 1.0;
+  stan::math::var_value<Eigen::Matrix<double, Eigen::Dynamic, 1>> xx1(x1);
+  stan::math::var_value<Eigen::Matrix<double, Eigen::Dynamic, 1>> xx2(x2);
+  auto y1 = stan::math::adj_jac_apply<SinFunctor>(xx1);
+  auto y2 = stan::math::adj_jac_apply<SinFunctor>(xx2);
+
+  y1.grad();
+  EXPECT_FLOAT_EQ(xx1.adj()(0), 0.5403023058681398);
+  EXPECT_FLOAT_EQ(xx2.adj()(0), 0.0);
+  EXPECT_FLOAT_EQ(xx2.adj()(1), 0.0);
+
+  stan::math::set_zero_all_adjoints();
+  stan::math::set_zero_all_adjoints();
+
+  y2.grad();
+  EXPECT_FLOAT_EQ(xx1.adj()(0), 0.0);
+  EXPECT_FLOAT_EQ(xx2.adj()(0), -0.4161468365471424);
+  EXPECT_FLOAT_EQ(xx2.adj()(1), 0.5403023058681398);
+
+  stan::math::set_zero_all_adjoints();
+  Eigen::Matrix<double, 1 , -1> sum_vec(2);
+  sum_vec << 1.73, 1.57;
+  auto sum_y2 = sum_vec * y2;
+  sum_y2.grad();
+  EXPECT_FLOAT_EQ(xx1.adj()(0), 0.0);
+  EXPECT_FLOAT_EQ(xx2.adj()(0), 1.73 * -0.4161468365471424);
+  EXPECT_FLOAT_EQ(xx2.adj()(1), 1.57 * 0.5403023058681398);
+
+}
