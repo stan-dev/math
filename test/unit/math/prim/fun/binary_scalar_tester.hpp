6552eb124721b6626d4b0614d7405d2e104c2d92
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-05-28 14:43:56 +0800

    Missed indexes

--- test/unit/math/prim/fun/binary_scalar_tester.hpp
+++ test/unit/math/prim/fun/binary_scalar_tester.hpp
@@ -20,38 +20,38 @@ template <typename F, typename T1, typename T2,
           require_all_not_std_vector_t<T1, T2>* = nullptr>
 void binary_scalar_tester_impl(const F& f, const T1& x, const T2& y) {
   auto vec_vec = f(x, y);
-  auto vec_scal = f(x, y(1));
-  auto scal_vec = f(x(1), y);
+  auto vec_scal = f(x, y(0));
+  auto scal_vec = f(x(0), y);
   for (int i = 0; i < x.size(); ++i) {
     EXPECT_FLOAT_EQ(f(x(i), y(i)), vec_vec(i));
-    EXPECT_FLOAT_EQ(f(x(i), y(1)), vec_scal(i));
-    EXPECT_FLOAT_EQ(f(x(1), y(i)), scal_vec(i));
+    EXPECT_FLOAT_EQ(f(x(i), y(0)), vec_scal(i));
+    EXPECT_FLOAT_EQ(f(x(0), y(i)), scal_vec(i));
   }
   std::vector<T1> nest_x{x, x, x};
   std::vector<T2> nest_y{y, y, y};
   auto nestvec_nestvec = f(nest_x, nest_y);
-  auto nestvec_scal = f(nest_x, y(1));
-  auto scal_nestvec = f(x(1), nest_y);
+  auto nestvec_scal = f(nest_x, y(0));
+  auto scal_nestvec = f(x(0), nest_y);
   for (int i = 0; i < 3; ++i) {
     for (int j = 0; j < x.size(); ++j) {
       EXPECT_FLOAT_EQ(f(nest_x[i](j), nest_y[i](j)), nestvec_nestvec[i](j));
-      EXPECT_FLOAT_EQ(f(nest_x[i](j), y(1)), nestvec_scal[i](j));
-      EXPECT_FLOAT_EQ(f(x(1), nest_y[i](j)), scal_nestvec[i](j));
+      EXPECT_FLOAT_EQ(f(nest_x[i](j), y(0)), nestvec_scal[i](j));
+      EXPECT_FLOAT_EQ(f(x(0), nest_y[i](j)), scal_nestvec[i](j));
     }
   }
   std::vector<std::vector<T1>> nest_nest_x{nest_x, nest_x, nest_x};
   std::vector<std::vector<T2>> nest_nest_y{nest_y, nest_y, nest_y};
   auto nestnestvec_nestnestvec = f(nest_nest_x, nest_nest_y);
-  auto nestnestvec_scal = f(nest_nest_x, y(1));
-  auto scal_nestnestvec = f(x(1), nest_nest_y);
+  auto nestnestvec_scal = f(nest_nest_x, y(0));
+  auto scal_nestnestvec = f(x(0), nest_nest_y);
   for (int i = 0; i < 3; ++i) {
     for (int j = 0; j < 3; ++j) {
       for (int k = 0; k < x.size(); ++k) {
         EXPECT_FLOAT_EQ(f(nest_nest_x[i][j](k), nest_nest_y[i][j](k)),
                         nestnestvec_nestnestvec[i][j](k));
-        EXPECT_FLOAT_EQ(f(nest_nest_x[i][j](k), y(1)),
+        EXPECT_FLOAT_EQ(f(nest_nest_x[i][j](k), y(0)),
                         nestnestvec_scal[i][j](k));
-        EXPECT_FLOAT_EQ(f(x(1), nest_nest_y[i][j](k)),
+        EXPECT_FLOAT_EQ(f(x(0), nest_nest_y[i][j](k)),
                         scal_nestnestvec[i][j](k));
       }
     }
