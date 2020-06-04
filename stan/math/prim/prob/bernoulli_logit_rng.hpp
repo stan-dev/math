01f8b469d50d7e1100a0cfa5eeb01600f7d37f93
Steve Bronder <Stevo15025@gmail.com>
2020-06-04 17:10:49 -0400

    Revert "Generalize bernoulli distribution functions"

--- stan/math/prim/prob/bernoulli_logit_rng.hpp
+++ stan/math/prim/prob/bernoulli_logit_rng.hpp
@@ -30,11 +30,10 @@ inline typename VectorBuilder<true, int, T_t>::type bernoulli_logit_rng(
     const T_t& t, RNG& rng) {
   using boost::bernoulli_distribution;
   using boost::variate_generator;
-  ref_type_t<T_t> t_ref = t;
   check_finite("bernoulli_logit_rng", "Logit transformed probability parameter",
-               t_ref);
+               t);
 
-  scalar_seq_view<T_t> t_vec(t_ref);
+  scalar_seq_view<T_t> t_vec(t);
   size_t N = stan::math::size(t);
   VectorBuilder<true, int, T_t> output(N);
 
