a07140c371481b548a932abf78618dd40031b63d
Stan Jenkins <mc.stanislaw@gmail.com>
2020-06-04 10:35:12 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2 (tags/RELEASE_600/final)

--- stan/math/rev/core/operator_plus_equal.hpp
+++ stan/math/rev/core/operator_plus_equal.hpp
@@ -10,14 +10,16 @@ namespace math {
 
 template <typename T>
 template <typename S, require_convertible_t<S&, promote_args_t<T>>*>
-inline var_value<T>& var_value<T, require_floating_point_t<T>>::operator+=(const var_value<S>& b) {
+inline var_value<T>& var_value<T, require_floating_point_t<T>>::operator+=(
+    const var_value<S>& b) {
   vi_ = new internal::add_vv_vari(vi_, b.vi_);
   return *this;
 }
 
 template <typename T>
 template <typename Arith, require_arithmetic_t<Arith>*>
-inline var_value<T>& var_value<T, require_floating_point_t<T>>::operator+=(Arith b) {
+inline var_value<T>& var_value<T, require_floating_point_t<T>>::operator+=(
+    Arith b) {
   if (b == 0.0) {
     return *this;
   }
