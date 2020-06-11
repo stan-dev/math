622755cd7115ff050e0739a0ba4efe70c407673d
Niko Huurre <nhuurre@protonmail.com>
2020-06-11 15:35:16 +0300

    more general check_matching_dims

--- stan/math/prim/err/check_matching_dims.hpp
+++ stan/math/prim/err/check_matching_dims.hpp
@@ -70,6 +70,43 @@ inline void check_matching_dims(const char* function, const char* name1,
   check_matching_dims(function, name1, y1, name2, y2);
 }
 
+/**
+ * Check if the two scalars are of the same size. The check always succeeds.
+ * @tparam T1 type of the first scalar
+ * @tparam T2 type of the second scalar
+ * @param function name of function (for error messages)
+ * @param name1 variable name for the first scalar (for error messages)
+ * @param y1 first scalar to test
+ * @param name2 variable name for the second scalar (for error messages)
+ * @param y2 second scalar to test
+ */
+template <typename T1, typename T2,
+          require_all_stan_scalar_t<T1, T2>* = nullptr>
+inline void check_matching_dims(const char* function, const char* name1,
+                                const T1& y1, const char* name2, const T2& y2) {
+}
+
+/**
+ * Check if the two containers have the same dimensions.
+ * @tparam Mat1 type of the first container
+ * @tparam Mat2 type of the second container
+ * @param function name of function (for error messages)
+ * @param name1 variable name for the first container (for error messages)
+ * @param y1 first container to test
+ * @param name2 variable name for the second container (for error messages)
+ * @param y2 second container to test
+ * @throw <code>std::invalid_argument</code> if the dimensions of the
+ *    containers do not match
+ */
+template <typename T1, typename T2>
+inline void check_matching_dims(const char* function, const char* name1,
+                                const std::vector<T1>& y1, const char* name2,
+                                const std::vector<T2>& y2) {
+  check_size_match(function, name1, y1.size(), name2, y2.size());
+  if (y1.size() != 0)
+    check_matching_dims(function, name1, y1[0], name2, y2[0]);
+}
+
 }  // namespace math
 }  // namespace stan
 #endif
