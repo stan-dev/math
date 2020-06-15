aa3d55299d84c8067c1407967beff264d81ab59b
Steve Bronder <stevo15025@gmail.com>
2020-06-15 16:16:34 -0400

    Have likely and unlikely defined by boost_likely/unlikely macros

--- stan/math/prim/meta/likely.hpp
+++ stan/math/prim/meta/likely.hpp
@@ -1,12 +1,8 @@
 #ifndef STAN_MATH_PRIM_META_LIKELY_HPP
 #define STAN_MATH_PRIM_META_LIKELY_HPP
 
-#ifdef __GNUC__
-#define likely(x) __builtin_expect(!!(x), 1)
-#define unlikely(x) __builtin_expect(!!(x), 0)
-#else
-#define likely(x) (x)
-#define unlikely(x) (x)
-#endif
+#include <boost/config.hpp>
+#define likely(x) BOOST_LIKELY(x)
+#define unlikely(x) BOOST_UNLIKELY(x)
 
 #endif
