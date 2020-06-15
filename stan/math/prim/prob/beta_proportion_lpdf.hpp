0b9c019747ce3b6876309b965e24d844506c4b6f
tadej <tadej.c@gmail.com>
2020-06-15 09:11:27 +0200

    added missing include

--- stan/math/prim/prob/beta_proportion_lpdf.hpp
+++ stan/math/prim/prob/beta_proportion_lpdf.hpp
@@ -9,6 +9,7 @@
 #include <stan/math/prim/fun/log.hpp>
 #include <stan/math/prim/fun/log1m.hpp>
 #include <stan/math/prim/fun/max_size.hpp>
+#include <stan/math/prim/fun/promote_scalar.hpp>
 #include <stan/math/prim/fun/size.hpp>
 #include <stan/math/prim/fun/size_zero.hpp>
 #include <stan/math/prim/fun/to_ref.hpp>
