e30e11121efd520cc9dace588bca52c3399b5491
Steve Bronder <stevo15025@gmail.com>
2020-06-16 14:54:49 -0400

    add dense eigen vari type

--- stan/math/rev/core/chainablestack.hpp
+++ stan/math/rev/core/chainablestack.hpp
@@ -2,19 +2,11 @@
 #define STAN_MATH_REV_CORE_CHAINABLESTACK_HPP
 
 #include <stan/math/rev/core/autodiffstackstorage.hpp>
+#include <stan/math/rev/core/make_vari_variadic.hpp>
 #include <boost/variant2/variant.hpp>
 namespace stan {
 namespace math {
-
-template <typename T, typename = void>
-class vari_value;
-class vari_base;
 class chainable_alloc;
-
-using vari_variant
-    = boost::variant2::variant<vari_value<double>*, vari_value<float>*,
-                               vari_value<long double>*>;
-
 using ChainableStack = AutodiffStackSingleton<vari_variant, chainable_alloc>;
 
 }  // namespace math
--- stan/math/rev/core/set_zero_all_adjoints_nested.hpp
+++ stan/math/rev/core/set_zero_all_adjoints_nested.hpp
@@ -27,7 +27,7 @@ static EIGEN_STRONG_INLINE void set_zero_all_adjoints_nested() {
   const auto stack_size = ChainableStack::instance_->var_stack_.size();
   // avoid wrap with unsigned when start1 == 0
   for (size_t i = (start1 == 0U) ? 0U : (start1 - 1); i < stack_size; ++i) {
-    boost::variant2::visit([](auto& x) { x->adj_ = 0.0; },
+    boost::variant2::visit([](auto& x) { x->set_zero_adjoint(); },
                            ChainableStack::instance_->var_stack_[i]);
   }
 
@@ -37,7 +37,7 @@ static EIGEN_STRONG_INLINE void set_zero_all_adjoints_nested() {
       = ChainableStack::instance_->var_nochain_stack_.size();
   for (size_t i = (start2 == 0U) ? 0U : (start2 - 1); i < nochain_stack_size;
        ++i) {
-    boost::variant2::visit([](auto& x) { x->adj_ = 0.0; },
+    boost::variant2::visit([](auto& x) { x->set_zero_adjoint(); },
                            ChainableStack::instance_->var_nochain_stack_[i]);
   }
 }
