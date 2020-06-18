ad59a61a8a768017e5531bc436179fd40e4b201c
tadej <tadej.c@gmail.com>
2020-06-18 11:15:27 +0200

    added missing include

--- stan/math/prim/prob/cauchy_lpdf.hpp
+++ stan/math/prim/prob/cauchy_lpdf.hpp
@@ -9,6 +9,7 @@
 #include <stan/math/prim/fun/max_size.hpp>
 #include <stan/math/prim/fun/size.hpp>
 #include <stan/math/prim/fun/size_zero.hpp>
+#include <stan/math/prim/fun/to_ref.hpp>
 #include <stan/math/prim/fun/value_of.hpp>
 #include <cmath>
 
