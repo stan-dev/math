b06ff9778336b5f500a1376eaa320ef649f0a442
Steve Bronder <stevo15025@gmail.com>
2020-06-23 22:09:38 -0400

    make_unique for ad_tape_observer

--- stan/math/rev/core/init_chainablestack.hpp
+++ stan/math/rev/core/init_chainablestack.hpp
@@ -24,7 +24,7 @@ namespace math {
  * on the observer concept.
  */
 class ad_tape_observer final : public tbb::task_scheduler_observer {
-  using stack_ptr = std::unique_ptr<ChainableStack>;
+  using stack_ptr = std::unique_ptr<const ChainableStack>;
   using ad_map = std::unordered_map<std::thread::id, stack_ptr>;
 
  public:
@@ -37,13 +37,9 @@ class ad_tape_observer final : public tbb::task_scheduler_observer {
 
   inline void on_scheduler_entry(bool worker) {
     std::lock_guard<std::mutex> thread_tape_map_lock(thread_tape_map_mutex_);
-    const std::thread::id thread_id = std::this_thread::get_id();
     if (thread_tape_map_.find(thread_id) == thread_tape_map_.end()) {
-      ad_map::iterator insert_elem;
-      bool status = false;
-      std::tie(insert_elem, status)
-          = thread_tape_map_.emplace(ad_map::value_type{thread_id, nullptr});
-      insert_elem->second = stack_ptr(new ChainableStack());
+      thread_tape_map_.emplace(std::piecewise_constuct, std::forward_as_tuple(thread_id),
+			 std::forward_as_tuple(std::make_unique<const ChainableStack>()));
     }
   }
 
