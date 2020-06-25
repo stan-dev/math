d5ea83e05c0d9150144d1e7f6c66f722e77fac81
Steve Bronder <stevo15025@gmail.com>
2020-06-25 13:57:23 -0400

    cleanup ad_tape_observer

--- stan/math/rev/core/init_chainablestack.hpp
+++ stan/math/rev/core/init_chainablestack.hpp
@@ -23,42 +23,33 @@ namespace math {
  * Refer to https://software.intel.com/en-us/node/506314 for details
  * on the observer concept.
  */
-class ad_tape_observer : public tbb::task_scheduler_observer {
-  using stack_ptr = std::unique_ptr<ChainableStack>;
-  using ad_map = std::unordered_map<std::thread::id, stack_ptr>;
+ class ad_tape_observer final : public tbb::task_scheduler_observer {
+   using stack_ptr = std::unique_ptr<const ChainableStack>;
+   using ad_map = std::unordered_map<std::thread::id, stack_ptr>;
 
- public:
-  ad_tape_observer() : tbb::task_scheduler_observer(), thread_tape_map_() {
-    on_scheduler_entry(true);  // register current process
-    observe(true);             // activates the observer
-  }
+  public:
+   ad_tape_observer() noexcept : tbb::task_scheduler_observer(), thread_tape_map_() {
+     on_scheduler_entry(true);  // register current process
+     observe(true);             // activates the observer
+   }
 
-  ~ad_tape_observer() { observe(false); }
+   ~ad_tape_observer() noexcept { observe(false); }
 
-  void on_scheduler_entry(bool worker) {
-    std::lock_guard<std::mutex> thread_tape_map_lock(thread_tape_map_mutex_);
-    const std::thread::id thread_id = std::this_thread::get_id();
-    if (thread_tape_map_.find(thread_id) == thread_tape_map_.end()) {
-      ad_map::iterator insert_elem;
-      bool status = false;
-      std::tie(insert_elem, status)
-          = thread_tape_map_.emplace(ad_map::value_type{thread_id, nullptr});
-      insert_elem->second = stack_ptr(new ChainableStack());
-    }
-  }
+   inline void on_scheduler_entry(bool worker) noexcept {
+     std::lock_guard<std::mutex> thread_tape_map_lock(thread_tape_map_mutex_);
+     thread_tape_map_.emplace(std::piecewise_construct,
+        std::forward_as_tuple(std::this_thread::get_id()), std::forward_as_tuple(std::make_unique<const ChainableStack>()));
+   }
 
-  void on_scheduler_exit(bool worker) {
-    std::lock_guard<std::mutex> thread_tape_map_lock(thread_tape_map_mutex_);
-    auto elem = thread_tape_map_.find(std::this_thread::get_id());
-    if (elem != thread_tape_map_.end()) {
-      thread_tape_map_.erase(elem);
-    }
-  }
+   inline void on_scheduler_exit(bool worker) noexcept {
+     std::lock_guard<std::mutex> thread_tape_map_lock(thread_tape_map_mutex_);
+     thread_tape_map_.erase(std::this_thread::get_id());
+   }
 
- private:
-  ad_map thread_tape_map_;
-  std::mutex thread_tape_map_mutex_;
-};
+  private:
+   ad_map thread_tape_map_;
+   std::mutex thread_tape_map_mutex_;
+ };
 
 namespace {
 
