60f54abaf2cb8405a74f01ae12dabea47e0e54cc
Steve Bronder <stevo15025@gmail.com>
2020-07-13 10:58:49 -0400

    Revert "move around fvar Eigen_Numtraits"
    This reverts commit fb317b7bf22dd348b6a4b824e7cdc8da7f5d24cb.

--- stan/math/fwd/meta/partials_type.hpp
+++ stan/math/fwd/meta/partials_type.hpp
@@ -1,15 +1,12 @@
 #ifndef STAN_MATH_FWD_META_PARTIALS_TYPE_HPP
 #define STAN_MATH_FWD_META_PARTIALS_TYPE_HPP
 
+#include <stan/math/fwd/core.hpp>
 #include <stan/math/fwd/meta/is_fvar.hpp>
 #include <stan/math/prim/meta/partials_type.hpp>
 #include <type_traits>
 
 namespace stan {
-namespace math {
-  template <typename T>
-  struct fvar;
-}
 
 template <typename T>
 struct partials_type<T, require_fvar_t<T>> {
