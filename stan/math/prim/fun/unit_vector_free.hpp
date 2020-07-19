ca3f57df50588ba53df9a88e738d50bc946b4178
Steve Bronder <stevo15025@gmail.com>
2020-07-19 18:34:16 -0400

    return eval'd value for var value_of and use forwarding in unit_vector_free

--- stan/math/prim/fun/unit_vector_free.hpp
+++ stan/math/prim/fun/unit_vector_free.hpp
@@ -19,7 +19,7 @@ namespace math {
  * @return Unit vector of dimension K considered "free"
  */
 template <typename EigVec, require_eigen_vector_t<EigVec>* = nullptr>
-auto unit_vector_free(EigVec&& x) {
+decltype(auto) unit_vector_free(EigVec&& x) {
   check_unit_vector("stan::math::unit_vector_free", "Unit vector variable", x);
   return std::forward<EigVec>(x);
 }
