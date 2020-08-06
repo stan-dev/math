cebcc1f6a7bdded93a3c61f3df218e110e3cc24b
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-08-07 00:12:06 +0800

    binary funs m-z

--- stan/math/prim/fun/multiply_log.hpp
+++ stan/math/prim/fun/multiply_log.hpp
@@ -3,6 +3,7 @@
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/fun/log.hpp>
+#include <stan/math/prim/functor/apply_scalar_binary.hpp>
 #include <cmath>
 
 namespace stan {
@@ -48,13 +49,31 @@ namespace math {
  * @param b the second variable
  * @return a * log(b)
  */
-template <typename T_a, typename T_b>
+template <typename T_a, typename T_b,
+          require_all_arithmetic_t<T_a, T_b>* = nullptr>
 inline return_type_t<T_a, T_b> multiply_log(const T_a a, const T_b b) {
   using std::log;
   if (b == 0.0 && a == 0.0) {
     return 0.0;
   }
   return a * log(b);
+}
+
+/**
+ * Enables the vectorised application of the multiply_log
+ * function, when the first and/or second arguments are containers.
+ *
+ * @tparam T1 type of first input
+ * @tparam T2 type of second input
+ * @param a First input
+ * @param b Second input
+ * @return multiply_log function applied to the two inputs.
+ */
+template <typename T1, typename T2, require_any_container_t<T1, T2>* = nullptr>
+inline auto multiply_log(const T1& a, const T2& b) {
+  return apply_scalar_binary(
+      a, b, [&](const auto& c, const auto& d) {
+        return multiply_log(c, d); });
 }
 
 }  // namespace math
