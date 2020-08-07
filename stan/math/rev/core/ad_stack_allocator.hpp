c42690e67e55f400a5e57a92a4ed91d1843975f9
tadej <tadej.c@gmail.com>
2020-08-07 13:51:28 +0200

    added missing file

---
+++ stan/math/rev/core/ad_stack_allocator.hpp
@@ -0,0 +1,48 @@
+#ifndef STAN_MATH_REV_CORE_AD_STACK_ALLOCATOR_HPP
+#define STAN_MATH_REV_CORE_AD_STACK_ALLOCATOR_HPP
+
+#include <stan/math/rev/core/chainablestack.hpp>
+
+namespace stan {
+namespace math {
+
+/**
+ * std library compatible allocator that uses AD stack.
+ * @tparam T type of scalar
+ */
+template <typename T>
+struct AD_stack_allocator {
+  using value_type = T;
+
+  /**
+   * Allocates space for `n` items of type `T`.
+   *
+   * @param n number of items to allocate space for
+   * @return pointer to allocated space
+   */
+  T* allocate(std::size_t n) {
+    return ChainableStack::instance_->memalloc_.alloc_array<T>(n);
+  }
+
+  /**
+   * No-op. Memory is dealocated by caling `recover_memory()`.
+   */
+  void deallocate(T* /*p*/, std::size_t /*n*/) noexcept {}
+
+  /**
+   * Equality comparison operator.
+   * @return true
+   */
+  bool operator==(const AD_stack_allocator&) { return true; }
+
+  /**
+   * Inequality comparison operator.
+   * @return false
+   */
+  bool operator!=(const AD_stack_allocator&) { return false; }
+};
+
+}  // namespace math
+}  // namespace stan
+
+#endif
