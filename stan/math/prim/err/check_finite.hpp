87b9734d05b7a7aa907593f7accf60e3ade8a5f7
Ben <bbbales2@gmail.com>
2020-08-07 17:45:09 -0700

    Added back in isFinite optimization (Issue #1635)

--- stan/math/prim/err/check_finite.hpp
+++ stan/math/prim/err/check_finite.hpp
@@ -5,6 +5,23 @@
 
 namespace stan {
 namespace math {
+  
+/**
+ * Check if each element of the eigen variable <code>y</code> is finite.
+ *
+ * @tparam T_y type of y
+ * @param function function name (for error messages)
+ * @param name variable name (for error messages)
+ * @param y variable to check
+ * @throw <code>domain_error</code> if y is infinity, -infinity, or NaN
+ */
+template <typename T_y, require_eigen_t<T_y>* = nullptr>
+inline void check_finite(const char* function, const char* name, const T_y& y) {
+  if(!value_of_rec(y).allFinite()) {
+    auto is_good = [](const auto& y) { return std::isfinite(y); };
+    elementwise_check(is_good, function, name, y, ", but must be finite!");
+  }
+}
 
 /**
  * Check if <code>y</code> is finite.
@@ -16,7 +33,7 @@ namespace math {
  * @param y variable to check
  * @throw <code>domain_error</code> if y is infinity, -infinity, or NaN
  */
-template <typename T_y>
+template <typename T_y, require_not_eigen_t<T_y>* = nullptr>
 inline void check_finite(const char* function, const char* name, const T_y& y) {
   auto is_good = [](const auto& y) { return std::isfinite(y); };
   elementwise_check(is_good, function, name, y, ", but must be finite!");
