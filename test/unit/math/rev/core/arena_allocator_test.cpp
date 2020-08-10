cf2c2ca89cd894e7585493d910a4655b228dd91d
Stan Jenkins <mc.stanislaw@gmail.com>
2020-08-10 07:58:41 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/rev/core/arena_allocator_test.cpp
+++ test/unit/math/rev/core/arena_allocator_test.cpp
@@ -8,22 +8,28 @@ void arena_allocator_test() {
   for (int i = 0; i < 5; i++) {
     v.push_back(1);
   }
-  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(v.data()));
-  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(v.data() + v.size()));
+  EXPECT_TRUE(
+      stan::math::ChainableStack::instance_->memalloc_.in_stack(v.data()));
+  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(
+      v.data() + v.size()));
 
   std::vector<int, stan::math::arena_allocator<int>> v2(2, 3);
   for (int i = 0; i < 5; i++) {
     v2.push_back(2);
   }
-  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(v2.data()));
-  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(v2.data() + v2.size()));
+  EXPECT_TRUE(
+      stan::math::ChainableStack::instance_->memalloc_.in_stack(v2.data()));
+  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(
+      v2.data() + v2.size()));
 
   std::vector<int, stan::math::arena_allocator<int>> v3(v2);
   for (int i = 0; i < 15; i++) {
     v3.push_back(3);
   }
-  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(v3.data()));
-  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(v3.data() + v3.size()));
+  EXPECT_TRUE(
+      stan::math::ChainableStack::instance_->memalloc_.in_stack(v3.data()));
+  EXPECT_TRUE(stan::math::ChainableStack::instance_->memalloc_.in_stack(
+      v3.data() + v3.size()));
 }
 
 TEST(AgradRev, arena_allocator_test) {
