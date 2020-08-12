a244f02de016e2206abf491cb6b82c5ba2af3930
tadej <tadej.c@gmail.com>
2020-08-12 11:38:00 +0200

    added missing includes

--- stan/math/rev/fun/softmax.hpp
+++ stan/math/rev/fun/softmax.hpp
@@ -2,11 +2,13 @@
 #define STAN_MATH_REV_FUN_SOFTMAX_HPP
 
 #include <stan/math/rev/meta.hpp>
+#include <stan/math/rev/fun/value_of.hpp>
 #include <stan/math/rev/functor/reverse_pass_callback.hpp>
 #include <stan/math/rev/functor/arena_matrix.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
 #include <stan/math/prim/fun/typedefs.hpp>
 #include <stan/math/prim/fun/softmax.hpp>
+#include <stan/math/prim/fun/value_of.hpp>
 #include <tuple>
 #include <vector>
 
