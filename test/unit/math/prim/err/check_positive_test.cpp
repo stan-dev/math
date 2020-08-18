14f08d9f5199c3779d6f67b0a5d3c493d339cf25
Stan Jenkins <mc.stanislaw@gmail.com>
2020-08-18 22:52:59 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/prim/err/check_positive_test.cpp
+++ test/unit/math/prim/err/check_positive_test.cpp
@@ -20,7 +20,8 @@ TEST(ErrorHandlingArr, CheckPositive_nan) {
 
   for (size_t i = 0; i < x.size(); i++) {
     x[i] = nan;
-    EXPECT_THROW(check_positive("check_positive_test", "x", x), std::domain_error);
+    EXPECT_THROW(check_positive("check_positive_test", "x", x),
+                 std::domain_error);
     x[i] = i;
   }
 }
@@ -34,7 +35,8 @@ TEST(ErrorHandlingMat, CheckPositive) {
   }
 
   x_mat(0) = 0;
-  EXPECT_THROW(check_positive("check_positive_test", "x", x_mat), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", x_mat),
+               std::domain_error);
 }
 
 TEST(ErrorHandlingMat, CheckPositive_nan) {
@@ -45,7 +47,8 @@ TEST(ErrorHandlingMat, CheckPositive_nan) {
   x_mat << 1, 2, 3;
   for (int i = 0; i < x_mat.size(); i++) {
     x_mat(i) = nan;
-    EXPECT_THROW(check_positive("check_positive_test", "x", x_mat), std::domain_error);
+    EXPECT_THROW(check_positive("check_positive_test", "x", x_mat),
+                 std::domain_error);
     x_mat(i) = i;
   }
 }
@@ -59,26 +62,32 @@ TEST(ErrorHandlingScalar, CheckPositive_nan) {
   using stan::math::check_positive;
   double nan = std::numeric_limits<double>::quiet_NaN();
 
-  EXPECT_THROW(check_positive("check_positive_test", "x", nan), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", nan),
+               std::domain_error);
 }
 
 TEST(ErrorHandlingScalar, CheckPositive_0) {
   using stan::math::check_positive;
-  EXPECT_THROW(check_positive("check_positive_test", "x", 0u), std::domain_error);
-  EXPECT_THROW(check_positive("check_positive_test", "x", (size_t)0), std::domain_error);
-  EXPECT_THROW(check_positive("check_positive_test", "x", 0.0), std::domain_error);
-  EXPECT_THROW(check_positive("check_positive_test", "x", -0.0), std::domain_error);
-  EXPECT_THROW(check_positive("check_positive_test", "x", 0), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", 0u),
+               std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", (size_t)0),
+               std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", 0.0),
+               std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", -0.0),
+               std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", 0),
+               std::domain_error);
 }
 
 TEST(ErrorHandlingScalar, CheckPositiveVectorization) {
   using stan::math::check_positive;
   Eigen::MatrixXd m = Eigen::MatrixXd::Constant(3, 2, 1);
-  EXPECT_NO_THROW(
-      check_positive("check_positive_test", "m", std::vector<Eigen::MatrixXd>{m, m, m}));
+  EXPECT_NO_THROW(check_positive("check_positive_test", "m",
+                                 std::vector<Eigen::MatrixXd>{m, m, m}));
   Eigen::MatrixXd m2 = m;
   m2(1, 1) = -1;
-  EXPECT_THROW(
-      check_positive("check_positive_test", "m", std::vector<Eigen::MatrixXd>{m, m2, m}),
-      std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "m",
+                              std::vector<Eigen::MatrixXd>{m, m2, m}),
+               std::domain_error);
 }
