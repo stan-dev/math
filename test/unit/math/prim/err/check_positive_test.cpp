165231190d767a293d067eb0d4556477235fc12a
Ben <bbbales2@gmail.com>
2020-08-18 15:45:52 -0700

    Added missing function names to tests(Issue #1635)

--- test/unit/math/prim/err/check_positive_test.cpp
+++ test/unit/math/prim/err/check_positive_test.cpp
@@ -8,7 +8,7 @@ TEST(ErrorHandlingArr, CheckPositive) {
   std::vector<double> x = {1, 2, 3};
 
   for (size_t i = 0; i < x.size(); i++) {
-    EXPECT_NO_THROW(check_positive(function, "x", x));
+    EXPECT_NO_THROW(check_positive("check_positive_test", "x", x));
   }
 }
 
@@ -20,7 +20,7 @@ TEST(ErrorHandlingArr, CheckPositive_nan) {
 
   for (size_t i = 0; i < x.size(); i++) {
     x[i] = nan;
-    EXPECT_THROW(check_positive(function, "x", x), std::domain_error);
+    EXPECT_THROW(check_positive("check_positive_test", "x", x), std::domain_error);
     x[i] = i;
   }
 }
@@ -30,11 +30,11 @@ TEST(ErrorHandlingMat, CheckPositive) {
   Eigen::Matrix<double, Eigen::Dynamic, 1> x_mat(3);
   x_mat << 1, 2, 3;
   for (int i = 0; i < x_mat.size(); i++) {
-    EXPECT_NO_THROW(check_positive(function, "x", x_mat));
+    EXPECT_NO_THROW(check_positive("check_positive_test", "x", x_mat));
   }
 
   x_mat(0) = 0;
-  EXPECT_THROW(check_positive(function, "x", x_mat), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", x_mat), std::domain_error);
 }
 
 TEST(ErrorHandlingMat, CheckPositive_nan) {
@@ -45,40 +45,40 @@ TEST(ErrorHandlingMat, CheckPositive_nan) {
   x_mat << 1, 2, 3;
   for (int i = 0; i < x_mat.size(); i++) {
     x_mat(i) = nan;
-    EXPECT_THROW(check_positive(function, "x", x_mat), std::domain_error);
+    EXPECT_THROW(check_positive("check_positive_test", "x", x_mat), std::domain_error);
     x_mat(i) = i;
   }
 }
 
 TEST(ErrorHandlingScalar, CheckPositive) {
   using stan::math::check_positive;
-  EXPECT_NO_THROW(check_positive(function, "x", 3.0));
+  EXPECT_NO_THROW(check_positive("check_positive_test", "x", 3.0));
 }
 
 TEST(ErrorHandlingScalar, CheckPositive_nan) {
   using stan::math::check_positive;
   double nan = std::numeric_limits<double>::quiet_NaN();
 
-  EXPECT_THROW(check_positive(function, "x", nan), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", nan), std::domain_error);
 }
 
 TEST(ErrorHandlingScalar, CheckPositive_0) {
   using stan::math::check_positive;
-  EXPECT_THROW(check_positive(function, "x", 0u), std::domain_error);
-  EXPECT_THROW(check_positive(function, "x", (size_t)0), std::domain_error);
-  EXPECT_THROW(check_positive(function, "x", 0.0), std::domain_error);
-  EXPECT_THROW(check_positive(function, "x", -0.0), std::domain_error);
-  EXPECT_THROW(check_positive(function, "x", 0), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", 0u), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", (size_t)0), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", 0.0), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", -0.0), std::domain_error);
+  EXPECT_THROW(check_positive("check_positive_test", "x", 0), std::domain_error);
 }
 
 TEST(ErrorHandlingScalar, CheckPositiveVectorization) {
   using stan::math::check_positive;
   Eigen::MatrixXd m = Eigen::MatrixXd::Constant(3, 2, 1);
   EXPECT_NO_THROW(
-      check_positive(function, "m", std::vector<Eigen::MatrixXd>{m, m, m}));
+      check_positive("check_positive_test", "m", std::vector<Eigen::MatrixXd>{m, m, m}));
   Eigen::MatrixXd m2 = m;
   m2(1, 1) = -1;
   EXPECT_THROW(
-      check_positive(function, "m", std::vector<Eigen::MatrixXd>{m, m2, m}),
+      check_positive("check_positive_test", "m", std::vector<Eigen::MatrixXd>{m, m2, m}),
       std::domain_error);
 }
