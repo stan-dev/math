92e24fd66ff490501877b818f3a0a64e9b6a7038
steve <stevo15025@gmail.com>
2020-08-24 19:47:58 -0400

    add type trait for plain dense eigen objects and fix doxygen for acos

--- test/unit/math/mix/meta/is_eigen_dense_plain_type_test.cpp
+++ test/unit/math/mix/meta/is_eigen_dense_plain_type_test.cpp
@@ -8,17 +8,17 @@ TEST(MathMetaPrim, is_eigen_dense_plain_type_hierarchy_tests) {
   using Eigen::Matrix;
   using stan::is_eigen_dense_plain_type;
   using stan::math::test::all_eigen_dense;
-  all_eigen_dense<false, true, true, true, true, is_eigen_dense_plain_type, Matrix,
+  all_eigen_dense<false, false, false, false, false, is_eigen_dense_plain_type, Matrix,
                   -1, -1>();
-  all_eigen_dense<false, true, true, true, true, is_eigen_dense_plain_type, Matrix,
+  all_eigen_dense<false, false, false, false, false, is_eigen_dense_plain_type, Matrix,
                   1, -1>();
-  all_eigen_dense<false, true, true, true, true, is_eigen_dense_plain_type, Matrix,
+  all_eigen_dense<false, false, false, false, false, is_eigen_dense_plain_type, Matrix,
                   -1, 1>();
-  all_eigen_dense<false, true, true, true, true, is_eigen_dense_plain_type, Array,
+  all_eigen_dense<false, false, false, false, false, is_eigen_dense_plain_type, Array,
                   -1, -1>();
-  all_eigen_dense<false, true, true, true, true, is_eigen_dense_plain_type, Array,
+  all_eigen_dense<false, false, false, false, false, is_eigen_dense_plain_type, Array,
                   1, -1>();
-  all_eigen_dense<false, true, true, true, true, is_eigen_dense_plain_type, Array,
+  all_eigen_dense<false, false, false, false, false, is_eigen_dense_plain_type, Array,
                   -1, 1>();
 }
 
