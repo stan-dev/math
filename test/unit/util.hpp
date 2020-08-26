2645257a0ed86dde5c4da005ae5a54fceff9922a
steve <stevo15025@gmail.com>
2020-08-26 18:52:46 -0400

    add teardown() to Agradrev

--- test/unit/util.hpp
+++ test/unit/util.hpp
@@ -224,6 +224,9 @@ struct AgradRev : public testing::Test {
     // make sure memory's clean before starting each test
     stan::math::recover_memory();
   }
+  void TearDown() {
+    stan::math::recover_memory();
+  }
 };
 
 #endif
