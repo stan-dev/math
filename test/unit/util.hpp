5b512d80390c75cca48259c539c38efd2e16c64a
Stan Jenkins <mc.stanislaw@gmail.com>
2020-08-30 18:43:34 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/util.hpp
+++ test/unit/util.hpp
@@ -219,9 +219,9 @@ auto make_sparse_matrix_random(int rows, int cols) {
 }  // namespace test
 
 namespace math {
-  // forward decl recover_memory
-  void recover_memory();
-}
+// forward decl recover_memory
+void recover_memory();
+}  // namespace math
 }  // namespace stan
 
 struct AgradRev : public testing::Test {
