434261b8e8d86616df20cbb13d806baf484e7627
steve <stevo15025@gmail.com>
2020-09-03 14:04:31 -0400

    uncomment vari vector view tests

--- test/unit/math/rev/core/vari_test.cpp
+++ test/unit/math/rev/core/vari_test.cpp
@@ -98,7 +98,6 @@ TEST(AgradRev, dense_vari_vector_views) {
   for (int i = 0; i < A.size(); ++i) {
     A(i) = i;
   }
-  /**
   stan::math::vari_value<eig_vec> A_v(A);
   auto A_sub = A_v.head(3);
   EXPECT_MATRIX_FLOAT_EQ(A_sub.val_, A_v.val_.head(3));
@@ -111,5 +110,4 @@ TEST(AgradRev, dense_vari_vector_views) {
   auto A_segment = A_v.segment(3, 5);
   EXPECT_MATRIX_FLOAT_EQ(A_segment.val_, A_v.val_.segment(3, 5));
   EXPECT_MATRIX_FLOAT_EQ(A, A_v.val_);
-  */
 }
