99c1ce5da96047b908b2cf02fb17b050b22b52ba
tadej <tadej.c@gmail.com>
2020-09-08 13:11:47 +0200

    added include

--- stan/math/prim/prob/inv_chi_square_lpdf.hpp
+++ stan/math/prim/prob/inv_chi_square_lpdf.hpp
@@ -9,6 +9,7 @@
 #include <stan/math/prim/fun/lgamma.hpp>
 #include <stan/math/prim/fun/log.hpp>
 #include <stan/math/prim/fun/max_size.hpp>
+#include <stan/math/prim/fun/promote_scalar.hpp>
 #include <stan/math/prim/fun/size.hpp>
 #include <stan/math/prim/fun/size_zero.hpp>
 #include <stan/math/prim/fun/value_of.hpp>
