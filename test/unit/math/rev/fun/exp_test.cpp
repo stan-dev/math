b5e2f114acab44ddd1dbb6256aabf0fba12704c8
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-09-14 18:26:32 +0800

    Simple test

--- test/unit/math/rev/fun/exp_test.cpp
+++ test/unit/math/rev/fun/exp_test.cpp
@@ -2,13 +2,6 @@
 #include <test/unit/util.hpp>
 #include <gtest/gtest.h>
 
-TEST(MathFunctions, expInt) {
-  using stan::math::exp;
-  EXPECT_FLOAT_EQ(std::exp(3), exp(3));
-  EXPECT_FLOAT_EQ(std::exp(3.1), exp(3.1));
-  EXPECT_FLOAT_EQ(std::exp(3.0), exp(3.0));
-}
-
 TEST(MathFunctions, exp_map) {
   using stan::math::exp;
   using stan::math::var;
