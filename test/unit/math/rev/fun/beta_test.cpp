7b9674054d7c7b83420b37e0affe173701f70581
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-09-18 16:03:47 +0800

    Fix beta test

--- test/unit/math/rev/fun/beta_test.cpp
+++ test/unit/math/rev/fun/beta_test.cpp
@@ -4,9 +4,8 @@
 
 TEST(MathFunctions, beta_map) {
   using stan::math::beta;
-  using stan::math::var;
-  using stan::math::vector_v;
-  vector_v in1 = matrix_v::Random(10000);
-  vector_v out = beta(in1, in1);
-  EXPECT_MATRIX_EQ(out.val(),beta(in1.val(), in1.val()));
+  using stan::math::matrix_v;
+  matrix_v in1 = matrix_v::Random(100, 100);
+  matrix_v out = beta(in1, in1);
+  EXPECT_MATRIX_EQ(out.val(), beta(in1.val(), in1.val()));
 }
