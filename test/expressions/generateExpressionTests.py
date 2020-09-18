69ab37c70680164e7e6a8b8c7891fd120c47b017
tadej <tadej.c@gmail.com>
2020-09-18 12:02:56 +0200

    set testing values

--- test/expressions/generateExpressionTests.py
+++ test/expressions/generateExpressionTests.py
@@ -133,6 +133,8 @@ special_arg_values = {
 	"categorical_log" : [None, 1],
 	"categorical_rng" : [1, None],
 	"categorical_lpmf" : [None, 1],
+  "dirichlet_log" : [1, None],
+  "dirichlet_lpdf" : [1, None],
 	"hmm_hidden_state_prob" : [None, 1, 1],
 	"hmm_latent_rng" : [None, 1, 1, None],
 	"hmm_marginal" : [None, 1, 1],
