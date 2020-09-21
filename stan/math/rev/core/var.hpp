01f00989fd18cf696ff91c1a8d168f96e3690fbc
Stan Jenkins <mc.stanislaw@gmail.com>
2020-09-21 21:00:40 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/core/var.hpp
+++ stan/math/rev/core/var.hpp
@@ -35,278 +35,272 @@ static void grad(Vari* vi);
  * var values objects.
  * @tparam T An Floating point type.
  */
- template <typename T>
- class var_value<T, require_floating_point_t<T>> {
-  public:
-   using value_type = std::decay_t<T>;  // type in vari_value.
-   using vari_type = vari_value<value_type>;
+template <typename T>
+class var_value<T, require_floating_point_t<T>> {
+ public:
+  using value_type = std::decay_t<T>;  // type in vari_value.
+  using vari_type = vari_value<value_type>;
 
-   static constexpr int RowsAtCompileTime{vari_type::RowsAtCompileTime};
-   static constexpr int ColsAtCompileTime{vari_type::ColsAtCompileTime};
+  static constexpr int RowsAtCompileTime{vari_type::RowsAtCompileTime};
+  static constexpr int ColsAtCompileTime{vari_type::ColsAtCompileTime};
 
-   /**
-    * Pointer to the implementation of this variable.
-    *
-    * This value should not be modified, but may be accessed in
-    * <code>var</code> operators to construct `vari_value<T>`
-    * instances.
-    */
-   vari_type* vi_;
+  /**
+   * Pointer to the implementation of this variable.
+   *
+   * This value should not be modified, but may be accessed in
+   * <code>var</code> operators to construct `vari_value<T>`
+   * instances.
+   */
+  vari_type* vi_;
 
-   /**
-    * Return `true` if this variable has been
-    * declared, but not been defined.  Any attempt to use an
-    * undefined variable's value or adjoint will result in a
-    * segmentation fault.
-    *
-    * @return <code>true</code> if this variable does not yet have
-    * a defined variable.
-    */
-   inline bool is_uninitialized() { return (vi_ == nullptr); }
+  /**
+   * Return `true` if this variable has been
+   * declared, but not been defined.  Any attempt to use an
+   * undefined variable's value or adjoint will result in a
+   * segmentation fault.
+   *
+   * @return <code>true</code> if this variable does not yet have
+   * a defined variable.
+   */
+  inline bool is_uninitialized() { return (vi_ == nullptr); }
 
-   /**
-    * Construct a variable for later assignment.
-    *
-    * This is implemented as a no-op, leaving the underlying implementation
-    * dangling.  Before an assignment, the behavior is thus undefined just
-    * as for a basic double.
-    */
-   var_value() : vi_(nullptr) {}
+  /**
+   * Construct a variable for later assignment.
+   *
+   * This is implemented as a no-op, leaving the underlying implementation
+   * dangling.  Before an assignment, the behavior is thus undefined just
+   * as for a basic double.
+   */
+  var_value() : vi_(nullptr) {}
 
-   /**
-    * Construct a variable from the specified floating point argument
-    * by constructing a new `vari_value<value_type>`. This constructor is only
-    * valid when `S` is convertible to this `vari_value`'s `value_type`.
-    * @tparam S A type that is convertible to `value_type`.
-    * @param x Value of the variable.
-    */
-   template <typename S, require_convertible_t<S&, value_type>* = nullptr>
-   var_value(const S& x) : vi_(new vari_type(x, false)) {}  // NOLINT
+  /**
+   * Construct a variable from the specified floating point argument
+   * by constructing a new `vari_value<value_type>`. This constructor is only
+   * valid when `S` is convertible to this `vari_value`'s `value_type`.
+   * @tparam S A type that is convertible to `value_type`.
+   * @param x Value of the variable.
+   */
+  template <typename S, require_convertible_t<S&, value_type>* = nullptr>
+  var_value(const S& x) : vi_(new vari_type(x, false)) {}  // NOLINT
 
-   /**
-    * Construct a variable from a pointer to a variable implementation.
-    * @param vi A vari_value pointer.
-    */
-   var_value(vari_type* vi) : vi_(vi) {}  // NOLINT
+  /**
+   * Construct a variable from a pointer to a variable implementation.
+   * @param vi A vari_value pointer.
+   */
+  var_value(vari_type* vi) : vi_(vi) {}  // NOLINT
 
-   /**
-    * Return a constant reference to the value of this variable.
-    *
-    * @return The value of this variable.
-    */
-   inline const auto& val() const { return vi_->val_; }
+  /**
+   * Return a constant reference to the value of this variable.
+   *
+   * @return The value of this variable.
+   */
+  inline const auto& val() const { return vi_->val_; }
 
-   /**
-    * Return a reference of the derivative of the root expression with
-    * respect to this expression.  This method only works
-    * after one of the `grad()` methods has been
-    * called.
-    *
-    * @return Adjoint for this variable.
-    */
-   inline auto& adj() const { return vi_->adj_; }
+  /**
+   * Return a reference of the derivative of the root expression with
+   * respect to this expression.  This method only works
+   * after one of the `grad()` methods has been
+   * called.
+   *
+   * @return Adjoint for this variable.
+   */
+  inline auto& adj() const { return vi_->adj_; }
 
-   /**
-    * Return a reference to the derivative of the root expression with
-    * respect to this expression.  This method only works
-    * after one of the `grad()` methods has been
-    * called.
-    *
-    * @return Adjoint for this variable.
-    */
-   inline auto& adj() { return vi_->adj_; }
+  /**
+   * Return a reference to the derivative of the root expression with
+   * respect to this expression.  This method only works
+   * after one of the `grad()` methods has been
+   * called.
+   *
+   * @return Adjoint for this variable.
+   */
+  inline auto& adj() { return vi_->adj_; }
 
-   inline Eigen::Index rows() const { return 0; }
-   inline Eigen::Index cols() const { return 0; }
-   inline Eigen::Index size() const { return 1; }
-   /**
-    * Compute the gradient of this (dependent) variable with respect to
-    * the specified vector of (independent) variables, assigning the
-    * specified vector to the gradient.
-    *
-    * The grad() function does <i>not</i> recover memory.  In Stan
-    * 2.4 and earlier, this function did recover memory.
-    *
-    * @tparam CheckContainer Not set by user. The default value of value_type
-    *  is used to require that grad is only available for scalar `var_value`
-    *  types.
-    * @param x Vector of independent variables.
-    * @param g Gradient vector of partial derivatives of this
-    * variable with respect to x.
-    */
-   inline void grad(std::vector<var_value<T>>& x, std::vector<value_type>& g) {
-     stan::math::grad(vi_);
-     g.resize(x.size());
-     for (size_t i = 0; i < x.size(); ++i) {
-       g[i] = x[i].vi_->adj_;
-     }
-   }
+  inline Eigen::Index rows() const { return 0; }
+  inline Eigen::Index cols() const { return 0; }
+  inline Eigen::Index size() const { return 1; }
+  /**
+   * Compute the gradient of this (dependent) variable with respect to
+   * the specified vector of (independent) variables, assigning the
+   * specified vector to the gradient.
+   *
+   * The grad() function does <i>not</i> recover memory.  In Stan
+   * 2.4 and earlier, this function did recover memory.
+   *
+   * @tparam CheckContainer Not set by user. The default value of value_type
+   *  is used to require that grad is only available for scalar `var_value`
+   *  types.
+   * @param x Vector of independent variables.
+   * @param g Gradient vector of partial derivatives of this
+   * variable with respect to x.
+   */
+  inline void grad(std::vector<var_value<T>>& x, std::vector<value_type>& g) {
+    stan::math::grad(vi_);
+    g.resize(x.size());
+    for (size_t i = 0; i < x.size(); ++i) {
+      g[i] = x[i].vi_->adj_;
+    }
+  }
 
-   /**
-    * Compute the gradient of this (dependent) variable with respect
-    * to all (independent) variables.
-    *
-    * @tparam CheckContainer Not set by user. The default value of value_type
-    *  is used to require that grad is only available for scalar `var_value`
-    *  types.
-    * The grad() function does <i>not</i> recover memory.
-    */
-   void grad() {
-     stan::math::grad(vi_);
-   }
+  /**
+   * Compute the gradient of this (dependent) variable with respect
+   * to all (independent) variables.
+   *
+   * @tparam CheckContainer Not set by user. The default value of value_type
+   *  is used to require that grad is only available for scalar `var_value`
+   *  types.
+   * The grad() function does <i>not</i> recover memory.
+   */
+  void grad() { stan::math::grad(vi_); }
 
-   // POINTER OVERRIDES
+  // POINTER OVERRIDES
 
-   /**
-    * Return a reference to underlying implementation of this variable.
-    *
-    * If <code>x</code> is of type <code>var</code>, then applying
-    * this operator, <code>*x</code>, has the same behavior as
-    * <code>*(x.vi_)</code>.
-    *
-    * <i>Warning</i>:  The returned reference does not track changes to
-    * this variable.
-    *
-    * @return variable
-    */
-   inline vari_type& operator*() { return *vi_; }
+  /**
+   * Return a reference to underlying implementation of this variable.
+   *
+   * If <code>x</code> is of type <code>var</code>, then applying
+   * this operator, <code>*x</code>, has the same behavior as
+   * <code>*(x.vi_)</code>.
+   *
+   * <i>Warning</i>:  The returned reference does not track changes to
+   * this variable.
+   *
+   * @return variable
+   */
+  inline vari_type& operator*() { return *vi_; }
 
-   /**
-    * Return a pointer to the underlying implementation of this variable.
-    *
-    * If <code>x</code> is of type <code>var</code>, then applying
-    * this operator, <code>x-&gt;</code>, behaves the same way as
-    * <code>x.vi_-&gt;</code>.
-    *
-    * <i>Warning</i>: The returned result does not track changes to
-    * this variable.
-    */
-   inline vari_type* operator->() { return vi_; }
+  /**
+   * Return a pointer to the underlying implementation of this variable.
+   *
+   * If <code>x</code> is of type <code>var</code>, then applying
+   * this operator, <code>x-&gt;</code>, behaves the same way as
+   * <code>x.vi_-&gt;</code>.
+   *
+   * <i>Warning</i>: The returned result does not track changes to
+   * this variable.
+   */
+  inline vari_type* operator->() { return vi_; }
 
-   // COMPOUND ASSIGNMENT OPERATORS
+  // COMPOUND ASSIGNMENT OPERATORS
 
-   /**
-    * The compound add/assignment operator for variables (C++).
-    *
-    * If this variable is a and the argument is the variable b,
-    * then (a += b) behaves exactly the same way as (a = a + b),
-    * creating an intermediate variable representing (a + b).
-    *
-    * @param b The variable to add to this variable.
-    * @return The result of adding the specified variable to this variable.
-    */
-   inline var_value<T>& operator+=(const var_value<T>& b);
+  /**
+   * The compound add/assignment operator for variables (C++).
+   *
+   * If this variable is a and the argument is the variable b,
+   * then (a += b) behaves exactly the same way as (a = a + b),
+   * creating an intermediate variable representing (a + b).
+   *
+   * @param b The variable to add to this variable.
+   * @return The result of adding the specified variable to this variable.
+   */
+  inline var_value<T>& operator+=(const var_value<T>& b);
 
-   /**
-    * The compound add/assignment operator for scalars (C++).
-    *
-    * If this variable is a and the argument is the scalar b, then
-    * (a += b) behaves exactly the same way as (a = a + b).  Note
-    * that the result is an assignable lvalue.
-    *
-    * @param b The scalar to add to this variable.
-    * @return The result of adding the specified variable to this variable.
-    */
-   inline var_value<T>& operator+=(T b);
+  /**
+   * The compound add/assignment operator for scalars (C++).
+   *
+   * If this variable is a and the argument is the scalar b, then
+   * (a += b) behaves exactly the same way as (a = a + b).  Note
+   * that the result is an assignable lvalue.
+   *
+   * @param b The scalar to add to this variable.
+   * @return The result of adding the specified variable to this variable.
+   */
+  inline var_value<T>& operator+=(T b);
 
-   /**
-    * The compound subtract/assignment operator for variables (C++).
-    *
-    * If this variable is a and the argument is the variable b,
-    * then (a -= b) behaves exactly the same way as (a = a - b).
-    * Note that the result is an assignable lvalue.
-    *
-    * @param b The variable to subtract from this variable.
-    * @return The result of subtracting the specified variable from
-    * this variable.
-    */
-   inline var_value<T>& operator-=(const var_value<T>& b);
+  /**
+   * The compound subtract/assignment operator for variables (C++).
+   *
+   * If this variable is a and the argument is the variable b,
+   * then (a -= b) behaves exactly the same way as (a = a - b).
+   * Note that the result is an assignable lvalue.
+   *
+   * @param b The variable to subtract from this variable.
+   * @return The result of subtracting the specified variable from
+   * this variable.
+   */
+  inline var_value<T>& operator-=(const var_value<T>& b);
 
-   /**
-    * The compound subtract/assignment operator for scalars (C++).
-    *
-    * If this variable is a and the argument is the scalar b, then
-    * (a -= b) behaves exactly the same way as (a = a - b).  Note
-    * that the result is an assignable lvalue.
-    *
-    * @param b The scalar to subtract from this variable.
-    * @return The result of subtracting the specified variable from this
-    * variable.
-    */
-   inline var_value<T>& operator-=(T b);
+  /**
+   * The compound subtract/assignment operator for scalars (C++).
+   *
+   * If this variable is a and the argument is the scalar b, then
+   * (a -= b) behaves exactly the same way as (a = a - b).  Note
+   * that the result is an assignable lvalue.
+   *
+   * @param b The scalar to subtract from this variable.
+   * @return The result of subtracting the specified variable from this
+   * variable.
+   */
+  inline var_value<T>& operator-=(T b);
 
-   /**
-    * The compound multiply/assignment operator for variables (C++).
-    *
-    * If this variable is a and the argument is the variable b,
-    * then (a *= b) behaves exactly the same way as (a = a * b).
-    * Note that the result is an assignable lvalue.
-    *
-    * @param b The variable to multiply this variable by.
-    * @return The result of multiplying this variable by the
-    * specified variable.
-    */
-   inline var_value<T>& operator*=(const var_value<T>& b);
+  /**
+   * The compound multiply/assignment operator for variables (C++).
+   *
+   * If this variable is a and the argument is the variable b,
+   * then (a *= b) behaves exactly the same way as (a = a * b).
+   * Note that the result is an assignable lvalue.
+   *
+   * @param b The variable to multiply this variable by.
+   * @return The result of multiplying this variable by the
+   * specified variable.
+   */
+  inline var_value<T>& operator*=(const var_value<T>& b);
 
-   /**
-    * The compound multiply/assignment operator for scalars (C++).
-    *
-    * If this variable is a and the argument is the scalar b, then
-    * (a *= b) behaves exactly the same way as (a = a * b).  Note
-    * that the result is an assignable lvalue.
-    *
-    * @param b The scalar to multiply this variable by.
-    * @return The result of multiplying this variable by the specified
-    * variable.
-    */
-   inline var_value<T>& operator*=(T b);
+  /**
+   * The compound multiply/assignment operator for scalars (C++).
+   *
+   * If this variable is a and the argument is the scalar b, then
+   * (a *= b) behaves exactly the same way as (a = a * b).  Note
+   * that the result is an assignable lvalue.
+   *
+   * @param b The scalar to multiply this variable by.
+   * @return The result of multiplying this variable by the specified
+   * variable.
+   */
+  inline var_value<T>& operator*=(T b);
 
-   /**
-    * The compound divide/assignment operator for variables (C++).  If this
-    * variable is a and the argument is the variable b, then (a /= b)
-    * behaves exactly the same way as (a = a / b).  Note that the
-    * result is an assignable lvalue.
-    *
-    * @param b The variable to divide this variable by.
-    * @return The result of dividing this variable by the
-    * specified variable.
-    */
-   inline var_value<T>& operator/=(const var_value<T>& b);
+  /**
+   * The compound divide/assignment operator for variables (C++).  If this
+   * variable is a and the argument is the variable b, then (a /= b)
+   * behaves exactly the same way as (a = a / b).  Note that the
+   * result is an assignable lvalue.
+   *
+   * @param b The variable to divide this variable by.
+   * @return The result of dividing this variable by the
+   * specified variable.
+   */
+  inline var_value<T>& operator/=(const var_value<T>& b);
 
-   /**
-    * The compound divide/assignment operator for scalars (C++).
-    *
-    * If this variable is a and the argument is the scalar b, then
-    * (a /= b) behaves exactly the same way as (a = a / b).  Note
-    * that the result is an assignable lvalue.
-    *
-    * @param b The scalar to divide this variable by.
-    * @return The result of dividing this variable by the specified
-    * variable.
-    */
-   inline var_value<T>& operator/=(T b);
+  /**
+   * The compound divide/assignment operator for scalars (C++).
+   *
+   * If this variable is a and the argument is the scalar b, then
+   * (a /= b) behaves exactly the same way as (a = a / b).  Note
+   * that the result is an assignable lvalue.
+   *
+   * @param b The scalar to divide this variable by.
+   * @return The result of dividing this variable by the specified
+   * variable.
+   */
+  inline var_value<T>& operator/=(T b);
 
-   /**
-    * Write the value of this autodiff variable and its adjoint to
-    * the specified output stream.
-    *
-    * @param os Output stream to which to write.
-    * @param v Variable to write.
-    * @return Reference to the specified output stream.
-    */
-   friend std::ostream& operator<<(std::ostream& os, const var_value<T>& v) {
-     if (v.vi_ == nullptr) {
-       return os << "uninitialized";
-     }
-     return os << v.val();
-   }
+  /**
+   * Write the value of this autodiff variable and its adjoint to
+   * the specified output stream.
+   *
+   * @param os Output stream to which to write.
+   * @param v Variable to write.
+   * @return Reference to the specified output stream.
+   */
+  friend std::ostream& operator<<(std::ostream& os, const var_value<T>& v) {
+    if (v.vi_ == nullptr) {
+      return os << "uninitialized";
+    }
+    return os << v.val();
+  }
 
-   inline auto& eval() noexcept {
-     return *this;
-   }
-
- };
-
+  inline auto& eval() noexcept { return *this; }
+};
 
 /**
  * Independent (input) and dependent (output) variables for gradients.
@@ -323,7 +317,9 @@ static void grad(Vari* vi);
  * @tparam T An Floating point type.
  */
 template <typename T>
-class var_value<T, require_t<bool_constant<is_eigen<T>::value || !is_plain_type<T>::value>>> {
+class var_value<
+    T,
+    require_t<bool_constant<is_eigen<T>::value || !is_plain_type<T>::value>>> {
   static_assert(
       std::is_floating_point<value_type_t<T>>::value,
       "The template for must be a floating point or a container holding"
@@ -397,8 +393,7 @@ class var_value<T, require_t<bool_constant<is_eigen<T>::value || !is_plain_type<
             require_convertible_t<S&, value_type>* = nullptr,
             require_not_plain_type_t<S>* = nullptr,
             require_plain_type_t<T_>* = nullptr>
-  var_value(const var_value<S>& other)
-      : vi_(new vari_type(other.vi_->val_)) {
+  var_value(const var_value<S>& other) : vi_(new vari_type(other.vi_->val_)) {
     reverse_pass_callback(
         [this_vi = this->vi_, other_vi = other.vi_]() mutable {
           other_vi->adj_ += this_vi->adj_;
