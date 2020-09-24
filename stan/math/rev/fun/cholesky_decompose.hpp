7e5c2b49b854adbd84a70c7c4affd8f410e76c73
Ben <bbbales2@gmail.com>
2020-09-23 23:06:59 -0400

    Added missing cholesky stuff (Issue #2018)

--- stan/math/rev/fun/cholesky_decompose.hpp
+++ stan/math/rev/fun/cholesky_decompose.hpp
@@ -23,7 +23,239 @@
 
 namespace stan {
 namespace math {
+namespace internal {
 
+/**
+ * Set the lower right triangular of a var matrix given a set of vari**
+ *
+ * @param L Matrix of vars
+ * @param vari_ref Values to be set in lower right triangular of L.
+ * @return None, L modified by reference.
+ */
+inline void set_lower_tri_coeff_ref(Eigen::Matrix<var, -1, -1>& L,
+                                    vari** vari_ref) {
+  size_t pos = 0;
+  vari* dummy = new vari(0.0, false);
+
+  for (size_type j = 0; j < L.cols(); ++j) {
+    for (size_type i = j; i < L.cols(); ++i) {
+      L.coeffRef(i, j).vi_ = vari_ref[pos++];
+    }
+    for (size_type k = 0; k < j; ++k) {
+      L.coeffRef(k, j).vi_ = dummy;
+    }
+  }
+  return;
+}
+}  // namespace internal
+
+class cholesky_block : public vari {
+ public:
+  int M_;
+  int block_size_;
+  using Block_ = Eigen::Block<Eigen::MatrixXd>;
+  vari** vari_ref_A_;
+  vari** vari_ref_L_;
+
+  /**
+   * Constructor for Cholesky function.
+   *
+   * Stores varis for A.  Instantiates and stores varis for L.
+   * Instantiates and stores dummy vari for upper triangular part of var
+   * result returned in cholesky_decompose function call
+   *
+   * variRefL aren't on the chainable autodiff stack, only used for storage
+   * and computation. Note that varis for L are constructed externally in
+   * cholesky_decompose.
+   *
+   * block_size_ determined using the same calculation Eigen/LLT.h
+   *
+   * @param A matrix
+   * @param L_A matrix, Cholesky factor of A
+   */
+  cholesky_block(const Eigen::Matrix<var, -1, -1>& A,
+                 const Eigen::Matrix<double, -1, -1>& L_A)
+      : vari(0.0),
+        M_(A.rows()),
+        vari_ref_A_(ChainableStack::instance_->memalloc_.alloc_array<vari*>(
+            A.rows() * (A.rows() + 1) / 2)),
+        vari_ref_L_(ChainableStack::instance_->memalloc_.alloc_array<vari*>(
+            A.rows() * (A.rows() + 1) / 2)) {
+    size_t pos = 0;
+    block_size_ = std::max(M_ / 8, 8);
+    block_size_ = std::min(block_size_, 128);
+    for (size_type j = 0; j < M_; ++j) {
+      for (size_type i = j; i < M_; ++i) {
+        vari_ref_A_[pos] = A.coeffRef(i, j).vi_;
+        vari_ref_L_[pos] = new vari(L_A.coeffRef(i, j), false);
+        ++pos;
+      }
+    }
+  }
+
+  /**
+   * Symbolic adjoint calculation for Cholesky factor A
+   *
+   * @param L Cholesky factor
+   * @param L_adj matrix of adjoints of L
+   */
+  inline void symbolic_rev(Block_& L, Block_& L_adj) {
+    using Eigen::Lower;
+    using Eigen::StrictlyUpper;
+    using Eigen::Upper;
+    L.transposeInPlace();
+    L_adj = (L * L_adj.triangularView<Lower>()).eval();
+    L_adj.triangularView<StrictlyUpper>()
+        = L_adj.adjoint().triangularView<StrictlyUpper>();
+    L.triangularView<Upper>().solveInPlace(L_adj);
+    L.triangularView<Upper>().solveInPlace(L_adj.transpose());
+  }
+
+  /**
+   * Reverse mode differentiation algorithm reference:
+   *
+   * Iain Murray: Differentiation of the Cholesky decomposition, 2016.
+   *
+   */
+  virtual void chain() {
+    using Eigen::Block;
+    using Eigen::Lower;
+    using Eigen::MatrixXd;
+    using Eigen::StrictlyUpper;
+    using Eigen::Upper;
+    auto L_adj = Eigen::MatrixXd::Zero(M_, M_).eval();
+    auto L = Eigen::MatrixXd::Zero(M_, M_).eval();
+    size_t pos = 0;
+    for (size_type j = 0; j < M_; ++j) {
+      for (size_type i = j; i < M_; ++i) {
+        L_adj.coeffRef(i, j) = vari_ref_L_[pos]->adj_;
+        L.coeffRef(i, j) = vari_ref_L_[pos]->val_;
+        ++pos;
+      }
+    }
+
+    for (int k = M_; k > 0; k -= block_size_) {
+      int j = std::max(0, k - block_size_);
+      Block_ R = L.block(j, 0, k - j, j);
+      Block_ D = L.block(j, j, k - j, k - j);
+      Block_ B = L.block(k, 0, M_ - k, j);
+      Block_ C = L.block(k, j, M_ - k, k - j);
+      Block_ R_adj = L_adj.block(j, 0, k - j, j);
+      Block_ D_adj = L_adj.block(j, j, k - j, k - j);
+      Block_ B_adj = L_adj.block(k, 0, M_ - k, j);
+      Block_ C_adj = L_adj.block(k, j, M_ - k, k - j);
+      if (C_adj.size() > 0) {
+        C_adj = D.transpose()
+                    .triangularView<Upper>()
+                    .solve(C_adj.transpose())
+                    .transpose();
+        B_adj.noalias() -= C_adj * R;
+        D_adj.noalias() -= C_adj.transpose() * C;
+      }
+      symbolic_rev(D, D_adj);
+      R_adj.noalias() -= C_adj.transpose() * B;
+      R_adj.noalias() -= D_adj.selfadjointView<Lower>() * R;
+      D_adj.diagonal() *= 0.5;
+      D_adj.triangularView<StrictlyUpper>().setZero();
+    }
+    pos = 0;
+    for (size_type j = 0; j < M_; ++j) {
+      for (size_type i = j; i < M_; ++i) {
+        vari_ref_A_[pos++]->adj_ += L_adj.coeffRef(i, j);
+      }
+    }
+  }
+};
+
+class cholesky_scalar : public vari {
+ public:
+  int M_;
+  vari** vari_ref_A_;
+  vari** vari_ref_L_;
+
+  /**
+   * Constructor for Cholesky function.
+   *
+   * Stores varis for A.  Instantiates and stores varis for L.  Instantiates
+   * and stores dummy vari for upper triangular part of var result returned
+   * in cholesky_decompose function call
+   *
+   * variRefL aren't on the chainable autodiff stack, only used for storage
+   * and computation. Note that varis for L are constructed externally in
+   * cholesky_decompose.
+   *
+   * @param A matrix
+   * @param L_A matrix, Cholesky factor of A
+   */
+  cholesky_scalar(const Eigen::Matrix<var, -1, -1>& A,
+                  const Eigen::Matrix<double, -1, -1>& L_A)
+      : vari(0.0),
+        M_(A.rows()),
+        vari_ref_A_(ChainableStack::instance_->memalloc_.alloc_array<vari*>(
+            A.rows() * (A.rows() + 1) / 2)),
+        vari_ref_L_(ChainableStack::instance_->memalloc_.alloc_array<vari*>(
+            A.rows() * (A.rows() + 1) / 2)) {
+    size_t accum = 0;
+    size_t accum_i = accum;
+    for (size_type j = 0; j < M_; ++j) {
+      for (size_type i = j; i < M_; ++i) {
+        accum_i += i;
+        size_t pos = j + accum_i;
+        vari_ref_A_[pos] = A.coeffRef(i, j).vi_;
+        vari_ref_L_[pos] = new vari(L_A.coeffRef(i, j), false);
+      }
+      accum += j;
+      accum_i = accum;
+    }
+  }
+
+  /**
+   * Reverse mode differentiation algorithm reference:
+   *
+   * Mike Giles. An extended collection of matrix derivative results for
+   * forward and reverse mode AD.  Jan. 2008.
+   *
+   * Note algorithm  as laid out in Giles is row-major, so Eigen::Matrices
+   * are explicitly storage order RowMajor, whereas Eigen defaults to
+   * ColumnMajor. Also note algorithm starts by calculating the adjoint for
+   * A(M_ - 1, M_ - 1), hence pos on line 94 is decremented to start at pos
+   * = M_ * (M_ + 1) / 2.
+   */
+  virtual void chain() {
+    using Eigen::Matrix;
+    using Eigen::RowMajor;
+    Matrix<double, -1, -1, RowMajor> adjL(M_, M_);
+    Matrix<double, -1, -1, RowMajor> LA(M_, M_);
+    Matrix<double, -1, -1, RowMajor> adjA(M_, M_);
+    size_t pos = 0;
+    for (size_type i = 0; i < M_; ++i) {
+      for (size_type j = 0; j <= i; ++j) {
+        adjL.coeffRef(i, j) = vari_ref_L_[pos]->adj_;
+        LA.coeffRef(i, j) = vari_ref_L_[pos]->val_;
+        ++pos;
+      }
+    }
+
+    --pos;
+    for (int i = M_ - 1; i >= 0; --i) {
+      for (int j = i; j >= 0; --j) {
+        if (i == j) {
+          adjA.coeffRef(i, j) = 0.5 * adjL.coeff(i, j) / LA.coeff(i, j);
+        } else {
+          adjA.coeffRef(i, j) = adjL.coeff(i, j) / LA.coeff(j, j);
+          adjL.coeffRef(j, j)
+              -= adjL.coeff(i, j) * LA.coeff(i, j) / LA.coeff(j, j);
+        }
+        for (int k = j - 1; k >= 0; --k) {
+          adjL.coeffRef(i, k) -= adjA.coeff(i, j) * LA.coeff(j, k);
+          adjL.coeffRef(j, k) -= adjA.coeff(i, j) * LA.coeff(i, k);
+        }
+        vari_ref_A_[pos--]->adj_ += adjA.coeffRef(i, j);
+      }
+    }
+  }
+};
+  
 #ifdef STAN_OPENCL
 class cholesky_opencl : public vari {
  public:
@@ -184,8 +416,6 @@ cholesky_decompose(const T& A) {
       cholesky_block* baseVari = new cholesky_block(A_ref, L_A);
       internal::set_lower_tri_coeff_ref(L, baseVari->vari_ref_L_);
     }
-    cholesky_block* baseVari = new cholesky_block(A_ref, L_A);
-    internal::set_lower_tri_coeff_ref(L, baseVari->vari_ref_L_);
   }
 
   return L;
