4005e1158cf38a1c72c197b0b53eec9131f2c49a
Stan Jenkins <mc.stanislaw@gmail.com>
2020-10-03 17:42:40 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.1-14 (tags/RELEASE_601/final)

--- stan/math/rev/fun/dot_self.hpp
+++ stan/math/rev/fun/dot_self.hpp
@@ -12,31 +12,31 @@
 namespace stan {
 namespace math {
 
-namespace internal {	
-class dot_self_vari : public vari {	
-protected:	
-  vari** v_;	
-  size_t size_;	
+namespace internal {
+class dot_self_vari : public vari {
+ protected:
+  vari** v_;
+  size_t size_;
 
-public:	
-  dot_self_vari(vari** v, size_t size)	
-      : vari(Eigen::Map<vector_vi>(v, size).val().squaredNorm()),	
-        v_(v),	
-        size_(size) {}	
-  template <typename T, require_eigen_t<T>* = nullptr>	
-  explicit dot_self_vari(const T& v)	
-      : vari(v.val().squaredNorm()), size_(v.size()) {	
-    v_ = reinterpret_cast<vari**>(	
-        ChainableStack::instance_->memalloc_.alloc(size_ * sizeof(vari*)));	
-    Eigen::Map<matrix_vi>(v_, v.rows(), v.cols()) = v.vi();	
-  }	
-  virtual void chain() {	
-    Eigen::Map<vector_vi> v_map(v_, size_);	
-    v_map.adj() += adj_ * 2.0 * v_map.val();	
-  }	
-};	
-}
-  
+ public:
+  dot_self_vari(vari** v, size_t size)
+      : vari(Eigen::Map<vector_vi>(v, size).val().squaredNorm()),
+        v_(v),
+        size_(size) {}
+  template <typename T, require_eigen_t<T>* = nullptr>
+  explicit dot_self_vari(const T& v)
+      : vari(v.val().squaredNorm()), size_(v.size()) {
+    v_ = reinterpret_cast<vari**>(
+        ChainableStack::instance_->memalloc_.alloc(size_ * sizeof(vari*)));
+    Eigen::Map<matrix_vi>(v_, v.rows(), v.cols()) = v.vi();
+  }
+  virtual void chain() {
+    Eigen::Map<vector_vi> v_map(v_, size_);
+    v_map.adj() += adj_ * 2.0 * v_map.val();
+  }
+};
+}  // namespace internal
+
 /**
  * Returns the dot product of a vector of var with itself.
  *
@@ -64,9 +64,9 @@ inline var dot_self(const T& v) {
     dot_self/1024/manual_time       2548 ns        19862 ns       275469
   */
 
-  //const Eigen::Ref<const plain_type_t<T>>& v_ref = v;
-  //return {new internal::dot_self_vari(v_ref)};
-  
+  // const Eigen::Ref<const plain_type_t<T>>& v_ref = v;
+  // return {new internal::dot_self_vari(v_ref)};
+
   /*
     --------------------------------------------------------------------
     Benchmark                          Time             CPU   Iterations
@@ -83,19 +83,18 @@ inline var dot_self(const T& v) {
     dot_self/512/manual_time        1277 ns         8948 ns       544405
     dot_self/1024/manual_time       2648 ns        21018 ns       267065
   */
-  
+
   arena_t<T> arena_v = v;
 
   var res = arena_v.val().squaredNorm();
 
   reverse_pass_callback([arena_v, res]() {
-    for(size_t i = 0; i < arena_v.size(); ++i) {
+    for (size_t i = 0; i < arena_v.size(); ++i) {
       arena_v.coeffRef(i).adj() += 2.0 * res.adj() * arena_v.coeff(i).val();
     }
   });
 
   return res;
-  
 
   /*
     --------------------------------------------------------------------
@@ -113,17 +112,17 @@ inline var dot_self(const T& v) {
     dot_self/512/manual_time         992 ns         9303 ns       703375
     dot_self/1024/manual_time       2270 ns        20923 ns       308428
   */
-  
+
   /*
   arena_t<T> arena_v = v;
 
   auto arena_v_val = to_arena(value_of(arena_v));
   var res = arena_v_val.squaredNorm();
-  
+
   reverse_pass_callback([arena_v, res, arena_v_val]() mutable {
   arena_v.adj() += (2.0 * res.adj()) * arena_v_val;
   });
-  
+
   return res;
   */
 }
