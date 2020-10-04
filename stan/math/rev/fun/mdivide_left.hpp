42d7ccb842a48ec18b39c7d51d3df8093a2c3396
Ben <bbbales2@gmail.com>
2020-10-04 13:39:49 -0400

    Updated mdivide_left (Issue #2018)

--- stan/math/rev/fun/mdivide_left.hpp
+++ stan/math/rev/fun/mdivide_left.hpp
@@ -14,22 +14,21 @@ namespace math {
 
 template <typename T1, typename T2, require_all_eigen_t<T1, T2>* = nullptr,
           require_any_vt_var<T1, T2>* = nullptr>
-inline Eigen::Matrix<var, T1::RowsAtCompileTime, T2::ColsAtCompileTime>
-mdivide_left(const T1& A, const T2& B) {
+inline auto mdivide_left(const T1& A, const T2& B) {
+  using ret_type = plain_type_t<decltype(A * B)>;
+  
   check_square("mdivide_left", "A", A);
   check_multiplicable("mdivide_left", "A", A, "B", B);
+
   if (A.size() == 0) {
-    return {0, B.cols()};
+    return ret_type(0, B.cols());
   }
 
-  using A_ref_t = ref_type_t<T1>;
-  using B_ref_t = ref_type_t<T2>;
+  const auto& A_ref = to_ref(A);
+  const auto& B_ref = to_ref(B);
 
-  A_ref_t A_ref = A;
-  B_ref_t B_ref = B;
-
-  arena_matrix<promote_scalar_t<var, T1>> arena_A;
-  arena_matrix<promote_scalar_t<var, T2>> arena_B;
+  arena_t<promote_scalar_t<var, T1>> arena_A;
+  arena_t<promote_scalar_t<var, T2>> arena_B;
 
   if (!is_constant<T1>::value) {
     arena_A = A_ref;
@@ -39,23 +38,22 @@ mdivide_left(const T1& A, const T2& B) {
     arena_B = B_ref;
   }
 
-  arena_matrix<Eigen::MatrixXd> arena_A_val = value_of(A_ref);
-  arena_matrix<Eigen::Matrix<var, T1::RowsAtCompileTime, T2::ColsAtCompileTime>>
-      res = arena_A_val.colPivHouseholderQr().solve(value_of(B_ref));
+  auto arena_A_val = to_arena(value_of(A_ref));
+  arena_t<ret_type>
+      res = arena_A_val.householderQr().solve(value_of(B_ref));
 
   reverse_pass_callback([arena_A, arena_B, arena_A_val, res]() mutable {
-    Eigen::Matrix<double, T1::RowsAtCompileTime, T2::ColsAtCompileTime> adjB
-        = arena_A_val.transpose().colPivHouseholderQr().solve(res.adj());
+    promote_scalar_t<double, T2> adjB
+      = arena_A_val.transpose().householderQr().solve(res.adj());
 
     if (!is_constant<T1>::value)
-      forward_as<promote_scalar_t<var, T1>>(arena_A).adj()
-          += -adjB * res.val().transpose().eval();
+      arena_A.adj() += -adjB * res.val().transpose().eval();
 
     if (!is_constant<T2>::value)
-      forward_as<promote_scalar_t<var, T2>>(arena_B).adj() += adjB;
+      arena_B.adj() += adjB;
   });
 
-  return res;
+  return ret_type(res);
 }
 
 }  // namespace math
