079462c0a952e1cc18fa4736aee41f61ca750cdf
Stan Jenkins <mc.stanislaw@gmail.com>
2020-10-04 15:04:09 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/fun/columns_dot_product.hpp
+++ stan/math/rev/fun/columns_dot_product.hpp
@@ -28,18 +28,18 @@ inline auto columns_dot_product(const Mat1& v1, const Mat2& v2) {
   auto arena_v1 = to_arena(v1);
   auto arena_v2 = to_arena(v2);
 
-  arena_t<ret_type> res =
-    (arena_v1.val().array() * arena_v2.val().array()).colwise().sum();
+  arena_t<ret_type> res
+      = (arena_v1.val().array() * arena_v2.val().array()).colwise().sum();
 
   reverse_pass_callback([res, arena_v1, arena_v2]() mutable {
-    for(size_t j = 0; j < arena_v1.cols(); ++j) {
-      for(size_t i = 0; i < arena_v1.rows(); ++i) {
-	if(!is_constant<Mat1>::value)
-	  forward_as<var>(arena_v1.coeffRef(i, j)).adj() +=
-	    value_of(arena_v2.coeff(i, j)) * res.coeff(j).adj();
-	if(!is_constant<Mat2>::value)
-	  forward_as<var>(arena_v2.coeffRef(i, j)).adj() +=
-	    value_of(arena_v1.coeff(i, j)) * res.coeff(j).adj();
+    for (size_t j = 0; j < arena_v1.cols(); ++j) {
+      for (size_t i = 0; i < arena_v1.rows(); ++i) {
+        if (!is_constant<Mat1>::value)
+          forward_as<var>(arena_v1.coeffRef(i, j)).adj()
+              += value_of(arena_v2.coeff(i, j)) * res.coeff(j).adj();
+        if (!is_constant<Mat2>::value)
+          forward_as<var>(arena_v2.coeffRef(i, j)).adj()
+              += value_of(arena_v1.coeff(i, j)) * res.coeff(j).adj();
       }
     }
   });
