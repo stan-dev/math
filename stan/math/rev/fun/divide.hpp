821561aafd5ddf8e912859fc06874ca27c384675
Ben <bbbales2@gmail.com>
2020-10-05 10:02:12 -0400

    Updated divide (Issue #2018)

--- stan/math/rev/fun/divide.hpp
+++ stan/math/rev/fun/divide.hpp
@@ -23,30 +23,22 @@ namespace math {
 template <typename Mat, typename Scal, require_eigen_t<Mat>* = nullptr,
           require_stan_scalar_t<Scal>* = nullptr,
           require_any_st_var<Mat, Scal>* = nullptr>
-inline Eigen::Matrix<var, Mat::RowsAtCompileTime, Mat::ColsAtCompileTime>
-divide(const Mat& m, const Scal& c) {
+inline auto divide(const Mat& m, const Scal& c) {
+  using ret_type = promote_scalar_t<var, Mat>;
+  
   const auto& m_ref = to_ref(m);
-  const auto& c_ref = to_ref(c);
 
   auto arena_m = to_arena_if<!is_constant<Mat>::value>(m_ref);
 
-  double invc = 1.0 / value_of(c_ref);
+  double invc = 1.0 / value_of(c);
 
-  using Mat_d = promote_scalar_t<double, Mat>;
-  using Mat_v = promote_scalar_t<var, Mat>;
-
-  arena_t<Mat_d> res_val = invc * value_of(m_ref);
-  arena_t<Mat_v> res = res_val;
-
-  reverse_pass_callback([arena_m, c, res, res_val, invc]() mutable {
-    const auto& adj = to_ref(res.adj());
-
-    if (!is_constant<Mat>::value) {
-      forward_as<Mat_v>(arena_m).adj() += invc * adj;
-    }
-    if (!is_constant<Scal>::value) {
-      forward_as<var>(c).adj() += -invc * (adj.array() * res_val.array()).sum();
-    }
+  arena_t<ret_type> res = invc * value_of(m_ref);
+  reverse_pass_callback([arena_m, c, res, invc]() mutable {
+    if(!is_constant<Mat>::value)
+      forward_as<ret_type>(arena_m).adj() += invc * res.adj();
+    if(!is_constant<Scal>::value)
+      forward_as<var>(c).adj() -=
+	invc * (res.adj().array() * res.val().array()).sum();
   });
 
   return res;
