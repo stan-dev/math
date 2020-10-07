25db65e6cc044361da1e4a6512ec858a7412a994
tadej <tadej.c@gmail.com>
2020-10-07 11:58:22 +0200

    added includes

--- stan/math/prim/prob/uniform_cdf.hpp
+++ stan/math/prim/prob/uniform_cdf.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun/max_size.hpp>
+#include <stan/math/prim/fun/promote_scalar.hpp>
 #include <stan/math/prim/fun/size.hpp>
 #include <stan/math/prim/fun/size_zero.hpp>
 #include <stan/math/prim/fun/to_ref.hpp>
