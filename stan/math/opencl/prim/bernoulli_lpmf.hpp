324458aa5a14d0726e6468809ec0eadf4796aff6
Stan Jenkins <mc.stanislaw@gmail.com>
2020-10-21 08:40:09 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/opencl/prim/bernoulli_lpmf.hpp
+++ stan/math/opencl/prim/bernoulli_lpmf.hpp
@@ -25,9 +25,11 @@ namespace math {
  * @throw std::domain_error if theta is not a valid probability
  * @throw std::invalid_argument if container sizes mismatch.
  */
-template <bool propto, typename T_n_cl, typename T_prob_cl,
-          require_all_prim_or_rev_kernel_expression_t<T_n_cl, T_prob_cl>* = nullptr>
-return_type_t<T_prob_cl> bernoulli_lpmf(const T_n_cl& n, const T_prob_cl& theta) {
+template <
+    bool propto, typename T_n_cl, typename T_prob_cl,
+    require_all_prim_or_rev_kernel_expression_t<T_n_cl, T_prob_cl>* = nullptr>
+return_type_t<T_prob_cl> bernoulli_lpmf(const T_n_cl& n,
+                                        const T_prob_cl& theta) {
   static const char* function = "bernoulli_lpmf(OpenCL)";
   using T_partials_return = partials_return_t<T_prob_cl>;
   constexpr bool is_n_vector = !is_stan_scalar<T_n_cl>::value;
