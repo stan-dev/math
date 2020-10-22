0de2c7b1a971c2861b897ea877c7e651d4096c56
Steve Bronder <stevo15025@gmail.com>
2020-10-22 13:51:02 -0400

    add vari class for opencl multiply but getting size error

--- stan/math/opencl/rev/multiply.hpp
+++ stan/math/opencl/rev/multiply.hpp
@@ -13,6 +13,47 @@
 namespace stan {
 namespace math {
 
+namespace internal {
+  template <typename T>
+  struct matrix_cl_alloc : public chainable_alloc {
+    T mat_;
+    matrix_cl_alloc(const T& x) : mat_(x) {}
+  };
+  template <typename T1, typename T2, typename T3, typename = void, typename = void>
+  struct multiply_cl_vari;
+
+  template <typename T1, typename T2, typename T3>
+  struct multiply_cl_vari<T1, T2, T3, require_var_t<T1>, require_not_var_t<T2>> : public vari {
+    T1 a_;
+    matrix_cl_alloc<T2>* b_;
+    T3 res_;
+    multiply_cl_vari(const T1& a, const T2& b, const T3& c) : vari(0),
+      a_(a), b_(new matrix_cl_alloc<T2>(b)), res_(c) {}
+
+    void chain() {
+      puts("Got 1");
+      a_.adj() = a_.adj() + res_.adj() * transpose(b_->mat_);
+    }
+
+  };
+
+  template <typename T1, typename T2, typename T3>
+  struct multiply_cl_vari<T1, T2, T3, require_not_var_t<T1>, require_var_t<T2>> : public vari {
+    matrix_cl_alloc<T1>* a_;
+    T2 b_;
+    T3 res_;
+    multiply_cl_vari(const T1& a, const T2& b, const T3& c) : vari(0),
+      a_(new matrix_cl_alloc<std::decay_t<T1>>(a)), b_(b), res_(c) {}
+
+    void chain() {
+      puts("Got 2");
+      b_.adj() = b_.adj() + transpose(a_->mat_) * res_.adj();
+    }
+
+  };
+
+}
+
 /**
  * Matrix multiplication of two reverse mode matrices and/or kernel generator
  * expressions.
@@ -25,25 +66,44 @@ namespace math {
 template <
     typename T_a, typename T_b,
     require_all_nonscalar_prim_or_rev_kernel_expression_t<T_a, T_b>* = nullptr,
-    require_any_var_t<T_a, T_b>* = nullptr>
+    require_all_var_t<T_a, T_b>* = nullptr>
 inline auto multiply(const T_a& a, const T_b& b) {
   check_size_match("multiply ((OpenCL))", "A.cols()", a.cols(), "B.rows()",
                    b.rows());
-  const arena_t<T_a>& a_arena = a;
-  const arena_t<T_b>& b_arena = b;
 
-  var_value<matrix_cl<double>> res = value_of(a_arena) * value_of(b_arena);
+  var_value<matrix_cl<double>> res = value_of(b) * value_of(a);
 
-  reverse_pass_callback([a_arena, b_arena, res]() mutable {
-    if (!is_constant<T_a>::value) {
-      auto& a_adj = forward_as<var_value<matrix_cl<double>>>(a_arena).adj();
-      a_adj = a_adj + res.adj() * transpose(value_of(b_arena));
-    }
-    if (!is_constant<T_b>::value) {
-      auto& b_adj = forward_as<var_value<matrix_cl<double>>>(b_arena).adj();
-      b_adj = b_adj + transpose(value_of(a_arena)) * res.adj();
-    }
+  reverse_pass_callback([a, b, res]() mutable {
+      a.adj() = a.adj() + res.adj() * transpose(b.val());
+      b.adj() = b.adj() + transpose(a.val()) * res.adj();
   });
+  return res;
+}
+
+template <
+    typename T_a, typename T_b,
+    require_all_nonscalar_prim_or_rev_kernel_expression_t<T_a, T_b>* = nullptr,
+    require_not_var_t<T_a>* = nullptr, require_var_t<T_b>* = nullptr>
+inline auto multiply(const T_a& a, const T_b& b) {
+  check_size_match("multiply ((OpenCL))", "A.cols()", a.cols(), "B.rows()",
+                   b.rows());
+
+  var_value<matrix_cl<double>> res = a * b.val();
+  auto* do_mult = new internal::multiply_cl_vari<plain_type_t<T_a>,
+   plain_type_t<T_b>, var_value<matrix_cl<double>>>(a, b, res);
+  return res;
+}
+
+template <
+    typename T_a, typename T_b,
+    require_all_nonscalar_prim_or_rev_kernel_expression_t<T_a, T_b>* = nullptr,
+    require_var_t<T_a>* = nullptr, require_not_var_t<T_b>* = nullptr>
+inline auto multiply(const T_a& a, const T_b& b) {
+  check_size_match("multiply ((OpenCL))", "A.cols()", a.cols(), "B.rows()",
+                   b.rows());
+  var_value<matrix_cl<double>> res = a.val() * b;
+  auto* do_mult = new internal::multiply_cl_vari<plain_type_t<T_a>,
+  plain_type_t<T_b>, var_value<matrix_cl<double>>>(a, b, res);
   return res;
 }
 
