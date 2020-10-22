e9d74010807cdc5a33c355b1f1888b979f9a498d
Stan Jenkins <mc.stanislaw@gmail.com>
2020-10-22 13:58:43 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.1-14 (tags/RELEASE_601/final)

--- stan/math/opencl/rev/multiply.hpp
+++ stan/math/opencl/rev/multiply.hpp
@@ -14,45 +14,46 @@ namespace stan {
 namespace math {
 
 namespace internal {
-  template <typename T>
-  struct matrix_cl_alloc : public chainable_alloc {
-    T mat_;
-    matrix_cl_alloc(const T& x) : mat_(x) {}
-  };
-  template <typename T1, typename T2, typename T3, typename = void, typename = void>
-  struct multiply_cl_vari;
+template <typename T>
+struct matrix_cl_alloc : public chainable_alloc {
+  T mat_;
+  matrix_cl_alloc(const T& x) : mat_(x) {}
+};
+template <typename T1, typename T2, typename T3, typename = void,
+          typename = void>
+struct multiply_cl_vari;
 
-  template <typename T1, typename T2, typename T3>
-  struct multiply_cl_vari<T1, T2, T3, require_var_t<T1>, require_not_var_t<T2>> : public vari {
-    T1 a_;
-    matrix_cl_alloc<T2>* b_;
-    T3 res_;
-    multiply_cl_vari(const T1& a, const T2& b, const T3& c) : vari(0),
-      a_(a), b_(new matrix_cl_alloc<T2>(b)), res_(c) {}
+template <typename T1, typename T2, typename T3>
+struct multiply_cl_vari<T1, T2, T3, require_var_t<T1>, require_not_var_t<T2>>
+    : public vari {
+  T1 a_;
+  matrix_cl_alloc<T2>* b_;
+  T3 res_;
+  multiply_cl_vari(const T1& a, const T2& b, const T3& c)
+      : vari(0), a_(a), b_(new matrix_cl_alloc<T2>(b)), res_(c) {}
 
-    void chain() {
-      puts("Got 1");
-      a_.adj() = a_.adj() + res_.adj() * transpose(b_->mat_);
-    }
+  void chain() {
+    puts("Got 1");
+    a_.adj() = a_.adj() + res_.adj() * transpose(b_->mat_);
+  }
+};
 
-  };
+template <typename T1, typename T2, typename T3>
+struct multiply_cl_vari<T1, T2, T3, require_not_var_t<T1>, require_var_t<T2>>
+    : public vari {
+  matrix_cl_alloc<T1>* a_;
+  T2 b_;
+  T3 res_;
+  multiply_cl_vari(const T1& a, const T2& b, const T3& c)
+      : vari(0), a_(new matrix_cl_alloc<std::decay_t<T1>>(a)), b_(b), res_(c) {}
 
-  template <typename T1, typename T2, typename T3>
-  struct multiply_cl_vari<T1, T2, T3, require_not_var_t<T1>, require_var_t<T2>> : public vari {
-    matrix_cl_alloc<T1>* a_;
-    T2 b_;
-    T3 res_;
-    multiply_cl_vari(const T1& a, const T2& b, const T3& c) : vari(0),
-      a_(new matrix_cl_alloc<std::decay_t<T1>>(a)), b_(b), res_(c) {}
+  void chain() {
+    puts("Got 2");
+    b_.adj() = b_.adj() + transpose(a_->mat_) * res_.adj();
+  }
+};
 
-    void chain() {
-      puts("Got 2");
-      b_.adj() = b_.adj() + transpose(a_->mat_) * res_.adj();
-    }
-
-  };
-
-}
+}  // namespace internal
 
 /**
  * Matrix multiplication of two reverse mode matrices and/or kernel generator
@@ -74,8 +75,8 @@ inline auto multiply(const T_a& a, const T_b& b) {
   var_value<matrix_cl<double>> res = value_of(b) * value_of(a);
 
   reverse_pass_callback([a, b, res]() mutable {
-      a.adj() = a.adj() + res.adj() * transpose(b.val());
-      b.adj() = b.adj() + transpose(a.val()) * res.adj();
+    a.adj() = a.adj() + res.adj() * transpose(b.val());
+    b.adj() = b.adj() + transpose(a.val()) * res.adj();
   });
   return res;
 }
@@ -89,8 +90,9 @@ inline auto multiply(const T_a& a, const T_b& b) {
                    b.rows());
 
   var_value<matrix_cl<double>> res = a * b.val();
-  auto* do_mult = new internal::multiply_cl_vari<plain_type_t<T_a>,
-   plain_type_t<T_b>, var_value<matrix_cl<double>>>(a, b, res);
+  auto* do_mult
+      = new internal::multiply_cl_vari<plain_type_t<T_a>, plain_type_t<T_b>,
+                                       var_value<matrix_cl<double>>>(a, b, res);
   return res;
 }
 
@@ -102,8 +104,9 @@ inline auto multiply(const T_a& a, const T_b& b) {
   check_size_match("multiply ((OpenCL))", "A.cols()", a.cols(), "B.rows()",
                    b.rows());
   var_value<matrix_cl<double>> res = a.val() * b;
-  auto* do_mult = new internal::multiply_cl_vari<plain_type_t<T_a>,
-  plain_type_t<T_b>, var_value<matrix_cl<double>>>(a, b, res);
+  auto* do_mult
+      = new internal::multiply_cl_vari<plain_type_t<T_a>, plain_type_t<T_b>,
+                                       var_value<matrix_cl<double>>>(a, b, res);
   return res;
 }
 
