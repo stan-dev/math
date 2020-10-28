c84c860429f61c27081b855c0a553b8f2741f9bc
Sebastian Weber <sdw.post@waebers.de>
2020-10-28 19:23:04 +0100

    revert tests

--- test/unit/math/rev/functor/reduce_sum_test.cpp
+++ test/unit/math/rev/functor/reduce_sum_test.cpp
@@ -8,9 +8,9 @@
 #include <set>
 
 TEST(StanMathRev_reduce_sum, no_args) {
-  using stan::math::var;
   using stan::math::test::get_new_msg;
   using stan::math::test::sum_lpdf;
+  using stan::math::var;
   std::vector<var> data(0);
   EXPECT_EQ(0.0, stan::math::reduce_sum_static<sum_lpdf>(
                      data, 1, stan::math::test::get_new_msg())
@@ -52,9 +52,9 @@ TEST(StanMathRev_reduce_sum, value) {
 }
 
 TEST(StanMathRev_reduce_sum, gradient) {
-  using stan::math::var;
   using stan::math::test::count_lpdf;
   using stan::math::test::get_new_msg;
+  using stan::math::var;
 
   double lambda_d = 10.0;
   const std::size_t elems = 10000;
@@ -89,10 +89,10 @@ TEST(StanMathRev_reduce_sum, gradient) {
       << "value of poisson lpdf : " << poisson_lpdf.val() << std::endl
       << "gradient wrt to lambda: " << lambda_adj << std::endl;
 
-  stan::math::set_zero_all_adjoints();
   var poisson_lpdf_static = stan::math::reduce_sum_static<count_lpdf<var>>(
       data, 5, get_new_msg(), vlambda_v, idata);
 
+  stan::math::set_zero_all_adjoints();
   stan::math::grad(poisson_lpdf_static.vi_);
   const double lambda_adj_static = lambda_v.adj();
   EXPECT_FLOAT_EQ(lambda_adj_static, lambda_ref_adj);
@@ -100,9 +100,9 @@ TEST(StanMathRev_reduce_sum, gradient) {
 }
 
 TEST(StanMathRev_reduce_sum, grainsize) {
-  using stan::math::var;
   using stan::math::test::count_lpdf;
   using stan::math::test::get_new_msg;
+  using stan::math::var;
 
   double lambda_d = 10.0;
   const std::size_t elems = 10000;
@@ -198,9 +198,9 @@ TEST(StanMathRev_reduce_sum, nesting_gradient) {
 }
 */
 TEST(StanMathRev_reduce_sum, grouped_gradient) {
-  using stan::math::var;
   using stan::math::test::get_new_msg;
   using stan::math::test::grouped_count_lpdf;
+  using stan::math::var;
 
   double lambda_d = 10.0;
   const std::size_t groups = 10;
@@ -247,11 +247,11 @@ TEST(StanMathRev_reduce_sum, grouped_gradient) {
       << "value of poisson lpdf : " << poisson_lpdf.val() << std::endl
       << "gradient wrt to lambda: " << lambda_adj << std::endl;
 
-  stan::math::set_zero_all_adjoints();
   var poisson_lpdf_static
       = stan::math::reduce_sum_static<grouped_count_lpdf<var>>(
           data, 5, get_new_msg(), vlambda_v, gidx);
 
+  stan::math::set_zero_all_adjoints();
   stan::math::grad(poisson_lpdf_static.vi_);
   const double lambda_adj_static = lambda_v.adj();
   EXPECT_FLOAT_EQ(lambda_adj_static, lambda_ref_adj);
@@ -259,9 +259,9 @@ TEST(StanMathRev_reduce_sum, grouped_gradient) {
 }
 
 TEST(StanMathRev_reduce_sum, grouped_gradient_eigen) {
-  using stan::math::var;
   using stan::math::test::get_new_msg;
   using stan::math::test::grouped_count_lpdf;
+  using stan::math::var;
 
   double lambda_d = 10.0;
   const std::size_t groups = 10;
@@ -308,11 +308,11 @@ TEST(StanMathRev_reduce_sum, grouped_gradient_eigen) {
       << "value of poisson lpdf : " << poisson_lpdf.val() << std::endl
       << "gradient wrt to lambda: " << lambda_adj << std::endl;
 
-  stan::math::set_zero_all_adjoints();
   var poisson_lpdf_static
       = stan::math::reduce_sum_static<grouped_count_lpdf<var>>(
           data, 5, get_new_msg(), vlambda_v, gidx);
 
+  stan::math::set_zero_all_adjoints();
   stan::math::grad(poisson_lpdf_static.vi_);
   const double lambda_adj_static = lambda_v.adj();
 
@@ -322,9 +322,9 @@ TEST(StanMathRev_reduce_sum, grouped_gradient_eigen) {
 }
 
 TEST(StanMathRev_reduce_sum, slice_group_gradient) {
-  using stan::math::var;
   using stan::math::test::get_new_msg;
   using stan::math::test::slice_group_count_lpdf;
+  using stan::math::var;
 
   double lambda_d = 10.0;
   const std::size_t groups = 10;
@@ -377,11 +377,11 @@ TEST(StanMathRev_reduce_sum, slice_group_gradient) {
       << "value of poisson lpdf : " << poisson_lpdf.val() << std::endl
       << "gradient wrt to lambda: " << lambda_adj << std::endl;
 
-  stan::math::set_zero_all_adjoints();
   var poisson_lpdf_static
       = stan::math::reduce_sum_static<slice_group_count_lpdf<var>>(
           vlambda_v, 5, get_new_msg(), data, gsidx);
 
+  stan::math::set_zero_all_adjoints();
   stan::math::grad(poisson_lpdf_static.vi_);
   const double lambda_adj_static = lambda_v.adj();
 
