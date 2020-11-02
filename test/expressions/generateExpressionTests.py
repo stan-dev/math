d8749bdcabd03fd1c28eec9915c89ab59138fdb8
tadej <tadej.c@gmail.com>
2020-11-02 08:44:26 +0100

    fix merge

--- test/expressions/generateExpressionTests.py
+++ test/expressions/generateExpressionTests.py
@@ -186,9 +186,9 @@ special_arg_values = {
     "pareto_type_2_cdf": [1.5, 0.7, None, None],
     "pareto_type_2_cdf_log": [1.5, 0.7, None, None],
     "pareto_type_2_lcdf": [1.5, 0.7, None, None],
-    "unit_vector_free" : [1.0],}
+    "unit_vector_free" : [1.0],
     "wiener_log" : [0.8, None, 0.4, None, None],
-    "wiener_lpdf" : [0.8, None, 0.4, None, None],
+    "wiener_lpdf" : [0.8, None, 0.4, None, None],}
 
 def make_arg_code(arg, scalar, var_name, var_number, function_name):
     """
