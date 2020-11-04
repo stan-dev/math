6d148b0089e5993f922897fc929918fe6a7068c2
tadej <tadej.c@gmail.com>
2020-11-04 09:15:45 +0100

    added missing includes

--- stan/math/opencl/prim/double_exponential_lpdf.hpp
+++ stan/math/opencl/prim/double_exponential_lpdf.hpp
@@ -4,6 +4,7 @@
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/err.hpp>
+#include <stan/math/prim/fun/constants.hpp>
 #include <stan/math/prim/fun/elt_divide.hpp>
 #include <stan/math/opencl/kernel_generator.hpp>
 #include <stan/math/opencl/prim/sign.hpp>
