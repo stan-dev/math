92dd2f306e065f058884f7ceccfbabaadc982488
Steve Bronder <stevo15025@gmail.com>
2020-11-16 01:50:53 -0500

    fix headers for var subtract

--- stan/math/rev/core/operator_subtraction.hpp
+++ stan/math/rev/core/operator_subtraction.hpp
@@ -1,57 +1,15 @@
 #ifndef STAN_MATH_REV_CORE_OPERATOR_SUBTRACTION_HPP
 #define STAN_MATH_REV_CORE_OPERATOR_SUBTRACTION_HPP
 
-#include <stan/math/prim/meta.hpp>
+#include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/core/var.hpp>
-#include <stan/math/rev/core/vv_vari.hpp>
-#include <stan/math/rev/core/vd_vari.hpp>
-#include <stan/math/rev/core/dv_vari.hpp>
+#include <stan/math/rev/core/arena_matrix.hpp>
+#include <stan/math/rev/core/callback_vari.hpp>
 #include <stan/math/prim/fun/constants.hpp>
 #include <stan/math/prim/fun/is_any_nan.hpp>
 
 namespace stan {
 namespace math {
-
-namespace internal {
-class subtract_vv_vari final : public op_vv_vari {
- public:
-  subtract_vv_vari(vari* avi, vari* bvi)
-      : op_vv_vari(avi->val_ - bvi->val_, avi, bvi) {}
-  void chain() {
-    if (unlikely(is_any_nan(avi_->val_, bvi_->val_))) {
-      avi_->adj_ = NOT_A_NUMBER;
-      bvi_->adj_ = NOT_A_NUMBER;
-    } else {
-      avi_->adj_ += adj_;
-      bvi_->adj_ -= adj_;
-    }
-  }
-};
-
-class subtract_vd_vari final : public op_vd_vari {
- public:
-  subtract_vd_vari(vari* avi, double b) : op_vd_vari(avi->val_ - b, avi, b) {}
-  void chain() {
-    if (unlikely(is_any_nan(avi_->val_, bd_))) {
-      avi_->adj_ = NOT_A_NUMBER;
-    } else {
-      avi_->adj_ += adj_;
-    }
-  }
-};
-
-class subtract_dv_vari final : public op_dv_vari {
- public:
-  subtract_dv_vari(double a, vari* bvi) : op_dv_vari(a - bvi->val_, a, bvi) {}
-  void chain() {
-    if (unlikely(is_any_nan(ad_, bvi_->val_))) {
-      bvi_->adj_ = NOT_A_NUMBER;
-    } else {
-      bvi_->adj_ -= adj_;
-    }
-  }
-};
-}  // namespace internal
 
 /**
  * Subtraction operator for variables (C++).
