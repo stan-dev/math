542e6113483bfe226be7f1ed7231c6d86f85222d
Stan Jenkins <mc.stanislaw@gmail.com>
2020-11-17 12:41:59 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/opencl/rev/exp_mod_normal_lpdf_test.cpp
+++ test/unit/math/opencl/rev/exp_mod_normal_lpdf_test.cpp
@@ -197,9 +197,11 @@ TEST(ProbDistributionsExpModNormal, opencl_matches_cpu_big) {
   Eigen::Matrix<double, Eigen::Dynamic, 1> mu
       = Eigen::Array<double, Eigen::Dynamic, 1>::Random(N, 1).abs();
   Eigen::Matrix<double, Eigen::Dynamic, 1> sigma
-      = Eigen::Array<double, Eigen::Dynamic, 1>::Random(N, 1).abs().array() + 1e-9;
+      = Eigen::Array<double, Eigen::Dynamic, 1>::Random(N, 1).abs().array()
+        + 1e-9;
   Eigen::Matrix<double, Eigen::Dynamic, 1> lambda
-      = Eigen::Array<double, Eigen::Dynamic, 1>::Random(N, 1).abs().array() + 1e-9;
+      = Eigen::Array<double, Eigen::Dynamic, 1>::Random(N, 1).abs().array()
+        + 1e-9;
 
   stan::math::test::compare_cpu_opencl_prim_rev(exp_mod_normal_lpdf_functor, y,
                                                 mu, sigma, lambda);
