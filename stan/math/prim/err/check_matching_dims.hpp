6e938cd722a1ead97eff013eef6b43abb193f197
Stan Jenkins <mc.stanislaw@gmail.com>
2020-11-30 17:57:58 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/err/check_matching_dims.hpp
+++ stan/math/prim/err/check_matching_dims.hpp
@@ -73,7 +73,8 @@ inline void check_matching_dims(const char* function, const char* name1,
  * @throw <code>std::invalid_argument</code> if the dimensions of the
  *    containers do not match
  */
-template <typename T1, typename T2, require_all_matrix_t<T1, T2>* = nullptr, require_all_not_std_vector_t<T1,T2>* = nullptr>
+template <typename T1, typename T2, require_all_matrix_t<T1, T2>* = nullptr,
+          require_all_not_std_vector_t<T1, T2>* = nullptr>
 inline void check_matching_dims(const char* function, const char* name1,
                                 const T1& y1, const char* name2, const T2& y2) {
   if (y1.rows() != y2.rows() || y1.cols() != y2.cols()) {
