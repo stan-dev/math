172f7e9beb9d7195de2b9d3868f8b9e3cc5d4f6a
spinkney <sean.pinkney@gmail.com>
2020-12-01 15:54:13 -0500

    finished genealized_inverse test

---
+++ test/unit/math/prim/fun/generalized_inverse_test.cpp
@@ -0,0 +1,20 @@
+#include <stan/math/prim.hpp>
+#include <gtest/gtest.h>
+
+TEST(MathMatrixPrim, generalized_inverse) {
+  stan::math::matrix_d m0(0, 0);
+  stan::math::matrix_d ginv = stan::math::generalized_inverse(m0);
+  EXPECT_EQ(0, ginv.rows());
+  EXPECT_EQ(0, ginv.cols());
+}
+
+TEST(MathMatrixPrim, inverse_correct) {
+  using stan::math::generalized_inverse;
+
+  stan::math::matrix_d m1(3, 2);
+  m1 << 1, 2, 2, 4, 1, 2;
+
+  stan::math::matrix_d m2(2, 3);
+  m2 << 1/30, 1/15, 1/30, 1/15, 2/15, 1/15;
+  EXPECT_MATRIX_FLOAT_EQ(generalized_inverse(m1), m2);
+}
