16f4da9f56d6862f763a1b1d066e71dee774efae
Sebastian Weber <sdw.post@waebers.de>
2020-12-02 18:33:32 +0100

    cpplint

--- test/unit/math/rev/core/scoped_chainablestack_test.cpp
+++ test/unit/math/rev/core/scoped_chainablestack_test.cpp
@@ -52,7 +52,7 @@ TEST_F(AgradLocalScoped, scoped_chainablestack_functor) {
     double a_val_;
     double cgrad_a_;
     
-    scoped_functor(double a_val) : a_val_(a_val), cgrad_a_(0.0) {}
+    explicit scoped_functor(double a_val) : a_val_(a_val), cgrad_a_(0.0) {}
     void operator()() {
       stan::math::start_nested();
       var a = a_val_;
