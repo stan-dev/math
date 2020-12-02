2192b906ec5001d3e3167fd47d97c1969416b784
Sean Pinkney <sean.pinkney@gmail.com>
2020-12-02 08:28:59 -0500

    Update test/unit/math/prim/fun/generalized_inverse_test.cpp
    Co-authored-by: Tadej Ciglarič <tadej.c@gmail.com>

--- test/unit/math/prim/fun/generalized_inverse_test.cpp
+++ test/unit/math/prim/fun/generalized_inverse_test.cpp
@@ -18,7 +18,5 @@ TEST(MathMatrixPrim, Equal) {
   stan::math::matrix_d m2(2, 3);
   m2 << 1 / 30.0, 1 / 15.0, 1 / 30.0, 1 / 15.0, 2 / 15.0, 1 / 15.0;
   stan::math::matrix_d m3 = generalized_inverse(m1);
-  for (int j = 0; j < m3.cols(); j++)
-    for (int i = 0; i < m3.rows(); i++)
-      EXPECT_NEAR(m2(i, j), m3(i, j), 1e-5);
+  EXPECT_MATRIX_NEAR(m2, m3, 1e-5);
 }
