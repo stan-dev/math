718940c970b26b7218d4bba8e27ff0e70e803de5
Ben <bbbales2@gmail.com>
2020-12-03 09:51:44 -0500

    Added `var<mat>` implementation of `mdivide_left` and added some stuff to avoid computing qr decomposition twice (Issue #2101)

--- stan/math/rev/fun/mdivide_left.hpp
+++ stan/math/rev/fun/mdivide_left.hpp
@@ -13,25 +13,46 @@
 namespace stan {
 namespace math {
 
-template <typename T1, typename T2, require_all_eigen_t<T1, T2>* = nullptr,
-          require_any_vt_var<T1, T2>* = nullptr>
+template <typename T>
+class chainable_object : public chainable_alloc {
+private:
+  T obj_;
+
+public:
+  template <typename S>
+  chainable_object(S&& obj) : obj_(std::forward<S>(obj)) {}
+
+  T& get() { return obj_; }
+};
+
+template <typename T>
+auto make_chainable_ptr(T&& obj) {
+  auto ptr = new chainable_object<plain_type_t<T>>(std::forward<T>(obj));
+  return ptr;
+}
+
+template <typename T1, typename T2, require_all_matrix_t<T1, T2>* = nullptr,
+          require_any_st_var<T1, T2>* = nullptr>
 inline auto mdivide_left(const T1& A, const T2& B) {
-  using ret_type = plain_type_t<decltype(A * B)>;
+  using ret_val_type = plain_type_t<decltype(value_of(A) * value_of(B))>;
+  using ret_type = promote_var_matrix_t<ret_val_type, T1, T2>;
 
   check_square("mdivide_left", "A", A);
   check_multiplicable("mdivide_left", "A", A, "B", B);
 
   if (A.size() == 0) {
-    return ret_type(0, B.cols());
+    return ret_type(ret_val_type(0, B.cols()));
   }
+
   if (!is_constant<T1>::value && !is_constant<T2>::value) {
     arena_t<promote_scalar_t<var, T1>> arena_A = A;
     arena_t<promote_scalar_t<var, T2>> arena_B = B;
-    arena_t<promote_scalar_t<double, T1>> arena_A_val = arena_A.val();
-    arena_t<ret_type> res = arena_A_val.householderQr().solve(arena_B.val());
-    reverse_pass_callback([arena_A, arena_B, arena_A_val, res]() mutable {
-      promote_scalar_t<double, T2> adjB
-          = arena_A_val.transpose().householderQr().solve(res.adj());
+
+    auto hqr_A_ptr = make_chainable_ptr(arena_A.val().householderQr());    
+    arena_t<ret_type> res = hqr_A_ptr->get().solve(arena_B.val());
+    reverse_pass_callback([arena_A, arena_B, hqr_A_ptr, res]() mutable {
+      promote_scalar_t<double, T2> adjB = hqr_A_ptr->get().householderQ() *
+	hqr_A_ptr->get().matrixQR().template triangularView<Eigen::Upper>().transpose().solve(res.adj());
       arena_A.adj() -= adjB * res.val_op().transpose();
       arena_B.adj() += adjB;
     });
@@ -39,19 +60,24 @@ inline auto mdivide_left(const T1& A, const T2& B) {
     return ret_type(res);
   } else if (!is_constant<T2>::value) {
     arena_t<promote_scalar_t<var, T2>> arena_B = B;
-    arena_t<promote_scalar_t<double, T1>> arena_A_val = value_of(A);
-    arena_t<ret_type> res = arena_A_val.householderQr().solve(arena_B.val());
-    reverse_pass_callback([arena_B, arena_A_val, res]() mutable {
-      arena_B.adj() += arena_A_val.transpose().householderQr().solve(res.adj());
+
+    auto hqr_A_ptr = make_chainable_ptr(value_of(A).householderQr());    
+    arena_t<ret_type> res = hqr_A_ptr->get().solve(arena_B.val());
+    reverse_pass_callback([arena_B, hqr_A_ptr, res]() mutable {
+      arena_B.adj() += hqr_A_ptr->get().householderQ() *
+	hqr_A_ptr->get().matrixQR().template triangularView<Eigen::Upper>().transpose().solve(res.adj());
     });
     return ret_type(res);
   } else {
     arena_t<promote_scalar_t<var, T1>> arena_A = A;
-    arena_t<ret_type> res = arena_A.val().householderQr().solve(value_of(B));
-    reverse_pass_callback([arena_A, res]() mutable {
+
+    auto hqr_A_ptr = make_chainable_ptr(arena_A.val().householderQr());    
+    arena_t<ret_type> res = hqr_A_ptr->get().solve(value_of(B));
+    reverse_pass_callback([arena_A, hqr_A_ptr, res]() mutable {
       arena_A.adj()
-          -= arena_A.val().transpose().householderQr().solve(res.adj())
-             * res.val_op().transpose();
+	-= hqr_A_ptr->get().householderQ() *
+	hqr_A_ptr->get().matrixQR().template triangularView<Eigen::Upper>().transpose().solve(res.adj()) *
+	res.val_op().transpose();
     });
     return ret_type(res);
   }
