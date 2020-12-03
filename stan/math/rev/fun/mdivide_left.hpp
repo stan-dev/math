bbfc1d5e4b0b486e214f88315abe4b1b639ef480
Stan Jenkins <mc.stanislaw@gmail.com>
2020-12-03 14:54:42 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/fun/mdivide_left.hpp
+++ stan/math/rev/fun/mdivide_left.hpp
@@ -15,10 +15,10 @@ namespace math {
 
 template <typename T>
 class chainable_object : public chainable_alloc {
-private:
+ private:
   T obj_;
 
-public:
+ public:
   template <typename S>
   chainable_object(S&& obj) : obj_(std::forward<S>(obj)) {}
 
@@ -48,11 +48,16 @@ inline auto mdivide_left(const T1& A, const T2& B) {
     arena_t<promote_scalar_t<var, T1>> arena_A = A;
     arena_t<promote_scalar_t<var, T2>> arena_B = B;
 
-    auto hqr_A_ptr = make_chainable_ptr(arena_A.val().householderQr());    
+    auto hqr_A_ptr = make_chainable_ptr(arena_A.val().householderQr());
     arena_t<ret_type> res = hqr_A_ptr->get().solve(arena_B.val());
     reverse_pass_callback([arena_A, arena_B, hqr_A_ptr, res]() mutable {
-      promote_scalar_t<double, T2> adjB = hqr_A_ptr->get().householderQ() *
-	hqr_A_ptr->get().matrixQR().template triangularView<Eigen::Upper>().transpose().solve(res.adj());
+      promote_scalar_t<double, T2> adjB
+          = hqr_A_ptr->get().householderQ()
+            * hqr_A_ptr->get()
+                  .matrixQR()
+                  .template triangularView<Eigen::Upper>()
+                  .transpose()
+                  .solve(res.adj());
       arena_A.adj() -= adjB * res.val_op().transpose();
       arena_B.adj() += adjB;
     });
@@ -61,23 +66,30 @@ inline auto mdivide_left(const T1& A, const T2& B) {
   } else if (!is_constant<T2>::value) {
     arena_t<promote_scalar_t<var, T2>> arena_B = B;
 
-    auto hqr_A_ptr = make_chainable_ptr(value_of(A).householderQr());    
+    auto hqr_A_ptr = make_chainable_ptr(value_of(A).householderQr());
     arena_t<ret_type> res = hqr_A_ptr->get().solve(arena_B.val());
     reverse_pass_callback([arena_B, hqr_A_ptr, res]() mutable {
-      arena_B.adj() += hqr_A_ptr->get().householderQ() *
-	hqr_A_ptr->get().matrixQR().template triangularView<Eigen::Upper>().transpose().solve(res.adj());
+      arena_B.adj() += hqr_A_ptr->get().householderQ()
+                       * hqr_A_ptr->get()
+                             .matrixQR()
+                             .template triangularView<Eigen::Upper>()
+                             .transpose()
+                             .solve(res.adj());
     });
     return ret_type(res);
   } else {
     arena_t<promote_scalar_t<var, T1>> arena_A = A;
 
-    auto hqr_A_ptr = make_chainable_ptr(arena_A.val().householderQr());    
+    auto hqr_A_ptr = make_chainable_ptr(arena_A.val().householderQr());
     arena_t<ret_type> res = hqr_A_ptr->get().solve(value_of(B));
     reverse_pass_callback([arena_A, hqr_A_ptr, res]() mutable {
-      arena_A.adj()
-	-= hqr_A_ptr->get().householderQ() *
-	hqr_A_ptr->get().matrixQR().template triangularView<Eigen::Upper>().transpose().solve(res.adj()) *
-	res.val_op().transpose();
+      arena_A.adj() -= hqr_A_ptr->get().householderQ()
+                       * hqr_A_ptr->get()
+                             .matrixQR()
+                             .template triangularView<Eigen::Upper>()
+                             .transpose()
+                             .solve(res.adj())
+                       * res.val_op().transpose();
     });
     return ret_type(res);
   }
