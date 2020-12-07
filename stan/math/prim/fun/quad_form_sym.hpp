8dfd5b0f88b8d699bbc1edb5504f16ef90388232
tadej <tadej.c@gmail.com>
2020-12-07 21:41:41 +0100

    make quad_form_sym use make_holder

--- stan/math/prim/fun/quad_form_sym.hpp
+++ stan/math/prim/fun/quad_form_sym.hpp
@@ -32,8 +32,9 @@ inline plain_type_t<EigMat2> quad_form_sym(const EigMat1& A, const EigMat2& B) {
   const auto& A_ref = to_ref(A);
   const auto& B_ref = to_ref(B);
   check_symmetric("quad_form_sym", "A", A_ref);
-  auto* ret = new plain_type_t<EigMat2>(B_ref.transpose() * A_ref * B_ref);
-  return holder(value_type_t<EigMat2>(0.5) * (*ret + ret->transpose()), ret);
+  return make_holder(
+      [](const auto& ret) { return 0.5 * (ret + ret.transpose()); },
+      (B_ref.transpose() * A_ref * B_ref).eval());
 }
 
 /**
