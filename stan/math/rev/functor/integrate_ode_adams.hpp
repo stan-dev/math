56b952f7e1458d3cb5e1ad8fd255c2e8a1323374
Sebastian Weber <sebastian.weber@novartis.com>
2020-12-07 17:04:58 +0100

    fix errors

--- stan/math/rev/functor/integrate_ode_adams.hpp
+++ stan/math/rev/functor/integrate_ode_adams.hpp
@@ -25,8 +25,9 @@ integrate_ode_adams(const F& f, const std::vector<T_y0>& y0, const T_t0& t0,
                     double absolute_tolerance = 1e-10,
                     long int max_num_steps = 1e8) {  // NOLINT(runtime/int)
   internal::integrate_ode_std_vector_interface_adapter<F> f_adapted(f);
+  const Eigen::VectorXd absolute_tolerance_state = Eigen::VectorXd::Constant(y0.size(), absolute_tolerance);
   auto y = ode_adams_tol_impl("integrate_ode_adams", f_adapted, to_vector(y0),
-                              t0, ts, relative_tolerance, absolute_tolerance,
+                              t0, ts, relative_tolerance, absolute_tolerance_state,
                               max_num_steps, msgs, theta, x, x_int);
 
   std::vector<std::vector<return_type_t<T_y0, T_param, T_t0, T_ts>>>
