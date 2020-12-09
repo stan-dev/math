3a7321e9dd478b4bd3d8293b52690e81a4d4ea71
Hamada S. Badr <hamada.s.badr@gmail.com>
2020-12-09 10:10:01 -0500

    Update TBB interface and allow using the system lib
    Define TBB_INTERFACE_NEW to use the new interface of Intel/OneAPI TBB system library.
    This can be added in the user-defined variables in `~/.config/stan/make.local` or `make/local`.
    
    TBB updated functionality: https://software.intel.com/content/www/us/en/develop/articles/tbb-revamp.html
    
    TBB release notes and backward compatibility: https://software.intel.com/content/www/us/en/develop/articles/intel-oneapi-threading-building-blocks-release-notes.html
    
    Intel OneAPI release notes: https://software.intel.com/content/www/us/en/develop/articles/intel-oneapi-toolkit-release-notes.html
    
    Note that Intel OneAPI automatically sets the required environment variables for TBB.
    
    Signed-off-by: Hamada S. Badr <hamada.s.badr@gmail.com>

--- test/unit/math/prim/core/init_threadpool_tbb_late_test.cpp
+++ test/unit/math/prim/core/init_threadpool_tbb_late_test.cpp
@@ -4,6 +4,28 @@
 #include <test/unit/util.hpp>
 #include <test/unit/math/prim/functor/utils_threads.hpp>
 
+#ifdef TBB_INTERFACE_NEW
+
+#include <tbb/global_control.h>
+#include <tbb/task_arena.h>
+
+TEST(intel_tbb_late_init, check_status) {
+  const int num_threads = tbb::global_control::default_num_threads();
+  tbb::global_control tbb_scheduler;
+
+  if (num_threads > 1) {
+    set_n_threads(num_threads - 1);
+    tbb::global_control& tbb_init = stan::math::init_threadpool_tbb();
+    EXPECT_TRUE(tbb_init.is_active());
+
+    // STAN_NUM_THREADS is not being honored if we have first
+    // initialized the TBB scheduler outside of init_threadpool_tbb
+    EXPECT_EQ(num_threads, tbb::this_task_arena::max_concurrency());
+  }
+}
+
+#else
+
 #include <tbb/task_scheduler_init.h>
 #include <tbb/task_arena.h>
 
@@ -21,3 +43,5 @@ TEST(intel_tbb_late_init, check_status) {
     EXPECT_EQ(num_threads, tbb::this_task_arena::max_concurrency());
   }
 }
+
+#endif
