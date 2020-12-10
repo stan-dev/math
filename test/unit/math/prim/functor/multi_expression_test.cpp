196f6c8204d32640fa99c274611fed8073ab6296
Stan Jenkins <mc.stanislaw@gmail.com>
2020-12-10 10:28:11 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/prim/functor/multi_expression_test.cpp
+++ test/unit/math/prim/functor/multi_expression_test.cpp
@@ -17,9 +17,9 @@ TEST(MathFunctions, multi_expression_mismatched_sizes) {
 
 TEST(MathFunctions, multi_expression_all_col_major) {
   Eigen::MatrixXd a(2, 3);
-  a << 1, 2, 3, 4,5,6;
+  a << 1, 2, 3, 4, 5, 6;
   Eigen::MatrixXd b(2, 3);
-  b << 1, 2, 3, 5,6,2;
+  b << 1, 2, 3, 5, 6, 2;
   Eigen::MatrixXd c, d;
   stan::math::eigen_results(c, d) = stan::math::eigen_expressions(a * 2, b + a);
 
@@ -33,9 +33,9 @@ TEST(MathFunctions, multi_expression_all_row_major) {
   using RowMat
       = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
   RowMat a(2, 3);
-  a << 1, 2, 3, 4,4,5;
+  a << 1, 2, 3, 4, 4, 5;
   RowMat b(2, 3);
-  b << 1, 2, 3, 5,7,8;
+  b << 1, 2, 3, 5, 7, 8;
   RowMat c, d;
   stan::math::eigen_results(c, d) = stan::math::eigen_expressions(a * 2, b + a);
 
@@ -145,20 +145,21 @@ TEST(MathFunctions, multi_expression_many_expressions) {
   using RowMat
       = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
   Eigen::MatrixXd a(2, 3);
-  a << 1, 2, 3, 4,5,6;
+  a << 1, 2, 3, 4, 5, 6;
   RowMat b(3, 3);
   b << 1, 2, 3, 4, 5, 6, 7, 8, 9;
   Eigen::MatrixXd c(4, 4);
   c << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
   Eigen::MatrixXd d, e(c);
   RowMat f, g;
-  stan::math::eigen_results(d, e.block(2, 1, 2, 3), f, g) = stan::math::eigen_expressions(
-      a * 2, b.block(0, 1, 3, 2).transpose() + a,
-      c.block(2, 1, 2, 3) + a, b.block(0, 1, 3, 2).transpose().array() - 2);
+  stan::math::eigen_results(d, e.block(2, 1, 2, 3), f, g)
+      = stan::math::eigen_expressions(
+          a * 2, b.block(0, 1, 3, 2).transpose() + a, c.block(2, 1, 2, 3) + a,
+          b.block(0, 1, 3, 2).transpose().array() - 2);
 
   Eigen::MatrixXd d_res = a * 2;
   Eigen::MatrixXd e_res = c;
-     e_res.block(2, 1, 2, 3) = b.block(0, 1, 3, 2).transpose() + a;
+  e_res.block(2, 1, 2, 3) = b.block(0, 1, 3, 2).transpose() + a;
   RowMat f_res = c.block(2, 1, 2, 3) + a;
   RowMat g_res = b.block(0, 1, 3, 2).transpose().array() - 2;
   EXPECT_MATRIX_EQ(d, d_res);
@@ -178,13 +179,14 @@ TEST(MathFunctions, multi_expression_many_expressions2) {
   c << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
   Eigen::MatrixXd d, e(c);
   RowMat f, g;
-  stan::math::eigen_results(d, e.block(2, 1, 2, 2), f, g) = stan::math::eigen_expressions(
-      a * 2, b.block(0, 1, 2, 2).transpose() + a,
-      c.block(2, 1, 2, 2) + a.transpose(), b.block(1, 0, 2, 2).array() - 2);
+  stan::math::eigen_results(d, e.block(2, 1, 2, 2), f, g)
+      = stan::math::eigen_expressions(
+          a * 2, b.block(0, 1, 2, 2).transpose() + a,
+          c.block(2, 1, 2, 2) + a.transpose(), b.block(1, 0, 2, 2).array() - 2);
 
   Eigen::MatrixXd d_res = a * 2;
   Eigen::MatrixXd e_res = c;
-     e_res.block(2, 1, 2, 2) = b.block(0, 1, 2, 2).transpose() + a;
+  e_res.block(2, 1, 2, 2) = b.block(0, 1, 2, 2).transpose() + a;
   RowMat f_res = c.block(2, 1, 2, 2) + a.transpose();
   RowMat g_res = b.block(1, 0, 2, 2).array() - 2;
   EXPECT_MATRIX_EQ(d, d_res);
