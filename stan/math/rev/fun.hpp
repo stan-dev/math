c5b9b4e12859b364c845d8d4555681e3a0c0d14a
Ben <bbbales2@gmail.com>
2020-12-12 19:57:14 -0500

    Added varmat support for `read_cov_L` (Issue #2101)

--- stan/math/rev/fun.hpp
+++ stan/math/rev/fun.hpp
@@ -127,6 +127,7 @@
 #include <stan/math/rev/fun/quad_form.hpp>
 #include <stan/math/rev/fun/quad_form_sym.hpp>
 #include <stan/math/rev/fun/read_corr_L.hpp>
+#include <stan/math/rev/fun/read_cov_L.hpp>
 #include <stan/math/rev/fun/rising_factorial.hpp>
 #include <stan/math/rev/fun/round.hpp>
 #include <stan/math/rev/fun/rows_dot_product.hpp>
--- stan/math/rev/fun/read_cov_L.hpp
+++ stan/math/rev/fun/read_cov_L.hpp
@@ -1,9 +1,11 @@
-#ifndef STAN_MATH_PRIM_FUN_READ_COV_L_HPP
-#define STAN_MATH_PRIM_FUN_READ_COV_L_HPP
+#ifndef STAN_MATH_REV_FUN_READ_COV_L_HPP
+#define STAN_MATH_REV_FUN_READ_COV_L_HPP
 
 #include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/rev/core.hpp>
 #include <stan/math/prim/fun/log.hpp>
-#include <stan/math/prim/fun/read_corr_L.hpp>
+#include <stan/math/prim/fun/read_cov_L.hpp>
+#include <stan/math/rev/fun/read_corr_L.hpp>
 #include <stan/math/prim/fun/sum.hpp>
 #include <stan/math/prim/fun/constants.hpp>
 
@@ -25,16 +27,26 @@ namespace math {
  * partial correlations.
  */
 template <typename T_CPCs, typename T_sds,
-          require_all_eigen_vector_t<T_CPCs, T_sds>* = nullptr,
+          require_any_var_vector_t<T_CPCs, T_sds>* = nullptr,
           require_vt_same<T_CPCs, T_sds>* = nullptr>
-Eigen::Matrix<value_type_t<T_CPCs>, Eigen::Dynamic, Eigen::Dynamic> read_cov_L(
-    const T_CPCs& CPCs, const T_sds& sds, value_type_t<T_CPCs>& log_prob) {
+inline auto read_cov_L(const T_CPCs& CPCs, const T_sds& sds, scalar_type_t<T_CPCs>& log_prob) {
   size_t K = sds.rows();
   // adjust due to transformation from correlations to covariances
-  log_prob += (sum(log(sds)) + LOG_TWO) * K;
-  return make_holder(
-      [&sds](const auto& b) { return sds.matrix().asDiagonal() * b; },
-      read_corr_L(CPCs, K, log_prob));
+  log_prob += (sum(log(sds.val())) + LOG_TWO) * K;
+
+  auto corr_L = read_corr_L(CPCs, K, log_prob);
+  var_value<Eigen::MatrixXd> res = sds.val().matrix().asDiagonal() * corr_L.val();
+
+  reverse_pass_callback([CPCs, sds, corr_L, log_prob, res]() mutable {
+    size_t K = sds.size();
+
+    corr_L.adj() += sds.val().matrix().asDiagonal() * res.adj();
+    sds.adj() += rows_dot_product(res.adj(), corr_L.val());
+
+    sds.adj() += (K * log_prob.adj() / sds.val().array()).matrix();
+  });
+  
+  return res;
 }
 
 }  // namespace math
