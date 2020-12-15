ec80ff1cbe8e156552a1787463c00638a1cb8893
Stan Jenkins <mc.stanislaw@gmail.com>
2020-12-15 00:32:56 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/fun/generalized_inverse.hpp
+++ stan/math/rev/fun/generalized_inverse.hpp
@@ -53,7 +53,7 @@ inline auto generalized_inverse(const VarMat& G) {
         mdivide_left_spd(A_spd.val_op(), G_arena.val_op()).transpose());
 
     arena_t<VarMat> res = inv_G;
-    
+
     auto PG = to_arena(-G_arena.val_op() * inv_G.val_op());
     PG.diagonal().array() += 1.0;
 
@@ -61,10 +61,13 @@ inline auto generalized_inverse(const VarMat& G) {
     GP.diagonal().array() += 1.0;
 
     reverse_pass_callback([G_arena, res, inv_G, GP, PG]() mutable {
-      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op() * inv_G.val_op().transpose();
-      G_arena.adj() +=  PG.val_op() * res.adj_op().transpose() * tcrossprod(inv_G.val_op());
-      G_arena.adj() += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
-	});
+      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op()
+                       * inv_G.val_op().transpose();
+      G_arena.adj() += PG.val_op() * res.adj_op().transpose()
+                       * tcrossprod(inv_G.val_op());
+      G_arena.adj()
+          += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
+    });
     return ret_type(inv_G);
   } else {
     arena_t<VarMat> G_arena(G);
@@ -81,9 +84,12 @@ inline auto generalized_inverse(const VarMat& G) {
     GP.diagonal().array() += 1.0;
 
     reverse_pass_callback([G_arena, res, inv_G, GP, PG]() mutable {
-      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op() * inv_G.val_op().transpose();
-      G_arena.adj() +=  PG.val_op() * res.adj_op().transpose() * tcrossprod(inv_G.val_op());
-      G_arena.adj() += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
+      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op()
+                       * inv_G.val_op().transpose();
+      G_arena.adj() += PG.val_op() * res.adj_op().transpose()
+                       * tcrossprod(inv_G.val_op());
+      G_arena.adj()
+          += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
     });
     return ret_type(inv_G);
   }
@@ -134,9 +140,12 @@ inline auto generalized_inverse(const VarMat& G, const double a) {
     GP.diagonal().array() += 1.0;
 
     reverse_pass_callback([G_arena, res, inv_G, GP, PG]() mutable {
-      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op() * inv_G.val_op().transpose();
-      G_arena.adj() +=  PG.val_op() * res.adj_op().transpose() * tcrossprod(inv_G.val_op());
-      G_arena.adj() += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
+      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op()
+                       * inv_G.val_op().transpose();
+      G_arena.adj() += PG.val_op() * res.adj_op().transpose()
+                       * tcrossprod(inv_G.val_op());
+      G_arena.adj()
+          += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
     });
     return ret_type(inv_G);
   } else {
@@ -145,7 +154,7 @@ inline auto generalized_inverse(const VarMat& G, const double a) {
     A_spd.diagonal().array() += a;
     arena_t<VarMat> inv_G(
         mdivide_right_spd(G_arena.val_op(), A_spd.val_op()).transpose());
- 
+
     arena_t<VarMat> res = inv_G;
 
     auto PG = to_arena(-G_arena.val_op() * inv_G.val_op());
@@ -155,9 +164,12 @@ inline auto generalized_inverse(const VarMat& G, const double a) {
     GP.diagonal().array() += 1.0;
 
     reverse_pass_callback([G_arena, res, inv_G, GP, PG]() mutable {
-      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op() * inv_G.val_op().transpose();
-      G_arena.adj() +=  PG.val_op() * res.adj_op().transpose() * tcrossprod(inv_G.val_op());
-      G_arena.adj() += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
+      G_arena.adj() -= inv_G.val_op().transpose() * res.adj_op()
+                       * inv_G.val_op().transpose();
+      G_arena.adj() += PG.val_op() * res.adj_op().transpose()
+                       * tcrossprod(inv_G.val_op());
+      G_arena.adj()
+          += crossprod(inv_G.val_op()) * res.adj_op().transpose() * GP.val_op();
     });
     return ret_type(inv_G);
   }
