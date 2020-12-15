5bbaa092a7a2740d3f3e6b0f0221a088c5d3ce8d
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-12-15 18:07:19 +0100

    add dims, num_elements

--- stan/math/opencl/prim.hpp
+++ stan/math/opencl/prim.hpp
@@ -139,6 +139,7 @@
 #include <stan/math/opencl/prim/neg_binomial_2_log_glm_lpmf.hpp>
 #include <stan/math/opencl/prim/normal_id_glm_lpdf.hpp>
 #include <stan/math/opencl/prim/normal_lpdf.hpp>
+#include <stan/math/opencl/prim/num_elements.hpp>
 #include <stan/math/opencl/prim/ordered_logistic_glm_lpmf.hpp>
 #include <stan/math/opencl/prim/pareto_lpdf.hpp>
 #include <stan/math/opencl/prim/pareto_type_2_lpdf.hpp>
