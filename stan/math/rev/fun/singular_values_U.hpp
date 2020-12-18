f674c6cf98e78a5f644a2ddc960c80079ea4f3b4
Dashadower <tttllshin@gmail.com>
2020-12-19 01:19:28 +0900

    WIP svd_U function and tests

---
+++ stan/math/rev/fun/singular_values_U.hpp
@@ -0,0 +1,73 @@
+#ifndef MATH_SINGULAR_VALUES_H
+#define MATH_SINGULAR_VALUES_H
+
+#include <stan/math/rev/meta.hpp>
+#include <stan/math/rev/core.hpp>
+#include <stan/math/rev/functor/reverse_pass_callback.hpp>
+#include <stan/math/rev/core/arena_matrix.hpp>
+#include <stan/math/rev/fun/value_of.hpp>
+#include <stan/math/rev/fun/typedefs.hpp>
+#include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/prim/err/check_symmetric.hpp>
+#include <stan/math/prim/err/check_nonzero_size.hpp>
+#include <stan/math/prim/fun/typedefs.hpp>
+#include <stan/math/prim/fun/value_of_rec.hpp>
+#include <Eigen/SVD>
+
+namespace stan {
+namespace math {
+
+/**
+ * Given input matrix m, return matrix U where \mathbf{m} = \mathbf{UDV^{T}}
+ *
+ * Equation (4) from Differentiable Programming Tensor Networks(H. Liao, J. Liu, et al., arXiv:1903.09650)
+ * is used for MxN input matrix's adjoint calculation.
+ *
+ * @tparam EigMat type of input matrix
+ * @param m MxN input matrix
+ * @return Orthogonal matrix U
+ */
+template <typename EigMat,
+    require_eigen_matrix_dynamic_t<EigMat>* = nullptr,
+	  require_vt_var<EigMat>* = nullptr>
+inline auto singular_values_U(const EigMat& m) {
+  using ret_type = promote_scalar_t<var, Eigen::MatrixXd>;
+  check_nonzero_size("singular_values", "m", m);
+
+  const int M = to_arena(m.rows());
+  auto arena_m = to_arena(m);
+  
+  Eigen::JacobiSVD<Eigen::MatrixXd> svd(arena_m.val(), Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+  auto arena_D = to_arena((Eigen::MatrixXd) svd.singularValues().asDiagonal());
+
+  Eigen::MatrixXd Fp(M, M);
+
+  for(int i = 0; i < M; i++) {
+    for(int j = 0; j < M; j++) {
+      if(j == i) {
+        Fp(i, j) = 0.0;
+      } else {
+        Fp(i, j)  = 1.0 / (arena_D(j, j) - arena_D(i, i)) + 1.0 / (arena_D(i, i) + arena_D(j, j));
+      }
+    }
+  }
+
+  auto arena_Fp = to_arena(Fp);
+  auto arena_U = to_arena(svd.matrixU());
+  auto arena_V = to_arena(svd.matrixV());
+
+
+  reverse_pass_callback(
+      [arena_m, arena_U, arena_D, arena_V, arena_Fp, M]() mutable {
+        arena_m.adj() += 0.5 * arena_U.val() * (arena_Fp.array() * (arena_U.val().transpose() * arena_U.adj() - arena_U.adj().transpose() * arena_U.val()).array()).matrix()
+         * arena_V.transpose() + (Eigen::MatrixXd::Identity(M, M) - arena_U.val() * arena_U.val().transpose())
+          * arena_U.adj() * arena_D.inverse() * arena_V.transpose();
+      });
+
+  return ret_type(arena_U);
+}
+}  // namespace math
+}  // namespace stan
+
+#endif
---
+++ test/unit/math/rev/fun/singular_values_U_test.cpp
@@ -0,0 +1,47 @@
+#include <stan/math/prim.hpp>
+#include <stan/math/rev/fun/singular_values_U.hpp>
+#include <stan/math/rev/fun/sum.hpp>
+#include <stan/math/rev/fun/log.hpp>
+#include <gtest/gtest.h>
+#include <test/unit/util.hpp>
+#include <typeinfo>
+TEST(AgradRev, singularvalues_U_gradient) {
+    using namespace stan::math;
+    Eigen::MatrixXd A(4, 4);
+    // Random symmetric matrix
+    A << 1.8904, 0.7204, -0.1599, 1.2028, 0.7204, 7.3394, 2.0895, -0.6151,
+    -0.1599, 2.0895, 2.4601, -1.7219, 1.2028, -0.6151, -1.7219, 4.5260;
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+    auto U = svd.matrixU();
+    Eigen::MatrixXd D = svd.singularValues().asDiagonal();
+    auto V = svd.matrixV();
+
+    Eigen:: MatrixXd Fp(4, 4);
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(i == j){
+                Fp(i, j) = 0.0;
+            }
+            else{
+                Fp(i, j) = 1.0 / (D(j, j) - D(i, i)) + 1.0 / (D(i, i) + D(j, j));
+            }
+        }
+    }
+
+    Eigen::MatrixXd adj_A_fd = Eigen::MatrixXd::Zero(4, 4);
+
+    Eigen::MatrixXd adj_U_fd = Eigen::MatrixXd::Ones(4, 4);
+
+    adj_A_fd += 0.5 * U * (Fp.cwiseProduct(U.transpose() * adj_U_fd - adj_U_fd.transpose() * U)).matrix() * V.transpose()
+        + (Eigen::MatrixXd::Identity(4, 4) - U * U.transpose()) * adj_U_fd * D.inverse() * V.transpose();
+
+
+    matrix_v arena_a(A);
+    auto stan_u = singular_values_U(arena_a);
+
+    EXPECT_MATRIX_FLOAT_EQ(U, stan_u.val());
+    
+    EXPECT_MATRIX_FLOAT_EQ(adj_A_fd, arena_a.adj())
+}
