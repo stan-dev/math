f9f0dc0cc56e0079eec979823a72112ab71e3ec4
Steve Bronder <stevo15025@gmail.com>
2020-12-18 15:11:29 -0500

    remove not used compiler attributes

--- stan/math/prim/meta/compiler_attributes.hpp
+++ stan/math/prim/meta/compiler_attributes.hpp
@@ -4,21 +4,11 @@
 #ifdef __GNUC__
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
-#define STAN_ALWAYS_INLINE __attribute__((always_inline))
-#define STAN_STRONG_INLINE inline STAN_ALWAYS_INLINE
-#define STAN_FLATTEN __attribute__((flatten))
-#define STAN_STRONGER_INLINE inline __attribute__((always_inline, flatten))
 #define STAN_COLD_PATH __attribute__((noinline, cold))
-#define STAN_HOT_PATH __attribute__((hot))
 #else
 #define likely(x) (x)
 #define unlikely(x) (x)
-#define STAN_ALWAYS_INLINE
-#define STAN_STRONG_INLINE inline
-#define STAN_FLATTEN
-#define STAN_STRONGER_INLINE
 #define STAN_COLD_PATH
-#define STAN_HOT_PATH
 #endif
 
 #endif
