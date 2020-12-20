7ba6df0a582aa3204ee23f92c00e7781ca96c23e
Ben <bbbales2@gmail.com>
2020-12-20 09:56:20 -0500

    Updated tests again (Issue #2101)

--- test/unit/math/mix/fun/svd_U_test.cpp
+++ test/unit/math/mix/fun/svd_U_test.cpp
@@ -14,17 +14,17 @@ TEST(MathMixMatFun, svd_U) {
   m11 << 1.1;
   stan::test::expect_ad(tols, f, m11);
 
-  /*Eigen::MatrixXd m22(2, 2);
+  Eigen::MatrixXd m22(2, 2);
   m22 << 3, -5, 7, 11;
   stan::test::expect_ad(tols, f, m22);
 
-  Eigen::MatrixXd m23(2, 3);
+  /*Eigen::MatrixXd m23(2, 3);
   m23 << 3, 5, -7, -11, 13, -17;
   Eigen::MatrixXd m32 = m23.transpose();
   stan::test::expect_ad(tols, f, m23);
-  stan::test::expect_ad(tols, f, m32);
+  stan::test::expect_ad(tols, f, m32);*/
 
   Eigen::MatrixXd a22(2, 2);
   a22 << 1, 2, 3, 4;
-  stan::test::expect_ad(tols, f, a22);*/
+  stan::test::expect_ad(tols, f, a22);
 }
