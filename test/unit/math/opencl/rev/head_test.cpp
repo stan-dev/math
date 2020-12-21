d0c0a032ac2178503f45c051af0224f441fa4d37
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-12-21 12:06:48 +0100

    use test utility

--- test/unit/math/opencl/rev/head_test.cpp
+++ test/unit/math/opencl/rev/head_test.cpp
@@ -47,41 +47,13 @@ TEST(MathMatrixCL, head_value_check_vector) {
   stan::math::vector_d m1(9);
   m1 << 1, 2, 3, 4, 5, 6, 7, 8, 9;
 
-  stan::math::test::compare_cpu_opencl_prim(head_functor, m1);
-
-  stan::math::vector_v m2 = m1;
-  stan::math::vector_v m3 = m1;
-  stan::math::var_value<stan::math::matrix_cl<double>> m3_cl
-      = stan::math::to_matrix_cl(m3);
-  stan::math::matrix_v m2_res = stan::math::head(m2, 5);
-  stan::math::var_value<stan::math::matrix_cl<double>> m3_res_cl
-      = stan::math::head(m3_cl, 5);
-  stan::math::var s = sum(m2_res) + sum(m3_res_cl);
-  s.grad();
-
-  EXPECT_MATRIX_NEAR(m2_res.val(), stan::math::from_matrix_cl(m3_res_cl.val()),
-                     1E-8);
-  EXPECT_MATRIX_NEAR(m2.adj(), m3.adj(), 1E-8);
+  stan::math::test::compare_cpu_opencl_prim_rev(head_functor, m1);
 }
 
 TEST(MathMatrixCL, head_value_check_row_vector) {
   stan::math::row_vector_d m1(9);
   m1 << 1, 2, 3, 4, 5, 6, 7, 8, 9;
 
-  stan::math::test::compare_cpu_opencl_prim(head_functor, m1);
-
-  stan::math::row_vector_v m2 = m1;
-  stan::math::row_vector_v m3 = m1;
-  stan::math::var_value<stan::math::matrix_cl<double>> m3_cl
-      = stan::math::to_matrix_cl(m3);
-  stan::math::matrix_v m2_res = stan::math::head(m2, 5);
-  stan::math::var_value<stan::math::matrix_cl<double>> m3_res_cl
-      = stan::math::head(m3_cl, 5);
-  stan::math::var s = sum(m2_res) + sum(m3_res_cl);
-  s.grad();
-
-  EXPECT_MATRIX_NEAR(m2_res.val(), stan::math::from_matrix_cl(m3_res_cl.val()),
-                     1E-8);
-  EXPECT_MATRIX_NEAR(m2.adj(), m3.adj(), 1E-8);
+  stan::math::test::compare_cpu_opencl_prim_rev(head_functor, m1);
 }
 #endif
