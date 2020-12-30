6122820948f1362fdbffbd85dc40a357a9bec529
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-12-30 13:01:51 +0800

    Use universal references

--- stan/math/prim/eigen_plugins.h
+++ stan/math/prim/eigen_plugins.h
@@ -9,7 +9,8 @@ template <typename> struct has_member { typedef int type; };
  *
  * TODO(Andrew): Replace with std::void_t after move to C++17
  */
-template <typename T, typename has_member<decltype(T::d_)>::type = 0>
+template <typename T,
+          typename has_member<decltype(std::decay_t<T>::d_)>::type = 0>
 struct is_fvar : std::true_type
 { };
 
@@ -18,7 +19,8 @@ struct is_fvar : std::true_type
  *
  * TODO(Andrew): Replace with std::void_t after move to C++17
  */
-template <typename T, typename has_member<decltype(T::vi_)>::type = 0>
+template <typename T,
+          typename has_member<decltype(std::decay_t<T>::vi_)>::type = 0>
 struct is_var : std::true_type
 { };
 
@@ -27,30 +29,32 @@ struct is_var : std::true_type
  *
  * TODO(Andrew): Replace with std::void_t after move to C++17
  */
-template <typename T, typename has_member<decltype(std::remove_pointer_t<T>::adj_)>::type = 0>
+template <typename T,
+          typename has_member<
+                      decltype(std::remove_pointer_t<
+                                  std::decay_t<T>>::adj_)>::type = 0>
 struct is_vari : std::true_type
 { };
 
 
-//TODO(Andrew): Replace std::is_const<>::value with std::is_const_v<> after move to C++17
-template <typename T>
-using double_return_t = std::conditional_t<std::is_const<std::remove_reference_t<T>>::value,
-                                         const double,
-                                         double>;
-template <typename T>
-using reverse_return_t = std::conditional_t<std::is_const<std::remove_reference_t<T>>::value,
-                                         const double&,
-                                         double&>;
+/**
+ * TODO(Andrew): Replace std::is_const<>::value with std::is_const_v<>
+ *                after move to C++17
+ */
+template <typename T_in, typename T_return>
+using match_const_t =
+        std::conditional_t<std::is_const<std::remove_reference_t<T_in>>::value,
+                           const T_return&, T_return&>;
 
 template <typename T>
-using vari_return_t = std::conditional_t<std::is_const<std::remove_reference_t<T>>::value,
-                                          const decltype(T::vi_)&,
-                                          decltype(T::vi_)&>;
+using reverse_return_t = match_const_t<T, double>;
 
 template <typename T>
-using forward_return_t = std::conditional_t<std::is_const<std::remove_reference_t<T>>::value,
-                                         const decltype(T::val_)&,
-                                         decltype(T::val_)&>;
+using vari_return_t = match_const_t<T, decltype(std::decay_t<T>::vi_)>;
+
+template <typename T>
+using forward_return_t = match_const_t<T, decltype(std::decay_t<T>::val_)>;
+
 /**
  * Structure to return a view to the values in a var, vari*, and fvar<T>.
  * To identify the correct member to call for a given input, templates
@@ -72,33 +76,27 @@ struct val_Op{
   template <typename T = Scalar,
            std::enable_if_t<is_vari<T>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    const double& operator()(T& v) const { return v->val_; }
+    reverse_return_t<T> operator()(T&& v) const { return v->val_; }
 
   //Returns value from a var
   template <typename T = Scalar,
            std::enable_if_t<is_var<T>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    const double& operator()(T& v) const { return v.vi_->val_; }
+    reverse_return_t<T> operator()(T&& v) const {
+      return v.vi_->val_;
+    }
 
   //Returns value from an fvar
   template <typename T = Scalar,
            std::enable_if_t<is_fvar<T>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    forward_return_t<T> operator()(T& v) const { return v.val_; }
+    forward_return_t<T> operator()(T&& v) const { return v.val_; }
 
-  //Returns double unchanged from input (by value)
+  //Returns double unchanged from input (by reference)
   template <typename T = Scalar,
-           std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+           std::enable_if_t<std::is_arithmetic<std::decay_t<T>>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    double_return_t<T> operator()(T v) const { return v; }
-
-  //Returns double unchanged from input (by reference)
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-  const double& operator()(const double& v) const { return v; }
-
-  //Returns double unchanged from input (by reference)
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-  double& operator()(double& v) const { return v; }
+  match_const_t<T, double> operator()(T&& v) const { return v; }
 };
 
 /**
@@ -106,9 +104,8 @@ struct val_Op{
  * vari*, fvar, or doubles and returning a view to the const matrix
  * containing the values
  */
-inline const CwiseUnaryOp<val_Op, const Derived>
-val() const { return CwiseUnaryOp<val_Op, const Derived>(derived());
-}
+inline const CwiseUnaryOp<val_Op, Derived>
+val() const { return CwiseUnaryOp<val_Op, Derived>(derived()); }
 
 /**
  * Coefficient-wise function applying val_Op struct to a matrix of var
@@ -116,17 +113,14 @@ val() const { return CwiseUnaryOp<val_Op, const Derived>(derived());
  * the values
  */
 inline const CwiseUnaryOp<val_Op, Derived>
-val_op() { return CwiseUnaryOp<val_Op, Derived>(derived());
-}
+val_op() const { return CwiseUnaryOp<val_Op, Derived>(derived()); }
 
 /**
  * Coefficient-wise function applying val_Op struct to a matrix of var
  * or vari* and returning a view to the values
-
  */
 inline CwiseUnaryView<val_Op, Derived>
-val() { return CwiseUnaryView<val_Op, Derived>(derived());
-}
+val() { return CwiseUnaryView<val_Op, Derived>(derived()); }
 
 /**
  * Structure to return tangent from an fvar.
@@ -135,18 +129,18 @@ struct d_Op {
   EIGEN_EMPTY_STRUCT_CTOR(d_Op);
 
   //Returns tangent from an fvar
-  template <typename T = Scalar>
+  template <typename T = Scalar,
+            std::enable_if_t<is_fvar<T>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    forward_return_t<T> operator()(T& v) const { return v.d_; }
+    forward_return_t<T> operator()(T&& v) const { return v.d_; }
 };
 
 /**
  * Coefficient-wise function applying d_Op struct to a matrix of const fvar<T>
  * and returning a const matrix of type T containing the tangents
  */
-inline const CwiseUnaryOp<d_Op, const Derived>
-d() const { return CwiseUnaryOp<d_Op, const Derived>(derived());
-}
+inline const CwiseUnaryOp<d_Op, Derived>
+d() const { return CwiseUnaryOp<d_Op, Derived>(derived()); }
 
 /**
  * Coefficient-wise function applying d_Op struct to a matrix of fvar<T>
@@ -161,8 +155,7 @@ d_op() const { return CwiseUnaryOp<d_Op, Derived>(derived()); }
  * be modified
  */
 inline CwiseUnaryView<d_Op, Derived>
-d() { return CwiseUnaryView<d_Op, Derived>(derived());
-}
+d() { return CwiseUnaryView<d_Op, Derived>(derived()); }
 
 /**
  * Structure to return adjoints from var and vari*.
@@ -174,22 +167,21 @@ struct adj_Op {
   template <typename T = Scalar,
            std::enable_if_t<is_vari<T>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    reverse_return_t<T> operator()(T& v) const { return v->adj_; }
+    reverse_return_t<T> operator()(T&& v) const { return v->adj_; }
 
   //Returns adjoint from a var
   template <typename T = Scalar,
            std::enable_if_t<is_var<T>::value>* = nullptr>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    reverse_return_t<T> operator()(T& v) const { return v.vi_->adj_; }
+    reverse_return_t<T> operator()(T&& v) const { return v.vi_->adj_; }
 };
 
 /**
  * Coefficient-wise function applying adj_Op struct to a matrix of const var
  * and returning a const matrix of type T containing the values
  */
-inline const CwiseUnaryOp<adj_Op, const Derived>
-adj() const { return CwiseUnaryOp<adj_Op, const Derived>(derived());
-}
+inline const CwiseUnaryOp<adj_Op, Derived>
+adj() const { return CwiseUnaryOp<adj_Op, Derived>(derived()); }
 
 /**
  * Coefficient-wise function applying adj_Op struct to a matrix of var
@@ -205,8 +197,7 @@ adj_op() const { return CwiseUnaryOp<adj_Op, Derived>(derived()); }
  * be modified
  */
 inline CwiseUnaryView<adj_Op, Derived>
-adj() { return CwiseUnaryView<adj_Op, Derived>(derived());
-}
+adj() { return CwiseUnaryView<adj_Op, Derived>(derived()); }
 
 /**
  * Structure to return vari* from a var.
@@ -217,24 +208,22 @@ struct vi_Op {
   //Returns vari* from a var
   template <typename T = Scalar>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
-    vari_return_t<T> operator()(T& v) const { return v.vi_; }
+    vari_return_t<T> operator()(T&& v) const { return v.vi_; }
 };
 
 /**
  * Coefficient-wise function applying vi_Op struct to a matrix of const var
  * and returning a const matrix of vari*
  */
-inline const CwiseUnaryOp<vi_Op, const Derived>
-vi() const { return CwiseUnaryOp<vi_Op, const Derived>(derived());
-}
+inline const CwiseUnaryOp<vi_Op, Derived>
+vi() const { return CwiseUnaryOp<vi_Op, Derived>(derived()); }
 
 /**
  * Coefficient-wise function applying vi_Op struct to a matrix of var
  * and returning a view to a matrix of vari* that can be modified
  */
 inline CwiseUnaryView<vi_Op, Derived>
-vi() { return CwiseUnaryView<vi_Op, Derived>(derived());
-}
+vi() { return CwiseUnaryView<vi_Op, Derived>(derived()); }
 
 #define EIGEN_STAN_MATRIXBASE_PLUGIN
 #define EIGEN_STAN_ARRAYBASE_PLUGIN
