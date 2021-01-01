417b1b52a1934f316bfbf0f0dfab893537bc643d
Stan Jenkins <mc.stanislaw@gmail.com>
2021-01-01 21:35:37 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/fun/trace.hpp
+++ stan/math/rev/fun/trace.hpp
@@ -23,9 +23,10 @@ template <typename T, require_rev_matrix_t<T>* = nullptr>
 inline auto trace(const T& m) {
   arena_t<T> arena_m = m;
 
-  return make_callback_var(arena_m.val_op().trace(), [arena_m](const auto& vi) mutable {
-      arena_m.adj().diagonal().array() += vi.adj();
-    });
+  return make_callback_var(arena_m.val_op().trace(),
+                           [arena_m](const auto& vi) mutable {
+                             arena_m.adj().diagonal().array() += vi.adj();
+                           });
 }
 
 }  // namespace math
