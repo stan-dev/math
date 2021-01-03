343a772309cef7fe8d62d2be3d0447aea16059a4
Simon Dirmeier <simon.dirmeier@web.de>
2021-01-03 12:15:11 +0100

    Add step varmat test

--- test/unit/math/mix/fun/step_test.cpp
+++ test/unit/math/mix/fun/step_test.cpp
@@ -1,5 +1,6 @@
 #include <test/unit/math/test_ad.hpp>
 #include <limits>
+#include <vector>
 
 TEST(mathMixScalFun, step) {
   auto f = [](const auto& x) { return stan::math::step(x); };
@@ -12,4 +13,22 @@ TEST(mathMixScalFun, step) {
   stan::test::expect_ad(f, 2713.5);
 
   stan::test::expect_ad(f, std::numeric_limits<double>::quiet_NaN());
+}
+
+TEST(mathMixMatFun, step_varmat) {
+  using stan::math::vec_concat;
+  using stan::test::expect_ad_vector_matvar;
+  using stan::test::internal::common_nonzero_args;
+  auto f = [](const auto& x) {
+    using stan::math::step;
+    return step(x);
+  };
+  std::vector<double> com_args = common_nonzero_args();
+  std::vector<double> args{-2.6, -2, -0.2, 0.7, 1.3, 3.4, 5};
+  auto all_args = vec_concat(com_args, args);
+  Eigen::VectorXd A(all_args.size());
+  for (int i = 0; i < all_args.size(); ++i) {
+    A(i) = all_args[i];
+  }
+  stan::test::expect_ad_vector_matvar(f, A);
 }
