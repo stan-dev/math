6cd747a0519a0132f224a75deb9c97389ad71771
Stan Jenkins <mc.stanislaw@gmail.com>
2021-01-04 17:12:36 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/fun/multiply.hpp
+++ stan/math/rev/fun/multiply.hpp
@@ -32,27 +32,26 @@ inline auto multiply(const T1& A, const T2& B) {
     arena_t<promote_scalar_t<var, T1>> arena_A = A;
     arena_t<promote_scalar_t<var, T2>> arena_B = B;
     using return_t
-        = return_var_matrix_t<decltype(arena_A.val_op() * arena_B.val_op()), T1, T2>;
+        = return_var_matrix_t<decltype(arena_A.val_op() * arena_B.val_op()), T1,
+                              T2>;
     arena_t<return_t> res = arena_A.val_op() * arena_B.val_op();
 
-    reverse_pass_callback(
-        [arena_A, arena_B, res]() mutable {
-          if (is_var_matrix<T1>::value || is_var_matrix<T2>::value) {
-            arena_A.adj() += res.adj_op() * arena_B.val_op().transpose();
-            arena_B.adj() += arena_A.val_op().transpose() * res.adj_op();
-          } else {
-            auto res_adj = res.adj().eval();
-            arena_A.adj() += res_adj * arena_B.val_op().transpose();
-            arena_B.adj() += arena_A.val_op().transpose() * res_adj;
-          }
-        });
+    reverse_pass_callback([arena_A, arena_B, res]() mutable {
+      if (is_var_matrix<T1>::value || is_var_matrix<T2>::value) {
+        arena_A.adj() += res.adj_op() * arena_B.val_op().transpose();
+        arena_B.adj() += arena_A.val_op().transpose() * res.adj_op();
+      } else {
+        auto res_adj = res.adj().eval();
+        arena_A.adj() += res_adj * arena_B.val_op().transpose();
+        arena_B.adj() += arena_A.val_op().transpose() * res_adj;
+      }
+    });
     return return_t(res);
   } else if (!is_constant<T2>::value) {
     arena_t<promote_scalar_t<double, T1>> arena_A = value_of(A);
     arena_t<promote_scalar_t<var, T2>> arena_B = B;
     using return_t
-        = return_var_matrix_t<decltype(arena_A * value_of(B).eval()), T1,
-                              T2>;
+        = return_var_matrix_t<decltype(arena_A * value_of(B).eval()), T1, T2>;
     arena_t<return_t> res = arena_A * arena_B.val_op();
     reverse_pass_callback([arena_B, arena_A, res]() mutable {
       arena_B.adj() += arena_A.transpose() * res.adj_op();
@@ -62,8 +61,8 @@ inline auto multiply(const T1& A, const T2& B) {
     arena_t<promote_scalar_t<var, T1>> arena_A = A;
     arena_t<promote_scalar_t<double, T2>> arena_B = value_of(B);
     using return_t
-        = return_var_matrix_t<decltype(value_of(arena_A).eval() * arena_B),
-                              T1, T2>;
+        = return_var_matrix_t<decltype(value_of(arena_A).eval() * arena_B), T1,
+                              T2>;
     arena_t<return_t> res = arena_A.val_op() * arena_B;
     reverse_pass_callback([arena_A, arena_B, res]() mutable {
       arena_A.adj() += res.adj_op() * arena_B.transpose();
