7909673239b67d36738015af53a5d0289a7a5afb
Stan Jenkins <mc.stanislaw@gmail.com>
2021-01-06 22:02:29 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/err/check_less.hpp
+++ stan/math/prim/err/check_less.hpp
@@ -45,8 +45,7 @@ struct less<T_y, T_high, true> {
           msg << ", but must be less than ";
           msg << high_vec[n];
           std::string msg_str(msg.str());
-          throw_domain_error(function, name, y_ref, "is ",
-			     msg_str.c_str());
+          throw_domain_error(function, name, y_ref, "is ", msg_str.c_str());
         }();
       }
     }
@@ -67,15 +66,15 @@ struct less<T_y, T_high, true> {
  * @throw <code>domain_error</code> if y is not less than low
  *   or if any element of y or high is NaN.
  */
-template <typename T_y, typename T_high,
-	  require_stan_scalar_t<T_y>* = nullptr,
-	  require_all_nonscalar_prim_or_rev_kernel_expression_t<T_high>* = nullptr>
+template <
+    typename T_y, typename T_high, require_stan_scalar_t<T_y>* = nullptr,
+    require_all_nonscalar_prim_or_rev_kernel_expression_t<T_high>* = nullptr>
 inline void check_less(const char* function, const char* name, const T_y& y,
                        const T_high& high) {
   internal::less<T_y, T_high, is_vector_like<T_y>::value>::check(function, name,
                                                                  y, high);
 }
-  
+
 /**
  * Check if <code>y</code> is strictly less than <code>high</code>.
  * This function is vectorized and will check each element of
@@ -190,7 +189,8 @@ inline void check_less(const char* function, const char* name, const T_y& y,
  */
 template <typename T_y, typename T_high, require_container_t<T_high>* = nullptr,
           require_stan_scalar_t<T_y>* = nullptr,
-	  require_all_not_nonscalar_prim_or_rev_kernel_expression_t<T_high>* = nullptr>
+          require_all_not_nonscalar_prim_or_rev_kernel_expression_t<
+              T_high>* = nullptr>
 inline void check_less(const char* function, const char* name, const T_y& y,
                        const T_high& high) {
   const auto& high_ref = as_array_or_scalar(to_ref(high));
