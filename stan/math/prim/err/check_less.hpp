5ea355dc9dfe71a1717ebbc4c2e90c8d74371101
Ben <bbbales2@gmail.com>
2021-01-06 16:54:35 -0500

    Added back check_less stuff (Issue #2101)

--- stan/math/prim/err/check_less.hpp
+++ stan/math/prim/err/check_less.hpp
@@ -12,6 +12,70 @@
 namespace stan {
 namespace math {
 
+namespace internal {
+template <typename T_y, typename T_high, bool is_vec>
+struct less {
+  static void check(const char* function, const char* name, const T_y& y,
+                    const T_high& high) {
+    scalar_seq_view<T_high> high_vec(high);
+    for (size_t n = 0; n < stan::math::size(high); n++) {
+      if (!(y < high_vec[n])) {
+        [&]() STAN_COLD_PATH {
+          std::stringstream msg;
+          msg << ", but must be less than ";
+          msg << high_vec[n];
+          std::string msg_str(msg.str());
+          throw_domain_error(function, name, y, "is ", msg_str.c_str());
+        }();
+      }
+    }
+  }
+};
+
+template <typename T_y, typename T_high>
+struct less<T_y, T_high, true> {
+  static void check(const char* function, const char* name, const T_y& y,
+                    const T_high& high) {
+    scalar_seq_view<T_high> high_vec(high);
+    const auto& y_ref = to_ref(y);
+    for (size_t n = 0; n < stan::math::size(y_ref); n++) {
+      if (!(stan::get(y_ref, n) < high_vec[n])) {
+        [&]() STAN_COLD_PATH {
+          std::stringstream msg;
+          msg << ", but must be less than ";
+          msg << high_vec[n];
+          std::string msg_str(msg.str());
+          throw_domain_error(function, name, y_ref, "is ",
+			     msg_str.c_str());
+        }();
+      }
+    }
+  }
+};
+}  // namespace internal
+
+/**
+ * Check if <code>y</code> is strictly less than <code>high</code>.
+ * This function is vectorized and will check each element of
+ * <code>y</code> against each element of <code>high</code>.
+ * @tparam T_y Type of y
+ * @tparam T_high Type of upper bound
+ * @param function Function name (for error messages)
+ * @param name Variable name (for error messages)
+ * @param y Variable to check
+ * @param high Upper bound
+ * @throw <code>domain_error</code> if y is not less than low
+ *   or if any element of y or high is NaN.
+ */
+template <typename T_y, typename T_high,
+	  require_stan_scalar_t<T_y>* = nullptr,
+	  require_all_nonscalar_prim_or_rev_kernel_expression_t<T_high>* = nullptr>
+inline void check_less(const char* function, const char* name, const T_y& y,
+                       const T_high& high) {
+  internal::less<T_y, T_high, is_vector_like<T_y>::value>::check(function, name,
+                                                                 y, high);
+}
+  
 /**
  * Check if <code>y</code> is strictly less than <code>high</code>.
  * This function is vectorized and will check each element of
@@ -125,7 +189,8 @@ inline void check_less(const char* function, const char* name, const T_y& y,
  *   or if any element of y or high is NaN.
  */
 template <typename T_y, typename T_high, require_container_t<T_high>* = nullptr,
-          require_stan_scalar_t<T_y>* = nullptr>
+          require_stan_scalar_t<T_y>* = nullptr,
+	  require_all_not_nonscalar_prim_or_rev_kernel_expression_t<T_high>* = nullptr>
 inline void check_less(const char* function, const char* name, const T_y& y,
                        const T_high& high) {
   const auto& high_ref = as_array_or_scalar(to_ref(high));
