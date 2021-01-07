d7124dff41dfeb7bc5fb58816d4658b21c8fdd78
Ben <bbbales2@gmail.com>
2021-01-07 08:24:18 -0500

    Added size tests to vector sequence view (Issue #2101)

--- test/unit/math/rev/fun/vector_seq_view_test.cpp
+++ test/unit/math/rev/fun/vector_seq_view_test.cpp
@@ -8,6 +8,7 @@ void test_vec_seq_var(const T& m1) {
   using stan::math::var;
   vector_seq_view<T> vsv(m1);
   EXPECT_FLOAT_EQ(m1.val()(1), vsv.val(12)(1));
+  EXPECT_EQ(vsv.size(), 1);
 
   std::vector<stan::plain_type_t<T>> v;
   v.push_back(m1);
@@ -17,6 +18,7 @@ void test_vec_seq_var(const T& m1) {
   EXPECT_FLOAT_EQ(m1.val()(0), vsv_vec.val(1)[3]);
   EXPECT_FLOAT_EQ(m1.val()(2), vsv_vec.val(1)[1]);
   EXPECT_FLOAT_EQ(m1.val()(2), vsv_vec.val(0)[2]);
+  EXPECT_EQ(vsv_vec.size(), 2);
 }
 TEST(MathMetaRev, VectorSeqViewVar) {
   Eigen::Matrix<double, -1, 1> values = Eigen::Matrix<double, -1, 1>::Random(4);
