aae1f741272f532ff7b16a54c8cacf54f19d0b86
Ben <bbbales2@gmail.com>
2021-01-11 13:05:12 -0500

    Restart tests (Issue #2101)

--- stan/math/rev/fun/to_var_value.hpp
+++ stan/math/rev/fun/to_var_value.hpp
@@ -1,8 +1,8 @@
 #ifndef STAN_MATH_REV_FUN_TO_VAR_VALUE_HPP
 #define STAN_MATH_REV_FUN_TO_VAR_VALUE_HPP
 
-#include <stan/math/rev/core.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/rev/core.hpp>
 #include <stan/math/rev/core/arena_matrix.hpp>
 #include <stan/math/rev/core/reverse_pass_callback.hpp>
 
