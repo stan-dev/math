71585daa4b6a0763bc0b06f1c53c609426d28a4b
Ben <bbbales2@gmail.com>
2021-01-11 13:33:46 -0500

    Add `dot_product` prim tests (Issue #2101)

--- test/unit/math/mix/fun/dot_product_test.cpp
+++ test/unit/math/mix/fun/dot_product_test.cpp
@@ -2,26 +2,24 @@
 #include <limits>
 #include <vector>
 
-template <class F>
-void test_dot_product(const F& f) {
-  using stan::test::relative_tolerance;
-  stan::test::ad_tolerances tols;
-  tols.hessian_hessian_ = relative_tolerance(1e-3, 1e-3);
-  tols.hessian_fvar_hessian_ = relative_tolerance(1e-3, 1e-3);
+TEST(mathMixMatFun, dotProduct) {
+  auto f = [](const auto& x, const auto& y) {
+    return stan::math::dot_product(x, y);
+  };
 
   // size 0
   Eigen::VectorXd v0;
   Eigen::RowVectorXd rv0;
   std::vector<double> sv0;
-  stan::test::expect_ad(tols, f, v0, v0);
-  stan::test::expect_ad(tols, f, v0, rv0);
-  stan::test::expect_ad(tols, f, rv0, v0);
-  stan::test::expect_ad(tols, f, rv0, rv0);
+  stan::test::expect_ad(f, v0, v0);
+  stan::test::expect_ad(f, v0, rv0);
+  stan::test::expect_ad(f, rv0, v0);
+  stan::test::expect_ad(f, rv0, rv0);
 
-  stan::test::expect_ad_matvar(tols, f, v0, v0);
-  stan::test::expect_ad_matvar(tols, f, v0, rv0);
-  stan::test::expect_ad_matvar(tols, f, rv0, v0);
-  stan::test::expect_ad_matvar(tols, f, rv0, rv0);
+  stan::test::expect_ad_matvar(f, v0, v0);
+  stan::test::expect_ad_matvar(f, v0, rv0);
+  stan::test::expect_ad_matvar(f, rv0, v0);
+  stan::test::expect_ad_matvar(f, rv0, rv0);
 
   // size 1
   Eigen::VectorXd v1(1);
@@ -32,10 +30,15 @@ void test_dot_product(const F& f) {
   v1b << 2;
   Eigen::RowVectorXd rv1b(1);
   rv1b << 2;
-  stan::test::expect_ad(tols, f, v1, v1b);
-  stan::test::expect_ad(tols, f, rv1, v1b);
-  stan::test::expect_ad(tols, f, v1, rv1b);
-  stan::test::expect_ad(tols, f, rv1, rv1b);
+  stan::test::expect_ad(f, v1, v1b);
+  stan::test::expect_ad(f, rv1, v1b);
+  stan::test::expect_ad(f, v1, rv1b);
+  stan::test::expect_ad(f, rv1, rv1b);
+
+  stan::test::expect_ad_matvar(f, v1, v1b);
+  stan::test::expect_ad_matvar(f, rv1, v1b);
+  stan::test::expect_ad_matvar(f, v1, rv1b);
+  stan::test::expect_ad_matvar(f, rv1, rv1b);
 
   // size 2
   Eigen::VectorXd v2(2);
@@ -46,10 +49,15 @@ void test_dot_product(const F& f) {
   v2b << 10, 100;
   Eigen::RowVectorXd rv2b(2);
   rv2b << 10, 100;
-  stan::test::expect_ad(tols, f, v2, v2b);
-  stan::test::expect_ad(tols, f, rv2, v2b);
-  stan::test::expect_ad(tols, f, v2, rv2b);
-  stan::test::expect_ad(tols, f, rv2, rv2b);
+  stan::test::expect_ad(f, v2, v2b);
+  stan::test::expect_ad(f, rv2, v2b);
+  stan::test::expect_ad(f, v2, rv2b);
+  stan::test::expect_ad(f, rv2, rv2b);
+
+  stan::test::expect_ad_matvar(f, v2, v2b);
+  stan::test::expect_ad_matvar(f, rv2, v2b);
+  stan::test::expect_ad_matvar(f, v2, rv2b);
+  stan::test::expect_ad_matvar(f, rv2, rv2b);
 
   // size 3
   Eigen::VectorXd v3(3);
@@ -60,20 +68,30 @@ void test_dot_product(const F& f) {
   v3b << 4, -2, -1;
   Eigen::RowVectorXd rv3b(3);
   rv3b << 4, -2, -1;
-  stan::test::expect_ad(tols, f, v3, v3b);
-  stan::test::expect_ad(tols, f, v3, rv3b);
-  stan::test::expect_ad(tols, f, rv3, v3b);
-  stan::test::expect_ad(tols, f, rv3, rv3b);
+  stan::test::expect_ad(f, v3, v3b);
+  stan::test::expect_ad(f, v3, rv3b);
+  stan::test::expect_ad(f, rv3, v3b);
+  stan::test::expect_ad(f, rv3, rv3b);
+
+  stan::test::expect_ad_matvar(f, v3, v3b);
+  stan::test::expect_ad_matvar(f, v3, rv3b);
+  stan::test::expect_ad_matvar(f, rv3, v3b);
+  stan::test::expect_ad_matvar(f, rv3, rv3b);
 
   // size 3, another case (originally from rev)
   v3 << -1, 0, 1;
   rv3 << -1, 0, 1;
   v3b << 1, 2, 3;
   rv3b << 1, 2, 3;
-  stan::test::expect_ad(tols, f, v3, v3b);
-  stan::test::expect_ad(tols, f, v3, rv3b);
-  stan::test::expect_ad(tols, f, rv3, v3b);
-  stan::test::expect_ad(tols, f, rv3, rv3b);
+  stan::test::expect_ad(f, v3, v3b);
+  stan::test::expect_ad(f, v3, rv3b);
+  stan::test::expect_ad(f, rv3, v3b);
+  stan::test::expect_ad(f, rv3, rv3b);
+
+  stan::test::expect_ad_matvar(f, v3, v3b);
+  stan::test::expect_ad_matvar(f, v3, rv3b);
+  stan::test::expect_ad_matvar(f, rv3, v3b);
+  stan::test::expect_ad_matvar(f, rv3, rv3b);
 
   // following throw due to mismatched size
   stan::test::expect_ad(f, v1, v3);
@@ -81,13 +99,11 @@ void test_dot_product(const F& f) {
   stan::test::expect_ad(f, rv1, v3);
   stan::test::expect_ad(f, rv1, rv3);
 
+  stan::test::expect_ad_matvar(f, v1, v3);
+  stan::test::expect_ad_matvar(f, v1, rv3);
+  stan::test::expect_ad_matvar(f, rv1, v3);
+  stan::test::expect_ad_matvar(f, rv1, rv3);
+
   // eliminated tests that matrix args throw in ad because
   // they don't compile in prim;  they shouldn't compile in ad, either
-}
-
-TEST(mathMixMatFun, dotProduct) {
-  auto g = [](const auto& x, const auto& y) {
-    return stan::math::dot_product(x, y);
-  };
-  test_dot_product(g);  // standard data type args
 }
