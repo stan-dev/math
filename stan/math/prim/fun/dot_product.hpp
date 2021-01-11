f5db400b6f43d12391e0a1541fd05d6e837f6993
Ben <bbbales2@gmail.com>
2021-01-11 13:00:26 -0500

    Removing `std::vector` specialization of `dot_product` and making `dot_product` work with varmat (Issue #2101)

--- stan/math/prim/fun/dot_product.hpp
+++ stan/math/prim/fun/dot_product.hpp
@@ -26,40 +26,6 @@ inline return_type_t<Vec1, Vec2> dot_product(const Vec1 &v1, const Vec2 &v2) {
   return v1.dot(v2);
 }
 
-/**
- * Returns the dot product of the specified arrays.
- *
- * @param v1 First array.
- * @param v2 Second array.
- * @param length Length of both arrays.
- */
-template <typename Scalar1, typename Scalar2,
-          typename = require_all_stan_scalar_t<Scalar1, Scalar2>,
-          typename = require_all_not_var_t<Scalar1, Scalar2>>
-inline auto dot_product(const Scalar1 *v1, const Scalar2 *v2, size_t length) {
-  return_type_t<Scalar1, Scalar2> result = 0;
-  for (size_t i = 0; i < length; i++) {
-    result += v1[i] * v2[i];
-  }
-  return result;
-}
-
-/**
- * Returns the dot product of the specified arrays.
- *
- * @param v1 First array.
- * @param v2 Second array.
- * @throw std::domain_error if the vectors are not the same size.
- */
-template <typename Scalar1, typename Scalar2, typename Alloc1, typename Alloc2,
-          typename = require_all_stan_scalar_t<Scalar1, Scalar2>,
-          typename = require_all_not_var_t<Scalar1, Scalar2>>
-inline auto dot_product(const std::vector<Scalar1, Alloc1> &v1,
-                        const std::vector<Scalar2, Alloc2> &v2) {
-  check_matching_sizes("dot_product", "v1", v1, "v2", v2);
-  return dot_product(&v1[0], &v2[0], v1.size());
-}
-
 }  // namespace math
 }  // namespace stan
 
--- test/unit/math/mix/fun/dot_product_test.cpp
+++ test/unit/math/mix/fun/dot_product_test.cpp
@@ -10,85 +10,76 @@ void test_dot_product(const F& f) {
   tols.hessian_fvar_hessian_ = relative_tolerance(1e-3, 1e-3);
 
   // size 0
-  Eigen::VectorXd v0(0);
-  Eigen::RowVectorXd rv0(0);
+  Eigen::VectorXd v0;
+  Eigen::RowVectorXd rv0;
   std::vector<double> sv0;
   stan::test::expect_ad(tols, f, v0, v0);
   stan::test::expect_ad(tols, f, v0, rv0);
   stan::test::expect_ad(tols, f, rv0, v0);
   stan::test::expect_ad(tols, f, rv0, rv0);
-  stan::test::expect_ad(tols, f, sv0, sv0);
+
+  stan::test::expect_ad_matvar(tols, f, v0, v0);
+  stan::test::expect_ad_matvar(tols, f, v0, rv0);
+  stan::test::expect_ad_matvar(tols, f, rv0, v0);
+  stan::test::expect_ad_matvar(tols, f, rv0, rv0);
 
   // size 1
   Eigen::VectorXd v1(1);
   v1 << 1;
   Eigen::RowVectorXd rv1(1);
   rv1 << 1;
-  std::vector<double> sv1{1};
   Eigen::VectorXd v1b(1);
   v1b << 2;
   Eigen::RowVectorXd rv1b(1);
   rv1b << 2;
-  std::vector<double> sv1b{2};
   stan::test::expect_ad(tols, f, v1, v1b);
   stan::test::expect_ad(tols, f, rv1, v1b);
   stan::test::expect_ad(tols, f, v1, rv1b);
   stan::test::expect_ad(tols, f, rv1, rv1b);
-  stan::test::expect_ad(tols, f, sv1, sv1b);
 
   // size 2
   Eigen::VectorXd v2(2);
   v2 << 1, 2;
   Eigen::RowVectorXd rv2(2);
   rv2 << 1, 2;
-  std::vector<double> sv2{1, 2};
   Eigen::VectorXd v2b(2);
   v2b << 10, 100;
   Eigen::RowVectorXd rv2b(2);
   rv2b << 10, 100;
-  std::vector<double> sv2b{10, 100};
   stan::test::expect_ad(tols, f, v2, v2b);
   stan::test::expect_ad(tols, f, rv2, v2b);
   stan::test::expect_ad(tols, f, v2, rv2b);
   stan::test::expect_ad(tols, f, rv2, rv2b);
-  stan::test::expect_ad(tols, f, sv2, sv2b);
 
   // size 3
   Eigen::VectorXd v3(3);
   v3 << 1, 3, -5;
   Eigen::RowVectorXd rv3(3);
   rv3 << 1, 3, -5;
-  std::vector<double> sv3{1, 3, -5};
   Eigen::VectorXd v3b(3);
   v3b << 4, -2, -1;
   Eigen::RowVectorXd rv3b(3);
   rv3b << 4, -2, -1;
-  std::vector<double> sv3b{4, -2, -1};
   stan::test::expect_ad(tols, f, v3, v3b);
   stan::test::expect_ad(tols, f, v3, rv3b);
   stan::test::expect_ad(tols, f, rv3, v3b);
   stan::test::expect_ad(tols, f, rv3, rv3b);
-  stan::test::expect_ad(tols, f, sv3, sv3b);
 
   // size 3, another case (originally from rev)
   v3 << -1, 0, 1;
   rv3 << -1, 0, 1;
-  sv3 = {-1, 0, 1};
   v3b << 1, 2, 3;
   rv3b << 1, 2, 3;
-  sv3b = {1, 2, 3};
   stan::test::expect_ad(tols, f, v3, v3b);
   stan::test::expect_ad(tols, f, v3, rv3b);
   stan::test::expect_ad(tols, f, rv3, v3b);
   stan::test::expect_ad(tols, f, rv3, rv3b);
-  stan::test::expect_ad(tols, f, sv3, sv3b);
 
   // following throw due to mismatched size
   stan::test::expect_ad(f, v1, v3);
   stan::test::expect_ad(f, v1, rv3);
   stan::test::expect_ad(f, rv1, v3);
   stan::test::expect_ad(f, rv1, rv3);
-  stan::test::expect_ad(f, sv1, sv3);
 
   // eliminated tests that matrix args throw in ad because
   // they don't compile in prim;  they shouldn't compile in ad, either
