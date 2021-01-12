f620a1e77b99d749b0507658900f30f79f983ee7
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-01-12 19:34:37 +0100

    move to class

--- stan/math/rev/core/profiling.hpp
+++ stan/math/rev/core/profiling.hpp
@@ -13,7 +13,108 @@
 
 namespace stan {
 namespace math {
+
+ /**
+ * Class used for storing profiling information.
+ */
+class profile_info {
+ private:
+  bool active_;
+  
+  double fwd_pass_time_;
+  double rev_pass_time_;
+  size_t n_fwd_AD_passes_;
+  size_t n_fwd_no_AD_passes_;
+  size_t n_rev_passes_;
+  size_t chain_stack_size_sum_;
+  size_t nochain_stack_size_sum_;
+  std::chrono::time_point<std::chrono::steady_clock> fwd_pass_tp_;
+  std::chrono::time_point<std::chrono::steady_clock> rev_pass_tp_;
+  size_t start_chain_stack_size_;
+  size_t start_nochain_stack_size_;
+ public:
+  profile_info() :
+    active_(false),
+    fwd_pass_time_(0.0), rev_pass_time_(0.0),
+    n_fwd_AD_passes_(0), n_fwd_no_AD_passes_(0), n_rev_passes_(0),
+    chain_stack_size_sum_(0), nochain_stack_size_sum_(0) { };
+
+  bool is_active() { return active_; }
+
+  template <typename T>
+  void fwd_pass_start() {
+    if (!is_constant<T>::value) {
+      start_chain_stack_size_ = ChainableStack::instance_->var_stack_.size();
+      start_nochain_stack_size_ = ChainableStack::instance_->var_nochain_stack_.size();
+    }
+    fwd_pass_tp_ = std::chrono::steady_clock::now();
+    active_ = true;
+  }
+
+  template <typename T>
+  void fwd_pass_stop() {
+    if (!is_constant<T>::value) {
+      n_fwd_AD_passes_++;
+      chain_stack_size_sum_ += (ChainableStack::instance_->var_stack_.size() - start_chain_stack_size_ - 1);
+      nochain_stack_size_sum_ += (ChainableStack::instance_->var_nochain_stack_.size() - start_nochain_stack_size_);
+    } else {
+      
+      n_fwd_no_AD_passes_++;
+    }
+    fwd_pass_time_ += 
+      std::chrono::duration<double>(std::chrono::steady_clock::now() - fwd_pass_tp_).count();
+    active_ = false;
+  }
+
+  void rev_pass_start() {
+    rev_pass_tp_ = std::chrono::steady_clock::now();
+  }
+
+  void rev_pass_stop() {
+    rev_pass_time_ += std::chrono::duration<double>(std::chrono::steady_clock::now() - rev_pass_tp_).count();
+    n_rev_passes_++;
+  }
+
+  size_t get_chain_stack_used() {
+    if (n_fwd_AD_passes_ == 0) {
+      return 0;
+    } else {
+      return chain_stack_size_sum_/n_fwd_AD_passes_;
+    }    
+  }
+
+  size_t get_nochain_stack_used() {
+    if (n_fwd_AD_passes_ == 0) {
+      return 0;
+    } else {
+      return nochain_stack_size_sum_/n_fwd_AD_passes_;
+    }    
+  }
+
+  size_t get_num_fwd_passes() {
+    return n_fwd_AD_passes_ + n_fwd_no_AD_passes_;
+  }
+
+  double get_fwd_time() {
+    return fwd_pass_time_;
+  }
+
+  size_t get_num_rev_passes() {
+    return n_rev_passes_;
+  }
+
+  double get_rev_time() {
+    return rev_pass_time_;
+  }
+
+};
+
+using profile_key = std::pair<std::string, std::thread::id>;
+
+using profile_map = std::map<profile_key, profile_info>;
 namespace internal {
+
+
 /**
  * Starts profiling the forward pass for the profile
  * with the specified name and adds a var that stops
@@ -29,23 +130,15 @@ template <typename T>
 inline auto profile_start(std::string name, profile_info& profile) {
   using std::chrono::duration;
   using std::chrono::steady_clock;
-  if (profile.meta.fwd_pass_active) {
+  if (profile.is_active()) {
     std::ostringstream msg;
-    msg << "Profile '" << name << "' ";
-    msg << "already started!";
+    msg << "Profile '" << name << "' already started!";
     throw std::runtime_error(msg.str());
   }
-  profile.meta.fwd_pass_start_tp = steady_clock::now();
-  profile.meta.fwd_pass_active = true;
+  profile.fwd_pass_start<T>();
   if (!is_constant<T>::value) {
-     profile.meta.start_chain_stack_size
-      = ChainableStack::instance_->var_stack_.size();
     reverse_pass_callback([&profile]() mutable {
-      profile.rev_pass_time += duration<double>(steady_clock::now()
-                                                - profile.meta.rev_pass_start_tp)
-                                  .count();
-      profile.meta.rev_pass_active = false;
-      profile.n_rev_pass++;
+      profile.rev_pass_stop();
     });
   } 
 }
@@ -65,20 +158,10 @@ template <typename T>
 inline auto profile_stop(std::string name, profile_info& profile) {
   using std::chrono::duration;
   using std::chrono::steady_clock;
-  profile.fwd_pass_time
-      += duration<double>(steady_clock::now() - profile.meta.fwd_pass_start_tp)
-             .count();
-  profile.meta.fwd_pass_active = false;
-  profile.n_fwd_pass++;
+  profile.fwd_pass_stop<T>();
   if (!is_constant<T>::value) {
-    profile.chain_stack_size_sum += (ChainableStack::instance_->var_stack_.size()
-                                    - profile.meta.start_chain_stack_size - 1);
-    profile.nochain_stack_size_sum
-        += (ChainableStack::instance_->var_nochain_stack_.size()
-            - profile.meta.start_nochain_stack_size);
     reverse_pass_callback([&profile]() mutable {
-      profile.meta.rev_pass_start_tp = steady_clock::now();
-      profile.meta.rev_pass_active = true;
+      profile.rev_pass_start();
     });
   }
 }
--- test/unit/math/rev/core/profiling_test.cpp
+++ test/unit/math/rev/core/profiling_test.cpp
@@ -15,12 +15,12 @@ TEST(Profiling, profile_double_basic) {
     c = a + b;
   }
   stan::math::profile_key key = {"p1", std::this_thread::get_id()};
-  EXPECT_EQ(profiles[key].chain_stack_size_sum, 0);
-  EXPECT_EQ(profiles[key].nochain_stack_size_sum, 0);
-  EXPECT_FLOAT_EQ(profiles[key].rev_pass_time, 0.0);
-  EXPECT_EQ(profiles[key].n_rev_pass, 0);
-  EXPECT_EQ(profiles[key].n_fwd_pass, 2);
-  EXPECT_TRUE(profiles[key].fwd_pass_time > 0.0);
+  EXPECT_EQ(profiles[key].get_chain_stack_used(), 0);
+  EXPECT_EQ(profiles[key].get_nochain_stack_used(), 0);
+  EXPECT_FLOAT_EQ(profiles[key].get_rev_time(), 0.0);
+  EXPECT_EQ(profiles[key].get_num_rev_passes(), 0);
+  EXPECT_EQ(profiles[key].get_num_fwd_passes(), 2);
+  EXPECT_TRUE(profiles[key].get_fwd_time() > 0.0);
 }
 
 TEST(Profiling, profiling_var_basic) {
@@ -37,11 +37,11 @@ TEST(Profiling, profiling_var_basic) {
   c.grad();
   stan::math::recover_memory();
   stan::math::profile_key key = {"t1", std::this_thread::get_id()};
-  EXPECT_EQ(profiles[key].chain_stack_size_sum, 1);
-  EXPECT_EQ(profiles[key].nochain_stack_size_sum, 2);
-  EXPECT_EQ(profiles[key].n_rev_pass, 1);
-  EXPECT_EQ(profiles[key].n_fwd_pass, 1);
-  EXPECT_TRUE(profiles[key].fwd_pass_time > 0.0);
-  EXPECT_TRUE(profiles[key].rev_pass_time > 0.0);  
+  EXPECT_EQ(profiles[key].get_chain_stack_used(), 1);
+  EXPECT_EQ(profiles[key].get_nochain_stack_used(), 2);
+  EXPECT_EQ(profiles[key].get_num_rev_passes(), 1);
+  EXPECT_EQ(profiles[key].get_num_fwd_passes(), 1);
+  EXPECT_TRUE(profiles[key].get_fwd_time() > 0.0); 
+  EXPECT_TRUE(profiles[key].get_rev_time() > 0.0);
 }
 
