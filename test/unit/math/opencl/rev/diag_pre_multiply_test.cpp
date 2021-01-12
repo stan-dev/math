3f94ca49ced23db24aaea7c7b02e67e18b238fbe
tadej <tadej.c@gmail.com>
2021-01-12 09:36:23 +0100

    completed diag_pre_multiply

--- test/unit/math/opencl/rev/diag_pre_multiply_test.cpp
+++ test/unit/math/opencl/rev/diag_pre_multiply_test.cpp
@@ -8,8 +8,16 @@
 auto diag_pre_multiply_functor
     = [](const auto& a, const auto& b) { return stan::math::diag_pre_multiply(a, b); };
 
-TEST(OpenCL_diag_pre_multiply, diag_pre_multiply_small) {
+TEST(OpenCL_diag_pre_multiply, diag_pre_multiply_small_vector) {
   Eigen::VectorXd in1(4);
+  in1 << 0.5, 3.4, 5.2, 7.5;
+  Eigen::MatrixXd in2(4,2);
+  in2 << 3.3, 0.9, 6.7, 1.8,1,2,3,4;
+  stan::math::test::compare_cpu_opencl_prim_rev(diag_pre_multiply_functor, in1, in2);
+}
+
+TEST(OpenCL_diag_pre_multiply, diag_pre_multiply_small_row_vector) {
+  Eigen::RowVectorXd in1(4);
   in1 << 0.5, 3.4, 5.2, 7.5;
   Eigen::MatrixXd in2(4,2);
   in2 << 3.3, 0.9, 6.7, 1.8,1,2,3,4;
