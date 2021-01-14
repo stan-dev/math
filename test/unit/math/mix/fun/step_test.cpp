95439f89b560f29890a34a3bd2e67b12b9a3b6f1
Ben <bbbales2@gmail.com>
2021-01-14 11:40:31 -0500

    Moved step function tests (Issue #2101)

--- test/unit/math/mix/fun/step_test.cpp
+++ test/unit/math/mix/fun/step_test.cpp
@@ -4,13 +4,8 @@
 
 TEST(mathMixScalFun, step) {
   auto f = [](const auto& x) { return stan::math::step(x); };
-  stan::test::expect_ad(f, -18765.3);
-  stan::test::expect_ad(f, -1.0);
-  stan::test::expect_ad(f, -0.001);
-  stan::test::expect_ad(f, 0.001);
-  stan::test::expect_ad(f, 1.0);
-  stan::test::expect_ad(f, 3.5);
-  stan::test::expect_ad(f, 2713.5);
+
+  stan::test::expect_common_prim([](auto x) { return x < 0.0 ? 0 : 1; }, f);
 
   stan::test::expect_ad(f, std::numeric_limits<double>::quiet_NaN());
 }
