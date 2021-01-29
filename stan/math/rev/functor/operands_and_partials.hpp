bc6083d9503c94f87553debd7e28ec3c8a64caae
Stan Jenkins <mc.stanislaw@gmail.com>
2021-01-29 23:44:00 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/functor/operands_and_partials.hpp
+++ stan/math/rev/functor/operands_and_partials.hpp
@@ -28,15 +28,16 @@ template <typename Scalar1, typename Scalar2, require_var_t<Scalar1>* = nullptr>
 inline void accumulate_adjoints(Scalar1&& x, Scalar2&& y, const var& z) {
   x.adj() += z.adj() * y;
 }
-template <typename EigT1, typename EigT2, require_rev_matrix_t<EigT1>* = nullptr>
+template <typename EigT1, typename EigT2,
+          require_rev_matrix_t<EigT1>* = nullptr>
 inline void accumulate_adjoints(EigT1&& x, EigT2&& y, const var& z) {
   x.adj().array() += z.adj() * y.array();
 }
 template <typename Arith, typename Alt, require_st_arithmetic<Arith>* = nullptr>
-inline void accumulate_adjoints(Arith&& /* x */, Alt&& /* y */, const var& z) {
-}
+inline void accumulate_adjoints(Arith&& /* x */, Alt&& /* y */, const var& z) {}
 
-template <typename StdVec1, typename StdVec2, require_std_vector_t<StdVec1>* = nullptr>
+template <typename StdVec1, typename StdVec2,
+          require_std_vector_t<StdVec1>* = nullptr>
 inline void accumulate_adjoints(StdVec1&& x, StdVec2&& y, const var& z) {
   for (size_t i = 0; i < x.size(); ++i) {
     accumulate_adjoints(x[i], y[i], z);
@@ -74,13 +75,15 @@ inline void accumulate_adjoints(StdVec1&& x, StdVec2&& y, const var& z) {
  * @tparam Ops Type of the operands placed into the edges
  * @tparam ReturnType The type returned from the `build()` method.
  */
- template <typename ReturnType, typename... Ops>
- class operands_and_partials_impl<ReturnType, require_var_t<ReturnType>, Ops...> {
+template <typename ReturnType, typename... Ops>
+class operands_and_partials_impl<ReturnType, require_var_t<ReturnType>,
+                                 Ops...> {
  public:
-  std::tuple<internal::ops_partials_edge<double, plain_type_t<std::decay_t<Ops>>>...> edges_;
+  std::tuple<
+      internal::ops_partials_edge<double, plain_type_t<std::decay_t<Ops>>>...>
+      edges_;
   template <typename... Types>
-  explicit operands_and_partials_impl(Types&&... ops) :
-    edges_(ops...) {}
+  explicit operands_and_partials_impl(Types&&... ops) : edges_(ops...) {}
 
   /** \ingroup type_trait
    * Build the node to be stored on the autodiff graph.
@@ -96,21 +99,22 @@ inline void accumulate_adjoints(StdVec1&& x, StdVec2&& y, const var& z) {
    * @return the node to be stored in the expression graph for autodiff
    */
   inline var build(double value) {
-    auto operands_tuple = stan::math::apply([](auto&&... edges) {
-      return std::make_tuple(edges.operand()...);
-    }, edges_);
-    auto partials_tuple = stan::math::apply([](auto&&... edges) {
-      return std::make_tuple(edges.partial()...);
-    }, edges_);
+    auto operands_tuple = stan::math::apply(
+        [](auto&&... edges) { return std::make_tuple(edges.operand()...); },
+        edges_);
+    auto partials_tuple = stan::math::apply(
+        [](auto&&... edges) { return std::make_tuple(edges.partial()...); },
+        edges_);
     var ret(value);
-    for_each([ret](auto& operand, auto& partial) mutable {
-      reverse_pass_callback([operand, partial, ret]() mutable {
-        accumulate_adjoints(operand, partial, ret);
-      });
-    }, operands_tuple, partials_tuple);
+    for_each(
+        [ret](auto& operand, auto& partial) mutable {
+          reverse_pass_callback([operand, partial, ret]() mutable {
+            accumulate_adjoints(operand, partial, ret);
+          });
+        },
+        operands_tuple, partials_tuple);
     return ret;
   }
-
 };
 
 /** \ingroup type_trait
@@ -121,20 +125,14 @@ class ops_partials_edge<double, var> {
  public:
   double partial_{0};
   broadcast_array<double> partials_{partial_};
-  explicit ops_partials_edge(const var& op) noexcept
-      : operands_(op) {}
+  explicit ops_partials_edge(const var& op) noexcept : operands_(op) {}
 
-  inline auto& partial() {
-    return partial_;
-  }
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& partial() { return partial_; }
+  inline auto& operand() noexcept { return operands_; }
 
   var operands_;
   static constexpr int size() { return 1; }
 };
-
 
 // Vectorized Univariate
 template <>
@@ -148,12 +146,8 @@ class ops_partials_edge<double, std::vector<var>> {
       : partials_(Eigen::VectorXd::Zero(op.size())),
         partials_vec_(partials_),
         operands_(to_arena(op)) {}
-  inline auto& partial() noexcept {
-    return partials_;
-  }
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& partial() noexcept { return partials_; }
+  inline auto& operand() noexcept { return operands_; }
   inline int size() const noexcept { return this->operands_.size(); }
   Op operands_;
 };
@@ -169,12 +163,8 @@ class ops_partials_edge<double, Op, require_eigen_st<is_var, Op>> {
         partials_vec_(partials_),
         operands_(to_arena(ops)) {}
 
-  inline auto& partial() noexcept {
-    return partials_;
-  }
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& partial() noexcept { return partials_; }
+  inline auto& operand() noexcept { return operands_; }
   arena_t<Op> operands_;
   inline auto size() const noexcept { return this->operands_.size(); }
 };
@@ -190,12 +180,8 @@ class ops_partials_edge<double, var_value<Op>, require_eigen_t<Op>> {
             plain_type_t<partials_t>::Zero(ops.vi_->rows(), ops.vi_->cols())),
         partials_vec_(partials_),
         operands_(ops) {}
-  inline auto& partial() noexcept {
-    return partials_;
-  }
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& partial() noexcept { return partials_; }
+  inline auto& operand() noexcept { return operands_; }
 
   const var_value<Op>& operands_;
   static constexpr int size() { return 0; }
@@ -216,12 +202,8 @@ class ops_partials_edge<double, std::vector<Eigen::Matrix<var, R, C>>> {
     }
   }
 
-  inline auto& partial() noexcept {
-    return partials_vec_;
-  }
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& partial() noexcept { return partials_vec_; }
+  inline auto& operand() noexcept { return operands_; }
   inline int size() const noexcept {
     if (unlikely(this->operands_.size() == 0)) {
       return 0;
@@ -240,16 +222,13 @@ class ops_partials_edge<double, std::vector<std::vector<var>>> {
   explicit ops_partials_edge(const std::vector<std::vector<var>>& ops)
       : partials_vec_(stan::math::size(ops)), operands_(to_arena(ops)) {
     for (size_t i = 0; i < stan::math::size(ops); ++i) {
-      partials_vec_[i] = to_arena(Eigen::Matrix<double, -1, 1>(stan::math::size(ops[i])));
+      partials_vec_[i]
+          = to_arena(Eigen::Matrix<double, -1, 1>(stan::math::size(ops[i])));
     }
   }
-  inline auto& partial() noexcept {
-    return partials_vec_;
-  }
+  inline auto& partial() noexcept { return partials_vec_; }
 
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& operand() noexcept { return operands_; }
   inline int size() const noexcept {
     if (unlikely(this->operands_.size() == 0)) {
       return 0;
@@ -273,12 +252,8 @@ class ops_partials_edge<double, std::vector<var_value<Op>>,
           = plain_type_t<Op>::Zero(ops[i].vi_->rows(), ops[i].vi_->cols());
     }
   }
-  inline auto& partial() noexcept {
-    return partials_vec_;
-  }
-  inline auto& operand() noexcept {
-    return operands_;
-  }
+  inline auto& partial() noexcept { return partials_vec_; }
+  inline auto& operand() noexcept { return operands_; }
   static constexpr int size() noexcept { return 0; }
   arena_t<std::vector<var_value<Op>>> operands_;
 };
