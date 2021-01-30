33915cdf0fe0583a097124c479c754f271de6329
Stan Jenkins <mc.stanislaw@gmail.com>
2021-01-30 05:22:20 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/prim/functor/operands_and_partials_test.cpp
+++ test/unit/math/prim/functor/operands_and_partials_test.cpp
@@ -8,7 +8,7 @@ TEST(MathMetaPrim, OperandsAndPartials) {
   auto o2 = operands_and_partials(2.0, 3.0, 4.0, 5.0);
 
   // TODO(Sean): Learn why it's size 6
-  //EXPECT_GT(sizeof(&o2), sizeof(o2));
+  // EXPECT_GT(sizeof(&o2), sizeof(o2));
 
   EXPECT_FLOAT_EQ(27.1, o1.build(27.1));
 }
