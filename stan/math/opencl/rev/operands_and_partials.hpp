ca0290495e91a3dead64f881b524cd49dd8f4d8a
Steve Bronder <stevo15025@gmail.com>
2021-02-05 17:02:23 -0500

    make operand const ref in opencl edge

--- stan/math/opencl/rev/operands_and_partials.hpp
+++ stan/math/opencl/rev/operands_and_partials.hpp
@@ -24,8 +24,8 @@ class ops_partials_edge<double, var_value<Op>, require_matrix_cl_t<Op>> {
         partials_vec_(partials_),
         operands_(ops) {}
   inline partials_t& partial() noexcept { return partials_; }
-  inline var_value<Op>& operand() noexcept { return operands_; }
-  var_value<Op> operands_;
+  inline const var_value<Op>& operand() const noexcept { return operands_; }
+  const var_value<Op>& operands_;
   static constexpr int size() { return 0; }
 };
 
