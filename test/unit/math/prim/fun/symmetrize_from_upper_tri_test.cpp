1852ad70475515c039bbb8f99a0002c7a20f8f59
HP01\Gregor <gregor.pirs@fri.uni-lj.si>
2021-02-05 14:46:49 +0100

    Added test for symmetrize_from_upper_tri.

---
+++ test/unit/math/prim/fun/symmetrize_from_upper_tri_test.cpp
@@ -0,0 +1,27 @@
+#include <stan/math/prim.hpp>
+#include <gtest/gtest.h>
+
+TEST(Symmetrize, Error) {
+  using Eigen::Dynamic;
+  using Eigen::Matrix;
+  using stan::math::symmetrize_from_upper_tri;
+
+  Matrix<double, Dynamic, Dynamic> m(3, 4);
+  for (int i = 0; i < 3; ++i)
+    for (int j = 0; j < 4; ++j)
+      m(i, j) = (i + 2) * (j + 1);
+  EXPECT_THROW(symmetrize_from_upper_tri(m), std::invalid_argument);
+}
+
+TEST(Symmetrize, Value) {
+  using Eigen::Dynamic;
+  using Eigen::Matrix;
+  using stan::math::is_symmetric;
+  using stan::math::symmetrize_from_upper_tri;
+
+  Matrix<double, Dynamic, Dynamic> m(4, 4);
+  for (int i = 0; i < 4; ++i)
+    for (int j = 0; j < 4; ++j)
+      m(i, j) = (i + 2) * (j + 1);
+  EXPECT_TRUE(is_symmetric(symmetrize_from_upper_tri(m)));
+}
