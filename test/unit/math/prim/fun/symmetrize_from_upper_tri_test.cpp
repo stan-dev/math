7220e275c56b626f6e5246650caf48ce65fe5be8
HP01\Gregor <gregor.pirs@fri.uni-lj.si>
2021-02-09 16:00:31 +0100

    Added equality test for symmetrize_from_upper_tri.

--- test/unit/math/prim/fun/symmetrize_from_upper_tri_test.cpp
+++ test/unit/math/prim/fun/symmetrize_from_upper_tri_test.cpp
@@ -1,4 +1,5 @@
 #include <stan/math/prim.hpp>
+#include <test/unit/util.hpp>
 #include <gtest/gtest.h>
 
 TEST(Symmetrize_upper, Error) {
@@ -24,4 +25,23 @@ TEST(Symmetrize_upper, Value) {
     for (int j = 0; j < 4; ++j)
       m(i, j) = (i + 2) * (j + 1);
   EXPECT_TRUE(is_symmetric(symmetrize_from_upper_tri(m)));
+}
+
+TEST(Symmetrize_upper, Value2) {
+  using Eigen::Dynamic;
+  using Eigen::Matrix;
+	using Eigen::MatrixXd;
+  using stan::math::is_symmetric;
+  using stan::math::symmetrize_from_upper_tri;
+	
+  Matrix<double, Dynamic, Dynamic> m(4, 4);
+  for (int i = 0; i < 4; ++i)
+    for (int j = 0; j < 4; ++j)
+      m(i, j) = (i + 2) * (j + 1);
+	MatrixXd m_sym_ref(4,4);
+	m_sym_ref << 2, 4, 6, 8,
+							 4, 6, 9, 12,
+							 6, 9, 12, 16,
+							 8, 12, 16, 20;
+  EXPECT_MATRIX_EQ(m_sym_ref, symmetrize_from_upper_tri(m));
 }
