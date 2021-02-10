19192b3d5dcab49384f758ed06da07c610c3ae4d
Ben <bbbales2@gmail.com>
2021-02-10 09:17:13 -0500

    Simplified mix tests a bit (Issue #482)

--- test/unit/math/mix/fun/symmetrize_from_upper_tri_test.cpp
+++ test/unit/math/mix/fun/symmetrize_from_upper_tri_test.cpp
@@ -1,36 +1,21 @@
 #include <test/unit/math/test_ad.hpp>
 
-void expect_upper_sym_tri(const Eigen::MatrixXd& m) {
-  auto f
-      = [](const auto& x) { return stan::math::symmetrize_from_upper_tri(x); };
-  Eigen::VectorXd v = stan::test::to_vector(m);
-  Eigen::RowVectorXd rv = stan::test::to_row_vector(m);
-  stan::test::expect_ad(f, m);
-  stan::test::expect_ad(f, v);
-  stan::test::expect_ad(f, rv);
-}
-
 TEST(MathMixMatFun, symmetrize_from_upper_tri) {
+  auto f = [](const auto& x) { return stan::math::symmetrize_from_upper_tri(x); };
+
   Eigen::MatrixXd a(0, 0);
-  expect_upper_sym_tri(a);
+  stan::test::expect_ad(f, a);
 
   Eigen::MatrixXd b(1, 1);
   b << -1.2;
-  expect_upper_sym_tri(b);
+  stan::test::expect_ad(f, b);
 
   Eigen::MatrixXd c(2, 2);
   c << -1, 2, 5, 10;
-  expect_upper_sym_tri(c);
+  stan::test::expect_ad(f, c);
 
+  // Test invalid sizes
   Eigen::MatrixXd d(2, 3);
   d << -1, 2, -3, 5, 10, 100;
-  expect_upper_sym_tri(d);
-
-  Eigen::MatrixXd e(3, 1);
-  e << 1, 2, 3;
-  expect_upper_sym_tri(e);
-
-  Eigen::MatrixXd g(3, 3);
-  g << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-  expect_upper_sym_tri(g);
+  stan::test::expect_ad(f, d);
 }
