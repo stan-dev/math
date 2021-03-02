0e07974a1e370b34f44afa7d98348930a8074400
Adam Haber <adamhaber@gmail.com>
2021-03-02 23:20:57 +0200

    use boost quantile function instead of reinventing stuff

--- stan/math/prim/fun/quantile.hpp
+++ stan/math/prim/fun/quantile.hpp
@@ -5,6 +5,14 @@
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
 #include <stan/math/prim/fun/as_array_or_scalar.hpp>
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/tail_quantile.hpp>
+#include <boost/accumulators/statistics/p_square_quantile.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+#include <boost/accumulators/statistics/covariance.hpp>
+#include <boost/accumulators/statistics/variates/covariate.hpp>
 #include <algorithm>
 #include <vector>
 
@@ -25,22 +33,38 @@ namespace math {
  * @return Sample quantile.
  * @throw std::domain_error If any of the values are NaN.
  */
-template <typename T>
-inline T quantile(const std::vector<T>& xs, double p) {
+template <typename T, require_vector_t<T>* = nullptr>
+inline double quantile(const T& xs, const double p) {
+  using boost::accumulators::accumulator_set;
+  using boost::accumulators::left;
+  using boost::accumulators::quantile;
+  using boost::accumulators::quantile_probability;
+  using boost::accumulators::right;
+  using boost::accumulators::stats;
+  using boost::accumulators::tag::tail;
+  using boost::accumulators::tag::tail_quantile;
+
   check_not_nan("quantile", "container argument", xs);
   check_bounded("quantile", "p", p, 0, 1);
-  size_t n_sample = xs.size();
-  if (n_sample == 1)
-    return xs[0];
-  if (p == 0.)
-    return *std::min_element(xs.begin(), xs.end());
-  if (p == 1.)
-    return *std::max_element(xs.begin(), xs.end());
-  size_t loc = std::floor(xs.size() * p);
 
-  std::vector<T> sorted(loc + 1);
-  std::partial_sort_copy(xs.begin(), xs.end(), sorted.begin(), sorted.end());
-  return sorted[loc];
+  const auto& x = as_array_or_scalar(xs);
+
+  double M = x.rows();
+
+  size_t cache_size = M;
+
+  if (p < 0.5) {
+    accumulator_set<double, stats<tail_quantile<left> > > acc(
+        tail<left>::cache_size = cache_size);
+    for (int i = 0; i < M; i++)
+      acc(x(i));
+    return quantile(acc, quantile_probability = p);
+  }
+  accumulator_set<double, stats<tail_quantile<right> > > acc(
+      tail<right>::cache_size = cache_size);
+  for (int i = 0; i < M; i++)
+    acc(x(i));
+  return quantile(acc, quantile_probability = p);
 }
 
 }  // namespace math
