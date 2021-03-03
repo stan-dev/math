1e1bd589fdc527465ebe79339d45e6b93e67f54b
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-03 12:21:03 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/opencl/ref_type_test.cpp
+++ test/unit/math/opencl/ref_type_test.cpp
@@ -5,8 +5,8 @@
 #include <gtest/gtest.h>
 
 TEST(MathMetaPrim, ref_type_matrix_cl) {
-  using stan::math::matrix_cl;
   using stan::ref_type_t;
+  using stan::math::matrix_cl;
   Eigen::MatrixXd a_eig(3, 3);
   a_eig << 1, 2, 3, 4, 5, 6, 7, 8, 9;
   matrix_cl<double> a(a_eig);
@@ -19,19 +19,15 @@ TEST(MathMetaPrim, ref_type_matrix_cl) {
   EXPECT_MATRIX_EQ(from_matrix_cl(a_ref2), from_matrix_cl(a));
   EXPECT_MATRIX_EQ(from_matrix_cl(a_ref3), from_matrix_cl(a));
 
-  EXPECT_TRUE(
-      (std::is_same<decltype(a), ref_type_t<decltype(a)&&>>::value));
-  EXPECT_TRUE(std::is_lvalue_reference<
-              ref_type_t<matrix_cl<double>>>::value);
-  EXPECT_TRUE(std::is_lvalue_reference<
-              ref_type_t<matrix_cl<double>&>>::value);
-  EXPECT_FALSE(
-      std::is_reference<ref_type_t<matrix_cl<double>&&>>::value);
+  EXPECT_TRUE((std::is_same<decltype(a), ref_type_t<decltype(a)&&>>::value));
+  EXPECT_TRUE(std::is_lvalue_reference<ref_type_t<matrix_cl<double>>>::value);
+  EXPECT_TRUE(std::is_lvalue_reference<ref_type_t<matrix_cl<double>&>>::value);
+  EXPECT_FALSE(std::is_reference<ref_type_t<matrix_cl<double>&&>>::value);
 }
 
 TEST(MathMetaPrim, ref_type_kg_light_expression) {
-  using stan::math::matrix_cl;
   using stan::ref_type_t;
+  using stan::math::matrix_cl;
   Eigen::MatrixXd m_eig(3, 3);
   m_eig << 1, 2, 3, 4, 5, 6, 7, 8, 9;
   matrix_cl<double> m(m_eig);
@@ -47,15 +43,13 @@ TEST(MathMetaPrim, ref_type_kg_light_expression) {
   EXPECT_MATRIX_EQ(from_matrix_cl(a_ref3), from_matrix_cl(a_eval));
 
   EXPECT_FALSE((stan::is_matrix_cl<ref_type_t<decltype(a)>>::value));
-  EXPECT_FALSE(
-      (stan::is_matrix_cl<ref_type_t<decltype(a)&>>::value));
-  EXPECT_FALSE(
-      (stan::is_matrix_cl<ref_type_t<decltype(a)&&>>::value));
+  EXPECT_FALSE((stan::is_matrix_cl<ref_type_t<decltype(a)&>>::value));
+  EXPECT_FALSE((stan::is_matrix_cl<ref_type_t<decltype(a)&&>>::value));
 }
 
 TEST(MathMetaPrim, ref_type_kg_heavy_expression) {
-  using stan::math::matrix_cl;
   using stan::ref_type_t;
+  using stan::math::matrix_cl;
   Eigen::MatrixXd m_eig(3, 3);
   m_eig << 1, 2, 3, 4, 5, 6, 7, 8, 9;
   matrix_cl<double> m(m_eig);
