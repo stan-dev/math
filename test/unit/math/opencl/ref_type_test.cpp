bff1a2a16fb7a4df2b0da2e36abc16c5b1b1b17e
tadej <tadej.c@gmail.com>
2021-03-03 13:16:26 +0100

    added OpenCL ref_type

--- test/unit/math/opencl/ref_type_test.cpp
+++ test/unit/math/opencl/ref_type_test.cpp
@@ -1,158 +1,80 @@
 #ifdef STAN_OPENCL
 #include <stan/math/opencl/prim.hpp>
 #include <stan/math/prim.hpp>
-
 #include <test/unit/util.hpp>
 #include <gtest/gtest.h>
-#include <stan/math/opencl/pinned_matrix.hpp>
 
-TEST(MathMetaPrim, ref_type_for_opencl_for_opencl_non_eigen) {
-  using stan::math::ref_type_for_opencl_t;
-  std::vector<int> a{1, 2, 3};
-  ref_type_for_opencl_t<std::vector<int>> a_ref1 = a;
-  ref_type_for_opencl_t<std::vector<int>&> a_ref2 = a;
-  ref_type_for_opencl_t<std::vector<int>&&> a_ref3 = std::vector<int>{1, 2, 3};
+TEST(MathMetaPrim, ref_type_matrix_cl) {
+  using stan::math::matrix_cl;
+  using stan::ref_type_t;
+  Eigen::MatrixXd a_eig(3, 3);
+  a_eig << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+  matrix_cl<double> a(a_eig);
+  matrix_cl<double> a2 = a;
+  ref_type_t<matrix_cl<double>> a_ref1 = a;
+  ref_type_t<matrix_cl<double>&> a_ref2 = a;
+  ref_type_t<matrix_cl<double>&&> a_ref3 = std::move(a2);
 
-  double b = 3;
-  ref_type_for_opencl_t<double> b_ref1 = b;
-  ref_type_for_opencl_t<double&> b_ref2 = b;
-  ref_type_for_opencl_t<double&&> b_ref3 = 3;
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref1), from_matrix_cl(a));
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref2), from_matrix_cl(a));
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref3), from_matrix_cl(a));
 
-  const std::vector<double> c{0.5, 4, 0.7};
-  ref_type_for_opencl_t<const std::vector<double>> c_ref1 = c;
-  ref_type_for_opencl_t<const std::vector<double>&> c_ref2 = c;
-
-  EXPECT_STD_VECTOR_FLOAT_EQ(a_ref1, a);
-  EXPECT_STD_VECTOR_FLOAT_EQ(a_ref2, a);
-  EXPECT_STD_VECTOR_FLOAT_EQ(a_ref3, a);
-  EXPECT_EQ(b_ref1, b);
-  EXPECT_EQ(b_ref2, b);
-  EXPECT_EQ(b_ref3, b);
-  EXPECT_STD_VECTOR_FLOAT_EQ(c_ref1, c);
-  EXPECT_STD_VECTOR_FLOAT_EQ(c_ref2, c);
-  EXPECT_TRUE(std::is_lvalue_reference<ref_type_for_opencl_t<double>>::value);
-  EXPECT_TRUE(std::is_lvalue_reference<ref_type_for_opencl_t<double&>>::value);
-  EXPECT_FALSE(std::is_reference<ref_type_for_opencl_t<double&&>>::value);
+  EXPECT_TRUE(
+      (std::is_same<decltype(a), ref_type_t<decltype(a)&&>>::value));
   EXPECT_TRUE(std::is_lvalue_reference<
-              ref_type_for_opencl_t<const std::vector<double>>>::value);
+              ref_type_t<matrix_cl<double>>>::value);
   EXPECT_TRUE(std::is_lvalue_reference<
-              ref_type_for_opencl_t<const std::vector<double>&>>::value);
-  EXPECT_FALSE(std::is_reference<
-               ref_type_for_opencl_t<const std::vector<double>&&>>::value);
-}
-
-TEST(MathMetaPrim, ref_type_for_opencl_eigen_contiguous) {
-  using stan::math::ref_type_for_opencl_t;
-  Eigen::MatrixXd a(3, 3);
-  a << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-  Eigen::MatrixXd a2 = a;
-  ref_type_for_opencl_t<Eigen::MatrixXd> a_ref1 = a;
-  ref_type_for_opencl_t<Eigen::MatrixXd&> a_ref2 = a;
-  ref_type_for_opencl_t<Eigen::MatrixXd&&> a_ref3 = std::move(a2);
-
-  auto b = a.leftCols(2);
-  ref_type_for_opencl_t<decltype(b)> b_ref1 = b;
-  ref_type_for_opencl_t<decltype(b)&> b_ref2 = b;
-  ref_type_for_opencl_t<decltype(b)&&> b_ref3 = a.leftCols(2);
-
-  using ContiguousMap = Eigen::Map<Eigen::MatrixXd, 0, Eigen::Stride<0, 0>>;
-  ContiguousMap c(a.data(), 3, 3);
-  ContiguousMap c2(a.data(), 3, 3);
-  ref_type_for_opencl_t<ContiguousMap> c_ref1 = c;
-  ref_type_for_opencl_t<ContiguousMap&> c_ref2 = c;
-  ref_type_for_opencl_t<ContiguousMap&&> c_ref3 = std::move(c2);
-
-  EXPECT_MATRIX_EQ(a_ref1, a);
-  EXPECT_MATRIX_EQ(a_ref2, a);
-  EXPECT_MATRIX_EQ(a_ref3, a);
-
-  EXPECT_MATRIX_EQ(b_ref1, b);
-  EXPECT_MATRIX_EQ(b_ref2, b);
-  EXPECT_MATRIX_EQ(b_ref3, b);
-
-  EXPECT_MATRIX_EQ(c_ref1, c);
-  EXPECT_MATRIX_EQ(c_ref2, c);
-  EXPECT_MATRIX_EQ(c_ref3, c);
-  EXPECT_TRUE(
-      (std::is_same<decltype(a), ref_type_for_opencl_t<decltype(a)&&>>::value));
-  EXPECT_TRUE(
-      (std::is_same<decltype(b), ref_type_for_opencl_t<decltype(b)&&>>::value));
-  EXPECT_TRUE(
-      (std::is_same<decltype(c), ref_type_for_opencl_t<decltype(c)&&>>::value));
-  EXPECT_TRUE(
-      std::is_lvalue_reference<ref_type_for_opencl_t<Eigen::MatrixXd>>::value);
-  EXPECT_TRUE(
-      std::is_lvalue_reference<ref_type_for_opencl_t<Eigen::MatrixXd&>>::value);
+              ref_type_t<matrix_cl<double>&>>::value);
   EXPECT_FALSE(
-      std::is_reference<ref_type_for_opencl_t<Eigen::MatrixXd&&>>::value);
+      std::is_reference<ref_type_t<matrix_cl<double>&&>>::value);
 }
 
-TEST(MathMetaPrim, ref_type_for_opencl_eigen_non_contiguous) {
-  using stan::math::ref_type_for_opencl_t;
-  Eigen::MatrixXd m(3, 3);
-  m << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-  using RowMajorMatrixXd
-      = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-  RowMajorMatrixXd a = m;
-  RowMajorMatrixXd a2 = m;
-  ref_type_for_opencl_t<RowMajorMatrixXd> a_ref1 = a;
-  ref_type_for_opencl_t<RowMajorMatrixXd&> a_ref2 = a;
-  ref_type_for_opencl_t<RowMajorMatrixXd&&> a_ref3 = std::move(a2);
+TEST(MathMetaPrim, ref_type_kg_light_expression) {
+  using stan::math::matrix_cl;
+  using stan::ref_type_t;
+  Eigen::MatrixXd m_eig(3, 3);
+  m_eig << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+  matrix_cl<double> m(m_eig);
+  auto a = stan::math::block_zero_based(m, 0, 1, 2, 2);
+  ref_type_t<decltype(a)> a_ref1 = a;
+  ref_type_t<decltype(a)&> a_ref2 = a;
+  ref_type_t<decltype(a)&&> a_ref3
+      = stan::math::block_zero_based(m, 0, 1, 2, 2);
 
-  auto b = m.block(1, 0, 2, 2);
-  ref_type_for_opencl_t<decltype(b)> b_ref1 = b;
-  ref_type_for_opencl_t<decltype(b)&> b_ref2 = b;
-  ref_type_for_opencl_t<decltype(b)&&> b_ref3 = a.block(1, 0, 2, 2);
+  matrix_cl<double> a_eval = a;
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref1), from_matrix_cl(a_eval));
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref2), from_matrix_cl(a_eval));
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref3), from_matrix_cl(a_eval));
 
-  Eigen::Ref<Eigen::MatrixXd> c = m;
-  Eigen::Ref<Eigen::MatrixXd> c2 = m;
-  ref_type_for_opencl_t<Eigen::Ref<Eigen::MatrixXd>> c_ref1 = c;
-  ref_type_for_opencl_t<Eigen::Ref<Eigen::MatrixXd>&> c_ref2 = c;
-  ref_type_for_opencl_t<Eigen::Ref<Eigen::MatrixXd>&&> c_ref3 = std::move(c2);
-
-  EXPECT_MATRIX_EQ(a_ref1, a);
-  EXPECT_MATRIX_EQ(a_ref2, a);
-  EXPECT_MATRIX_EQ(a_ref3, a);
-
-  EXPECT_MATRIX_EQ(b_ref1, b);
-  EXPECT_MATRIX_EQ(b_ref2, b);
-  EXPECT_MATRIX_EQ(b_ref3, b);
-
-  EXPECT_MATRIX_EQ(c_ref1, c);
-  EXPECT_MATRIX_EQ(c_ref2, c);
-  EXPECT_MATRIX_EQ(c_ref3, c);
-  EXPECT_TRUE((std::is_same<stan::math::pinned_matrix<Eigen::MatrixXd>,
-                            ref_type_for_opencl_t<decltype(a)&&>>::value));
-  EXPECT_TRUE((std::is_same<stan::math::pinned_matrix<Eigen::MatrixXd>,
-                            ref_type_for_opencl_t<decltype(b)&&>>::value));
-  EXPECT_TRUE((std::is_same<stan::math::pinned_matrix<Eigen::MatrixXd>,
-                            ref_type_for_opencl_t<decltype(c)&&>>::value));
+  EXPECT_FALSE((stan::is_matrix_cl<ref_type_t<decltype(a)>>::value));
+  EXPECT_FALSE(
+      (stan::is_matrix_cl<ref_type_t<decltype(a)&>>::value));
+  EXPECT_FALSE(
+      (stan::is_matrix_cl<ref_type_t<decltype(a)&&>>::value));
 }
 
-TEST(MathMetaPrim, ref_type_for_opencl_eigen_expression) {
-  using stan::plain_type_t;
-  using stan::math::ref_type_for_opencl_t;
-  Eigen::MatrixXd m(3, 3);
-  m << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+TEST(MathMetaPrim, ref_type_kg_heavy_expression) {
+  using stan::math::matrix_cl;
+  using stan::ref_type_t;
+  Eigen::MatrixXd m_eig(3, 3);
+  m_eig << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+  matrix_cl<double> m(m_eig);
   auto a = m * 3;
-  ref_type_for_opencl_t<decltype(a)> a_ref1 = a;
-  ref_type_for_opencl_t<decltype(a)&> a_ref2 = a;
-  ref_type_for_opencl_t<decltype(a)&&> a_ref3 = m * 3;
+  ref_type_t<decltype(a)> a_ref1 = a;
+  ref_type_t<decltype(a)&> a_ref2 = a;
+  ref_type_t<decltype(a)&&> a_ref3 = m * 3;
 
-  Eigen::MatrixXd a_eval = a;
-  EXPECT_MATRIX_EQ(a_ref1, a_eval);
-  EXPECT_MATRIX_EQ(a_ref2, a_eval);
-  EXPECT_MATRIX_EQ(a_ref3, a_eval);
+  matrix_cl<double> a_eval = a;
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref1), from_matrix_cl(a_eval));
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref2), from_matrix_cl(a_eval));
+  EXPECT_MATRIX_EQ(from_matrix_cl(a_ref3), from_matrix_cl(a_eval));
 
-  EXPECT_TRUE(
-      (std::is_same<stan::math::pinned_matrix<plain_type_t<decltype(a)>>,
-                    ref_type_for_opencl_t<decltype(a)>>::value));
-  EXPECT_TRUE(
-      (std::is_same<stan::math::pinned_matrix<plain_type_t<decltype(a)>>,
-                    ref_type_for_opencl_t<decltype(a)&>>::value));
-  EXPECT_TRUE(
-      (std::is_same<stan::math::pinned_matrix<plain_type_t<decltype(a)>>,
-                    ref_type_for_opencl_t<decltype(a)&&>>::value));
+  EXPECT_TRUE((std::is_same<stan::math::matrix_cl<double>,
+                            ref_type_t<decltype(a)>>::value));
+  EXPECT_TRUE((std::is_same<stan::math::matrix_cl<double>,
+                            ref_type_t<decltype(a)&>>::value));
+  EXPECT_TRUE((std::is_same<stan::math::matrix_cl<double>,
+                            ref_type_t<decltype(a)&&>>::value));
 }
 
 #endif
