3a27edaf4cf32a7691d362b9ba87aaade393cfd5
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-09 11:51:18 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/prim/functor/apply_scalar_binary_test.cpp
+++ test/unit/math/prim/functor/apply_scalar_binary_test.cpp
@@ -6,9 +6,13 @@ TEST(MathFunctions, apply_scalar_binary_copy_scalars) {
   Eigen::VectorXd lambda(1);
   lambda << 0.4;
 
-  const auto& y1 = stan::math::apply_scalar_binary(n + 1, lambda, [&](const auto& c, const auto& d) { return stan::math::gamma_q(c, d); });
+  const auto& y1 = stan::math::apply_scalar_binary(
+      n + 1, lambda,
+      [&](const auto& c, const auto& d) { return stan::math::gamma_q(c, d); });
   EXPECT_NO_THROW(stan::math::sum(y1));
 
-  const auto& y2 = stan::math::apply_scalar_binary(lambda, n + 1, [&](const auto& d, const auto& c) { return stan::math::gamma_q(c, d); });
+  const auto& y2 = stan::math::apply_scalar_binary(
+      lambda, n + 1,
+      [&](const auto& d, const auto& c) { return stan::math::gamma_q(c, d); });
   EXPECT_NO_THROW(stan::math::sum(y2));
 }
