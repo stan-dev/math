75d3bf1c814e228a7794832f0f435f2a136a67e3
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-09 22:28:57 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/functor/for_each.hpp
+++ stan/math/prim/functor/for_each.hpp
@@ -82,7 +82,8 @@ template <typename F, typename T1, typename T2>
 constexpr inline auto for_each(F&& f, T1&& t1, T2&& t2) {
   constexpr auto t1_size = std::tuple_size<std::decay_t<T1>>::value;
   constexpr auto t2_size = std::tuple_size<std::decay_t<T2>>::value;
-  static_assert(t1_size == t2_size, "Size Mismatch between t1 and t2 in for_each");
+  static_assert(t1_size == t2_size,
+                "Size Mismatch between t1 and t2 in for_each");
   return internal::for_each(
       std::forward<F>(f), std::forward<T1>(t1), std::forward<T2>(t2),
       std::make_index_sequence<std::tuple_size<std::decay_t<T1>>::value>());
@@ -104,8 +105,10 @@ constexpr inline auto for_each(F&& f, T1&& t1, T2&& t2, T3&& t3) {
   constexpr auto t1_size = std::tuple_size<std::decay_t<T1>>::value;
   constexpr auto t2_size = std::tuple_size<std::decay_t<T2>>::value;
   constexpr auto t3_size = std::tuple_size<std::decay_t<T3>>::value;
-  static_assert(t1_size == t2_size, "Size Mismatch between t1 and t2 in for_each");
-  static_assert(t1_size == t3_size, "Size Mismatch between t1 and t3 in for_each");
+  static_assert(t1_size == t2_size,
+                "Size Mismatch between t1 and t2 in for_each");
+  static_assert(t1_size == t3_size,
+                "Size Mismatch between t1 and t3 in for_each");
   return internal::for_each(
       std::forward<F>(f), std::forward<T1>(t1), std::forward<T2>(t2),
       std::forward<T3>(t3),
