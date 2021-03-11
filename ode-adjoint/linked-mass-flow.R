fd74b602c863180cbeca9ddf19e01befb7265bdf
Sebastian Weber <sebastian.weber@novartis.com>
2021-03-11 13:56:52 +0100

    bump examples

--- ode-adjoint/linked-mass-flow.R
+++ ode-adjoint/linked-mass-flow.R
@@ -1,30 +1,43 @@
-##install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+if(FALSE) {
+    ## run as needed to get latest cmdstanr
+    install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+    ## or even install the latest dev version with profiling support
+    Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
+    remotes::install_github("stan-dev/cmdstanr")
+}
 
 library(cmdstanr)
-library(posterior)
-library(bayesplot)
-color_scheme_set("brightblue")
 
 set_cmdstan_path("~/work/cmdstan")
 
+## model which scales number of states and parameters
 stan_model_file <- "linked-mass-flow.stan"
+## model which scales only number of states, but recycles parameters
+stan_model_fixed_file <- "linked-mass-flow-fixed.stan"
 
 mod <- cmdstan_model(stan_model_file)
+mod_fixed <- cmdstan_model(stan_model_fixed_file)
 
 base_data <- list(rel_tol=1e-6,
-                  abs_tol=1e-6,
-                  max_num_steps=10000,
+                  abs_tol=1e-4,
                   adjoint_integrator=0,
+                  max_num_steps=10000,
+                  num_checkpoints=150,
+                  interpolation_polynomial=1,
+                  solver_f=2,
+                  solver_b=2,
                   system_size=2,
                   num_obs=8,
                   sigma_sim=2.0,
-                  sigma_y=0.2)
+                  sigma_y=0.2
+                  )
 
-run_benchmark <- function(..., num_iter=250, adapt_delta=0.8) {
+run_benchmark <- function(..., model, num_iter=250, adapt_delta=0.8) {
     fit_args <- list(...)
     fit_data <- modifyList(base_data, fit_args)
     print(str(fit_data))
-    fit <- mod$sample(
+    fit <- model$sample(
                    data = fit_data,
                    iter_warmup = num_iter,
                    iter_sampling = num_iter,
@@ -33,20 +46,13 @@ run_benchmark <- function(..., num_iter=250, adapt_delta=0.8) {
                    parallel_chains = 1,
                    refresh = round(num_iter/5),
                    save_warmup=TRUE,
-                   adapt_delta=adapt_delta
+                   adapt_delta=adapt_delta,
+                   init=0.1
                    ##verbose=TRUE
                )
     fit
 }
 
-bdf_fit <- run_benchmark(adjoint_integrator=0)
-adjoint_fit <- run_benchmark(adjoint_integrator=1)
-
-if(FALSE) {
-    adjoint_fit_2 <- run_benchmark(adjoint_integrator=1, adapt_delta=0.65)
-    adjoint_fit_3 <- run_benchmark(adjoint_integrator=1, adapt_delta=0.95)
-    adjoint_fit_4 <- run_benchmark(adjoint_integrator=1, abs_tol=1E-10, rel_tol=1E-10)
-}
 
 summarize_benchmark <- function(fit) {
     sdiag_all  <- fit$sampler_diagnostics(inc_warmup=TRUE)
@@ -67,18 +73,26 @@ summarize_benchmark <- function(fit) {
          ess_lp_speed=ess_speed, lp_est=lp_est)
 }
 
-mcmc_rhat(rhat = rhat(bdf_fit))  + yaxis_text(hjust = 0)
+
+bdf_fit <- run_benchmark(adjoint_integrator=0, num_iter=50)
+adjoint_fit <- run_benchmark(adjoint_integrator=1, num_iter=50)
+## vary solvers, e.g.
+##adjoint_fit <- run_benchmark(adjoint_integrator=1, solver_f=2, solver_b=1, num_iter=50)
 
 str(summarize_benchmark(bdf_fit))
 str(summarize_benchmark(adjoint_fit))
 
+Lbdf_fit <- run_benchmark(adjoint_integrator=0, system_size=5, model=mod, num_iter=50)
+Ladjoint_fit <- run_benchmark(adjoint_integrator=1, solver_f=2, solver_b=2, system_size=5, interpolation_polynomial=1, model=mod, num_iter=50)
+Ladjoint_fit_1 <- run_benchmark(adjoint_integrator=1, solver_f=2, solver_b=1, system_size=5, interpolation_polynomial=1, model=mod, num_iter=50)
 
-str(summarize_benchmark(adjoint_fit_2))
-str(summarize_benchmark(adjoint_fit_3))
+str(summarize_benchmark(Lbdf_fit))
+str(summarize_benchmark(Ladjoint_fit))
+str(summarize_benchmark(Ladjoint_fit_1))
 
-bdf_fit$metadata()
+Lbdf_fit_fixed <- run_benchmark(adjoint_integrator=0, system_size=5, model=mod_fixed, num_iter=50)
+Ladjoint_fit_fixed <- run_benchmark(adjoint_integrator=1, solver_f=2, solver_b=2, system_size=5, model=mod_fixed, interpolation_polynomial=1, num_iter=50)
 
-bdf_fit$time()
+str(summarize_benchmark(Lbdf_fit_fixed))
+str(summarize_benchmark(Ladjoint_fit_fixed))
 
-
-bdf_fit$summary("lp__")
