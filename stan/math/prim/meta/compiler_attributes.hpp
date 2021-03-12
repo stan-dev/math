4f92e24ea3d24dd2efbf9967d88387694a30ec16
Steve Bronder <stevo15025@gmail.com>
2021-03-12 16:01:23 -0500

    adds macro for making bounds checks a no-op

--- stan/math/prim/meta/compiler_attributes.hpp
+++ stan/math/prim/meta/compiler_attributes.hpp
@@ -4,11 +4,24 @@
 #ifdef __GNUC__
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
+#ifdef __has_attribute
+#if __has_attribute (noinline) && __has_attribute (cold)
 #define STAN_COLD_PATH __attribute__((noinline, cold))
+#endif
+#endif
 #else
 #define likely(x) (x)
 #define unlikely(x) (x)
 #define STAN_COLD_PATH
+#endif
+
+
+#ifndef STAN_NO_RANGE_AND_SIZE_CHECK
+#ifdef NDEBUG
+#define STAN_NO_RANGE_AND_SIZE_CHECK return
+#endif
+#else
+#define STAN_NO_RANGE_AND_SIZE_CHECK
 #endif
 
 #endif
