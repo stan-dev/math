be701cb7d3aaadab527c36f26600351bd5847dd1
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-13 20:56:49 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/meta/compiler_attributes.hpp
+++ stan/math/prim/meta/compiler_attributes.hpp
@@ -2,34 +2,34 @@
 #define STAN_MATH_PRIM_META_COMPILER_ATTRIBUTES_HPP
 
 #ifdef __GNUC__
-  #define likely(x) __builtin_expect(!!(x), 1)
-  #define unlikely(x) __builtin_expect(!!(x), 0)
-  #ifdef __has_attribute
-    #if __has_attribute (noinline) && __has_attribute (cold)
-      /**
-       * Functions tagged with this attribute are not inlined and moved
-       *  to a cold branch to tell the CPU to not attempt to pre-fetch
-       *  the associated function.
-       */
-      #define STAN_COLD_PATH __attribute__((noinline, cold))
-    #endif
-  #endif
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#ifdef __has_attribute
+#if __has_attribute(noinline) && __has_attribute(cold)
+/**
+ * Functions tagged with this attribute are not inlined and moved
+ *  to a cold branch to tell the CPU to not attempt to pre-fetch
+ *  the associated function.
+ */
+#define STAN_COLD_PATH __attribute__((noinline, cold))
+#endif
+#endif
 #else
-  #define likely(x) (x)
-  #define unlikely(x) (x)
-  #define STAN_COLD_PATH
+#define likely(x) (x)
+#define unlikely(x) (x)
+#define STAN_COLD_PATH
 #endif
 
 /**
  * Turns all range and size checks into no-ops
  */
 #ifndef STAN_NO_RANGE_AND_SIZE_CHECK
-  #ifdef STAN_NDEBUG
-    #define STAN_NO_RANGE_AND_SIZE_CHECK return
-  #else
-    #define STAN_NO_RANGE_AND_SIZE_CHECK
-  #endif
+#ifdef STAN_NDEBUG
+#define STAN_NO_RANGE_AND_SIZE_CHECK return
 #else
-  #define STAN_NO_RANGE_AND_SIZE_CHECK
+#define STAN_NO_RANGE_AND_SIZE_CHECK
+#endif
+#else
+#define STAN_NO_RANGE_AND_SIZE_CHECK
 #endif
 #endif
