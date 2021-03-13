77f684785efa99af1c6998a19c1e14cb652d3fd1
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-13 20:26:23 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/meta/compiler_attributes.hpp
+++ stan/math/prim/meta/compiler_attributes.hpp
@@ -5,7 +5,7 @@
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #ifdef __has_attribute
-#if __has_attribute (noinline) && __has_attribute (cold)
+#if __has_attribute(noinline) && __has_attribute(cold)
 /**
  * Functions tagged with this attribute are not inlined and moved
  *  to a cold branch to tell the CPU to not attempt to pre-fetch
