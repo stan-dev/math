b9fe223c175391f322f808b39495dec0d3dbf98e
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-13 21:09:51 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/meta/compiler_attributes.hpp
+++ stan/math/prim/meta/compiler_attributes.hpp
@@ -2,22 +2,22 @@
 #define STAN_MATH_PRIM_META_COMPILER_ATTRIBUTES_HPP
 
 #ifdef __GNUC__
-  #define likely(x) __builtin_expect(!!(x), 1)
-  #define unlikely(x) __builtin_expect(!!(x), 0)
-  #ifdef __has_attribute
-    #if __has_attribute(noinline) && __has_attribute(cold)
-      /**
-       * Functions tagged with this attribute are not inlined and moved
-       *  to a cold branch to tell the CPU to not attempt to pre-fetch
-       *  the associated function.
-       */
-      #define STAN_COLD_PATH __attribute__((noinline, cold))
-    #else
-      #define STAN_COLD_PATH
-    #endif
-  #else
-    #define STAN_COLD_PATH
-  #endif
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#ifdef __has_attribute
+#if __has_attribute(noinline) && __has_attribute(cold)
+/**
+ * Functions tagged with this attribute are not inlined and moved
+ *  to a cold branch to tell the CPU to not attempt to pre-fetch
+ *  the associated function.
+ */
+#define STAN_COLD_PATH __attribute__((noinline, cold))
+#else
+#define STAN_COLD_PATH
+#endif
+#else
+#define STAN_COLD_PATH
+#endif
 #else
 #define likely(x) (x)
 #define unlikely(x) (x)
