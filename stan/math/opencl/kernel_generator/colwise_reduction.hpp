25c4c990909ad9f6c505f8508059bb53aa387aa2
Rok Cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-03-18 20:04:28 +0100

    another fix for doxy

--- stan/math/opencl/kernel_generator/colwise_reduction.hpp
+++ stan/math/opencl/kernel_generator/colwise_reduction.hpp
@@ -310,7 +310,7 @@ class colwise_max_ : public colwise_reduction<
  * reductions can not be used as arguments to other operations - they can only
  * be evaluated.
  * @tparam T type of input expression
- * @param a expression to reduce
+ * @param a an expression to reduce
  * @return max
  */
 template <typename T, require_all_kernel_expressions_t<T>* = nullptr>
@@ -359,7 +359,7 @@ class colwise_min_ : public colwise_reduction<
  * reductions can not be used as arguments to other operations - they can only
  * be evaluated.
  * @tparam T type of input expression
- * @param an expression to reduce
+ * @param a an expression to reduce
  * @return min
  */
 template <typename T, require_all_kernel_expressions_t<T>* = nullptr>
