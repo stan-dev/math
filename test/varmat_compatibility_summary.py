b74e08240037832dc42678e22fa760ef6d1f7686
Ben <bbbales2@gmail.com>
2021-03-18 13:45:43 -0400

    Revert "Removed varmat compatibility tests for now"
    This reverts commit 9fb6801da6549891b30b97ec92ad08c026ff272f.

---
+++ test/varmat_compatibility_summary.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+
+import argparse
+import json
+import sys
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+import sig_utils
+
+def main(results_file, functions, print_which, print_fully, print_names):
+    """
+    Generates benchmark code, compiles it and runs the benchmark. Optionally plots the results.
+    :param results_file: File containing results of varmat compatibility calculation
+    :param functions: List of function names to check compatibility for
+    :param print_which: For which type of compatibility should functions be printed
+    :param print_fully: Only print fully compatible/incompatible signatures (no partial varmat support)
+    :param print_names: Print function names, not signatures
+    """
+    with open(results_file, "r") as f:
+        results = json.load(f)
+
+    compatible_signatures = set()
+    incompatible_signatures = set()
+    impossible_signatures = set()
+    if "compatible_signatures" in results:
+        compatible_signatures = set(results["compatible_signatures"])
+    if "incompatible_signatures" in results:
+        incompatible_signatures = set(results["incompatible_signatures"])
+    if "impossible_signatures" in results:
+        impossible_signatures = set(results["impossible_signatures"])
+
+    requested_functions = set(functions)
+
+    names_to_print = set()
+
+    def convert_signatures_list_to_functions(signatures):
+        functions = set()
+        for signature in signatures:
+            return_type, function_name, stan_args = sig_utils.parse_signature(signature)
+
+            if len(requested_functions) > 0 and function_name not in requested_functions:
+                continue
+
+            functions.add(function_name)
+        return functions
+
+    compatible_functions = convert_signatures_list_to_functions(compatible_signatures)
+    incompatible_functions = convert_signatures_list_to_functions(incompatible_signatures)
+    impossible_functions = convert_signatures_list_to_functions(impossible_signatures)
+
+    if print_fully:
+        fully_compatible_functions = compatible_functions - incompatible_functions
+        fully_incompatible_functions = incompatible_functions - compatible_functions
+        fully_impossible_functions = impossible_functions - compatible_functions - incompatible_functions
+        compatible_functions = fully_compatible_functions
+        incompatible_functions = fully_incompatible_functions
+        impossible_functions = fully_impossible_functions
+    
+    if print_names:
+        if print_which == "compatible":
+            names_to_print = compatible_functions
+        elif print_which == "incompatible":
+            names_to_print = incompatible_functions
+        else:
+            names_to_print = impossible_functions
+    else:
+        if print_which == "compatible":
+            names_to_print = compatible_signatures
+        elif print_which == "incompatible":
+            names_to_print = incompatible_signatures
+        else:
+            names_to_print = impossible_signatures
+    
+    for name in sorted(names_to_print):
+        if print_names:
+            print(name.strip())
+        else:
+            print(name.replace("scalar_return_t", "real").strip())
+
+class FullErrorMsgParser(ArgumentParser):
+    """
+    Modified ArgumentParser that prints full error message on any error.
+    """
+
+    def error(self, message):
+        sys.stderr.write("error: %s\n" % message)
+        self.print_help()
+        sys.exit(2)
+
+
+def processCLIArgs():
+    """
+    Define and process the command line interface to the varmat compatibility summarize script.
+    """
+    parser = FullErrorMsgParser(
+        description="Process results of varmat compatibility test.",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "results_file",
+        type=str,
+        default=[],
+        help="File with results of varmat compatibility test.",
+    )
+    parser.add_argument(
+        "--functions",
+        nargs="+",
+        type=str,
+        default=[],
+        help="Function names to summarize. By default summarize everything.",
+    )
+    parser.add_argument(
+        "--which",
+        default = "compatible",
+        choices = ["compatible", "incompatible", "impossible"],
+        help = "Print by compatibility."
+    )
+    parser.add_argument(
+        "--fully",
+        default = False,
+        help = "When printing compatible or incompatible function names, print those that are fully compatible or incompatible, ignoring impossible functions. When printing impossible functions, print only those with no compatible or incompatible versions.",
+        action = "store_true"
+    )
+    parser.add_argument(
+        "--names",
+        default=False,
+        help="Print function names, not signatures.",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    main(
+        args.results_file, args.functions,
+        print_which = args.which,
+        print_fully = args.fully,
+        print_names = args.names
+    )
+
+if __name__ == "__main__":
+    processCLIArgs()
---
+++ test/varmat_compatibility_test.py
@@ -0,0 +1,257 @@
+#!/usr/bin/python
+
+import itertools
+import json
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+import os
+import Queue
+import subprocess
+import sys
+import tempfile
+import threading
+
+from sig_utils import make, handle_function_list, get_signatures
+from signature_parser import SignatureParser
+from code_generator import CodeGenerator
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+TEST_FOLDER = os.path.abspath(os.path.join(HERE, "..", "test"))
+sys.path.append(TEST_FOLDER)
+WORKING_FOLDER = "test/varmat-compatibility"
+
+BENCHMARK_TEMPLATE = """
+static void {benchmark_name}() {{
+{code}
+}}
+"""
+
+def run_command(command):
+    """
+    Runs given command and waits until it finishes executing.
+    :param command: command to execute
+    """
+    proc = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    
+    if proc.poll() == 0:
+        return (True, stdout, stderr)
+    else:
+        return (False, stdout, stderr)
+
+def build_signature(cpp_code):
+    """
+    Try to build the given cpp code
+
+    Return a tuple (successful, attempted)
+
+    attempted is true if there was an attempt to build the code
+
+    successful is true is the code was successfully built
+
+    :param cpp_code Code to build
+    """
+    attempted = False
+    successful = False
+
+    if cpp_code:
+        attempted = True
+
+        f = tempfile.NamedTemporaryFile("w", dir = WORKING_FOLDER, suffix = "_test.cpp", delete = False)
+        f.write("#include <test/expressions/expression_test_helpers.hpp>\n\n")
+        f.write(cpp_code)
+        f.close()
+
+        cpp_path = os.path.join(WORKING_FOLDER, os.path.basename(f.name))
+
+        object_path = cpp_path.replace(".cpp", ".o")
+        dependency_path = cpp_path.replace(".cpp", ".d")
+        stdout_path = cpp_path.replace(".cpp", ".stdout")
+        stderr_path = cpp_path.replace(".cpp", ".stderr")
+
+        successful, stdout, stderr = run_command([make, object_path])
+
+        # Only clean up files if check succesful
+        if successful:
+            try:
+                os.remove(cpp_path)
+                os.remove(dependency_path)
+                os.remove(object_path)
+            except OSError:
+                pass
+        else:
+            with open(stdout_path, "w") as stdout_f:
+                stdout_f.write(stdout.decode("utf-8"))
+
+            with open(stderr_path, "w") as stderr_f:
+                stderr_f.write(stderr.decode("utf-8"))
+        
+    return successful, attempted
+
+def main(functions_or_sigs, results_file, cores):
+    """
+    Attempt to build all the signatures in functions_or_sigs, or all the signatures
+    associated with all the functions in functions_or_sigs, or if functions_or_sigs
+    is empty every signature the stanc3 compiler exposes.
+
+    Results are written to a results json file. Individual signatures are classified
+    as either compatible, incompatible, or impossible
+
+    Compatible signatures can be compiled with varmat types in every argument that
+    could possibly be a varmat (the matrix-like ones).
+
+    Incompatible signatures cannot all be built, and for impossible signatures it does
+    not make sense to try to build them (there are no matrix arguments, or the function
+    does not support reverse mode autodiff, etc)
+
+    Compilation is done in parallel using the number of specified cores
+
+    :param functions_or_sigs: List of function names and/or signatures to benchmark
+    :param results_file: File to use as a results cache
+    :param cores: Number of cores to use for compiling
+    """
+    all_signatures = get_signatures()
+    functions, signatures = handle_function_list(functions_or_sigs)
+
+    requested_functions = set(functions)
+
+    compatible_signatures = set()
+    incompatible_signatures = set()
+    impossible_signatures = set()
+
+    # Read the arguments and figure out the exact list of signatures to test
+    signatures_to_check = set()
+    for signature in all_signatures:
+        sp = SignatureParser(signature)
+
+        if len(requested_functions) > 0 and sp.function_name not in requested_functions:
+            continue
+        
+        signatures_to_check.add(signature)
+
+    work_queue = Queue.Queue()
+
+    # For each signature, generate cpp code to test
+    for n, signature in enumerate(signatures_to_check):
+        sp = SignatureParser(signature)
+
+        if sp.is_high_order():
+            work_queue.put((n, signature, None))
+            continue
+
+        cpp_code = ""
+        any_overload_uses_varmat = False
+        
+        for m, overloads in enumerate(itertools.product(("Prim", "Rev", "RevVarmat"), repeat = sp.number_arguments())):
+            cg = CodeGenerator()
+
+            arg_list_base = cg.build_arguments(sp, overloads)
+
+            arg_list = []
+            for overload, arg in zip(overloads, arg_list_base):
+                if arg.is_reverse_mode() and arg.is_varmat_compatible():
+                    any_overload_uses_varmat = True
+                    arg = cg.to_var_value(arg)
+                
+                arg_list.append(arg)
+
+            cg.function_call_assign("stan::math::" + sp.function_name, *arg_list)
+
+            cpp_code += BENCHMARK_TEMPLATE.format(
+                benchmark_name = sp.function_name + repr(m),
+                code=cg.cpp(),
+            )
+
+        if any_overload_uses_varmat:
+            work_queue.put((n, signature, cpp_code))
+        else:
+            work_queue.put((n, signature, None))
+
+    output_lock = threading.Lock()
+
+    # Test if each cpp file builds and update the output file
+    # This part is done in parallel
+    def worker():
+        while True:
+            try:
+                n, signature, cpp_code = work_queue.get(False)
+            except Queue.Empty:
+                return # If queue is empty, worker quits
+
+            # Test the signature
+            successful, attempted = build_signature(cpp_code)
+
+            # Acquire a lock to do I/O
+            with output_lock:
+                print("Check results of test {0} / {1}, {2} ... ".format(n, len(signatures_to_check), signature.strip()), end = '')
+
+                if attempted:
+                    if successful:
+                        print("Success!")
+                        compatible_signatures.add(signature)
+                    else:
+                        print("Fail!")
+                        incompatible_signatures.add(signature)
+                else:
+                    print("Impossible!")
+                    impossible_signatures.add(signature)
+
+                if results_file:
+                    with open(results_file, "w") as f:
+                        json.dump({
+                            "compatible_signatures" : list(compatible_signatures),
+                            "incompatible_signatures" : list(incompatible_signatures),
+                            "impossible_signatures" : list(impossible_signatures)
+                        }, f, indent = 4, sort_keys = True)
+
+            work_queue.task_done()
+
+    for i in range(cores):
+        threading.Thread(target = worker).start()
+
+    work_queue.join()
+
+class FullErrorMsgParser(ArgumentParser):
+    """
+    Modified ArgumentParser that prints full error message on any error.
+    """
+
+    def error(self, message):
+        sys.stderr.write("error: %s\n" % message)
+        self.print_help()
+        sys.exit(2)
+
+
+def processCLIArgs():
+    """
+    Define and process the command line interface to the benchmark.py script.
+    """
+    parser = FullErrorMsgParser(
+        description="Generate and run_command benchmarks.",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--functions",
+        nargs="+",
+        type=str,
+        default=[],
+        help="Signatures and/or function names to benchmark.",
+    )
+    parser.add_argument(
+        "-j",
+        type=int,
+        default=1,
+        help="Number of parallel cores to use.",
+    )
+    parser.add_argument(
+        "results_file",
+        type=str,
+        default=None,
+        help="File to save results in.",
+    )
+    args = parser.parse_args()
+
+    main(functions_or_sigs=args.functions, results_file = args.results_file, cores = args.j)
+
+
+if __name__ == "__main__":
+    processCLIArgs()
