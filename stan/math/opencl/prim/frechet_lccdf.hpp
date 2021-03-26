55462da2746f06e142a7daa53cb6585e2d0831cc
tadej <tadej.c@gmail.com>
2021-03-26 14:10:44 +0100

    bugfix

--- stan/math/opencl/prim/frechet_lccdf.hpp
+++ stan/math/opencl/prim/frechet_lccdf.hpp
@@ -67,7 +67,7 @@ return_type_t<T_y_cl, T_shape_cl, T_scale_cl> frechet_lccdf(
   auto exp_n = exp(-pow_n);
   auto lccdf_expr = colwise_sum(log1m(exp_n));
 
-  auto rep_deriv = elt_divide(pow_n, elt_divide(1.0, exp_n - 1.0));
+  auto rep_deriv = elt_divide(pow_n, elt_divide(1.0, exp_n) - 1.0);
   auto y_deriv = elt_multiply(elt_divide(-alpha_val, y_val), rep_deriv);
   auto alpha_deriv
       = elt_multiply(-log(elt_divide(y_val, sigma_val)), rep_deriv);
