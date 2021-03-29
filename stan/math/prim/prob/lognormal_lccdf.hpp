1df072993ecbba3a8e5f91d738479d27ca3cf605
tadej <tadej.c@gmail.com>
2021-03-29 09:02:18 +0200

    tmp

--- stan/math/prim/prob/lognormal_lccdf.hpp
+++ stan/math/prim/prob/lognormal_lccdf.hpp
@@ -22,7 +22,9 @@
 namespace stan {
 namespace math {
 
-template <typename T_y, typename T_loc, typename T_scale>
+template <typename T_y, typename T_loc, typename T_scale,
+          require_all_not_nonscalar_prim_or_rev_kernel_expression_t<
+              T_y, T_loc, T_scale>* = nullptr>
 return_type_t<T_y, T_loc, T_scale> lognormal_lccdf(const T_y& y,
                                                    const T_loc& mu,
                                                    const T_scale& sigma) {
