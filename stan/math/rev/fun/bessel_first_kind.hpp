c4895008c2284d1214ba79da7518c02b6150027b
Stan Jenkins <mc.stanislaw@gmail.com>
2021-03-30 21:54:46 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/rev/fun/bessel_first_kind.hpp
+++ stan/math/rev/fun/bessel_first_kind.hpp
@@ -10,8 +10,8 @@ namespace math {
 
 inline var bessel_first_kind(int v, const var& a) {
   double ret_val = bessel_first_kind(v, a.val());
-  auto precomp_bessel = v * ret_val / a.val()
-                        - bessel_first_kind(v + 1, a.val());
+  auto precomp_bessel
+      = v * ret_val / a.val() - bessel_first_kind(v + 1, a.val());
   return make_callback_var(ret_val,
                            [precomp_bessel, a](const auto& vi) mutable {
                              a.adj() += vi.adj_ * precomp_bessel;
--- stan/math/rev/fun/bessel_second_kind.hpp
+++ stan/math/rev/fun/bessel_second_kind.hpp
@@ -10,12 +10,11 @@ namespace math {
 
 inline var bessel_second_kind(int v, const var& a) {
   double ret_val = bessel_second_kind(v, a.val());
-  auto precomp_bessel = v * ret_val / a.val()
-                        - bessel_second_kind(v + 1, a.val());
-  return make_callback_var(ret_val,
-                           [precomp_bessel, a](auto& vi) mutable {
-                             a.adj() += vi.adj() * precomp_bessel;
-                           });
+  auto precomp_bessel
+      = v * ret_val / a.val() - bessel_second_kind(v + 1, a.val());
+  return make_callback_var(ret_val, [precomp_bessel, a](auto& vi) mutable {
+    a.adj() += vi.adj() * precomp_bessel;
+  });
 }
 
 }  // namespace math
