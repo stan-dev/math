d73865579a2fe007bf94df6dc0caa14ca262aa24
Steve Bronder <stevo15025@gmail.com>
2021-03-31 13:59:48 -0400

    test for setting number of threads

--- test/unit/math/prim/core/init_threadpool_tbb_late_test.cpp
+++ test/unit/math/prim/core/init_threadpool_tbb_late_test.cpp
@@ -25,6 +25,21 @@ TEST(intel_tbb_new_late_init, check_status) {
   }
 }
 
+TEST(intel_tbb_new_late_init, set_threads) {
+  const int num_threads = tbb::global_control::max_allowed_parallelism;
+
+  if (num_threads > 1) {
+    tbb::task_arena& tbb_arena = stan::math::init_threadpool_tbb(num_threads);
+    tbb_arena.execute([&]() {
+      EXPECT_TRUE(tbb_arena.is_active());
+
+      // STAN_NUM_THREADS is not being honored if we have first
+      // initialized the TBB scheduler outside of init_threadpool_tbb
+      EXPECT_EQ(num_threads, tbb::this_task_arena::max_concurrency());
+    });
+  }
+}
+
 #else
 
 #include <tbb/task_scheduler_init.h>
@@ -37,6 +52,19 @@ TEST(intel_tbb_late_init, check_status) {
   if (num_threads > 1) {
     set_n_threads(num_threads - 1);
     tbb::task_scheduler_init& tbb_init = stan::math::init_threadpool_tbb();
+    EXPECT_TRUE(tbb_init.is_active());
+
+    // STAN_NUM_THREADS is not being honored if we have first
+    // initialized the TBB scheduler outside of init_threadpool_tbb
+    EXPECT_EQ(num_threads, tbb::this_task_arena::max_concurrency());
+  }
+}
+
+TEST(intel_tbb_late_init, set_threads) {
+  const int num_threads = tbb::task_scheduler_init::default_num_threads();
+  if (num_threads > 1) {
+    tbb::task_scheduler_init tbb_scheduler;
+    tbb::task_scheduler_init& tbb_init = stan::math::init_threadpool_tbb(num_threads);
     EXPECT_TRUE(tbb_init.is_active());
 
     // STAN_NUM_THREADS is not being honored if we have first
