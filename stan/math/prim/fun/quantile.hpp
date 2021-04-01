281fa2ba4a5a081ae94dd355113cd0c53188af55
Adam Haber <adamhaber@gmail.com>
2021-04-01 22:39:32 +0300

    revert to R's implementation

--- stan/math/prim/fun/quantile.hpp
+++ stan/math/prim/fun/quantile.hpp
@@ -5,14 +5,6 @@
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
 #include <stan/math/prim/fun/as_array_or_scalar.hpp>
-#include <boost/accumulators/accumulators.hpp>
-#include <boost/accumulators/statistics/stats.hpp>
-#include <boost/accumulators/statistics/mean.hpp>
-#include <boost/accumulators/statistics/tail_quantile.hpp>
-#include <boost/accumulators/statistics/p_square_quantile.hpp>
-#include <boost/accumulators/statistics/variance.hpp>
-#include <boost/accumulators/statistics/covariance.hpp>
-#include <boost/accumulators/statistics/variates/covariate.hpp>
 #include <algorithm>
 #include <vector>
 
@@ -35,29 +27,28 @@ namespace math {
  */
 template <typename T, require_vector_t<T>* = nullptr>
 inline double quantile(const T& xs, const double p) {
-  using boost::accumulators::accumulator_set;
-  using boost::accumulators::left;
-  using boost::accumulators::quantile;
-  using boost::accumulators::quantile_probability;
-  using boost::accumulators::right;
-  using boost::accumulators::stats;
-  using boost::accumulators::tag::tail;
-  using boost::accumulators::tag::tail_quantile;
-
   check_not_nan("quantile", "container argument", xs);
-  check_bounded("sort_asc", "p", p, 0, 1);
+  check_bounded("quantile", "p", p, 0, 1);
   size_t n_sample = xs.size();
-  if (n_sample == 1)
-    return xs[0];
-  if (p == 0.)
-    return *std::min_element(xs.begin(), xs.end());
-  if (p == 1.)
-    return *std::max_element(xs.begin(), xs.end());
-  size_t loc = std::floor(xs.size() * p);
 
-  std::vector<T> sorted(loc + 1);
-  std::partial_sort_copy(xs.begin(), xs.end(), sorted.begin(), sorted.end());
-  return sorted[loc];
+  auto& x = as_array_or_scalar(xs);
+
+  if (n_sample == 1)
+    return x[0];
+  if (p == 0.)
+    return *std::min_element(x.data(), x.data() + n_sample);
+  if (p == 1.)
+    return *std::max_element(x.data(), x.data() + n_sample);
+
+  double index = (n_sample - 1) * p;
+  size_t lo = std::floor(index);
+  size_t hi = std::ceil(index);
+
+  std::sort(x.data(), x.data() + n_sample);
+
+  double q = x[lo];
+  double h = index - lo;
+  return (1 - h) * q + h * x[hi];
 }
 
 }  // namespace math
--- test/unit/math/prim/fun/quantile_test.cpp
+++ test/unit/math/prim/fun/quantile_test.cpp
@@ -4,29 +4,57 @@
 #include <limits>
 #include <stdexcept>
 
-template <typename T>
-void test_quantile_double() {
-  using stan::math::index_type_t;
+// template <typename T>
+// void test_quantile_double() {
+//   using stan::math::index_type_t;
+//   using stan::math::quantile;
+
+//   T c(1);
+//   c[0] = 1.7;
+// EXPECT_EQ(quantile(c, 0), 1.7);
+// EXPECT_EQ(quantile(c, 1), 1.7);
+// EXPECT_EQ(quantile(c, 0.33), 1.7);
+// EXPECT_EQ(quantile(c, 0.68), 1.7);
+
+// T v(5);
+// v[0] = 1.0;
+// v[1] = 2.0;
+// v[2] = 3.0;
+// v[3] = -1.0;
+// v[4] = 19.3;
+// EXPECT_EQ(quantile(v, 0), -1.);
+// EXPECT_EQ(quantile(v, 1), 19.3);
+// EXPECT_EQ(quantile(v, 0.2), 1.);
+// std::cout << quantile(v, 0) << std::endl;
+// std::cout << quantile(v, 0.1) << std::endl;
+// std::cout << quantile(v, 0.8) << std::endl;
+// std::cout << quantile(v, 1) << std::endl;
+// }
+
+// TEST(MathFunctions, quantileStdVecDouble) {
+//   test_quantile_double<std::vector<double> >();
+// }
+
+TEST(MathFunctions, bla) {
   using stan::math::quantile;
-
-  T c(1);
-  c[0] = 1.7;
-  EXPECT_EQ(quantile(c, 0), 1.7);
-  EXPECT_EQ(quantile(c, 1), 1.7);
-  EXPECT_EQ(quantile(c, 0.33), 1.7);
-  EXPECT_EQ(quantile(c, 0.68), 1.7);
-
-  T v(5);
+  std::vector<double> v(5);
+  // Eigen::VectorXd v(5);
+  // Eigen::RowVectorXd v(5);
   v[0] = 1.0;
   v[1] = 2.0;
   v[2] = 3.0;
   v[3] = -1.0;
   v[4] = 19.3;
-  EXPECT_EQ(quantile(v, 0), -1.);
-  EXPECT_EQ(quantile(v, 1), 19.3);
-  EXPECT_EQ(quantile(v, 0.2), 1.);
-}
 
-TEST(MathFunctions, quantileStdVecDouble) {
-  test_quantile_double<std::vector<double> >();
+  std::cout << quantile(v, 0.01) << std::endl;
+  std::cout << quantile(v, 0.1) << std::endl;
+  std::cout << quantile(v, 0.2) << std::endl;
+  std::cout << quantile(v, 0.3) << std::endl;
+  std::cout << quantile(v, 0.4) << std::endl;
+  std::cout << quantile(v, 0.5) << std::endl;
+  std::cout << quantile(v, 0.6) << std::endl;
+  std::cout << quantile(v, 0.7) << std::endl;
+  std::cout << quantile(v, 0.8) << std::endl;
+  std::cout << quantile(v, 0.9) << std::endl;
+  std::cout << quantile(v, 0.99) << std::endl;
 }
