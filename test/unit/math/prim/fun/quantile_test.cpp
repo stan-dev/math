bdf390d6bee54ca5b720fb02f221e29c3db08694
Adam Haber <adamhaber@gmail.com>
2021-04-02 08:37:01 +0300

    add tests against R impl.

--- test/unit/math/prim/fun/quantile_test.cpp
+++ test/unit/math/prim/fun/quantile_test.cpp
@@ -4,57 +4,45 @@
 #include <limits>
 #include <stdexcept>
 
-// template <typename T>
-// void test_quantile_double() {
-//   using stan::math::index_type_t;
-//   using stan::math::quantile;
-
-//   T c(1);
-//   c[0] = 1.7;
-// EXPECT_EQ(quantile(c, 0), 1.7);
-// EXPECT_EQ(quantile(c, 1), 1.7);
-// EXPECT_EQ(quantile(c, 0.33), 1.7);
-// EXPECT_EQ(quantile(c, 0.68), 1.7);
-
-// T v(5);
-// v[0] = 1.0;
-// v[1] = 2.0;
-// v[2] = 3.0;
-// v[3] = -1.0;
-// v[4] = 19.3;
-// EXPECT_EQ(quantile(v, 0), -1.);
-// EXPECT_EQ(quantile(v, 1), 19.3);
-// EXPECT_EQ(quantile(v, 0.2), 1.);
-// std::cout << quantile(v, 0) << std::endl;
-// std::cout << quantile(v, 0.1) << std::endl;
-// std::cout << quantile(v, 0.8) << std::endl;
-// std::cout << quantile(v, 1) << std::endl;
-// }
-
-// TEST(MathFunctions, quantileStdVecDouble) {
-//   test_quantile_double<std::vector<double> >();
-// }
-
-TEST(MathFunctions, bla) {
+template <typename T>
+void test_quantile_double() {
+  using stan::math::index_type_t;
   using stan::math::quantile;
-  std::vector<double> v(5);
-  // Eigen::VectorXd v(5);
-  // Eigen::RowVectorXd v(5);
-  v[0] = 1.0;
-  v[1] = 2.0;
-  v[2] = 3.0;
-  v[3] = -1.0;
-  v[4] = 19.3;
 
-  std::cout << quantile(v, 0.01) << std::endl;
-  std::cout << quantile(v, 0.1) << std::endl;
-  std::cout << quantile(v, 0.2) << std::endl;
-  std::cout << quantile(v, 0.3) << std::endl;
-  std::cout << quantile(v, 0.4) << std::endl;
-  std::cout << quantile(v, 0.5) << std::endl;
-  std::cout << quantile(v, 0.6) << std::endl;
-  std::cout << quantile(v, 0.7) << std::endl;
-  std::cout << quantile(v, 0.8) << std::endl;
-  std::cout << quantile(v, 0.9) << std::endl;
-  std::cout << quantile(v, 0.99) << std::endl;
+  T c(1);
+  c[0] = 1.7;
+  EXPECT_EQ(quantile(c, 0), 1.7);
+  EXPECT_EQ(quantile(c, 1), 1.7);
+  EXPECT_EQ(quantile(c, 0.33), 1.7);
+  EXPECT_EQ(quantile(c, 0.68), 1.7);
+
+  T v(5);
+  v[0] = -0.07;
+  v[1] = 0.35;
+  v[2] = 2.65;
+  v[3] = -0.28;
+  v[4] = 1.23;
+  EXPECT_FLOAT_EQ(quantile(v, 0), -0.28);
+  EXPECT_FLOAT_EQ(quantile(v, 0.1), -0.196);
+  EXPECT_FLOAT_EQ(quantile(v, 0.2), -0.112);
+  EXPECT_FLOAT_EQ(quantile(v, 0.3), 0.014);
+  EXPECT_FLOAT_EQ(quantile(v, 0.4), 0.182);
+  EXPECT_FLOAT_EQ(quantile(v, 0.5), 0.350);
+  EXPECT_FLOAT_EQ(quantile(v, 0.6), 0.702);
+  EXPECT_FLOAT_EQ(quantile(v, 0.7), 1.054);
+  EXPECT_FLOAT_EQ(quantile(v, 0.8), 1.514);
+  EXPECT_FLOAT_EQ(quantile(v, 0.9), 2.082);
+  EXPECT_FLOAT_EQ(quantile(v, 1), 2.65);
+}
+
+TEST(MathFunctions, quantileStdVecDouble) {
+  test_quantile_double<std::vector<double> >();
+}
+
+TEST(MathFunctions, quantileEigenVectorXd) {
+  test_quantile_double<Eigen::VectorXd>();
+}
+
+TEST(MathFunctions, quantileEigenRowVectorXd) {
+  test_quantile_double<Eigen::RowVectorXd>();
 }
