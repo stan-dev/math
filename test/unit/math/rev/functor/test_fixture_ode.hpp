62281d213da848621d11c4d20829112b0d9374a1
Stan Jenkins <mc.stanislaw@gmail.com>
2021-04-28 19:59:52 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- test/unit/math/rev/functor/test_fixture_ode.hpp
+++ test/unit/math/rev/functor/test_fixture_ode.hpp
@@ -109,11 +109,7 @@
  */
 template <class ode_problem_type>
 struct ODETestFixture : public ::testing::Test {
-
-
-  virtual void TearDown() {
-    stan::math::recover_memory();
-  }
+  virtual void TearDown() { stan::math::recover_memory(); }
   /**
    * test ODE solver pass
    *
