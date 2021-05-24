162fc964acb867ce83fa1bee1bd0259e382e6783
tadej <tadej.c@gmail.com>
2021-05-24 12:45:29 +0200

    incomplete fixes to double double

--- stan/math/opencl/double_d.hpp
+++ stan/math/opencl/double_d.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <cmath>
 
+#include <limits>
+
 #define STRINGIFY2(...) \
 #__VA_ARGS__;         \
   __VA_ARGS__
@@ -11,301 +13,353 @@ namespace stan {
 namespace math {
 namespace internal {
 // \cond
-//const char* double_d_src = STRINGIFY2(
-    // \endcond
-    typedef struct double_d {
-      double high, low;
+// const char* double_d_src = STRINGIFY2(
+// \endcond
+typedef struct double_d {
+  double high, low;
 
 #ifdef __cplusplus
-      double_d() {}
-      double_d(double a) : high(a), low(0) {}
-      double_d(double h, double l) : high(h), low(l) {}
+  double_d() {}
+  double_d(double a) : high(a), low(0) {}
+  double_d(double h, double l) : high(h), low(l) {}
 #endif
-    } double_d;
+} double_d;
 
-    inline double_d add_quick_d_d(double a, double b) {
-      double_d res;
-      res.high = a + b;
-      res.low = b - (res.high - a);
+inline double_d add_quick_d_d(double a, double b) {
+  double_d res;
+  res.high = a + b;
+  res.low = b - (res.high - a);
+  return res;
+}
+
+inline double_d add_d_d(double a, double b) {
+#ifdef __cplusplus
+  using std::isfinite;
+#endif
+  double_d res;
+  res.high = a + b;
+  if (isfinite(res.high)) {
+    double v = res.high - a;
+    res.low = (a - (res.high - v)) + (b - v);
+  } else {
+    res.low = 0;
+  }
+  return res;
+}
+
+inline void split(double a, double& hi, double& lo) {
+  int const QD_BITS = (std::numeric_limits<double>::digits + 1) / 2;
+  static double const QD_SPLITTER = std::ldexp(1.0, QD_BITS) + 1.0;
+  static double const QD_SPLIT_THRESH
+      = std::ldexp((std::numeric_limits<double>::max)(), -QD_BITS - 1);
+
+  double temp;
+
+  if (std::abs(a) > QD_SPLIT_THRESH) {
+    a = std::ldexp(a, -QD_BITS - 1);
+    temp = QD_SPLITTER * a;
+    hi = temp - (temp - a);
+    lo = a - hi;
+    hi = std::ldexp(hi, QD_BITS + 1);
+    lo = std::ldexp(lo, QD_BITS + 1);
+  } else {
+    temp = QD_SPLITTER * a;
+    hi = temp - (temp - a);
+    lo = a - hi;
+  }
+}
+
+//    inline double_d mul_d_d(double a, double b) {
+//      double_d res;
+//      volatile double temp = a * b;
+//      res.high = temp;
+//      res.low = fma(a, b, -temp);
+//      return res;
+//    }
+inline double_d mul_d_d(double a, double b) {
+#ifdef __cplusplus
+  using std::isfinite;
+#endif
+  double_d res;
+  const double C = (2 << 26) + 1;
+  double p = a * C;
+  if (!isfinite(p)) {
+    res.high = p;
+    res.low = 0;
+    return res;
+  }
+  double hx = a - p + p;
+  double tx = a - hx;
+  p = b * C;
+  if (!isfinite(p)) {
+    res.high = p;
+    res.low = 0;
+    return res;
+  }
+  double hy = b - p + p;
+  double ty = b - hy;
+
+//  double hx, tx;
+//  split(a, hx, tx);
+//  double hy, ty;
+//  split(a, hy, ty);
+//  double p;
+
+
+  p = hx * hy;
+  double q = hx * ty + tx * hy;
+  res.high = p + q;
+  if (isfinite(res.high)) {
+    res.low = p - res.high + q + tx * ty;
+  } else {
+    res.low = 0;
+  }
+  return res;
+}
+
+inline double_d neg(double_d a) {
+  double_d res;
+  res.high = -a.high;
+  res.low = -a.low;
+  return res;
+}
+
+inline double_d add_dd_dd(double_d a, double_d b) {
+#ifdef __cplusplus
+  using std::isfinite;
+#endif
+  //      double_d high = add_d_d(a.high, b.high);
+  //      if (isfinite(high.high)) {
+  //        double_d low = add_d_d(a.low, b.low);
+  //        double_d mid = add_d_d(high.low, low.high);
+  //        mid.low += low.high;
+  //        double_d high2 = add_d_d(high.high, mid.high);
+  //        double_d mid2 = add_d_d(mid.low, high2.high);
+  //        double_d low2 = add_d_d(high2.low, mid2.low);
+  //        return { mid2.high, low2.high }
+
+  //        //        double_d v = add_quick_d_d(high.high, high.low +
+  //        low.high);
+  //        //        return add_quick_d_d(high.high, low.low + v.low);
+  //      } else {
+  //        return high;
+  //      }
+  double_d res;
+  double high = a.high + b.high;
+  if (isfinite(high)) {
+    double low;
+    if (fabs(a.high) > fabs(b.high)) {
+      low = a.high - high + b.high;
+    } else {
+      low = b.high - high + a.high;
+    }
+    low += a.low + b.low;
+    res.high = high + low;
+    res.low = high - res.high + low;
+  } else {
+    res.high = high;
+    res.low = 0;
+  }
+  return res;
+}
+
+inline double_d add_dd_d(double_d a, double b) {
+#ifdef __cplusplus
+  using std::isfinite;
+#endif
+  //      double_d high = add_d_d(a.high, b);
+  //      if (isfinite(high.high)) {
+  //        double_d low = add_d_d(a.low, b);
+  //        double_d high2 = add_d_d(high.high, low.high);
+  //        double_d mid2 = add_d_d(low.low, high2.high);
+  //        double_d low2 = add_d_d(high2.low, mid2.low);
+  //        return {mid2.high, low2.high};
+  //      } else {
+  //        return high;
+  //      }
+
+  //      //    double_d v = add_quick_d_d(high.high, high.low + a.low);
+  //      //    return add_quick_d_d(high.high, v.low);
+  double_d res;
+  double high = a.high + b;
+  if (isfinite(high)) {
+    double low;
+    if (fabs(a.high) > fabs(b)) {
+      low = a.high - high + b;
+    } else {
+      low = b - high + a.high;
+    }
+    low += a.low;
+    res.high = high + low;
+    res.low = high - res.high + low;
+  } else {
+    res.high = high;
+    res.low = 0;
+  }
+  return res;
+}
+
+inline double_d sub_dd_dd(double_d a, double_d b) {
+  return add_dd_dd(a, neg(b));
+}
+
+inline double_d sub_dd_d(double_d a, double b) { return add_dd_d(a, -b); }
+
+inline double_d sub_d_dd(double a, double_d b) { return add_dd_d(neg(b), a); }
+
+inline double_d mul_dd_dd(double_d a, double_d b) {
+#ifdef __cplusplus
+  using std::isfinite;
+#endif
+  double_d high = mul_d_d(a.high, b.high);
+  double_d res;
+  if (isfinite(high.high)) {
+    high.low += a.high * b.low + a.low * b.high;
+    res.high = high.high + high.low;
+    res.low = high.high - res.high + high.low;
+  } else {
+    high.low = 0;
+    return high;
+  }
+  return res;
+}
+
+inline double_d mul_dd_d(double_d a, double b) {
+#ifdef __cplusplus
+  using std::isfinite;
+#endif
+  double_d high = mul_d_d(a.high, b);
+  double_d res;
+  if (isfinite(high.high)) {
+    high.low += a.low * b;
+    res.high = high.high + high.low;
+    res.low = high.high - res.high + high.low;
+    return res;
+  } else {
+    high.low = 0;
+    return high;
+  }
+}
+
+inline double_d div_dd_dd(double_d a, double_d b) {
+#ifdef __cplusplus
+  using std::isfinite;
+  using std::isnan;
+#endif
+  double high = a.high / b.high;
+  double_d res;
+  if (isfinite(high)) {
+    double_d u = mul_d_d(high, b.high);
+    if (isfinite(u.high)) {
+      double low = (a.high - u.high - u.low + a.low - high * b.low) / b.high;
+      res.high = high + low;
+      res.low = high - res.high + low;
       return res;
     }
+  }
+  res.high = high;
+  res.low = 0;
+  return res;
+}
 
-    inline double_d add_d_d(double a, double b) {
+inline double_d div_dd_d(double_d a, double b) {
 #ifdef __cplusplus
-      using std::isfinite;
+  using std::isfinite;
 #endif
-      double_d res;
-      res.high = a + b;
-      if (isfinite(res.high)) {
-        double v = res.high - a;
-        res.low = (a - (res.high - v)) + (b - v);
-      } else {
-        res.low = 0;
-      }
-      return res;
-    }
+  double high = a.high / b;
+  double_d res;
+  if (isfinite(high)) {
+    double_d u = mul_d_d(high, b);
+    double low = (a.high - u.high - u.low + a.low) / b;
+    res.high = high + low;
+    res.low = high - res.high + low;
+  } else {
+    res.high = high;
+    res.low = 0;
+  }
+  return res;
+}
 
-    inline double_d mul_d_d(double a, double b) {
-      double_d res;
-      res.high = a * b;
-      res.low = fma(a, b, -res.high);
-      return res;
-    }
-
-    inline double_d neg(double_d a) {
-      double_d res;
-      res.high = -a.high;
-      res.low = -a.low;
-      return res;
-    }
-
-    inline double_d add_dd_dd(double_d a, double_d b) {
+inline double_d div_d_dd(double a, double_d b) {
 #ifdef __cplusplus
-      using std::isfinite;
+  using std::isfinite;
 #endif
-      //      double_d high = add_d_d(a.high, b.high);
-      //      if (isfinite(high.high)) {
-      //        double_d low = add_d_d(a.low, b.low);
-      //        double_d mid = add_d_d(high.low, low.high);
-      //        mid.low += low.high;
-      //        double_d high2 = add_d_d(high.high, mid.high);
-      //        double_d mid2 = add_d_d(mid.low, high2.high);
-      //        double_d low2 = add_d_d(high2.low, mid2.low);
-      //        return { mid2.high, low2.high }
+  double high = a / b.high;
+  double_d res;
+  if (isfinite(high)) {
+    double_d u = mul_d_d(high, b.high);
+    double low = (a - u.high - u.low - high * b.low) / b.high;
+    res.high = high + low;
+    res.low = high - res.high + low;
+  } else {
+    res.high = high;
+    res.low = 0;
+  }
+  return res;
+}
 
-      //        //        double_d v = add_quick_d_d(high.high, high.low +
-      //        low.high);
-      //        //        return add_quick_d_d(high.high, low.low + v.low);
-      //      } else {
-      //        return high;
-      //      }
-      double_d res;
-      double r = a.high + b.high;
-      if (isfinite(r)) {
-        double s;
-        if (fabs(a.high) > fabs(b.high)) {
-          s = a.high - r + b.high;
-        } else {
-          s = b.high - r + a.high;
-        }
-        s += a.low + b.low;
-        res.high = r + s;
-        res.low = r - res.high + s;
-      } else {
-        res.high = r;
-        res.low = 0;
-      }
-      return res;
-    }
-
-    inline double_d add_dd_d(double_d a, double b) {
+inline double copysign(double a, double_d b) {
 #ifdef __cplusplus
-      using std::isfinite;
+  using std::copysign;
 #endif
-      //      double_d high = add_d_d(a.high, b);
-      //      if (isfinite(high.high)) {
-      //        double_d low = add_d_d(a.low, b);
-      //        double_d high2 = add_d_d(high.high, low.high);
-      //        double_d mid2 = add_d_d(low.low, high2.high);
-      //        double_d low2 = add_d_d(high2.low, mid2.low);
-      //        return {mid2.high, low2.high};
-      //      } else {
-      //        return high;
-      //      }
+  return copysign(a, b.high);
+}
 
-      //      //    double_d v = add_quick_d_d(high.high, high.low + a.low);
-      //      //    return add_quick_d_d(high.high, v.low);
-      double_d res;
-      double r = a.high + b;
-      if (isfinite(r)) {
-        double s;
-        if (fabs(a.high) > fabs(b)) {
-          s = a.high - r + b;
-        } else {
-          s = b - r + a.high;
-        }
-        s += a.low;
-        res.high = r + s;
-        res.low = r - res.high + s;
-      } else {
-        res.high = r;
-        res.low = 0;
-      }
-      return res;
-    }
-
-    inline double_d sub_dd_dd(double_d a, double_d b) {
-      return add_dd_dd(a, neg(b));
-    }
-
-    inline double_d sub_dd_d(double_d a, double b) { return add_dd_d(a, -b); }
-
-    inline double_d sub_d_dd(double a, double_d b) {
-      return add_dd_d(neg(b), a);
-    }
-
-    inline double_d mul_dd_dd(double_d a, double_d b) {
+inline double_d fabs(double_d a) {
 #ifdef __cplusplus
-      using std::isfinite;
+  using std::fabs;
 #endif
-      double_d high = mul_d_d(a.high, b.high);
-      double_d res;
-      if (isfinite(high.high)) {
-        high.low += a.high * b.low + a.low * b.high;
-        res.high = high.high + high.low;
-        res.low = high.high - res.high + high.low;
-      } else {
-        high.low = 0;
-        return high;
-      }
-      return res;
-    }
+  return a.high > 0 ? a : neg(a);
+}
 
-    inline double_d mul_dd_d(double_d a, double b) {
+inline bool isnan(double_d a) {
 #ifdef __cplusplus
-      using std::isfinite;
+  using std::isnan;
 #endif
-      double_d high = mul_d_d(a.high, b);
-      double_d res;
-      if (isfinite(high.high)) {
-        high.low += a.low * b;
-        res.high = high.high + high.low;
-        res.low = high.high - res.high + high.low;
-        return res;
-      } else {
-        high.low = 0;
-        return high;
-      }
-    }
+  return isnan(a.high);
+}
 
-    inline double_d div_dd_dd(double_d a, double_d b) {
+inline bool isinf(double_d a) {
 #ifdef __cplusplus
-      using std::isfinite;
+  using std::isinf;
 #endif
-      double high = a.high / b.high;
-      double_d res;
-      if (isfinite(high)) {
-        double_d u = mul_d_d(high, b.high);
-        double low = (a.high - u.high - u.low + a.low - high * b.low) / b.high;
-        res.high = high + low;
-        res.low = high - res.high + low;
-      } else {
-        res.high = high;
-        res.low = 0;
-      }
-      return res;
-    }
+  return isinf(a.high);
+}
 
-    inline double_d div_dd_d(double_d a, double b) {
-#ifdef __cplusplus
-      using std::isfinite;
-#endif
-      double high = a.high / b;
-      double_d res;
-      if (isfinite(high)) {
-        double_d u = mul_d_d(high, b);
-        double low = (a.high - u.high - u.low + a.low) / b;
-        res.high = high + low;
-        res.low = high - res.high + low;
-      } else {
-        res.high = high;
-        res.low = 0;
-      }
-      return res;
-    }
+inline bool lt_dd_dd(double_d a, double_d b) {
+  return a.high < b.high || (a.high == b.high && a.low < b.low);
+}
 
-    inline double_d div_d_dd(double a, double_d b) {
-#ifdef __cplusplus
-      using std::isfinite;
-#endif
-      double high = a / b.high;
-      double_d res;
-      if (isfinite(high)) {
-        double_d u = mul_d_d(high, b.high);
-        double low = (a - u.high - u.low - high * b.low) / b.high;
-        res.high = high + low;
-        res.low = high - res.high + low;
-      } else {
-        res.high = high;
-        res.low = 0;
-      }
-      return res;
-    }
+inline bool lt_d_dd(double a, double_d b) { return lt_dd_dd({a, 0}, b); }
 
-    inline double copysign(double a, double_d b) {
-#ifdef __cplusplus
-      using std::copysign;
-#endif
-      return copysign(a, b.high);
-    }
+inline bool lt_dd_d(double_d a, double b) { return lt_dd_dd(a, {b, 0}); }
 
-    inline double_d fabs(double_d a) {
-#ifdef __cplusplus
-      using std::fabs;
-#endif
-      return a.high > 0 ? a : neg(a);
-    }
+inline bool gt_dd_dd(double_d a, double_d b) {
+  return a.high > b.high || (a.high == b.high && a.low > b.low);
+}
 
-    inline bool isnan(double_d a) {
-#ifdef __cplusplus
-      using std::isnan;
-#endif
-      return isnan(a.high);
-    }
+inline bool gt_d_dd(double a, double_d b) { return gt_dd_dd({a, 0}, b); }
 
-    inline bool isinf(double_d a) {
-#ifdef __cplusplus
-      using std::isinf;
-#endif
-      return isinf(a.high);
-    }
+inline bool gt_dd_d(double_d a, double b) { return gt_dd_dd(a, {b, 0}); }
 
-    inline bool lt_dd_dd(double_d a, double_d b) {
-      return a.high < b.high || (a.high == b.high && a.low < b.low);
-    }
+inline bool le_dd_dd(double_d a, double_d b) { return !gt_dd_dd(a, b); }
+inline bool le_d_dd(double a, double_d b) { return !gt_d_dd(a, b); }
+inline bool le_dd_d(double_d a, double b) { return !gt_dd_d(a, b); }
 
-    inline bool lt_d_dd(double a, double_d b) {
-      return lt_dd_dd({a, 0}, b);
-    }
+inline bool ge_dd_dd(double_d a, double_d b) { return !lt_dd_dd(a, b); }
+inline bool ge_d_dd(double a, double_d b) { return !lt_d_dd(a, b); }
+inline bool ge_dd_d(double_d a, double b) { return !lt_dd_d(a, b); }
 
-    inline bool lt_dd_d(double_d a, double b) {
-      return lt_dd_dd(a, {b, 0});
-    }
-
-    inline bool gt_dd_dd(double_d a, double_d b) {
-      return a.high > b.high || (a.high == b.high && a.low > b.low);
-    }
-
-    inline bool gt_d_dd(double a, double_d b) {
-      return gt_dd_dd({a, 0}, b);
-    }
-
-    inline bool gt_dd_d(double_d a, double b) {
-      return gt_dd_dd(a, {b, 0});
-    }
-
-    inline bool le_dd_dd(double_d a, double_d b) {
-      return !gt_dd_dd(a, b);
-    } inline bool le_d_dd(double a, double_d b) {
-      return !gt_d_dd(a, b);
-    } inline bool le_dd_d(double_d a, double b) { return !gt_dd_d(a, b); }
-
-    inline bool ge_dd_dd(double_d a, double_d b) {
-      return !lt_dd_dd(a, b);
-    } inline bool ge_d_dd(double a, double_d b) {
-      return !lt_d_dd(a, b);
-    } inline bool ge_dd_d(double_d a, double b) { return !lt_dd_d(a, b); }
-
-    // \cond
+// \cond
 //);
 // \endcond
 
-inline double_d operator+(double_d a, double b) { return add_dd_d(a, b); }
+// inline double_d operator+(double_d a, double b) { return add_dd_d(a, b); }
 inline double_d operator+(double a, double_d b) { return add_dd_d(b, a); }
 inline double_d operator+(double_d a, double_d b) { return add_dd_dd(a, b); }
 
 inline double_d operator-(double a, double_d b) { return sub_d_dd(a, b); }
-inline double_d operator-(double_d a, double b) { return sub_dd_d(a, b); }
+// inline double_d operator-(double_d a, double b) { return sub_dd_d(a, b); }
 inline double_d operator-(double_d a, double_d b) { return sub_dd_dd(a, b); }
 
 inline double_d operator*(double_d a, double b) { return mul_dd_d(a, b); }
@@ -324,11 +378,11 @@ inline bool operator<(double_d a, double b) { return lt_dd_d(a, b); }
 inline bool operator>(double_d a, double_d b) { return gt_dd_dd(a, b); }
 inline bool operator>(double a, double_d b) { return gt_d_dd(a, b); }
 inline bool operator>(double_d a, double b) { return gt_dd_d(a, b); }
-inline bool operator<=(double_d a, double_d b) { return le_dd_dd(a, b); }
-inline bool operator<=(double a, double_d b) { return le_d_dd(a, b); }
-inline bool operator<=(double_d a, double b) { return le_dd_d(a, b); }
-inline bool operator>=(double_d a, double_d b) { return ge_dd_dd(a, b); }
-inline bool operator>=(double a, double_d b) { return ge_d_dd(a, b); }
+// inline bool operator<=(double_d a, double_d b) { return le_dd_dd(a, b); }
+// inline bool operator<=(double a, double_d b) { return le_d_dd(a, b); }
+// inline bool operator<=(double_d a, double b) { return le_dd_d(a, b); }
+// inline bool operator>=(double_d a, double_d b) { return ge_dd_dd(a, b); }
+// inline bool operator>=(double a, double_d b) { return ge_d_dd(a, b); }
 inline bool operator>=(double_d a, double b) { return ge_dd_d(a, b); }
 
 }  // namespace internal
--- test/unit/math/opencl/double_d_test.cpp
+++ test/unit/math/opencl/double_d_test.cpp
@@ -1,7 +1,9 @@
 
 #include <stan/math/opencl/double_d.hpp>
+//#include <test/unit/math/expect_near_rel.hpp>
 #include <gtest/gtest.h>
 #include <limits>
+#include <Eigen/Core>
 
 #define EXPECT_NORMALIZED(a) \
   EXPECT_LT(std::abs(a.low), \
@@ -41,7 +43,7 @@ TEST(double_d, mul_dd_dd_test) {
   double eps = std::numeric_limits<double>::epsilon();
   double h_eps = eps * 0.5;
   double_d a{1.0, h_eps * 0.001};
-  double_d b{h_eps, h_eps * h_eps * h_eps* h_eps};
+  double_d b{h_eps, h_eps * h_eps * h_eps * h_eps};
   double_d c{1.0, h_eps};
   double_d d{1.0, -h_eps};
 
@@ -65,12 +67,12 @@ TEST(double_d, mul_dd_dd_test) {
 }
 
 TEST(double_d, div_dd_dd_test) {
-  using stan::math::internal::double_d;
   using stan::math::internal::div_dd_dd;
+  using stan::math::internal::double_d;
   double eps = std::numeric_limits<double>::epsilon();
   double h_eps = eps * 0.5;
   double_d a{1.0, h_eps};
-  double_d b{1.0, h_eps*(1-eps)};
+  double_d b{1.0, h_eps * (1 - eps)};
   double_d c{0.0, 0.0};
   double_d d{1.0, -h_eps};
 
@@ -83,13 +85,53 @@ TEST(double_d, div_dd_dd_test) {
   res = div_dd_dd(a, b);
   EXPECT_NORMALIZED(res);
   EXPECT_TRUE(res.high == 1.0);
-  EXPECT_TRUE(res.low == h_eps*eps);
+  EXPECT_TRUE(res.low == h_eps * eps);
 
-  //div by zero
+  // div by zero
   res = div_dd_dd(a, c);
   EXPECT_TRUE(res.high == std::numeric_limits<double>::infinity());
   EXPECT_TRUE(res.low == 0);
 }
+
+#include <quadmath.h>
+
+#define EXPECT_DD_F128_EQ(dd, f128) \
+  tmp = (dd);              \
+  EXPECT_NEAR((__float128)tmp.high + tmp.low, (f128), 1e-30*1e30)
+
+TEST(double_d, all) {
+  using stan::math::internal::double_d;
+  for (int i = 0; i < 10000000; i++) {
+    double_d tmp;
+    double high = Eigen::MatrixXd::Random(0, 0).coeff(0, 0)*1e30;
+    double low = Eigen::MatrixXd::Random(0, 0).coeff(0, 0) * 1e-20*1e30;
+    double_d dd = high;
+    dd.low = low;
+    __float128 f128 = high;
+    f128 += low;
+    EXPECT_DD_F128_EQ(dd, f128);
+
+    double high2 = Eigen::MatrixXd::Random(0, 0).coeff(0, 0);
+    double low2 = Eigen::MatrixXd::Random(0, 0).coeff(0, 0) * 1e-20;
+    double_d dd2 = high2;
+    dd2.low = low2;
+    __float128 f1282 = high2;
+    f1282 += low2;
+    EXPECT_DD_F128_EQ(dd2, f1282);
+
+    EXPECT_DD_F128_EQ(dd + dd2, f128 + f1282);
+    EXPECT_DD_F128_EQ(dd - dd2, f128 - f1282);
+    EXPECT_DD_F128_EQ(dd * dd2, f128 * f1282);
+    EXPECT_DD_F128_EQ(dd / dd2, f128 / f1282);
+
+    __float128 f1283 = high;
+    __float128 f1284 = high2;
+    EXPECT_DD_F128_EQ(stan::math::internal::mul_d_d(high, high2), f1283 * f1284);
+  }
+}
+
+
+
 
 #ifdef STAN_OPENCL
 
