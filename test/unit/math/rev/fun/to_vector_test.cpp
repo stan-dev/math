4f18acde110e3a067a54446347a56100da2e715e
Steve Bronder <stevo15025@gmail.com>
2021-06-28 17:16:11 -0400

    adds check that adj is referenced w/o a copy

--- test/unit/math/rev/fun/to_vector_test.cpp
+++ test/unit/math/rev/fun/to_vector_test.cpp
@@ -7,7 +7,9 @@ TEST(MathFunRev, to_vector_var_value) {
   constexpr Eigen::Index n = 100;
   Eigen::MatrixXd a_val = Eigen::MatrixXd::Random(n, n);
   stan::math::var_value<Eigen::MatrixXd> a(a_val);
-  const auto& tmp = stan::math::to_vector(a);
+  auto&& tmp = stan::math::to_vector(a);
   EXPECT_TRUE((stan::is_col_vector<decltype(tmp.val())>::value));
   EXPECT_MATRIX_EQ(tmp.val(), Eigen::Map<Eigen::VectorXd>(a_val.data(), a.rows() * a.cols()));
+  tmp.adj().array() += 1.0;
+  EXPECT_MATRIX_EQ(tmp.adj(), Eigen::Map<Eigen::VectorXd>(a.vi_->adj_.data(), a.rows() * a.cols()));
 }
