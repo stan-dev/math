7f6a0491bc63dad3343842a66967139d08766a4b
Steve Bronder <stevo15025@gmail.com>
2021-07-16 19:23:14 -0400

    fix includes

--- stan/math/rev/fun/rep_row_vector.hpp
+++ stan/math/rev/fun/rep_row_vector.hpp
@@ -5,6 +5,7 @@
 #include <stan/math/rev/core.hpp>
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/prim/err.hpp>
+#include <stan/math/prim/fun/rep_row_vector.hpp>
 
 namespace stan {
 namespace math {
