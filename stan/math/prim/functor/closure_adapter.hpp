6990768136daeb463f89a077e34ddc80e770480b
Niko Huurre <nhuurre@protonmail.com>
2021-07-19 14:42:45 +0300

    generalize base_closure

--- stan/math/prim/functor/closure_adapter.hpp
+++ stan/math/prim/functor/closure_adapter.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/prim/meta/error_index.hpp>
 #include <stan/math/prim/meta/is_stan_closure.hpp>
 #include <stan/math/prim/meta/return_type.hpp>
+#include <stan/math/prim/functor/apply.hpp>
 #include <ostream>
 
 namespace stan {
@@ -11,64 +12,51 @@ namespace math {
 namespace internal {
 
 /**
- * A closure that wraps a C++ lambda.
+ * A closure that wraps a C++ lambda and captures values.
  */
-template <typename F>
-struct empty_closure {
-  using captured_scalar_t__ = double;
-  using ValueOf__ = empty_closure<F>;
-  using CopyOf__ = empty_closure<F>;
-  F f_;
-
-  explicit empty_closure(const F& f) : f_(f) {}
-
-  template <typename... Args>
-  auto operator()(std::ostream* msgs, Args... args) const {
-    return f_(args..., msgs);
-  }
-  size_t count_vars__() const { return 0; }
-  auto value_of__() const { return ValueOf__(f_); }
-  auto copy_of__() const { return CopyOf__(f_); }
-  auto deep_copy_vars__() const { return CopyOf__(f_); }
-  void zero_adjoints__() const {}
-  double* accumulate_adjoints__(double* dest) const { return dest; }
-  template <typename Vari>
-  Vari** save_varis(Vari** dest) const {
-    return dest;
-  }
-};
-
-/**
- * A closure that holds one autodiffable capture.
- */
-template <bool Ref, typename F, typename T>
-struct one_arg_closure {
-  using captured_scalar_t__ = return_type_t<T>;
+template <bool Ref, typename F, typename... Ts>
+struct base_closure {
+  using captured_scalar_t__ = return_type_t<Ts...>;
   using ValueOf__
-      = one_arg_closure<false, F, decltype(eval(value_of(std::declval<T>())))>;
-  using CopyOf__ = one_arg_closure<false, F, T>;
+      = base_closure<false, F,
+                         decltype(eval(value_of(std::declval<Ts>())))...>;
+  using CopyOf__ = base_closure<false, F, Ts...>;
   F f_;
-  capture_type_t<T, Ref> s_;
+  std::tuple<capture_type_t<Ts, Ref>...> captures_;
 
-  explicit one_arg_closure(const F& f, const T& s) : f_(f), s_(s) {}
+  explicit base_closure(const F& f, const Ts&... args)
+      : f_(f), captures_(args...) {}
 
   template <typename... Args>
   auto operator()(std::ostream* msgs, Args... args) const {
-    return f_(s_, args..., msgs);
+    return apply(
+        [this, msgs, args...](auto... s) { return f_(s..., args..., msgs); },
+        captures_);
   }
-  size_t count_vars__() const { return count_vars(s_); }
-  auto value_of__() const { return ValueOf__(f_, eval(value_of(s_))); }
-  auto copy_of__() const { return CopyOf__(f_, s_); }
+  size_t count_vars__() const {
+    return apply([this](auto... s) { return count_vars(s...); }, captures_);
+  }
+  auto value_of__() const {
+    return apply(
+        [this](auto... s) { return ValueOf__(f_, eval(value_of(s))...); },
+        captures_);
+  }
   auto deep_copy_vars__() const {
-    return CopyOf__(f_, eval(deep_copy_vars(s_)));
+    return apply(
+        [this](auto... s) { return CopyOf__(f_, eval(deep_copy_vars(s))...); },
+        captures_);
   }
-  void zero_adjoints__() { zero_adjoints(s_); }
+  void zero_adjoints__() {
+    apply([](auto... s) { zero_adjoints(s...); }, captures_);
+  }
   double* accumulate_adjoints__(double* dest) const {
-    return accumulate_adjoints(dest, s_);
+    return apply([dest](auto... s) { return accumulate_adjoints(dest, s...); },
+                 captures_);
   }
   template <typename Vari>
   Vari** save_varis__(Vari** dest) const {
-    return save_varis(dest, s_);
+    return apply([dest](auto... s) { return save_varis(dest, s...); },
+                 captures_);
   }
 };
 
@@ -173,19 +161,11 @@ struct ode_closure_adapter {
 };
 
 /**
- * Create a closure object from a callable.
+ * Create a closure from a C++ lambda and captures.
  */
-template <typename F>
-auto from_lambda(const F& f) {
-  return internal::empty_closure<F>(f);
-}
-
-/**
- * Create a closure that captures a single argument.
- */
-template <typename F, typename T>
-auto from_lambda(const F& f, const T& a) {
-  return internal::one_arg_closure<true, F, T>(f, a);
+template <typename F, typename... Ts>
+auto from_lambda(const F& f, const Ts&... a) {
+  return internal::base_closure<true, F, Ts...>(f, a...);
 }
 
 /**
