7d72591f47a60e1983f1003c9ece33f30c95b7c1
Steve Bronder <stevo15025@gmail.com>
2021-07-22 14:04:52 -0400

    cleanup template for prim pow()

--- stan/math/prim/fun/pow.hpp
+++ stan/math/prim/fun/pow.hpp
@@ -39,46 +39,13 @@ inline complex_return_t<U, V> complex_pow(const U& x, const V& y) {
  * @return pow function applied to the two inputs.
  */
 template <typename T1, typename T2, require_any_container_t<T1, T2>* = nullptr,
-          require_all_not_matrix_t<T1, T2>* = nullptr>
+ require_all_not_matrix_st<is_var, T1, T2>* = nullptr>
 inline auto pow(const T1& a, const T2& b) {
   return apply_scalar_binary(a, b, [&](const auto& c, const auto& d) {
     using std::pow;
     return pow(c, d);
   });
 }
-
-/**
- * Specialization for Matrices, scalars, and Eigen matrices of mixed scalar
- * types.
- *
- * @tparam T1 A Scalar or type derived from EigenBase
- * @tparam T2 A Scalar or type derived from EigenBase
- * @param a base.
- * @param b The exponent to raise the base to.
- */
-template <typename T1, typename T2, require_any_eigen_t<T1, T2>* = nullptr,
-          require_all_not_st_var<T1, T2>* = nullptr,
-          require_any_not_matrix_vt<std::is_arithmetic, T1, T2>* = nullptr>
-inline auto pow(const T1& a, const T2& b) {
-  return apply_scalar_binary(a, b, [&](const auto& c, const auto& d) {
-    using std::pow;
-    return pow(c, d);
-  });
-}
-
-/**
- * Specialization for Arithmetic matrices
- * @tparam T1 Type derived from EigenBase with an Arithmetic scalar
- * @tparam T2 Type derived from EigenBase with an Arithmetic scalar
- * @param a base.
- * @param b The exponent to raise the base to.
- */
-template <typename T1, typename T2,
-          require_all_matrix_vt<std::is_arithmetic, T1, T2>* = nullptr>
-inline auto pow(const T1& base, const T2& exponent) {
-  return base.array().pow(exponent.array()).matrix();
-}
-
 }  // namespace math
 }  // namespace stan
 #endif
