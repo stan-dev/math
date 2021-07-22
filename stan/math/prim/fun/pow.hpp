023bf9ec84af1f7ff4ae77b54f8d9e2e5fae1056
Stan Jenkins <mc.stanislaw@gmail.com>
2021-07-22 18:37:38 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/fun/pow.hpp
+++ stan/math/prim/fun/pow.hpp
@@ -39,7 +39,7 @@ inline complex_return_t<U, V> complex_pow(const U& x, const V& y) {
  * @return pow function applied to the two inputs.
  */
 template <typename T1, typename T2, require_any_container_t<T1, T2>* = nullptr,
- require_all_not_matrix_st<is_var, T1, T2>* = nullptr>
+          require_all_not_matrix_st<is_var, T1, T2>* = nullptr>
 inline auto pow(const T1& a, const T2& b) {
   return apply_scalar_binary(a, b, [&](const auto& c, const auto& d) {
     using std::pow;
