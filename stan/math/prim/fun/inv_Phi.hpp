9f2eac98be601f9836d21ce85c5e763e938d46f9
Stan Jenkins <mc.stanislaw@gmail.com>
2021-08-28 15:52:23 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/fun/inv_Phi.hpp
+++ stan/math/prim/fun/inv_Phi.hpp
@@ -146,7 +146,8 @@ inline double inv_Phi_lambda(double p) {
  * @return Real value of the inverse cdf for the standard normal distribution.
  */
 inline double inv_Phi(double p) {
-  return p >= 0.9999 ? -internal::inv_Phi_lambda((large_int - large_int * p) / large_int)
+  return p >= 0.9999 ? -internal::inv_Phi_lambda((large_int - large_int * p)
+                                                 / large_int)
                      : internal::inv_Phi_lambda(p);
 }
 
