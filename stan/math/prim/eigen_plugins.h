4b374485652aef45d6f137facd9e1efe4e7a2b5a
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2021-10-18 00:53:24 +0800

    Revert "Remove coefreff"
    This reverts commit 264ef56b4097aec5ab1a13db6bde044310f0a9e2.

--- stan/math/prim/eigen_plugins.h
+++ stan/math/prim/eigen_plugins.h
@@ -5,5 +5,14 @@
 #include "plugins/d_view.h"
 #include "plugins/vi_view.h"
 
+
+
+EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE
+const Scalar& coeffRef(Index row, Index col) const {
+    eigen_internal_assert(row >= 0 && row < rows()
+                          && col >= 0 && col < cols());
+    return internal::evaluator<Derived>(derived()).coeffRef(row, col);
+}
+
 #define EIGEN_STAN_MATRIXBASE_PLUGIN
 #define EIGEN_STAN_ARRAYBASE_PLUGIN
