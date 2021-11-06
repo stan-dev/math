4075c0963c23cfe81facabdda672c718dd36960f
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2021-11-06 21:20:17 +0100

    add requires

--- stan/math/prim/fun/fmod.hpp
+++ stan/math/prim/fun/fmod.hpp
@@ -18,7 +18,7 @@ namespace math {
  * @param b Second input
  * @return fmod function applied to the two inputs.
  */
-template <typename T1, typename T2>
+template <typename T1, typename T2, require_all_not_nonscalar_prim_or_rev_kernel_expression_t<T1, T2>* = nullptr>
 inline auto fmod(const T1& a, const T2& b) {
   return apply_scalar_binary(a, b, [&](const auto& c, const auto& d) {
     using std::fmod;
