7adbc75e21edd13c5195beebd408075d5f7583b3
Lyndon D <lyndon.duong@gmail.com>
2021-12-31 14:59:37 -0500

    added forward-mode specializations

---
+++ stan/math/fwd/fun/norm1.hpp
@@ -0,0 +1,37 @@
+#ifndef STAN_MATH_FWD_FUN_LOG_SUM_EXP_HPP
+#define STAN_MATH_FWD_FUN_LOG_SUM_EXP_HPP
+
+#include <stan/math/fwd/meta.hpp>
+#include <stan/math/fwd/core.hpp>
+#include <stan/math/prim/meta.hpp>
+#include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/prim/fun/constants.hpp>
+#include <stan/math/prim/fun/norm1.hpp>
+#include <stan/math/prim/fun/to_ref.hpp>
+#include <cmath>
+#include <vector>
+
+namespace stan {
+namespace math {
+
+/**
+ * Compute the L1 norm of the specified vector of values.
+ *
+ * @tparam T Type of input vector.
+ * @param[in] x Vector of specified values.
+ * @return L1 norm of x.
+ */
+template <typename Container,
+          require_container_st<is_fvar, Container>* = nullptr>
+inline auto norm1(const Container& x) {
+  return apply_vector_unary<ref_type_t<T>>::reduce(
+      to_ref(x), [&](const auto& v) {
+        using T_fvar_inner = typename value_type_t<decltype(v)>::Scalar;
+        return fvar<T_fvar_inner>(
+            norm1(v.val().array()), v.d().array() * sign(v.val().array()));
+      });
+}
+
+}  // namespace math
+}  // namespace stan
+#endif
