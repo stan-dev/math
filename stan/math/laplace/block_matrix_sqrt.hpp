7a401422d76fd24bc5b0e3430e5bf1e75cb54036
Steve Bronder <stevo15025@gmail.com>
2022-01-27 12:26:49 -0500

    update tolerance for nearness tests

--- stan/math/laplace/block_matrix_sqrt.hpp
+++ stan/math/laplace/block_matrix_sqrt.hpp
@@ -11,36 +11,11 @@ namespace math {
  * Return the matrix square-root for a block diagonal matrix.
  */
 inline Eigen::SparseMatrix<double> block_matrix_sqrt(
-    Eigen::SparseMatrix<double> W, const Eigen::Index block_size) {
+    const Eigen::SparseMatrix<double>& W, const Eigen::Index block_size) {
   const Eigen::Index n_block = W.cols() / block_size;
   Eigen::MatrixXd local_block(block_size, block_size);
-  Eigen::MatrixXd local_block_sqrt(block_size, block_size);
   Eigen::SparseMatrix<double> W_root(W.rows(), W.cols());
-  W_root.reserve(Eigen::VectorXi::Constant(W_root.cols(), block_size));
-
-  // No block operation available for sparse matrices, so we have to loop.
-  // See https://eigen.tuxfamily.org/dox/group__TutorialSparse.html#title7
-  for (Eigen::Index i = 0; i < n_block; i++) {
-    for (Eigen::Index j = 0; j < block_size; j++) {
-      for (Eigen::Index k = 0; k < block_size; k++) {
-        local_block(j, k) = W.coeffRef(i * block_size + j, i * block_size + k);
-      }
-    }
-
-    local_block_sqrt = local_block.sqrt();
-    // local_block_sqrt = cholesky_decompose(local_block);
-
-    for (Eigen::Index j = 0; j < block_size; j++) {
-      for (Eigen::Index k = 0; k < block_size; k++) {
-        W_root.insert(i * block_size + j, i * block_size + k)
-            = local_block_sqrt(j, k);
-      }
-    }
-  }
-
-  W_root.makeCompressed();
-
-  return W_root;
+  return W.sqrt();
 }
 
 }  // namespace math
