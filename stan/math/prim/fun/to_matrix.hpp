3b1193ba73d513f14e7360edf33c4cf9475cc866
Rok Češnovar <rok.cesnovar@fri.uni-lj.si>
2022-02-22 08:18:00 +0100

    break lines

--- stan/math/prim/fun/to_matrix.hpp
+++ stan/math/prim/fun/to_matrix.hpp
@@ -34,10 +34,11 @@ inline EigMat to_matrix(EigMat&& x) {
  * @return the matrix representation of the input
  */
 template <typename EigMat, require_eigen_vector_t<EigMat>* = nullptr>
-inline Eigen::Matrix<value_type_t<EigMat>, Eigen::Dynamic, Eigen::Dynamic> to_matrix(
-    const EigMat& matrix) {
+inline Eigen::Matrix<value_type_t<EigMat>, Eigen::Dynamic, Eigen::Dynamic> 
+  to_matrix(const EigMat& matrix) {
   using T = value_type_t<EigMat>;
-  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> res(matrix.rows(), matrix.cols());
+  Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> 
+      res(matrix.rows(), matrix.cols());
   Eigen::Map<
       Eigen::Matrix<T, EigMat::RowsAtCompileTime, EigMat::ColsAtCompileTime>>
       res_map(res.data(), matrix.rows(), matrix.cols());
