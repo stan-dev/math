31092cecb9cd5a9b5301abe21e16ff1bf186446c
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2022-03-31 12:13:26 +0200

    fix varmat tests

--- test/varmat_compatibility_test.py
+++ test/varmat_compatibility_test.py
@@ -17,7 +17,7 @@ class VarmatCompatibilityMainTest(unittest.TestCase):
         self.assertSetEqual(set(["compatible_signatures", "incompatible_signatures", "irrelevant_signatures"]), set(results.keys()))
         self.assertSetEqual(set(["sum(row_vector) => real\n", "sum(vector) => real\n", "sum(matrix) => real\n"]), set(results["compatible_signatures"]))
         self.assertEqual(len(results["incompatible_signatures"]), 0)
-        self.assertSetEqual(set(["sum(array[] int) => int\n", "sum(array[] real) => real\n"]), set(results["irrelevant_signatures"]))
+        self.assertSetEqual(set(["sum(array[] int) => int\n", "sum(array[] real) => real\n", "sum(complex_row_vector) => complex\n", "sum(array[] complex) => complex\n", "sum(complex_matrix) => complex\n", "sum(complex_vector) => complex\n"]), set(results["irrelevant_signatures"]))
 
         os.remove(f.name)
 
