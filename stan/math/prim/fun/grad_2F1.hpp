b619b90bf06de34b4458075421b57b2641af2919
spinkney <sean.pinkney@gmail.com>
2022-05-04 15:35:46 -0400

    reviewer comments

--- stan/math/prim/fun/grad_2F1.hpp
+++ stan/math/prim/fun/grad_2F1.hpp
@@ -8,6 +8,7 @@
 #include <stan/math/prim/fun/fabs.hpp>
 #include <stan/math/prim/fun/inv.hpp>
 #include <stan/math/prim/fun/log.hpp>
+#include <stan/math/prim/fun/fmax.hpp>
 #include <stan/math/prim/fun/sign.hpp>
 #include <cmath>
 
@@ -19,33 +20,33 @@ namespace internal {
  * hypergeometric function (2F1).
  *
  * @tparam T scalar type or `var_value`
- * @tparam T_array array of doubles or `var_values`
- * @tparam T_array_int array of int
+ * @tparam T_StdVec std::array of doubles or `var_values`
+ * @tparam T_StdVecInt array of int
  * @tparam T_int integer
  * @param[out] log_g_old log of previous gradient values
  * @param[out] log_g_old_sign sign of previous gradient values
  * @param[out] log_t_new log of power series calculation
  * @param[out] log_t_new_sign sign of power series calcuation
  * @param[in] p intermediate power series value
- * @param[in] log_z log_z see hypergeometric function 2F1 definition
+ * @param[in] log_z see hypergeometric function 2F1 definition
  * @param[in] log_t_old log of previous power series calculation
  * @param[in] log_t_old_sign log of previous power series calculation
  * @param[in] k loop iteration
- * @param[in] a1 a1 see hypergeometric function 2F1 definition
- * @param[in] a2 a2 see hypergeometric function 2F1 definition
- * @param[in] b1 b1 see hypergeometric function 2F1 definition
+ * @param[in] a1 see hypergeometric function 2F1 definition
+ * @param[in] a2 see hypergeometric function 2F1 definition
+ * @param[in] b1 see hypergeometric function 2F1 definition
  */
-template <typename T, typename T_array, typename T_array_int, typename T_int>
-void calc_lambda(T_array& log_g_old, T_array_int& log_g_old_sign, T& log_t_new,
+template <typename T, typename T_StdVec, typename T_StdVecInt, typename T_int>
+void calc_lambda(T_StdVec& log_g_old, T_StdVecInt& log_g_old_sign, T& log_t_new,
                  T_int& log_t_new_sign, const T& p, const T& log_z,
                  const T& log_t_old, const T_int& log_t_old_sign, const int k,
                  const T& a1, const T& a2, const T& b1) {
-  using T_plain = return_type_t<T, T_int, T_array, T_array_int>;
+  using ret_t = return_type_t<T, T_int, T_StdVec, T_StdVecInt>;
 
   log_t_new += log(fabs(p)) + log_z;
   log_t_new_sign = p >= 0.0 ? log_t_new_sign : -log_t_new_sign;
 
-  T_plain term
+  ret_t term
       = log_g_old_sign[0] * log_t_old_sign * exp(log_g_old[0] - log_t_old)
         + inv(a1 + k);
   log_g_old[0] = log_t_new + log(fabs(term));
@@ -78,13 +79,13 @@ void calc_lambda(T_array& log_g_old, T_array_int& log_g_old_sign, T& log_t_new,
  * @tparam T2 scalar or `var_value`
  * @tparam T3 scalar or `var_value`
  * @tparam T_z scalar type
- * @param[out] g_a1 g_a1 reference to gradient of 2F1 w.r.t. a1, result
- * @param[out] g_a2 g_a2 reference to gradient of 2F1 w.r.t. a2, result
- * @param[out] g_b1 g_b1 reference to gradient of 2F1 w.r.t. b1, result
- * @param[in] a1 a1 see generalized hypergeometric function definition
- * @param[in] a2 a2 see generalized hypergeometric function definition
- * @param[in] b1 b1 see generalized hypergeometric function definition
- * @param[in] z z see generalized hypergeometric function definition
+ * @param[out] g_a1 reference to gradient of 2F1 w.r.t. a1, result
+ * @param[out] g_a2 reference to gradient of 2F1 w.r.t. a2, result
+ * @param[out] g_b1 reference to gradient of 2F1 w.r.t. b1, result
+ * @param[in] a1 see generalized hypergeometric function definition
+ * @param[in] a2 see generalized hypergeometric function definition
+ * @param[in] b1 see generalized hypergeometric function definition
+ * @param[in] z see generalized hypergeometric function definition
  * @param[in] precision magnitude of the increment of the infinite sum
  *   to truncate the sum
  * @param[in] max_steps number of steps to take
@@ -96,11 +97,11 @@ void grad_2F1(T1& g_a1, T2& g_a2, T3& g_b1, const T1& a1, const T2& a2,
   check_2F1_converges("grad_2F1", a1, a2, b1, z);
 
   using stan::math::value_of_rec;
-  using std::exp;
-  using std::fabs;
-  using std::log;
-  using std::max;
-  using TP = return_type_t<T1, T2, T3, T_z>;
+  // using std::exp;
+  // using std::fabs;
+  // using std::log;
+  // using std::max;
+  using ret_t = return_type_t<T1, T2, T3, T_z>;
 
   g_a1 = 0.0;
   g_a2 = 0.0;
@@ -110,21 +111,21 @@ void grad_2F1(T1& g_a1, T2& g_a2, T3& g_b1, const T1& a1, const T2& a2,
     return;
   }
 
-  TP log_g_old[3];
+  ret_t log_g_old[3];
   for (auto& i : log_g_old) {
     i = NEGATIVE_INFTY;
   }
 
-  TP log_t_old = 0.0;
-  TP log_t_new = 0.0;
+  ret_t log_t_old = 0.0;
+  ret_t log_t_new = 0.0;
   int sign_z = sign(z);
-  TP log_z = log(fabs(z));
+  ret_t log_z = log(fabs(z));
 
-  TP log_precision = log(precision);
-  TP log_t_new_sign = 1.0;
-  TP log_t_old_sign = 1.0;
-  TP log_g_old_sign[3];
-  for (TP& x : log_g_old_sign) {
+  ret_t log_precision = log(precision);
+  ret_t log_t_new_sign = 1.0;
+  ret_t log_t_old_sign = 1.0;
+  ret_t log_g_old_sign[3];
+  for (ret_t& x : log_g_old_sign) {
     x = 1.0;
   }
 
@@ -132,7 +133,7 @@ void grad_2F1(T1& g_a1, T2& g_a2, T3& g_b1, const T1& a1, const T2& a2,
 
   if (sign_z > 0) {
     for (int k = 0; k <= max_steps; ++k) {
-      TP p = ((a1 + k) * (a2 + k) / ((b1 + k) * (1 + k)));
+      ret_t p = ((a1 + k) * (a2 + k) / ((b1 + k) * (1 + k)));
       if (p == 0) {
         return;
       }
@@ -145,14 +146,14 @@ void grad_2F1(T1& g_a1, T2& g_a2, T3& g_b1, const T1& a1, const T2& a2,
       g_a2 += log_g_old_sign[1] > 0 ? exp(log_g_old[1]) : -exp(log_g_old[1]);
       g_b1 += log_g_old_sign[2] > 0 ? exp(log_g_old[2]) : -exp(log_g_old[2]);
 
-      if (log_g_old[0] <= std::max(
-              std::log(std::abs(value_of_rec(g_a1))) + log_precision,
+      if (log_g_old[0] <= fmax(
+              log(fabs(value_of_rec(g_a1))) + log_precision,
               log_precision)
-          && log_g_old[1] <= std::max(
-                 std::log(std::abs(value_of_rec(g_a2))) + log_precision,
+          && log_g_old[1] <= fmax(
+                 log(fabs(value_of_rec(g_a2))) + log_precision,
                  log_precision)
-          && log_g_old[2] <= std::max(
-                 std::log(std::abs(value_of_rec(g_b1))) + log_precision,
+          && log_g_old[2] <= fmax(
+                 log(fabs(value_of_rec(g_b1))) + log_precision,
                  log_precision)) {
         return;
       }
@@ -162,7 +163,7 @@ void grad_2F1(T1& g_a1, T2& g_a2, T3& g_b1, const T1& a1, const T2& a2,
     }
   } else {
     for (int k = 0; k <= max_steps; ++k) {
-      TP p = ((a1 + k) * (a2 + k) / ((b1 + k) * (1 + k)));
+      ret_t p = ((a1 + k) * (a2 + k) / ((b1 + k) * (1 + k)));
       if (p == 0) {
         return;
       }
@@ -179,14 +180,14 @@ void grad_2F1(T1& g_a1, T2& g_a2, T3& g_b1, const T1& a1, const T2& a2,
         g_a2 += log_g_old_sign[1] > 0 ? exp(log_g_old[1]) : -exp(log_g_old[1]);
         g_b1 += log_g_old_sign[2] > 0 ? exp(log_g_old[2]) : -exp(log_g_old[2]);
       }
-      if (log_g_old[0] <= std::max(
-              std::log(std::abs(value_of_rec(g_a1))) + log_precision,
+      if (log_g_old[0] <= fmax(
+              log(fabs(value_of_rec(g_a1))) + log_precision,
               log_precision)
-          && log_g_old[1] <= std::max(
-                 std::log(std::abs(value_of_rec(g_a2))) + log_precision,
+          && log_g_old[1] <= fmax(
+                 log(std::abs(value_of_rec(g_a2))) + log_precision,
                  log_precision)
-          && log_g_old[2] <= std::max(
-                 std::log(std::abs(value_of_rec(g_b1))) + log_precision,
+          && log_g_old[2] <= fmax(
+                 log(fabs(value_of_rec(g_b1))) + log_precision,
                  log_precision)) {
         return;
       }
