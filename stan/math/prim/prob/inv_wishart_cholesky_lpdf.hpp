4bd325925a0ec5136560190dc058820ecdc62a73
spinkney <sean.pinkney@gmail.com>
2022-05-06 09:43:07 -0400

    Update inv_wishart_cholesky_lpdf.hpp

--- stan/math/prim/prob/inv_wishart_cholesky_lpdf.hpp
+++ stan/math/prim/prob/inv_wishart_cholesky_lpdf.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/err.hpp>
 #include <stan/math/prim/fun/dot_self.hpp>
+#include <stan/math/prim/fun/dot_product.hpp>
 #include <stan/math/prim/fun/lmgamma.hpp>
 #include <stan/math/prim/fun/mdivide_left_tri.hpp>
 #include <stan/math/prim/fun/constants.hpp>
@@ -18,7 +19,7 @@ namespace math {
  lower-triangular
  * Cholesky factor of the scale matrix.
  *
- * The scale matrix, L_S, must be a lower Cholesky factor.
+ * The scale matrix, `L_S`, must be a lower Cholesky factor.
  * Dimension, k, is implicit.
  * nu must be greater than k-1
  *
@@ -42,13 +43,12 @@ template <bool propto, typename T_y, typename T_dof, typename T_scale,
           require_all_matrix_t<T_y, T_scale>* = nullptr>
 return_type_t<T_y, T_dof, T_scale> inv_wishart_cholesky_lpdf(
     const T_y& L_Y, const T_dof& nu, const T_scale& L_S) {
-  using Eigen::Dynamic;
   using Eigen::Lower;
-  using Eigen::Matrix;
   using T_L_Y_ref = ref_type_t<T_y>;
   using T_nu_ref = ref_type_t<T_dof>;
   using T_L_S_ref = ref_type_t<T_scale>;
-  static const char* function = "wishart_cholesky_lpdf";
+  using T_return = return_type_t<T_y, T_dof, T_scale>;
+  static const char* function = "inv_wishart_cholesky_lpdf";
   Eigen::Index k = L_Y.rows();
   check_size_match(function, "Rows of random variable", L_Y.rows(),
                    "columns of scale parameter", L_S.rows());
@@ -64,23 +64,27 @@ return_type_t<T_y, T_dof, T_scale> inv_wishart_cholesky_lpdf(
   check_positive(function, "Cholesky Scale matrix", L_S_ref.diagonal());
   check_positive(function, "columns of Cholesky Scale matrix", L_S_ref.cols());
 
-  return_type_t<T_y, T_dof, T_scale> lp(0.0);
+  T_return lp(0.0);
 
   if (include_summand<propto, T_dof>::value) {
     lp += k * LOG_TWO * (1 - 0.5 * nu_ref);
-  }
-
-  if (include_summand<propto, T_dof>::value) {
     lp -= lmgamma(k, 0.5 * nu_ref);
   }
 
   if (include_summand<propto, T_dof, T_scale, T_y>::value) {
     auto L_YinvL_S = mdivide_left_tri<Eigen::Lower>(L_Y_ref, L_S_ref);
+    T_return dot_LYinvLS(0.0);
+    Eigen::Matrix<T_return, 1, Eigen::Dynamic> linspaced_rv(k);
+    T_return nu_plus_1 = nu_ref + 1;
+
     for (int i = 0; i < k; i++) {
-      lp -= 0.5 * dot_self(L_YinvL_S.row(i).head(i + 1))
-            - nu_ref * log(L_S_ref.coeff(i, i))
-            + (nu_ref + i + 1) * log(L_Y_ref.coeff(i, i));
+      dot_LYinvLS += dot_self(L_YinvL_S.row(i).head(i + 1));
+      linspaced_rv(i) = nu_plus_1 + i;
     }
+
+    lp += -0.5 * dot_LYinvLS
+          - dot_product(linspaced_rv, log(L_Y_ref.diagonal()))
+          + nu_ref * sum(log(L_S_ref.diagonal()));
   }
 
   return lp;
