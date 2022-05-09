1f5cfc722e298b528ed9a29e81576490260acdc7
Stan BuildBot <mc.stanislaw@gmail.com>
2022-05-09 08:08:15 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1
    fix

--- stan/math/prim/fun/inv_Phi_log.hpp
+++ stan/math/prim/fun/inv_Phi_log.hpp
@@ -24,147 +24,108 @@ namespace math {
  * @return Real value of the inverse cdf for the standard normal distribution.
  */
 struct inv_Phi_log_fun {
-template <typename T>
-static inline T fun(const T& log_p) {
-  using ret_t = return_type_t<T>; 
-  check_less_or_equal("inv_Phi_log", "Probability variable", log_p, 0.);
+  template <typename T>
+  static inline T fun(const T& log_p) {
+    using ret_t = return_type_t<T>;
+    check_less_or_equal("inv_Phi_log", "Probability variable", log_p, 0.);
 
-  if (log_p == NEGATIVE_INFTY) {
-    return NEGATIVE_INFTY;
-  }
-  if (log_p == 0) {
-    return INFTY;
-  }
-
-  static const double log_a[8]
-      = {1.2199838032983212,
-         4.8914137334471356,
-         7.5865960847956080,
-         9.5274618535358388,
-         10.734698580862359,
-         11.116406781896242,
-         10.417226196842595,
-         7.8276718012189362};
-  static const double log_b[8]
-      = {0.,
-         3.7451021830139207,
-         6.5326064640478618,
-         8.5930788436817044,
-         9.9624069236663077,
-         10.579180688621286,
-         10.265665328832871,
-         8.5614962136628454};
-  static const double log_c[8]
-      = {0.3530744474482423,
-         1.5326298343683388,
-         1.7525849400614634,
-         1.2941374937060454,
-         0.2393776640901312,
-         -1.419724057885092,
-         -3.784340465764968,
-         -7.163234779359426};
-  static const double log_d[8]
-      = {0.,
-         0.7193954734947205,
-         0.5166395879845317,
-         -0.371400933927844,
-         -1.909840708457214,
-         -4.186547581055928,
-         -7.509976771225415,
-         -20.67376157385924};
-  static const double log_e[8]
-      = {1.8958048169567149,
-         1.6981417567726154,
-         0.5793212339927351,
-         -1.215503791936417,
-         -3.629396584023968,
-         -6.690500273261249,
-         -10.51540298415323,
-         -15.41979457491781};
-  static const double log_f[8]
-      = {0.,
-         -0.511105318617135,
-         -1.988286302259815,
-         -4.208049039384857,
-         -7.147448611626374,
-         -10.89973190740069,
-         -15.76637472711685,
-         -33.82373901099482};
-
-  ret_t val;
-  ret_t log_q = log_p <= LOG_HALF
-                          ? log_diff_exp(0, log_sum_exp(log_p, LOG_HALF))
-                          : log_diff_exp(log_p, LOG_HALF);
-  int log_q_sign = log_p <= LOG_HALF ? -1 : 1;
-
-  if (log_q <= -0.85566611005772) {
-    ret_t log_r = log_diff_exp(
-        -1.71133222011544,
-        2 * log_q);
-    ret_t log_agg_a = log_sum_exp(log_a[7] + log_r, log_a[6]);
-    ret_t log_agg_b = log_sum_exp(log_b[7] + log_r, log_b[6]);
-
-    for (int i = 0; i < 6; i++) {
-      log_agg_a = log_sum_exp(log_agg_a + log_r, log_a[5 - i]);
-      log_agg_b = log_sum_exp(log_agg_b + log_r, log_b[5 - i]);
+    if (log_p == NEGATIVE_INFTY) {
+      return NEGATIVE_INFTY;
+    }
+    if (log_p == 0) {
+      return INFTY;
     }
 
-    return log_q_sign * exp(log_q + log_agg_a - log_agg_b);
-  } else {
-    ret_t log_r = log_q_sign == -1 ? log_p : log_diff_exp(0., log_p);
+    static const double log_a[8]
+        = {1.2199838032983212, 4.8914137334471356, 7.5865960847956080,
+           9.5274618535358388, 10.734698580862359, 11.116406781896242,
+           10.417226196842595, 7.8276718012189362};
+    static const double log_b[8] = {0.,
+                                    3.7451021830139207,
+                                    6.5326064640478618,
+                                    8.5930788436817044,
+                                    9.9624069236663077,
+                                    10.579180688621286,
+                                    10.265665328832871,
+                                    8.5614962136628454};
+    static const double log_c[8]
+        = {0.3530744474482423, 1.5326298343683388, 1.7525849400614634,
+           1.2941374937060454, 0.2393776640901312, -1.419724057885092,
+           -3.784340465764968, -7.163234779359426};
+    static const double log_d[8] = {0.,
+                                    0.7193954734947205,
+                                    0.5166395879845317,
+                                    -0.371400933927844,
+                                    -1.909840708457214,
+                                    -4.186547581055928,
+                                    -7.509976771225415,
+                                    -20.67376157385924};
+    static const double log_e[8]
+        = {1.8958048169567149, 1.6981417567726154, 0.5793212339927351,
+           -1.215503791936417, -3.629396584023968, -6.690500273261249,
+           -10.51540298415323, -15.41979457491781};
+    static const double log_f[8] = {0.,
+                                    -0.511105318617135,
+                                    -1.988286302259815,
+                                    -4.208049039384857,
+                                    -7.147448611626374,
+                                    -10.89973190740069,
+                                    -15.76637472711685,
+                                    -33.82373901099482};
 
-    if (stan::math::is_inf(log_r)) {
-      return 0;
-    }
+    ret_t val;
+    ret_t log_q = log_p <= LOG_HALF
+                      ? log_diff_exp(0, log_sum_exp(log_p, LOG_HALF))
+                      : log_diff_exp(log_p, LOG_HALF);
+    int log_q_sign = log_p <= LOG_HALF ? -1 : 1;
 
-    log_r = log(sqrt(-log_r));
-
-    if (log_r
-        <= 1.60943791243410) {
-      log_r = log_diff_exp(
-          log_r, 0.47000362924573);
-      ret_t log_agg_c = log_sum_exp(log_c[7] + log_r, log_c[6]);
-      ret_t log_agg_d = log_sum_exp(log_d[7] + log_r, log_d[6]);
+    if (log_q <= -0.85566611005772) {
+      ret_t log_r = log_diff_exp(-1.71133222011544, 2 * log_q);
+      ret_t log_agg_a = log_sum_exp(log_a[7] + log_r, log_a[6]);
+      ret_t log_agg_b = log_sum_exp(log_b[7] + log_r, log_b[6]);
 
       for (int i = 0; i < 6; i++) {
-        log_agg_c = log_sum_exp(log_agg_c + log_r, log_c[5 - i]);
-        log_agg_d = log_sum_exp(log_agg_d + log_r, log_d[5 - i]);
+        log_agg_a = log_sum_exp(log_agg_a + log_r, log_a[5 - i]);
+        log_agg_b = log_sum_exp(log_agg_b + log_r, log_b[5 - i]);
       }
-      val = exp(log_agg_c - log_agg_d);
+
+      return log_q_sign * exp(log_q + log_agg_a - log_agg_b);
     } else {
-      log_r = log_diff_exp(
-          log_r,
-          1.60943791243410);
-      ret_t log_agg_e = log_sum_exp(log_e[7] + log_r, log_e[6]);
-      ret_t log_agg_f = log_sum_exp(log_f[7] + log_r, log_f[6]);
+      ret_t log_r = log_q_sign == -1 ? log_p : log_diff_exp(0., log_p);
 
-      for (int i = 0; i < 6; i++) {
-        log_agg_e = log_sum_exp(log_agg_e + log_r, log_e[5 - i]);
-        log_agg_f = log_sum_exp(log_agg_f + log_r, log_f[5 - i]);
+      if (stan::math::is_inf(log_r)) {
+        return 0;
       }
-      val = exp(log_agg_e - log_agg_f);
-    }
-    if (log_q_sign == -1)
-      return -val;
-  }
-  return val;
-}
-};
 
-/**
- * Structure to wrap inv_Phi_log() so it can be vectorized.
- *
- * @tparam T type of variable
- * @param x variable in range [0, 1]
- * @return Inverse unit normal CDF of x.
- * @throw std::domain_error if x is not between 0 and 1.
- */
-// struct inv_Phi_log_fun {
-//   template <typename T>
-//   static inline T fun(const T& x) {
-//     return inv_Phi_log(x);
-//   }
-// };
+      log_r = log(sqrt(-log_r));
+
+      if (log_r <= 1.60943791243410) {
+        log_r = log_diff_exp(log_r, 0.47000362924573);
+        ret_t log_agg_c = log_sum_exp(log_c[7] + log_r, log_c[6]);
+        ret_t log_agg_d = log_sum_exp(log_d[7] + log_r, log_d[6]);
+
+        for (int i = 0; i < 6; i++) {
+          log_agg_c = log_sum_exp(log_agg_c + log_r, log_c[5 - i]);
+          log_agg_d = log_sum_exp(log_agg_d + log_r, log_d[5 - i]);
+        }
+        val = exp(log_agg_c - log_agg_d);
+      } else {
+        log_r = log_diff_exp(log_r, 1.60943791243410);
+        ret_t log_agg_e = log_sum_exp(log_e[7] + log_r, log_e[6]);
+        ret_t log_agg_f = log_sum_exp(log_f[7] + log_r, log_f[6]);
+
+        for (int i = 0; i < 6; i++) {
+          log_agg_e = log_sum_exp(log_agg_e + log_r, log_e[5 - i]);
+          log_agg_f = log_sum_exp(log_agg_f + log_r, log_f[5 - i]);
+        }
+        val = exp(log_agg_e - log_agg_f);
+      }
+      if (log_q_sign == -1)
+        return -val;
+    }
+    return val;
+  }
+};
 
 /**
  * Vectorized version of inv_Phi_log().
@@ -181,14 +142,6 @@ template <
 inline auto inv_Phi_log(const T& x) {
   return apply_scalar_unary<inv_Phi_log_fun, T>::apply(x);
 }
-// template <
-//     typename Container,
-//     require_not_container_st<std::is_arithmetic, Container>* = nullptr,
-//     require_not_var_matrix_t<Container>* = nullptr,
-//     require_not_nonscalar_prim_or_rev_kernel_expression_t<Container>* = nullptr>
-// inline auto inv_Phi_log(const Container& x) {
-//   return apply_scalar_unary<inv_Phi_log_fun, Container>::apply(x);
-// }
 
 }  // namespace math
 }  // namespace stan
--- test/unit/math/mix/fun/inv_Phi_log_test.cpp
+++ test/unit/math/mix/fun/inv_Phi_log_test.cpp
@@ -2,41 +2,39 @@
 
 TEST(mathMixMatFun, invPhiLog) {
   auto f = [](const auto& x1) { return stan::math::inv_Phi_log(x1); };
-  stan::test::expect_common_unary_vectorized(f);
-//   stan::test::expect_unary_vectorized(f, log(0.02425), log(0.97575));  // breakpoints
-//   stan::test::expect_unary_vectorized(f, -100.25, -2, 0.01, 0.1, 0.98, 0.5,
-//                                       2.0);
+  //  stan::test::expect_common_unary_vectorized(f);
+  stan::test::expect_unary_vectorized(f, log(0.02425),
+                                      log(0.97575));  // breakpoints
+  stan::test::expect_unary_vectorized(f, -100.25, -2, 0.01, 0.1, 0.98, 0.5,
+                                      2.0);
 }
 
-// TEST(mathMixScalLogFun, invPhiLog) {
-//   auto f = [](const auto& x1) {
-//     return stan::math::inv_Phi_log(x1);
-//   };
-//   // stan::test::expect_common_nonzero_binary(f);
-//   stan::test::expect_ad(f, -1.1);
-// }
+TEST(mathMixScalLogFun, invPhiLog) {
+  auto f = [](const auto& x1) { return stan::math::inv_Phi_log(x1); };
+  // stan::test::expect_common_nonzero_binary(f);
+  stan::test::expect_ad(f, -1.1);
+}
 
-// TEST(mathMixMatFunLog, invPhiLog) {
-//   auto f = [](const auto& x1) { return stan::math::inv_Phi_log(x1); };
-//   stan::test::expect_common_unary_vectorized(f);
-//   stan::test::expect_unary_vectorized(f, 0.02425, 0.97575);  // breakpoints
-//   stan::test::expect_unary_vectorized(f, -100.25, -2, 0.01, 0.1, 0.98, 0.5,
-//                                       2.0);
-// }
+TEST(mathMixMatFunLog, invPhiLog) {
+  auto f = [](const auto& x1) { return stan::math::inv_Phi_log(x1); };
+  // stan::test::expect_common_unary_vectorized(f);
+  stan::test::expect_unary_vectorized(f, log(0.02425),
+                                      log(0.97575));  // breakpoints
+  stan::test::expect_unary_vectorized(f, -100.25, -2, 0.01, 0.1, 0.98, 0.5,
+                                      2.0);
+}
 
 // TEST(mathMixMatFunLog, invPhiLog_varmat) {
 //   using stan::math::vec_concat;
 //   using stan::test::expect_ad_vector_matvar;
 //   using stan::test::internal::common_args;
 //   auto f = [](const auto& x1) {
-//     using stan::math::inv_Phi;
-//     return inv_Phi(x1);
+//     return stan::math::inv_Phi_log(x1);
 //   };
 //   std::vector<double> com_args = common_args();
-//   std::vector<double> args{0.02425, 0.97575, -2, 0.1, 0.5, 2.0};
-//   auto all_args = vec_concat(com_args, args);
-//   Eigen::VectorXd A(all_args.size());
-//   for (int i = 0; i < all_args.size(); ++i) {
+//   std::vector<double> args{log(0.02425), log(0.97575), 2, log(0.1), 0.5,
+//   log(2.0)}; auto all_args = vec_concat(com_args, args); Eigen::VectorXd
+//   A(all_args.size()); for (int i = 0; i < all_args.size(); ++i) {
 //     A(i) = all_args[i];
 //   }
 //   expect_ad_vector_matvar(f, A);
