19588e44c5a0004be225d8f5785f05f3489746f0
Steve Bronder <stevo15025@gmail.com>
2022-05-25 16:22:10 -0400

    turn on fifth root mix check

--- test/unit/math/mix/functor/root_finder_test.cpp
+++ test/unit/math/mix/functor/root_finder_test.cpp
@@ -1,5 +1,4 @@
 #include <test/unit/math/test_ad.hpp>
-
 /*
 TEST(MixFun, root_finder_cubed) {
   using stan::math::root_finder;
@@ -27,6 +26,27 @@ TEST(MixFun, root_finder_cubed) {
   };
   stan::test::expect_ad(full_f, x);
 }
+*/
+
+
+struct FifthRootFinder {
+  template <bool ReturnDeriv, typename T1, typename T2,
+   std::enable_if_t<ReturnDeriv>* = nullptr>
+  static auto run(T1&& g, T2&& x) {
+    auto g_pow = g * g * g;
+    auto second_deriv = 20 * g_pow;
+    g_pow *= g;
+    auto first_deriv = 5 * g_pow;
+    g_pow *= g;
+    auto func_val = g_pow - x;
+    return std::make_tuple(func_val, first_deriv, second_deriv);
+  }
+  template <bool ReturnDeriv, typename T1, typename T2,
+   std::enable_if_t<!ReturnDeriv>* = nullptr>
+  static auto run(T1&& g, T2&& x) {
+    return g * g * g * g * g - x;
+  }
+};
 
 TEST(MixFun, root_finder_fifth) {
   using stan::math::root_finder;
@@ -46,85 +66,57 @@ TEST(MixFun, root_finder_fifth) {
   const int digits = std::numeric_limits<double>::digits;
   int get_digits = static_cast<int>(digits * 0.4);
   std::uintmax_t maxit = 20;
-  auto f = [](const auto& g, const auto& x) { return g * g * g * g * g - x; };
-  auto f_g = [](const auto& g, const auto& x) { return 5 * g * g * g * g; };
-  auto f_gg = [](const auto& g, const auto& x) { return 20 * g * g * g; };
-  auto full_f = [&f, &f_g, &f_gg, guess, min, max](auto&& xx) {
-    return root_finder(std::make_tuple(f, f_g, f_gg), guess, min, max, xx);
+  auto f_hailey = [guess, min, max](auto&& x) {
+    return stan::math::root_finder_hailey<FifthRootFinder>(guess, min, max, x);
   };
-  stan::test::expect_ad(full_f, x);
+  stan::test::expect_ad(f_hailey, x);
 }
-*/
-TEST(MixFun, root_finder_beta_log) {
-  auto beta_lpdf_deriv = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-    using stan::math::log;
-    using stan::math::lgamma;
-    using stan::math::log1m;
-    auto val = -((beta * log1m(x) + (-2 + alpha) * log(x)
-                  + log(1 - 2 * x - alpha + x * alpha + x * beta)
-                  + lgamma(alpha + beta))
-                 - (2 * log1m(x) + lgamma(alpha) + lgamma(beta)));
-    return val;
-  };
-  auto f = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-    return stan::math::beta_lcdf(x, alpha, beta) - p;
-  };
-  auto f_g = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-    return stan::math::beta_lpdf(x, alpha, beta);
-  };
-  auto f_gg
-      = [&beta_lpdf_deriv](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-          return beta_lpdf_deriv(x, alpha, beta, p);
-        };
-  double guess = .3;
-  double min = 0.0000000001;
-  double max = 1;
-  auto full_f = [&f, &f_g, &f_gg, guess, min, max](auto&& alpha, auto&& beta,
-                                                   auto&& p) {
-    std::uintmax_t max_its = 1000;
-    return stan::math::root_finder_hailey(std::make_tuple(f, f_g, f_gg), guess,
-                                          min, max, alpha, beta, p);
-  };
-  double p = 0.45;
-  double alpha = .5;
-  double beta = .4;
-  stan::test::expect_ad(full_f, alpha, beta, p);
-}
+
+/*
+struct BetaCdfRoot {
+  template <bool ReturnDeriv, typename T1, typename T2, typename T3,
+            typename T4, std::enable_if_t<ReturnDeriv>* = nullptr>
+  static auto run(T1&& x, T2&& alpha, T3&& beta, T4&& p) {
+    auto f = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
+      return stan::math::beta_cdf(x, alpha, beta) - p;
+    }(x, alpha, beta, p);
+    auto beta_pdf_deriv = [](auto&& x, auto&& alpha, auto&& beta) {
+      using stan::math::lgamma;
+      using stan::math::pow;
+      auto val = -(pow((1 - x), beta) * pow(x, (-2 + alpha))
+                   * (1. - 2. * x - alpha + x * alpha + x * beta)
+                   * lgamma(alpha + beta))
+                 / (pow((-1 + x), 2) * lgamma(alpha) * lgamma(beta));
+      return val;
+    }(x, alpha, beta);
+    auto beta_pdf = [](auto&& x, auto&& alpha, auto&& beta) {
+      using stan::math::lgamma;
+      using stan::math::pow;
+      auto val
+          = (pow(x, alpha - 1) * pow((1 - x), beta - 1) * lgamma(alpha + beta))
+            / ((lgamma(alpha) * lgamma(beta)));
+      return val;
+    }(x, alpha, beta);
+    return std::make_tuple(f, beta_pdf, beta_pdf_deriv);
+  }
+  template <bool ReturnDeriv, typename T1, typename T2, typename T3,
+            typename T4, std::enable_if_t<!ReturnDeriv>* = nullptr>
+  static auto run(T1&& x, T2&& alpha, T3&& beta, T4&& p) {
+    return stan::math::beta_cdf(x, alpha, beta) - p;
+  }
+};
 
 TEST(MixFun, root_finder_beta) {
-  auto beta_pdf_deriv = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-    using stan::math::lgamma;
-    using stan::math::pow;
-    auto val = -(pow((1 - x), beta) * pow(x, (-2 + alpha))
-                 * (1. - 2. * x - alpha + x * alpha + x * beta)
-                 * lgamma(alpha + beta))
-               / (pow((-1 + x), 2) * lgamma(alpha) * lgamma(beta));
-    return val;
+  constexpr double guess = .5;
+  constexpr double min = 0;
+  constexpr double max = 1;
+  auto f_hailey = [](auto&& alpha, auto&& beta, auto&& p) {
+    return stan::math::root_finder_hailey<BetaCdfRoot>(p, min, max, alpha, beta,
+                                                       p);
   };
-  auto beta_pdf = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-    using stan::math::lgamma;
-    using stan::math::pow;
-    auto val
-        = (pow(x, alpha - 1) * pow((1 - x), beta - 1) * lgamma(alpha + beta))
-          / ((lgamma(alpha) * lgamma(beta)));
-    return val;
-  };
-
-  auto f = [](auto&& x, auto&& alpha, auto&& beta, auto&& p) {
-    return stan::math::beta_cdf(x, alpha, beta) - p;
-  };
-  double guess = .7;
-  double min = 0.0000000001;
-  double max = 1;
-  auto full_f = [&f, &beta_pdf, &beta_pdf_deriv, guess, min, max](
-                    auto&& alpha, auto&& beta, auto&& p) {
-    std::uintmax_t max_its = 1000;
-    return stan::math::root_finder_hailey(
-        std::make_tuple(f, beta_pdf, beta_pdf_deriv), guess, min, max, alpha,
-        beta, p);
-  };
-  double p = 0.8;
-  double alpha = .4;
+  double alpha = .5;
   double beta = .5;
-  stan::test::expect_ad(full_f, alpha, beta, p);
+  double p = .3;
+  stan::test::expect_ad(f_hailey, alpha, beta, p);
 }
+*/
