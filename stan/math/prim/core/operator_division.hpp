dd724d600a8599741d69b238b319f89207bafc27
Brian Ward <bward@flatironinstitute.org>
2022-06-13 11:33:38 -0400

    Add is_stan_scalar.hpp

--- stan/math/prim/core/operator_division.hpp
+++ stan/math/prim/core/operator_division.hpp
@@ -2,6 +2,7 @@
 #define STAN_MATH_PRIM_CORE_OPERATOR_DIVISION_HPP
 
 #include <stan/math/prim/meta/return_type.hpp>
+#include <stan/math/prim/meta/is_stan_scalar.hpp>
 #include <complex>
 
 namespace stan {
