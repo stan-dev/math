ff2af39fcdc04fc4755e6b4f9c57c8d84c592c78
Nicusor Serban <serban.nicusor@toptal.com>
2022-06-21 15:56:58 +0200

    pre-release/v4.4.0: updating version numbers

--- stan/math/version.hpp
+++ stan/math/version.hpp
@@ -12,8 +12,8 @@
 #endif
 
 #define STAN_MATH_MAJOR 4
-#define STAN_MATH_MINOR 3
-#define STAN_MATH_PATCH 2
+#define STAN_MATH_MINOR 4
+#define STAN_MATH_PATCH 0
 
 namespace stan {
 namespace math {
