3aff49820a17775538dafc34824f73f3b19be665
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-06-27 09:12:15 +0300

    Reduce changes

--- stan/math/fwd/fun/exp.hpp
+++ stan/math/fwd/fun/exp.hpp
@@ -3,13 +3,17 @@
 
 #include <stan/math/fwd/meta.hpp>
 #include <stan/math/fwd/core.hpp>
-#include <stan/math/fwd/functor/function_gradients.hpp>
 #include <stan/math/prim/fun/exp.hpp>
 #include <cmath>
 #include <complex>
 
 namespace stan {
 namespace math {
+template <typename T>
+inline fvar<T> exp(const fvar<T>& x) {
+  using std::exp;
+  return fvar<T>(exp(x.val_), x.d_ * exp(x.val_));
+}
 
 /**
  * Return the natural exponentiation (base e) of the specified complex number.
--- stan/math/rev/fun/exp.hpp
+++ stan/math/rev/fun/exp.hpp
@@ -11,12 +11,39 @@
 #include <stan/math/rev/fun/is_nan.hpp>
 #include <stan/math/rev/fun/cos.hpp>
 #include <stan/math/rev/fun/sin.hpp>
-#include <stan/math/rev/functor/function_gradients.hpp>
 #include <cmath>
 #include <complex>
 
 namespace stan {
 namespace math {
+
+/**
+ * Return the exponentiation of the specified variable (cmath).
+ *
+   \f[
+   \mbox{exp}(x) =
+   \begin{cases}
+     e^x & \mbox{if } -\infty\leq x \leq \infty \\[6pt]
+     \textrm{NaN} & \mbox{if } x = \textrm{NaN}
+   \end{cases}
+   \f]
+
+   \f[
+   \frac{\partial\, \mbox{exp}(x)}{\partial x} =
+   \begin{cases}
+     e^x & \mbox{if } -\infty\leq x\leq \infty \\[6pt]
+     \textrm{NaN} & \mbox{if } x = \textrm{NaN}
+   \end{cases}
+   \f]
+ *
+ * @param a Variable to exponentiate.
+ * @return Exponentiated variable.
+ */
+inline var exp(const var& a) {
+  return make_callback_var(std::exp(a.val()), [a](auto& vi) mutable {
+    a.adj() += vi.adj() * vi.val();
+  });
+}
 
 /**
  * Return the exponentiation (base e) of the specified complex number.
@@ -25,6 +52,21 @@ namespace math {
  */
 inline std::complex<var> exp(const std::complex<var>& z) {
   return internal::complex_exp(z);
+}
+
+/**
+ * Return the exponentiation of the elements of x
+ *
+ * @tparam T type of x
+ * @param x argument
+ * @return elementwise exponentiation of x
+ */
+template <typename T, require_var_matrix_t<T>* = nullptr>
+inline auto exp(const T& x) {
+  return make_callback_var(
+      x.val().array().exp().matrix(), [x](const auto& vi) mutable {
+        x.adj() += (vi.val().array() * vi.adj().array()).matrix();
+      });
 }
 
 }  // namespace math
