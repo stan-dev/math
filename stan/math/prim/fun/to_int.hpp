e52e1850a6690146e41af3daf626c5d5ca375c4d
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-06-29 10:49:35 +0300

    Address review comments

--- stan/math/prim/fun/to_int.hpp
+++ stan/math/prim/fun/to_int.hpp
@@ -1,25 +1,52 @@
 #ifndef STAN_MATH_PRIM_FUN_TO_INT_HPP
 #define STAN_MATH_PRIM_FUN_TO_INT_HPP
 
+#include <stan/math/prim/err/check_not_nan.hpp>
+#include <stan/math/prim/err/check_finite.hpp>
 #include <stan/math/prim/functor/apply_scalar_unary.hpp>
 
 namespace stan {
 namespace math {
 
 /**
- * Returns the input scalar as an integer type
+ * Returns the input scalar as an integer type. Specialisation for integral
+ * types which do not need conversion, reduces to a no-op.
+ *
+ * @tparam T type of integral argument
+ * @param x argument
+ * @return Input argument unchanged
+ */
+template <typename T, require_integral_t<T>* = nullptr>
+inline T to_int(T x) {
+  return std::forward<T>(x);
+}
+
+/**
+ * Returns the input scalar as an integer type. This function performs no
+ * rounding and simply truncates the decimal to return only the signficand as an
+ * integer.
+ *
+ * Casting NaN and Inf values to integers is considered undefined behavior as
+ * NaN and Inf cannot be represented as an integer and most implementations
+ * simply overflow, as such this function throws for these inputs.
+ *
+ * The function also throws for floating-point values that are too large to be
+ * represented as an integer.
  *
  * @tparam T type of argument (must be arithmetic)
  * @param x argument
  * @return iIteger value of argument
  */
-template <typename T, require_arithmetic_t<T>* = nullptr>
+template <typename T, require_floating_point_t<T>* = nullptr>
 inline int to_int(T x) {
+  static const char* function = "to_int";
+  check_not_nan(function, "x", x);
+  check_finite(function, "x", x);
   if (x < std::numeric_limits<int>::min()
       || x > std::numeric_limits<int>::max()) {
     std::ostringstream msg;
     msg << "Value " << x << " is too large to be represented as an integer";
-    throw std::invalid_argument(msg.str());
+    throw std::domain_error(msg.str());
   }
   return static_cast<int>(x);
 }
--- test/unit/math/prim/fun/to_int_test.cpp
+++ test/unit/math/prim/fun/to_int_test.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <limits>
 
-TEST(MathFunctions, to_int) {
+TEST(MathFunctions, to_int_values) {
   using stan::math::to_int;
 
   EXPECT_EQ(2, to_int(2.0));
@@ -16,11 +16,25 @@ TEST(MathFunctions, to_int) {
   EXPECT_EQ(-36574, to_int(-36574.1));
   EXPECT_EQ(-36574, to_int(-36574.9));
   EXPECT_EQ(-36574, to_int(-36574.999999999));
+}
+
+TEST(MathFunctions, to_int_errors) {
+  using stan::math::to_int;
+  using stan::math::NOT_A_NUMBER;
+  using stan::math::INFTY;
+  using stan::math::NEGATIVE_INFTY;
 
   EXPECT_THROW(to_int(std::numeric_limits<int>::max() + 1.0),
-               std::invalid_argument);
+               std::domain_error);
   EXPECT_THROW(to_int(std::numeric_limits<int>::min() - 1.0),
-               std::invalid_argument);
+               std::domain_error);
+
+  EXPECT_THROW(to_int(NOT_A_NUMBER),
+               std::domain_error);
+  EXPECT_THROW(to_int(INFTY),
+               std::domain_error);
+  EXPECT_THROW(to_int(NEGATIVE_INFTY),
+               std::domain_error);
 }
 
 TEST(MathFunctions, to_int_vec) {
@@ -37,5 +51,11 @@ TEST(MathFunctions, to_int_vec) {
   EXPECT_STD_VECTOR_EQ(to_int(inputs), target_result);
 
   inputs[0][2] = std::numeric_limits<int>::min() - 1.0;
-  EXPECT_THROW(to_int(inputs), std::invalid_argument);
+  EXPECT_THROW(to_int(inputs), std::domain_error);
+
+  std::vector<double> inputs_empty;
+  std::vector<double> inputs_size_one{1.5};
+
+  EXPECT_NO_THROW(to_int(inputs_empty));
+  EXPECT_STD_VECTOR_EQ(to_int(inputs_size_one), std::vector<int>{1});
 }
