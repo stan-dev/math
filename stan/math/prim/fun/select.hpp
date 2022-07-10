0edeb5fe326eb5253143cf1193f58f1c156139bc
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-07-10 20:51:18 +0300

    Update doc

--- stan/math/prim/fun/select.hpp
+++ stan/math/prim/fun/select.hpp
@@ -8,13 +8,9 @@ namespace math {
 
 /**
  * Return the second argument if the first argument is true
- * and otherwise return the second argument.
+ * and otherwise return the third argument.
  *
- * <p>This is just a convenience method to provide a function
- * with the same behavior as the built-in ternary operator.
- * In general, this function behaves as if defined by
- *
- * <p><code>select(c, y1, y0) = c ? y1 : y0</code>.
+ * <code>select(c, y1, y0) = c ? y1 : y0</code>.
  *
  * @tparam T_true type of the true argument
  * @tparam T_false type of the false argument
@@ -28,17 +24,37 @@ inline auto select(const bool c, const T_true y_true, const T_false y_false) {
   return c ? y_true : y_false;
 }
 
+/**
+ * Return the second argument if the first argument is true
+ * and otherwise return the third argument. Overload for use with two Eigen
+ * objects.
+ *
+ * @tparam T_true type of the true argument
+ * @tparam T_false type of the false argument
+ * @param c Boolean condition value.
+ * @param y_true Value to return if condition is true.
+ * @param y_false Value to return if condition is false.
+ */
 template <typename T_true, typename T_false,
           require_all_eigen_t<T_true, T_false>* = nullptr>
 inline auto select(const bool c, const T_true y_true, const T_false y_false) {
-  using return_scalar_t = return_type_t<T_true, T_false>;
-  if (c) {
-    return y_true.template cast<return_scalar_t>().eval();
-  }
-
-  return y_false.template cast<return_scalar_t>().eval();
+  return y_true.binaryExpr(y_false, [&](auto&& x, auto&& y) {
+    return c ? x : y;
+  }).eval();
 }
 
+/**
+ * Return the second Eigen argument if the first argument is true
+ * and otherwise return the second Eigen argument. Overload for use with one
+ * scalar and one Eigen object. If chosen, the scalar is returned as an Eigen
+ * object of the same size and type as the provided argument.
+ *
+ * @tparam T_true type of the true argument
+ * @tparam T_false type of the false argument
+ * @param c Boolean condition value.
+ * @param y_true Value to return if condition is true.
+ * @param y_false Value to return if condition is false.
+ */
 template <typename T_true, typename T_false,
           require_eigen_t<T_true>* = nullptr,
           require_stan_scalar_t<T_false>* = nullptr>
@@ -51,18 +67,43 @@ inline plain_type_t<T_true> select(const bool c, const T_true& y_true,
   return y_true.unaryExpr([&](auto&& y){ return y_false; });
 }
 
-
+/**
+ * Return the second Eigen argument if the first argument is true
+ * and otherwise return the second Eigen argument. Overload for use with one
+ * scalar and one Eigen object. If chosen, the scalar is returned as an Eigen
+ * object of the same size and type as the provided argument.
+ *
+ * @tparam T_true type of the true argument
+ * @tparam T_false type of the false argument
+ * @param c Boolean condition value.
+ * @param y_true Value to return if condition is true.
+ * @param y_false Value to return if condition is false.
+ */
 template <typename T_true, typename T_false,
           require_stan_scalar_t<T_true>* = nullptr,
           require_eigen_t<T_false>* = nullptr>
-inline plain_type_t<T_false> select(const bool c, const T_true y_true, const T_false y_false) {
+inline plain_type_t<T_false> select(const bool c, const T_true y_true,
+                                    const T_false y_false) {
   if (c) {
-  return y_false.unaryExpr([&](auto&& y){ return y_true; });
+    return y_false.unaryExpr([&](auto&& y){ return y_true; });
   }
 
   return y_false;
 }
 
+/**
+ * Return the second argument if the first argument is true
+ * and otherwise return the third argument. Overload for use with an Eigen
+ * object of booleans, and two scalars. The chosen scalar is returned as an
+ * Eigen object of the same dimension as the input Eigen argument
+ *
+ * @tparam T_bool type of Eigen boolean object
+ * @tparam T_true type of the true argument
+ * @tparam T_false type of the false argument
+ * @param c Eigen object of boolean condition values.
+ * @param y_true Value to return if condition is true.
+ * @param y_false Value to return if condition is false.
+ */
 template <typename T_bool, typename T_true, typename T_false,
           require_eigen_array_t<T_bool>* = nullptr,
           require_all_stan_scalar_t<T_true, T_false>* = nullptr>
@@ -73,6 +114,18 @@ inline auto select(const T_bool c, const T_true y_true,
   }).eval();
 }
 
+/**
+ * Return the second argument if the first argument is true
+ * and otherwise return the third argument. Overload for use with an Eigen
+ * object of booleans, and at least one Eigen object as input.
+ *
+ * @tparam T_bool type of Eigen boolean object
+ * @tparam T_true type of the true argument
+ * @tparam T_false type of the false argument
+ * @param c Eigen object of boolean condition values.
+ * @param y_true Value to return if condition is true.
+ * @param y_false Value to return if condition is false.
+ */
 template <typename T_bool, typename T_true, typename T_false,
           require_eigen_array_t<T_bool>* = nullptr,
           require_any_eigen_array_t<T_true, T_false>* = nullptr>
