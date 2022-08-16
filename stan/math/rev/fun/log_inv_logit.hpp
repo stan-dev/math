9d916e4bd8d98175417444d53a8551b8402c5c51
Steve Bronder <stevo15025@gmail.com>
2022-08-16 16:22:58 -0400

    adds varmat for log_inv_log

--- stan/math/rev/fun/log_inv_logit.hpp
+++ stan/math/rev/fun/log_inv_logit.hpp
@@ -16,11 +16,13 @@ namespace math {
  * @param u argument
  * @return log inverse logit of the argument
  */
-inline var log_inv_logit(const var& u) {
+template <typename T>
+inline auto log_inv_logit(const var_value<T>& u) {
   return make_callback_var(log_inv_logit(u.val()), [u](auto& vi) mutable {
-    u.adj() += vi.adj() * inv_logit(-u.val());
+    as_array_or_scalar(u.adj()) += as_array_or_scalar(vi.adj()) * as_array_or_scalar(inv_logit(-u.val()));
   });
 }
+
 
 }  // namespace math
 }  // namespace stan
