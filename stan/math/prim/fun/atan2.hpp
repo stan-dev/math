a4292c8e55f5f94775619370823993e7f62dbf00
Sean Pinkney <sean.pinkney@gmail.com>
2022-09-09 05:11:25 -0400

    vectorize atan2

--- stan/math/prim/fun/atan2.hpp
+++ stan/math/prim/fun/atan2.hpp
@@ -23,6 +23,23 @@ double atan2(T1 y, T2 x) {
   return std::atan2(y, x);
 }
 
+/**
+ * Enables the vectorised application of the atan2 function, when
+ * the first and/or second arguments are containers.
+ *
+ * @tparam T1 type of first input
+ * @tparam T2 type of second input
+ * @param a First input
+ * @param b Second input
+ * @return Returns the atan2 function applied to the two inputs.
+ */
+template <typename T1, typename T2, require_any_container_t<T1, T2>* = nullptr,
+          require_all_not_var_matrix_t<T1, T2>* = nullptr>
+inline auto atan2(const T1& a, const T2& b) {
+  return apply_scalar_binary(
+      a, b, [](const auto& c, const auto& d) { return atan2(c, d); });
+}
+
 }  // namespace math
 }  // namespace stan
 
