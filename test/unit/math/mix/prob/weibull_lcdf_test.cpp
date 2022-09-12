680671bad7f8540cf5e6b403913fac7f8cc28c1e
Sean Pinkney <sean.pinkney@gmail.com>
2022-09-12 07:18:56 -0400

    add tests

---
+++ test/unit/math/mix/prob/weibull_lcdf_test.cpp
@@ -0,0 +1,16 @@
+#include <stan/math/mix.hpp>
+#include <test/unit/math/test_ad.hpp>
+
+TEST(mathMixScalFun, weibull_lcdf) {
+  auto f = [](const double alpha, const double sigma) {
+    return [=](const auto& y) { return stan::math::weibull_lcdf(y, alpha, sigma); };
+  };
+
+  stan::test::expect_ad(f(0.1, 1.0), 0.1);
+  stan::test::expect_ad(f(1.0, 1.0), 0.001);
+  stan::test::expect_ad(f(0.1, 1.0), 1.25);
+  stan::test::expect_ad(f(1.0, 1.0), 0.15);
+  stan::test::expect_ad(f(0.5, 0.5), 0.25);
+  stan::test::expect_ad(f(1.0, 0.5), 2.0);
+  stan::test::expect_ad(f(2.5, 3.0), 1.5);
+}
