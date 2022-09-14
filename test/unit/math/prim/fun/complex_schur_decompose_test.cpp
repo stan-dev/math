c4e280b906f099b054a2acf41d91e3cd31ee2f1d
Brian Ward <bward@flatironinstitute.org>
2022-09-14 09:53:58 -0400

    Add EXPECT_MATRIX_COMPLEX_NEAR

--- test/unit/math/prim/fun/complex_schur_decompose_test.cpp
+++ test/unit/math/prim/fun/complex_schur_decompose_test.cpp
@@ -2,18 +2,6 @@
 #include <test/unit/util.hpp>
 #include <gtest/gtest.h>
 
-template <typename T, typename U>
-void expect_complex_mat_eq(const T& x, const U& y, double tol = 1e-8) {
-  EXPECT_EQ(x.rows(), y.rows());
-  EXPECT_EQ(x.cols(), y.cols());
-  for (int j = 0; j < x.cols(); ++j) {
-    for (int i = 0; i < x.rows(); ++i) {
-      EXPECT_NEAR(real(x(i, j)), real(y(i, j)), tol);
-      EXPECT_NEAR(imag(x(i, j)), imag(y(i, j)), tol);
-    }
-  }
-}
-
 TEST(primFun, complex_schur_decompose) {
   using c_t = std::complex<double>;
   using stan::math::complex_schur_decompose_t;
@@ -26,7 +14,7 @@ TEST(primFun, complex_schur_decompose) {
   auto A_t = complex_schur_decompose_t(A);
   auto A_u = complex_schur_decompose_u(A);
   auto A_recovered = A_u * A_t * A_u.adjoint();
-  expect_complex_mat_eq(stan::math::to_complex(A, 0), A_recovered);
+  EXPECT_MATRIX_NEAR(A, stan::math::get_real(A_recovered), 1e-8);
 
   Eigen::MatrixXcd B(3, 3);
   B << 0, 2, 2, 0, c_t(0, 1), 2, 1, 0, 1;
@@ -35,6 +23,5 @@ TEST(primFun, complex_schur_decompose) {
   auto B_u = complex_schur_decompose_u(B);
 
   auto B_recovered = B_u * B_t * B_u.adjoint();
-
-  expect_complex_mat_eq(B, B_recovered);
+  EXPECT_MATRIX_COMPLEX_NEAR(B, B_recovered, 1e-8);
 }
