17a050ccd6c96bd3c5068040290fbacbb0ce58f3
Brian Ward <bward@flatironinstitute.org>
2022-09-14 10:01:19 -0400

    Update mix tests

--- test/unit/math/mix/fun/complex_schur_decompose_test.cpp
+++ test/unit/math/mix/fun/complex_schur_decompose_test.cpp
@@ -33,18 +33,15 @@ template <typename V>
 void test_complex_schur_decompose(const Eigen::MatrixXd& x) {
   using stan::math::complex_schur_decompose_t;
   using stan::math::complex_schur_decompose_u;
+  using stan::math::get_real;
   using stan::math::value_of_rec;
-  Eigen::Matrix<V, -1, -1> X(x.rows(), x.cols());
-  for (int i = 0; i < x.size(); ++i)
-    X(i) = x(i);
+  Eigen::Matrix<V, -1, -1> X = x;
+
   auto T = complex_schur_decompose_t(X);
   auto U = complex_schur_decompose_u(X);
   auto X2 = U * T * U.adjoint();
-  for (int j = 0; j < x.cols(); ++j) {
-    for (int i = 0; i < x.rows(); ++i) {
-      EXPECT_FLOAT_EQ(x(i, j), value_of_rec(X2(i, j).real()));
-    }
-  }
+
+  EXPECT_MATRIX_NEAR(x, value_of_rec(get_real(X2)), 1e-8);
 }
 
 template <typename V>
@@ -58,14 +55,8 @@ void test_complex_schur_decompose_complex(const Eigen::MatrixXd& x) {
   auto T = complex_schur_decompose_t(X);
   auto U = complex_schur_decompose_u(X);
   auto X2 = U * T * U.adjoint();
-  for (int j = 0; j < x.cols(); ++j) {
-    for (int i = 0; i < x.rows(); ++i) {
-      EXPECT_NEAR(value_of_rec(X(i, j).real()), value_of_rec(X2(i, j).real()),
-                  1e-8);
-      EXPECT_NEAR(value_of_rec(X(i, j)).imag(), value_of_rec(X2(i, j).imag()),
-                  1e-8);
-    }
-  }
+
+  EXPECT_MATRIX_COMPLEX_NEAR(value_of_rec(X), value_of_rec(X2), 1e-8);
 }
 
 TEST(mathMixFun, complexSchurDecompose) {
