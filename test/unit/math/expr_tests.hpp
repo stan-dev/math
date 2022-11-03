9002b49db07e1ad3c4e542b6e1dd0c26aa20b967
stevebronder <stevo15025@gmail.com>
2022-11-03 10:35:24 -0400

    fix static init for scalar_type helper

--- test/unit/math/expr_tests.hpp
+++ test/unit/math/expr_tests.hpp
@@ -23,6 +23,7 @@ struct char_scalar_type<double> {
   static constexpr const char* scalar{"double"};
 };
 constexpr const char* char_scalar_type<double>::scalar;
+
 template <>
 struct char_scalar_type<stan::math::var> {
   static constexpr const char* scalar{"var"};
@@ -33,12 +34,14 @@ template <>
 struct char_scalar_type<stan::math::fvar<double>> {
   static constexpr const char* scalar{"fvar<double>"};
 };
+constexpr const char* char_scalar_type<stan::math::fvar<stan::math::var>>::scalar;
 
 template <>
 struct char_scalar_type<std::complex<double>> {
   static constexpr const char* scalar{"std::complex<double>"};
 };
 constexpr const char* char_scalar_type<std::complex<double>>::scalar;
+
 template <>
 struct char_scalar_type<std::complex<stan::math::var>> {
   static constexpr const char* scalar{"std::complex<var>"};
@@ -49,9 +52,7 @@ template <>
 struct char_scalar_type<std::complex<stan::math::fvar<double>>> {
   static constexpr const char* scalar{"std::complex<fvar<double>>"};
 };
-
-constexpr const char*
-    char_scalar_type<std::complex<stan::math::fvar<double>>>::scalar;
+constexpr const char* char_scalar_type<std::complex<stan::math::fvar<double>>>::scalar;
 
 /**
  * Check that the evaluations were less than the size of the input size.
