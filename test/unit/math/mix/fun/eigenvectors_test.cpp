09676e5a116b1e540d7baf5449445b1c99959378
Brian Ward <bward@flatironinstitute.org>
2022-11-14 12:06:20 -0500

    Add overloads to eigendecomposition for complex matrices

--- test/unit/math/mix/fun/eigenvectors_test.cpp
+++ test/unit/math/mix/fun/eigenvectors_test.cpp
@@ -15,6 +15,20 @@ TEST(mathMixFun, eigenvectors) {
   EXPECT_THROW(f(a32), std::invalid_argument);
 }
 
+TEST(mathMixFun, eigenvectorsComplex) {
+  auto f = [](const auto& x) {
+    using stan::math::eigenvectors;
+    return eigenvectors(stan::math::to_complex(x, 0));
+  };
+  for (const auto& x : stan::test::square_test_matrices(0, 2)) {
+    stan::test::expect_ad(f, x);
+  }
+
+  Eigen::MatrixXd a32(3, 2);
+  a32 << 3, -5, 7, -7.2, 9.1, -6.3;
+  EXPECT_THROW(f(a32), std::invalid_argument);
+}
+
 template <typename T>
 void expect_identity_matrix(const T& x) {
   EXPECT_EQUAL(x.rows(), x.cols());
