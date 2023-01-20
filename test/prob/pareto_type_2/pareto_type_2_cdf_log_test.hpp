6576960ca6b6df0c78a030191f5b857458ce5e23
Daniel Lee <bearlee@alum.mit.edu>
2023-01-20 07:18:13 -0500

    updating test/prob/pareto_type_2

--- test/prob/pareto_type_2/pareto_type_2_cdf_log_test.hpp
+++ test/prob/pareto_type_2/pareto_type_2_cdf_log_test.hpp
@@ -1,6 +1,6 @@
 // Arguments: Doubles, Doubles, Doubles, Doubles
-#include <stan/math/prim.hpp>
-
+#include <stan/math/prim/prob/pareto_type_2_cdf_log.hpp>
+#include <stan/math/prim/fun/log1m.hpp>
 using stan::math::var;
 using std::numeric_limits;
 using std::vector;
