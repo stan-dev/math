66601e9d25f85d0ce7d621653ee01824d0293493
Daniel Lee <bearlee@alum.mit.edu>
2023-01-19 23:31:43 -0500

    updating test/prob/neg_binomial

--- test/prob/neg_binomial/neg_binomial_cdf_test.hpp
+++ test/prob/neg_binomial/neg_binomial_cdf_test.hpp
@@ -1,5 +1,7 @@
 // Arguments: Ints, Doubles, Doubles
-#include <stan/math/prim.hpp>
+#include <stan/math/prim/prob/neg_binomial_cdf.hpp>
+#include <stan/math/prim/fun/log.hpp>
+#include <stan/math/prim/fun/exp.hpp>
 #include <boost/math/special_functions/binomial.hpp>
 
 using stan::math::var;
