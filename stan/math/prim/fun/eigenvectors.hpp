bc3a33e6d3ae06794fb061d5d5813c77a9cf170a
Brian Ward <bward@flatironinstitute.org>
2023-02-06 11:36:06 -0500

    Explicit return types, improved testing

--- stan/math/prim/fun/eigenvectors.hpp
+++ stan/math/prim/fun/eigenvectors.hpp
@@ -18,7 +18,8 @@ namespace math {
  */
 template <typename EigMat, require_eigen_matrix_dynamic_t<EigMat>* = nullptr,
           require_not_vt_complex<EigMat>* = nullptr>
-inline auto eigenvectors(const EigMat& m) {
+inline Eigen::Matrix<complex_return_t<value_type_t<EigMat>>, -1, -1>
+eigenvectors(const EigMat& m) {
   using PlainMat = plain_type_t<EigMat>;
   const PlainMat& m_eval = m;
   check_nonzero_size("eigenvectors", "m", m_eval);
@@ -31,16 +32,18 @@ inline auto eigenvectors(const EigMat& m) {
 /**
  * Return the eigenvectors of a (complex-valued) matrix
  *
- * @tparam EigMat type of complex matrix argument
+ * @tparam EigCplxMat type of complex matrix argument
  * @param[in] m matrix to find the eigenvectors of. Must be square and have a
  * non-zero size.
  * @return a complex-valued matrix with columns representing the eigenvectors of
  * `m`
  */
-template <typename EigMat, require_eigen_matrix_dynamic_t<EigMat>* = nullptr,
-          require_vt_complex<EigMat>* = nullptr>
-inline auto eigenvectors(const EigMat& m) {
-  using PlainMat = Eigen::Matrix<scalar_type_t<EigMat>, -1, -1>;
+template <typename EigCplxMat,
+          require_eigen_matrix_dynamic_t<EigCplxMat>* = nullptr,
+          require_vt_complex<EigCplxMat>* = nullptr>
+inline Eigen::Matrix<complex_return_t<value_type_t<EigCplxMat>>, -1, -1>
+eigenvectors(const EigCplxMat& m) {
+  using PlainMat = Eigen::Matrix<scalar_type_t<EigCplxMat>, -1, -1>;
   const PlainMat& m_eval = m;
   check_nonzero_size("eigenvectors", "m", m_eval);
   check_square("eigenvectors", "m", m_eval);
