324d24cc8f521e914ce2dd91d8479b2977e4f842
Steve Bronder <sbronder@flatironinstitute.org>
2023-05-04 10:22:58 -0400

    upgrading to boost v1.82.0; pruning files

--- lib/boost_1.82.0/libs/mysql/test/integration/entry_point.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Copyright (c) 2019-2023 Ruben Perez Hidalgo (rubenperez038 at gmail dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#define BOOST_TEST_MODULE boost_mysql_integrationtests
-#include <boost/test/unit_test.hpp>
