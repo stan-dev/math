ffa76777847e451ae101b6c213af8ed41b07fc9a
Stan Jenkins <mc.stanislaw@gmail.com>
2023-07-01 09:53:06 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- stan/math/prim/prob/poisson_log_glm_lpmf.hpp
+++ stan/math/prim/prob/poisson_log_glm_lpmf.hpp
@@ -91,8 +91,7 @@ return_type_t<T_x, T_alpha, T_beta> poisson_log_glm_lpmf(const T_y& y,
   T_beta_ref beta_ref = beta;
 
   const auto& y_val = value_of(y_ref);
-  const auto& x_val
-      = to_ref_if<!is_constant<T_beta>::value>(value_of(x_ref));
+  const auto& x_val = to_ref_if<!is_constant<T_beta>::value>(value_of(x_ref));
   const auto& alpha_val = value_of(alpha_ref);
   const auto& beta_val = value_of(beta_ref);
 
