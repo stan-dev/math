7f7a8744b29ef23aceeb381f357d9e56c34d594c
Stan Jenkins <mc.stanislaw@gmail.com>
2023-07-24 18:05:13 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- test/unit/math/mix/fun/read_cov_matrix_test.cpp
+++ test/unit/math/mix/fun/read_cov_matrix_test.cpp
@@ -4,7 +4,8 @@ TEST(mathMixMatFun, read_cov_matrix) {
   auto f = [](int K) {
     Eigen::VectorXd rx2 = (Eigen::VectorXd::Random(K).array() + 2.0).matrix();
     return [K, rx2](const auto& x1) {
-      stan::plain_type_t<std::decay_t<decltype(x1)>> x2 = stan::math::add(x1.head(K), rx2);
+      stan::plain_type_t<std::decay_t<decltype(x1)>> x2
+          = stan::math::add(x1.head(K), rx2);
       return stan::math::read_cov_matrix(x1, x2);
     };
   };
@@ -28,7 +29,8 @@ TEST(mathMixMatFun, read_cov_matrix_lp) {
     Eigen::VectorXd rx2 = (Eigen::VectorXd::Random(K).array() + 2.0).matrix();
     return [K, rx2](const auto& x1) {
       stan::scalar_type_t<decltype(x1)> lp = 0.0;
-      stan::plain_type_t<std::decay_t<decltype(x1)>> x2 = stan::math::add(x1.head(K), rx2);
+      stan::plain_type_t<std::decay_t<decltype(x1)>> x2
+          = stan::math::add(x1.head(K), rx2);
       return stan::math::read_cov_matrix(x1, x2, lp);
     };
   };
