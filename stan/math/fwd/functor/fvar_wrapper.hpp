a26d064f53a50c78799164af7fc8ac8b45ad8527
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-08-07 13:32:52 +0300

    Working single-order gradients

--- stan/math/fwd/functor/fvar_wrapper.hpp
+++ stan/math/fwd/functor/fvar_wrapper.hpp
@@ -3,12 +3,18 @@
 
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/functor/apply.hpp>
+#include <stan/math/prim/functor/for_each.hpp>
 #include <stan/math/prim/functor/finite_diff_gradient_auto.hpp>
 #include <stan/math/prim/fun/value_of.hpp>
+#include <stan/math/prim/fun/as_array_or_scalar.hpp>
 #include <stan/math/prim/fun/sum.hpp>
+#include <stan/math/prim/fun/eval.hpp>
 #include <stan/math/prim/fun/elt_multiply.hpp>
 #include <test/unit/math/serializer.hpp>
 
+namespace stan {
+namespace math {
+namespace internal {
 template <typename T, require_arithmetic_t<T>* = nullptr>
 constexpr double grad(const T& x) {
   return 0;
@@ -16,13 +22,14 @@ constexpr double grad(const T& x) {
 
 template <typename T, require_fvar_t<T>* = nullptr>
 auto grad(const T& x) {
-  return x.d();
+  return math::eval(x.d());
+}
 }
 
 template <typename F, typename... TArgs>
 auto fvar_wrapper(const F& func, const TArgs&... args) {
   // Create a tuple of double-only arguments
-  auto prim_args = std::forward_as_tuple(stan::math::value_of(args)...);
+  auto prim_args = std::make_tuple(stan::math::value_of(args)...);
 
   // Flatten the input arguments to a single column vector
   auto serial_args = stan::math::apply(
@@ -44,11 +51,20 @@ auto fvar_wrapper(const F& func, const TArgs&... args) {
   finite_diff_gradient_auto(serial_functor, serial_args, rtn_value, grad);
 
   auto ds_grad = stan::test::to_deserializer(grad);
-  double rtn_grad;
+  auto grads = std::make_tuple(ds_grad.read(args)...);
+  auto args_tuple = std::forward_as_tuple(args...);
+  double rtn_grad = 0;
 
-  (void)std::initializer_list<int>{ (rtn_grad += sum(elt_multiply(ds.read(args), grad(args))), 0)... };
+  math::for_each([&](auto&& fun_grad, auto&& arg){
+    if (is_fvar<scalar_type_t<decltype(arg)>>::value) {
+      rtn_grad += sum(elt_multiply(fun_grad, internal::grad(arg)));
+    }
+    return;
+  }, std::forward<decltype(grads)>(grads), std::forward<decltype(args_tuple)>(args_tuple));
 
   return fvar<double>(rtn_value, rtn_grad);
 }
+}  // namespace math
+}  // namespace stan
 
 #endif
--- stan/math/fwd/functor/integrate_1d.hpp
+++ stan/math/fwd/functor/integrate_1d.hpp
@@ -3,6 +3,8 @@
 
 #include <stan/math/fwd/meta.hpp>
 #include <stan/math/prim/functor/integrate_1d.hpp>
+#include <stan/math/prim/fun/value_of.hpp>
+#include <stan/math/prim/meta/forward_as.hpp>
 #include <stan/math/fwd/functor/fvar_wrapper.hpp>
 
 namespace stan {
@@ -13,20 +15,37 @@ template <typename F, typename T_a, typename T_b, typename... Args,
 inline return_type_t<T_a, T_b, Args...> integrate_1d_impl(
     const F &f, const T_a &a, const T_b &b, double relative_tolerance,
     std::ostream *msgs, const Args &... args) {
-  auto func = [&f,&msgs,&relative_tolerance,](auto&& a_var, auto&& b_var, auto&&... args_var) {
-    return integrate_1d_impl(f, a_var, b_var, relative_tolerance,
-                           msgs, args_var...);
+  double a_val = value_of(a);
+  double b_val = value_of(b);
+  auto func = [f,msgs,relative_tolerance,a_val,b_val](const auto&... args_var) {
+    return integrate_1d_impl(f, a_val, b_val, relative_tolerance, msgs, args_var...);
   };
-  return fvar_wrapper(func, a, b, args...);
+  fvar<double> ret = fvar_wrapper(func, args...);
+  if (is_fvar<T_a>::value || is_fvar<T_b>::value) {
+    auto val_args = std::make_tuple(value_of(args)...);
+    if (is_fvar<T_a>::value) {
+      ret.d_ += math::forward_as<fvar<double>>(a).d_ *
+        math::apply([&](auto&&... tuple_args) {
+          return -f(a_val, 0.0, msgs, tuple_args...);
+        }, val_args);
+    }
+    if (is_fvar<T_b>::value) {
+      ret.d_ += math::forward_as<fvar<double>>(b).d_ *
+        math::apply([&](auto&&... tuple_args) {
+          return f(b_val, 0.0, msgs, tuple_args...);
+        }, val_args);
+    }
+  }
+  return ret;
 }
 
 template <typename F, typename T_a, typename T_b, typename T_theta,
-          typename = require_any_fvar_t<T_a, T_b, T_theta>>
+          require_any_fvar_t<T_a, T_b, T_theta> * = nullptr>
 inline return_type_t<T_a, T_b, T_theta> integrate_1d(
     const F &f, const T_a &a, const T_b &b, const std::vector<T_theta> &theta,
     const std::vector<double> &x_r, const std::vector<int> &x_i,
-    std::ostream *msgs, const double relative_tolerance = std::sqrt(EPSILON)) {
-  auto func = [&f,&msgs,&x_r,&x_i](auto&& a_var, auto&& b_var, auto&& theta_var) {
+    std::ostream *msgs, const double relative_tolerance) {
+  auto func = [&f,&msgs,&x_r,&x_i,&relative_tolerance](auto&& a_var, auto&& b_var, auto&& theta_var) {
     return integrate_1d_impl(integrate_1d_adapter<F>(f), a_var, b_var, relative_tolerance,
                            msgs, theta_var, x_r, x_i);
   };
