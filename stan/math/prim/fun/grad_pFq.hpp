d5ce3db8ee7320e3868afc72d9ff8822c6af0b3f
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2023-10-04 13:07:13 +0300

    Don't calc for non-autodiff

--- stan/math/prim/fun/grad_pFq.hpp
+++ stan/math/prim/fun/grad_pFq.hpp
@@ -36,15 +36,16 @@ auto grad_pFq(const Ta& a, const Tb& b, const Tz& z,
                 double precision = 1e-16,
                 int max_steps = 1e8) {
   using T_partials = partials_return_t<Ta, Tb, Tz>;
-  using Ta_partials = promote_scalar_t<T_partials, plain_type_t<Ta>>;
-  using Tb_partials = promote_scalar_t<T_partials, plain_type_t<Tb>>;
-  using Tz_partials = promote_scalar_t<T_partials, plain_type_t<Tz>>;
+  using Ta_partials = partials_return_t<Ta>;
+  using Tb_partials = partials_return_t<Tb>;
+  using Tz_partials = partials_return_t<Tz>;
   using T_ArrayPartials = promote_scalar_t<T_partials, Eigen::Array<double, -1, 1>>;
-  using T_return = std::tuple<Ta_partials, Tb_partials, Tz_partials>;
   using IntArray = Eigen::Array<int, -1, 1>;
+  using Ta_PartialsArray = promote_scalar_t<Ta_partials, Eigen::Array<double, -1, 1>>;
+  using Tb_PartialsArray = promote_scalar_t<Tb_partials, Eigen::Array<double, -1, 1>>;
 
-  T_ArrayPartials a_val = as_value_column_array_or_scalar(a);
-  T_ArrayPartials b_val = as_value_column_array_or_scalar(b);
+  Ta_PartialsArray a_val = as_value_column_array_or_scalar(a);
+  Tb_PartialsArray b_val = as_value_column_array_or_scalar(b);
   Tz_partials z_val = value_of(z);
 
   IntArray a_signs = sign(value_of_rec(a_val));
@@ -55,29 +56,34 @@ auto grad_pFq(const Ta& a, const Tb& b, const Tz& z,
   IntArray b_k_signs = b_signs;
   int z_k_sign = 1;
 
-  T_ArrayPartials a_k = a_val;
-  T_ArrayPartials b_k = b_val;
+  Ta_PartialsArray a_k = a_val;
+  Tb_PartialsArray b_k = b_val;
   Tz_partials log_z_pow_k = 0.0;
   Tz_partials log_z_k = log(abs(z_val));
 
-  T_ArrayPartials log_phammer_a_k = T_ArrayPartials::Zero(a.size());
-  T_ArrayPartials log_phammer_b_k = T_ArrayPartials::Zero(b.size());
+  Ta_PartialsArray log_phammer_a_k = T_ArrayPartials::Zero(a.size());
+  Tb_PartialsArray log_phammer_b_k = T_ArrayPartials::Zero(b.size());
   IntArray phammer_a_k_signs = IntArray::Ones(a.size());
   IntArray phammer_b_k_signs = IntArray::Ones(b.size());
 
-  T_ArrayPartials digamma_a = inv(a_k);
-  T_ArrayPartials digamma_b = inv(b_k);
-  T_ArrayPartials digamma_a_k = digamma_a;
-  T_ArrayPartials digamma_b_k = digamma_b;
-  T_ArrayPartials log_digamma_a_k = log(abs(digamma_a));
-  T_ArrayPartials log_digamma_b_k = log(abs(digamma_b));
+  Ta_PartialsArray digamma_a = inv(a_k);
+  Tb_PartialsArray digamma_b = inv(b_k);
+  Ta_PartialsArray digamma_a_k = digamma_a;
+  Tb_PartialsArray digamma_b_k = digamma_b;
+  Ta_PartialsArray log_digamma_a_k = log(abs(digamma_a));
+  Tb_PartialsArray log_digamma_b_k = log(abs(digamma_b));
   IntArray digamma_a_k_signs = a_k_signs;
   IntArray digamma_b_k_signs = b_k_signs;
+
   T_ArrayPartials a_log_infsum = T_ArrayPartials::Constant(a.size(), NEGATIVE_INFTY);
   T_ArrayPartials b_log_infsum = T_ArrayPartials::Constant(b.size(), NEGATIVE_INFTY);
-
   IntArray a_infsum_sign = IntArray::Ones(a.size());
   IntArray b_infsum_sign = IntArray::Ones(b.size());
+
+  T_ArrayPartials a_grad = T_ArrayPartials::Constant(a.size(), NEGATIVE_INFTY);
+  IntArray a_grad_signs = IntArray::Ones(a.size());
+  T_ArrayPartials b_grad = T_ArrayPartials::Constant(a.size(), NEGATIVE_INFTY);
+  IntArray b_grad_signs = IntArray::Ones(a.size());
 
   int k = 0;
   double curr_log_prec = 1.0;
@@ -87,21 +93,28 @@ auto grad_pFq(const Ta& a, const Tb& b, const Tz& z,
     T_partials log_base = (sum(log_phammer_a_k) + log_z_pow_k)
                           - (log_factorial_k + sum(log_phammer_b_k));
 
-    T_ArrayPartials a_grad = log_digamma_a_k + log_base;
-    T_ArrayPartials b_grad = log_digamma_b_k + log_base;
-    IntArray a_grad_signs = base_sign * digamma_a_k_signs;
-    IntArray b_grad_signs = base_sign * digamma_b_k_signs;
+    if (!is_constant_all<Ta>::value) {
+      a_grad = log_digamma_a_k + log_base;
+      a_grad_signs = base_sign * digamma_a_k_signs;
 
-    for (int i = 0; i < a.size(); i++) {
-      std::forward_as_tuple(a_log_infsum(i), a_infsum_sign(i))
-        = log_sum_exp_signed(a_log_infsum(i), a_infsum_sign(i),
-                              a_grad(i), a_grad_signs(i));
+      for (int i = 0; i < a.size(); i++) {
+        std::forward_as_tuple(a_log_infsum(i), a_infsum_sign(i))
+          = log_sum_exp_signed(a_log_infsum(i), a_infsum_sign(i),
+                                a_grad(i), a_grad_signs(i));
+      }
     }
-    for (int i = 0; i < b.size(); i++) {
-      std::forward_as_tuple(b_log_infsum(i), b_infsum_sign(i))
-        = log_sum_exp_signed(b_log_infsum(i), b_infsum_sign(i),
-                              b_grad(i), b_grad_signs(i));
+
+    if (!is_constant_all<Tb>::value) {
+      b_grad = log_digamma_b_k + log_base;
+      b_grad_signs = base_sign * digamma_b_k_signs;
+
+      for (int i = 0; i < b.size(); i++) {
+        std::forward_as_tuple(b_log_infsum(i), b_infsum_sign(i))
+          = log_sum_exp_signed(b_log_infsum(i), b_infsum_sign(i),
+                                b_grad(i), b_grad_signs(i));
+      }
     }
+
     curr_log_prec = max(a_grad.maxCoeff(), b_grad.maxCoeff());
     log_phammer_a_k += log(abs(a_k));
     log_phammer_b_k += log(abs(b_k));
@@ -127,13 +140,23 @@ auto grad_pFq(const Ta& a, const Tb& b, const Tz& z,
     log_factorial_k += log(k);
   }
 
-  T_partials pfq_val = hypergeometric_pFq(a_val, b_val, z_val);
-  T_partials pfq_p1_val = hypergeometric_pFq(a_val + 1, b_val + 1, z_val);
+  std::tuple<promote_scalar_t<T_partials, plain_type_t<Ta>>,
+              promote_scalar_t<T_partials, plain_type_t<Tb>>,
+              T_partials> ret_tuple;
 
-  T_return ret_tuple;
-  std::get<0>(ret_tuple) = a_infsum_sign * exp(a_log_infsum) - digamma_a * pfq_val;
-  std::get<1>(ret_tuple) = digamma_b * pfq_val - b_infsum_sign * exp(b_log_infsum);
-  std::get<2>(ret_tuple) = prod(a_val) / prod(b_val) * pfq_p1_val;
+  if (!is_constant_all<Ta, Tb>::value) {
+    T_partials pfq_val = hypergeometric_pFq(a_val, b_val, z_val);
+    if (!is_constant_all<Ta>::value) {
+      std::get<0>(ret_tuple) = a_infsum_sign * exp(a_log_infsum) - digamma_a * pfq_val;
+    }
+    if (!is_constant_all<Tb>::value) {
+      std::get<1>(ret_tuple) = digamma_b * pfq_val - b_infsum_sign * exp(b_log_infsum);
+    }
+  }
+  if (!is_constant_all<Tz>::value) {
+    T_partials pfq_p1_val = hypergeometric_pFq(a_val + 1, b_val + 1, z_val);
+    std::get<2>(ret_tuple) = prod(a_val) / prod(b_val) * pfq_p1_val;
+  }
   return ret_tuple;
 }
 
