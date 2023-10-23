6e469ef910d76a60a3e357780824f0175a19711d
Franzi2114 <franziska.henrich@psychologie.uni-freiburg.de>
2023-10-23 09:23:02 +0200

    test test

---
+++ test/unit/math/mix/prob/wiener5_lpdf_x_test_test.cpp
@@ -0,0 +1,20 @@
+#include <stan/math/mix.hpp>
+#include <test/unit/math/test_ad.hpp>
+
+TEST(mathMixScalFun_a_v_sv, wiener5_lpdf) {
+  auto f_a_v_sv = [](const auto& y, const auto& t0, const auto& w) {
+    return [&y, &t0, &w](const auto& a, const auto& v, const auto& sv) {
+      return stan::math::wiener5_lpdf(y, a, t0, w, v, sv, 1e-4);
+    };
+  };
+
+  double y = 1.0;
+  double a = 2.0;
+  double t0 = 0.2;
+  double w = 0.5;
+  double v = 2.0;
+  double sv = 0.2;
+
+  stan::test::expect_ad(f_a_v_sv(y, t0, w), a, v, sv);
+}
+
