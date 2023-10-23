141c044b7f907e2ad1e185fb6951e37e49eeb9af
Franzi2114 <franziska.henrich@psychologie.uni-freiburg.de>
2023-10-23 09:28:44 +0200

    test

--- test/unit/math/mix/prob/wiener5_lpdf_x_test_test.cpp
+++ test/unit/math/mix/prob/wiener5_lpdf_x_test_test.cpp
@@ -18,7 +18,7 @@
   stan::test::expect_ad(f_a_v_sv(y, t0, w), a, v, sv);
 }*/
 
-TEST(mathMixScalFun_a_v_sv, wiener5_lpdf) {
+/*TEST(mathMixScalFun_a_v_sv, wiener5_lpdf) {
   auto f_a_v_sv = [](const auto& y, const auto& t0, const auto& w, const auto& sv) {
     return [&y, &t0, &w, &sv](const auto& a, const auto& v) {
       return stan::math::wiener5_lpdf(y, a, t0, w, v, sv, 1e-4);
@@ -33,6 +33,23 @@ TEST(mathMixScalFun_a_v_sv, wiener5_lpdf) {
   double sv = 0.2;
 
   stan::test::expect_ad(f_a_v_sv(y, t0, w, sv), a, v);
+}*/
+
+TEST(mathMixScalFun_a_v_sv, wiener5_lpdf) {
+  auto f_a_v_sv = [](const auto& y, const auto& t0, const auto& w, const auto& v, const auto& sv) {
+    return [&y, &t0, &w, &v, &sv](const auto& a) {
+      return stan::math::wiener5_lpdf(y, a, t0, w, v, sv, 1e-4);
+    };
+  };
+
+  double y = 1.0;
+  double a = 2.0;
+  double t0 = 0.2;
+  double w = 0.5;
+  double v = 2.0;
+  double sv = 0.2;
+
+  stan::test::expect_ad(f_a_v_sv(y, t0, w, v, sv), a);
 }
 
 
