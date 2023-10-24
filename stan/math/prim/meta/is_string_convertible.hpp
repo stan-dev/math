5c1c078de05fa7b553501dc688427b7506c89e85
Daniel Lee <bearlee@alum.mit.edu>
2023-10-23 22:11:47 -0400

    removing unused require_ from stan/math/prim/meta/is_string_convertible.hpp

--- stan/math/prim/meta/is_string_convertible.hpp
+++ stan/math/prim/meta/is_string_convertible.hpp
@@ -17,80 +17,8 @@ using is_string_convertible = std::is_convertible<T, std::string>;
 
 // STAN_ADD_REQUIRE_UNARY(string_convertible, is_string_convertible,
 // require_std);
-template <typename T>
-using require_string_convertible_t
-    = require_t<is_string_convertible<std::decay_t<T>>>;
-
-template <typename T>
-using require_not_string_convertible_t
-    = require_not_t<is_string_convertible<std::decay_t<T>>>;
-
-template <typename... Types>
-using require_all_string_convertible_t
-    = require_all_t<is_string_convertible<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_string_convertible_t
-    = require_any_t<is_string_convertible<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_all_not_string_convertible_t
-    = require_all_not_t<is_string_convertible<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_not_string_convertible_t
-    = require_any_not_t<is_string_convertible<std::decay_t<Types>>...>;
-
 // STAN_ADD_REQUIRE_UNARY_INNER(string_convertible, is_string_convertible,
 // require_std);
-template <typename T>
-using require_vt_string_convertible
-    = require_t<is_string_convertible<value_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_vt_string_convertible
-    = require_not_t<is_string_convertible<value_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_vt_string_convertible = require_all_t<
-    is_string_convertible<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_vt_string_convertible = require_any_t<
-    is_string_convertible<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_vt_string_convertible = require_all_not_t<
-    is_string_convertible<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_vt_string_convertible = require_any_not_t<
-    is_string_convertible<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename T>
-using require_st_string_convertible
-    = require_t<is_string_convertible<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_st_string_convertible
-    = require_not_t<is_string_convertible<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_st_string_convertible = require_all_t<
-    is_string_convertible<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_st_string_convertible = require_any_t<
-    is_string_convertible<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_st_string_convertible = require_all_not_t<
-    is_string_convertible<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_st_string_convertible = require_any_not_t<
-    is_string_convertible<scalar_type_t<std::decay_t<Types>>>...>;
-
 }  // namespace stan
 
 #endif
