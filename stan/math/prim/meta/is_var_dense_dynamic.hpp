c4625524395c706acffd5dea214d3892145d31bd
Daniel Lee <bearlee@alum.mit.edu>
2023-10-24 01:23:58 -0400

    removing unused require_ from stan/math/prim/meta/is_var_dense_dynamic.hpp

--- stan/math/prim/meta/is_var_dense_dynamic.hpp
+++ stan/math/prim/meta/is_var_dense_dynamic.hpp
@@ -20,80 +20,8 @@ struct is_var_dense_dynamic
 
 // STAN_ADD_REQUIRE_UNARY(var_dense_dynamic, is_var_dense_dynamic,
 // require_eigens_types);
-template <typename T>
-using require_var_dense_dynamic_t
-    = require_t<is_var_dense_dynamic<std::decay_t<T>>>;
-
-template <typename T>
-using require_not_var_dense_dynamic_t
-    = require_not_t<is_var_dense_dynamic<std::decay_t<T>>>;
-
-template <typename... Types>
-using require_all_var_dense_dynamic_t
-    = require_all_t<is_var_dense_dynamic<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_var_dense_dynamic_t
-    = require_any_t<is_var_dense_dynamic<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_all_not_var_dense_dynamic_t
-    = require_all_not_t<is_var_dense_dynamic<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_not_var_dense_dynamic_t
-    = require_any_not_t<is_var_dense_dynamic<std::decay_t<Types>>...>;
-
 // STAN_ADD_REQUIRE_UNARY_INNER(var_dense_dynamic, is_var_dense_dynamic,
 // require_eigens_types);
-template <typename T>
-using require_vt_var_dense_dynamic
-    = require_t<is_var_dense_dynamic<value_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_vt_var_dense_dynamic
-    = require_not_t<is_var_dense_dynamic<value_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_vt_var_dense_dynamic
-    = require_all_t<is_var_dense_dynamic<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_vt_var_dense_dynamic
-    = require_any_t<is_var_dense_dynamic<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_vt_var_dense_dynamic = require_all_not_t<
-    is_var_dense_dynamic<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_vt_var_dense_dynamic = require_any_not_t<
-    is_var_dense_dynamic<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename T>
-using require_st_var_dense_dynamic
-    = require_t<is_var_dense_dynamic<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_st_var_dense_dynamic
-    = require_not_t<is_var_dense_dynamic<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_st_var_dense_dynamic = require_all_t<
-    is_var_dense_dynamic<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_st_var_dense_dynamic = require_any_t<
-    is_var_dense_dynamic<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_st_var_dense_dynamic = require_all_not_t<
-    is_var_dense_dynamic<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_st_var_dense_dynamic = require_any_not_t<
-    is_var_dense_dynamic<scalar_type_t<std::decay_t<Types>>>...>;
-
 }  // namespace stan
 
 #endif
