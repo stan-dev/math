b53b8c1ffc7c155e925bdf21a382a1d269aa99a0
Daniel Lee <bearlee@alum.mit.edu>
2023-10-24 01:23:03 -0400

    removing unused require_ from stan/math/prim/meta/is_tuple.hpp

--- stan/math/prim/meta/is_tuple.hpp
+++ stan/math/prim/meta/is_tuple.hpp
@@ -29,64 +29,7 @@ using require_not_tuple_t = require_not_t<is_tuple<std::decay_t<T>>>;
 template <typename... Types>
 using require_all_tuple_t = require_all_t<is_tuple<std::decay_t<Types>>...>;
 
-template <typename... Types>
-using require_any_tuple_t = require_any_t<is_tuple<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_all_not_tuple_t
-    = require_all_not_t<is_tuple<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_not_tuple_t
-    = require_any_not_t<is_tuple<std::decay_t<Types>>...>;
-
 // STAN_ADD_REQUIRE_UNARY_INNER(tuple, is_tuple, require_std);
-template <typename T>
-using require_vt_tuple = require_t<is_tuple<value_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_vt_tuple
-    = require_not_t<is_tuple<value_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_vt_tuple
-    = require_all_t<is_tuple<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_vt_tuple
-    = require_any_t<is_tuple<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_vt_tuple
-    = require_all_not_t<is_tuple<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_vt_tuple
-    = require_any_not_t<is_tuple<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename T>
-using require_st_tuple = require_t<is_tuple<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_st_tuple
-    = require_not_t<is_tuple<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_st_tuple
-    = require_all_t<is_tuple<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_st_tuple
-    = require_any_t<is_tuple<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_st_tuple
-    = require_all_not_t<is_tuple<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_st_tuple
-    = require_any_not_t<is_tuple<scalar_type_t<std::decay_t<Types>>>...>;
-
 }  // namespace math
 }  // namespace stan
 
