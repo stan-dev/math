cf11045623bafe33d28cad984f096aa10b928c4a
Daniel Lee <bearlee@alum.mit.edu>
2023-10-23 22:04:03 -0400

    removing unused require_ from stan/math/prim/meta/is_eigen_sparse_base.hpp

--- stan/math/prim/meta/is_eigen_sparse_base.hpp
+++ stan/math/prim/meta/is_eigen_sparse_base.hpp
@@ -28,88 +28,8 @@ template <typename T>
 using require_eigen_sparse_base_t
     = require_t<is_eigen_sparse_base<std::decay_t<T>>>;
 
-template <typename T>
-using require_not_eigen_sparse_base_t
-    = require_not_t<is_eigen_sparse_base<std::decay_t<T>>>;
-
-template <typename... Types>
-using require_all_eigen_sparse_base_t
-    = require_all_t<is_eigen_sparse_base<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_eigen_sparse_base_t
-    = require_any_t<is_eigen_sparse_base<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_all_not_eigen_sparse_base_t
-    = require_all_not_t<is_eigen_sparse_base<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_not_eigen_sparse_base_t
-    = require_any_not_t<is_eigen_sparse_base<std::decay_t<Types>>...>;
-
 // STAN_ADD_REQUIRE_CONTAINER(eigen_sparse_base, is_eigen_sparse_base,
 // require_eigens_types);
-template <template <class...> class TypeCheck, class... Check>
-using require_eigen_sparse_base_vt
-    = require_t<container_type_check_base<is_eigen_sparse_base, value_type_t,
-                                          TypeCheck, Check...>>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_not_eigen_sparse_base_vt
-    = require_not_t<container_type_check_base<
-        is_eigen_sparse_base, value_type_t, TypeCheck, Check...>>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_any_eigen_sparse_base_vt
-    = require_any_t<container_type_check_base<
-        is_eigen_sparse_base, value_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_any_not_eigen_sparse_base_vt
-    = require_any_not_t<container_type_check_base<
-        is_eigen_sparse_base, value_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_all_eigen_sparse_base_vt
-    = require_all_t<container_type_check_base<
-        is_eigen_sparse_base, value_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_all_not_eigen_sparse_base_vt
-    = require_all_not_t<container_type_check_base<
-        is_eigen_sparse_base, value_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_eigen_sparse_base_st
-    = require_t<container_type_check_base<is_eigen_sparse_base, scalar_type_t,
-                                          TypeCheck, Check...>>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_not_eigen_sparse_base_st
-    = require_not_t<container_type_check_base<
-        is_eigen_sparse_base, scalar_type_t, TypeCheck, Check...>>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_any_eigen_sparse_base_st
-    = require_any_t<container_type_check_base<
-        is_eigen_sparse_base, scalar_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_any_not_eigen_sparse_base_st
-    = require_any_not_t<container_type_check_base<
-        is_eigen_sparse_base, scalar_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_all_eigen_sparse_base_st
-    = require_all_t<container_type_check_base<
-        is_eigen_sparse_base, scalar_type_t, TypeCheck, Check>...>;
-
-template <template <class...> class TypeCheck, class... Check>
-using require_all_not_eigen_sparse_base_st
-    = require_all_not_t<container_type_check_base<
-        is_eigen_sparse_base, scalar_type_t, TypeCheck, Check>...>;
-
 }  // namespace stan
 
 #endif
