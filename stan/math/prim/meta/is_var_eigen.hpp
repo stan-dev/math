c3320f39464b7c0023c0ac440217cd29f14d2b60
Daniel Lee <bearlee@alum.mit.edu>
2023-10-24 01:24:59 -0400

    removing unused require_ from stan/math/prim/meta/is_var_eigen.hpp

--- stan/math/prim/meta/is_var_eigen.hpp
+++ stan/math/prim/meta/is_var_eigen.hpp
@@ -19,77 +19,7 @@ struct is_var_eigen
           math::conjunction<is_var<T>, is_eigen<value_type_t<T>>>::value> {};
 
 // STAN_ADD_REQUIRE_UNARY(var_eigen, is_var_eigen, require_eigens_types);
-template <typename T>
-using require_var_eigen_t = require_t<is_var_eigen<std::decay_t<T>>>;
-
-template <typename T>
-using require_not_var_eigen_t = require_not_t<is_var_eigen<std::decay_t<T>>>;
-
-template <typename... Types>
-using require_all_var_eigen_t
-    = require_all_t<is_var_eigen<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_var_eigen_t
-    = require_any_t<is_var_eigen<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_all_not_var_eigen_t
-    = require_all_not_t<is_var_eigen<std::decay_t<Types>>...>;
-
-template <typename... Types>
-using require_any_not_var_eigen_t
-    = require_any_not_t<is_var_eigen<std::decay_t<Types>>...>;
-
 // STAN_ADD_REQUIRE_UNARY_INNER(var_eigen, is_var_eigen, require_eigens_types);
-template <typename T>
-using require_vt_var_eigen
-    = require_t<is_var_eigen<value_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_vt_var_eigen
-    = require_not_t<is_var_eigen<value_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_vt_var_eigen
-    = require_all_t<is_var_eigen<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_vt_var_eigen
-    = require_any_t<is_var_eigen<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_vt_var_eigen
-    = require_all_not_t<is_var_eigen<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_vt_var_eigen
-    = require_any_not_t<is_var_eigen<value_type_t<std::decay_t<Types>>>...>;
-
-template <typename T>
-using require_st_var_eigen
-    = require_t<is_var_eigen<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename T>
-using require_not_st_var_eigen
-    = require_not_t<is_var_eigen<scalar_type_t<std::decay_t<T>>>>;
-
-template <typename... Types>
-using require_all_st_var_eigen
-    = require_all_t<is_var_eigen<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_st_var_eigen
-    = require_any_t<is_var_eigen<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_all_not_st_var_eigen
-    = require_all_not_t<is_var_eigen<scalar_type_t<std::decay_t<Types>>>...>;
-
-template <typename... Types>
-using require_any_not_st_var_eigen
-    = require_any_not_t<is_var_eigen<scalar_type_t<std::decay_t<Types>>>...>;
-
 }  // namespace stan
 
 #endif
