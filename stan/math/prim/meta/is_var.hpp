d8d92d65530918838890c590d35b0a976fe7bfcb
Daniel Lee <bearlee@alum.mit.edu>
2023-11-27 22:36:36 -0500

    adding back in require_all_var_t

--- stan/math/prim/meta/is_var.hpp
+++ stan/math/prim/meta/is_var.hpp
@@ -16,10 +16,14 @@ struct is_var : std::false_type {};
 // STAN_ADD_REQUIRE_UNARY(var, is_var, require_stan_scalar_real);
 template <typename T>
 using require_var_t = require_t<is_var<std::decay_t<T>>>;
-
+  
 template <typename T>
 using require_not_var_t = require_not_t<is_var<std::decay_t<T>>>;
 
+template <typename... Types>
+using require_all_var_t
+    = require_all_t<is_var<std::decay_t<Types>>...>;
+  
 template <typename... Types>
 using require_any_var_t = require_any_t<is_var<std::decay_t<Types>>...>;
 
