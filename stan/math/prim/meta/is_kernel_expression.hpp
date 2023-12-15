b6967ab736e301be7e99fc061a7f2befad8b1d87
Stan Jenkins <mc.stanislaw@gmail.com>
2023-12-14 21:18:51 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- stan/math/prim/meta/is_kernel_expression.hpp
+++ stan/math/prim/meta/is_kernel_expression.hpp
@@ -115,7 +115,7 @@ using require_kernel_expression_lhs_t
 template <typename T>
 using require_rev_kernel_expression_t
     = require_t<is_rev_kernel_expression<std::decay_t<T>>>;
-  
+
 //  STAN_ADD_REQUIRE_UNARY(prim_or_rev_kernel_expression,
 //  is_prim_or_rev_kernel_expression, opencl_kernel_generator);
 template <typename... Types>
