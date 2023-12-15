032b135e48d5673cdce1e39bbd22c26180c26805
Daniel Lee <bearlee@alum.mit.edu>
2023-12-14 21:17:57 -0500

    adding back in a require trait that is used by Stan

--- stan/math/prim/meta/is_kernel_expression.hpp
+++ stan/math/prim/meta/is_kernel_expression.hpp
@@ -112,6 +112,10 @@ using require_kernel_expression_lhs_t
 
 // STAN_ADD_REQUIRE_UNARY(rev_kernel_expression, is_rev_kernel_expression,
 // opencl_kernel_generator);
+template <typename T>
+using require_rev_kernel_expression_t
+    = require_t<is_rev_kernel_expression<std::decay_t<T>>>;
+  
 //  STAN_ADD_REQUIRE_UNARY(prim_or_rev_kernel_expression,
 //  is_prim_or_rev_kernel_expression, opencl_kernel_generator);
 template <typename... Types>
