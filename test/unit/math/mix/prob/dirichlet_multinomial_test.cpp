40151acf2f054352bfb16b2bb40dafe86175be10
Christiaan van Dorp <chvandorp@gmail.com>
2023-12-16 15:26:34 -0500

    style changes, removed _log file, optimized lpmf

--- test/unit/math/mix/prob/dirichlet_multinomial_test.cpp
+++ test/unit/math/mix/prob/dirichlet_multinomial_test.cpp
@@ -1,6 +1,6 @@
 #include <test/unit/math/test_ad.hpp>
 
-TEST(ProbDistributions, dirichlet_multinomial) {
+TEST(ProbDistributionsDirichletMultinomial, dirichlet_multinomial) {
   // bind integer vector arg because can't autodiff through
   auto f = [](const std::vector<int>& y) {
     return [=](const auto& alpha) {
@@ -11,6 +11,8 @@ TEST(ProbDistributions, dirichlet_multinomial) {
 
   std::vector<int> y1 = {1, 2, 3, 4};
   std::vector<int> y2 = {30, 75, 409, 34};
+  // test if zero counts are handled correctly
+  std::vector<int> y3 = {0, 5, 0, 10};
 
   Eigen::VectorXd alpha1(4);
   alpha1 << 1.0, 2.0, 3.0, 4.0;
@@ -25,9 +27,11 @@ TEST(ProbDistributions, dirichlet_multinomial) {
   EXPECT_NO_THROW(f(y1)(alpha2));
   EXPECT_NO_THROW(f(y2)(alpha1));
   EXPECT_NO_THROW(f(y2)(alpha2));
+  EXPECT_NO_THROW(f(y3)(alpha1));
 
   stan::test::expect_ad(f(y1), alpha1);
   stan::test::expect_ad(f(y1), alpha2);
   stan::test::expect_ad(f(y2), alpha1);
   stan::test::expect_ad(f(y2), alpha2);
+  stan::test::expect_ad(f(y3), alpha1);
 }
