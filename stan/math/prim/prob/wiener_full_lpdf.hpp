899652e997c2c087f2567e70629c42a055b4056a
Franzi2114 <franziska.henrich@psychologie.uni-freiburg.de>
2024-02-16 11:00:00 +0100

    docu

--- stan/math/prim/prob/wiener_full_lpdf.hpp
+++ stan/math/prim/prob/wiener_full_lpdf.hpp
@@ -277,6 +277,11 @@ inline auto wiener7_integrate(const Wiener7FunctorT& wiener7_functor,
  @code
  target += wiener_lpdf(y, a, 0, w, v, sv, sw, 0)
  @endcode
+ * If only inter-trial variability for the drift rate is needed can write 
+ something like:
+ @code
+ target += wiener_lpdf(y, a, t0, w, v, sv)
+ @endcode
  *
  * To also control the precision in the estimation of the partial derivatives:
  @code
