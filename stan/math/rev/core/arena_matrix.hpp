b8e496d6c63f8f065fbafbda809872e097ef1497
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-03-27 15:46:27 +0200

    Fix hmm_marginal

--- stan/math/rev/core/arena_matrix.hpp
+++ stan/math/rev/core/arena_matrix.hpp
@@ -4,6 +4,7 @@
 #include <stan/math/prim/fun/Eigen.hpp>
 #include <stan/math/rev/core/chainable_alloc.hpp>
 #include <stan/math/rev/core/chainablestack.hpp>
+#include <stan/math/rev/core/chainable_object.hpp>
 #include <stan/math/rev/core/var_value_fwd_declare.hpp>
 #include <stan/math/prim/fun/to_ref.hpp>
 
