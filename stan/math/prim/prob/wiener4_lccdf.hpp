72617b15fa672d0b4f70531aa24210f872cfad46
Franzi2114 <franziska.henrich@psychologie.uni-freiburg.de>
2024-04-02 22:00:18 +0200

    add include to wiener4_lccdf

--- stan/math/prim/prob/wiener4_lccdf.hpp
+++ stan/math/prim/prob/wiener4_lccdf.hpp
@@ -2,6 +2,7 @@
 #define STAN_MATH_PRIM_PROB_WIENER4_LCCDF_HPP
 
 #include <stan/math/prim/fun.hpp>
+#include <stan/math/prim/prob/wiener5_lpdf.hpp>
 
 namespace stan {
 namespace math {
@@ -212,8 +213,7 @@ inline auto wiener4_ccdf(const T_y& y, const T_a& a, const T_v& v, const T_w& w,
                          T_wildcard&& wildcard = 0.0,
                          T_err&& err = log(1e-12)) noexcept {
   const auto prob = exp(wiener_prob(a, v, w));
-  const auto cdf
-      = internal::wiener4_distribution<GradientCalc::ON>(y, a, v, w, 0, err);
+  const auto cdf = internal::wiener4_distribution<GradientCalc::ON>(y, a, v, w, 0, err);
   return prob - cdf;
 }
 
