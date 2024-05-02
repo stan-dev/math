650e1e644349259465b255a54ae78b306b16959f
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-05-02 08:54:38 +0300

    num_elements ambiguous

--- stan/math/prim/fun/num_elements.hpp
+++ stan/math/prim/fun/num_elements.hpp
@@ -2,27 +2,46 @@
 #define STAN_MATH_PRIM_FUN_NUM_ELEMENTS_HPP
 
 #include <stan/math/prim/fun/Eigen.hpp>
-#include <stan/math/prim/fun/size.hpp>
-#include <stan/math/prim/fun/sum.hpp>
 #include <stan/math/prim/meta.hpp>
-#include <stan/math/prim/functor/apply.hpp>
 #include <vector>
-#include <algorithm>
 
 namespace stan {
 namespace math {
 
 /**
- * Returns the number of elements in the specified, non-nested input
+ * Returns 1, the number of elements in a primitive type.
  *
- * @tparam T type of input
- * @param v argument
+ * @tparam T scalar type
+ * @param x Argument of primitive type.
+ * @return 1
+ */
+template <typename T, require_stan_scalar_t<T>* = nullptr>
+inline size_t num_elements(const T& x) {
+  return 1;
+}
+
+/**
+ * Returns the size of the specified matrix.
+ *
+ * @tparam T type of the matrix
+ *
+ * @param m argument matrix
+ * @return size of matrix
+ */
+template <typename T, require_matrix_t<T>* = nullptr>
+inline size_t num_elements(const T& m) {
+  return m.size();
+}
+
+/**
+ * Returns the number of elements in the specified vector.
+ * @tparam T type of elements in the vector
+ * @param v argument vector
  * @return number of contained arguments
  */
-template <typename T, require_not_std_vector_vt<is_container, T>* = nullptr,
-          require_not_tuple_t<T>* = nullptr>
-inline size_t num_elements(const T& v) {
-  return math::size(v);
+template <typename T, require_stan_scalar_t<T>* = nullptr>
+inline size_t num_elements(const std::vector<T>& v) {
+  return v.size();
 }
 
 /**
@@ -32,8 +51,8 @@ inline size_t num_elements(const T& v) {
  * @param v argument vector
  * @return number of contained arguments
  */
-template <typename T, require_std_vector_vt<is_container, T>* = nullptr>
-inline size_t num_elements(const T& v) {
+template <typename T, require_container_t<T>* = nullptr>
+inline size_t num_elements(const std::vector<T>& v) {
   size_t size = 0;
   for (auto&& v_val : v) {
     size += num_elements(v_val);
@@ -41,6 +60,13 @@ inline size_t num_elements(const T& v) {
   return size;
 }
 
+/**
+ * Returns the number of elements in the specified tuple
+ *
+ * @tparam T type of tuple
+ * @param v tuple
+ * @return number of contained arguments
+ */
 template <typename T, require_tuple_t<T>* = nullptr>
 inline size_t num_elements(const T& v) {
   size_t size = 0;
