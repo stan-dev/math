7ccf07cc80377561b85f1e0feb379fce4b150504
Steve Bronder <sbronder@flatironinstitute.org>
2024-05-29 15:11:15 -0400

    fix windows compile tests

--- test/unit/unit_pch.cpp
+++ test/unit/unit_pch.cpp
@@ -1,7 +1,7 @@
-#include <stan/math.hpp>
+#include <test/unit/math/test_ad.hpp>
+#include <test/unit/math/test_ad_matvar.hpp>
 #include <stan/math/mix.hpp>
 #include <stan/math/fwd.hpp>
 #include <stan/math/rev.hpp>
 #include <stan/math/prim.hpp>
-#include <test/unit/math/test_ad.hpp>
-#include <test/unit/math/test_ad_matvar.hpp>
+#include <stan/math.hpp>
