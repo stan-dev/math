508b87044867bd6c578142ff96b05f3904c2d907
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2024-07-31 10:48:58 +0300

    Use std_normal_lpdf for quantile gradients

--- stan/math/rev/prob/std_normal_log_qf.hpp
+++ stan/math/rev/prob/std_normal_log_qf.hpp
@@ -3,8 +3,10 @@
 
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/core.hpp>
+#include <stan/math/prim/functor/apply_scalar_ternary.hpp>
 #include <stan/math/prim/fun/constants.hpp>
 #include <stan/math/prim/fun/sign.hpp>
+#include <stan/math/prim/prob/std_normal_lpdf.hpp>
 #include <stan/math/prim/prob/std_normal_log_qf.hpp>
 #include <cmath>
 
@@ -21,13 +23,13 @@ template <typename T, require_stan_scalar_or_eigen_t<T>* = nullptr>
 inline auto std_normal_log_qf(const var_value<T>& log_p) {
   return make_callback_var(
       std_normal_log_qf(log_p.val()), [log_p](auto& vi) mutable {
-        auto vi_array = as_array_or_scalar(vi.val());
-        auto vi_sign = sign(as_array_or_scalar(vi.adj()));
+        auto deriv
+          = apply_scalar_ternary(
+              [](const auto& logp_val, const auto& vi_val, const auto& vi_adj) {
+                return vi_adj * exp(logp_val - std_normal_lpdf(vi_val));
+              }, log_p.val(), vi.val(), vi.adj());
 
-        const auto& deriv = as_array_or_scalar(log_p).val()
-                            + log(as_array_or_scalar(vi.adj()) * vi_sign)
-                            - NEG_LOG_SQRT_TWO_PI + 0.5 * square(vi_array);
-        as_array_or_scalar(log_p).adj() += vi_sign * exp(deriv);
+        as_array_or_scalar(log_p).adj() += as_array_or_scalar(deriv);
       });
 }
 
