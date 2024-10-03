a931acdc82347852164ac4df77c786e8ce97ac6c
Brian Ward <bward@flatironinstitute.org>
2024-10-03 16:03:07 -0400

    QoL improvements when running a subset of the expression tests

--- test/sig_utils.py
+++ test/sig_utils.py
@@ -132,7 +132,7 @@ special_arg_values = {
     "uniform_lcdf": [None, 0.2, 0.9],
     "uniform_lpdf": [None, 0.2, 0.9],
     "uniform_rng": [0.2, 1.9, None],
-    "wiener_lpdf": [0.8, None, 0.4, None, None],
+    "wiener_lpdf": [0.8, None, 0.4, None, None, None, None, None],
 }
 
 # list of functions we do not test. These are mainly functions implemented in compiler
