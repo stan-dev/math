75145164a66712cbfc7a7db2c6dc4975abbeb325
Zhi Ling <1336265834@qq.com>
2024-10-31 21:30:55 +0800

    add function to prim/prob.hpp

--- stan/math/prim/prob.hpp
+++ stan/math/prim/prob.hpp
@@ -26,6 +26,7 @@
 #include <stan/math/prim/prob/beta_lcdf.hpp>
 #include <stan/math/prim/prob/beta_lpdf.hpp>
 #include <stan/math/prim/prob/beta_neg_binomial_lccdf.hpp>
+#include <stan/math/prim/prob/beta_neg_binomial_lcdf.hpp>
 #include <stan/math/prim/prob/beta_neg_binomial_lpmf.hpp>
 #include <stan/math/prim/prob/beta_proportion_ccdf_log.hpp>
 #include <stan/math/prim/prob/beta_proportion_cdf_log.hpp>
