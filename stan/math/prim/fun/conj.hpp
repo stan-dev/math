19689947af80693fe6845382c24e7b29f1fa2c8d
Steve Bronder <sbronder@flatironinstitute.org>
2024-11-07 18:25:22 -0500

    fix complex conj

--- stan/math/prim/fun/conj.hpp
+++ stan/math/prim/fun/conj.hpp
@@ -31,21 +31,6 @@ inline V conj(const V& z) {
   return {z.real(), -z.imag()};
 }
 
-/**
- * Return the complex conjugate the Eigen object.
- *
- * @tparam Eig A type derived from `Eigen::EigenBase` with an inner complex arithmetic type.
- * @param[in] z argument
- * @return complex conjugate of the argument
- */
-template <typename Eig, require_eigen_vt<is_complex_arithmetic, Eig>* = nullptr>
-inline auto conj(Eig&& z) {
-  return make_holder([](auto&& z_inner) {
-    return z_inner.unaryExpr([](auto&& z_i) {
-      return conj(z_i);
-    });
-  }, std::forward<Eig>(z));
-}
 
 /**
  * Return the complex conjugate the Eigen object.
@@ -54,7 +39,7 @@ inline auto conj(Eig&& z) {
  * @param[in] z argument
  * @return complex conjugate of the argument
  */
-template <typename Eig, require_eigen_vt<is_complex_ad, Eig>* = nullptr>
+template <typename Eig, require_eigen_vt<is_complex, Eig>* = nullptr>
 inline auto conj(Eig&& z) {
   return make_holder([](auto&& z_inner) {
     return z_inner.unaryExpr([](auto&& z_i) {
@@ -70,7 +55,7 @@ inline auto conj(Eig&& z) {
  * @param[in] z argument
  * @return complex conjugate of the argument
  */
-template <typename StdVec, require_std_vector_st<is_complex_arithmetic, StdVec>* = nullptr>
+template <typename StdVec, require_std_vector_st<is_complex, StdVec>* = nullptr>
 inline auto conj(const StdVec& z) {
   const auto z_size = z.size();
   promote_scalar_t<scalar_type_t<StdVec>, StdVec> result(z_size);
@@ -80,22 +65,6 @@ inline auto conj(const StdVec& z) {
   return result;
 }
 
-/**
- * Return the complex conjugate the vector with complex scalar components.
- *
- * @tparam StdVec A `std::vector` type with complex scalar type
- * @param[in] z argument
- * @return complex conjugate of the argument
- */
-template <typename StdVec, require_std_vector_st<is_complex_ad, StdVec>* = nullptr>
-inline auto conj(const StdVec& z) {
-  const auto z_size = z.size();
-  promote_scalar_t<scalar_type_t<StdVec>, StdVec> result(z_size);
-  for (std::size_t i = 0; i < z_size; ++i) {
-    result[i] = conj(z[i]);
-  }
-  return result;
-}
 
 }  // namespace math
 }  // namespace stan
