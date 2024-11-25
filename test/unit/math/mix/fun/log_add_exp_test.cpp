0ef959683efda3bb6869f04bbf86e8a7a23a8683
Stan Jenkins <mc.stanislaw@gmail.com>
2024-11-25 12:47:50 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- test/unit/math/mix/fun/log_add_exp_test.cpp
+++ test/unit/math/mix/fun/log_add_exp_test.cpp
@@ -4,68 +4,68 @@
 #include <iostream>
 
 TEST(MathMixMatFun, logAddExp) {
-    auto f = [](const auto& x, const auto& y) {
+  auto f = [](const auto& x, const auto& y) {
     return stan::math::log_add_exp(x, y);
-    };
-    // Test with finite values
-    Eigen::VectorXd x1(2);
-    x1 << 2.0, 1.0;
-    Eigen::VectorXd y1(2);
-    y1 << 3.0, 2.0;
-    stan::test::expect_ad(f, x1, y1);
+  };
+  // Test with finite values
+  Eigen::VectorXd x1(2);
+  x1 << 2.0, 1.0;
+  Eigen::VectorXd y1(2);
+  y1 << 3.0, 2.0;
+  stan::test::expect_ad(f, x1, y1);
 
-    // Test with negative infinity
+  // Test with negative infinity
 
-    stan::test::expect_ad(f, stan::math::NEGATIVE_INFTY, 1.0);
-    stan::test::expect_ad(f, 1.0, stan::math::NEGATIVE_INFTY);
+  stan::test::expect_ad(f, stan::math::NEGATIVE_INFTY, 1.0);
+  stan::test::expect_ad(f, 1.0, stan::math::NEGATIVE_INFTY);
 
-    // Test with infinity
-    stan::test::expect_ad(f, stan::math::INFTY, stan::math::INFTY);
+  // Test with infinity
+  stan::test::expect_ad(f, stan::math::INFTY, stan::math::INFTY);
 }
 
 TEST(MathMixMatFun, log_add_exp_elementwise_values) {
-    auto f = [](const auto& x, const auto& y) {
+  auto f = [](const auto& x, const auto& y) {
     return stan::math::log_add_exp(x, y);
-    };
+  };
 
-    Eigen::VectorXd x1(2);
-    x1 << 2.0, 1.0;
-    Eigen::VectorXd y1(2);
-    y1 << 3.0, 2.0;
-    stan::test::expect_ad(f, x1, y1);
+  Eigen::VectorXd x1(2);
+  x1 << 2.0, 1.0;
+  Eigen::VectorXd y1(2);
+  y1 << 3.0, 2.0;
+  stan::test::expect_ad(f, x1, y1);
 
-    Eigen::VectorXd x2(2);
-    x2 << 0.5, -1.0;
-    Eigen::VectorXd y2(2);
-    y2 << 1.0, 2.0;
-    stan::test::expect_ad(f, x2, y2);
+  Eigen::VectorXd x2(2);
+  x2 << 0.5, -1.0;
+  Eigen::VectorXd y2(2);
+  y2 << 1.0, 2.0;
+  stan::test::expect_ad(f, x2, y2);
 
-    // Test with infinity
-    Eigen::VectorXd x3(2);
-    x3 << std::numeric_limits<double>::infinity(), 1.0;
-    Eigen::VectorXd y3(2);
-    y3 << 2.0, std::numeric_limits<double>::infinity();
-    stan::test::expect_ad(f, x3, y3);
+  // Test with infinity
+  Eigen::VectorXd x3(2);
+  x3 << std::numeric_limits<double>::infinity(), 1.0;
+  Eigen::VectorXd y3(2);
+  y3 << 2.0, std::numeric_limits<double>::infinity();
+  stan::test::expect_ad(f, x3, y3);
 }
 
 TEST(MathMixMatFun, log_add_exp_edge_cases) {
-    auto f = [](const auto& x, const auto& y) {
+  auto f = [](const auto& x, const auto& y) {
     return stan::math::log_add_exp(x, y);
-    };
+  };
 
-    stan::test::expect_ad(f, stan::math::NEGATIVE_INFTY, 1.0);
-    stan::test::expect_ad(f, 1.0, stan::math::NEGATIVE_INFTY);
-    stan::test::expect_ad(f, stan::math::INFTY, stan::math::INFTY);
+  stan::test::expect_ad(f, stan::math::NEGATIVE_INFTY, 1.0);
+  stan::test::expect_ad(f, 1.0, stan::math::NEGATIVE_INFTY);
+  stan::test::expect_ad(f, stan::math::INFTY, stan::math::INFTY);
 }
 
 TEST(MathMixMatFun, log_add_exp_mismatched_sizes) {
-    auto f = [](const auto& x, const auto& y) {
+  auto f = [](const auto& x, const auto& y) {
     return stan::math::log_add_exp(x, y);
-    };
+  };
 
-    std::vector<double> x{1.0, 2.0};
-    std::vector<double> y{1.0, 2.0, 3.0};
+  std::vector<double> x{1.0, 2.0};
+  std::vector<double> y{1.0, 2.0, 3.0};
 
-    stan::test::expect_ad(f, x, y);
-    stan::test::expect_ad(f, y, x);
+  stan::test::expect_ad(f, x, y);
+  stan::test::expect_ad(f, y, x);
 }
