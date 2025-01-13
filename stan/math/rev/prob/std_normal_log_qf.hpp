b713e5808feaf6702350631760d094a46929a4fc
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-01-13 23:41:31 +0800

    Update vectorisation

--- stan/math/rev/prob/std_normal_log_qf.hpp
+++ stan/math/rev/prob/std_normal_log_qf.hpp
@@ -4,7 +4,8 @@
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/core.hpp>
 #include <stan/math/prim/prob/std_normal_log_qf.hpp>
-#include <stan/math/prim/functor/apply_scalar_ternary.hpp>
+#include <stan/math/prim/functor/apply_scalar_binary.hpp>
+#include <stan/math/prim/fun/elt_multiply.hpp>
 #include <cmath>
 
 namespace stan {
@@ -20,11 +21,11 @@ template <typename T, require_stan_scalar_or_eigen_t<T>* = nullptr>
 inline auto std_normal_log_qf(const var_value<T>& log_p) {
   const auto& arena_rtn = to_arena(std_normal_log_qf(log_p.val()));
   return make_callback_var(arena_rtn, [log_p, arena_rtn](auto& vi) mutable {
-    log_p.adj() += apply_scalar_ternary(
-        [](const auto& adj, const auto& logp_val, const auto& rtn_val) {
-          return adj * exp(logp_val - std_normal_lpdf(rtn_val));
-        },
-        vi.adj(), log_p.val(), arena_rtn.val());
+    auto deriv = apply_scalar_binary(log_p.val(), arena_rtn,
+          [](const auto& logp_val, const auto& rtn_val) {
+            return exp(logp_val - std_normal_lpdf(rtn_val));
+          });
+    log_p.adj() += elt_multiply(vi.adj(), deriv);
   });
 }
 
