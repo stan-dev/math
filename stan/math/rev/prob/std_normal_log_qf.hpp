1e105c3e7a622ed7d05f7d247085d21ac3b893f8
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-01-13 23:03:37 +0800

    Improve numerical stability of normal quantile gradients

--- stan/math/rev/prob/std_normal_log_qf.hpp
+++ stan/math/rev/prob/std_normal_log_qf.hpp
@@ -3,9 +3,8 @@
 
 #include <stan/math/rev/meta.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/prim/fun/constants.hpp>
-#include <stan/math/prim/fun/sign.hpp>
 #include <stan/math/prim/prob/std_normal_log_qf.hpp>
+#include <stan/math/prim/functor/apply_scalar_ternary.hpp>
 #include <cmath>
 
 namespace stan {
@@ -19,15 +18,12 @@ namespace math {
  */
 template <typename T, require_stan_scalar_or_eigen_t<T>* = nullptr>
 inline auto std_normal_log_qf(const var_value<T>& log_p) {
-  return make_callback_var(
-      std_normal_log_qf(log_p.val()), [log_p](auto& vi) mutable {
-        auto vi_array = as_array_or_scalar(vi.val());
-        auto vi_sign = sign(as_array_or_scalar(vi.adj()));
-
-        const auto& deriv = as_array_or_scalar(log_p).val()
-                            + log(as_array_or_scalar(vi.adj()) * vi_sign)
-                            - NEG_LOG_SQRT_TWO_PI + 0.5 * square(vi_array);
-        as_array_or_scalar(log_p).adj() += vi_sign * exp(deriv);
+  const auto& arena_rtn = to_arena(std_normal_log_qf(log_p.val()));
+  return make_callback_var(arena_rtn, [log_p, arena_rtn](auto& vi) mutable {
+        log_p.adj() += apply_scalar_ternary(
+          [](const auto& adj, const auto& logp_val, const auto& rtn_val) {
+            return adj * exp(logp_val - std_normal_lpdf(rtn_val));
+          }, vi.adj(), log_p.val(), arena_rtn.val());
       });
 }
 
