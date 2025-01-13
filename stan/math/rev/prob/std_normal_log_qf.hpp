d7edcbd598c9d3c36593a654bf9a9d438234eb34
Stan Jenkins <mc.stanislaw@gmail.com>
2025-01-13 10:43:20 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- stan/math/rev/prob/std_normal_log_qf.hpp
+++ stan/math/rev/prob/std_normal_log_qf.hpp
@@ -21,10 +21,10 @@ template <typename T, require_stan_scalar_or_eigen_t<T>* = nullptr>
 inline auto std_normal_log_qf(const var_value<T>& log_p) {
   const auto& arena_rtn = to_arena(std_normal_log_qf(log_p.val()));
   return make_callback_var(arena_rtn, [log_p, arena_rtn](auto& vi) mutable {
-    auto deriv = apply_scalar_binary(log_p.val(), arena_rtn,
-          [](const auto& logp_val, const auto& rtn_val) {
-            return exp(logp_val - std_normal_lpdf(rtn_val));
-          });
+    auto deriv = apply_scalar_binary(
+        log_p.val(), arena_rtn, [](const auto& logp_val, const auto& rtn_val) {
+          return exp(logp_val - std_normal_lpdf(rtn_val));
+        });
     log_p.adj() += elt_multiply(vi.adj(), deriv);
   });
 }
