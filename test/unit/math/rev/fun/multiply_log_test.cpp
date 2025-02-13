7afd1a97982d99d4404408ab727cc307aabafe0e
stevebronder <stevebronder@stevebronder.com>
2025-02-13 15:17:22 -0500

    fix cpplint

--- test/unit/math/rev/fun/multiply_log_test.cpp
+++ test/unit/math/rev/fun/multiply_log_test.cpp
@@ -4,7 +4,7 @@
 #include <test/unit/math/rev/util.hpp>
 
 TEST(RevTest, multiply_log_issue3146) {
-  long int x = 1;
+  std::int32_t x = 1;
   using stan::math::var_value;
   using mat_t = Eigen::Matrix<double, -1, -1, 1, -1, -1>;
   var_value<mat_t> y = Eigen::MatrixXd::Random(10, 10);
