0f135b864c27c4eea0a417768f788640762339b6
isaacwachsman-dot <149420416+isaacwachsman-dot@users.noreply.github.com>
2025-02-17 22:45:58 -0500

    Update vectorized_logic.hpp

--- stan/math/rev/fun/vectorized_logic.hpp
+++ stan/math/rev/fun/vectorized_logic.hpp
@@ -58,77 +58,77 @@ vector<int> logical_geq(const vector<T>& vector1, const vector<T>& vector2) {
 }
 // Vector-scalar equality
 template <typename T>
-vector<int> logical_eq(const vector<T>& vector, T scalar) {
-    vector<int> result(vec.size());
-    for (size_t i = 0; i < vector.size(); i++) 
+vector<int> logical_eq(const vector<T>& vector1, T scalar) {
+    vector<int> result(vector1.size());
+    for (size_t i = 0; i < vector1.size(); i++) 
       {
-        result[i] = (vector[i] == scalar);  
+        result[i] = (vector1[i] == scalar);  
       }
     return result;
 }
 
 // Vector-scalar less than
 template <typename T>
-vector<int> logical_lt(const vector<T>& vector, T scalar) {
-    vector<int> result(vec.size());
-    for (size_t i = 0; i < vector.size(); i++) 
+vector<int> logical_lt(const vector<T>& vector1, T scalar) {
+    vector<int> result(vector1.size());
+    for (size_t i = 0; i < vector1.size(); i++) 
       {
-        result[i] = (vector[i] < scalar);  
+        result[i] = (vector1[i] < scalar);  
       }
     return result;
 }
 
 // Vector-scalar greater than
 template <typename T>
-vector<int> logical_gt(const vector<T>& vector, T scalar) {
-    vector<int> result(vec.size());
-    for (size_t i = 0; i < vector.size(); i++) 
+vector<int> logical_gt(const vector<T>& vector1, T scalar) {
+    vector<int> result(vector1.size());
+    for (size_t i = 0; i < vector1.size(); i++) 
       {
-        result[i] = (vector[i] > scalar);  
+        result[i] = (vector1[i] > scalar);  
       }
     return result;
 }
 
 // Vector-scalar less than or equal
 template <typename T>
-vector<int> logical_leq(const vector<T>& vector, T scalar) {
-    vector<int> result(vec.size());
-    for (size_t i = 0; i < vector.size(); i++) 
+vector<int> logical_leq(const vector<T>& vector1, T scalar) {
+    vector<int> result(vector1.size());
+    for (size_t i = 0; i < vector1.size(); i++) 
       {
-        result[i] = (vector[i] <= scalar);  
+        result[i] = (vector1[i] <= scalar);  
       }
     return result;
 }
 
 // Vector-scalar greater than or equal
 template <typename T>
-vector<int> logical_geq(const vector<T>& vector, T scalar) {
-    vector<int> result(vec.size());
-    for (size_t i = 0; i < vector.size(); i++) 
+vector<int> logical_geq(const vector<T>& vector1, T scalar) {
+    vector<int> result(vector1.size());
+    for (size_t i = 0; i < vector1.size(); i++) 
       {
-        result[i] = (vector[i] >= scalar);  
+        result[i] = (vector1[i] >= scalar);  
       }
     return result;
 }
 
 // Vector element-wise negation
 template <typename T>
-vector<int> logical_not(const vector<T>& vector) {
-    vector<int> result(vector.size());
-    for (size_t i = 0; i < vector.size(); i++) 
+vector<int> logical_not(const vector<T>& vector1) {
+    vector<int> result(vector1.size());
+    for (size_t i = 0; i < vector1.size(); i++) 
       {
-        result[i] = (vector[i] == 0);
+        result[i] = (vector1[i] == 0);
       }
     return result;
 }
 
 // Vector size-wise OR
 template <typename T>
-int any(const vector<T>& vector) {
-    for (size_t i = 0; i < vector.size(); i++) {
-        if (vector[i] != 0) 
+int any(const vector<T>& vector1) {
+    for (size_t i = 0; i < vector1.size(); i++) {
+        if (vector1[i] != 0) 
         {
-          return 1
+          return 1;
         };
     }
     return 0;
@@ -136,9 +136,9 @@ int any(const vector<T>& vector) {
 
 // Vector size-wise AND
 template <typename T>
-int all(const vector<T>& vector) {
-    for (size_t i = 0; i < vector.size(); i++) {
-        if (vector[i] == 0) 
+int all(const vector<T>& vector1) {
+    for (size_t i = 0; i < vector1.size(); i++) {
+        if (vector1[i] == 0) 
         {
           return 0;
         }
