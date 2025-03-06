e76cca419641b8a1d2a94e6b9280706750453fac
Brian Ward <bward@flatironinstitute.org>
2025-03-06 10:35:42 -0500

    Missed one Lp...

--- stan/math/prim/constraint/sum_to_zero_constrain.hpp
+++ stan/math/prim/constraint/sum_to_zero_constrain.hpp
@@ -100,16 +100,31 @@ inline plain_type_t<Vec> sum_to_zero_constrain(const Vec& y, Lp& lp) {
  * @tparam Vec A standard vector with inner type inheriting from
  * `Eigen::DenseBase` or a `var_value` with inner type inheriting from
  * `Eigen::DenseBase` with compile time dynamic rows and 1 column
+ * @param[in] y free vector
+ * @return Zero-sum vectors of dimensionality one greater than `y`
+ */
+template <typename T, require_std_vector_t<T>* = nullptr>
+inline auto sum_to_zero_constrain(const T& y) {
+  return apply_vector_unary<T>::apply(
+      y, [](auto&& v) { return sum_to_zero_constrain(v); });
+}
+
+/**
+ * Return a vector with sum zero corresponding to the specified
+ * free vector.
+ * This overload handles looping over the elements of a standard vector.
+ *
+ * @tparam Vec A standard vector with inner type inheriting from
+ * `Eigen::DenseBase` or a `var_value` with inner type inheriting from
+ * `Eigen::DenseBase` with compile time dynamic rows and 1 column
  * @tparam Lp unused
  * @param[in] y free vector
  * @param[in, out] lp unused
  * @return Zero-sum vectors of dimensionality one greater than `y`
  */
-template <typename T, typename... Lp, require_std_vector_t<T>* = nullptr>
-inline auto sum_to_zero_constrain(const T& y, Lp&... lp) {
-  static_assert(sizeof...(lp) == 0 || sizeof...(lp) == 1,
-                "sum_to_zero_constrain should be called with either "
-                "one or two arguments");
+template <typename T, typename Lp, require_std_vector_t<T>* = nullptr,
+          require_convertible_t<return_type_t<T>, Lp>* = nullptr>
+inline auto sum_to_zero_constrain(const T& y, Lp& lp) {
   return apply_vector_unary<T>::apply(
       y, [](auto&& v) { return sum_to_zero_constrain(v); });
 }
