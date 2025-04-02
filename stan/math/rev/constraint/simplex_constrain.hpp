91a83c09a7ae811657af2194c9d53af71f56b534
Brian Ward <bward@flatironinstitute.org>
2025-04-02 15:46:52 -0400

    Dead simple reverse mode, could be specialized further

--- stan/math/rev/constraint/simplex_constrain.hpp
+++ stan/math/rev/constraint/simplex_constrain.hpp
@@ -21,7 +21,9 @@ namespace math {
  * to 0 that sum to 1.  A vector with (K-1) unconstrained values
  * will produce a simplex of size K.
  *
- * The transform is based on a centered stick-breaking process.
+ * The simplex transform is defined using the Isometric Log Ratio
+ * transform (ILR). This is equilvalent to
+ * `softmax(sum_to_zero_constrain(y))`.
  *
  * @tparam T Type of vector to constrain
  * @param y Free vector input of dimensionality K - 1
@@ -29,43 +31,7 @@ namespace math {
  */
 template <typename T, require_rev_col_vector_t<T>* = nullptr>
 inline auto simplex_constrain(const T& y) {
-  using ret_type = plain_type_t<T>;
-
-  size_t N = y.size();
-  arena_t<T> arena_y = y;
-  arena_t<Eigen::VectorXd> arena_z(N);
-  Eigen::VectorXd x_val(N + 1);
-
-  double stick_len(1.0);
-  for (Eigen::Index k = 0; k < N; ++k) {
-    double log_N_minus_k = std::log(N - k);
-    arena_z.coeffRef(k) = inv_logit(arena_y.val().coeff(k) - log_N_minus_k);
-    x_val.coeffRef(k) = stick_len * arena_z.coeff(k);
-    stick_len -= x_val(k);
-  }
-  x_val.coeffRef(N) = stick_len;
-
-  arena_t<ret_type> arena_x = x_val;
-
-  if (unlikely(N == 0)) {
-    return ret_type(arena_x);
-  }
-
-  reverse_pass_callback([arena_y, arena_x, arena_z]() mutable {
-    int N = arena_y.size();
-    double stick_len_val = arena_x.val().coeff(N);
-    double stick_len_adj = arena_x.adj().coeff(N);
-    for (Eigen::Index k = N; k-- > 0;) {
-      arena_x.adj().coeffRef(k) -= stick_len_adj;
-      stick_len_val += arena_x.val().coeff(k);
-      stick_len_adj += arena_x.adj().coeff(k) * arena_z.coeff(k);
-      double arena_z_adj = arena_x.adj().coeff(k) * stick_len_val;
-      arena_y.adj().coeffRef(k)
-          += arena_z_adj * arena_z.coeff(k) * (1.0 - arena_z.coeff(k));
-    }
-  });
-
-  return ret_type(arena_x);
+  return softmax(sum_to_zero_constrain(y));
 }
 
 /**
@@ -73,8 +39,9 @@ inline auto simplex_constrain(const T& y) {
  * and increment the specified log probability reference with
  * the log absolute Jacobian determinant of the transform.
  *
- * The simplex transform is defined through a centered
- * stick-breaking process.
+ * The simplex transform is defined using the Isometric Log Ratio
+ * transform (ILR). This is equilvalent to
+ * `softmax(sum_to_zero_constrain(y))`.
  *
  * @tparam T type of the vector to constrain
  * @param y Free vector input of dimensionality N.
@@ -85,50 +52,13 @@ template <typename T, require_rev_col_vector_t<T>* = nullptr>
 auto simplex_constrain(const T& y, scalar_type_t<T>& lp) {
   using ret_type = plain_type_t<T>;
 
-  size_t N = y.size();
-  arena_t<T> arena_y = y;
-  arena_t<Eigen::VectorXd> arena_z(N);
-  Eigen::VectorXd x_val(N + 1);
+  arena_t<ret_type> log_x = log_softmax(sum_to_zero_constrain(y));
 
-  double stick_len(1.0);
-  for (Eigen::Index k = 0; k < N; ++k) {
-    double log_N_minus_k = std::log(N - k);
-    double adj_y_k = arena_y.val().coeff(k) - log_N_minus_k;
-    arena_z.coeffRef(k) = inv_logit(adj_y_k);
-    x_val.coeffRef(k) = stick_len * arena_z.coeff(k);
-    lp += log(stick_len);
-    lp -= log1p_exp(-adj_y_k);
-    lp -= log1p_exp(adj_y_k);
-    stick_len -= x_val(k);
-  }
-  x_val.coeffRef(N) = stick_len;
+  const auto N = y.size() + 1;
 
-  arena_t<ret_type> arena_x = x_val;
+  lp += sum(log_x) + 0.5 * log(N);
 
-  if (unlikely(N == 0)) {
-    return ret_type(arena_x);
-  }
-
-  reverse_pass_callback([arena_y, arena_x, arena_z, lp]() mutable {
-    int N = arena_y.size();
-    double stick_len_val = arena_x.val().coeff(N);
-    double stick_len_adj = arena_x.adj().coeff(N);
-    for (Eigen::Index k = N; k-- > 0;) {
-      arena_x.adj().coeffRef(k) -= stick_len_adj;
-      stick_len_val += arena_x.val().coeff(k);
-      double log_N_minus_k = std::log(N - k);
-      double adj_y_k = arena_y.val().coeff(k) - log_N_minus_k;
-      arena_y.adj().coeffRef(k) -= lp.adj() * inv_logit(adj_y_k);
-      arena_y.adj().coeffRef(k) += lp.adj() * inv_logit(-adj_y_k);
-      stick_len_adj += lp.adj() / stick_len_val;
-      stick_len_adj += arena_x.adj().coeff(k) * arena_z.coeff(k);
-      double arena_z_adj = arena_x.adj().coeff(k) * stick_len_val;
-      arena_y.adj().coeffRef(k)
-          += arena_z_adj * arena_z.coeff(k) * (1.0 - arena_z.coeff(k));
-    }
-  });
-
-  return ret_type(arena_x);
+  return ret_type(exp(log_x));
 }
 
 }  // namespace math
