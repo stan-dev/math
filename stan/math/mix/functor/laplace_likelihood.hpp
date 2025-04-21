8f176a61c4b48f596099f158aa488128fa5924af
Steve Bronder <sbronder@flatironinstitute.org>
2025-04-21 15:34:08 -0400

    fixing headers for laplace_likelihood

--- stan/math/mix/functor/laplace_likelihood.hpp
+++ stan/math/mix/functor/laplace_likelihood.hpp
@@ -2,9 +2,6 @@
 #define STAN_MATH_MIX_FUNCTOR_LAPLACE_LIKELIHOOD_HPP
 
 // #include <stan/math/mix/laplace/hessian_times_vector.hpp>
-#include <stan/math/fwd/fun/Eigen_NumTraits.hpp>
-#include <stan/math/rev/core/Eigen_NumTraits.hpp>
-#include <stan/math/prim/fun/Eigen.hpp>
 #include <stan/math/mix/functor/hessian_block_diag.hpp>
 #include <stan/math/rev/fun.hpp>
 #include <stan/math/fwd/fun.hpp>
