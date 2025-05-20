ef69c948fd190a74931dff0a39da96247b203a0b
Brian Ward <bward@flatironinstitute.org>
2025-05-20 11:54:50 -0400

    upgrading to boost v1.87.0; removing old boost library

--- lib/boost_1.84.0/libs/cobalt/cmake/coroutine.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Copyright (c) 2023 Klemens Morgenstern (klemens.morgenstern@gmx.net)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
-#include <coroutine>
