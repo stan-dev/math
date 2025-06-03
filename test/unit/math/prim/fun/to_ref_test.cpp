6c476f6f8ed8d892cd4ec285a712c8aae84b0fc9
Steve Bronder <sbronder@flatironinstitute.org>
2025-06-03 11:40:29 -0400

    update to_ref test to use Eigen::all instead of Eigen::placeholders::all

--- test/unit/math/prim/fun/to_ref_test.cpp
+++ test/unit/math/prim/fun/to_ref_test.cpp
@@ -25,7 +25,7 @@ TEST(MathMatrix, to_ref_matrix_exprs_tuple) {
 TEST(MathMatrix, to_ref_matrix_views_tuple) {
   Eigen::MatrixXd a = Eigen::MatrixXd::Random(3, 3);
   auto x = std::make_tuple(a.block(0, 0, 1, 1),
-                           a(Eigen::placeholders::all, std::vector{2, 1, 1}),
+                           a(Eigen::all, std::vector{2, 1, 1}),
                            a.array());
   auto x_ref = stan::math::to_ref(x);
   using x_ref_t = decltype(x_ref);
@@ -47,10 +47,10 @@ TEST(MathMatrix, to_ref_matrix_views_exprs_tuple) {
   auto x = std::make_tuple(
       a.block(0, 0, 1, 1),
       std::make_tuple(a.block(0, 0, 1, 1),
-                      a(Eigen::placeholders::all, std::vector{2, 1, 1}),
+                      a(Eigen::all, std::vector{2, 1, 1}),
                       a.array()),
       std::make_tuple(a * a, a, a.array() * 3),
-      a(Eigen::placeholders::all, std::vector{2, 1, 1}), a.array() * a.array());
+      a(Eigen::all, std::vector{2, 1, 1}), a.array() * a.array());
   auto x_ref = stan::math::to_ref(x);
   using x_ref_t = decltype(x_ref);
   using stan::test::is_same_tuple_element_v;
@@ -99,11 +99,11 @@ TEST(MathMatrix, to_ref_matrix_views_exprs_moves_tuple) {
       a.block(0, 0, 1, 1),
       std::forward_as_tuple(
           a.block(0, 0, 1, 1),
-          a(Eigen::placeholders::all,
+          a(Eigen::all,
             std::vector{Eigen::Index{2}, Eigen::Index{1}, Eigen::Index{1}}),
           a.array()),
       std::forward_as_tuple(a * a, a, a.array() * 3),
-      a(Eigen::placeholders::all,
+      a(Eigen::all,
         std::vector{Eigen::Index{2}, Eigen::Index{1}, Eigen::Index{1}}),
       a.array() * a.array()));
   using x_ref_t = decltype(x_ref);
