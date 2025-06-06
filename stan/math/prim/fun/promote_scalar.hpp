909e5087fef1f77a692255a59c83e8cb919c1e5a
Brian Ward <bward@flatironinstitute.org>
2025-06-06 09:09:40 -0400

    static assert in promote_scalar else branch

--- stan/math/prim/fun/promote_scalar.hpp
+++ stan/math/prim/fun/promote_scalar.hpp
@@ -44,9 +44,10 @@ inline constexpr auto promote_scalar(UnPromotedTypes&& x) {
   } else if constexpr (is_stan_scalar_v<UnPromotedTypes>) {
     return PromotionScalars(std::forward<UnPromotedTypes>(x));
   } else {
-    throw std::domain_error(
-        "promote_scalar: "
-        "Unrecognized type for promotion");
+    static_assert(sizeof(std::decay_t<UnPromotedTypes>*) == 0,
+                  "promote_scalar: Unrecognized type for promotion. "
+                  "This is an internal error, please report it to the stan "
+                  "github as an issue.");
   }
 }
 
