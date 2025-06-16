8b6806676b2a2c0692aed703b3e885d9ee66ad49
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-06-16 23:02:05 +0800

    Update test construction

--- test/unit/math/mix/prob/gaussian_copula_cholesky_test.cpp
+++ test/unit/math/mix/prob/gaussian_copula_cholesky_test.cpp
@@ -4,8 +4,8 @@ TEST_F(AgradRev, ProbDistributionsGaussCopulaCholesky) {
   // Bind functors for compatibility with AD framework
   auto f = [](const auto func1, const auto func2) {
     return [=](const auto& y, const auto& args, const auto& sigma) {
-      auto lcdf_functors = std::make_tuple(std::make_tuple(func1, args[0]),
-                                           std::make_tuple(func2, args[1]));
+      auto lcdf_functors = std::make_tuple(std::make_tuple(func1, args[0], args[1]),
+                                           std::make_tuple(func2));
       auto sigma_sym = stan::math::multiply(0.5, sigma + sigma.transpose());
       auto L = stan::math::cholesky_decompose(sigma_sym);
       return stan::math::gaussian_copula_cholesky_lpdf(y, lcdf_functors, L);
@@ -17,15 +17,15 @@ TEST_F(AgradRev, ProbDistributionsGaussCopulaCholesky) {
   Eigen::VectorXd y1(2);
   y1 << 1.0, 0.1;
   Eigen::VectorXd args(2);
-  args << 0, 2.0;
+  args << 2, 1;
 
-  auto func1 = [](const auto& y, const auto& mu) {
-    return stan::math::normal_lcdf(y, mu, 1.0);
+  auto func1 = [](const auto& y, const auto& shape, const auto& scale) {
+    return stan::math::gamma_lcdf(y, shape, scale);
   };
   auto func2 = [](const auto& y) { return stan::math::std_normal_lcdf(y); };
 
   Eigen::MatrixXd Sigma22(2, 2);
   Sigma22 << 2.0, 0.5, 0.5, 1.1;
 
-  stan::test::expect_ad(f(func2, func2), y1, args, Sigma22);
+  stan::test::expect_ad(f(func1, func2), y1, args, Sigma22);
 }
