f8c67018d4d85a36c03e278ee4dd5c7c666e2d15
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-06-19 13:53:13 +0800

    Compile fixs

--- stan/math/rev/prob/student_t_qf.hpp
+++ stan/math/rev/prob/student_t_qf.hpp
@@ -27,7 +27,7 @@ var student_t_qf(const T_p& p, const T_mu& mu,
   double ibeta_arg(0);
 
   if (p == 0.0) {
-    ret_val = NEGATIVE_INFTY
+    ret_val = NEGATIVE_INFTY;
   } else if (p == 1.0) {
     ret_val = INFTY;
   } else if (p == 0.5) {
@@ -41,7 +41,8 @@ var student_t_qf(const T_p& p, const T_mu& mu,
                   * math::sqrt(-1.0 + 1.0 / ibeta_arg);
   }
 
-  return make_callback_var(ret_val, [p, mu, sigma, nu, ibeta_arg, nu_val, p_sign](auto& vi) mutable {
+  return make_callback_var(ret_val, [p, mu, sigma, nu, ibeta_arg, nu_val, ret_val](auto& vi) mutable {
+    const double p_val = value_of(p);
     if constexpr (!is_constant<T_p>::value) {
       if (p.val() > 0.0 && p.val() < 1.0) {
         p.adj() += vi.adj() * exp(log(p_val) - student_t_lpdf(ret_val, mu.val(), sigma.val(), nu_val));
@@ -58,6 +59,8 @@ var student_t_qf(const T_p& p, const T_mu& mu,
       }
     }
     if constexpr (!is_constant<T_nu>::value) {
+      const double sigma_val = value_of(sigma);
+      const double p_sign = p_val < 0.5 ? -1.0 : 1.0;
       const double half_nu = nu_val / 2.0;
       const double hyper_arg = hypergeometric_3F2({0.5, half_nu, half_nu}, {1 + half_nu, 1 + half_nu}, ibeta_arg);
       const double hyper2f1 = hypergeometric_2F1(1, (1 + nu_val) / 2, (2 + nu_val) / 2, ibeta_arg);
@@ -67,7 +70,7 @@ var student_t_qf(const T_p& p, const T_mu& mu,
       const double arg_1 = (4 * hyper_arg * sqrt(1 - ibeta_arg)) / nu_val;
       const double arg_2 = -2 * hyper2f1 * (-1 + ibeta_arg) * (log(ibeta_arg) - digamma_a1 + digamma_a2);
 
-      const double num1 = s * (-2 + (2 - arg_1 + arg_2) / ibeta_arg);
+      const double num1 = sigma_val * (-2 + (2 - arg_1 + arg_2) / ibeta_arg);
       const double den1 = 4 * sqrt(nu_val) * sqrt(-1 + 1 / ibeta_arg);
       nu.adj() += vi.adj() * p_sign * num1 / den1;
     }
