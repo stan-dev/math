63004a792b09398bd2cbf27c5a8639cd82e1380d
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2025-06-20 09:40:45 +0800

    Dynamic

--- stan/math/rev/prob/student_t_qf.hpp
+++ stan/math/rev/prob/student_t_qf.hpp
@@ -64,10 +64,12 @@ var student_t_qf(const T_p& p, const T_nu& nu, const T_mu& mu,
     if constexpr (!is_constant<T_nu>::value) {
       const double half_nu = nu_val / 2.0;
       const double nu_p1_div_2 = (1 + nu_val) / 2.0;
-      Eigen::Vector3d hyper_arg_a{0.5, half_nu, half_nu};
-      Eigen::Vector2d hyper_arg_b{1 + half_nu, 1 + half_nu};
-      const double hyper_arg
-          = hypergeometric_pFq(hyper_arg_a, hyper_arg_b, ibeta_arg);
+      Eigen::VectorXd hyper_arg_a(3);
+      hyper_arg_a << 0.5, half_nu, half_nu;
+      Eigen::VectorXd hyper_arg_b(2);
+      hyper_arg_b << 1 + half_nu, 1 + half_nu;
+      const double hyper_arg = hypergeometric_pFq(hyper_arg_a, hyper_arg_b,
+                                                  ibeta_arg);
 
       const double hyper2f1 = hypergeometric_2F1(
           1.0, nu_p1_div_2, (2.0 + nu_val) / 2.0, ibeta_arg);
