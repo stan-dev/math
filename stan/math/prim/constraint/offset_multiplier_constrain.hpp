2b9d2611d4aef4cf5ea66e4c897bb2af8d92654f
Stan Jenkins <mc.stanislaw@gmail.com>
2025-07-02 14:48:59 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- stan/math/prim/constraint/offset_multiplier_constrain.hpp
+++ stan/math/prim/constraint/offset_multiplier_constrain.hpp
@@ -42,7 +42,7 @@ namespace math {
 template <typename T, typename M, typename S,
           require_all_not_nonscalar_prim_or_rev_kernel_expression_t<
               T, M, S>* = nullptr,
-              require_all_not_std_vector_t<T, M, S>* = nullptr>
+          require_all_not_std_vector_t<T, M, S>* = nullptr>
 inline auto offset_multiplier_constrain(T&& x, M&& mu, S&& sigma) {
   auto&& mu_ref = to_ref(std::forward<M>(mu));
   auto&& sigma_ref = to_ref(std::forward<S>(sigma));
@@ -96,7 +96,7 @@ template <typename T, typename M, typename S, typename Lp,
           require_convertible_t<return_type_t<T, M, S>, Lp>* = nullptr,
           require_all_not_nonscalar_prim_or_rev_kernel_expression_t<
               T, M, S>* = nullptr,
-              require_all_not_std_vector_t<T, M, S>* = nullptr>
+          require_all_not_std_vector_t<T, M, S>* = nullptr>
 inline auto offset_multiplier_constrain(T&& x, M&& mu, S&& sigma, Lp& lp) {
   auto&& mu_ref = to_ref(std::forward<M>(mu));
   auto&& sigma_ref = to_ref(std::forward<S>(sigma));
