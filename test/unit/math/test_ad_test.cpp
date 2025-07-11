4edea9fea5ab89c27f4ca50f20ccb54f70b7b72e
Steve Bronder <sbronder@flatironinstitute.org>
2025-07-11 18:24:28 -0400

    update test_ad_test to not use apply::return_t

--- test/unit/math/test_ad_test.cpp
+++ test/unit/math/test_ad_test.cpp
@@ -274,7 +274,7 @@ struct baz_fun {
 };
 
 template <typename T>
-inline typename stan::math::apply_scalar_unary<baz_fun, T>::return_t baz(
+inline auto baz(
     const T& x) {
   return stan::math::apply_scalar_unary<baz_fun, T>::apply(x);
 }
