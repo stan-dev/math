451713317b733f9458cdfbab7c21973a127d93e1
Stan Jenkins <mc.stanislaw@gmail.com>
2025-07-16 15:16:24 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- stan/math/rev/fun/norm2.hpp
+++ stan/math/rev/fun/norm2.hpp
@@ -33,7 +33,8 @@ inline var norm2(const Container& x) {
 /**
  * Returns the L2 norm of a `var_value<Vector>`.
  *
- * @tparam Container `var_value<>` whose inner type has one compile-time row or column.
+ * @tparam Container `var_value<>` whose inner type has one compile-time row or
+ * column.
  * @param[in] x Vector.
  * @return L2 norm of x.
  */
