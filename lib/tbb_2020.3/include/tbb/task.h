1ea4e083bbb1870adcbec5e707267d8c7981e94b
Brian Ward <bward@flatironinstitute.org>
2025-07-18 10:47:59 -0400

    Try: Mark some parts of task.h as constexpr for clang 21.1

--- lib/tbb_2020.3/include/tbb/task.h
+++ lib/tbb_2020.3/include/tbb/task.h
@@ -586,10 +586,10 @@ private:
     friend class task;
     friend class internal::allocate_root_with_context_proxy;
 
-    static const kind_type binding_required = bound;
-    static const kind_type binding_completed = kind_type(bound+1);
-    static const kind_type detached = kind_type(binding_completed+1);
-    static const kind_type dying = kind_type(detached+1);
+    static constexpr kind_type binding_required = bound;
+    static constexpr kind_type binding_completed = kind_type(bound+1);
+    static constexpr kind_type detached = kind_type(binding_completed+1);
+    static constexpr kind_type dying = kind_type(detached+1);
 
     //! Propagates any state change detected to *this, and as an optimisation possibly also upward along the heritage line.
     template <typename T>
