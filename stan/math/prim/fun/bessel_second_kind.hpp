58a6d4e93650065ce2e6b463ac62cf09d9dccea0
Steve Bronder <sbronder@flatironinstitute.org>
2025-08-15 12:25:32 -0400

    fix template requires for bessel_second_kind

--- stan/math/prim/fun/bessel_second_kind.hpp
+++ stan/math/prim/fun/bessel_second_kind.hpp
@@ -53,7 +53,7 @@ inline T2 bessel_second_kind(int v, const T2 z) {
  * @return Bessel second kind function applied to the two inputs.
  */
 template <typename T1, typename T2, require_any_container_t<T1, T2>* = nullptr,
-          require_not_var_matrix_t<T2>* = nullptr>
+          require_not_var_t<T2>* = nullptr>
 inline auto bessel_second_kind(T1&& a, T2&& b) {
   return apply_scalar_binary(
       [](auto&& c, auto&& d) {
