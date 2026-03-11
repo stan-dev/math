bd44b692d34130a137a2b88713408d40c0667a26
Steve Bronder <sbronder@flatironinstitute.org>
2026-03-11 16:47:21 -0400

    update stan repo with minor fixes

--- stan/math/opencl/prim/normal_lcdf.hpp
+++ stan/math/opencl/prim/normal_lcdf.hpp
@@ -156,7 +156,9 @@ const char opencl_normal_lcdf_ldncdf_impl[] = STRINGIFY(
                - 0.0170137970924080 * normal_lcdf_deriv_scaled_diff
                - 0.0100428567412041;
       }
-    } else { normal_ldncdf = INFINITY; });
+    } else {
+      normal_ldncdf = INFINITY;
+    });
 }  // namespace internal
 
 /** \ingroup opencl
