6e1dc0af543749daaa7a84e813bf3ad51fd55b09
Daniel Lee <bearlee@alum.mit.edu>
2026-03-11 22:20:17 -0400

    explicit types

--- stan/math/prim/fun/log_gamma_q_dgamma.hpp
+++ stan/math/prim/fun/log_gamma_q_dgamma.hpp
@@ -39,23 +39,23 @@ inline return_type_t<T_a, T_z> log_q_gamma_cf(const T_a& a, const T_z& z,
                                               double precision = 1.49012e-08,
                                               int max_steps = 250) {
   using T_return = return_type_t<T_a, T_z>;
-  const auto log_prefactor = a * log(z) - z - lgamma(a);
+  const T_return log_prefactor = a * log(z) - z - lgamma(a);
 
-  auto b_init = z + 1.0 - a;
-  auto C = (fabs(value_of(b_init)) >= EPSILON) ? b_init : std::decay_t<decltype(b_init)>(EPSILON);
-  auto D = 0.0;
-  auto f = C;
+  T_return b_init = z + 1.0 - a;
+  T_return C = (fabs(value_of(b_init)) >= EPSILON) ? b_init : std::decay_t<decltype(b_init)>(EPSILON);
+  T_return D = 0.0;
+  T_return f = C;
   for (int i = 1; i <= max_steps; ++i) {
     T_a an = -i * (i - a);
-    const auto b = b_init + 2.0 * i;
+    const T_return b = b_init + 2.0 * i;
     D = b + an * D;
     D = (fabs(value_of(D)) >= EPSILON) ? D : std::decay_t<decltype(D)>(EPSILON);
     C = b + an / C;
     C = (fabs(value_of(C)) >= EPSILON) ? C : std::decay_t<decltype(C)>(EPSILON);
     D = inv(D);
-    auto delta = C * D;
+    const T_return delta = C * D;
     f *= delta;
-    const auto delta_m1 = fabs(value_of(delta) - 1.0);
+    const double delta_m1 = fabs(value_of(delta) - 1.0);
     if (delta_m1 < precision) {
       break;
     }
@@ -83,30 +83,30 @@ inline return_type_t<T_a, T_z> log_q_gamma_cf(const T_a& a, const T_z& z,
  * @return structure containing log(Q(a,z)) and d/da log(Q(a,z))
  */
 template <typename T_a, typename T_z>
-inline auto log_gamma_q_dgamma(
+inline std::pair<return_type_t<T_a, T_z>, return_type_t<T_a, T_z>> log_gamma_q_dgamma(
     const T_a& a, const T_z& z, double precision = 1.49012e-08, int max_steps = 250) {
   using T_return = return_type_t<T_a, T_z>;
-  const auto a_val = value_of(a);
-  const auto z_val = value_of(z);
+  const double a_val = value_of(a);
+  const double z_val = value_of(z);
   // For z > a + 1, use continued fraction for better numerical stability
   if (z_val > a_val + 1.0) {
     std::pair<T_return, T_return> result{internal::log_q_gamma_cf(a_val, z_val, precision, max_steps), 0.0};
     // For gradient, use: d/da log(Q) = (1/Q) * dQ/da
     // grad_reg_inc_gamma computes dQ/da
-    const auto Q_val = exp(result.first);
-    const auto dQ_da
+    const T_return Q_val = exp(result.first);
+    const double dQ_da
         = grad_reg_inc_gamma(a_val, z_val, tgamma(a_val), digamma(a_val));
     result.second = dQ_da / Q_val;
     return result;
   } else {
     // For z <= a + 1, use log1m(P(a,z)) for better numerical accuracy
-    const auto P_val = gamma_p(a_val, z_val);
+    const double P_val = gamma_p(a_val, z_val);
     std::pair<T_return, T_return> result{log1m(P_val), 0.0};
     // Gradient: d/da log(Q) = (1/Q) * dQ/da
     // grad_reg_inc_gamma computes dQ/da
-    const auto Q_val = exp(result.first);
+    const T_return Q_val = exp(result.first);
     if (Q_val > 0) {
-      const auto dQ_da
+      const double dQ_da
           = grad_reg_inc_gamma(a_val, z_val, tgamma(a_val), digamma(a_val));
       result.second = dQ_da / Q_val;
     } else {
