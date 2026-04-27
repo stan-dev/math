0d66c6f4bafbe2cebb29574f65f45a5c7e9a3b0c
Andre Zapico <andrezapico@gmail.com>
2026-04-27 06:26:27 -0400

    try no threading

--- test/unit/math/prim/fun/exp_test.cpp
+++ test/unit/math/prim/fun/exp_test.cpp
@@ -66,6 +66,42 @@ TEST(MathFunctions, expVecBench) {
 }
 
 #else
+
+TEST(MathFunctions, expVecBench) {
+  std::cout << "NO THREADING\n";
+  // std timing includes
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::duration;
+  using std::chrono::milliseconds;
+
+  // stan math includes
+  using stan::math::exp;
+  size_t N = 10000; // we're computing exp 10000 times but  scaling number of threads
+  // scaling Nthreads by squares N, N^2, N^3
+  std::cout << "N,noThreads,msInt,msDouble\n";
+  std::vector<double> vec(N);
+  for (size_t i = 0; i < N; ++i) {
+    vec[i] = i + 1;
+  }
+  std::vector<double> vec_test;
+  
+  auto t1 = high_resolution_clock::now();
+  EXPECT_NO_THROW(vec_test = stan::math::exp(vec));
+  auto t2 = high_resolution_clock::now();
+
+  /* Getting number of milliseconds as an integer. */
+  auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+  /* Getting number of milliseconds as a double. */
+  duration<double, std::milli> ms_double = t2 - t1;
+  
+  std::cout << N << ",";
+  std::cout << "NA" << ",";
+  std::cout << ms_int.count() << "ms,";
+  std::cout << ms_double.count() << "ms\n";
+}
+
 TEST(MathFunctions, expInt) {
   using stan::math::exp;
   EXPECT_FLOAT_EQ(std::exp(3), exp(3));
