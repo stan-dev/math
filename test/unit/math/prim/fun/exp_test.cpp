2db147491367b4329ea4bdb3e7708ccbc0bb98cf
Andre Zapico <andrezapico@gmail.com>
2026-04-27 06:05:03 -0400

    begin benchmarks

--- test/unit/math/prim/fun/exp_test.cpp
+++ test/unit/math/prim/fun/exp_test.cpp
@@ -11,7 +11,7 @@ TEST(MathFunctions, expInt) {
   EXPECT_FLOAT_EQ(std::exp(3.0), exp(3.0));
 }
 
-TEST(MathFunctions, expVec) {
+TEST(MathFunctions, expVecN100) {
   using stan::math::exp;
   size_t N = 100;
   std::vector<double> vec(N);
@@ -20,14 +20,49 @@ TEST(MathFunctions, expVec) {
   }
   std::vector<double> vec_test;
   EXPECT_NO_THROW(vec_test = stan::math::exp_test(vec));
+  for (size_t i = 0; i < N; ++i) {
+    EXPECT_FLOAT_EQ(std::exp(i + 1), vec_test[i]);
+  }
+}
 
-  // std::vector<double> vec_test;
-  // vec_test = stan::math::exp_test(vec);
-  // for (size_t i = 0; i < N; ++i) {
-  //   std::cout << vec_test[i] << "\n";
-  //   std::cout << std::exp(i + 1) << "\n";
-  //   //EXPECT_FLOAT_EQ(std::exp(i + 1), vec_test[i]);
-  // }
+TEST(MathFunctions, expVecBench) {
+  // std timing includes
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::duration;
+  using std::chrono::milliseconds;
+
+  // stan math includes
+  using stan::math::exp;
+  using stan::math::init_threadpool_tbb;
+  size_t N = 10000; // we're computing exp 10000 times but  scaling number of threads
+  // scaling Nthreads by squares N, N^2, N^3
+  std::cout << "N,nThreads,msInt,msDouble\n";
+  for (int i = 1; i < 10; ++i) { 
+    size_t Nthreads = 2;
+    Nthreads  = std::pow(Nthreads, i);
+    stan::math::init_threadpool_tbb(Nthreads);
+    std::vector<double> vec(N);
+    for (size_t i = 0; i < N; ++i) {
+      vec[i] = i + 1;
+    }
+    std::vector<double> vec_test;
+    
+    auto t1 = high_resolution_clock::now();
+    EXPECT_NO_THROW(vec_test = stan::math::exp_test(vec));
+    auto t2 = high_resolution_clock::now();
+
+	/* Getting number of milliseconds as an integer. */
+	auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+	/* Getting number of milliseconds as a double. */
+	duration<double, std::milli> ms_double = t2 - t1;
+
+	std::cout << N << ",";
+	std::cout << Nthreads << ",";
+	std::cout << ms_int.count() << "ms,";
+	std::cout << ms_double.count() << "ms\n";
+  }
 }
 
 #else
