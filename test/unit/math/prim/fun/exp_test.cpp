c539a38da41f2ac37247ef1c40cc93e1726a382a
Andre Zapico <andrezapico@gmail.com>
2026-04-27 23:24:28 -0400

    add only 10 test for multithreading

--- test/unit/math/prim/fun/exp_test.cpp
+++ test/unit/math/prim/fun/exp_test.cpp
@@ -104,6 +104,47 @@ TEST(MathFunctions, expVecBench_10000000_17) {
   }
 }
 
+TEST(MathFunctions, expVecBench_10000000_17_all10) {
+  // std timing includes
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::duration;
+  using std::chrono::milliseconds;
+
+  // stan math includes
+  using stan::math::exp;
+  using stan::math::init_threadpool_tbb;
+  size_t N = 10000000; // we're computing exp 10000 times but  scaling number of threads
+  // scaling Nthreads by squares N, N^2, N^3
+  std::cout << "all 10\n";
+  std::cout << "N,nThreads,msInt,msDouble\n";
+  for (int i = 1; i < 17; ++i) { 
+    size_t Nthreads = 2;
+    Nthreads  = std::pow(Nthreads, i);
+    stan::math::init_threadpool_tbb(Nthreads);
+    std::vector<double> vec(N);
+    for (size_t i = 0; i < N; ++i) {
+      vec[i] = 10;
+    }
+    std::vector<double> vec_test;
+    
+    auto t1 = high_resolution_clock::now();
+    EXPECT_NO_THROW(vec_test = stan::math::exp_test(vec));
+    auto t2 = high_resolution_clock::now();
+
+	/* Getting number of milliseconds as an integer. */
+	auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+	/* Getting number of milliseconds as a double. */
+	duration<double, std::milli> ms_double = t2 - t1;
+
+	std::cout << N << ",";
+	std::cout << Nthreads << ",";
+	std::cout << ms_int.count() << "ms,";
+	std::cout << ms_double.count() << "ms\n";
+  }
+}
+
 
 #else
 
