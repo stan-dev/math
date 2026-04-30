94db16230ed2057c7194904ad2a6b4b754a1f4d7
Andre Zapico <andrezapico@gmail.com>
2026-04-30 07:36:15 -0400

    fix return to satisfy compiler

--- stan/math/prim/fun/exp.hpp
+++ stan/math/prim/fun/exp.hpp
@@ -73,10 +73,11 @@ class apply_exp {
  public:
   Container operator()(const tbb::blocked_range<std::size_t>& r) const {
     Container a = my_a;
+    Container a_out = my_a;
     for (std::size_t i = r.begin(); i != r.end(); ++i) {
-      a[i] = exp(a[i]);
+      a_out[i] = exp(a[i]);
     }
-    return a;
+    return a_out;
   }
   apply_exp<Container>(Container a) : my_a(a) {}
 };
