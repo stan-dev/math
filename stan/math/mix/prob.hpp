ebff9a78fa02644509cdde1db27f7e47f5c994bb
Florence Bockting <florence.bockting@aalto.fi>
2026-06-29 14:23:26 +0300

    chore: add laplace_latent_solve to prob.hpp

--- stan/math/mix/prob.hpp
+++ stan/math/mix/prob.hpp
@@ -5,6 +5,7 @@
 #include <stan/math/mix/prob/laplace_latent_poisson_log_rng.hpp>
 #include <stan/math/mix/prob/laplace_latent_neg_binomial_2_log_rng.hpp>
 #include <stan/math/mix/prob/laplace_latent_rng.hpp>
+#include <stan/math/mix/prob/laplace_latent_solve.hpp>
 #include <stan/math/mix/prob/laplace_marginal.hpp>
 #include <stan/math/mix/prob/laplace_marginal_neg_binomial_2_log_lpmf.hpp>
 #include <stan/math/mix/prob/laplace_marginal_bernoulli_logit_lpmf.hpp>
