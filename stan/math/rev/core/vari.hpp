883570ef0d1349243c02068cede799431f9bc175
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2026-07-22 20:31:46 +0800

    Downstream needs val_op

--- stan/math/rev/core/vari.hpp
+++ stan/math/rev/core/vari.hpp
@@ -612,7 +612,7 @@ class vari_view<
    * @return The value of this vari.
    */
   inline const auto& val() const noexcept { return val_; }
-  inline auto& val() noexcept { return val_; }
+  inline auto& val_op() noexcept { return val_; }
 
   /**
    * Return a reference to the derivative of the root expression with
@@ -763,7 +763,7 @@ class vari_value<T, require_all_t<is_plain_type<T>, is_eigen_dense_base<T>>>
    * @return The value of this vari.
    */
   inline const auto& val() const noexcept { return val_; }
-  inline auto& val() noexcept { return val_; }
+  inline auto& val_op() noexcept { return val_; }
 
   /**
    * Return a reference to the derivative of the root expression with
@@ -927,7 +927,7 @@ class vari_value<T, require_eigen_sparse_base_t<T>> : public vari_base {
    * @return The value of this vari.
    */
   inline const auto& val() const { return val_; }
-  inline auto& val() { return val_; }
+  inline auto& val_op() { return val_; }
 
   /**
    * Return a reference to the derivative of the root expression with
