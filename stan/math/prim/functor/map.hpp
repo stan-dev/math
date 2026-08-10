adc33cb993a039a33cb75a45b01f0e363c6940e9
Florence Bockting <florence.bockting@aalto.fi>
2026-08-10 16:36:33 +0300

    fix: apply review requests regarding API, style, and checks

--- stan/math/prim/functor/map.hpp
+++ stan/math/prim/functor/map.hpp
@@ -2,6 +2,7 @@
 #define STAN_MATH_PRIM_FUNCTOR_MAP_HPP
 
 #include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/prim/fun/eval.hpp>
 #include <stan/math/prim/fun/to_ref.hpp>
 #include <stan/math/prim/meta.hpp>
 #include <stan/math/prim/err/check_consistent_sizes.hpp>
@@ -63,51 +64,26 @@ inline auto map(F&& f, T&& x, Args&&... args) {
  * @throw std::invalid_argument if the inputs have different sizes
  */
 template <typename F, typename Tuple, typename... Args,
-          require_tuple_t<Tuple>* = nullptr>
+          require_all_tuple_elements_t<is_std_vector, Tuple>* = nullptr>
 inline auto mapN(F&& f, Tuple&& xs, Args&&... args) {
-  static_assert(tuple_size_v<Tuple> >= 2,
-                "mapN requires a tuple of at least two std::vector inputs.");
   static constexpr const char* function = "mapN";
 
   return apply(
-      [&](auto&&... xs) {
-        static_assert((is_std_vector<std::decay_t<decltype(xs)>>::value && ...),
-                      "mapN tuple elements must be std::vector.");
-        check_consistent_sizes(function, xs...);
+      [&f, &args...](auto&&... xs_inner) {
+        check_consistent_sizes(function, xs_inner...);
 
-        const std::size_t n = std::get<0>(std::forward_as_tuple(xs...)).size();
-        using T_return = std::decay_t<decltype(f((xs[0])..., args...))>;
+        const std::size_t n
+            = std::get<0>(std::forward_as_tuple(xs_inner...)).size();
+        using T_return = std::decay_t<decltype(f((xs_inner[0])..., args...))>;
         std::vector<T_return> result;
         result.reserve(n);
 
         for (std::size_t i = 0; i < n; ++i) {
-          result.push_back(f((xs[i])..., args...));
+          result.push_back(f((xs_inner[i])..., args...));
         }
         return result;
       },
       std::forward<Tuple>(xs));
-}
-
-/**
- * Return a std::vector whose i-th element is `f(x1[i], x2[i], ...)`.
- * Convenience overload equivalent to
- * `mapN(f, std::forward_as_tuple(x1, x2, xs...))`.
- *
- * @tparam F type of functor
- * @tparam T1 type of first std::vector
- * @tparam T2 type of second std::vector
- * @tparam Ts types of additional std::vector inputs
- * @param[in] f functor applied to each tuple of elements
- * @param[in] x1 first std::vector
- * @param[in] x2 second std::vector
- * @param[in] xs additional std::vector inputs
- * @return std::vector of results
- * @throw std::invalid_argument if the inputs have different sizes
- */
-template <typename F, typename T1, typename T2, typename... Ts,
-          require_all_std_vector_t<T1, T2, Ts...>* = nullptr>
-inline auto mapN(F&& f, T1&& x1, T2&& x2, Ts&&... xs) {
-  return mapN(std::forward<F>(f), std::forward_as_tuple(x1, x2, xs...));
 }
 
 /**
@@ -130,26 +106,28 @@ template <typename F, typename T, typename... Args,
 inline auto row_map(F&& f, T&& m, Args&&... args) {
   static constexpr const char* function = "row_map";
   decltype(auto) m_ref = to_ref(std::forward<T>(m));
-  using result_row_t = plain_type_t<decltype(f(m_ref.row(0), args...))>;
-  using T_return = scalar_type_t<result_row_t>;
-  using matrix_t = Eigen::Matrix<T_return, Eigen::Dynamic, Eigen::Dynamic>;
+
+  using matrix_t = Eigen::Matrix<
+      scalar_type_t<plain_type_t<decltype(f(m_ref.row(0), args...))>>,
+      Eigen::Dynamic, Eigen::Dynamic>;
 
   const Eigen::Index n_rows = m_ref.rows();
   if (n_rows == 0) {
     return matrix_t(0, 0);
   }
 
-  result_row_t first_row = f(m_ref.row(0), args...);
-  if (first_row.size() == 0) {
+  auto row_i = eval(f(m_ref.row(0), args...));
+
+  if (row_i.size() == 0) {
     return matrix_t(0, 0);
   }
-  matrix_t result(n_rows, first_row.cols());
-  result.row(0) = first_row;
+  matrix_t result(n_rows, row_i.cols());
+  result.row(0) = row_i;
   for (Eigen::Index i = 1; i < n_rows; ++i) {
-    result_row_t row = f(m_ref.row(i), args...);
+    row_i = f(m_ref.row(i), args...);
     check_size_match(function, "columns of result", result.cols(),
-                     "columns of returned row", row.cols());
-    result.row(i) = row;
+                     "columns of returned row", row_i.cols());
+    result.row(i) = row_i;
   }
   return result;
 }
@@ -174,26 +152,28 @@ template <typename F, typename T, typename... Args,
 inline auto col_map(F&& f, T&& m, Args&&... args) {
   static constexpr const char* function = "col_map";
   decltype(auto) m_ref = to_ref(std::forward<T>(m));
-  using result_col_t = plain_type_t<decltype(f(m_ref.col(0), args...))>;
-  using T_return = scalar_type_t<result_col_t>;
-  using matrix_t = Eigen::Matrix<T_return, Eigen::Dynamic, Eigen::Dynamic>;
+
+  using matrix_t = Eigen::Matrix<
+      scalar_type_t<plain_type_t<decltype(f(m_ref.col(0), args...))>>,
+      Eigen::Dynamic, Eigen::Dynamic>;
 
   const Eigen::Index n_cols = m_ref.cols();
   if (n_cols == 0) {
     return matrix_t(0, 0);
   }
 
-  result_col_t first_col = f(m_ref.col(0), args...);
-  if (first_col.size() == 0) {
+  auto col_j = eval(f(m_ref.col(0), args...));
+
+  if (col_j.size() == 0) {
     return matrix_t(0, 0);
   }
-  matrix_t result(first_col.rows(), n_cols);
-  result.col(0) = first_col;
+  matrix_t result(col_j.rows(), n_cols);
+  result.col(0) = col_j;
   for (Eigen::Index j = 1; j < n_cols; ++j) {
-    result_col_t col = f(m_ref.col(j), args...);
+    col_j = f(m_ref.col(j), args...);
     check_size_match(function, "rows of result", result.rows(),
-                     "rows of returned column", col.rows());
-    result.col(j) = col;
+                     "rows of returned column", col_j.rows());
+    result.col(j) = col_j;
   }
   return result;
 }
@@ -219,13 +199,10 @@ inline auto col_map(F&& f, T&& m, Args&&... args) {
 template <typename F, typename Tuple, typename... Args,
           require_tuple_t<Tuple>* = nullptr>
 inline auto row_mapN(F&& f, Tuple&& ms, Args&&... args) {
-  static_assert(tuple_size_v<Tuple> >= 2,
-                "row_mapN requires a tuple of at least two Eigen matrix "
-                "inputs.");
   static constexpr const char* function = "row_mapN";
 
   return apply(
-      [&](auto&&... mats) {
+      [&f, &args...](auto&&... mats) {
         static_assert(
             (is_eigen_matrix_dynamic<std::decay_t<decltype(mats)>>::value
              && ...),
@@ -234,58 +211,36 @@ inline auto row_mapN(F&& f, Tuple&& ms, Args&&... args) {
 
         auto m_refs = std::tuple{to_ref(std::forward<decltype(mats)>(mats))...};
         const Eigen::Index n_rows = std::get<0>(m_refs).rows();
-        using result_row_t = plain_type_t<decltype(f(
-            (std::declval<ref_type_t<decltype(mats)>>().row(0))..., args...))>;
-        using T_return = scalar_type_t<result_row_t>;
-        using matrix_t
-            = Eigen::Matrix<T_return, Eigen::Dynamic, Eigen::Dynamic>;
-
-        if (n_rows == 0) {
-          return matrix_t(0, 0);
-        }
 
         return apply(
-            [&](auto&&... mrs) {
-              result_row_t first_row = f((mrs.row(0))..., args...);
-              if (first_row.size() == 0) {
+            [&f, &args..., &n_rows](auto&&... mrs) {
+              using matrix_t
+                  = Eigen::Matrix<scalar_type_t<plain_type_t<decltype(f(
+                                      (mrs.row(0))..., args...))>>,
+                                  Eigen::Dynamic, Eigen::Dynamic>;
+
+              if (n_rows == 0) {
                 return matrix_t(0, 0);
               }
-              matrix_t result(n_rows, first_row.cols());
-              result.row(0) = first_row;
+
+              auto row_i = eval(f((mrs.row(0))..., args...));
+
+              if (row_i.size() == 0) {
+                return matrix_t(0, 0);
+              }
+              matrix_t result(n_rows, row_i.cols());
+              result.row(0) = row_i;
               for (Eigen::Index i = 1; i < n_rows; ++i) {
-                result_row_t row = f((mrs.row(i))..., args...);
+                row_i = f((mrs.row(i))..., args...);
                 check_size_match(function, "columns of result", result.cols(),
-                                 "columns of returned row", row.cols());
-                result.row(i) = row;
+                                 "columns of returned row", row_i.cols());
+                result.row(i) = row_i;
               }
               return result;
             },
             m_refs);
       },
       std::forward<Tuple>(ms));
-}
-
-/**
- * Return a matrix whose i-th row is `f(m1.row(i), m2.row(i), ...)`.
- * Convenience overload equivalent to
- * `row_mapN(f, std::forward_as_tuple(m1, m2, ms...))`.
- *
- * @tparam F type of functor
- * @tparam T1 type of first Eigen matrix
- * @tparam T2 type of second Eigen matrix
- * @tparam Ts types of additional Eigen matrix inputs
- * @param[in] f functor applied to each tuple of rows
- * @param[in] m1 first Eigen matrix
- * @param[in] m2 second Eigen matrix
- * @param[in] ms additional Eigen matrix inputs
- * @return matrix of results
- * @throw std::invalid_argument if the inputs have different dimensions or
- * returned rows have inconsistent sizes
- */
-template <typename F, typename T1, typename T2, typename... Ts,
-          require_all_eigen_matrix_dynamic_t<T1, T2, Ts...>* = nullptr>
-inline auto row_mapN(F&& f, T1&& m1, T2&& m2, Ts&&... ms) {
-  return row_mapN(std::forward<F>(f), std::forward_as_tuple(m1, m2, ms...));
 }
 
 /**
@@ -309,13 +264,10 @@ inline auto row_mapN(F&& f, T1&& m1, T2&& m2, Ts&&... ms) {
 template <typename F, typename Tuple, typename... Args,
           require_tuple_t<Tuple>* = nullptr>
 inline auto col_mapN(F&& f, Tuple&& ms, Args&&... args) {
-  static_assert(tuple_size_v<Tuple> >= 2,
-                "col_mapN requires a tuple of at least two Eigen matrix "
-                "inputs.");
   static constexpr const char* function = "col_mapN";
 
   return apply(
-      [&](auto&&... mats) {
+      [&f, &args...](auto&&... mats) {
         static_assert(
             (is_eigen_matrix_dynamic<std::decay_t<decltype(mats)>>::value
              && ...),
@@ -324,58 +276,36 @@ inline auto col_mapN(F&& f, Tuple&& ms, Args&&... args) {
 
         auto m_refs = std::tuple{to_ref(std::forward<decltype(mats)>(mats))...};
         const Eigen::Index n_cols = std::get<0>(m_refs).cols();
-        using result_col_t = plain_type_t<decltype(f(
-            (std::declval<ref_type_t<decltype(mats)>>().col(0))..., args...))>;
-        using T_return = scalar_type_t<result_col_t>;
-        using matrix_t
-            = Eigen::Matrix<T_return, Eigen::Dynamic, Eigen::Dynamic>;
-
-        if (n_cols == 0) {
-          return matrix_t(0, 0);
-        }
 
         return apply(
-            [&](auto&&... mrs) {
-              result_col_t first_col = f((mrs.col(0))..., args...);
-              if (first_col.size() == 0) {
+            [&f, &args..., &n_cols](auto&&... mrs) {
+              using matrix_t
+                  = Eigen::Matrix<scalar_type_t<plain_type_t<decltype(f(
+                                      (mrs.col(0))..., args...))>>,
+                                  Eigen::Dynamic, Eigen::Dynamic>;
+
+              if (n_cols == 0) {
                 return matrix_t(0, 0);
               }
-              matrix_t result(first_col.rows(), n_cols);
-              result.col(0) = first_col;
+
+              auto col_j = eval(f((mrs.col(0))..., args...));
+
+              if (col_j.size() == 0) {
+                return matrix_t(0, 0);
+              }
+              matrix_t result(col_j.rows(), n_cols);
+              result.col(0) = col_j;
               for (Eigen::Index j = 1; j < n_cols; ++j) {
-                result_col_t col = f((mrs.col(j))..., args...);
+                col_j = f((mrs.col(j))..., args...);
                 check_size_match(function, "rows of result", result.rows(),
-                                 "rows of returned column", col.rows());
-                result.col(j) = col;
+                                 "rows of returned column", col_j.rows());
+                result.col(j) = col_j;
               }
               return result;
             },
             m_refs);
       },
       std::forward<Tuple>(ms));
-}
-
-/**
- * Return a matrix whose j-th column is `f(m1.col(j), m2.col(j), ...)`.
- * Convenience overload equivalent to
- * `col_mapN(f, std::forward_as_tuple(m1, m2, ms...))`.
- *
- * @tparam F type of functor
- * @tparam T1 type of first Eigen matrix
- * @tparam T2 type of second Eigen matrix
- * @tparam Ts types of additional Eigen matrix inputs
- * @param[in] f functor applied to each tuple of columns
- * @param[in] m1 first Eigen matrix
- * @param[in] m2 second Eigen matrix
- * @param[in] ms additional Eigen matrix inputs
- * @return matrix of results
- * @throw std::invalid_argument if the inputs have different dimensions or
- * returned columns have inconsistent sizes
- */
-template <typename F, typename T1, typename T2, typename... Ts,
-          require_all_eigen_matrix_dynamic_t<T1, T2, Ts...>* = nullptr>
-inline auto col_mapN(F&& f, T1&& m1, T2&& m2, Ts&&... ms) {
-  return col_mapN(std::forward<F>(f), std::forward_as_tuple(m1, m2, ms...));
 }
 
 }  // namespace math
