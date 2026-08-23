70c24edf1097e6c918b59d2fb90b2c8dab1c46fa
Gidon Frischkorn <gidon.frischkorn@uzh.ch>
2026-08-23 18:12:36 +0200

    test: cover vectorized cdf/lcdf gradients and std::vector inputs

--- test/unit/math/mix/prob/inv_gaussian_test.cpp
+++ test/unit/math/mix/prob/inv_gaussian_test.cpp
@@ -108,3 +108,56 @@ TEST_F(AgradRev, mathMixScalFun_inv_gaussian_lccdf_vectorized) {
   };
   stan::test::expect_ad(f, y, mu, lambda);
 }
+
+// The cdf's partials scale by the whole-container product, which a scalar
+// test does not exercise.
+TEST_F(AgradRev, mathMixScalFun_inv_gaussian_cdf_vectorized) {
+  Eigen::VectorXd y(3);
+  y << 0.3, 1.2, 4.0;
+  Eigen::VectorXd mu(3);
+  mu << 0.5, 1.0, 2.0;
+  Eigen::VectorXd lambda(3);
+  lambda << 2.0, 5.0, 0.7;
+
+  auto f = [](const auto& y, const auto& mu, const auto& lambda) {
+    return stan::math::inv_gaussian_cdf(y, mu, lambda);
+  };
+  stan::test::expect_ad(f, y, mu, lambda);
+}
+
+TEST_F(AgradRev, mathMixScalFun_inv_gaussian_lcdf_vectorized) {
+  Eigen::VectorXd y(3);
+  y << 0.3, 1.2, 4.0;
+  Eigen::VectorXd mu(3);
+  mu << 0.5, 1.0, 2.0;
+  Eigen::VectorXd lambda(3);
+  lambda << 2.0, 5.0, 0.7;
+
+  auto f = [](const auto& y, const auto& mu, const auto& lambda) {
+    return stan::math::inv_gaussian_lcdf(y, mu, lambda);
+  };
+  stan::test::expect_ad(f, y, mu, lambda);
+}
+
+TEST_F(AgradRev, mathMixScalFun_inv_gaussian_std_vector) {
+  std::vector<double> y{0.3, 1.2, 4.0};
+  std::vector<double> mu{0.5, 1.0, 2.0};
+  std::vector<double> lambda{2.0, 5.0, 0.7};
+
+  auto f_lpdf = [](const auto& y, const auto& mu, const auto& lambda) {
+    return stan::math::inv_gaussian_lpdf(y, mu, lambda);
+  };
+  auto f_cdf = [](const auto& y, const auto& mu, const auto& lambda) {
+    return stan::math::inv_gaussian_cdf(y, mu, lambda);
+  };
+  auto f_lcdf = [](const auto& y, const auto& mu, const auto& lambda) {
+    return stan::math::inv_gaussian_lcdf(y, mu, lambda);
+  };
+  auto f_lccdf = [](const auto& y, const auto& mu, const auto& lambda) {
+    return stan::math::inv_gaussian_lccdf(y, mu, lambda);
+  };
+  stan::test::expect_ad(f_lpdf, y, mu, lambda);
+  stan::test::expect_ad(f_cdf, y, mu, lambda);
+  stan::test::expect_ad(f_lcdf, y, mu, lambda);
+  stan::test::expect_ad(f_lccdf, y, mu, lambda);
+}
