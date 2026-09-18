6aff1cd2f0d9d831a155f7591f49418044900190
Aki Vehtari <Aki.Vehtari@aalto.fi>
2026-09-18 22:15:46 +0300

    Keep the previous OpenCL erfcx device function, which is 8 to 13 percent faster

--- stan/math/opencl/kernels/device_functions/erfcx.hpp
+++ stan/math/opencl/kernels/device_functions/erfcx.hpp
@@ -88,41 +88,24 @@ static constexpr const char* erfcx_device_function
            * @param y argument
            * @return scaled complementary error function
            */
-          inline double erfcx_cody_middle(double y) {
-            const double p[] = {1230.33935479799725, 2051.07837782607147,
-                                1712.04761263407058, 881.952221241769090,
-                                298.635138197400131, 66.1191906371416295,
-                                8.88314979438837594, 5.64188496988670089e-1};
-
-            const double q[] = {
-                1230.33935480374942, 3439.36767414372164, 4362.61909014324716,
-                3290.79923573345963, 1621.38957456669019, 537.181101862009858,
-                117.693950891312499, 15.7449261107098347,
-            };
-
-            const double y2 = y * y;
-
-            const double p01 = fma(p[1], y, p[0]);
-            const double p23 = fma(p[3], y, p[2]);
-            const double p45 = fma(p[5], y, p[4]);
-            const double p67 = fma(p[7], y, p[6]);
-
-            const double q01 = fma(q[1], y, q[0]);
-            const double q23 = fma(q[3], y, q[2]);
-            const double q45 = fma(q[5], y, q[4]);
-            const double q67 = fma(q[7], y, q[6]);
-
-            double num = fma(2.15311535474403846e-8, y2, p67);
-            num = fma(num, y2, p45);
-            num = fma(num, y2, p23);
-            num = fma(num, y2, p01);
-
-            double den = y2 + q67;
-            den = fma(den, y2, q45);
-            den = fma(den, y2, q23);
-            den = fma(den, y2, q01);
-
-            return num / den;
+          double erfcx_cody_middle(double y) {
+            double p = 2.15311535474403846e-8 * y;
+            p = (p + 5.64188496988670089e-1) * y;
+            p = (p + 8.88314979438837594) * y;
+            p = (p + 66.1191906371416295) * y;
+            p = (p + 298.635138197400131) * y;
+            p = (p + 881.952221241769090) * y;
+            p = (p + 1712.04761263407058) * y;
+            p = (p + 2051.07837782607147) * y;
+            double q = y;
+            q = (q + 15.7449261107098347) * y;
+            q = (q + 117.693950891312499) * y;
+            q = (q + 537.181101862009858) * y;
+            q = (q + 1621.38957456669019) * y;
+            q = (q + 3290.79923573345963) * y;
+            q = (q + 4362.61909014324716) * y;
+            q = (q + 3439.36767414372164) * y;
+            return (p + 1230.33935479799725) / (q + 1230.33935480374942);
           }
 
           /** \ingroup opencl_kernels
@@ -134,49 +117,26 @@ static constexpr const char* erfcx_device_function
            * @param x argument
            * @return scaled complementary error function
            */
-          inline double erfcx_small(double x) {
-            const double p[] = {1.0,
-                                -1.12837916709551256,
-                                1.0,
-                                -7.52252778063651983e-01,
-                                4.99999999999992839e-01,
-                                -3.00901111227312890e-01,
-                                1.66666666667239644e-01,
-                                -8.59717459974174147e-02,
-                                4.16666666458337179e-02,
-                                -1.91048337772546720e-02,
-                                8.33333374332981443e-03,
-                                -3.47359067853470795e-03,
-                                1.38888415444527033e-03,
-                                -5.34506929034156810e-04,
-                                1.98445679338826757e-04,
-                                -7.08163358203131886e-05,
-                                2.46655529768908249e-05,
-                                -9.35890030086883823e-06};
-
-            const double x2 = x * x;
-
-            // Independent linear terms.
-            const double p01 = fma(p[1], x, p[0]);
-            const double p23 = fma(p[3], x, p[2]);
-            const double p45 = fma(p[5], x, p[4]);
-            const double p67 = fma(p[7], x, p[6]);
-            const double p89 = fma(p[9], x, p[8]);
-            const double p1011 = fma(p[11], x, p[10]);
-            const double p1213 = fma(p[13], x, p[12]);
-            const double p1415 = fma(p[15], x, p[14]);
-            const double p1617 = fma(p[17], x, p[16]);
-
-            // Combine using Horner's method in x².
-            double result = fma(3.05977060678449757e-06, x2, p1617);
-            result = fma(result, x2, p1415);
-            result = fma(result, x2, p1213);
-            result = fma(result, x2, p1011);
-            result = fma(result, x2, p89);
-            result = fma(result, x2, p67);
-            result = fma(result, x2, p45);
-            result = fma(result, x2, p23);
-            return fma(result, x2, p01);
+          double erfcx_small(double x) {
+            double p = 3.05977060678449757e-06;
+            p = -9.35890030086883823e-06 + x * p;
+            p = 2.46655529768908249e-05 + x * p;
+            p = -7.08163358203131886e-05 + x * p;
+            p = 1.98445679338826757e-04 + x * p;
+            p = -5.34506929034156810e-04 + x * p;
+            p = 1.38888415444527033e-03 + x * p;
+            p = -3.47359067853470795e-03 + x * p;
+            p = 8.33333374332981443e-03 + x * p;
+            p = -1.91048337772546720e-02 + x * p;
+            p = 4.16666666458337179e-02 + x * p;
+            p = -8.59717459974174147e-02 + x * p;
+            p = 1.66666666667239644e-01 + x * p;
+            p = -3.00901111227312890e-01 + x * p;
+            p = 4.99999999999992839e-01 + x * p;
+            p = -7.52252778063651983e-01 + x * p;
+            p = 1.0 + x * p;
+            p = -1.12837916709551256 + x * p;
+            return 1.0 + x * p;
           }
 
           /** \ingroup opencl_kernels
@@ -205,11 +165,14 @@ static constexpr const char* erfcx_device_function
           double erfcx(double x) {
             if (x >= 4.0) {
               return erfcx_cody_tail(x);
-            } else if (x >= 0.46875) {
+            }
+            if (x >= 0.46875) {
               return erfcx_cody_middle(x);
-            } else if (x > -0.46875) {
+            }
+            if (x > -0.46875) {
               return erfcx_small(x);
-            } else if (x < -27.0) {
+            }
+            if (x < -27.0) {
               return INFINITY;
             }
             double h = x * x;
