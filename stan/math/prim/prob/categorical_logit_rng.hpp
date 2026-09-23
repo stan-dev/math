d83319cf1863787ed57d749c8f2b01e8694508f5
Rajit <rajitsareen@gmail.com>
2026-09-23 16:59:44 -0500

    updated categorical_logit_rng and tests to accept infinite values

--- stan/math/prim/prob/categorical_logit_rng.hpp
+++ stan/math/prim/prob/categorical_logit_rng.hpp
@@ -6,6 +6,7 @@
 #include <stan/math/prim/fun/cumulative_sum.hpp>
 #include <stan/math/prim/fun/softmax.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
+#include <stan/math/prim/fun/constants.hpp>
 #include <boost/random/uniform_01.hpp>
 #include <boost/random/variate_generator.hpp>
 
@@ -24,16 +25,30 @@ namespace math {
  * @param beta Vector of unnormalized log probabilities.
  * @param rng Pseudo-random number generator.
  * @return Categorical random variate
+ * @throw std::domain_error if beta contains NaN
+ * @throw std::domain_error if every entry of beta is negative infinity
  */
 template <class RNG>
 inline int categorical_logit_rng(const Eigen::VectorXd& beta, RNG& rng) {
   using boost::uniform_01;
   using boost::variate_generator;
   static constexpr const char* function = "categorical_logit_rng";
-  check_finite(function, "Log odds parameter", beta);
+  check_not_nan(function, "Log odds parameter", beta);
+  check_nonzero_size(function, "Log odds parameter", beta);
+  check_greater(function, "Log odds parameter", beta.maxCoeff(),
+                NEGATIVE_INFTY);
+
+  auto is_pos_inf = (beta.array() == INFTY);
+  int num_infty = is_pos_inf.count();
+
+  Eigen::VectorXd theta;
+  if (num_infty > 0) {
+    theta = is_pos_inf.template cast<double>() / num_infty;
+  } else {
+    theta = softmax(beta);
+  }
 
   variate_generator<RNG&, uniform_01<> > uniform01_rng(rng, uniform_01<>());
-  Eigen::VectorXd theta = softmax(beta);
   Eigen::VectorXd index = cumulative_sum(theta);
 
   double c = uniform01_rng();
